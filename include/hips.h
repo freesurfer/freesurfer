1a5734ea107bcb5c639340be879274173b212c48
gio <gio@nmr.mgh.harvard.edu>
1996-03-04 16:38:52 +0000

    added logmap prototype

--- include/hips.h
+++ include/hips.h
@@ -27,5 +27,9 @@ int h_reduce(struct header *hdi, struct header *hdo, float xf, float yf) ;
 int h_flipquad(struct header *hdi, struct header *hdo) ;
 int h_rot180(struct header *hdi, struct header *hdo) ;
 int h_minmax(struct header *hd, Pixelval *minval, Pixelval *maxval,int nzflag);
+struct header *h_logmap(struct header *phd,float a,int R,int rows,int cols,
+      int Fx,int Fy,float disk_size) ;
+struct header *h_logmap_f(struct header *phd,float a,int R,int rows,int cols,
+      int Fx,int Fy,float disk_size) ;
 
 #endif
