3d1f4c38e6d37d776fb8523252c60c9e6efd3001
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1996-03-13 14:28:37 +0000

    Initial revision

---
+++ include/proto.h
@@ -0,0 +1,35 @@
+#ifndef PROTO_H
+#define PROTO_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/timeb.h>
+
+#ifdef SPARC
+
+int fprintf(FILE *fp, char *fmt, ...) ;
+int printf(char *fmt, ...) ;
+int vsprintf(char *str, char *fmt, va_list args) ;
+int vfprintf(FILE *fp, char *fmt, va_list args) ;
+int sscanf(char *str, char *fmt, ...) ;
+void system(char *command_string) ;
+void perror(char *s) ;
+int fgetc(FILE *fp) ; 
+time_t time(time_t *tloc) ;
+void fclose(FILE *fp) ;
+void rewind(FILE *fp) ;
+char toupper(char c) ;
+char tolower(char c) ;
+int fseek(FILE *fp, long offset, int whence) ;
+int fread(void *ptr, int size, int nitems, FILE *fp) ;
+int fwrite(void *ptr, int size, int nitems, FILE *fp) ;
+int stricmp(char *str1, char *str2) ;
+void ftime(struct timeb *tm) ;
+
+#endif
+
+
+#endif
+
