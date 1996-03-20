21a9e305f89143010a55448f592d443447be7f8c
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1996-03-20 13:36:25 +0000

    allow installable routines for printf and vfprintf

--- utils/error.c
+++ utils/error.c
@@ -26,10 +26,41 @@
                     MACROS AND CONSTANTS
 -------------------------------------------------------*/
 
+#define ERROR_FNAME  "error.log"
+
+/*-----------------------------------------------------
+                    STATIC DATA
+-------------------------------------------------------*/
+
+static char error_fname[100] = ERROR_FNAME ;
+static int (*error_vprintf)(char *fmt, va_list args) = vprintf ;
+static int (*error_vfprintf)(FILE *fp, char *fmt, va_list args) = vfprintf ;
+
 /*-----------------------------------------------------
                     GLOBAL FUNCTIONS
 -------------------------------------------------------*/
 
+/*-----------------------------------------------------
+        Parameters:
+
+        Returns value:
+
+        Description
+------------------------------------------------------*/
+int
+ErrorInit(char *fname, 
+                  int (*vfprint)(FILE *fp, char *fmt, va_list args),
+                  int (*vprint)(char *fmt, va_list args))
+{
+  if (fname)
+    strcpy(error_fname, fname) ;
+  if (vfprint)
+    error_vfprintf = vfprint ;
+  if (vprint)
+    error_vprintf = vprint ;
+
+  return(NO_ERROR) ;
+}
 /*-----------------------------------------------------
         Parameters:
 
@@ -63,14 +94,19 @@ int
 ErrorPrintf(int ecode, char *fmt, ...)
 {
   va_list  args ;
+  FILE     *fp ;
 
   va_start(args, fmt) ;
-  vfprintf(stderr, fmt, args) ;
+  (*error_vfprintf)(stderr, fmt, args) ;
   if (errno)
     perror(NULL) ;
   if (hipserrno)
     perr(ecode, "Hips error:") ;
 
+  fp = fopen(ERROR_FNAME, "a") ;
+  (*error_vfprintf)(fp, fmt, args) ;
+  fclose(fp) ;     /* close file to flush changes */
+  
   return(ecode) ;
 }
 
