2c79758b710c0d5fa7c84cceda4dda1d87bf8f46
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1996-12-05 15:35:18 +0000

    included huge_memcpy macro

--- include/hmem.h
+++ include/hmem.h
@@ -9,6 +9,7 @@
                                                
 #define hmemset(buf, val, count)   huge_memset((huge char *)buf, (char)val, (long)count)
 #define hcalloc(n,size)            huge_alloc((hsize_t)n*size, 1)
+#define hmemcpy(Dst, Src, Count)   huge_memcpy((huge char *)Dst, (huge char *)Src, (long)Count)
 
 #endif
 
