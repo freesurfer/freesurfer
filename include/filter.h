4e9b9fe95e77ab47e8f516e528bc125d0977939c
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1996-12-19 00:28:20 +0000

    defined FILTER_OFFSET bit for offsetting other filter types

--- include/filter.h
+++ include/filter.h
@@ -12,24 +12,31 @@
 #define FILTER_H
 
 /* filter constants must be defined before inclusion of image.h */
-#define FILTER_NONE          0
-#define FILTER_MEDIAN        1
-#define FILTER_MEDIAN_OFFSET 2
-#define FILTER_EXP_SUM       3
-#define FILTER_GAUSSIAN      4
-#define FILTER_NITSHI        5
-#define FILTER_GFA           6
-#define FILTER_DIFFUSE       7
-#define FILTER_EXP_LAPLACIAN 8
-#define FILTER_LAPLACIAN     9
-#define FILTER_EXPONENTIAL   10
-#define FILTER_DIFFUSE_CURV  11
-#define FILTER_DIFFUSE_GRAD  FILTER_DIFFUSE
-#define FILTER_OFFSET_SCALE  12
-#define FILTER_SIGMA         13
-#define FILTER_DIFFUSE_HV    14
-#define FILTER_MEAN          15
-#define FILTER_OFFSET        0x0100
+#define FILTER_NONE              0
+#define FILTER_MEDIAN            1
+#define FILTER_EXP_SUM           2
+#define FILTER_GAUSSIAN          3
+#define FILTER_NITSHI            4
+#define FILTER_GFA               5
+#define FILTER_DIFFUSE           6
+#define FILTER_EXP_LAPLACIAN     7
+#define FILTER_LAPLACIAN         8
+#define FILTER_EXPONENTIAL       9
+#define FILTER_DIFFUSE_CURV      10
+#define FILTER_DIFFUSE_GRAD      FILTER_DIFFUSE
+#define FILTER_OFFSET_SCALE      11
+#define FILTER_SIGMA             12
+#define FILTER_DIFFUSE_HV        13
+#define FILTER_MEAN              14
+#define FILTER_OFFSET            0x0100
+
+#define FILTER_MEDIAN_OFFSET     (FILTER_MEDIAN | FILTER_OFFSET)
+#define FILTER_MEAN_OFFSET       (FILTER_MEAN | FILTER_OFFSET)
+#define FILTER_GAUSSIAN_OFFSET   (FILTER_GAUSSIAN | FILTER_OFFSET)
+
+#define FILTER_OFFSET_MEDIAN     FILTER_MEDIAN_OFFSET
+#define FILTER_OFFSET_MEAN       FILTER_MEAN_OFFSET
+#define FILTER_OFFSET_GAUSSIAN   FILTER_GAUSSIAN_OFFSET
 
 #include "image.h"
 
