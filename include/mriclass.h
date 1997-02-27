b08b123353af61447bedf195760ed8635746e9d0
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-02-27 23:03:02 +0000

    added reading and wrigin of classifiers

--- include/mriclass.h
+++ include/mriclass.h
@@ -2,22 +2,41 @@
 #define MRI_CLASS_H
 
 #include "gclass.h"
+#include "volume_io.h"
 
+/*
+  the classifiers are spaced so that there is scale/2 padding at each
+  border, then a classifier center every scale pixels.
+  */
 typedef struct
 {
   int       scale ;     /* size reduction from original full-res. image */
   int       width ;
   int       height ;
   int       depth ;
+  int       swidth ;    /* dimensions of source MRI */
+  int       sheight ;
+  int       sdepth ;
   int       nvars ;
   GCLASSIFY ****gcs ;
+  Transform *transform ;
+  Transform *inverse_transform ;
 } MRI_CLASSIFIER, MRIC ;
 
 
 MRIC    *MRIclassAlloc(int width, int height, int depth, int scale, int nvars);
+int     MRIclassSetTransform(MRIC *mric, Transform *transform, 
+                             Transform *inverse_transform) ;
 int     MRIclassFree(MRIC **pmric) ;
 int     MRIclassTrain(MRIC *mric, MRI *mri_src,MRI *mri_norm,MRI *mri_target);
 MRI     *MRIclassify(MRIC *mric, MRI *mri_src, MRI *mri_dst, MRI *mri_norm,
                      float conf) ;
+int     MRIclassToVoxel(MRIC *mric, int xc, int yc, int zc,
+                        int *pxv, int *pyv, int *pzv) ;
+int     MRIvoxelToClass(MRIC *mric, int xv, int yv, int zv,
+                        int *pxc, int *pyc, int *pzc) ;
+MRIC    *MRIclassRead(char *fname) ;
+int     MRIclassWrite(MRIC *mric, char *fname) ;
+
 
 #endif
