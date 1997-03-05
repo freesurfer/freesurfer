84eac20bc5d554f2d141d20dbda1963ecb7c8e0a
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-03-05 17:11:40 +0000

    first try at IRIX timing

--- unix/ftime.c
+++ unix/ftime.c
@@ -2,12 +2,20 @@
 
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/times.h>
 #include <sys/timeb.h>
+#include <sys/param.h>
 
 int 
 ftime(struct timeb *t)
 {
-  t->millitm = 0 ;
+  struct tms tm ;
+
+  t->millitm = 1000 * times(&tm) / HZ ;
+#if 0
+  t->millitm = 1000 * tm.tms_utime / HZ ;
+#endif
+  t->time = 0 ;
   return(time(NULL)) ;
 }
 
