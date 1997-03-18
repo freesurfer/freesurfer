99a5b8342753a210eeaf722a7b7f650ce8859246
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-03-18 18:18:59 +0000

    use a variety of classifiers

--- include/mriclass.h
+++ include/mriclass.h
@@ -1,59 +1,37 @@
-#ifndef MRI_CLASS_H
-#define MRI_CLASS_H
+#ifndef MRICLASS_H
+#define MRICLASS_H
 
+#include "classify.h"
+#include "backprop.h"
 #include "gclass.h"
-#include "mri.h"
-#include "volume_io.h"
+#include "artmap.h"
+#include "box.h"
 
-/*
-  the classifiers are spaced so that there is scale/2 padding at each
-  border, then a classifier center every scale pixels.
-  */
+
 typedef struct
 {
-  int       scale ;     /* size reduction from original full-res. image */
-  int       width ;
-  int       height ;
-  int       depth ;
-  int       swidth ;    /* dimensions of source MRI */
-  int       sheight ;
-  int       sdepth ;
-  int       nvars ;
-  GCLASSIFY ****gcs ;
-  Transform *transform ;
-  Transform *inverse_transform ;
-  Real      xstart ;     /* coord. of (0,0,0) in Talairach space */
-  Real      ystart ;
-  Real      zstart ;
+  int         type ;        /* what kind of classifier are we using */
+  int         ninputs ;     /* # of inputs to the classifier */
+  void        *parms ;      /* classifier specific parameters */
+  GCLASSIFY   *gc ;         /* if type == CLASSIFIER_GAUSSIAN */
+  BACKPROP    *bp ;         /* if type == CLASSIFIER_BACKPROP */
+  ARTMAP      *artmap ;     /* if type == CLASSIFIER_ARTMAP */
 } MRI_CLASSIFIER, MRIC ;
 
-
-MRIC    *MRIclassAlloc(MRI *mri_template, int scale, int nvars);
-MRIC    *MRIclassTrainAll(MRIC *mric, char *training_file_name, int scale,
-                          int nvars) ;
-int     MRIclassSetTransform(MRIC *mric, Transform *transform, 
-                             Transform *inverse_transform) ;
-int     MRIclassFree(MRIC **pmric) ;
-int     MRIclassTrain(MRIC *mric, MRI *mri_src,MRI *mri_norm,MRI *mri_target);
-int     MRIclassUpdate(MRIC *mric, MRI *mri_src,MRI *mri_norm,MRI *mri_target);
-int     MRIclassFinish(MRIC *mric) ;
-MRI     *MRIclassify(MRIC *mric, MRI *mri_src, MRI *mri_dst, MRI *mri_norm,
-                     float conf, MRI *mri_probs, MRI *mri_classes) ;
-int     MRIclassToVoxel(MRIC *mric, int xc, int yc, int zc,
-                        int *pxv, int *pyv, int *pzv) ;
-int     MRIvoxelToClass(MRIC *mric, int xv, int yv, int zv,
-                        int *pxc, int *pyc, int *pzc) ;
-MRIC    *MRIclassRead(char *fname) ;
-int     MRIclassWrite(MRIC *mric, char *fname) ;
-MRI     *MRIclassThreshold(MRIC *mric, MRI *mri_probs, MRI *mri_classes,
-                           MRI *mri_dst, float threshold) ;
-
-GCLASSIFY *MRIgetClassifier(MRIC *mric, MRI *mri, int xv, int yv, int zv) ;
-int      MRIclassUpdateMeans(MRIC *mric, MRI *mris[], MRI *mri_target, 
-                             int nimages) ;
-int      MRIclassComputeMeans(MRIC *mric) ;
-int      MRIclassComputeCovariances(MRIC *mric) ;
-int      MRIclassUpdateCovariances(MRIC *mric, MRI *mris[], MRI *mri_target, 
-                             int nimages) ;
+MRIC   *MRICalloc(int type, int ninputs, void *parms) ;
+int    MRICfree(MRIC **pmri) ;
+int    MRICtrain(MRIC *mric, char *file_name) ;
+/*int    MRICclassify(MRIC *mric, MRI *mri_src, float *pprob) ;*/
+MRIC   *MRICread(char *fname) ;
+int    MRICwrite(MRIC *mric, char *fname) ;
+MRI    *MRICclassify(MRIC *mric, MRI *mri_src, MRI *mri_dst, float conf, 
+                     MRI *mri_probs, MRI *mri_classes) ;
+int    MRICupdateMeans(MRIC *mric, MRI *mri_src, MRI *mri_target, BOX *bbox) ;
+int    MRICcomputeMeans(MRIC *mric) ;  
+int    MRICupdateCovariances(MRIC *mric, MRI *mri_src, MRI *mri_target, 
+                             BOX *bbox) ;
+int    MRICcomputeCovariances(MRIC *mric) ;
+int    MRICcomputeInputs(MRI *mri,float src, int x,int y,int z,float *obs,
+                         int ninputs) ;
 
 #endif
