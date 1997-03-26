2466d51331c84265546f06caa719ed4b488b3be3
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-03-26 14:41:57 +0000

    added some PLANE_OF_LEAST_VARIANCE constants

--- include/filter.h
+++ include/filter.h
@@ -12,33 +12,40 @@
 #define FILTER_H
 
 /* filter constants must be defined before inclusion of image.h */
-#define FILTER_NONE              0
-#define FILTER_MEDIAN            1
-#define FILTER_EXP_SUM           2
-#define FILTER_GAUSSIAN          3
-#define FILTER_NITSHI            4
-#define FILTER_GFA               5
-#define FILTER_DIFFUSE           6
-#define FILTER_EXP_LAPLACIAN     7
-#define FILTER_LAPLACIAN         8
-#define FILTER_EXPONENTIAL       9
-#define FILTER_DIFFUSE_CURV      10
-#define FILTER_DIFFUSE_GRAD      FILTER_DIFFUSE
-#define FILTER_OFFSET_SCALE      11
-#define FILTER_SIGMA             12
-#define FILTER_DIFFUSE_HV        13
-#define FILTER_MEAN              14
-#define FILTER_SOBEL             15
-#define FILTER_ERODE             16
-#define FILTER_DILATE            17
-#define FILTER_OPEN              18
-#define FILTER_CLOSE             19
-#define FILTER_MINMAX            20
-#define FILTER_DIRECTION         21
-#define FILTER_ERODE6            22
-#define FILTER_DILATE6           23
-#define FILTER_OPEN6             24
-#define FILTER_CLOSE6            25
+#define FILTER_NONE                    0
+#define FILTER_MEDIAN                  1
+#define FILTER_EXP_SUM                 2
+#define FILTER_GAUSSIAN                3
+#define FILTER_NITSHI                  4
+#define FILTER_GFA                     5
+#define FILTER_DIFFUSE                 6
+#define FILTER_EXP_LAPLACIAN           7
+#define FILTER_LAPLACIAN               8
+#define FILTER_EXPONENTIAL             9
+#define FILTER_DIFFUSE_CURV            10
+#define FILTER_DIFFUSE_GRAD            FILTER_DIFFUSE
+#define FILTER_OFFSET_SCALE            11
+#define FILTER_SIGMA                   12
+#define FILTER_DIFFUSE_HV              13
+#define FILTER_MEAN                    14
+#define FILTER_SOBEL                   15
+#define FILTER_ERODE                   16
+#define FILTER_DILATE                  17
+#define FILTER_OPEN                    18
+#define FILTER_CLOSE                   19
+#define FILTER_MINMAX                  20
+#define FILTER_DIRECTION               21
+#define FILTER_ERODE6                  22
+#define FILTER_DILATE6                 23
+#define FILTER_OPEN6                   24
+#define FILTER_CLOSE6                  25
+#define FILTER_PLANE_OF_LEAST_VARIANCE 26
+#define FILTER_POLV_MEDIAN             27
+#define FILTER_POLV_STD                28
+#define FILTER_POLV_MEAN               29
+#define FILTER_POLV_NORMAL_CURVATURE   30
+#define FILTER_POLV_CURVATURE          31
+
 #define FILTER_OFFSET            0x0100
 
 #define FILTER_MEDIAN_OFFSET     (FILTER_MEDIAN | FILTER_OFFSET)
