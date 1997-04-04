ca83309df5965c7bdb80e2e075b0e50e2065022d
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-04-04 16:17:24 +0000

    added point and region drawing

--- unix/xvmri.h
+++ unix/xvmri.h
@@ -18,8 +18,13 @@
 
 #include "xvutil.h"
 #include "mri.h"
+#include "region.h"
 
 void mri_event_handler(XV_FRAME *xvf,int depth,MRI *mri,Event *event,
-                       DIMAGE *dimage, int view) ;
+                       DIMAGE *dimage, int view, int *px, int *py, int *pz) ;
+void XVMRIdrawPoint(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
+                    int x, int y, int z, int color) ;
 
+void XVMRIdrawRegion(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
+                MRI_REGION *reg, int color) ;
 #endif
