843a508424ddd6f9df2548c59dc25fa7ba4b4c41
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-04-07 13:54:41 +0000

    consolidated various mr utilities in here

--- unix/xvmri.h
+++ unix/xvmri.h
@@ -19,12 +19,25 @@
 #include "xvutil.h"
 #include "mri.h"
 #include "region.h"
+#include "image.h"
 
-void mri_event_handler(XV_FRAME *xvf,int depth,MRI *mri,Event *event,
-                       DIMAGE *dimage, int view, int *px, int *py, int *pz) ;
+void mri_event_handler(XV_FRAME *xvf, Event *event,
+                       DIMAGE *dimage, int *px, int *py, int *pz) ;
 void XVMRIdrawPoint(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                     int x, int y, int z, int color) ;
 
 void XVMRIdrawRegion(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                 MRI_REGION *reg, int color) ;
+IMAGE *XVMRIshow(XV_FRAME *xvf, MRI *mri, int which, int slice) ;
+int  XVMRIinit(XV_FRAME *xvf, int view_row, int view_col) ;
+void XVMRIshowAll(XV_FRAME *xvf) ;
+int  XVMRIfree(MRI **pmri, int which) ;
+int  XVMRIsetView(XV_FRAME *xvf, int which, int view) ;
+
+
+
+extern IMAGE *Idisplay[] ;
+extern int   mri_views[] ;
+extern int   mri_depths[] ;
+
 #endif
