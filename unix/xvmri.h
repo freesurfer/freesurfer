d7e8db6b86b38abcfb49d8d0fe669fa448a8cc68
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-04-18 18:45:13 +0000

    added utility for showing a specific frame, and keep track of current frame.s

--- unix/xvmri.h
+++ unix/xvmri.h
@@ -29,6 +29,7 @@ void XVMRIdrawPoint(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
 void XVMRIdrawRegion(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                 MRI_REGION *reg, int color) ;
 IMAGE *XVMRIshow(XV_FRAME *xvf, MRI *mri, int which, int slice) ;
+IMAGE *XVMRIshowFrame(XV_FRAME *xvf, MRI *mri, int which, int slice,int frame);
 int  XVMRIinit(XV_FRAME *xvf, int view_row, int view_col) ;
 void XVMRIshowAll(XV_FRAME *xvf) ;
 int  XVMRIfree(MRI **pmri, int which) ;
@@ -40,5 +41,6 @@ extern IMAGE *Idisplay[] ;
 extern int   mri_views[] ;
 extern int   mri_depths[] ;
 extern MRI   *mris[] ;
+extern int   mri_frames[] ;
 
 #endif
