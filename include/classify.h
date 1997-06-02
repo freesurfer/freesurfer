0cc338e70359ca117a9a2c96d386e56f8072c0ab
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-06-02 16:23:02 +0000

    added RBF classifier type

--- include/classify.h
+++ include/classify.h
@@ -6,5 +6,6 @@
 #define CLASSIFIER_BACKPROP        1
 #define CLASSIFIER_ARTMAP          2
 #define CLASSIFIER_GAUSSIAN_ARRAY  3
+#define CLASSIFIER_RBF             4   /* radial basis function */
 
 #endif
