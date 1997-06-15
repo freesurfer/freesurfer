512f7b5ed1242070e85beedb69a4f0c2b17cab7d
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-06-15 21:47:09 +0000

    added XVMRIredisplayFrame function and made click coordinates global.

--- unix/xvmri.h
+++ unix/xvmri.h
@@ -30,6 +30,8 @@ void XVMRIdrawRegion(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                 MRI_REGION *reg, int color) ;
 IMAGE *XVMRIshow(XV_FRAME *xvf, MRI *mri, int which, int slice) ;
 IMAGE *XVMRIshowFrame(XV_FRAME *xvf, MRI *mri, int which, int slice,int frame);
+IMAGE *XVMRIredisplayFrame(XV_FRAME *xvf, MRI *mri, int which, int slice,
+                           int frame);
 int  XVMRIinit(XV_FRAME *xvf, int view_row, int view_col) ;
 void XVMRIshowAll(XV_FRAME *xvf) ;
 int  XVMRIfree(MRI **pmri, int which) ;
@@ -45,5 +47,8 @@ extern int   mri_depths[] ;
 extern MRI   *mris[] ;
 extern int   mri_frames[] ;
 extern int   which_click ;
+extern int   x_click ;
+extern int   y_click ;
+extern int   z_click ;
 
 #endif
