1e7854df64ebe6e4886b01533829716837cb0d5f
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-06-27 02:35:40 +0000

    added thin strands as a seaparate class.

--- include/mriclass.h
+++ include/mriclass.h
@@ -8,15 +8,17 @@
 #include "mri.h"
 #include "rbf.h"
 
-#define NCLASSES                           5
+#define NCLASSES                           6
 #define GAUSSIAN_NCLASSES                  NCLASSES
 #define BACKGROUND                         0
 #define CSF                                BACKGROUND
 #define GREY_MATTER                        1
 #define GRAY_MATTER                        GREY_MATTER
-#define BORDER_MATTER                      2
-#define WHITE_MATTER                       3
-#define BRIGHT_MATTER                      4
+#define THIN_STRANDS                       2
+#define THIN_STRAND                        THIN_STRANDS
+#define BORDER_MATTER                      3
+#define WHITE_MATTER                       4
+#define BRIGHT_MATTER                      5
 
 #define LO_LIM                             70
 #define HI_LIM                             100
@@ -25,7 +27,8 @@
 /* range for scatter matrix plotting */
 #define MAX_SIGMA                          2.0f
 
-#define ISWHITE(c)  (((c) == BORDER_MATTER) || ((c) == WHITE_MATTER))
+#define ISWHITE(c)  (((c) == BORDER_MATTER) || \
+                     ((c) == THIN_STRANDS) || ((c) == WHITE_MATTER))
 
 /* all stuff classified as white below this value is assigned to
    the subcortical gray class, as long as its Talairach coordinate is
@@ -42,28 +45,36 @@ typedef struct
 } RBF_PARMS ;
 
 /* bitfield of feature types */
-#define FEATURE_INTENSITY      0x00001
-#define FEATURE_ZSCORE3        0x00002
-#define FEATURE_ZSCORE5        0x00004
-#define FEATURE_DIRECTION      0x00010
-#define FEATURE_MEAN3          0x00020
-#define FEATURE_MEAN5          0x00040
-#define FEATURE_CPOLV_MEAN3    0x00080
-#define FEATURE_CPOLV_MEAN5    0x00100
-#define FEATURE_CPOLV_MEDIAN3  0x00200
-#define FEATURE_CPOLV_MEDIAN5  0x00400
-#define FEATURE_MIN3           0x00800
-#define FEATURE_MIN5           0x01000
-#define FEATURE_MIN7           0x02000
-#define FEATURE_X_POSITION     0x04000
-#define FEATURE_Y_POSITION     0x08000
-#define FEATURE_Z_POSITION     0x10000
-#define FEATURE_PRIORS         0x20000
+#define FEATURE_INTENSITY      0x00000001
+#define FEATURE_ZSCORE3        0x00000002
+#define FEATURE_ZSCORE5        0x00000004
+#define FEATURE_DIRECTION      0x00000008
+#define FEATURE_MEAN3          0x00000010
+#define FEATURE_MEAN5          0x00000020
+#define FEATURE_CPOLV_MEAN3    0x00000040
+#define FEATURE_CPOLV_MEAN5    0x00000080
+#define FEATURE_CPOLV_MEDIAN3  0x00000100
+#define FEATURE_CPOLV_MEDIAN5  0x00000200
+#define FEATURE_MIN3           0x00000400
+#define FEATURE_MIN5           0x00000800
+#define FEATURE_MIN7           0x00001000
+#define FEATURE_X_POSITION     0x00002000
+#define FEATURE_Y_POSITION     0x00004000
+#define FEATURE_Z_POSITION     0x00008000
+#define FEATURE_PRIORS         0x00010000
+#define FEATURE_CPOLV_ZSCORE5  0x00020000
+#define FEATURE_CPOLV_ZSCORE7  0x00040000
+#define FEATURE_CPOLV_CURV5    0x00080000
+#define FEATURE_CPOLV_CURV7    0x00100000
+#define FEATURE_CPOLV_ORDER    0x00200000
 #define FEATURE_POSITION       (FEATURE_X_POSITION | \
-                                FEATURE_Y_POSITION | FEATURE_Y_POSITION)
+                                FEATURE_Y_POSITION | FEATURE_Z_POSITION)
 
 #define FEATURE_CPOLV          (FEATURE_CPOLV_MEAN3 | FEATURE_CPOLV_MEAN5 | \
-                                FEATURE_CPOLV_MEDIAN3 | FEATURE_CPOLV_MEDIAN5)
+                                FEATURE_CPOLV_MEDIAN3 |\
+                                FEATURE_CPOLV_ZSCORE5 | FEATURE_CPOLV_ZSCORE7|\
+                                FEATURE_CPOLV_CURV5 | FEATURE_CPOLV_CURV7 | \
+                                FEATURE_CPOLV_MEDIAN5)
 #define MAX_FEATURE            0x80000000
 
 #define MAX_ROUNDS             5
@@ -91,6 +102,7 @@ typedef struct
 MRIC   *MRICalloc(int nrounds, int types[], int features[], void *parms) ;
 int    MRICfree(MRIC **pmri) ;
 int    MRICtrain(MRIC *mric, char *file_name, char *prior_fname) ;
+int    MRICretrain(MRIC *mric, char *file_name) ;
 MRIC   *MRICread(char *fname) ;
 MRIC   *MRICquickRead(char *fname) ;
 int    MRICwrite(MRIC *mric, char *fname) ;
@@ -111,10 +123,11 @@ char   *MRICfeatureName(MRIC *mric, int round, int feature_number) ;
 int    MRICfeatureCode(MRIC *mric, int round, int feature_number) ;
 int    MRICfeatureNumberCode(int feature_number) ;
 int    MRICfeatureNumber(MRIC *mric, int round, int feature_code) ;
+char   *MRICfeatureNumberToName(int feature_number) ;
 int    MRICexamineTrainingSet(MRIC *mric, char *file_name, int round) ;
 int    MRICbuildScatterPlot(MRIC *mric, int class, MATRIX *m_scatter,
                  char *training_file_name) ;
-int    MRICsetRegionSize(MRIC *mric, int rsize) ;
+int    MRICsetRegionSize(MRIC *mric, int rwidth, int rheight, int rdepth) ;
 int    MRICresetRegionSize(MRIC *mric) ;
 
 extern char *class_names[] ;
