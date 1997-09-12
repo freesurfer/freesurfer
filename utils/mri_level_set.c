26079b37f5db6c427435438de6b7053fa595d7c1
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-09-12 21:43:22 +0000

    changed name to regionGrow, and use power laws to scale gradient contribution.

--- utils/mri_level_set.c
+++ utils/mri_level_set.c
@@ -38,32 +38,39 @@
            Description:
 ----------------------------------------------------------------------*/
 #define DT           0.5
-#define E_DIFFUSION  0.0001  /* increasing this gives a smoother solution */
-#define E_CURV       0.01   /* increasing this gives less curvature to front */
-#define MAG_SCALE    5.0    /* increasing this increases the effect of grad */
+#define E_DIFFUSION  5e-8   /* increasing this gives a smoother solution */
+#define E_CURV       0.4   /* increasing this gives less curvature to front */
+#define MAG_SCALE    1.0    /* increasing this increases the effect of grad */
+#define MAG_POW      4.0
 #define WRITE_ITER   1000
 #define MIN_DIST     2.0
 
 /* solve ut + f |grad(u)| = e uxx */
 
 MRI *
-MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
+MRIregionGrow(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,
+              int niter)
 {
   int     width, height, depth, t, x, y, z, xp1, xm1, yp1, ym1, zp1, zm1,
           write_iter;
   double  dx, dy, dz, dxx, dyy, dzz, dxy, dxz, dyz, dist_xp1, dist_xm1, 
           dist_yp1, dist_ym1, dist_zp1, dist_zm1, dist, km, denom, mag,
           dist_xp1yp1, dist_xp1zp1, dist_yp1zp1, grad, F, laplacian,
-          dx_f, dx_b, dy_f, dy_b, dz_f, dz_b, sdx, sdy, sdz ;
-  double  mag_scale, dt, e_diffusion, e_curv ;
+          dx_f, dx_b, dy_f, dy_b, dz_f, dz_b, sdx, sdy, sdz, orig_mag ;
+  double  mag_pow, mag_scale, dt, e_diffusion, e_curv ;
   float   f ;
-  BUFTYPE src, src_xp1, src_xm1, src_yp1, src_ym1, src_zp1, src_zm1 ;
+  BUFTYPE src, src_xp1, src_xm1, src_yp1, src_ym1, src_zp1, src_zm1,
+          min_orig_src, max_orig_src ;
   char    *cp ;
 
   cp = getenv("WRITE_ITER") ;
   write_iter = WRITE_ITER ;
   if (cp)
     sscanf(cp, "%d", &write_iter) ;
+  cp = getenv("MAG_POW") ;
+  mag_pow = MAG_POW ;
+  if (cp)
+    sscanf(cp, "%lf", &mag_pow) ;
   cp = getenv("MAG_SCALE") ;
   mag_scale = MAG_SCALE ;
   if (cp)
@@ -81,8 +88,8 @@ MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
   if (cp)
     sscanf(cp, "%lf", &dt) ;
 
-  fprintf(stderr, "mag_scale = %2.1f, e_curv = %2.4f, e_diffusion = %2.4f, "
-          "dt=%2.2f\n", mag_scale, e_curv, e_diffusion, dt) ;
+  fprintf(stderr, "mag_scale = %2.1f^%2.1f, e_curv = %2.4f, e_diffusion = "
+          "%2.2e, dt=%2.2f\n", mag_scale, mag_pow, e_curv, e_diffusion, dt) ;
   width = mri_src->width ;
   height = mri_src->height ;
   depth = mri_src->depth ;
@@ -96,17 +103,42 @@ MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
 
   if (!mri_distance)
     mri_distance = MRIbuildDistanceMap(mri_src, NULL, x0, y0, z0, 2.0f) ;
-  
+
+  min_orig_src = 255 ; max_orig_src = 0 ;
+  for (z = 0 ; z < depth ; z++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      for (x = 0 ; x < width ; x++)
+      {
+        src = MRIvox(mri_src, x, y, z) ;
+        dist = MRIFvox(mri_distance, x, y, z) ;
+        if (dist <= 0.0f)
+        {
+          if (src > max_orig_src)
+            max_orig_src = src ;
+          if (src < min_orig_src)
+            min_orig_src = src ;
+        }
+      }
+    }
+  }
+
+  fprintf(stderr, "min_src = %d, max_src = %d\n", min_orig_src, max_orig_src) ;
   for (t = 0 ; t < niter ; t++)
   {
-    fprintf(stderr, "\r%4.4d     ", t) ;
+    if (!(t % 10))
+      fprintf(stderr, "\r%4.4d of %d     ", t, niter) ;
     if (!(t % write_iter) && (Gdiag & DIAG_WRITE))
     {
       char fname[100] ;
       MRI  *mri_interior ;
 
-      sprintf(fname, "dist%d.mnc", t/write_iter) ;
-      MRIwrite(mri_distance, fname) ;
+      if (!(t%(write_iter*10)))
+      {
+        sprintf(fname, "dist%d.mnc", t/write_iter) ;
+        MRIwrite(mri_distance, fname) ;
+      }
       sprintf(fname, "front%d.mnc", t/write_iter) ;
       mri_interior = MRIextractInterior(mri_src, mri_distance, NULL) ;
       MRIwrite(mri_interior, fname) ;
@@ -127,7 +159,7 @@ MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
           if (dist > MIN_DIST)
             continue ;
 
-          if (x == 20 && y == 13 && z == 22)
+          if (x == 23 && y == 9 && z == 24)
             DiagBreak() ;
           xp1 = mri_src->xi[x+1] ;
           xm1 = mri_src->xi[x-1] ;
@@ -187,7 +219,7 @@ MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
             km = 0.0 ;
 
           /* speed function F based on normal and curvature */
-          F = 1.0 - km * e_curv ;   
+          F = 1.0 - km * e_curv ;
 
           /* modify it by gradient magnitude in source image */
           src = MRIvox(mri_src, x, y, z) ;
@@ -225,8 +257,31 @@ MRIfill(MRI *mri_src, MRI *mri_distance, float x0, float y0,float z0,int niter)
             sdz = dz_b ;
           else             /* information coming from in front of us */
             sdz = dz_f ;
-          mag = sqrt(sdx*sdx+sdy*sdy+sdz*sdz)*mag_scale ;
-          F *= 1.0 / (1.0 + mag*mag*mag) ; 
+          mag = sqrt(sdx*sdx+sdy*sdy+sdz*sdz) ;
+          if (x == 23 && y == 9 && z == 24)
+          {
+            static double min_mag = 100000.0 ;
+            if (mag < min_mag)
+            {
+              min_mag = mag ;
+              fprintf(stderr, "min mag set to %2.3f\n", min_mag) ;
+              DiagBreak() ;
+            }
+          }
+          mag *= mag_scale ;
+          mag = pow(mag, mag_pow) ;
+          F *= 1.0 / (1.0 + mag) ; 
+
+          /* modify by intensity distance from original seed */
+          if (src < min_orig_src)
+            orig_mag = 0.0f /* (float)(min_orig_src - src)/5.0f */ ;
+          else if (src > max_orig_src)
+            orig_mag = (float)(src - max_orig_src) ;
+          else
+            orig_mag = 0.0f ;
+          orig_mag = pow(orig_mag, 2.0) ;  /* was 8.0 */
+          F *= 1.0 / (1.0 + orig_mag) ;
+
           dist += dt * (e_diffusion * laplacian - F * grad) ;
           MRIFvox(mri_distance, x, y, z) = (float)dist ;
         }
