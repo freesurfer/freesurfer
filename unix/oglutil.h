15fdf156a5f206a4b9821d61325a92f198723140
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-11-01 22:41:38 +0000

    allow user to change lighting.

--- unix/oglutil.h
+++ unix/oglutil.h
@@ -6,6 +6,8 @@
 int    OGLUinit(MRI_SURFACE *mris, long frame_xsize, long frame_ysize) ;
 int    OGLUcompile(MRI_SURFACE *mris, int *marked_vertices, int flags, 
                    float cslope) ;
+void   OGLUsetLightingModel(float lite0, float lite1, float lite2, 
+                            float lite3, float newoffset) ;
 
 #define SCALE_FACTOR       0.55f
 
@@ -14,5 +16,7 @@ extern double oglu_fov ;
 #define PATCH_FLAG   0x0001
 #define TP_FLAG      0x0002   /* show tangent plane and principal directions */
                               /*  of marked vertices */
+
+#define LIGHT_OFFSET  0.25
 
 #endif
