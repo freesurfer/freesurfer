b41b2b05ca7ba79db6d9c275c1fbc1f851da91d7
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-11-06 22:54:45 +0000

    ansified version of wmfilter with some (minimal) help messages.

--- mri_wmfilter/mri_wmfilter.c
+++ mri_wmfilter/mri_wmfilter.c
@@ -1,109 +1,484 @@
- #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <ctype.h>
-#include <memory.h>
-
-#include "filter.h"
-#include "macros.h"
-#include "error.h"
-#include "diag.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "MRIio.h"
+/*#include "typedefs.h"*/  /* not needed without matrix stuff */
+#include "matrix.h"
 #include "proto.h"
-#include "utils.h"
-#include "mri.h"
-#include "region.h"
 
-static char vcid[] = "$Id: mri_wmfilter.c,v 1.1 1997/07/23 03:41:01 fischl Exp $";
+static char vcid[] = "$Id: mri_wmfilter.c,v 1.2 1997/11/06 22:54:45 fischl Exp $";
 
-int main(int argc, char *argv[]) ;
-static int get_option(int argc, char *argv[]) ;
-static void usage_exit(void) ;
-static void print_usage(void) ;
-static void print_help(void) ;
-static void print_version(void) ;
+/*-------------------------------------------------------------------
+                                CONSTANTS
+-------------------------------------------------------------------*/
 
-#define FILTER_WSIZE         5
-#define DEFAULT_FRACTION_ON  0.6f
+#define SQR(x) ((x)*(x))
+#define IMGSIZE 256
+#define MAXIM 512
+#define MAXCOR 500
+#define MAXLEN 100
+#define DIR_FILE "ic1.tri"
+
+/*-------------------------------------------------------------------
+                                GLOBAL DATA
+-------------------------------------------------------------------*/
+
+int xnum=256,ynum=256;
+unsigned long bufsize;
+unsigned char **im[MAXIM];  /* image matrix  */
+unsigned char **im2[MAXIM];
+unsigned char **fill[MAXIM]; 
+unsigned char *buf;  /* scratch memory  */
+int imnr0,imnr1,numimg;
+int wx0=100,wy0=100;
+float white_hilim = 140;
+float white_lolim = 90;
+float gray_hilim = 100;
+float threshold = 30;
+float xmin,xmax;
+float ymin,ymax;
+float zmin,zmax;
+float st,ps,fov,xx0,xx1,yy0,yy1,zz0,zz1;
+float ctrx,ctry,ctrz;
+float dfrac;
+#if 0
+FLOATTYPE **mat,**mati,*vec,*vec2;
+#endif
+float xcor[MAXCOR],ycor[MAXCOR],zcor[MAXCOR];
+int nver,ncor;
 
 char *Progname ;
 
-static int   filter_window_size = FILTER_WSIZE ;
+/*-------------------------------------------------------------------
+                             STATIC PROTOTYPES
+-------------------------------------------------------------------*/
 
-static float frac_on = DEFAULT_FRACTION_ON ;
-static float white_hilim = 100;
-static float white_lolim = 60; 
-static float gray_hilim = 70; 
-static float threshold = 30;
+void main(int argc,char *argv[]) ;
+static void read_image_info(char *fpref) ;
+static void plane_filter(int niter) ;
+static void read_images(char *fpref) ;
+static void write_images(char *fpref) ;
+static int get_option(int argc, char *argv[]) ;
+static void print_version(void) ;
+static void print_help(void) ;
 
-static void read_parameter_file(char *fname) ;
+/*-------------------------------------------------------------------
+                                FUNCTIONS
+-------------------------------------------------------------------*/
 
-int
-main(int argc, char *argv[])
+void
+main(int argc,char *argv[])
 {
-  char        **av ;
-  int         ac, nargs, num_above_threshold ;
-  char        *in_fname, *out_fname ;
-  MRI         *mri_full, *mri_src, *mri_dst, *mri_cpolv, *mri_cpolv_count ;
-  MRI_REGION  clip_region ;
+  int   i,j,option=1, nargs;
+  float x,y,z;
+  FILE  *fptr;
+  char  fname[200],mfname[200],pfname[200],dfname[200];
+  char  fpref[200],pname[200];
+  char  *data_dir,*mri_dir;
 
   Progname = argv[0] ;
-  ErrorInit(NULL, NULL, NULL) ;
-  DiagInit(NULL, NULL, NULL) ;
-
-  ac = argc ;
-  av = argv ;
-  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  for ( ; argc > 1 && (*argv[1] == '-') ; argc--, argv++)
   {
     nargs = get_option(argc, argv) ;
     argc -= nargs ;
     argv += nargs ;
   }
+  
+  if (argc < 2)
+    print_help() ;   /* will exit */
 
-  if (argc < 3)
-    usage_exit() ;
-
-  num_above_threshold = nint((float)filter_window_size * frac_on) ;
-  read_parameter_file("wmfilter.dat") ;
-  in_fname = argv[1] ;
-  out_fname = argv[2] ;
-
-  mri_full = MRIread(in_fname) ;
-  if (!mri_full)
-    ErrorExit(ERROR_NOFILE, "%s: could not read '%s'", Progname, in_fname) ;
-
-  /* only work on the region with actual data */
-  MRIboundingBox(mri_full, threshold, &clip_region) ;
-  REGIONexpand(&clip_region, &clip_region, (filter_window_size+1)/2) ;
-  mri_src = MRIextractRegion(mri_full, NULL, &clip_region) ;
-
-  /* compute the plane of least variance */
-  mri_cpolv = MRIplaneOfLeastVarianceNormal(mri_src, NULL, filter_window_size);
-MRIwrite(mri_cpolv, "cpolv.mnc") ;
-
-  /* find only those pixels that lie in with white-like intensity values */
-  mri_cpolv_count = MRIpolvCount(mri_src, NULL, mri_cpolv, 
-                                 filter_window_size, white_lolim, white_hilim);
-MRIwrite(mri_cpolv_count, "order.mnc") ;
-  mri_dst = MRIorderThreshold(mri_src,NULL,mri_cpolv_count,
-                              num_above_threshold);
-
-  /* also include other values that are in the correct range */
-  MRIthresholdRangeInto(mri_src, mri_dst, gray_hilim+1, white_hilim) ;
-  if (!mri_dst)
-    ErrorExit(ERROR_NOFILE, "%s: could allocate space for destination image", 
-              Progname) ;
-
-  /* now put it back in the full size image */
-  MRIextractIntoRegion(mri_dst, mri_full, 0, 0, 0, &clip_region);
-  MRIfree(&mri_dst) ;
-  MRIwrite(mri_full, out_fname) ;
-  MRIfree(&mri_full) ;
-  if (DIAG_VERBOSE_ON && (Gdiag & DIAG_SHOW))
-    fprintf(stderr, "done.\n") ;
+#if 0
+  mat = matrix(4,4);
+  mati = matrix(4,4);
+  vec = vector(4);
+  vec2 = vector(4);
+#endif
+  
+  if (argc<2)
+  {
+    exit(0);
+  }
+  
+  sprintf(pname,"%s",argv[1]);
+  if (argc>2)
+    sscanf(argv[2],"%d",&option);
+  
+  data_dir = getenv("SUBJECTS_DIR");
+  if (data_dir==NULL)
+  {
+    printf("environment variable SUBJECTS_DIR undefined (use setenv)\n");
+    exit(0);
+  }
+  mri_dir = getenv("MRI_DIR");
+  if (mri_dir==NULL)
+  {
+    printf("environment variable MRI_DIR undefined (use setenv)\n");
+    exit(0);
+  }
+  
+  sprintf(fpref,"%s/%s",data_dir,pname);
+  sprintf(mfname,"%s/mri/brain/COR-",fpref);
+  sprintf(pfname,"%s/mri/wm/COR-",fpref);
+  sprintf(dfname,"wmfilter.dat");
+  read_image_info(mfname);
+  read_images(mfname);
+  
+  sprintf(fname,"%s",dfname);
+  fptr = fopen(fname,"r");
+  if (fptr) 
+  {
+    fscanf(fptr,"%*s %f",&white_hilim);
+    fscanf(fptr,"%*s %f",&white_lolim);
+    fscanf(fptr,"%*s %f",&gray_hilim);
+    fclose(fptr);
+  }
+  else
+    printf("File %s not found - using defaults.\n",fname);
+  printf("white_hilim = %f, white_lolim = %f, gray_hilim = %f\n",
+         white_hilim,white_lolim,gray_hilim);
+  
+  sprintf(fname,"%s/lib/bem/%s",mri_dir,DIR_FILE);
+  fptr = fopen(fname,"r");
+  if (fptr==NULL) {printf("File %s not found.\n",fname);exit(0);}
+  fscanf(fptr,"%d",&nver);
+  for (i=0,ncor=0;i<nver;i++)
+  {
+    fscanf(fptr,"%*d %f %f %f",&x,&y,&z);
+    for (j=0;j<ncor;j++)
+      if ((x==-xcor[j]) && (y==-ycor[j]) && (z==-zcor[j])) goto L1;
+    xcor[ncor] = x;
+    ycor[ncor] = y;
+    zcor[ncor] = z;
+    ncor++;
+  L1:;
+  }
+  printf("%d unique orientations\n",ncor);
+  plane_filter(option);
+  write_images(pfname);
   exit(0) ;
-  return(0) ;  /* for ansi */
 }
 
+static void
+read_image_info(char *fpref)
+{
+  FILE *fptr;
+  char fname[100];
+
+  sprintf(fname,"%s.info",fpref);
+  fptr = fopen(fname,"r");
+  if (fptr==NULL) {printf("File %s not found.\n",fname);exit(1);}
+  fscanf(fptr,"%*s %d",&imnr0);
+  fscanf(fptr,"%*s %d",&imnr1);
+  fscanf(fptr,"%*s %*d");
+  fscanf(fptr,"%*s %d",&xnum);
+  fscanf(fptr,"%*s %d",&ynum);
+  fscanf(fptr,"%*s %f",&fov);
+  fscanf(fptr,"%*s %f",&ps);
+  fscanf(fptr,"%*s %f",&st);
+  fscanf(fptr,"%*s %*f"); /* locatn */
+  fscanf(fptr,"%*s %f",&xx0); /* strtx */
+  fscanf(fptr,"%*s %f",&xx1); /* endx */
+  fscanf(fptr,"%*s %f",&yy0); /* strty */
+  fscanf(fptr,"%*s %f",&yy1); /* endy */
+  fscanf(fptr,"%*s %f",&zz0); /* strtz */
+  fscanf(fptr,"%*s %f",&zz1); /* endz */
+  fov *= 1000;
+  ps *= 1000;
+  st *= 1000;
+  xx0 *= 1000;
+  xx1 *= 1000;
+  yy0 *= 1000;
+  yy1 *= 1000;
+  zz0 *= 1000;
+  zz1 *= 1000;
+  fclose(fptr);
+  numimg = imnr1-imnr0+1;
+  ctrx = (xx0+xx1)/2.0;
+  ctry = (yy0+yy1)/2.0;
+  ctrz = (zz0+zz1)/2.0;
+}
+
+static void
+plane_filter(int niter)
+{
+  int i,j,k,di,dj,dk,m,n,u,ws2=2,maxi,mini;
+  float numvox,numnz,numz;
+  float f,f2,a,b,c,s;
+  double sum2,sum,var,avg,tvar,maxvar,minvar;
+  double sum2v[MAXLEN],sumv[MAXLEN],avgv[MAXLEN],varv[MAXLEN],nv[MAXLEN];
+  float cfracz = 0.6;
+  float cfracnz = 0.6;
+/*
+  printf("plane_filter(%d)\n",niter);
+*/
+  for (k=0;k<numimg;k++)
+  {
+    fill[k] = (unsigned char **)lcalloc(IMGSIZE,sizeof(char *));
+    im2[k] = (unsigned char **)lcalloc(IMGSIZE,sizeof(char *));
+    for (i=0;i<IMGSIZE;i++)
+    {
+      fill[k][i] = (unsigned char *)lcalloc(IMGSIZE,sizeof(char));
+      im2[k][i] = (unsigned char *)lcalloc(IMGSIZE,sizeof(char));
+    }
+  }
+
+  for (k=0;k<numimg-1;k++)
+  for (i=0;i<IMGSIZE-1;i++)
+  for (j=0;j<IMGSIZE-1;j++)
+  {
+    im2[k][i][j] = im[k][i][j];
+    if (im[k][i][j]>white_hilim || im[k][i][j]<white_lolim) im[k][i][j] = 0;
+    fill[k][i][j] = im[k][i][j];
+  }
+
+  for (k=ws2;k<numimg-1-ws2;k++)
+/*
+  if (k==106)
+*/
+  {
+    printf("%d\n",k+1);
+    for (i=ws2;i<IMGSIZE-1-ws2;i++)
+    for (j=ws2;j<IMGSIZE-1-ws2;j++)
+    {
+      numvox = numnz = numz = 0;
+      for (dk = -ws2;dk<=ws2;dk++)
+      for (di = -ws2;di<=ws2;di++)
+      for (dj = -ws2;dj<=ws2;dj++) {
+         f = im[k+dk][i+di][j+dj];
+         s = dk*c+di*b+dj*a;
+         numvox++;
+         if (f!=0) {
+           numnz++;
+         } else 
+           numz++;
+       }
+    if ((im[k][i][j]==0 && numnz>=cfracnz*SQR(2*ws2+1)) || 
+        (im[k][i][j]!=0 && numz>=cfracz*SQR(2*ws2+1)))
+    {
+      maxvar = -1000000;
+      minvar = 1000000;
+      maxi = mini = -1;
+      for (m=0;m<ncor;m++)
+      {
+        a = xcor[m];
+        b = ycor[m];
+        c = zcor[m];
+        sum = sum2 = n = 0;
+        for (u=0;u<2*ws2+1;u++)
+          sumv[u] = sum2v[u] = nv[u] = 0;
+        for (dk = -ws2;dk<=ws2;dk++)
+        for (di = -ws2;di<=ws2;di++)
+        for (dj = -ws2;dj<=ws2;dj++)
+        {
+          u = ws2+floor(dk*c+di*b+dj*a+0.5);
+          u = (u<0)?0:(u>2*ws2+1-1)?2*ws2+1-1:u;
+
+          f = im[k+dk][i+di][j+dj];
+
+/*
+          f = im2[k+dk][i+di][j+dj];
+*/
+          sum2v[u] += f*f;
+          sumv[u] += f;
+          nv[u] += 1;
+          sum2 += f*f;
+          sum += f;
+          n += 1;
+        }
+        avg = sum/n;
+        var = sum2/n-avg*avg;
+        tvar = 0;
+        for (u=0;u<2*ws2+1;u++)
+        {
+          avgv[u] = sumv[u]/nv[u];
+          varv[u] = sum2v[u]/nv[u]-avgv[u]*avgv[u];
+          tvar += varv[u];
+        }
+        tvar /= (2*ws2+1);
+        if (tvar>maxvar) {maxvar=tvar;maxi=m;}
+        if (tvar<minvar) {minvar=tvar;mini=m;}
+      }
+      
+      a = xcor[mini];
+      b = ycor[mini];
+      c = zcor[mini];
+/*
+      printf(" -> %d,%d: (%f,%f) %f, %f, %f\n",mini,maxi,minvar,maxvar,a,b,c);
+*/
+/*
+      for (m=0;m<3;m++)
+      for (n=0;n<3;n++)
+        mat[m][n] = 0;
+      for (m=0;m<3;m++)
+       vec[m] = 0;
+      for (dk = -ws2;dk<=ws2;dk++)
+      for (di = -ws2;di<=ws2;di++)
+      for (dj = -ws2;dj<=ws2;dj++)
+      {
+         f = im2[k+dk][i+di][j+dj];
+         s = dk*c+di*b+dj*a;
+         s2 = s*s;
+         mat[0][0] += s2*s2;
+         mat[0][1] = (mat[1][0] += s2*s);
+         mat[0][2] = (mat[2][0] += s2);
+         mat[1][1] += s*s;
+         mat[1][2] = (mat[2][1] += s);
+         mat[2][2] += 1;
+         vec[0] += f*s2;
+         vec[1] += f*s;
+         vec[2] += f;
+      }
+      inverse(mat,mati,3);
+      vector_multiply(mati,vec,vec2,3,3);
+      f = im[k][i][j];
+      if (vec2[0]>2.0 && f<=gray_hilim) f = 0;
+      f = (f>255)?255:(f<0)?0:f;
+      fill[k][i][j] = f;
+*/
+      numvox = numnz = numz = sum = sum2 = 0;
+      for (dk = -ws2;dk<=ws2;dk++)
+      for (di = -ws2;di<=ws2;di++)
+      for (dj = -ws2;dj<=ws2;dj++) {
+         f = im[k+dk][i+di][j+dj];
+         f2 = im2[k+dk][i+di][j+dj];
+         s = dk*c+di*b+dj*a;
+         if (fabs(s)<=0.5) {
+           numvox++;
+           sum2 += f2;
+           if (f!=0) {
+             numnz++;
+             sum += f;
+           } else 
+             numz++;
+         }
+      }
+      if (numnz!=0) sum /= numnz;
+      if (numvox!=0) sum2 /= numvox;
+      f = im[k][i][j];
+      f2 = im2[k][i][j];
+/*
+      printf("%d %d %d %d %f\n",
+             k,i,j,(int)f,(int)numvox,(int)numnz,(int)numz,numz/numvox);
+*/
+      if (f!=0 && numz/numvox>cfracz && f<=gray_hilim) 
+      {
+/*
+        printf(" *** %d,%d,%d: %d->0 %d %d %d\n",
+               k,i,j,(int)f,(int)numvox,(int)numnz,(int)numz);
+*/
+        f=0;
+      }
+      else if (f==0 && numnz/numvox>cfracnz) 
+/*
+      else if (f==0 && 
+               (numnz/numvox>cfracnz || (vec2[0]<-1.0 && f2>=white_lolim-5))) 
+*/
+      {
+/*
+        printf(" !!! %d,%d,%d: %d(%d)->%d %d %d %d %f\n",
+               k,i,j,(int)f,(int)f2,(int)sum,
+               (int)numvox,(int)numnz,(int)numz,vec2[0]);
+*/
+        f=sum;
+      }
+/*
+      f = sum2;
+*/
+      fill[k][i][j] = f;
+    }
+    }
+  }
+  for (k=0;k<numimg-1;k++)
+  for (i=0;i<IMGSIZE-1;i++)
+  for (j=0;j<IMGSIZE-1;j++)
+  {
+    im[k][i][j] = fill[k][i][j];
+    if (im[k][i][j]>white_hilim || im[k][i][j]<white_lolim) im[k][i][j] = 0;
+  }
+
+}
+
+static void
+read_images(char *fpref)
+{
+  int i,k;                   /* loop counters */
+  FILE *fptr;
+  char fname[100];
+
+  numimg = imnr1-imnr0+1;
+  bufsize = ((unsigned long)xnum)*ynum;
+  buf = (unsigned char *)lcalloc(bufsize,sizeof(char));
+  for (k=0;k<numimg;k++)
+  {
+    im[k] = (unsigned char **)lcalloc(IMGSIZE,sizeof(char *));
+    for (i=0;i<IMGSIZE;i++)
+    {
+      im[k][i] = (unsigned char *)lcalloc(IMGSIZE,sizeof(char));
+    }
+  }
+  for (k=0;k<numimg;k++)
+  {
+    file_name(fpref,fname,k+imnr0,"%03d");
+    fptr = fopen(fname,"rb");
+    if (fptr==NULL) {printf("File %s not found.\n",fname);exit(1);}
+    fread(buf,sizeof(char),bufsize,fptr);
+    buffer_to_image(buf,im[k],xnum,ynum);
+    fclose(fptr);
+/*
+  printf("file %s read\n",fname);
+*/
+  }
+  printf("images %s read\n",fpref);
+}
+
+static void
+write_images(char *fpref)
+{
+  int imnr;
+  char fname[200];
+  FILE *fptr;
+
+  for (imnr=0;imnr<numimg;imnr++)
+  {
+    file_name(fpref,fname,imnr+imnr0,"%03d");
+    fptr = fopen(fname,"wb");
+    image_to_buffer(im[imnr],buf,IMGSIZE,IMGSIZE);
+    fwrite(buf,sizeof(char),bufsize,fptr);
+    fclose(fptr);
+/*
+    printf("File %s written\n",fname);
+*/
+  }
+  printf("images %s written\n",fpref);
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static void
+print_version(void)
+{
+  fprintf(stderr, "%s version %s\n", vcid, Progname) ;
+  exit(1) ;
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static void
+print_help(void)
+{
+  fprintf(stderr, "Usage: %s subject_name [option ...]\n\n", Progname);
+  fprintf(stderr, 
+          "this program will read an input volume from\n"
+          "$SUBJECTS_DIR/<subject_name>/brain and set all voxels which\n"
+          "are determined to be other than white matter to 0, writing\n"
+          "the output volume to $SUBJECTS_DIR/<subject_name>/wm.\n") ;
+          
+  exit(1) ;
+}
 /*----------------------------------------------------------------------
             Parameters:
 
@@ -116,23 +491,15 @@ get_option(int argc, char *argv[])
   char *option ;
   
   option = argv[1] + 1 ;            /* past '-' */
-  if (!stricmp(option, "-help"))
+  if (!strcmp(option, "-help"))
     print_help() ;
-  else if (!stricmp(option, "-version"))
+  else if (!strcmp(option, "-version"))
     print_version() ;
   else switch (toupper(*option))
   {
-  case 'W':
-    if (sscanf(argv[2], "%d", &filter_window_size) < 1)
-      ErrorExit(ERROR_BADPARM, "%s: could not scan window size from '%s'",
-                Progname, argv[2]) ;
-    if (filter_window_size < 3)
-      ErrorExit(ERROR_BADPARM, "%s: filter window size must be >= 3",Progname);
-    nargs = 1 ;
-    break ;
   case '?':
   case 'U':
-    print_usage() ;
+    print_help() ;
     exit(1) ;
     break ;
   default:
@@ -143,92 +510,3 @@ get_option(int argc, char *argv[])
 
   return(nargs) ;
 }
-
-static void
-usage_exit(void)
-{
-  print_usage() ;
-  exit(1) ;
-}
-
-static void
-print_usage(void)
-{
-  fprintf(stderr, 
-          "usage: %s [options] <input image file> <output image file>\n",
-          Progname) ;
-}
-
-static void
-print_help(void)
-{
-  print_usage() ;
-#if 0
-  fprintf(stderr, 
-          "\nThis program will process the image contained in "
-          "<input image file>\n"
-          "using a nonlocal filter, and write the results to "
-          "<output image file>.\n"
-          "\nThe default filter is the median, but either Gaussian or mean "
-          "filtering\n"
-          "can be specified through command line options (see below).\n"
-          "\nBy default the image is smoothed using a Gaussian kernel "
-          "(sigma=%2.3f)\n"
-          "before the offset field is calculated. This can be modified using\n"
-          "the -blur command line option.\n"
-          "\nThe input and output image formats are specified by the file name"
-          " extensions.\n"
-          "Supported formats are:\n\n"
-          "   HIPS   (.hipl or .hips)\n"
-          "   MATLAB (.mat)\n"
-          "   TIFF   (.tif or .tiff).\n"
-          "\nNote that 8-bit output images, which are generated if the input\n"
-          "image is 8-bit, are scaled to be in the range 0-255.\n", 
-          BLUR_SIGMA) ;
-  fprintf(stderr, "\nvalid options are:\n\n") ;
-  fprintf(stderr, 
-          "  -blur <sigma>     - specify sigma of blurring "
-          "kernel (default=%2.3f).\n", BLUR_SIGMA) ;
-  fprintf(stderr, 
-          "  -gaussian <sigma> - filter with Gaussian instead of median.\n") ;
-  fprintf(stderr, 
-          "  -mean             - filter with mean instead of median.\n") ;
-  fprintf(stderr, 
-          "  -w <window size>  - specify window used for offset calculation "
-          "(default=%d).\n", OFFSET_WSIZE) ;
-  fprintf(stderr, 
-          "  --version         - display version #.\n") ;
-  fprintf(stderr, 
-          "  --help            - display this help message.\n\n") ;
-#endif
-  exit(1) ;
-}
-
-static void
-print_version(void)
-{
-  fprintf(stderr, "%s\n", vcid) ;
-  exit(1) ;
-}
-
-static void
-read_parameter_file(char *fname)
-{
-  FILE *fp ;
-
-  fp = fopen(fname,"r");
-  if (fp==NULL) 
-    ErrorExit(ERROR_NOFILE, "File %s not found.\n",fname);
-
-  if (fscanf(fp,"%*s %f",&white_hilim) != 1)
-    ErrorExit(ERROR_BADFILE, "could not scan white hilim from %s", fname) ;
-
-  if (fscanf(fp,"%*s %f",&white_lolim) != 1)
-    ErrorExit(ERROR_BADFILE, "could not scan white lolim from %s", fname) ;
-  if (fscanf(fp,"%*s %f",&gray_hilim) != 1)
-    ErrorExit(ERROR_BADFILE, "could not scan gray hilim from %s", fname) ;
-  fclose(fp);
-  printf("white_hilim = %f, white_lolim = %f, gray_hilim = %f\n",
-         white_hilim,white_lolim,gray_hilim);
-}
-
