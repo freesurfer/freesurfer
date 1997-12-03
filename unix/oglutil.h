c83b0433fb6564f1e36f2c1af8a16d2beaaf02e4
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1997-12-03 21:57:33 +0000

    added displaying of mesh, and disabling of automatic FOV calculation added displaying of mesh, and disabling of automatic FOV calculation

--- unix/oglutil.h
+++ unix/oglutil.h
@@ -8,6 +8,7 @@ int    OGLUcompile(MRI_SURFACE *mris, int *marked_vertices, int flags,
                    float cslope) ;
 void   OGLUsetLightingModel(float lite0, float lite1, float lite2, 
                             float lite3, float newoffset) ;
+int    OGLUnoscale(void) ;
 
 #define SCALE_FACTOR       0.55f
 
@@ -16,6 +17,7 @@ extern double oglu_fov ;
 #define PATCH_FLAG   0x0001
 #define TP_FLAG      0x0002   /* show tangent plane and principal directions */
                               /*  of marked vertices */
+#define MESH_FLAG    0x0004
 
 #define LIGHT_OFFSET  0.25
 
