5b135e93bddde94d90280e9821adc0ac69752a06
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-01-08 17:59:31 +0000

    restored phi and theta in vertex structuree.

--- include/mrisurf.h
+++ include/mrisurf.h
@@ -105,11 +105,11 @@ typedef struct vertex_type_
   int   oripflag,origripflag;  /* cuts flags */
   float coord[3];
   float ftmp ;          /* temporary floating pt. storage */
-  float theta, phi ;    /* parameterization */
 #endif
-  int   marked;            /* for a variety of uses */
+  float theta, phi ;     /* parameterization */
+  int   marked;          /* for a variety of uses */
   int   ripflag ;
-  int   border;            /* flag */
+  int   border;          /* flag */
   float area,origarea ;
   int   tethered ;
   float K ;             /* Gaussian curvature */
