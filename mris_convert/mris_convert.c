21e8d897dfe8cdf4feb2d011ede2b2ffb0103135
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-01-09 17:36:24 +0000

    Initial revision

---
+++ mris_convert/mris_convert.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrisurf.h"
+#include "macros.h"
+
+static char vcid[] = "$Id: mris_convert.c,v 1.1 1998/01/09 17:36:24 fischl Exp $";
+
+
+/*-------------------------------- CONSTANTS -----------------------------*/
+
+/*-------------------------------- PROTOTYPES ----------------------------*/
+
+int main(int argc, char *argv[]) ;
+
+static int  get_option(int argc, char *argv[]) ;
+static void usage_exit(void) ;
+static void print_usage(void) ;
+static void print_help(void) ;
+static void print_version(void) ;
+
+/*-------------------------------- DATA ----------------------------*/
+
+char *Progname ;
+
+static int talairach_flag = 0 ;
+static int patch_flag = 0 ;
+
+/*-------------------------------- FUNCTIONS ----------------------------*/
+
+int
+main(int argc, char *argv[])
+{
+  MRI_SURFACE  *mris ;
+  char         **av, *in_fname, *out_fname, fname[100], hemi[10],
+               *cp, path[100] ;
+  int          ac, nargs ;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 2)
+    usage_exit() ;
+
+  in_fname = argv[1] ;
+  out_fname = argv[2] ;
+
+  if (patch_flag)   /* read in orig surface before reading in patch */
+  {
+    FileNamePath(in_fname, path) ;
+    cp = strrchr(in_fname, '/') ;
+    if (cp)
+    {
+      cp = strchr(cp, '.') ;
+      if (cp)
+      {
+        strncpy(hemi, cp-2, 2) ;
+        hemi[2] = 0 ;
+      }
+      else
+        strcpy(hemi, "lh") ;
+    }
+    else
+      strcpy(hemi, "lh") ;
+    sprintf(fname, "%s/%s.orig", path, hemi) ;
+    mris = MRISread(fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+                Progname, fname) ;
+    if (MRISreadPatch(mris, in_fname) != NO_ERROR)
+      ErrorExit(ERROR_NOFILE, "%s: could not read patch file %s",
+                Progname, in_fname) ;
+  }
+  else
+  {
+    mris = MRISread(in_fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+                Progname, in_fname) ;
+  }
+  if (patch_flag)
+    MRISwritePatch(mris, out_fname) ;
+  else
+    MRISwrite(mris, out_fname) ;
+
+  MRISfree(&mris) ;
+
+  exit(0) ;
+  return(0) ;  /* for ansi */
+}
+
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  if (!stricmp(option, "-help"))
+    print_help() ;
+  else if (!stricmp(option, "-version"))
+    print_version() ;
+  else switch (toupper(*option))
+  {
+  case 'P':
+    patch_flag = 1 ;
+    nargs = 0 ;
+    break ;
+  case 'T':
+    talairach_flag = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    print_usage() ;
+    exit(1) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static void
+usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+
+static void
+print_usage(void)
+{
+  fprintf(stderr, 
+          "usage: %s [options] <input surface file> <output surface file>\n",
+          Progname) ;
+}
+
+static void
+print_help(void)
+{
+  print_usage() ;
+  fprintf(stderr, 
+          "\nThis program will convert an MRI surface to ascii.\n") ;
+  fprintf(stderr, "\nvalid options are:\n\n") ;
+  exit(1) ;
+}
+
+static void
+print_version(void)
+{
+  fprintf(stderr, "%s\n", vcid) ;
+  exit(1) ;
+}
+
