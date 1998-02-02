927df08022f6cd78bed5be6d57ea3521386c8575
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-02-02 18:30:23 +0000

    added someprototypes for showing 3 orthogonal views.

--- unix/xvmri.h
+++ unix/xvmri.h
@@ -20,19 +20,20 @@
 #include "mri.h"
 #include "region.h"
 #include "image.h"
+#include "mrisurf.h"
 
 int  mri_event_handler(XV_FRAME *xvf, Event *event,
                        DIMAGE *dimage, int *px, int *py, int *pz) ;
 void XVMRIdrawPoint(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                     int x, int y, int z, int color) ;
-int  XVMRIdraw3DPoint(XV_FRAME *xvf, int which,int x, int y, int z, int color);
+int  XVMRIdraw3DPoint(XV_FRAME *xvf, int which,int x, int y,int z,int color);
 
 void XVMRIdrawRegion(XV_FRAME *xvf, int which, int view, int depth, MRI *mri,
                 MRI_REGION *reg, int color) ;
 IMAGE *XVMRIshow(XV_FRAME *xvf, MRI *mri, int which, int slice) ;
 IMAGE *XVMRIshowRange(XV_FRAME *xvf, MRI *mri, int which, int slice,
                       float fmin, float fmax) ;
-IMAGE *XVMRIshowFrame(XV_FRAME *xvf, MRI *mri, int which, int slice,int frame);
+IMAGE *XVMRIshowFrame(XV_FRAME *xvf, MRI *mri,int which,int slice,int frame);
 IMAGE *XVMRIredisplayFrame(XV_FRAME *xvf, MRI *mri, int which, int slice,
                            int frame);
 int  XVMRIinit(XV_FRAME *xvf, int view_row, int view_col) ;
@@ -42,6 +43,8 @@ int  XVMRIsetView(XV_FRAME *xvf, int which, int view) ;
 int  XVMRIsetPoint(XV_FRAME *xvf, int which, int x, int y, int z) ;
 int  XVMRIsetImageName(XV_FRAME *xvf, char *image_name) ;
 int  XVMRIfixDepth(XV_FRAME *xvf, int which, int fix) ;
+int  XVMRIspecifySurface(XV_FRAME *xvf, MRI_SURFACE *mrisurf) ;
+int  XVMRIshowThreeViews(XV_FRAME *xvf) ;
 
 
 
