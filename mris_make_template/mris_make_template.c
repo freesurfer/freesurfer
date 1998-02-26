aeb1060f4a510f9f2d6a0a93600e03749473a5a6
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-02-26 19:56:06 +0000

    Initial revision

---
+++ mris_make_template/mris_make_template.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrisurf.h"
+#include "mri.h"
+#include "macros.h"
+
+static char vcid[] = "$Id: mris_make_template.c,v 1.1 1998/02/26 19:56:06 fischl Exp $";
+
+int main(int argc, char *argv[]) ;
+
+static int  get_option(int argc, char *argv[]) ;
+static void usage_exit(void) ;
+static void print_usage(void) ;
+static void print_help(void) ;
+static void print_version(void) ;
+
+char *Progname ;
+
+static char *surface_names[] = 
+{
+  "inflated",
+  "smoothwm",
+  "smoothwm"
+} ;
+
+static char *curvature_names[] = 
+{
+  NULL,
+  "sulc",
+  NULL
+} ;
+
+
+#define IMAGES_PER_SURFACE   3   /* mean, variance, and dof */
+#define SURFACES         sizeof(curvature_names) / sizeof(curvature_names[0])
+#define PARAM_IMAGES         (IMAGES_PER_SURFACE*SURFACES)
+
+static int nbrs = 2 ;
+static int navgs = 0 ;
+static float scale = 1 ;
+
+int
+main(int argc, char *argv[])
+{
+  char         **av, surf_fname[100], *template_fname, *hemi, *sphere_name,
+               subjects_dir[100], *cp, *subject ;
+  int          ac, nargs, ino, sno ;
+  MRI_SURFACE  *mris ;
+  MRI_SP       *mrisp, *mrisp_template ;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 5)
+    usage_exit() ;
+
+  cp = getenv("SUBJECTS_DIR") ;
+  if (!cp)
+    ErrorExit(ERROR_BADPARM, "%s: SUBJECTS_DIR not defined in environment.\n",
+              Progname) ;
+  strcpy(subjects_dir, cp) ;
+  hemi = argv[1] ;
+  sphere_name = argv[2] ;
+  template_fname = argv[argc-1] ;
+  if (!FileExists(template_fname))  /* first time - create it */
+  {
+    fprintf(stderr, "creating new parameterization...\n") ;
+    mrisp_template = MRISPalloc(scale, PARAM_IMAGES); 
+  }
+  else
+  {
+    fprintf(stderr, "reading template parameterization from %s...\n",
+            template_fname) ;
+    mrisp_template = MRISPread(template_fname) ;
+    if (!mrisp_template)
+      ErrorExit(ERROR_NOFILE, "%s: could not open template file %s",
+                Progname, template_fname) ;
+  }
+
+  argv += 3 ; argc -= 3 ;
+  for (ino = 0 ; ino < argc-1 ; ino++)
+  {
+    subject = argv[ino] ;
+    fprintf(stderr, "processing subject %s\n", subject) ;
+    sprintf(surf_fname, "%s/%s/surf/%s.%s", 
+            subjects_dir, subject, hemi, sphere_name) ;
+    fprintf(stderr, "reading spherical surface %s...\n", surf_fname) ;
+    mris = MRISread(surf_fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+              Progname, surf_fname) ;
+    MRISsaveVertexPositions(mris, ORIGINAL_VERTICES) ;
+
+    for (sno = 0; sno < SURFACES ; sno++)
+    {
+      if (curvature_names[sno])  /* read in precomputed curvature file */
+      {
+        sprintf(surf_fname, "%s/%s/surf/%s.%s", 
+                subjects_dir, subject, hemi, curvature_names[sno]) ;
+        if (MRISreadCurvatureFile(mris, surf_fname) != NO_ERROR)
+          ErrorExit(Gerror, "%s: could not read curvature file '%s'\n",
+                    Progname, surf_fname) ;
+      }
+      else                       /* compute curvature of surface */
+      {
+        sprintf(surf_fname, "%s/%s/surf/%s.%s", 
+                subjects_dir, subject, hemi, surface_names[sno]) ;
+        if (MRISreadVertexPositions(mris, surf_fname) != NO_ERROR)
+          ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+                    Progname, surf_fname) ;
+        
+        MRISsetNeighborhoodSize(mris, nbrs) ;
+        MRIScomputeMetricProperties(mris) ;
+        MRIScomputeSecondFundamentalForm(mris) ;
+        MRISuseMeanCurvature(mris) ;
+        MRISaverageCurvatures(mris, navgs) ;
+        MRISrestoreVertexPositions(mris, ORIGINAL_VERTICES) ;
+        MRISnormalizeCurvature(mris) ;
+      }
+      fprintf(stderr, "computing parameterization for surface %s...\n",
+              surf_fname);
+      mrisp = MRIStoParameterization(mris, NULL, scale, 0) ;
+      MRISPcombine(mrisp, mrisp_template, sno*3) ;
+      MRISPfree(&mrisp) ;
+    }
+    MRISfree(&mris) ;
+  }
+
+  fprintf(stderr, "writing updated template to %s...\n", template_fname) ;
+  MRISPwrite(mrisp_template, template_fname) ;
+  MRISPfree(&mrisp_template) ;
+  exit(0) ;
+  return(0) ;  /* for ansi */
+}
+
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  if (!stricmp(option, "-help"))
+    print_help() ;
+  else if (!stricmp(option, "-version"))
+    print_version() ;
+  else if (!stricmp(option, "nbrs"))
+  {
+    nbrs = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "using neighborhood size = %d\n", nbrs) ;
+  }
+  else switch (toupper(*option))
+  {
+  case 'S':
+    scale = atof(argv[2]) ;
+    fprintf(stderr, "scaling parameterization by %2.1f\n", scale) ;
+    nargs = 1 ;
+    break ;
+  case 'A':
+    navgs = atoi(argv[2]) ;
+    fprintf(stderr, "averaging curvature patterns %d times.\n", navgs) ;
+    nargs = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    print_usage() ;
+    exit(1) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static void
+usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+
+static void
+print_usage(void)
+{
+  fprintf(stderr, 
+          "usage: %s [options] <hemi> <surface name> <subject> <subject> ... "
+          "<output name>\n", Progname) ;
+}
+
+static void
+print_help(void)
+{
+  print_usage() ;
+  fprintf(stderr, 
+       "\nThis program will add a template into an average surface.\n");
+  fprintf(stderr, "\nvalid options are:\n\n") ;
+  exit(1) ;
+}
+
+static void
+print_version(void)
+{
+  fprintf(stderr, "%s\n", vcid) ;
+  exit(1) ;
+}
+
