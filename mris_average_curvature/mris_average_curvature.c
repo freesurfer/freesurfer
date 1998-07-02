576fb2c023432e551a8256f7b43825db7cde29d9
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-07-02 22:56:37 +0000

    added code for generating stat maps from curvature files.

--- mris_average_curvature/mris_average_curvature.c
+++ mris_average_curvature/mris_average_curvature.c
@@ -13,7 +13,7 @@
 #include "mri.h"
 #include "macros.h"
 
-static char vcid[] = "$Id: mris_average_curvature.c,v 1.2 1998/06/17 22:47:36 fischl Exp $";
+static char vcid[] = "$Id: mris_average_curvature.c,v 1.3 1998/07/02 22:56:37 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -28,6 +28,8 @@ char *Progname ;
 static int normalize_flag = 0 ;
 static int condition_no = 0 ;
 static int stat_flag = 0 ;
+static char *output_surf_name = NULL ;
+static float sigma = 0.0f ;
 
 int
 main(int argc, char *argv[])
@@ -79,12 +81,34 @@ main(int argc, char *argv[])
       MRISnormalizeCurvature(mris) ;
 #endif
     mrisp = MRIStoParameterization(mris, NULL, 1, 0) ;
+    if (!FZERO(sigma))
+    {
+      MRI_SP  *mrisp_blur ;
+
+#if 0
+      mrisp_blur = MRISPblur(mrisp, NULL, sigma, 0) ;
+#else
+      mrisp_blur = MRISPconvolveGaussian(mrisp, NULL, sigma,mris->radius,0);
+#endif
+      MRISPfree(&mrisp) ;
+      mrisp = mrisp_blur ;
+    }
     MRISPcombine(mrisp, mrisp_total, 0) ;
     MRISPfree(&mrisp) ;
     if (i < argc-2)
       MRISfree(&mris) ;
   }
-  
+
+  if (output_surf_name)
+  {
+    sprintf(fname, "%s/%s/surf/%s.%s", sdir,output_surf_name,hemi,surf_name);
+    fprintf(stderr, "reading output surface %s...\n", fname) ;
+    MRISfree(&mris) ;
+    mris = MRISread(fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+              Progname, fname) ;
+  }
   MRISfromParameterization(mrisp_total, mris, 0) ;
   if (stat_flag)    /* write out summary statistics files */
   {
@@ -102,12 +126,12 @@ main(int argc, char *argv[])
     /* change variances to squared standard errors */
     dof = *IMAGEFseq_pix(mrisp_total->Ip, 0, 0, 2) ;
     if (!FZERO(dof)) for (vno = 0 ; vno < mris->nvertices ; vno++)
-    {
-      v = &mris->vertices[vno] ;
-      if (v->ripflag)
-        continue ;
-      v->val /= dof ;   /* turn it into a standard error */
-    }
+      {
+        v = &mris->vertices[vno] ;
+        if (v->ripflag)
+          continue ;
+        v->curv /= dof ;   /* turn it into a standard error */
+      }
 
     sprintf(fname, "%s/sigvar%d-%s.w", out_fname, condition_no, hemi);
     fprintf(stderr, "writing output variances to %s\n", fname) ;
@@ -154,6 +178,16 @@ get_option(int argc, char *argv[])
     print_version() ;
   else switch (toupper(*option))
   {
+  case 'A':
+    sigma = atof(argv[2]) ;
+    fprintf(stderr, "blurring thickness measures with sigma=%2.3f\n",sigma);
+    nargs = 1 ;
+    break ;
+  case 'O':
+    output_surf_name = argv[2] ;
+    nargs = 1 ;
+    fprintf(stderr, "painting output onto subject %s.\n", output_surf_name);
+    break ;
   case '?':
   case 'U':
     print_usage() ;
@@ -161,7 +195,7 @@ get_option(int argc, char *argv[])
     break ;
   case 'S':   /* write out stats */
     stat_flag = 1 ;
-    condition_no = atoi(argv[1]) ;
+    condition_no = atoi(argv[2]) ;
     nargs = 1 ;
     fprintf(stderr, "writing out summary statistics as condition %d\n",
             condition_no) ;
@@ -192,7 +226,11 @@ print_usage(void)
           "usage: %s [options] <input curv. file> <hemi> <surface> <subject> ... "
           " <output curv file >\n", Progname) ;
   fprintf(stderr, "the output curvature file will be painted onto the last "
-          "subject name specified\non the command line.\n") ;
+          "subject name specified\non the command line.\n"
+          "if the -s flag is specified then the last parameter specifies\n"
+          "the directory in which to write the statistical maps.\n"
+          "if the -o flag is specified then it overrides the last subject\n"
+          "as the output surface.\n") ;
 }
 
 static void
