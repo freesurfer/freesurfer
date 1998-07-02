e84d4bf7f29f18fe7f6e8d2eb89bce6cebdfebdb
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-07-02 22:57:18 +0000

    added conversion of ascii text files to .w files.

--- mris_convert/mris_convert.c
+++ mris_convert/mris_convert.c
@@ -11,8 +11,9 @@
 #include "proto.h"
 #include "mrisurf.h"
 #include "macros.h"
+#include "fio.h"
 
-static char vcid[] = "$Id: mris_convert.c,v 1.3 1998/05/20 15:11:42 fischl Exp $";
+static char vcid[] = "$Id: mris_convert.c,v 1.4 1998/07/02 22:57:18 fischl Exp $";
 
 
 /*-------------------------------- CONSTANTS -----------------------------*/
@@ -26,6 +27,7 @@ static void usage_exit(void) ;
 static void print_usage(void) ;
 static void print_help(void) ;
 static void print_version(void) ;
+static int convertWFile(char *in_fname, char *out_fname) ;
 
 /*-------------------------------- DATA ----------------------------*/
 
@@ -34,6 +36,7 @@ char *Progname ;
 static int talairach_flag = 0 ;
 static int patch_flag = 0 ;
 static int read_orig_positions = 0 ;
+static int w_file_flag = 0 ;
 
 /*-------------------------------- FUNCTIONS ----------------------------*/
 
@@ -42,7 +45,7 @@ main(int argc, char *argv[])
 {
   MRI_SURFACE  *mris ;
   char         **av, *in_fname, *out_fname, fname[100], hemi[10],
-               *cp, path[100] ;
+               *cp, path[100], *dot, ext[100] ;
   int          ac, nargs ;
 
   Progname = argv[0] ;
@@ -64,6 +67,20 @@ main(int argc, char *argv[])
 
   in_fname = argv[1] ;
   out_fname = argv[2] ;
+
+  dot = strrchr(out_fname, '.') ;
+  if (dot)
+  {
+    strcpy(ext, dot+1) ;
+    if (!stricmp(ext, "W")) 
+      w_file_flag = 1 ;
+  }
+
+  if (w_file_flag)
+  {
+    convertWFile(in_fname, out_fname) ;
+    exit(0) ;
+  }
 
   if (patch_flag)   /* read in orig surface before reading in patch */
   {
@@ -188,5 +205,44 @@ print_version(void)
 {
   fprintf(stderr, "%s\n", vcid) ;
   exit(1) ;
+}
+static int
+convertWFile(char *in_fname, char *out_fname)
+{
+  FILE   *infp, *outfp ;
+  char   line[300], *cp ;
+  int    vno, l = 0, num ;
+  float  val ;
+
+  fprintf(stderr, "writing w file %s...\n", out_fname) ;
+  outfp = fopen(out_fname,"wb");
+  if (outfp==NULL) 
+    ErrorExit(ERROR_NOFILE, "%s: Can't create file %s\n",Progname,out_fname) ;
+
+  infp = fopen(in_fname,"rb");
+  if (infp==NULL) 
+    ErrorExit(ERROR_NOFILE, "%s: Can't create file %s\n",Progname,in_fname) ;
+
+
+  cp = fgetl(line, 299, infp) ;
+  num = atoi(cp) ;
+  fwrite2(0,outfp);
+  fwrite3(num,outfp);
+
+  while ((cp = fgetl(line, 299, infp)) != NULL)
+  {
+    l++ ;
+    if (sscanf(cp, "%d %f", &vno, &val) != 2)
+    {
+      ErrorPrintf(ERROR_BADFILE,"%s: could not scan parms from line %d: %s.\n",
+                Progname, l, cp) ;
+      val = 0.0f ;   /* don't know what it is... */
+    }
+    fwrite3(vno,outfp);
+    fwriteFloat(val, outfp) ;
+  }
+  fclose(infp) ;
+  fclose(outfp) ;
+  return(NO_ERROR) ;
 }
 
