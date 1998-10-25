c0a10a74f5e05dd4e346341c1aa652252b8934b9
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-10-25 13:40:07 +0000

    now computes optimal linear and 3d morphs (not quite done yet).

--- mri_register/mri_register.c
+++ mri_register/mri_register.c
@@ -10,32 +10,21 @@
 #include "proto.h"
 #include "macros.h"
 #include "error.h"
+#include "timer.h"
 #include "diag.h"
+#include "mrimorph.h"
 
 char         *Progname ;
+static MORPH_PARMS  parms ;
 
-static unsigned char threshold = 40 ;
-
-/* options */
-static int    dx = 0, dy = 0, dz = 0 ;
-static float  xa = 0.0f, ya = 0.0f, za = 0.0f ;
-static int    verbose = 0 ;
-
-typedef struct
-{
-  MATRIX *evectors ;
-  MATRIX *evalues ;
-  MATRIX *means ;
-} REG_PARMS ;
-
-/*static MRI  *register_mri(MRI *mri_in, MRI *mri_reg) ;*/
-static MRI  *register_mri(MRI *mri_in, MRI *mri_ref, MRI *mri_reg,
-                          REG_PARMS *ref_parms, REG_PARMS *in_parms);
 static int get_option(int argc, char *argv[]) ;
-int main(int argc, char *argv[]) ;
-static int load_parms(char *dir_name, REG_PARMS *parms) ;
-static int write_parms(char *dir_name, REG_PARMS *parms) ;
-static int free_parms(REG_PARMS *parms) ;
+static MRI  *register_mri(MRI *mri_in, MRI *mri_ref, MRI *mri_reg, MP *parms) ;
+static unsigned char thresh_low = 90 ;
+static unsigned char thresh_hi = 120 ;
+char *FileNameRemoveExtension(char *in_fname, char *out_fname) ;
+
+static int linear = 0 ;
+static char *transform_fname = NULL ;
 
 /* 
    command line consists of three inputs:
@@ -44,15 +33,20 @@ static int free_parms(REG_PARMS *parms) ;
    argv[2]  - directory containing brain to be registered
    argv[3]  - directory in which to write out registered brain.
 */
+
 int
 main(int argc, char *argv[])
 {
-  char       ref_dir_name[STR_LEN], in_dir_name[STR_LEN],out_dir_name[STR_LEN];
-  MRI        *mri_ref, *mri_in, *mri_reg ;
-  char       **av ;
-  int        ac, nargs ;
-  REG_PARMS  ref_parms, in_parms ;
+  char         *ref_fname, *in_fname, *out_fname, fname[100], **av ;
+  MRI          *mri_ref, *mri_in, *mri_reg ;
+  int          ac, nargs ;
+  FILE         *fp ;
+  int          msec, minutes, seconds ;
+  struct timeb start ;
 
+  parms.niterations = 15 ;
+  parms.l_intensity = parms.l_dist = 1.0f ;
+  parms.dt = .005 ;
   Progname = argv[0] ;
 
   DiagInit(NULL, NULL, NULL) ;
@@ -67,187 +61,88 @@ main(int argc, char *argv[])
     argv += nargs ;
   }
 
-  if (argc < 3)
+  if (argc < 4)
     ErrorExit(ERROR_BADPARM, 
-              "usage: %s <ref brain dir> <in brain dir> <out brain dir>\n",
+              "usage: %s <ref brain> <in brain> <output file name>\n",
               Progname) ;
 
-  strcpy(ref_dir_name, argv[1]) ;
-  strcpy(in_dir_name, argv[2]) ;
+  ref_fname = argv[1] ;
+  in_fname = argv[2] ;
+  out_fname = argv[3] ;
+  FileNameOnly(out_fname, fname) ;
+  FileNameRemoveExtension(fname, fname) ;
+  strcpy(parms.base_name, fname) ;
+  Gdiag |= DIAG_WRITE ;
+  fprintf(stderr, "logging results to %s.log\n", parms.base_name) ;
 
-  if (argc < 4)  /* no output directory specified - construct name */
-    sprintf(out_dir_name, "%s_reg", in_dir_name) ;
-  else
-    strcpy(out_dir_name, argv[3]) ;
-
+  TimerStart(&start) ;
   /*
      check to make sure output directory exists, and, if not, create it.
      */
-  if (verbose)
-    fprintf(stderr, "reading '%s'...", ref_dir_name) ;
-  mri_ref = MRIread(ref_dir_name) ;
-  if (verbose)
+  fprintf(stderr, "reading '%s'...", ref_fname) ;
+  fflush(stderr) ;
+  mri_ref = MRIread(ref_fname) ;
+  if (!mri_ref)
+    ErrorExit(ERROR_NOFILE, "%s: could not open reference volume %s.\n",
+              Progname, ref_fname) ;
+  fprintf(stderr, "done.\n") ;
+  fflush(stderr) ;
+
+  fprintf(stderr, "reading '%s'...", in_fname) ;
+  fflush(stderr) ;
+  mri_in = MRIread(in_fname) ;
+  if (!mri_in)
+    ErrorExit(ERROR_NOFILE, "%s: could not open input volume %s.\n",
+              Progname, in_fname) ;
+  fprintf(stderr, "done.\n") ;
+  fflush(stderr) ;
+
+  if (linear || !parms.m_L)    /* find optimal linear transformation */
   {
-    fprintf(stderr, "done.\n") ;
-    fflush(stderr) ;
+    parms.dt = 5e-6 ;
+    fprintf(stderr, "computing optimal linear transformation...\n") ;
+    mri_reg = register_mri(mri_in, mri_ref, NULL, &parms) ;
+    
+    fprintf(stderr, "writing output transformation to %s...\n", out_fname) ;
+    fp = fopen(out_fname, "w") ;
+    if (!fp)
+      ErrorExit(ERROR_NOFILE, "%s: could not open output file %s\n",
+                Progname, out_fname) ;
+    fprintf(fp, "#%s %s %s\n", Progname, ref_fname, in_fname) ;
+    MatrixAsciiWriteInto(fp, parms.m_L) ;
+    fclose(fp) ;
+    parms.niterations = 15 ;
+    parms.l_intensity = parms.l_dist = 1.0f ;
+    parms.dt = .005 ;
   }
 
-  if (verbose)
-    fprintf(stderr, "reading '%s'...", in_dir_name) ;
-  mri_in = MRIread(in_dir_name) ;
-  if (verbose)
+  if (!linear)   /* do the 3d morph */
   {
-    fprintf(stderr, "done.\n") ;
-    fflush(stderr) ;
+    MORPH_3D *m3d ;
+    
+    m3d = MRImorph3D(mri_in, mri_ref, &parms, NULL) ;
+    mri_reg = MRIapply3DMorph(mri_in, mri_ref, m3d, NULL) ;
+    
+    fprintf(stderr, "writing transformed volume to %s...\n", out_fname) ;
+    MRImorph3DFree(&m3d) ;
+    MRIwrite(mri_reg, out_fname) ;
   }
-
-  load_parms(ref_dir_name, &ref_parms) ;
-  mri_reg = register_mri(mri_in, mri_ref, NULL, &ref_parms, &in_parms) ;
-  write_parms(out_dir_name, &in_parms) ;
-  write_parms(ref_dir_name, &ref_parms) ;
-  MRIwrite(mri_reg, out_dir_name) ;
-
-
-#if 0
-  if (!FZERO(za))
-  {
-    mri_tmp = MRIrotateZ(mri_in, NULL, za) ;
-    MRIcopy(mri_tmp, mri_in) ;
-    MRIfree(&mri_tmp) ;
-  }
-  if (!FZERO(ya))
-  {
-    mri_tmp = MRIrotateY(mri_in, NULL, ya) ;
-    MRIcopy(mri_tmp, mri_in) ;
-    MRIfree(&mri_tmp) ;
-  }
-  if (!FZERO(xa))
-  {
-    mri_tmp = MRIrotateX(mri_in, NULL, xa) ;
-    MRIcopy(mri_tmp, mri_in) ;
-    MRIfree(&mri_tmp) ;
-  }
-
-  if (dx || dy || dz)
-  {
-    fprintf(stderr, "translating by (%d, %d, %d)\n", dx, dy, dz) ;
-    mri_tmp = MRItranslate(mri_in, NULL, -25, 5, 0) ;
-    MRIcopy(mri_tmp, mri_in) ;
-    MRIfree(&mri_tmp) ;
-  }
-#endif
-
-  free_parms(&ref_parms) ;
-  free_parms(&in_parms) ;
   if (mri_reg)
     MRIfree(&mri_reg) ;
   if (mri_ref)
     MRIfree(&mri_ref) ;
   if (mri_in)
     MRIfree(&mri_in) ;
+  msec = TimerStop(&start) ;
+  seconds = nint((float)msec/1000.0f) ;
+  minutes = seconds / 60 ;
+  seconds = seconds % 60 ;
+  fprintf(stderr, "morphing took %d minutes and %d seconds.\n", 
+          minutes, seconds) ;
   exit(0) ;
   return(0) ;
 }
 
-static MRI *
-register_mri(MRI *mri_in, MRI *mri_ref, MRI *mri_reg, REG_PARMS *ref_parms,
-             REG_PARMS *in_parms)
-{
-  int    dx, dy, dz, row, col, in_means[3], ref_means[3] ;
-  MATRIX *mRot, *m_in_T, *mOrigin ;
-  MRI    *mri_tmp ;
-  float  x_angle, y_angle, z_angle, r11, r21, r31, r32, r33, cosy, dot ;
-  float  ref_evalues[3], in_evalues[3] ;
-
-  in_parms->evectors = MatrixAlloc(3,3,MATRIX_REAL) ;
-  in_parms->evalues = MatrixAlloc(3,1, MATRIX_REAL) ;
-  in_parms->means = MatrixAlloc(3,1,MATRIX_REAL) ;
-
-  if (!ref_parms->evectors)  /* not loaded from file */
-  {
-    ref_parms->evectors = MatrixAlloc(3,3,MATRIX_REAL) ;
-    ref_parms->evalues = MatrixAlloc(3,1, MATRIX_REAL) ;
-    ref_parms->means = MatrixAlloc(3,1,MATRIX_REAL) ;
-    MRIprincipleComponents(mri_ref, ref_parms->evectors, ref_evalues, 
-                           ref_means, threshold);
-    
-    for (row = 1 ; row <= 3 ; row++)
-    {
-      ref_parms->evalues->rptr[row][1] = ref_evalues[row-1] ;
-      ref_parms->means->rptr[row][1] = ref_means[row-1] ;
-    }
-  }
-  else
-  {
-    /* copy eigenvalues and means from MATRIX structures */
-    for (row = 1 ; row <= 3 ; row++)
-    {
-      ref_evalues[row-1] = ref_parms->evalues->rptr[row][1] ;
-      ref_means[row-1]  = ref_parms->means->rptr[row][1] ;
-    }
-  }
-
-  MRIprincipleComponents(mri_in, in_parms->evectors, in_evalues,in_means,
-                         threshold);
-  if (verbose)
-    MatrixPrint(stdout, in_parms->evectors) ;
-
-  /* copy eigenvalues and means into MATRIX structures */
-  for (row = 1 ; row <= 3 ; row++)
-  {
-    in_parms->evalues->rptr[row][1] = in_evalues[row-1] ;
-    in_parms->means->rptr[row][1] = in_means[row-1] ;
-  }
-
-  /* check to make sure eigenvectors aren't reversed */
-  for (col = 1 ; col <= 3 ; col++)
-  {
-    for (dot = 0.0f, row = 1 ; row <= 3 ; row++)
-      dot += 
-        in_parms->evectors->rptr[row][col] * 
-          ref_parms->evectors->rptr[row][col] ;
-
-    if (dot < 0.0f)
-    {
-      dot *= -1.0f ;
-      for (row = 1 ; row <= 3 ; row++)
-        in_parms->evectors->rptr[row][col] *= -1.0f ;
-    }
-  }
-
-  m_in_T = MatrixTranspose(in_parms->evectors, NULL) ;
-  mRot = MatrixMultiply(ref_parms->evectors, m_in_T, NULL) ;
-
-  r11 = mRot->rptr[1][1] ;
-  r21 = mRot->rptr[2][1] ;
-  r31 = mRot->rptr[3][1] ;
-  r32 = mRot->rptr[3][2] ;
-  r33 = mRot->rptr[3][3] ;
-  y_angle = atan2(-r31, sqrt(r11*r11+r21*r21)) ;
-  cosy = cos(y_angle) ;
-  z_angle = atan2(r21 / cosy, r11 / cosy) ;
-  x_angle = atan2(r32 / cosy, r33 / cosy) ;
-  if (verbose)
-    fprintf(stderr, "rotation: (%2.2f, %2.2f, %2.2f)\n",
-            DEGREES(x_angle), DEGREES(y_angle), DEGREES(z_angle)) ;
-  mOrigin = VectorAlloc(3, MATRIX_REAL) ;
-  mOrigin->rptr[1][1] = in_means[0] ;
-  mOrigin->rptr[2][1] = in_means[1] ;
-  mOrigin->rptr[3][1] = in_means[2] ;
-
-  dx = nint(ref_means[0] - in_means[0]) ;
-  dy = nint(ref_means[1] - in_means[1]) ;
-  dz = nint(ref_means[2] - in_means[2]) ;
-
-  mri_tmp = MRIrotate(mri_in, NULL, mRot, mOrigin) ;
-  mri_reg = MRItranslate(mri_tmp, mri_reg, dx, dy, dz) ;
-
-  if (mri_tmp)
-    MRIfree(&mri_tmp) ;
-  MatrixFree(&mRot) ;
-  VectorFree(&mOrigin) ;
-  return(mri_reg) ;
-}
 /*----------------------------------------------------------------------
             Parameters:
 
@@ -261,48 +156,76 @@ get_option(int argc, char *argv[])
 
   option = argv[1] + 1 ;            /* past '-' */
   StrUpper(option) ;
-  if (!strcmp(option, "DX"))
+  if (!strcmp(option, "DIST"))
   {
-    sscanf(argv[2], "%d", &dx) ;
+    parms.l_dist = atof(argv[2]) ;
     nargs = 1 ;
+    fprintf(stderr, "l_dist = %2.2f\n", parms.l_dist) ;
   }
-  else if (!strcmp(option, "DY"))
+  else if (!strcmp(option, "DT"))
   {
-    sscanf(argv[2], "%d", &dy) ;
+    parms.dt = atof(argv[2]) ;
     nargs = 1 ;
+    fprintf(stderr, "dt = %2.2e\n", parms.dt) ;
   }
-  else if (!strcmp(option, "DZ"))
+  else if (!strcmp(option, "AREA"))
   {
-    sscanf(argv[2], "%d", &dz) ;
+    parms.l_area = atof(argv[2]) ;
     nargs = 1 ;
+    fprintf(stderr, "l_area = %2.2f\n", parms.l_area) ;
+  }
+  else if (!strcmp(option, "INTENSITY"))
+  {
+    parms.l_intensity = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "l_intensity = %2.2f\n", parms.l_intensity) ;
   }
   else switch (*option)
   {
   case '?':
   case 'U':
-    printf("usage: %s [input directory] [output directory]\n", argv[0]) ;
+    printf("usage: %s [image file name]\n", argv[0]) ;
     exit(1) ;
     break ;
-  case 'X':
-    sscanf(argv[2], "%f", &xa) ;
-    nargs = 1 ;
-    fprintf(stderr, "rotating about x axis by %2.1f degrees\n", xa) ;
-    xa = RADIANS(xa) ;
+#if 0
+  case 'T':
+    thresh_low = atoi(argv[2]) ;
+    thresh_hi = atoi(argv[3]) ;
+    fprintf(stderr, "thresholds set to %d --> %d\n", thresh_low, thresh_hi) ;
+    nargs = 2 ;
     break ;
-  case 'Y':
-    sscanf(argv[2], "%f", &ya) ;
-    nargs = 1 ;
-    fprintf(stderr, "rotating about y axis by %2.1f degrees\n", ya) ;
-    ya = RADIANS(ya) ;
+#endif
+  case 'T':
+    {
+      transform_fname = argv[2] ;
+      fprintf(stderr, "reading optimal linear alignment from %s...\n",
+              transform_fname) ;
+      nargs = 1 ;
+      parms.m_L = MatrixAsciiRead(transform_fname, NULL) ;
+      if (!parms.m_L)
+        ErrorExit(ERROR_NOFILE, "%s: could not open transform file %s\n",
+                  Progname, transform_fname) ;
+    }
     break ;
-  case 'Z':
-    sscanf(argv[2], "%f", &za) ;
-    nargs = 1 ;
-    fprintf(stderr, "rotating about z axis by %2.1f degrees\n", za) ;
-    za = RADIANS(za) ;
+  case 'L':
+    linear = 1 ;
+    fprintf(stderr, "finding optimal linear alignment...\n") ;
     break ;
-  case 'V':
-    verbose = 1 ;
+  case 'N':
+    parms.niterations = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "niterations = %d\n", parms.niterations) ;
+    break ;
+  case 'W':
+    parms.write_iterations = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "write iterations = %d\n", parms.write_iterations) ;
+    Gdiag |= DIAG_WRITE ;
+    break ;
+  case 'M':
+    parms.momentum = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "momentum = %2.2f\n", parms.momentum) ;
     break ;
   default:
     fprintf(stderr, "unknown option %s\n", argv[1]) ;
@@ -313,49 +236,31 @@ get_option(int argc, char *argv[])
   return(nargs) ;
 }
 
-static int
-load_parms(char *dir_name, REG_PARMS *parms)
+static MRI *
+register_mri(MRI *mri_in, MRI *mri_ref, MRI *mri_reg, MORPH_PARMS *parms)
 {
-  char fname[STR_LEN] ;
+  /*  parms->write_iterations = 0 ; */
+  if (!parms->niterations)
+    parms->niterations = 1000 ;
+  parms->l_intensity = parms->l_area = parms->l_dist = 1.0f ;
+  parms->m_L = MatrixIdentity(4, NULL) ;
+  MRIremoveNeck(mri_ref, mri_ref, thresh_low, thresh_hi, parms, 1) ; 
+  MRIremoveNeck(mri_in, mri_in, thresh_low, thresh_hi, parms, -1) ; 
+  MRIlinearAlign(mri_in, mri_ref, parms) ;
+  mri_reg = MRIlinearTransform(mri_in, mri_reg, parms->m_L) ;
 
-  sprintf(fname, "%s/evectors.mat", dir_name) ;
-  parms->evectors = MatrixRead(fname) ;
-  if (!parms->evectors)
-    return(ERROR_NO_FILE) ;
-
-  if (verbose)
-    fprintf(stderr, "using previously calculated reference parameters\n") ;
-  sprintf(fname, "%s/evalues.mat", dir_name) ;
-  parms->evalues = MatrixRead(fname) ;
-
-  sprintf(fname, "%s/means.mat", dir_name) ;
-  parms->means = MatrixRead(fname) ;
-  return(NO_ERROR) ;
+  return(mri_reg) ;
 }
-
-static int
-write_parms(char *dir_name, REG_PARMS *parms)
+char *
+FileNameRemoveExtension(char *in_fname, char *out_fname)
 {
-  char fname[STR_LEN] ;
+  char *dot ;
 
-  sprintf(fname, "%s/evectors.mat", dir_name) ;
-  MatrixWrite(parms->evectors, fname, "evectors") ;
-  sprintf(fname, "%s/evalues.mat", dir_name) ;
-  MatrixWrite(parms->evalues, fname, "evalues") ;
-  sprintf(fname, "%s/means.mat", dir_name) ;
-  MatrixWrite(parms->means, fname, "means") ;
-  return(NO_ERROR) ;
-}
-
-static int
-free_parms(REG_PARMS *parms)
-{
-  if (parms->evectors)
-    MatrixFree(&parms->evectors) ;
-  if (parms->evalues)
-    MatrixFree(&parms->evalues) ;
-  if (parms->means)
-    MatrixFree(&parms->means) ;
-  return(NO_ERROR) ;
+  if (out_fname != in_fname)
+    strcpy(out_fname, in_fname) ;
+  dot = strrchr(out_fname, '.') ;
+  if (dot)
+    *dot = 0 ;
+  return(out_fname) ;
 }
 
