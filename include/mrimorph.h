7a17b0810c7d1d801e9101cfa1b07d8f02ba3726
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-10-28 16:11:09 +0000

    added some things to the parms structure.

--- include/mrimorph.h
+++ include/mrimorph.h
@@ -9,6 +9,7 @@ typedef struct
 {
   double     l_intensity ;   /* coefficient of intensity term */
   double     l_area ;        /* coefficient of area term */
+  double     l_narea ;       /* coefficient of negative area term */
   double     l_dist ;        /* coefficient of distance term */
   MATRIX     *m_L ;          /* 4x4 linear homogenous tranform */
   double     dt ;
@@ -17,17 +18,21 @@ typedef struct
   int        write_iterations ;
   char       base_name[100] ;
   FILE       *log_fp ;
+  int        start_t ;
+  float      sigma ;         /* upper bound on amount of blurring */
+  int        levels ;
+  MRI        *mri_in, *mri_ref ;  /* for diagnostics to get at originals */
 } MORPH_PARMS, MP ;
 
 #define NEIGHBORS  6
 typedef struct
 {
-#if 0
   float      ox, oy, oz ;    /* original coordinates */
-#endif
   float      x, y, z ;       /* current coordinates */
   float      orig_dist[NEIGHBORS] ;  /* original distances to 6 neighbors */
   float      orig_area ;             /* original area */
+  float      area ;                  /* current area */
+  float      dx, dy, dz ;            /* movement delta */
 } MORPH_NODE ;
 
 typedef struct
@@ -60,8 +65,11 @@ int       MRIfindMeans(MRI *mri, float *means) ;
 int       MRIfindCenterOfBrain(MRI *mri, float *px0, float *py0, float *pz0) ;
 MRI       *MRIapply3DMorph(MRI *mri_in, MRI *mri_ref, MORPH_3D *m3d, 
                            MRI *mri_morphed) ;
-MORPH_3D  *MRImorph3D(MRI *mri_in, MRI *mri_ref, MORPH_PARMS *parms, 
-                      MORPH_3D *m3d) ;
-int       MRImorph3DFree(MORPH_3D **pm3d) ;
+MORPH_3D  *MRI3Dmorph(MRI *mri_in, MRI *mri_ref, MORPH_PARMS *parms) ;
+int       MRI3DmorphFree(MORPH_3D **pm3d) ;
+int       MRI3Dwrite(MORPH_3D *m3d, char *fname) ;
+MORPH_3D  *MRI3Dread(char *fname) ;
+int      MRIsample3DmorphOrig(MORPH_3D *m3d, float x, float y, float z, 
+                                 float *pxd, float *pyd, float *pzd);
 
 #endif
