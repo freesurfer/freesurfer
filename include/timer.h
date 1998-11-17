8fc5275ab0afcc00878562e1bc9161c74cdcfc86
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-11-17 20:41:04 +0000

    prototype ftime for linux (I don't know why I need it, but I do).

--- include/timer.h
+++ include/timer.h
@@ -6,4 +6,9 @@
 struct timeb *TimerStart(struct timeb *then) ;
 int TimerStop(struct timeb *then) ;
 
+#ifdef Linux
+/* don't know why this doesn't work on linux, but.... */
+extern int ftime (struct timeb *__timebuf);
+#endif
+
 #endif
