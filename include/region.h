9605a1bb60a94fb0b04c79bd29737cbd7f308a6a
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-12-18 21:22:36 +0000

    added prototype for finding the min corner distance to a point.

--- include/region.h
+++ include/region.h
@@ -16,6 +16,7 @@ MRI_REGION *REGIONalloc(void) ;
 MRI_REGION *REGIONcopy(MRI_REGION *rsrc, MRI_REGION *rdst) ;
 int        REGIONinside(MRI_REGION *reg, int x, int y, int z) ;
 MRI_REGION *REGIONexpand(MRI_REGION *rsrc, MRI_REGION *rdst, int n) ;
+float      REGIONminCornerDistance(MRI_REGION *r1, MRI_REGION *r2) ;
 
 
 #define REGION_INSIDE      1
