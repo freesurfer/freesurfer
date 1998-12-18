9628d95745cbb512b42848d662900658ca29a533
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1998-12-18 21:17:04 +0000

    Initial revision

---
+++ mri_linear_register/mri_linear_register.c
@@ -0,0 +1,292 @@
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "mri.h"
+#include "matrix.h"
+#include "proto.h"
+#include "macros.h"
+#include "error.h"
+#include "timer.h"
+#include "diag.h"
+#include "mrimorph.h"
+
+char         *Progname ;
+static MORPH_PARMS  parms ;
+
+static int get_option(int argc, char *argv[]) ;
+static int register_mri(MRI *mri_in, MRI *mri_ref, MP *parms) ;
+static unsigned char thresh_low = 90 ;
+static unsigned char thresh_hi = 120 ;
+char *FileNameRemoveExtension(char *in_fname, char *out_fname) ;
+
+static int num_xforms = 1 ;
+static int transform_loaded = 0 ;
+
+/* 
+   command line consists of three inputs:
+
+   argv[1]  - directory containing 'canonical' brain
+   argv[2]  - directory containing brain to be registered
+   argv[3]  - directory in which to write out registered brain.
+*/
+
+int
+main(int argc, char *argv[])
+{
+  char         ref_fname[100], *in_fname, *out_fname, fname[100], **av ;
+  MRI          *mri_ref, *mri_in ;
+  int          ac, nargs ;
+  int          msec, minutes, seconds ;
+  struct timeb start ;
+
+  parms.l_intensity = 1.0f ;
+  parms.niterations = 100 ;
+  parms.levels = -1 ;   /* use default */
+  parms.dt = 1e-6 ;  /* was 5e-6 */
+  parms.tol = INTEGRATION_TOL*5 ;
+
+  parms.dt = 5e-6 ;  /* was 5e-6 */
+  parms.tol = 1e-3 ;
+  parms.momentum = 0.8 ;
+  parms.niterations = 15 ;
+  Progname = argv[0] ;
+
+  DiagInit(NULL, NULL, NULL) ;
+  ErrorInit(NULL, NULL, NULL) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 4)
+    ErrorExit(ERROR_BADPARM, 
+              "usage: %s <in brain> <template> <output file name>\n",
+              Progname) ;
+
+  in_fname = argv[1] ;
+  strcpy(ref_fname, argv[2]) ;
+  if (strchr(ref_fname, '#') == NULL)
+    strcat(ref_fname, "#-2") ;
+  out_fname = argv[3] ;
+  FileNameOnly(out_fname, fname) ;
+  FileNameRemoveExtension(fname, fname) ;
+  strcpy(parms.base_name, fname) ;
+  Gdiag |= DIAG_WRITE ;
+  fprintf(stderr, "logging results to %s.log\n", parms.base_name) ;
+
+  TimerStart(&start) ;
+  fprintf(stderr, "reading '%s'...", ref_fname) ;
+  fflush(stderr) ;
+  mri_ref = MRIread(ref_fname) ;
+  if (!mri_ref)
+    ErrorExit(ERROR_NOFILE, "%s: could not open reference volume %s.\n",
+              Progname, ref_fname) ;
+  fprintf(stderr, "done.\n") ;
+  fflush(stderr) ;
+
+  fprintf(stderr, "reading '%s'...", in_fname) ;
+  fflush(stderr) ;
+  mri_in = MRIread(in_fname) ;
+  if (!mri_in)
+    ErrorExit(ERROR_NOFILE, "%s: could not open input volume %s.\n",
+              Progname, in_fname) ;
+  fprintf(stderr, "done.\n") ;
+  fflush(stderr) ;
+  if (!transform_loaded)   /* wasn't preloaded */
+    parms.lta = LTAalloc(1, mri_in) ;
+
+  register_mri(mri_in, mri_ref, &parms) ;
+  
+  fprintf(stderr, "writing output transformation to %s...\n", out_fname) ;
+  LTAwrite(parms.lta, out_fname) ;
+  if (mri_ref)
+    MRIfree(&mri_ref) ;
+  if (mri_in)
+    MRIfree(&mri_in) ;
+  msec = TimerStop(&start) ;
+  seconds = nint((float)msec/1000.0f) ;
+  minutes = seconds / 60 ;
+  seconds = seconds % 60 ;
+  fprintf(stderr, "registration took %d minutes and %d seconds.\n", 
+          minutes, seconds) ;
+  exit(0) ;
+  return(0) ;
+}
+
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+
+  option = argv[1] + 1 ;            /* past '-' */
+  StrUpper(option) ;
+  if (!strcmp(option, "DIST") || !strcmp(option, "DISTANCE"))
+  {
+    parms.l_dist = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "l_dist = %2.2f\n", parms.l_dist) ;
+  }
+  else if (!strcmp(option, "DT"))
+  {
+    parms.dt = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "dt = %2.2e\n", parms.dt) ;
+  }
+  else if (!strcmp(option, "TOL"))
+  {
+    parms.tol = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "tol = %2.2e\n", parms.tol) ;
+  }
+  else if (!strcmp(option, "NUM"))
+  {
+    num_xforms = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "finding a total of %d linear transforms\n", num_xforms) ;
+  }
+  else if (!strcmp(option, "AREA"))
+  {
+    parms.l_area = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "l_area = %2.2f\n", parms.l_area) ;
+  }
+  else if (!strcmp(option, "NAREA"))
+  {
+    parms.l_narea = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "l_narea = %2.2f\n", parms.l_narea) ;
+  }
+  else if (!strcmp(option, "LEVELS"))
+  {
+    parms.levels = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "levels = %d\n", parms.levels) ;
+  }
+  else if (!strcmp(option, "INTENSITY") || !strcmp(option, "CORR"))
+  {
+    parms.l_intensity = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "l_intensity = %2.2f\n", parms.l_intensity) ;
+  }
+  else switch (*option)
+  {
+  case 'T':
+    parms.lta = LTAread(argv[2]) ;
+    if (!parms.lta)
+      ErrorExit(ERROR_BADFILE, "%s: could not read transform file %s",
+                Progname, argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "using previously computed transform %s\n", argv[2]) ;
+    transform_loaded = 1 ;
+    break ;
+  case 'S':
+    parms.sigma = atof(argv[2]) ;
+    fprintf(stderr, "using sigma=%2.3f as upper bound on blurring.\n", 
+            parms.sigma) ;
+    nargs = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    printf("usage: %s <in volume> <template volume> <output transform>\n", 
+           argv[0]) ;
+    exit(1) ;
+    break ;
+#if 0
+  case 'T':
+    thresh_low = atoi(argv[2]) ;
+    thresh_hi = atoi(argv[3]) ;
+    fprintf(stderr, "thresholds set to %d --> %d\n", thresh_low, thresh_hi) ;
+    nargs = 2 ;
+    break ;
+#endif
+  case 'N':
+    parms.niterations = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "niterations = %d\n", parms.niterations) ;
+    break ;
+  case 'W':
+    parms.write_iterations = atoi(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "write iterations = %d\n", parms.write_iterations) ;
+    Gdiag |= DIAG_WRITE ;
+    break ;
+  case 'M':
+    parms.momentum = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "momentum = %2.2f\n", parms.momentum) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static int
+register_mri(MRI *mri_in, MRI *mri_ref, MORPH_PARMS *parms)
+{
+  MRI  *mri_in_red, *mri_ref_red ;
+
+  parms->mri_ref = mri_ref ; parms->mri_in = mri_in ;  /* for diagnostics */
+  mri_in_red = MRIreduceByte(mri_in, NULL) ;
+  mri_ref_red = MRIreduceMeanAndStdByte(mri_ref,NULL);
+
+  /*  parms->write_iterations = 0 ; */
+  if (!parms->niterations)
+    parms->niterations = 1000 ;
+  if (transform_loaded)  /* don't recompute rotation based on neck */
+  {
+    MRIremoveNeck(mri_ref_red, mri_ref_red, thresh_low, thresh_hi, NULL, 1) ; 
+    MRIremoveNeck(mri_in_red, mri_in_red, thresh_low, thresh_hi, NULL, -1) ; 
+  }
+  else
+  {
+    MRIremoveNeck(mri_ref_red, mri_ref_red, thresh_low, thresh_hi, parms, 1) ; 
+    MRIremoveNeck(mri_in_red, mri_in_red, thresh_low, thresh_hi, parms, -1) ; 
+  }
+
+  while (parms->lta->num_xforms < num_xforms)
+    LTAdivide(parms->lta, mri_in_red) ;
+  fprintf(stderr,"computing %d linear transformation%s...\n",
+          parms->lta->num_xforms, parms->lta->num_xforms>1?"s":"");
+  MRIlinearAlign(mri_in_red, mri_ref_red, parms) ;
+
+  MRIfree(&mri_in_red) ; MRIfree(&mri_ref_red) ;
+#if 0
+  mri_reg = MRIlinearTransform(mri_in_red, mri_reg, parms->m_L) ;
+
+  return(mri_reg) ;
+#else
+  return(NO_ERROR) ;
+#endif
+}
+char *
+FileNameRemoveExtension(char *in_fname, char *out_fname)
+{
+  char *dot ;
+
+  if (out_fname != in_fname)
+    strcpy(out_fname, in_fname) ;
+  dot = strrchr(out_fname, '.') ;
+  if (dot)
+    *dot = 0 ;
+  return(out_fname) ;
+}
+
---
+++ mri_make_template/mri_make_template.c
@@ -0,0 +1,319 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "mri.h"
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrimorph.h"
+#include "transform.h"
+
+int main(int argc, char *argv[]) ;
+static int get_option(int argc, char *argv[]) ;
+
+char *Progname ;
+
+static void usage_exit(int code) ;
+int MRIaccumulateMeansAndVariances(MRI *mri, MRI *mri_mean, MRI *mri_std) ;
+int MRIcomputeMeansAndStds(MRI *mri_mean, MRI *mri_std, int ndof) ;
+MRI *MRIfloatToChar(MRI *mri_src, MRI *mri_dst) ;
+
+static int use_wm = 0 ;
+static int make_edit_volume = 0 ;
+static char *transform_fname = NULL ;
+
+#define T1_VOLUME     0
+#define WM_VOLUME     1
+#define EDIT_VOLUME   2
+#define MAX_VOLUMES   3
+
+static int first_transform = 0 ;
+
+int
+main(int argc, char *argv[])
+{
+  char   **av, *volume_name, subjects_dir[100], *cp ;
+  int    ac, nargs, i, which, dof, no_transform ;
+  MRI    *mri, *mri_mean = NULL, *mri_std ;
+  char   *subject_name, *out_fname, fname[100] ;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  cp = getenv("SUBJECTS_DIR") ;
+  if (!cp)
+    ErrorExit(ERROR_BADPARM, "%s: SUBJECTS_DIR not defined in environment.\n",
+              Progname) ;
+  strcpy(subjects_dir, cp) ;
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 3)
+    usage_exit(1) ;
+
+  out_fname = argv[argc-1] ;
+
+  for (which = 0 ; which < MAX_VOLUMES ; which++) /* 1st T1, then wm volumes */
+  {
+    if ((which > 0 && !use_wm) || (which > 1 && !make_edit_volume))
+      break ;
+    volume_name = which == T1_VOLUME ? "T1" : "wm" ;
+
+    /* for each subject specified on cmd line */
+    no_transform = first_transform ;
+    for (dof = 0, i = 1 ; i < argc-1 ; i++) 
+    {
+      if (*argv[i] == '-')   /* don't do transform for next subject */
+      { no_transform = 1 ; continue ; }
+      dof++ ;
+      subject_name = argv[i] ;
+      sprintf(fname, "%s/%s/mri/%s", subjects_dir, subject_name, volume_name) ;
+      fprintf(stderr, "reading %s...", fname) ;
+      mri = MRIread(fname) ;
+      if (!mri)
+        ErrorExit(ERROR_NOFILE, "%s: could not open volume %s",Progname,fname);
+      if (which == WM_VOLUME)   /* labeled white matter volume - binarize it */
+        MRIbinarize(mri, mri, 5, 0, 10) ;
+      else if (which == EDIT_VOLUME)
+        MRIbinarize(mri, mri, 255, 0, 1) ; /* only edits will survive */
+      fprintf(stderr, "done.\n") ;
+      if (transform_fname && no_transform-- <= 0)
+      {
+        int       type ;
+        MORPH_3D  *m3d ;
+        LTA       *lta ;
+        MRI       *mri_tmp ;
+        
+        sprintf(fname, "%s/%s/mri/transforms/%s", 
+                subjects_dir, subject_name, transform_fname) ;
+
+        fprintf(stderr, "reading and applying transform %s...", fname) ;
+        type = TransformFileNameType(transform_fname) ;
+        switch (type)
+        {
+        default:
+        case TRANSFORM_ARRAY_TYPE:
+          lta = LTAread(transform_fname) ;
+          if (!lta)
+            ErrorExit(ERROR_NOFILE, "%s: could not open transform file %s\n",
+                      Progname, transform_fname) ;
+          mri_tmp = LTAtransform(mri, NULL, lta) ;
+          LTAfree(&lta) ;
+          break ;
+        case MORPH_3D_TYPE:
+          m3d = MRI3DreadSmall(fname) ;
+          if (!m3d)
+            ErrorExit(ERROR_NOFILE, "%s: could not open transform file %s\n",
+                      Progname, transform_fname) ;
+          mri_tmp = MRIapply3DMorph(mri, m3d, NULL) ;
+          MRI3DmorphFree(&m3d) ;
+          break ;
+        }
+        MRIfree(&mri) ; mri = mri_tmp ;
+        fprintf(stderr, "done.\n") ;
+      }
+      if (!mri_mean)
+      {
+        mri_mean = MRIalloc(mri->width, mri->height, mri->depth, MRI_FLOAT) ;
+        mri_std = MRIalloc(mri->width, mri->height, mri->depth, MRI_FLOAT) ;
+        if (!mri_mean || !mri_std)
+          ErrorExit(ERROR_NOMEMORY, "%s: could not allocate templates.\n",
+                    Progname) ;
+      }
+
+      MRIaccumulateMeansAndVariances(mri, mri_mean, mri_std) ;
+      MRIfree(&mri) ;
+    }
+    MRIcomputeMeansAndStds(mri_mean, mri_std, dof) ;
+#if 1
+    fprintf(stderr, "\nwriting %s means to %s...", volume_name, out_fname) ;
+    mri_mean->dof = dof ;
+    mri = MRIfloatToChar(mri_mean, NULL) ;
+    if (!which)
+      MRIwrite(mri, out_fname) ;
+    else
+      MRIappend(mri, out_fname) ;
+    MRIfree(&mri_mean) ;
+    fprintf(stderr, "\nwriting %s variances to %s...", volume_name, out_fname);
+    mri = MRIfloatToChar(mri_std, NULL) ;
+    if (dof <= 1) /* can't calulate variances - set them to reasonable val */
+      MRIreplaceValues(mri, mri, 0, 1) ;
+    MRIappend(mri, out_fname) ;
+    fprintf(stderr, "done.\n") ;
+#endif
+    MRIfree(&mri_std) ;
+  }
+
+  fprintf(stderr, "done.\n") ;
+  exit(0) ;
+  return(0) ;
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  switch (toupper(*option))
+  {
+  case 'E':
+    use_wm = make_edit_volume = 1 ;
+    fprintf(stderr, 
+            "appending wm volume to template and building auto-edit volue.\n");
+    break ;
+  case 'W':
+    use_wm = 1 ;
+    fprintf(stderr, "appending wm volume to template.\n") ;
+    break ;
+  case 'N':
+    first_transform = 1 ;  /* don't use transform on first volume */
+    break ;
+  case 'T':
+    transform_fname = argv[2] ;
+    fprintf(stderr, "applying transformation %s to each volume\n", 
+            transform_fname) ;
+    nargs = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    usage_exit(0) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static void
+usage_exit(int code)
+{
+  printf("usage: %s <subject> <subject> ... <output volume>\n", Progname) ;
+  exit(code) ;
+}
+
+int
+MRIaccumulateMeansAndVariances(MRI *mri, MRI *mri_mean, MRI *mri_std)
+{
+  int    x, y, z, width, height, depth ;
+  float  val ;
+  
+  width = mri->width ; height = mri->height ; depth = mri->depth ;
+
+  for (z = 0 ; z < depth ; z++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      for (x = 0 ; x < width ; x++)
+      {
+        if (x == 128 && y == 128 && z == 128)
+          DiagBreak() ;
+        val = MRIvox(mri,x,y,z) ;
+        MRIFvox(mri_mean,x,y,z) += val ;
+        MRIFvox(mri_std,x,y,z) += val*val ;
+      }
+    }
+  }
+  return(NO_ERROR) ;
+}
+
+int
+MRIcomputeMeansAndStds(MRI *mri_mean, MRI *mri_std, int ndof)
+{
+  int    x, y, z, width, height, depth ;
+  float  sum, sum_sq, mean, var ;
+  
+  width = mri_std->width ; height = mri_std->height ; depth = mri_std->depth ;
+
+  for (z = 0 ; z < depth ; z++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      for (x = 0 ; x < width ; x++)
+      {
+        if (x == 128 && y == 128 && z == 128)
+          DiagBreak() ;
+        sum = MRIFvox(mri_mean,x,y,z) ;
+        sum_sq = MRIFvox(mri_std,x,y,z) / ndof ;
+        mean = MRIFvox(mri_mean,x,y,z) = sum / ndof ; 
+        var = sum_sq - mean*mean; 
+        MRIFvox(mri_std,x,y,z) = sqrt(var) ;
+      }
+    }
+  }
+  return(NO_ERROR) ;
+}
+
+MRI *
+MRIfloatToChar(MRI *mri_src, MRI *mri_dst)
+{
+  int   width, height, depth/*, x, y, z, out_val*/ ;
+  /*  float fmax, fmin ;*/
+  
+  width = mri_src->width ; height = mri_src->height ; depth = mri_src->depth ; 
+  mri_dst = MRIalloc(width, height, depth, MRI_UCHAR) ;
+#if 1
+  MRIcopy(mri_src, mri_dst) ;
+#else
+  MRIvalRange(mri_src, &fmin, &fmax) ;
+  for (z = 0 ; z < depth ; z++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      for (x = 0 ; x < width ; x++)
+      {
+      }
+    }
+  }
+#endif
+  return(mri_dst) ;
+}
+
+#if 0
+MRI *
+MRIbinarizeEditting(MRI *mri_src, MRI *mri_dst)
+{
+  int     width, height, depth, x, y, z, val ;
+  BUFTYPE *
+  
+  width = mri_src->width ; height = mri_src->height ; depth = mri_src->depth ; 
+  mri_dst = MRIalloc(width, height, depth, MRI_UCHAR) ;
+  if (!mri_dst)
+    mri_dst = MRIclone(mri_src, NULL) ;
+
+  MRIvalRange(mri_src, &fmin, &fmax) ;
+  for (z = 0 ; z < depth ; z++)
+  {
+    for (y = 0 ; y < height ; y++)
+    {
+      for (x = 0 ; x < width ; x++)
+      {
+      }
+    }
+  }
+
+  return(mri_dst) ;
+}
+#endif
