6502de5fbb5c5d690768beb9599706ce99344ed5
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1999-01-15 00:00:35 +0000

    added intensity-histogram-based segmentation.

--- utils/mrihisto.c
+++ utils/mrihisto.c
@@ -177,9 +177,14 @@ mriHistogramRegion(MRI *mri, int nbins, HISTOGRAM *histo, MRI_REGION *region)
 
   if (mri->type != MRI_UCHAR)
     ErrorReturn(NULL, 
-                (ERROR_UNSUPPORTED,"MRIhistogramRegion: must by type UCHAR"));
+                (ERROR_UNSUPPORTED,"mriHistogramRegion: must by type UCHAR"));
 
-  fmin = MRIvalRange(mri, &fmin, &fmax) ;
+  if (mri->type == MRI_UCHAR)
+  {
+    fmin = 0 ; fmax = 255 ;
+  }
+  else
+    fmin = MRIvalRange(mri, &fmin, &fmax) ;
   bmin = (BUFTYPE)fmin ; bmax = (BUFTYPE)fmax ;
   if (!nbins)
     nbins = bmax - bmin + 1 ;
@@ -481,4 +486,278 @@ MRIhistogram(MRI *mri, int nbins)
     }
   }
   return(histo) ;
+}
+/*-----------------------------------------------------
+        Parameters:
+
+        Returns value:
+
+        Description
+          Remove small inconsistincies in the labelling of a volume.
+------------------------------------------------------*/
+#define MIN_VOXELS_PCT       0.15   /* peak must average 15% of max count */
+#define PEAK_SEPARATION      11     /* min width of peak */
+#define VALLEY_WIDTH         3
+#define GRAY_LOW             65     /* lowest interesting gray matter */
+
+#define SMOOTH_SIGMA         4.0f
+
+#define X_DB2                 98
+#define Y_DB2                 147
+#define Z_DB2                 93
+
+#define X_DB1                 106
+#define Y_DB1                 158
+#define Z_DB1                 137
+
+#define X_DB                  157
+#define Y_DB                  153
+#define Z_DB                  128
+
+MRI *
+MRIhistoSegment(MRI *mri_src, MRI *mri_labeled, int wm_low, int wm_hi,
+                int gray_hi, int wsize, float sigma)
+{
+  int        width, height, depth, x, y, z, whalf, in_val, label, nvox, 
+             valley, wm_peak, gray_peak, thresh, nlabeled ;
+  BUFTYPE    *pdst, *psrc ;
+  MRI_REGION region ;
+  HISTOGRAM  *histo, *hsmooth ;
+
+  if (sigma < 0)
+    sigma = SMOOTH_SIGMA ;
+
+  thresh = (gray_hi + wm_low) / 2 ;
+  histo = hsmooth = NULL ;
+  width = mri_src->width ;
+  height = mri_src->height ;
+  depth = mri_src->depth ;
+  if (!mri_labeled)
+    mri_labeled = MRIclone(mri_src, NULL) ;
+
+  whalf = wsize/2 ;
+  region.dx = region.dy = region.dz = wsize ;
+
+  if ((Gdiag & DIAG_WRITE) && DIAG_VERBOSE_ON)
+  {
+    MRIwrite(mri_labeled, "label.mnc") ;
+    MRIwrite(mri_src, "src.mnc") ;
+  }
+  for (nlabeled = nvox = z = 0 ; z < depth ; z++)
+  {
+    DiagHeartbeat((float)z / (float)(depth-1)) ;
+    for (y = 0 ; y < height ; y++)
+    {
+      pdst = &MRIvox(mri_labeled, 0, y, z) ;
+      psrc = &MRIvox(mri_src, 0, y, z) ;
+      for (x = 0 ; x < width ; x++)
+      {
+        if (x == X_DB && y == Y_DB && z == Z_DB)
+          DiagBreak() ;
+
+        label = *pdst ;
+        in_val = *psrc++ ;
+        if (x == X_DB && y == Y_DB && z == Z_DB)
+          DiagBreak() ;
+        if (label != MRI_AMBIGUOUS)
+        {
+          pdst++ ;
+          continue ;
+        }
+        nvox++ ;
+        region.x = x-whalf ; region.y = y-whalf ; region.z = z-whalf ;
+        histo = mriHistogramRegion(mri_src, 0, histo, &region) ;
+        /*        HISTOclearBins(histo, histo, in_val-1, in_val+1) ;*/
+        hsmooth = HISTOsmooth(histo, hsmooth, sigma) ;
+#if 0
+        HISTOclearBins(histo, histo, 0, GRAY_LOW-5) ;
+        HISTOclearBins(histo, histo, wm_hi+5, 255) ;
+#endif
+        wm_peak = 
+          HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT, 
+                                    wm_low/*GRAY_LOW*/, 
+                                    wm_hi-PEAK_SEPARATION/2-2);
+        gray_peak = 
+          HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT, 
+                                    GRAY_LOW+PEAK_SEPARATION/2+2,
+                                    wm_peak-PEAK_SEPARATION+1) ;
+        while (gray_peak > gray_hi)  /* white matter is bimodal */
+        {
+          wm_peak = gray_peak ;
+          gray_peak = 
+            HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT,
+                                      GRAY_LOW+PEAK_SEPARATION/2+2,
+                                      wm_peak-PEAK_SEPARATION+1) ;
+        }
+
+        if ((wm_peak < 0) || (gray_peak < 0))  /* unimodal - take best guess */
+          valley = -1 /*thresh*/ ;
+        else   /* bimodal, find min between peaks and use it as descriminant */
+          valley = HISTOfindValley(hsmooth, VALLEY_WIDTH,
+                                   gray_peak+VALLEY_WIDTH-1,
+                                   wm_peak-VALLEY_WIDTH+1);
+        if (valley > gray_hi)  /* can't be proper descriminant */
+          valley = -1 ;
+#if 0
+        if (x == X_DB && y == Y_DB && z == Z_DB)
+        {
+          FILE *fp = fopen("histo.dat", "w") ;
+          fprintf(fp, "histogram at (%d, %d, %d) = %d\n", x, y, z, in_val) ;
+          fprintf(fp, "wm peak = %d, gray peak = %d, valley = %d\n",
+                  wm_peak, gray_peak, valley) ;
+          HISTOdump(histo, fp) ;
+          fprintf(fp, "smooth histo:\n") ;
+          HISTOdump(hsmooth, fp) ;
+          fclose(fp) ;
+          HISTOplot(hsmooth, "hsmooth.plt") ;
+          HISTOplot(histo, "histo.plt") ;
+        }
+        if (x == X_DB1 && y == Y_DB1 && z == Z_DB1)
+        {
+          FILE *fp = fopen("histo1.dat", "w") ;
+          fprintf(fp, "histogram at (%d, %d, %d) = %d\n", x, y, z, in_val) ;
+          fprintf(fp, "wm peak = %d, gray peak = %d, valley = %d\n",
+                  wm_peak, gray_peak, valley) ;
+          HISTOdump(histo, fp) ;
+          fprintf(fp, "smooth histo:\n") ;
+          HISTOdump(hsmooth, fp) ;
+          fclose(fp) ;
+          HISTOplot(hsmooth, "hsmooth1.plt") ;
+          HISTOplot(histo, "histo1.plt") ;
+        }
+#endif
+#if 0
+        if (valley < 0)
+          valley = (wm_peak + gray_peak) / 2 ;  /* assume equal sigmas */
+#endif
+        if (valley < 0 || abs(in_val-valley)<=2)
+          *pdst++ = MRI_AMBIGUOUS ;
+        else if (valley >= gray_hi)
+          *pdst++ = MRI_AMBIGUOUS ;
+        else if (in_val >= valley)
+        {
+          nlabeled++ ;
+          *pdst++ = MRI_WHITE ;
+        }
+        else
+        {
+          nlabeled++ ;
+          *pdst++ = MRI_NOT_WHITE ;
+        }
+      }
+    }
+  }
+  if (Gdiag & DIAG_SHOW)
+  {
+    fprintf(stderr, "              %8d voxels processed (%2.2f%%)\n",
+            nvox, 100.0f*(float)nvox / (float)(width*height*depth)) ;
+    fprintf(stderr, "              %8d voxels labeled (%2.2f%%)\n",
+            nlabeled, 100.0f*(float)nlabeled / (float)(width*height*depth)) ;
+  }
+  HISTOfree(&histo) ;
+  HISTOfree(&hsmooth) ;
+  return(mri_labeled) ;
+}
+/*-----------------------------------------------------
+        Parameters:
+
+        Returns value:
+
+        Description
+          Remove small inconsistincies in the labelling of a volume.
+------------------------------------------------------*/
+MRI *
+MRIhistoSegmentVoxel(MRI *mri_src, MRI *mri_labeled, int wm_low, int wm_hi,
+                int gray_hi, int wsize, int x, int y, int z,
+                     HISTOGRAM *histo, HISTOGRAM *hsmooth, float sigma)
+{
+  int        whalf, in_val, valley, wm_peak, gray_peak, thresh/*, npeaks,
+             peaks[300]*/ ;
+  MRI_REGION region ;
+  float      sig ;
+
+  thresh = (gray_hi + wm_low) / 2 ;
+
+  whalf = wsize/2 ;
+  region.dx = region.dy = region.dz = wsize ;
+
+  in_val = MRIvox(mri_src, x, y, z) ;
+  if (x == X_DB && y == Y_DB && z == Z_DB)
+    DiagBreak() ;
+
+  region.x = x-whalf ; region.y = y-whalf ; region.z = z-whalf ;
+  histo = mriHistogramRegion(mri_src, 0, histo, &region) ;
+  hsmooth = HISTOsmooth(histo, hsmooth, sigma) ;
+  /*        HISTOclearBins(histo, histo, in_val-1, in_val+1) ;*/
+
+#if 1
+  sig = sigma ;
+#else
+  sig = 0.0f ;
+  do
+  {
+    hsmooth = HISTOsmooth(histo, hsmooth, sigma) ;
+    npeaks = 
+      HISTOcountPeaksInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT, 
+                                peaks, 5,
+                                GRAY_LOW, wm_hi-PEAK_SEPARATION/2-2);
+    if (npeaks != 2)
+    {
+      sig += 0.5f ;
+      if (sig > 8.0f)
+      {
+        fprintf(stderr, "could not find two peaks\n") ;
+        return(NULL) ;
+      }
+    }
+  } while (npeaks != 2) ;
+#endif
+
+  HISTOclearBins(histo, histo, 0, GRAY_LOW-5) ;
+  HISTOclearBins(histo, histo, wm_hi+5, 255) ;
+  wm_peak = 
+    HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT, 
+                              GRAY_LOW, wm_hi-PEAK_SEPARATION/2-2);
+  gray_peak = 
+    HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT, 
+                              GRAY_LOW+PEAK_SEPARATION/2+2,
+                              wm_peak-PEAK_SEPARATION+1) ;
+  while (gray_peak > gray_hi)  /* white matter is bimodal */
+  {
+    wm_peak = gray_peak ;
+    gray_peak = 
+      HISTOfindLastPeakInRegion(hsmooth, PEAK_SEPARATION, MIN_VOXELS_PCT,
+                                GRAY_LOW+PEAK_SEPARATION/2+2,
+                                wm_peak-PEAK_SEPARATION+1) ;
+  }
+  
+  if ((wm_peak < 0) || (gray_peak < 0))  /* unimodal - take best guess */
+    valley = -1 ; /* was thresh */
+  else   /* bimodal, find min between peaks and use it as descriminant */
+    valley = HISTOfindValley(hsmooth, VALLEY_WIDTH,gray_peak,wm_peak);
+
+  {
+    FILE *fp = fopen("histo.dat", "w") ;
+    fprintf(fp, "histogram at (%d, %d, %d) = %d\n", x, y, z, in_val) ;
+    fprintf(fp, "wm peak = %d, gray peak = %d, valley = %d, sigma = %2.2f\n",
+            wm_peak, gray_peak, valley, sig) ;
+    printf("histogram at (%d, %d, %d) = %d\n", x, y, z, in_val) ;
+    printf("wm peak = %d, gray peak = %d, valley = %d\n",
+            wm_peak, gray_peak, valley) ;
+    HISTOdump(histo, fp) ;
+    fprintf(fp, "smooth histo:\n") ;
+    HISTOdump(hsmooth, fp) ;
+    fclose(fp) ;
+    HISTOplot(hsmooth, "hsmooth.plt") ;
+    HISTOplot(histo, "histo.plt") ;
+  }
+  if (valley < 0)
+    valley = -1 ; /*(wm_peak + gray_peak) / 2 ;*/  /* assume equal sigmas */
+  if (valley < 0)
+    MRIvox(mri_labeled, x, y, z) = MRI_AMBIGUOUS ;
+  else if (in_val >= valley)
+    MRIvox(mri_labeled, x, y, z) = MRI_WHITE ;
+  else
+    MRIvox(mri_labeled, x, y, z) = MRI_NOT_WHITE ;
+  return(mri_labeled) ;
 }
