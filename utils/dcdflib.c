7b9b27864ce2b5bc5380b0ab4be458b93c796de6
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1999-03-09 16:45:05 +0000

    include proto.h for definition of EXIT_FAILURE on SunOS.

--- utils/dcdflib.c
+++ utils/dcdflib.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "proto.h"
 #include "cdflib.h"
+
 static void E0000(int,int*,double*,double*,unsigned long*,
                   unsigned long*,double*,double*,double*,
                   double*,double*,double*,double*);
