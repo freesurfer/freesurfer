309fd00a8c0bc9d9604b82f26021a38882aaf78c
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1999-05-06 21:55:10 +0000

    Initial revision

---
+++ mris_rescale/mris_rescale.c
@@ -0,0 +1,131 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrisurf.h"
+#include "mri.h"
+#include "macros.h"
+
+static char vcid[] = "$Id: mris_rescale.c,v 1.1 1999/05/06 21:55:10 fischl Exp $";
+
+int main(int argc, char *argv[]) ;
+
+static int  get_option(int argc, char *argv[]) ;
+static void usage_exit(void) ;
+static void print_usage(void) ;
+static void print_help(void) ;
+static void print_version(void) ;
+
+char *Progname ;
+
+
+int
+main(int argc, char *argv[])
+{
+  char         **av, *in_fname, *out_fname ;
+  int          ac, nargs ;
+  MRI_SURFACE  *mris ;
+  float        radius, scale ;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 3)
+    usage_exit() ;
+
+  in_fname = argv[1] ;
+  out_fname = argv[2] ;
+
+  mris = MRISread(in_fname) ;
+  if (!mris)
+    ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+              Progname, in_fname) ;
+  radius = MRISaverageRadius(mris) ;
+  scale = DEFAULT_RADIUS / radius ;
+  MRISscaleBrain(mris, mris, scale) ;
+  MRISwrite(mris, out_fname) ;
+
+  exit(0) ;
+  return(0) ;  /* for ansi */
+}
+
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  if (!stricmp(option, "-help"))
+    print_help() ;
+  else if (!stricmp(option, "-version"))
+    print_version() ;
+  else switch (toupper(*option))
+  {
+  case '?':
+  case 'U':
+    print_usage() ;
+    exit(1) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static void
+usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+
+static void
+print_usage(void)
+{
+  fprintf(stderr, "usage: %s [options] <input surface file> <output surf>\n", 
+          Progname) ;
+}
+
+static void
+print_help(void)
+{
+  print_usage() ;
+  fprintf(stderr, 
+      "\nThis program will rescale a surface representation\n") ;
+  fprintf(stderr, "\nvalid options are:\n\n") ;
+  exit(1) ;
+}
+
+static void
+print_version(void)
+{
+  fprintf(stderr, "%s\n", vcid) ;
+  exit(1) ;
+}
+
