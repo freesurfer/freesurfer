555eda5c00cdf4cf362e715de9fbd661ca325ae6
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1999-06-17 17:15:03 +0000

    added freadShort.

--- include/fio.h
+++ include/fio.h
@@ -11,6 +11,7 @@ int   fread3(int *v, FILE *fp) ;
 int   fread4(float *v, FILE *fp) ;
 float freadFloat(FILE *fp) ;
 int   freadInt(FILE *fp) ;
+short freadShort(FILE *fp) ;
 
 int   fwriteFloat(float f, FILE *fp) ;
 int   fwriteInt(int v, FILE *fp) ;
