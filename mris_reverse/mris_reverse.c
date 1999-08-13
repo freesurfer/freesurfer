c32f51c54817ff23971c2be1df8dab6a0aca1df4
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
1999-08-13 23:13:10 +0000

    support reversing of patches.

--- mris_reverse/mris_reverse.c
+++ mris_reverse/mris_reverse.c
@@ -13,7 +13,7 @@
 #include "mri.h"
 #include "macros.h"
 
-static char vcid[] = "$Id: mris_reverse.c,v 1.2 1999/04/18 03:16:49 fischl Exp $";
+static char vcid[] = "$Id: mris_reverse.c,v 1.3 1999/08/13 23:13:10 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -25,12 +25,14 @@ static void print_version(void) ;
 
 char *Progname ;
 
+static int patch_flag = 0 ;
 static int which = REVERSE_X ;
 
 int
 main(int argc, char *argv[])
 {
-  char               **av, *in_fname, *out_fname, path[100] ;
+  char               **av, *in_fname, *out_fname, path[STRLEN], fname[STRLEN],
+                     hemi[10], *cp ;
   int                ac, nargs ;
   MRI_SURFACE        *mris ;
 
@@ -52,18 +54,42 @@ main(int argc, char *argv[])
 
   in_fname = argv[1] ;
   out_fname = argv[2] ;
+  
+  if (patch_flag)
+  {
+    FileNamePath(in_fname, path) ;
+    FileNameOnly(in_fname, hemi) ;
+    cp = strchr(hemi, '.') ;
+    if (cp)
+      *cp = 0 ;
+    else
+      ErrorExit(ERROR_BADPARM, "%s: could not scan hemisphere from %s\n",
+                in_fname) ;
+    sprintf(fname, "%s/%s.%s", path, hemi, ORIG_NAME) ;
+    mris = MRISread(fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+                Progname, fname) ;
+    if (MRISreadPatch(mris, in_fname) != NO_ERROR)
+      ErrorExit(Gerror, "%s: could not read patch\n", Progname) ;
+  }
+  else
+  {
+    mris = MRISread(in_fname) ;
+    if (!mris)
+      ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+                Progname, in_fname) ;
+  }
+
   FileNamePath(out_fname, path) ;
-
-  mris = MRISread(in_fname) ;
-  if (!mris)
-    ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
-              Progname, in_fname) ;
-
   MRISreverse(mris, which) ;
   if (Gdiag & DIAG_SHOW)
     fprintf(stderr, "writing reversed surface to %s\n", out_fname) ;
   mris->type = MRIS_TRIANGULAR_SURFACE ;
-  MRISwrite(mris, out_fname) ;
+  if (patch_flag)
+    MRISwritePatch(mris, out_fname) ;
+  else
+    MRISwrite(mris, out_fname) ;
   exit(0) ;
   return(0) ;  /* for ansi */
 }
@@ -90,6 +116,9 @@ get_option(int argc, char *argv[])
   case 'U':
     print_usage() ;
     exit(1) ;
+    break ;
+  case 'P':
+    patch_flag = 1 ;
     break ;
   case 'Y':
     which = REVERSE_Y ;
