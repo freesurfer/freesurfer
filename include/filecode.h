0f8aee3edfbdbacd2604100b36f7851d52d3c6e7
Doug Greve <greve@nmr.mgh.harvard.edu>
1999-11-09 23:03:31 +0000

    Initial revision

---
+++ include/filecode.h
@@ -0,0 +1,22 @@
+#ifndef FILECODE_H
+#define FILECODE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define FILECODE_STATSURF        14
+#define FILECODE_SPARSEVOL       15
+
+#define FILECODE_VSS             16
+#define FILECODE_VIT             17
+#define FILECODE_VPM             18
+#define FILECODE_ISS             19
+#define FILECODE_VST             20
+
+#ifdef	__cplusplus
+}
+#endif /* #ifdef __cplusplus */
+
+#endif /* #ifndef FILECODE_H */
+
