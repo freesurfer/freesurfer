b5fe46e2186a8e4d84ed63683320dfa82173af34
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2000-09-28 15:11:00 +0000

    Initial revision

---
+++ mris_expand/mris_expand.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "mri.h"
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrisurf.h"
+#include "utils.h"
+#include "const.h"
+#include "timer.h"
+
+int main(int argc, char *argv[]) ;
+static int get_option(int argc, char *argv[]) ;
+
+char *Progname ;
+static void usage_exit(int code) ;
+
+
+int
+main(int argc, char *argv[])
+{
+  char         **av ;
+  int          ac, nargs ;
+  char         *in_fname, *out_fname ;
+  int          msec, minutes, seconds ;
+  struct timeb start ;
+  float        mm_out ;
+  MRI_SURFACE  *mris ;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  TimerStart(&start) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 4)
+    usage_exit(1) ;
+
+  in_fname = argv[1] ;
+  mm_out = atof(argv[2]) ;
+  out_fname = argv[3] ;
+
+  printf("expanding surface %s by %2.1f mm and writing it to %s\n",
+         in_fname, mm_out, out_fname) ;
+  mris = MRISread(in_fname) ;
+  if (!mris)
+    ErrorExit(ERROR_NOFILE, "%s: MRISread(%s) failed", Progname, in_fname);
+  MRISexpandSurface(mris, mm_out) ;
+  printf("writing expanded surface to %s...\n", out_fname) ;
+  MRISwrite(mris, out_fname) ;
+  msec = TimerStop(&start) ;
+  seconds = nint((float)msec/1000.0f) ;
+  minutes = seconds / 60 ;
+  seconds = seconds % 60 ;
+  fprintf(stderr, "surface expansion took %d minutes"
+          " and %d seconds.\n", minutes, seconds) ;
+  exit(0) ;
+  return(0) ;
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  switch (toupper(*option))
+  {
+  case '?':
+  case 'U':
+    usage_exit(0) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static void
+usage_exit(int code)
+{
+  printf("usage: %s [options] <input surface> <mm> <output surface>\n", 
+         Progname) ;
+  exit(code) ;
+}
