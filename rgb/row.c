18a861c00defc81c72f0dcb82fb6678278871df8
witzel <witzel@nmr.mgh.harvard.edu>
2000-11-07 21:52:14 +0000

    added putrow_uc still with that warning, check it later

--- rgb/row.c
+++ rgb/row.c
@@ -7,6 +7,138 @@
 #include  <stdio.h>
 #include  "rgb_image.h"
 
+/* twitzel added this function */
+
+int putrow_uc(RGB_IMAGE *image, unsigned char *buffer,
+    unsigned int y, unsigned int z) 
+{
+    register unsigned char   *sptr;
+    register unsigned char      *cptr;
+    register unsigned int x;
+    register unsigned long min, max;
+    register long cnt;
+
+    if( !(image->flags & (_IORW|_IOWRT)) )
+  return -1;
+    if(image->dim<3)
+  z = 0;
+    if(image->dim<2)
+  y = 0;
+    if(ISVERBATIM(image->type)) {
+      switch(BPP(image->type)) {
+      case 1: 
+  min = image->min;
+  max = image->max;
+  cptr = (unsigned char *)image->tmpbuf;
+  sptr = buffer;
+  /* this might be uneccessary for this uc function */
+  for(x=image->xsize; x--;) { 
+    *cptr = *sptr++;
+    if (*cptr > max) max = *cptr;
+    if (*cptr < min) min = *cptr;
+    cptr++;
+  }
+  image->min = min;
+  image->max = max;
+  img_seek(image,y,z);
+  cnt = image->xsize;
+  if (img_write(image,(char *)(image->tmpbuf),cnt) != cnt) {
+    i_errhdlr("putrow: error on write of row\n",0,0,0,0);
+    return -1;
+  } else
+    return cnt;
+  /* NOTREACHED */
+  
+      case 2: 
+  min = image->min;
+  max = image->max;
+  sptr = buffer;
+  for(x=image->xsize; x--;) { 
+    if (*sptr > max) max = *sptr;
+    if (*sptr < min) min = *sptr;
+    sptr++;
+  }
+  image->min = min;
+  image->max = max;
+  img_seek(image,y,z);
+  cnt = image->xsize<<1;
+  /* if(image->dorev)  
+     cvtshorts(buffer,cnt); */
+  if (img_write(image,(char *)(buffer),cnt) != cnt) {
+    /* if(image->dorev)  
+       cvtshorts(buffer,cnt); */
+    i_errhdlr("putrow: error on write of row\n",0,0,0,0);
+    return -1;
+  } else {
+    /* if(image->dorev)  
+       cvtshorts(buffer,cnt); */
+    return image->xsize;
+  }
+  /* NOTREACHED */
+  
+      default:
+  i_errhdlr("putrow: weird bpp\n",0,0,0,0);
+      }
+    } else if(ISRLE(image->type)) {
+      switch(BPP(image->type)) {
+      case 1: 
+  min = image->min;
+  max = image->max;
+  sptr = buffer;
+  for(x=image->xsize; x--;) { 
+    if (*sptr > max) max = *sptr;
+    if (*sptr < min) min = *sptr;
+    sptr++;
+  }
+  image->min = min;
+  image->max = max;
+  cnt = img_rle_compact(buffer,2,image->tmpbuf,1,image->xsize);
+  img_setrowsize(image,cnt,y,z);
+  img_seek(image,y,z);
+  if (img_write(image,(char *)(image->tmpbuf),cnt) != cnt) {
+    i_errhdlr("putrow: error on write of row\n",0,0,0,0);
+    return -1;
+  } else
+    return image->xsize;
+  /* NOTREACHED */
+  
+      case 2: 
+  min = image->min;
+  max = image->max;
+  sptr = buffer;
+  for(x=image->xsize; x--;) { 
+    if (*sptr > max) max = *sptr;
+    if (*sptr < min) min = *sptr;
+    sptr++;
+  }
+  image->min = min;
+  image->max = max;
+  cnt = img_rle_compact(buffer,2,image->tmpbuf,2,image->xsize);
+  cnt <<= 1;
+  img_setrowsize(image,cnt,y,z);
+  img_seek(image,y,z);
+  if(image->dorev)
+    cvtshorts(image->tmpbuf,cnt);
+  if (img_write(image,(char *)(image->tmpbuf),cnt) != cnt) {
+    if(image->dorev)
+      cvtshorts(image->tmpbuf,cnt);
+    i_errhdlr("putrow: error on write of row\n",0,0,0,0);
+    return -1;
+  } else {
+    if(image->dorev)
+      cvtshorts(image->tmpbuf,cnt);
+    return image->xsize;
+  }
+  /* NOTREACHED */
+
+      default:
+  i_errhdlr("putrow: weird bpp\n",0,0,0,0);
+      }
+    } else 
+      i_errhdlr("putrow: weird image type\n",0,0,0,0);
+    return(-1);
+}
+
 int putrow(RGB_IMAGE *image, unsigned short *buffer,
     unsigned int y, unsigned int z) 
 {
