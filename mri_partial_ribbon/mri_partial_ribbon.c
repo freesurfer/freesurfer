0bde0102d3c94ca167c10b42bc0be320938e9d7e
Andre van der Kouwe <andre@nmr.mgh.harvard.edu>
2001-05-08 03:41:27 +0000

    Erosion implemented in masking ribbon code.

--- mri_partial_ribbon/mri_partial_ribbon.c
+++ mri_partial_ribbon/mri_partial_ribbon.c
@@ -12,52 +12,57 @@ char *Progname;
 
 int main(int argc, char *argv[])
 {
-  char *inner_mris_fname,*outer_mris_fname,*input_mri_pref,*output_mri_pref,*mask_mri_pref;
+  char *inner_mris_fname_lh,*outer_mris_fname_lh,*inner_mris_fname_rh,*outer_mris_fname_rh,*input_mri_pref,*output_mri_pref,*mask_mri_pref;
   MRI *mri,*mri_src,*mri_mask;
-  MRI_SURFACE *inner_mris,*outer_mris;
-  int hemisphere;
+  MRI_SURFACE *inner_mris_lh,*outer_mris_lh,*inner_mris_rh,*outer_mris_rh;
 
   /* Set command-line parameters */
-  if ((argc!=5)&&(argc!=7)) {
-    printf("Usage: mri_partial_ribbon inner_surface_fname outer_surface_fname input_volume_pref output_volume_pref [mask_fname left/right]\n");
+  if ((argc!=7)&&(argc!=8)) {
+    printf("Usage: mri_partial_ribbon inner_surface_fname_lh outer_surface_fname_lh inner_surface_fname_rh outer_surface_fname_rh input_volume_pref output_volume_pref [mask_fname]\n");
     exit(1);
   }
   Progname=argv[0];
 
-  inner_mris_fname=argv[1];
-  outer_mris_fname=argv[2];
-  input_mri_pref=argv[3];
-  output_mri_pref=argv[4];
-  if (argc==7) {
-    mask_mri_pref=argv[5];
-    if (strcasecmp(argv[6],"left")==0)
-      hemisphere=MRI_LEFT_HEMISPHERE;
-    else
-      if (strcasecmp(argv[6],"right")==0)
-        hemisphere=MRI_RIGHT_HEMISPHERE;
-      else {
-        fprintf(stderr,"Hemisphere must be specified as left or right.\n");
-        exit(1);
-      }
-  }
-  else {
+  inner_mris_fname_lh=argv[1];
+  outer_mris_fname_lh=argv[2];
+  inner_mris_fname_rh=argv[3];
+  outer_mris_fname_rh=argv[4];
+  input_mri_pref=argv[5];
+  output_mri_pref=argv[6];
+  if (argc==8)
+    mask_mri_pref=argv[7];
+  else
     mask_mri_pref=NULL;
-    hemisphere=0;
-  }
 
-  /* Read surface information from inner surface file */
-  printf("Reading surface file %s.\n",inner_mris_fname);
-  inner_mris=MRISread(inner_mris_fname);
-  if (!inner_mris) {
-    fprintf(stderr,"Could not read surface file %s.\n",inner_mris_fname);
+  /* Read surface information from left inner surface file */
+  printf("Reading left inner surface file %s.\n",inner_mris_fname_lh);
+  inner_mris_lh=MRISread(inner_mris_fname_lh);
+  if (!inner_mris_lh) {
+    fprintf(stderr,"Could not read surface file %s.\n",inner_mris_fname_lh);
     exit(1);
   }
 
-  /* Read surface information from outer surface file */
-  printf("Reading surface file %s.\n",outer_mris_fname);
-  outer_mris=MRISread(outer_mris_fname);
-  if (!outer_mris) {
-    fprintf(stderr,"Could not read surface file %s.\n",outer_mris_fname);
+  /* Read surface information from left outer surface file */
+  printf("Reading left outer surface file %s.\n",outer_mris_fname_lh);
+  outer_mris_lh=MRISread(outer_mris_fname_lh);
+  if (!outer_mris_lh) {
+    fprintf(stderr,"Could not read surface file %s.\n",outer_mris_fname_lh);
+    exit(1);
+  }
+
+  /* Read surface information from right inner surface file */
+  printf("Reading right inner surface file %s.\n",inner_mris_fname_rh);
+  inner_mris_rh=MRISread(inner_mris_fname_rh);
+  if (!inner_mris_rh) {
+    fprintf(stderr,"Could not read surface file %s.\n",inner_mris_fname_rh);
+    exit(1);
+  }
+
+  /* Read surface information from right outer surface file */
+  printf("Reading right outer surface file %s.\n",outer_mris_fname_rh);
+  outer_mris_rh=MRISread(outer_mris_fname_rh);
+  if (!outer_mris_rh) {
+    fprintf(stderr,"Could not read surface file %s.\n",outer_mris_fname_rh);
     exit(1);
   }
 
@@ -70,7 +75,7 @@ int main(int argc, char *argv[])
   }
 
   /* Read example volume from file */
-  if (argc==7) {
+  if (argc==8) {
     printf("Reading MRI volume directory %s.\n",mask_mri_pref);
     mri_mask=MRIread(mask_mri_pref);
     if (!mri_mask) {
@@ -83,7 +88,7 @@ int main(int argc, char *argv[])
 
   /* Extract ribbon */
   printf("Extracting ribbon.\n");
-  mri=MRISpartialribbon(inner_mris,outer_mris,mri_src,NULL,mri_mask,hemisphere);
+  mri=MRISpartialribbon(inner_mris_lh,outer_mris_lh,inner_mris_rh,outer_mris_rh,mri_src,NULL,mri_mask);
 
   /* Save MRI volume to directory */
   printf("Writing volume file %s.\n",output_mri_pref);
@@ -92,8 +97,10 @@ int main(int argc, char *argv[])
   MRIfree(&mri);
   MRIfree(&mri_src);
   MRIfree(&mri_mask);
-  MRISfree(&inner_mris);
-  MRISfree(&outer_mris);
+  MRISfree(&inner_mris_lh);
+  MRISfree(&outer_mris_lh);
+  MRISfree(&inner_mris_rh);
+  MRISfree(&outer_mris_rh);
 
   return 0;
 }
