9f34ccf76206b2aad54b05ca0b36e53df2dbb96c
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2002-01-10 00:38:10 +0000

    added support for different named lookup file (for CMA).

--- include/annotation.h
+++ include/annotation.h
@@ -2,8 +2,10 @@
 #define ANNOTATION_H
 
 int   read_annotation_table(void) ;
+int   read_named_annotation_table(char *fname) ;
 char  *annotation_to_name(int annotation, int *pindex) ;
 int   annotation_to_index(int annotation) ;
 int   print_annotation_table(FILE *fp);
+int   index_to_annotation(int index) ;
 
 #endif
