c0efa81be2c3beacc0748a334eb2196bde4ffe91
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-02-18 22:27:02 +0000

    Added function to read tlas. Added function to automatically determine >> whether an transform is a minc xfm or a tla.

--- include/registerio.h
+++ include/registerio.h
@@ -10,17 +10,20 @@ Routines for handling register.dat files
 #include "matrix.h"
 
 int regio_read_register(char *regfile, char **subject, float *inplaneres, 
-			float *betplaneres, float *intensity,  MATRIX **R,
-			int *float2int);
+      float *betplaneres, float *intensity,  MATRIX **R,
+      int *float2int);
 
 int regio_print_register(FILE *fp, char *subject, float inplaneres, 
-			 float betplaneres, float intensity, MATRIX *R,
-			 int float2int);
+       float betplaneres, float intensity, MATRIX *R,
+       int float2int);
 
 int regio_write_register(char *regfile, char *subject, float inplaneres, 
-			 float betplaneres, float intensity, MATRIX *R, 
-			 int float2int);
+       float betplaneres, float intensity, MATRIX *R, 
+       int float2int);
 
 int regio_read_mincxfm(char *xfmfile, MATRIX **R);
+int regio_read_xfm4(char *xfmfile, MATRIX **R);
+int regio_read_xfm(char *xfmfile, MATRIX **R);
+
 
 #endif /*BF_H_INC*/
