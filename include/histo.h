f89b0bd84c3f1410e665ab706bcf90113ca64132
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2002-05-07 22:06:46 +0000

    added findlastpeakrelative.

--- include/histo.h
+++ include/histo.h
@@ -39,6 +39,7 @@ HISTOGRAM *HISTOsubtract(HISTOGRAM *h1, HISTOGRAM *h2, HISTOGRAM *histo_dst) ;
 HISTOGRAM *HISTOclearBins(HISTOGRAM *h1, HISTOGRAM *h2, int b0, int b1) ;
 HISTOGRAM *HISTOsmooth(HISTOGRAM *histo_src, HISTOGRAM *histo_dst,float sigma);
 int       HISTOfindLastPeak(HISTOGRAM *h, int wsize, float min_pct) ;
+int       HISTOfindLastPeakRelative(HISTOGRAM *h, int wsize, float min_pct) ;
 int       HISTOfindFirstPeak(HISTOGRAM *h, int wsize, float min_pct) ;
 int       HISTOfindValley(HISTOGRAM *h, int wsize, int b0, int b1) ;
 int       HISTOfindLastPeakInRegion(HISTOGRAM *h, int wsize, float min_pct, 
