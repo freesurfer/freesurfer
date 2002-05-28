ebcc1c871ab2673d29027d02af7c2f692c10c9c2
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2002-05-28 21:59:43 +0000

    moved include of mri.h down to after LTA declaration.

--- include/transform.h
+++ include/transform.h
@@ -2,7 +2,6 @@
 #define TRANSFORM_H
 
 #include "matrix.h"
-#include "mri.h"
 
 typedef struct
 {
@@ -23,6 +22,7 @@ typedef struct
   int               type ;
 } LINEAR_TRANSFORM_ARRAY, LTA ;
 
+#include "mri.h"
 typedef struct
 {
   int        type ;
