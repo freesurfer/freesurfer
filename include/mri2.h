92722b6c5035596265dc986301e6649340408875
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-07-18 19:02:49 +0000

    Changed mri_binarize to allow invert.

--- include/mri2.h
+++ include/mri2.h
@@ -6,7 +6,8 @@ MRI *mri_load_bvolume(char *bfstem);
 int  mri_save_as_bvolume(MRI *vol, char *stem, int svendian, int svtype);
 MRI *mri_load_bvolume_frame(char *bfstem, int frameno);
 int  mri_framepower(MRI *vol, float *framepower);
-MRI *mri_binarize(MRI *vol, float thresh, char *sign, MRI *volbin, int *nover);
+MRI *mri_binarize(MRI *vol, float thresh, char *tail, int invert,
+      MRI *volbin, int *nover);
 MRI *mri_rescale(MRI *invol, float min, float max, MRI *outvol);
 int  mri_save_as_cor(MRI *vol,  char *cordir, int frame, int rescale);
 int mri_minmax(MRI *vol, float *min, float *max);
