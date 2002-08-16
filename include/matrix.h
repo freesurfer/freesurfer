63d132a15f3e4f760c0dd26d79ab025f5191533b
Kevin Teich <kteich@nmr.mgh.harvard.edu>
2002-08-16 21:01:39 +0000

    *** empty log message ***

--- include/matrix.h
+++ include/matrix.h
@@ -179,8 +179,8 @@ float MatrixTrace(MATRIX *M);
 MATRIX *MatrixVertCat(MATRIX *m1, MATRIX *m2, MATRIX *mcat);
 MATRIX *MatrixHorCat(MATRIX *m1, MATRIX *m2, MATRIX *mcat);
 
-MATRIX *MatrixConstVal(float val, int rows, int cols, MATRIX *X);
-MATRIX *MatrixZero(int rows, int cols, MATRIX *X);
+MATRIX *MatrixConstVal(float val, int rows, int cols, MATRIX *m);
+MATRIX *MatrixZero(int rows, int cols, MATRIX *m);
 
 #endif
 
