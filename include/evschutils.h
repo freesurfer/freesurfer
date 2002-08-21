3e26eb975a580e56cff9c1873147d7f3c787962e
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-08-21 23:47:12 +0000

    Utilities for optimizing event-related schedules.

---
+++ include/evschutils.h
@@ -0,0 +1,88 @@
+
+#ifndef EVSCHUTILS_H
+#define EVSCHUTILS_H
+
+#ifdef X
+  #undef X
+#endif
+
+#define EVS_COST_UNKNOWN   0
+#define EVS_COST_EFF       1
+#define EVS_COST_VRFAVG    2
+#define EVS_COST_VRFSTD    3
+#define EVS_COST_VRFAVGSTD 4
+
+typedef struct {
+
+  int    nevents;
+  float *tevent;
+  int   *eventid;
+  float *weight;
+
+  int    nEvTypes;
+  int   *nEvReps;
+  float  *EvDur;
+
+  int   nthsearched;
+  float cb1err;
+  float eff;
+  float vrfavg;
+  float vrfstd;
+  float vrfmin;
+  float vrfmax;
+  float vrfrange;
+  float cost;
+
+} EVENT_SCHEDULE, EVSCH;
+
+EVENT_SCHEDULE *AllocEvSch(int nevents, int allocweight);
+int EVSfree(EVENT_SCHEDULE **ppEvSch);
+int PrintEvSch(FILE *fp, EVENT_SCHEDULE *EvSch);
+
+EVENT_SCHEDULE *EVSreadPar(char *parfile);
+int EVSwritePar(char *parfile, EVENT_SCHEDULE *EvSch, 
+		char **lables, float TR, float tMax);
+int EVSmaxId(EVSCH *EvSch);
+
+EVENT_SCHEDULE *EVSsynth(int nEvTypes, int *nPer, float *tPer, 
+			 float tRes, float tMax, float tPreScan,
+			 int nCB1Search);
+
+EVENT_SCHEDULE *RandEvSch(int nevents, int ntypes, float dtmin, 
+			  float dtnullavg, int randweights);
+EVENT_SCHEDULE *SynthEvSch(int nEvTypes, int *nPer, float *tPer, 
+			   float tRes, float tMax, float tPreScan);
+MATRIX *EvSch2FIRMtx(int EvId, EVSCH *EvSch, float tDelay, float TR,
+		     int Ntps, float PSDMin, float PSDMax, float dPSD,
+		     MATRIX *X);
+MATRIX *EVSfirMtxAll(EVSCH *EvSch, float tDelay, float TR, int Ntps, 
+		     float PSDMin, float PSDMax, float dPSD);
+
+
+int EvSchSort(EVSCH **EvSchList, int nList);
+
+MATRIX *EVScb1ProbMatrix(EVSCH *EvSch);
+MATRIX *EVScb1IdealProbMatrix(EVSCH *EvSch);
+MATRIX *EVScb1Matrix(EVSCH *EvSch);
+float   EVScb1Error(EVSCH *EvSch);
+
+EVSCH *EVSRandSequence(int nEvTypes, int *nEvReps);
+int    EVSRandTiming(EVSCH *EvSch, float *EvDur, 
+		  float tRes, float tMax, float tPreScan);
+EVSCH *EVScb1Optimize(int nEvTypes, int *nEvReps, int nSearch);
+char  *EVScostString(int CostId);
+int    EVScostId(char *CostString);
+
+int EVSdesignMtxStats(MATRIX *Xtask, MATRIX *Xnuis, EVSCH *EvSch);
+float EVScost(EVSCH *EvSch, int CostId, float *params);
+
+int *RandPerm(int N, int *v);
+int  RandPermList(int N, int *v);
+
+MATRIX *EVSfirXtXIdeal(int nEvTypes, int *nEvReps, float *EvDur, 
+		       float TR, int Ntp, 
+		       float PSDMin, float PSDMax, float dPSD);
+
+#endif //#ifndef EVSCHUTILS_H
+
+
