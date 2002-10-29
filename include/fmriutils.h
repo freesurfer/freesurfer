67bb5b24896cf56be06b52129384d15663edb4d2
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-10-29 17:46:24 +0000

    Routines for performing functional analysis using the MRI structure.

---
+++ include/fmriutils.h
@@ -0,0 +1,17 @@
+
+#ifndef FMRIUTILS_INC
+#define FMRIUTILS_INC
+
+#include "matrix.h"
+#include "mri.h"
+
+#ifdef X
+#undef X
+#endif
+
+MRI *fMRImatrixMultiply(MRI *inmri, MATRIX *M, MRI *outmri);
+MRI *fMRIvariance(MRI *fmri, float DOF, int RmMean, MRI *var);
+MRI *fMRIcomputeT(MRI *ces, MATRIX *X, MATRIX *C, MRI *var, MRI *t);
+MRI *fMRIsigT(MRI *t, float DOF, MRI *p);
+
+#endif
