bffbaf417a3621f53dfa07a58752448243b32785
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-10-30 01:07:20 +0000

    Added routine to compute sum of squares.

--- include/fmriutils.h
+++ include/fmriutils.h
@@ -13,5 +13,6 @@ MRI *fMRImatrixMultiply(MRI *inmri, MATRIX *M, MRI *outmri);
 MRI *fMRIvariance(MRI *fmri, float DOF, int RmMean, MRI *var);
 MRI *fMRIcomputeT(MRI *ces, MATRIX *X, MATRIX *C, MRI *var, MRI *t);
 MRI *fMRIsigT(MRI *t, float DOF, MRI *p);
+MRI *fMRIsumSquare(MRI *fmri, int Update, MRI *sumsqr);
 
 #endif
