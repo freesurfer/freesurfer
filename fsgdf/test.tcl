c1d752703eb1f2a4677d66f4e010f403ab8cd284
Kevin Teich <kteich@nmr.mgh.harvard.edu>
2002-11-19 23:33:47 +0000

    *** empty log message ***

--- fsgdf/test.tcl
+++ fsgdf/test.tcl
@@ -12,11 +12,27 @@ FsgdfPlot_Init
 set err [FsgdfPlot_ParseHeader "$fnTestDataDir/fsgdf/y-lh.fsgd"]
 if { $err } { puts "!!!! FsgdfPlot_ParseHeader failed." ; exit }
 FsgdfPlot_ShowWindow
-FsgdfPlot_SetPoint 10000 0 0
-FsgdfPlot_SetInfo "vno 10000"
 
-FsgdfPlot_SaveToTable test.table
-FsgdfPlot_SaveToPostscript test.ps
+#FsgdfPlot_SaveToTable test.table
+#FsgdfPlot_SaveToPostscript test.ps
+
+
+proc ScheduleRedraw {} {
+    global gRedraws
+    after 500 {
+	set vno [expr round(rand() * 10000)]
+	FsgdfPlot_SetPoint $vno 0 0
+	FsgdfPlot_SetInfo "vno $vno"
+	incr gRedraws -1
+	if { $gRedraws > 0 } { ScheduleRedraw }
+    }
+}
+
+set gRedraws 10
+ScheduleRedraw
+
+
+
 
 if { 0 } {
 after 5000 {
