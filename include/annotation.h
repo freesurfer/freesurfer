72ca6296bf8e482a6b320846e2992fdfe0398044
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-12-11 21:00:35 +0000

    Added global variable to be able to spec the annotaion table file.

--- include/annotation.h
+++ include/annotation.h
@@ -1,6 +1,13 @@
 #ifndef ANNOTATION_H
 #define ANNOTATION_H
 
+
+#ifdef ANNOTATION_SRC
+char *annotation_table_file = NULL;
+#else
+extern char *annotation_table_file;
+#endif
+
 int   read_annotation_table(void) ;
 int   read_named_annotation_table(char *fname) ;
 char  *annotation_to_name(int annotation, int *pindex) ;
