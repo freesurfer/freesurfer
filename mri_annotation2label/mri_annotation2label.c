8f47a3359a9115014862e79e9317ebab04fcbb71
Doug Greve <greve@nmr.mgh.harvard.edu>
2002-12-11 21:01:40 +0000

    Added requirement that user specify the annotation table file. Added ability to specify that the labels be saved using the names from the annotation table.

--- mri_annotation2label/mri_annotation2label.c
+++ mri_annotation2label/mri_annotation2label.c
@@ -1,51 +1,9 @@
 /*----------------------------------------------------------
   Name: mri_annotation2label.c
-  $Id: mri_annotation2label.c,v 1.5 2002/10/23 15:56:09 fischl Exp $
+  $Id: mri_annotation2label.c,v 1.6 2002/12/11 21:01:40 greve Exp $
   Author: Douglas Greve
   Purpose: Converts an annotation to a labels.
 
-  User specifies the subject, hemisphere, label base, and 
-  (optionally) the annotation base. By default, the annotation
-  base is aparc. The program will get the annotations from
-  SUBJECTS_DIR/subject/label/hemi_annotbase.annot. A separate
-  label file is created for each annotation index; the name of
-  the file conforms to labelbase-XXX.label, where XXX is the
-  zero-padded 3 digit annotation index. If labelbase includes
-  a directory path, that directory must already exist. If there
-  are no points in the annotation file for a particular index,
-  no label file is created.
-
-  Example:
-
-  mri_annotation2label --subject LW --hemi rh --labelbase ./labels/aparc-rh
-
-  This will get annotations from $SUBJECTS_DIR/LW/label/rh_aparc.annot
-  and then create about 94 label files: aparc-rh-001.label, 
-  aparc-rh-002.label, ... Note that the directory labels must already
-  exist. 
-
-  The human readable names that correspond to the annotation index can
-  be found in:
-
-     $MRI_DIR/christophe_parc.txt
-
-  Testing:
-  1. Load annotations in tksurfer:  
-       tksurfer -LW lh inflated
-       % read_annotations lh_aparc
-     When a point is clicked on, it prints out a lot of info, including
-       annot = S_temporalis_sup (93, 3988701) (221, 220, 60)
-     This indicates that annotion number 93 was hit. Save this point.
-   
-  2. Load label into tksurfer (new job):
-       tksurfer -LW lh inflated
-       [edit label field and read]
-     Verify that label pattern looks like that from the annotation
-
-  3. Load label into tkmedit
-       tkmedit LW T1
-       [Load the label]
-       [Goto the point saved from step 1]
   -----------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -72,14 +30,16 @@ static int  singledash(char *flag);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_annotation2label.c,v 1.5 2002/10/23 15:56:09 fischl Exp $";
+static char vcid[] = "$Id: mri_annotation2label.c,v 1.6 2002/12/11 21:01:40 greve Exp $";
 char *Progname = NULL;
 
 char  *subject   = NULL;
 char  *annotation = "aparc";
 char  *hemi       = NULL;
-char  *labelbase = NULL;
 char  *surfacename = "white";
+
+char  *outdir = NULL;
+char  *labelbase = NULL;
 
 MRI_SURFACE *Surf;
 LABEL *label     = NULL;
@@ -94,6 +54,7 @@ char annotfile[1000];
 char labelfile[1000];
 int  nperannot[1000];
 
+
 /*-------------------------------------------------*/
 /*-------------------------------------------------*/
 /*-------------------------------------------------*/
@@ -101,6 +62,9 @@ int main(int argc, char **argv)
 {
   VERTEX *vtx;
   int nthpoint,err,vtxno,ano,ani,vtxani,animax;
+  int annotnum;
+
+  annotation_table_file = NULL; /* defined in annoation.h */
 
   printf("\n");
 
@@ -159,9 +123,17 @@ int main(int argc, char **argv)
       printf("%3d  %5d   --- skipping \n",ani,nperannot[ani]);
       continue;
     }
-    printf("%3d  %5d\n",ani,nperannot[ani]);
 
-    sprintf(labelfile,"%s-%03d.label",labelbase,ani);
+    if(labelbase != NULL) 
+      sprintf(labelfile,"%s-%03d.label",labelbase,ani);
+    if(outdir != NULL){
+      annotnum = index_to_annotation(ani);
+      sprintf(labelfile,"%s/%s.%s.label",outdir,hemi,
+	      annotation_to_name(annotnum,NULL));
+    }
+
+    printf("%3d  %5d %s\n",ani,nperannot[ani],labelfile);
+
     label = LabelAlloc(nperannot[ani],subject,labelfile);
     label->n_points = nperannot[ani];
 
@@ -171,11 +143,11 @@ int main(int argc, char **argv)
       ano = Surf->vertices[vtxno].annotation;
       vtxani = annotation_to_index(ano);
       if(vtxani == ani){
-  label->lv[nthpoint].vno = vtxno;
-  label->lv[nthpoint].x = vtx->x;
-  label->lv[nthpoint].y = vtx->y;
-  label->lv[nthpoint].z = vtx->z;
-  nthpoint ++;
+	label->lv[nthpoint].vno = vtxno;
+	label->lv[nthpoint].x = vtx->x;
+	label->lv[nthpoint].y = vtx->y;
+	label->lv[nthpoint].z = vtx->z;
+	nthpoint ++;
       }
     }
 
@@ -227,9 +199,19 @@ static int parse_commandline(int argc, char **argv)
       labelbase = pargv[0];
       nargsused = 1;
     }
+    else if (!strcmp(option, "--outdir")){
+      if(nargc < 1) argnerr(option,1);
+      outdir = pargv[0];
+      nargsused = 1;
+    }
     else if (!strcmp(option, "--annotation")){
       if(nargc < 1) argnerr(option,1);
       annotation = pargv[0];
+      nargsused = 1;
+    }
+    else if (!strcmp(option, "--table")){
+      if(nargc < 1) argnerr(option,1);
+      annotation_table_file = pargv[0];
       nargsused = 1;
     }
     else if (!strcmp(option, "--hemi")){
@@ -257,17 +239,20 @@ static void usage_exit(void)
 /* --------------------------------------------- */
 static void print_usage(void)
 {
-  fprintf(stdout, "USAGE: %s \n",Progname) ;
-  fprintf(stdout, "\n");
-  fprintf(stdout, "   --subject    source subject\n");
-  fprintf(stdout, "   --hemi       hemisphere (lh or rh) (with surface)\n");
-  fprintf(stdout, "   --labelbase  output will be base-XXX.label \n");
-  fprintf(stdout, "\n");
-  fprintf(stdout, "   --annotation as found in SUBJDIR/labels <aparc>\n");
-  fprintf(stdout, "   --surface    name of surface <white>\n");  
-  fprintf(stdout, "   --help       display help\n");  
-  fprintf(stdout, "   --version    display version\n");  
-  fprintf(stdout, "\n");
+  printf("USAGE: %s \n",Progname) ;
+  printf("\n");
+  printf("   --subject    source subject\n");
+  printf("   --hemi       hemisphere (lh or rh) (with surface)\n");
+  printf("   --table      annotation table file\n");  
+  printf("   --labelbase  output will be base-XXX.label \n");
+  printf("   --outdir dir :  output will be dir/hemi.name.label \n");
+  printf("\n");
+  printf("   --annotation as found in SUBJDIR/labels <aparc>\n");
+  printf("   --surface    name of surface <white>\n");  
+
+  printf("   --help       display help\n");  
+  printf("   --version    display version\n");  
+  printf("\n");
 }
 /* --------------------------------------------- */
 static void dump_options(FILE *fp)
@@ -275,7 +260,8 @@ static void dump_options(FILE *fp)
   fprintf(fp,"subject = %s\n",subject);
   fprintf(fp,"annotation = %s\n",annotation);
   fprintf(fp,"hemi = %s\n",hemi);
-  fprintf(fp,"labelbase = %s\n",labelbase);
+  if(labelbase) fprintf(fp,"labelbase = %s\n",labelbase);
+  if(outdir) fprintf(fp,"outdir = %s\n",labelbase);
   fprintf(fp,"surface   = %s\n",surfacename);
   fprintf(fp,"\n");
   return;
@@ -291,17 +277,25 @@ static void print_help(void)
 "the annotation base and surface. By default, the annotation base is\n"
 "aparc. The program will retrieves the annotations from\n"
 "SUBJECTS_DIR/subject/label/hemi_annotbase.annot. A separate label file\n"
-"is created for each annotation index; the name of the file conforms to\n"
+"is created for each annotation index. The output file names can take\n"
+"one of two forms: (1) If --outdir dir is used, then the output will\n"
+"be dir/hemi.name.lable, where name is the corresponding name in \n"
+"the table. (2) If --labelbase is used, name of the file conforms to\n"
 "labelbase-XXX.label, where XXX is the zero-padded 3 digit annotation\n"
 "index. If labelbase includes a directory path, that directory must\n"
 "already exist. If there are no points in the annotation file for a\n"
 "particular index, no label file is created. The xyz coordinates in the\n"
 "label file are obtained from the values at that vertex of the\n"
 "specified surface. The default surface is 'white'. Other options\n"
-"include 'pial' and 'orig'.  The human-readable names that correspond\n"
-"to the annotation indices for aparc can be found in\n"
-"$MRI_DIR/christophe_parc.txt.  This annotation key also appears at the\n"
-"end of this help.\n"
+"include 'pial' and 'orig'.\n"
+"\n"
+"The human-readable names that correspond\n"
+"to the annotation indices for aparc depend upon how the annotations\n"
+"were created. They are created with the program mris_ca_label, and\n"
+"the human readable names are in the file given as the argument to \n"
+"the -t flag. Unfortunately, this information is not maintained inside\n"
+"the annotation file, and it must be supplied to mri_annotation2label \n"
+"through the --table flag.\n"
 " \n"
 "Bugs:\n"
 "\n"
@@ -312,18 +306,29 @@ static void print_help(void)
 "\n"
 "Example:\n"
 "\n"
-"  mri_annotation2label --subject LW --hemi rh --labelbase ./labels/aparc-rh\n"
+"  mri_annotation2label --subject LW --hemi rh \n"
+"        --labelbase ./labels/aparc-rh \n"
+"        --table $FREESURFER_HOME/Simple_surface_labels2002.txt \n"
 "\n"
 "  This will get annotations from $SUBJECTS_DIR/LW/label/rh_aparc.annot\n"
 "  and then create about 94 label files: aparc-rh-001.label, \n"
 "  aparc-rh-002.label, ... Note that the directory 'labels' must already\n"
 "  exist. \n"
 "\n"
+"Example:\n"
+"\n"
+"  mri_annotation2label --subject LW --hemi rh \n"
+"        --outdir ./labels \n"
+"        --table $FREESURFER_HOME/Simple_surface_labels2002.txt \n"
+"\n"
+"  This will do the same thing as above except that the output files\n"
+"  will have names of the form lh.S_occipital_anterior.label\n"
+"\n"
 "Testing:\n"
 "\n"
 "  1. Start tksurfer:  \n"
 "       tksurfer -LW lh inflated\n"
-"       % read_annotations lh_aparc\n"
+"       read_annotations lh_aparc\n"
 "     When a point is clicked on, it prints out a lot of info, including\n"
 "     something like:\n"
 "       annot = S_temporalis_sup (93, 3988701) (221, 220, 60)\n"
@@ -340,15 +345,15 @@ static void print_help(void)
 "       [Load the label]\n"
 "      [Goto the point saved from step 1] \n\n\n");
 
-  printf("Annotation Key\n");
-  print_annotation_table(stdout);
+  //printf("Annotation Key\n");
+  //print_annotation_table(stdout);
 
   exit(1) ;
 }
 /* --------------------------------------------- */
 static void print_version(void)
 {
-  fprintf(stdout, "%s\n", vcid) ;
+  printf("%s\n", vcid) ;
   exit(1) ;
 }
 /* --------------------------------------------- */
@@ -367,12 +372,20 @@ static void check_options(void)
     fprintf(stderr,"ERROR: no source subject specified\n");
     exit(1);
   }
-  if(labelbase == NULL){
-    fprintf(stderr,"ERROR: No label base specified\n");
-    exit(1);
-  }
   if(hemi == NULL){
     fprintf(stderr,"ERROR: No hemisphere specified\n");
+    exit(1);
+  }
+  if(annotation_table_file == NULL){
+    fprintf(stderr,"ERROR: No annotation table specified\n");
+    exit(1);
+  }
+  if(outdir == NULL && labelbase == NULL){
+    fprintf(stderr,"ERROR: no output specified\n");
+    exit(1);
+  }
+  if(outdir != NULL && labelbase != NULL){
+    fprintf(stderr,"ERROR: cannot specify both --outdir and --labelbase\n");
     exit(1);
   }
 
