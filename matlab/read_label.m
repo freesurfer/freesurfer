025656afe3abba9f66bff5d110bd7d5f71ea4200
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-03-11 22:26:34 +0000

    Added ability to ignore subject name and treat lname as a full path.

--- matlab/read_label.m
+++ matlab/read_label.m
@@ -1,22 +1,39 @@
 function [l] = read_label(sname, lname)
-
-% l = read_label(sname, lname)
-
+% l = read_label(<sname>, lname)
 %
 % reads the label file 'lname' from the subject 'sname' 
 % in the subject's label directory into the vector l
+% l will be nvertices-by-5, where each column means:
+% (1) vertex number, (2-4) xyz at each vertex, (5) stat
 %
 
-sdir = getenv('SUBJECTS_DIR') ;
-fname = sprintf('%s/%s/label/%s.label', sdir, sname, lname) ;
+l = [];
+
+if(nargin ~= 2)
+  fprintf('l = read_label(<sname>, lname)\n');
+  return;
+end
+
+if(~isempty(sname))
+  sdir = getenv('SUBJECTS_DIR') ;
+  fname = sprintf('%s/%s/label/%s.label', sdir, sname, lname) ;
+else
+  fname = lname;
+end
 
 % open it as an ascii file
 fid = fopen(fname, 'r') ;
 fgets(fid) ;
+if(fid == -1)
+  fprintf('ERROR: could not open %s\n',fname);
+  return;
+end
+
 line = fgets(fid) ;
 nv = sscanf(line, '%d') ;
 l = fscanf(fid, '%d %f %f %f %f\n') ;
 l = reshape(l, 5, nv) ;
 l = l' ;
+
 fclose(fid) ;
 
