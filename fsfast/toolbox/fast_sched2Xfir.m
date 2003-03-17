42c74b34e4530b39b17f4a833fb5d1cff58b4532
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-03-17 06:29:23 +0000

    Changed input args. Added ability to include convolution with boxcar.

--- fsfast/toolbox/fast_sched2Xfir.m
+++ fsfast/toolbox/fast_sched2Xfir.m
@@ -1,18 +1,17 @@
-function X = fast_sched2Xfir(tPres,ntrs,TR,TER,TPreStim,TimeWindow,W)
+function X = fast_sched2Xfir(tPres,ntrs,TR,IRFPSD,BCW,tDelay,PerEvW)
 %
-% X = fast_sched2Xfir(tPres,ntrs,TR,TER,TPreStim,TimeWindow,W)
+% X = fast_sched2Xfir(tPres,ntrs,TR,IRFPSD,BCW,tDelay,PerEvW)
 %
 % Creates a design matrix (aka stimulus convolution matrix) modeling
 % the hemodynamic response as an FIR with adjustable tap weights. The
 % matrix is for a single event type whose schedule is passed by tPres.
-% The size of the matrix will be ntrs by Nh, Nh = TimeWindow/TER.
 %
 % tPres - list of presentation times (ie, the schedule) in seconds 
-% for one event type. Time=0 is defined as the time the first stored 
-% image was collected (ie, ignore discarded acquisitions). An event
-% before t=0 will have an effect on the matrix if its Post Stimulus 
-% Window encompasses t=0; otherwise it is ignored. Events found
-% after the termination of data collection are ignored (a warning 
+% for one event type. Time=tDelay is defined as the time the first 
+% stored image was collected (ie, ignore discarded acquisitions). An 
+% event before t=0 will have an effect on the matrix if its Post 
+% Stimulus Window encompasses t=0; otherwise it is ignored. Events 
+% found after the termination of data collection are ignored (a warning 
 % is printed). If tPres is empty, a matrix of zeros is returned. 
 %
 % ntrs - number of functional volumes collected (do not include 
@@ -21,66 +20,91 @@ function X = fast_sched2Xfir(tPres,ntrs,TR,TER,TPreStim,TimeWindow,W)
 % TR - TR of the exmperiment (ie, time between acquisitions of 
 % functional volumes) in seconds.
 %
-% TER - temporal estimation resolution (in seconds). This is the
-% resolution at which the hemodynamic response is reconstructed.
-% The TER must be an integer divisor of the TR. Note: arbitrary
-% schedules cannot be used to perform sub TR estimation. The 
-% schudule must have been designed for that purpose.
+% IRFPSD - three component vector indicating the Post Stimulus
+% Delay window of the Impulse Response Function. The three
+% components are: PSDMin dPSD PSDMax. dPSD has also been called
+% the TER. Note: this models the Impulse Response only. The
+% Event Response is the Impulse Response convolved with 
+% a Box Car (see BCW)
 %
-% TPreStim - prestimulus window (in seconds). This creates a matrix
-% which will allow averaging to begin before the stimulus onset.
-% Must be a integer multiple of the TER.
+% BCW - Box Car Width (in sec). The Event Response is the Impulse
+% Response convolved with a Box Car of width BCW. The Event
+% Response PSD Window is that of the IRF except that 
+% ERFPSDMax = IRFPSDMax + BCW.
 %
-% TimeWindow - total time window (in seconds) in which the hemodynamic
-% response will be modeled, including the TPreStim. Must be a integer 
-% multiple of the TER. The Post Stimulus Window = TimeWindow - TPreStim.
+% tDelay - set the definition of t=0. This can be handy for
+% taking the slice acquisition delay into account. Ie, creating
+% a different X matrix for each slice. In this case, set tDelay
+% equal to the time between when the first slice is acquired 
+% and the nth slice.
 %
-% W - presentation weighting. Length should be NPresentations. The
+% PerEvW - presentation weighting. Length should be NPresentations. The
 % matrix entry for each presentation is given the value W(n) instead
 % of 1. Ignored if W=[].
 %
-% $Id: fast_sched2Xfir.m,v 1.2 2003/03/14 04:17:33 greve Exp $ 
+% X will have size: Ntp by (ERFPSDMax-IRFPSDMin)/dIRFPSD
+% 
+% $Id: fast_sched2Xfir.m,v 1.3 2003/03/17 06:29:23 greve Exp $ 
 
 X = [];
 
-if(nargin ~= 7)
-  msg = 'USAGE: X = fast_sched2Xfir(tPres,ntrs,TR,TER,TPreStim,TimeWindow,W)';
+if(nargin < 4 & nargin > 7)
+  msg = 'X = fast_sched2Xfir(tPres,ntrs,TR,IRFPSD,<BCW,tDelay,PerEvW>)';
   fprintf('%s\n',msg);
   return;
 end
 
-% Check that the TR is an integer mult of the TER %
-if(rem(TR,TER) ~= 0)
-  msg = sprintf('ERROR: TR (%g) must be a multiple of TER (%g)',...
-		TR,TER);
+if(exist('BCW') ~= 1) BCW = 0; end
+if(exist('tDelay') ~= 1) tDelay = 0; end
+
+% Compute Event Response Function Post Stimulus Window %
+PSDMin = IRFPSD(1);
+dPSD   = IRFPSD(2);
+PSDMax = IRFPSD(3)+BCW;
+
+% Check that the TR is an integer mult of the dPSD %
+if(rem(TR,dPSD) ~= 0)
+  msg = sprintf('ERROR: TR (%g) must be a multiple of dPSD (%g)',...
+		TR,dPSD);
   fprintf('%s\n',msg);
   return;
 end
 
-% Check that the TW is an interger mult of the TER %
-if(rem(TimeWindow,TER) ~= 0)
-  msg = sprintf('ERROR: TimeWindow (%g) must be a multiple of TER (%g)',...
-		TimeWindow,TER);
+% Check that the PSDMin is an integer mult of the dPSD %
+if(rem(PSDMin,dPSD) ~= 0)
+  msg = sprintf('ERROR: PSDMin (%g) must be a multiple of dPSD (%g)',...
+		PSDMin,dPSD);
   fprintf('%s\n',msg);
   return;
 end
 
-% Check that the TPreStim is an interger mult of the TER %
-if(rem(TPreStim,TER) ~= 0)
-  msg = sprintf('ERROR: TPreStim (%g) must be a multiple of TER (%g)',...
-		TPreStim,TER);
+% Check that the BCW is an integer mult of the dPSD %
+if(rem(BCW,dPSD) ~= 0)
+  msg = sprintf('ERROR: BCW (%g) must be a multiple of dPSD (%g)',...
+		BCW,dPSD);
   fprintf('%s\n',msg);
   return;
 end
+
+% Check that the PSDMax is an integer mult of the dPSD %
+if(rem(PSDMax,dPSD) ~= 0)
+  msg = sprintf('ERROR: PSDMax (%g) must be a multiple of dPSD (%g)',...
+		PSDMax,dPSD);
+  fprintf('%s\n',msg);
+  return;
+end
+
+TimeWindow = PSDMax - PSDMin;
 
 % Compute time of last acq
 tmax = TR*(ntrs - 1);
 % Compute the resampling rate
-Rss = TR/TER;
+Rss = TR/dPSD;
 % Compute the number of Estimates
-Nh = round(TimeWindow/TER);
-% Compute the Post Stimulus Window
-TPostStim = TimeWindow - TPreStim - TER;
+Nh = round(TimeWindow/dPSD);
+% Compute the Post Stimulus Delay at the start of the last 
+% point in the window
+TPostStim = PSDMax - dPSD;
 
 % Number of presentations
 Npres = length(tPres);
@@ -90,27 +114,43 @@ if(Npres == 0)
 end
 tPres = reshape(tPres,[Npres 1]); % Just to make sure %
 
-if(isempty(W)) W = ones(Npres,1); end
-if(length(W) ~= Npres)
-  fprintf('ERROR: length(W) (%d) does not equal Npres (%d)\n',length(W),Npres);
+if(exist('PerEvW') ~= 1) PerEvW = []; end
+if(isempty(PerEvW)) PerEvW = ones(Npres,1); end
+if(length(PerEvW) ~= Npres)
+  fprintf('ERROR: length(PerEvW) (%d) does not equal Npres (%d)\n',...
+	  length(PerEvW),Npres);
   return;
 end
-W = reshape(W,[Npres 1]); % Just to make sure %
+PerEvW = reshape(PerEvW,[Npres 1]); % Just to make sure %
+
+% Subtract tDelay from tPres
+tPres = tPres - tDelay;
 
 X = zeros(Rss*ntrs,Nh);
 h = 1;
-for d = -TPreStim:TER:TPostStim,
+for d = PSDMin:dPSD:TPostStim,
    td = tPres+d;
    iok = find(td >= 0 & td <= tmax);
    td = td(iok);
-   iPres = round(td/TER)+1;
-   X(iPres,h) = W(iok);
+   iPres = round(td/dPSD)+1;
+   X(iPres,h) = PerEvW(iok);
    h = h + 1;
 end
 
 % SubSample %
 if(Rss ~= 1)
   X = X(1:Rss:Rss*ntrs,:);
+end
+
+if(BCW ~= 0)
+  Nb = BCW/dPSD;
+  Na = (IRFPSD(3)-PSDMin)/dPSD;
+  P = [zeros(Nb-1,Na); eye(Na); ];
+  b = [ones(1,Nb) zeros(1,Na-1)];
+  c = zeros(Na+Nb,1);
+  c(1) = 1;
+  B = toeplitz(c,b);
+  X = X*B*P;
 end
 
 return;
