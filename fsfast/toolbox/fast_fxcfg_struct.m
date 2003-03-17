3c7666c14a2f5633b3b2af5672291192e949b4fc
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-03-17 06:31:59 +0000

    Defines structure for fsfast effect configuration.

---
+++ fsfast/toolbox/fast_fxcfg_struct.m
@@ -0,0 +1,24 @@
+function fxcfg = fast_fxcfg_struct
+% Create an FS-FAST Effects Configuration structure with all 
+% the fields. 
+
+fxcfg.fxtype = '';  % fixed or random
+fxcfg.label = '';   % effect name
+fxcfg.model = '';   % model string
+fxcfg.params = [];  % non-string parameters
+fxcfg.sparams = []; % string parameters
+fxcfg.M = [];       % Generic matrix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
