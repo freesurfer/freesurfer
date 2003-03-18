faaa235ca73f30d233d65d2eee5c93eff94b9941
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-03-18 06:19:42 +0000

    Changed evsch to evschlist, which is an array, each with its own evsch matrix. This matrix will be [tPres EvId <wPres>].

--- fsfast/toolbox/fast_sesscfg_struct.m
+++ fsfast/toolbox/fast_sesscfg_struct.m
@@ -7,11 +7,13 @@ sesscfg.ntp = [];           % Ntp for each run
 sesscfg.runweight = [];     % List of weights for each run
 sesscfg.fstemlist = '';     % List of abs path to func stems for each run
 sesscfg.evschfilelist = ''; % List of abs path to EvSch Files
-sesscfg.evsch = [];         % Array of event schedules, one for each run
+sesscfg.evschlist = [];     % Array of event schedules, one for each run
 
-% evsch.tpres(nthrun)
-% evsch.evidpres(nthrun)
-% evsch.wpres(nthrun)
+% evschlist(nthrun).evsch
+% evsch must have at least 2 columns:
+% (1) presentation time
+% (2) event id (-1 for tpx)
+% (3) presentation weight (optional)
 
 return;
 
