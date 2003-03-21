9aa25a998ecfd3adae551924d344e32ddb9ad659
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-03-21 05:19:06 +0000

    boxcarmat produces a matrix that will convolve an irf with a boxcar. extreg is the fxcfg for an external regressor.

---
+++ fsfast/toolbox/fast_boxcarmat.m
@@ -0,0 +1,49 @@
+function B = fast_boxcarmat(psdwin)
+% B = fast_boxcarmat(psdwin)
+%
+% Creates a matrix that implements convolution with a boxcar.
+%
+% X = Xfir * B * Airf, Aerf = B * Airf
+%   Xfir is built assuming the ERF
+%
+% B will be nerfpsdwin by nirfpsdwin
+%
+% $Id: fast_boxcarmat.m,v 1.1 2003/03/21 05:19:06 greve Exp $
+
+B = [];
+
+if(nargin ~= 1)
+  fprintf('B = fast_boxcarmat(psdwin)\n');
+  return;
+end
+
+if(fast_psdwin(psdwin,'check') ~= 1) return; end
+
+psdmin = psdwin(1);
+dpsd   = psdwin(2);
+psdmax = psdwin(3);
+if(length(psdwin) == 3) bcw = 0;
+else                    bcw = psdwin(4);
+end
+
+Na = fast_psdwin(psdwin,'nirfpsdwin');
+Nb = bcw/dpsd;
+
+if(bcw == 0)
+  B = eye(Na);
+  return;
+end
+
+% M1 just pads Airf to be long enough to multiply
+% by boxcar matrix
+M1 = [zeros(Nb-1,Na); eye(Na); ];
+
+% M2 is the transpose of an Xfir for a boxcar
+b = [ones(1,Nb) zeros(1,Na-1)];
+c = zeros(Na+Nb-1,1);
+c(1) = 1;
+M2 = toeplitz(c,b);
+
+B = M2*M1;
+
+return;
---
+++ fsfast/toolbox/fast_fxcfg_extreg.m
@@ -0,0 +1,206 @@
+function rt = fast_fxcfg_extreg(DoWhat,thing)
+% rt = fast_fxcfg_extreg(DoWhat,thing)
+%
+% DoWhat can be:
+%  iserm  - returns 0 because this is not an erm. thing not needed.
+%  nparams - returns number of parameters in model. thing not needed.
+%  nregressors - number of regressors in current X matrix. thing=flacfg
+%  matrix - X matrix. thing=flacfg
+%  amatrix - not applicable.
+%  parseline - parses the line, thing = line
+%  createline - create a model line. thing=flacfg
+%  autopsd - not applicable
+%
+% thing - line to be parsed or flacfg (see fast_flacfg_struct).
+%
+% ExtReg Parameters:
+%  1. fname - name relative to fsd/RRR (stored in sparams not params)
+%  2. nextreg  - number of extreg to use (-1 for all) (stored in params)
+% $Id: fast_fxcfg_extreg.m,v 1.1 2003/03/21 05:19:06 greve Exp $
+
+rt = [];
+
+if(nargin ~= 1 & nargin ~= 2)
+  fprintf('rt = fast_fxcfg_extreg(DoWhat,<thing>)\n');
+  return;
+end
+
+if(exist('thing') ~= 1) 
+  thing = []; 
+  flacfg = [];
+  line = [];
+else
+  if(isfield(thing,'flaname')) 
+    flacfg = thing;
+    line = [];
+  else 
+    line = thing;
+    flacfg = [];
+  end
+end
+
+%----------------------------%
+DoWhat = lower(DoWhat);
+switch(DoWhat)
+ 
+ case 'iserm'
+  rt = 0;
+ 
+ case 'nparams'
+  rt = 2;
+ 
+ case 'nregressors'
+  if(isempty(flacfg)) pr_fla_needed(DoWhat); return; end
+  rt = get_nregressors(flacfg);
+ 
+ case 'autopsd'
+  fprintf('ERROR: extreg: cannot autopsd\n');
+  return;
+ 
+ case 'amatrix'
+  fprintf('ERROR: extreg: cannot amatrix\n');
+  return;
+ 
+ case 'matrix'
+  if(isempty(flacfg)) pr_fla_needed(DoWhat); return; end
+  X = get_matrix(flacfg);
+  if(isempty(X)) return; end
+  rt = X;
+ 
+ case 'parseline'
+  if(isempty(line))
+    fprintf('ERROR: extreg: line needed with parseline\n');
+    return;
+  end
+  rt = parseline(line);
+  
+ case 'createline'
+  if(isempty(flacfg)) pr_fla_needed(DoWhat); return; end
+  rt = createline(flacfg);
+ 
+ otherwise
+  fprintf('ERROR: fast_fxcfg_extreg: getwhat = %s, unrecognized\n',getwhat);
+
+end
+
+return;
+
+%------------------------------------------------------------%
+function pr_fla_needed(DoWhat)
+fprintf('ERROR: flacfg needed with %s\n',DoWhat);
+return;
+
+%------------------------------------------------------------%
+function fxcfg = parseline(line)
+% Read and check input line
+% InputLine: Effect F/R Label ExtReg fname 
+% Parameters: 
+%  1. fname - name relative to fsd/RRR (stored in sparams)
+%  2. nextreg (-1 for all) (stored in params)
+
+fxcfg = [];
+
+nparams = fast_fxcfg_extreg('nparams');
+[tmp nitems] = sscanf(line,'%s',inf);
+if(nitems ~= nparams + 4)
+  fprintf('ERROR: extreg: line has wrong number of items (%d)\n',nitems);
+  fprintf('%s\n',line);
+  return;
+end
+
+fxcfg = fast_fxcfg_struct;
+
+[fxcfg.fxtype n]  = sscanf(line,'%*s %s ',1);
+[fxcfg.label  n]  = sscanf(line,'%*s %*s %s',1);
+[fxcfg.model  n]  = sscanf(line,'%*s %*s %*s %s',1);
+
+fxcfg.fxtype = lower(fxcfg.fxtype);
+fxcfg.model = lower(fxcfg.model);
+
+if(~strcmp(fxcfg.model,'extreg'))
+  fprintf('ERROR: extreg: model not extreg (%s)\n',fxcfg.model);
+  fprintf('%s\n',line);
+  fxcfg = [];
+  return;
+end
+
+fxcfg.sparams = sscanf(line,'%*s %*s %*s %*s %s',1);
+fxcfg.params  = sscanf(line,'%*s %*s %*s %*s %*s %d',1);
+return;
+
+%-----------------------------------------------------------%
+function line = createline(flacfg)
+line = [];
+
+fxcfg = fast_fxcfg('getfxcfg',flacfg);
+if(isempty(fxcfg)) return; end
+
+line = sprintf('Effect %s %s %s %s',fxcfg.fxtype,...
+	       fxcfg.label,fxcfg.model,fxcfg.sparams(1));
+
+return;
+
+%------------------------------------------------------------%
+function nr = get_nregressors(flacfg)
+
+nr = [];
+
+fxcfg = fast_fxcfg('getfxcfg',flacfg);
+if(isempty(fxcfg)) return; end
+
+if(isempty(fxcfg.npmlist(flacfg.nthrun).M))
+  fprintf('ERROR: extreg: extreg not loaded\n');
+  return;
+end
+
+nextreg = fxcfg.params(1);
+if(nextreg == -1)
+  nr = size(fxcfg.npmlist(flacfg.nthrun).M,2);
+else
+  nr = nextreg;
+end
+
+return;
+
+%------------------------------------------------------------%
+function  X = get_matrix(flacfg)
+X = [];
+
+ntp = fast_fxcfg('getntp',flacfg);
+if(isempty(ntp)) return; end
+fxcfg = fast_fxcfg('getfxcfg',flacfg);
+if(isempty(fxcfg)) return; end
+if(isempty(fxcfg.npmlist(flacfg.nthrun).M))
+  fprintf('ERROR: extreg: extreg not loaded\n');
+  return;
+end
+M = fxcfg.npmlist(flacfg.nthrun).M;
+if(size(M,1) ~= ntp)
+  fprintf('ERROR: extreg: extreg dimension mismatch\n');
+  return;
+end
+
+% Really do this?
+M = M - repmat(mean(M), [ntp 1]);
+M = M./repmat(std(M), [ntp 1]);
+
+nextreg = fxcfg.params(1);
+if(nextreg == -1)
+  X = M;
+else
+  if(nextreg > size(M,2))
+    fprintf('ERROR: extreg: extreg does not have enough cols\n');
+    return;
+  end
+  X = M(:,1:nextreg);
+end
+
+% tpx and nskip are handled in fast_fxcfg('matrix',flacfg)
+
+return;
+%------------------------------------------------------------%
+
+
+
+
+
