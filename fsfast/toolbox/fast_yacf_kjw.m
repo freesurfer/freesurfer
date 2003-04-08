db4a126c87314c745b656e34fca798da16ee31ac
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-04-08 04:49:33 +0000

    Now calls fast_kjw_mtx()

--- fsfast/toolbox/fast_yacf_kjw.m
+++ fsfast/toolbox/fast_yacf_kjw.m
@@ -6,36 +6,13 @@ if(nargin ~= 2)
   return;
 end
 
-nf = length(racf);
-M = zeros(nf,nf);
+[nf nv] = size(racf);
+M = fast_kjw_mtx(R,nf);
 
-tic;
-Dl = eye(nf);
-for l = 1:nf
-  %Dl = diag(ones(nf-(l-1),1),l-1);  
-  D(:,:,l) = Dl;
-  DpDt(:,:,l) = Dl+Dl'; %'
-  Dl = fast_mshift(Dl,[0 1],0);
-end
-
-
-M = zeros(nf,nf);
-for l = 1:nf
-  fprintf('l = %d, %g\n',l,toc);
-  %Dl = diag(ones(nf-l,1),l);
-  Dl = diag(ones(nf-(l-1),1),l-1);  
-  %RDl = fast_mshift(R,[0 l-1],0);
-  RDlR = R*Dl*R;
-  M(l,1) = trace(RDlR);
-  for j = 2:nf
-    M(l,j) = trace(RDlR*DpDt(:,:,j)); 
-  end
-end
-
-%tmp = racf .* [nf:-1:1]'; %'
-%yacf = inv(M)*tmp;
 yacf = inv(M)*racf;
-yacf = yacf/yacf(1);
+yacf = yacf./repmat(yacf(1,:),[nf 1]);
+
+
 toc
 
 
