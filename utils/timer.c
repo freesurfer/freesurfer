3edcfcc61ed601649e0796fdd5fc9137246e1e95
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2003-04-14 15:53:14 +0000

    use gettimeofday

--- utils/timer.c
+++ utils/timer.c
@@ -8,13 +8,24 @@
 struct timeb *
 TimerStart(struct timeb *then)
 {
+  /* according to the header ftime() is obsolete */
+#if 0
   ftime(then) ;
   return(then) ;
+#endif
+  /* struct timeval { long tv_sec; long tv_usec; }; */
+  struct timeval tv; 
+  gettimeofday(&tv, 0);
+  then->time = tv.tv_sec;
+  then->millitm = (tv.tv_usec+500)/1000;
+  then->dstflag = 0;
+  return then;
 }
 
 int
 TimerStop(struct timeb *then)
 {
+#if 0
   struct timeb now ;
   int          msec, now_msec, then_msec ;
 
@@ -23,4 +34,12 @@ TimerStop(struct timeb *then)
   now_msec = (int)now.time * 1000 + (int)now.millitm ;
   msec = now_msec - then_msec ;
   return(msec) ;
+#endif
+  int msec;
+  struct timeval tvnow, tvthen;
+  gettimeofday(&tvnow, 0); 
+  tvthen.tv_sec = then->time;
+  tvthen.tv_usec = ((long) (then->millitm))*1000;
+  msec = 1000*(tvnow.tv_sec - tvthen.tv_sec) + (tvnow.tv_usec - tvthen.tv_usec+500)/1000;
+  return msec;
 }
