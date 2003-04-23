a2d990bf320e13fd347112ee98e2bbde0981dbf1
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-04-23 05:54:56 +0000

    fixed stuff

--- fsfast/toolbox/fast_svdfilter.m
+++ fsfast/toolbox/fast_svdfilter.m
@@ -1,25 +1,27 @@
 % fast_svdfilter.m
 
-% instem
-% maskstem
-% pforder
-% xmatfile
-% pthresh
-% pvsthresh
-% 
-% outstem
-% extregstem
+% extregname
+% mergeextreg
+% mergedextreg
+% filter option
 
 tic
 
 fsd = '/space/greve/1/users/greve/wagner/JL/bold';
-funcrstem = 'fmcsm5';
-maskrstem = 'brain';
 analysis = 'onoff';
-dimfrac = .75;
-sigthresh = 2;
 runidlist = strvcat('006','007','008','009');
 
+fsd = '/home/greve/sg1/nh-rest/bold';
+analysis = 'onoff60s';
+runidlist = strvcat('006','007','008','009');
+
+funcrstem = 'fmcsm5';
+maskrstem = 'brain';
+dimfrac   = 1;
+sigthresh = 3;
+
+
+%--------------------------------------------------%
 pthresh = 10.^(-sigthresh);
 
 maskastem = sprintf('%s/masks/%s',fsd,maskrstem);
@@ -52,12 +54,14 @@ for nthrun = 1:nruns
     return; quit;
   end
   X = XX.Xfinal;
+  nf = size(X,1);
   Ntask = XX.Nnnc*XX.Navgs_per_cond;
   Nbeta = size(X,2);
   C = zeros(Ntask,Nbeta);
   C(1:Ntask,1:Ntask) = 1;
   dof = size(X,1)-size(X,2);
   B = inv(X'*X)*X';
+  R = eye(nf)-X*B;
   
   fprintf('Loading functionals (%g)\n',toc);
   y = fmri_ldbvolume(funcastem);
@@ -78,19 +82,43 @@ for nthrun = 1:nruns
   end
   y = reshape(y,[nv nf])';
   y = y(:,indmask);
+  y0 = y;
+  
+  if(0)
+    fprintf('Computing whitening matrix (%g)\n',toc);
+    r = R*y;
+    racf = fast_acorr(r);
+    racfmn0 = mean(racf,2);
+    [cnd mineig S] = fast_acfcond(racfmn0);
+    racfmn = racfmn0;
+    while(mineig < 0 & cnd > 100)
+      racfmn = racfmn .* tukeytaper(nf);
+      [cnd mineig S] = fast_acfcond(racfmn);
+    end
+    [u s v] = svd(S);
+    W = u*inv(sqrt(s))*v';
+    WX = W*X;
+    Wy = W*y;
+    WB = inv(WX'*WX)*WX';
+    beta = WB*Wy;
+    r = Wy-WX*beta;
+  else
+    beta = B*y;
+    r = y-X*beta;
+  end
   
   fprintf('Analyzing functionals to get exclusion mask (%g)\n',toc);
-  beta = B*y;
-  r = y-X*beta;
   rvar = sum(r.^2)/dof;
   [F, dof1, dof2, g] = fast_fratio(beta,X,rvar,C);
   p = FTest(dof1, dof2, F);
-
+  [pdf alpha nx fpr] = ComputePDF(p,0,1,.001);
+  
   indexclude = find(p < pthresh);
   indinclude = find(p >= pthresh);
   nexclude = length(indexclude);
   ninclude = length(indinclude);
-  fprintf('Excluding %d voxels as active\n',nexclude);
+  fprintf('Excluding %d (%g%%) voxels as active (%g)\n',...
+	  nexclude,100*nexclude/nmask,sigthresh);
   fprintf('Continuing with %d voxels\n',ninclude);
   y = y(:,indinclude);
   
@@ -103,25 +131,43 @@ for nthrun = 1:nruns
   
   [u s blah] = svd(My);
   ds = diag(s);
-  plot(100*ds/sum(ds));
+  %plot(100*ds/sum(ds));
   
   [dim0 pvs pvsw] = fast_estdimsvd(s);
   dim = ceil(dim0*dimfrac);
-  fprintf('run = %s  dim0 = %d, dim = %d\n',runid,dim0,dim);
+  fprintf('run = %s  dim0 = %d (%g), dim = %d (th=%g)\n',...
+	  runid,dim0,sum(pvs(1:dim0)),dim,sum(pvs(1:dim)));
   
   % Only keep the first dim components of u %
   u = u(:,1:dim);
 
   % Test u for signal %
-  beta = B*u
+  beta = B*u;
   uhat = X*beta;
   r = u - uhat;
   rvar = sum(r.^2)/dof;
   [F, dof1, dof2] = fast_fratio(beta,X,rvar,C);
   p = FTest(dof1, dof2, F);
   
-keyboard  
-
+  X = [X u];
+  Ntask = XX.Nnnc*XX.Navgs_per_cond;
+  Nbeta = size(X,2);
+  C = zeros(Ntask,Nbeta);
+  C(1:Ntask,1:Ntask) = 1;
+  dof = size(X,1)-size(X,2);
+  B = inv(X'*X)*X';
+  beta = B*y0;
+  r = y0-X*beta;
+  rvar = sum(r.^2)/dof;
+  [F, dof1, dof2, g] = fast_fratio(beta,X,rvar,C);
+  p = FTest(dof1, dof2, F);
+  [pdf2 alpha2 nx fpr2] = ComputePDF(p,0,1,.001);
+  
+  figure(nthrun);
+  loglog(alpha,alpha,alpha,fpr,alpha2,fpr2);
+  legend('ideal','none','svdfil');
+  title(sprintf('Run %s',runid));
+  
 end
 
 toc
