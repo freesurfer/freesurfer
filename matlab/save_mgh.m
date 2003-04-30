a88d5c333b6f628fbaa1e87da66170dc42319aa0
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-04-30 21:17:09 +0000

    replaced with the *2.m version

--- matlab/save_mgh.m
+++ matlab/save_mgh.m
@@ -1,17 +1,34 @@
-function [] = save_mgh(vol,fname, M, sizes)
+function r = save_mgh2(vol, fname, M, mr_parms);
 %
-% Loads the indicated mgh format volume
-%   $SUBJECTS_DIR/subject/mri/volumeid
+% save_mgh2(vol,fname, M, <mr_parms>);
 %
-% save_mgh(vol,fname, M, sizes)  
+% M is the 4x4 vox2ras transform such that
+% y(i1,i2,i3), xyz = M*[i1 i2 i3 1] where the
+% indicies are 0-based
 %
-% $Id: save_mgh.m,v 1.1 2002/10/30 00:22:28 fischl Exp $
+% mr_parms = [tr flipangle te ti]
+%
+% See also: load_mgh, vox2ras_0to1
+%
+% $Id: save_mgh.m,v 1.2 2003/04/30 21:17:09 greve Exp $
+%
+
+r = 1;
 
 if(nargin < 2 | nargin > 4)
-  msg = 'USAGE: save_mgh(vol,fname)';
-  qoe(msg);error(msg);
+  msg = 'USAGE: save_mgh2(vol,fname,M)';
+  return;
 end
 
+if(exist('mr_parms')~=1) mr_parms = []; end
+if(isempty(mr_parms))   mr_parms = [0 0 0 0]; end
+if(length(mr_parms) ~= 4)
+  fprintf('ERROR: mr_parms length = %d, must be 4\n', ...
+	  lenght(mr_parms));
+  return;
+end
+
+% These dont appear to be used %
 MRI_UCHAR =  0 ;
 MRI_INT =    1 ;
 MRI_LONG =   2 ;
@@ -19,73 +36,58 @@ MRI_FLOAT =  3 ;
 MRI_SHORT =  4 ;
 MRI_BITMAP = 5 ;
 MRI_TENSOR = 6 ;
-slices   = [1:256];
 
 fid = fopen(fname, 'wb', 'b') ;
-[width,height,depth,rows,cols] = size(vol) ;
-fwrite(fid, 1, 'int') ;					 % magic #
-fwrite(fid, width, 'int') ; 
-fwrite(fid, height, 'int') ; 
-fwrite(fid, depth, 'int') ; 
-fwrite(fid, 1, 'int') ;					 % # of frames
-if (ndims(vol) == 5)
-	 is_tensor = 1 ;
-	 fwrite(fid, MRI_TENSOR, 'int') ;	 % type = MRI_TENSOR
-else
-	 is_tensor = 0 ;
-	 fwrite(fid, MRI_FLOAT, 'int') ;	 % type = MRI_FLOAT
+if(fid == -1)
+  fprintf('ERROR: could not open %s for writing\n',fname);
+  return;
 end
 
+
+[ndim1,ndim2,ndim3,rows,cols] = size(vol) ;
+fwrite(fid, 1, 'int') ;		% magic #
+fwrite(fid, ndim1, 'int') ; 
+fwrite(fid, ndim2, 'int') ; 
+fwrite(fid, ndim3, 'int') ; 
+fwrite(fid, 1, 'int') ;		% # of frames
+if (ndims(vol) == 5)
+  is_tensor = 1 ;
+  fwrite(fid, MRI_TENSOR, 'int') ; % type = MRI_TENSOR
+else
+  is_tensor = 0 ;
+  fwrite(fid, MRI_FLOAT, 'int') ;  % type = MRI_FLOAT
+end
+
+%%?????????????%%%
 fwrite(fid, 1, 'int') ;          % dof (not used)
 dof = fread(fid, 1, 'int') ; 
 
 UNUSED_SPACE_SIZE= 256;
 USED_SPACE_SIZE = (3*4+4*3*4);  % space for ras transform
 
+MdcD = M(1:3,1:3);
+delta = sqrt(sum(MdcD.^2));
+
+Mdc = MdcD./repmat(delta,[3 1]);
+Pcrs_c = [ndim1/2 ndim2/2 ndim3/2 1]'; %'
+Pxyz_c = M*Pcrs_c;
+Pxyz_c = Pxyz_c(1:3);
+
+fwrite(fid, 1,      'short') ;       % ras_good_flag = 1
+fwrite(fid, delta,  'float32') ; 
+fwrite(fid, Mdc,    'float32') ; 
+fwrite(fid, Pxyz_c, 'float32') ; 
+
 unused_space_size = UNUSED_SPACE_SIZE-2 ;
-if (nargin > 2)
-	 fwrite(fid, 1, 'short') ;       % ras_good_flag = 0
-	 unused_space_size = unused_space_size - USED_SPACE_SIZE ;
-	 fwrite(fid, sizes(1), 'float32') ; % xsize
-	 fwrite(fid, sizes(2), 'float32') ; % ysize
-	 fwrite(fid, sizes(3), 'float32') ; % zsize
-	 
-	 fwrite(fid, M(1,1), 'float32') ;  % x_r
-	 fwrite(fid, M(2,1), 'float32') ;  % x_a
-	 fwrite(fid, M(3,1), 'float32') ;  % x_s
-
-	 fwrite(fid, M(1,2), 'float32') ;  % y_r
-	 fwrite(fid, M(2,2), 'float32') ;  % y_a
-	 fwrite(fid, M(3,2), 'float32') ;  % y_s
-
-	 fwrite(fid, M(1,3), 'float32') ;  % z_r
-	 fwrite(fid, M(2,3), 'float32') ;  % z_a
-	 fwrite(fid, M(3,3), 'float32') ;  % z_s
-
-	 fwrite(fid, M(1,4), 'float32') ;  % c_r
-	 fwrite(fid, M(2,4), 'float32') ;  % c_a
-	 fwrite(fid, M(3,4), 'float32') ;  % c_s
-else
-	 fwrite(fid, 0, 'short') ;       % ras_good_flag = 0
-
-end
-
+unused_space_size = unused_space_size - USED_SPACE_SIZE ;
 fwrite(fid, zeros(unused_space_size,1), 'char') ;
-bpv = 4 ;   % bytes/voxel
 
-						
-nelts = width * height ;  % bytes per slice 
+fwrite(fid,vol,'float32');
 
-for row=1:rows
-	for col=1:cols
-		for z=1:depth
-				slice = reshape(vol(:,:,z, row, col), [width height]) ;
-				fwrite(fid, slice, 'float32') ; 
-				vol(:,:,z) = reshape(slice, [width height]) ;
-		end
-	end
-end
-
+fwrite(fid, mr_parms, 'float32') ; 
 fclose(fid) ;
 
+r = 0;
+
+return;
 
