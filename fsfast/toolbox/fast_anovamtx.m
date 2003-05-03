93548e7f71713799f5568f949f5842849347d052
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-05-03 01:08:08 +0000

    Creates an ANOVA matrix

---
+++ fsfast/toolbox/fast_anovamtx.m
@@ -0,0 +1,62 @@
+function M = fast_anovamtx(nlevels,factorlist)
+% M = fast_anovamtx(nlevels,<factorlist>)
+% 
+% nlevels is a vector of the number of levels in each 
+% factor. nfactors = length(nlevels);
+%
+% factorlist are the factors to test for an interaction
+% across. To test the main effect of a factor, list
+% only that factor in the factorlist.
+%
+% The order of estimates must be last factor fastest.
+% See fast_glm2anova_mtx.
+%
+% $Id: fast_anovamtx.m,v 1.1 2003/05/03 01:08:08 greve Exp $
+
+M = [];
+
+if(nargin < 1 | nargin > 2)
+  fprintf('M = fast_anovamtx(nlevels,<factorlist>)\n');
+  return;
+end
+
+nfactors = length(nlevels);
+if(exist('factorlist') ~= 1) factorlist = []; end
+if(isempty(factorlist)) factorlist = [1:nfactors]; end
+
+% Check that the factor list is ok
+if(max(factorlist) > nfactors)
+  fprintf('ERROR: bad factor list (max)\n');
+  return;
+end
+if(min(factorlist) < 1)
+  fprintf('ERROR: bad factor list (min)\n');
+  return;
+end
+
+M = 1;
+for f = 1:nfactors
+  inlist = ~isempty(find(f==factorlist));
+  if(inlist) Mf = omnibusmtx(nlevels(f));
+  else       Mf = sumvect(nlevels(f));
+  end
+  M = kron(M,Mf);
+end
+
+return;
+
+%-----------------------------------------------%
+function O = omnibusmtx(levels)
+O = [eye(levels-1) -ones(levels-1,1)];
+return;
+
+%-----------------------------------------------%
+function s = sumvect(levels)
+s = ones(1,levels);
+return;
+
+
+
+
+
+
