6b1446f679afcf57d2ccdfde25f14668cac60c4e
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-05-07 04:27:31 +0000

    routines to read and write fla config files

---
+++ fsfast/toolbox/fast_flacfg_load.m
@@ -0,0 +1,71 @@
+function flacfg = fast_flacfg_load(cfgfile)
+% flacfg = fast_flacfg_load(cfgfile)
+flacfg = [];
+
+if(nargin ~= 1)
+  fprintf('flacfg = fast_flacfg_load(cfgfile)\n');
+  return;
+end
+
+fid = fopen(cfgfile,'r');
+if(fid == -1)
+  fprintf('ERROR: cannot open cfg file %s\n',cfgfile);
+  return;
+end
+
+tline = fgetl(fid);
+
+flacfgid = sscanf(tline,'%s',1);
+if(~strcmp(flacfgid,'FSFAST-FLACFG'))
+  fprintf('ERROR: format error (id string) in cfg file %s\n',cfgfile);
+  fclose(fid);
+  return;
+end
+
+flacfgversion = sscanf(tline,'%*s %d',1);
+if(flacfgversion ~= 2)
+  fprintf('ERROR: cfg file %s, version = %d\n',cfgfile,flacfgversion);
+  fclose(fid);
+  return;
+end
+
+flacfg = fast_flacfg_struct;
+nthfx = 1;  
+while(1)
+
+  % scroll through any blank lines or comments
+  while(1)
+    tline = fgetl(fid);
+    if(~isempty(tline) & tline(1) ~= '#') break; end
+  end
+  if(tline(1) == -1) break; end
+
+  key = lower(sscanf(tline,'%s',1));
+  %fprintf('key = %s\n',key);
+  
+  switch(key)
+   case 'flaname',     flacfg.flaname     = sscanf(tline,'%*s %s',1);
+   case 'nskip',       flacfg.nskip       = sscanf(tline,'%*s %d',1);
+   case 'tr',          flacfg.TR          = sscanf(tline,'%*s %f',1);
+   case 'fsd',         flacfg.fsd         = sscanf(tline,'%*s %s',1);
+   case 'funcstem',    flacfg.funcstem    = sscanf(tline,'%*s %s',1);
+   case 'maskstem',    flacfg.maskstem    = sscanf(tline,'%*s %s',1);
+   case 'inorm',       flacfg.inorm       = sscanf(tline,'%*s %f',1);
+   case 'runlistfile', flacfg.runlistfile = sscanf(tline,'%*s %s',1);
+   case 'evschfname',  flacfg.evschfname  = sscanf(tline,'%*s %s',1);
+   case 'slicetiming', flacfg.slicetiming = sscanf(tline,'%*s %s',1);
+   case 'usetpexclude', flacfg.usetpexclude = sscanf(tline,'%*s %d',1);
+   case 'effect', 
+    flacfg.fxlist(nthfx).fx = fast_fxcfg('parseline',tline);
+    nthfx = nthfx + 1;
+   otherwise
+    fprintf('INFO: key %s unrecognized, skipping\n',key);
+  end
+end % while (1)
+
+fclose(fid);
+
+flacfg.sesscfg = fast_sesscfg_struct;
+
+
+return;
---
+++ fsfast/toolbox/fast_flacfg_write.m
@@ -0,0 +1,41 @@
+function err = fast_flacfg_write(c,cfgfile)
+% err = fast_flacfg_write(cfg,cfgfile)
+err = 1;
+
+if(nargin ~= 2)
+  fprintf('err = fast_flacfg_write(cfg,cfgfile)\n');
+  return;
+end
+
+fid = fopen(cfgfile,'w');
+if(fid == -1)
+  fprintf('ERROR: cannot open cfg file %s\n',cfgfile);
+  return;
+end
+
+fprintf(fid,'FSFAST-FLACFG %d\n',c.version);
+if(~isempty(c.flaname)) fprintf(fid,'flaname %s\n',c.flaname); end
+if(~isempty(c.TR)) fprintf(fid,'tr %f\n',c.TR); end
+if(~isempty(c.inorm)) fprintf(fid,'inorm %f\n',c.inorm); end
+if(~isempty(c.nskip)) fprintf(fid,'nskip %d\n',c.nskip); end
+if(~isempty(c.slicetiming)) fprintf(fid,'slicetiming %s\n',c.slicetiming); end
+if(~isempty(c.evschfname)) fprintf(fid,'evschfname %s\n',c.evschfname); end
+if(~isempty(c.runlistfile)) fprintf(fid,'runlistfile %s\n',c.runlistfile); end
+if(~isempty(c.fsd)) fprintf(fid,'fsd %s\n',c.fsd); end
+if(~isempty(c.funcstem)) fprintf(fid,'funcstem %s\n',c.funcstem); end
+if(~isempty(c.maskstem)) fprintf(fid,'maskstem %s\n',c.maskstem); end
+if(~isempty(c.usetpexclude)) fprintf(fid,'usetpexclude %d\n',c.usetpexclude); end
+
+nfx = length(c.fxlist);
+for nthfx = 1:nfx
+  c.nthfx = nthfx;
+  tline = fast_fxcfg('createline',c);
+  fprintf(fid,'%s\n',tline);
+end
+
+fclose(fid);
+
+err = 0;
+return;
+
+
