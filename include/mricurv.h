826167cecda5932ba56f252ebbf4f890c0a1a76a
Florent Segonne <segonne@nmr.mgh.harvard.edu>
2003-05-28 17:19:56 +0000

    New file to compute the curvature (locally)

---
+++ include/mricurv.h
@@ -0,0 +1,9 @@
+#include "mri.h"
+#include "mri_topology.h"
+
+#ifndef MRICURV_INCLUDED
+#define MRICURV_INCLUDED
+
+float MRIcurvature(MRI *mri,int i,int j,int k,int label,int connectivity);
+float Nbhcurvature(Nbh *nbh,int connectivity);
+#endif
