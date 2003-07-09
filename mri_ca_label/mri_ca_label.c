6e9575f738ddcf803dbb646ce32c56ac65b872ed
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2003-07-09 21:40:58 +0000

    *** empty log message ***

--- mri_ca_label/mri_ca_label.c
+++ mri_ca_label/mri_ca_label.c
@@ -108,7 +108,7 @@ main(int argc, char *argv[])
   TRANSFORM     *transform ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_ca_label.c,v 1.40 2003/07/08 19:08:13 fischl Exp $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_ca_label.c,v 1.41 2003/07/09 21:40:58 fischl Exp $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
