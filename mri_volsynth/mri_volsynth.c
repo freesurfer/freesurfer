ab4f44dc181c3e3400698f3fcc9aaefbd7d64155
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-07-15 20:33:13 +0000

    Added auto seed and template.

--- mri_volsynth/mri_volsynth.c
+++ mri_volsynth/mri_volsynth.c
@@ -4,7 +4,7 @@
   email:   analysis-bugs@nmr.mgh.harvard.edu
   Date:    2/27/02
   Purpose: Synthesize a volume.
-  $Id: mri_volsynth.c,v 1.3 2003/07/11 00:50:11 greve Exp $
+  $Id: mri_volsynth.c,v 1.4 2003/07/15 20:33:13 greve Exp $
 */
 
 #include <stdio.h>
@@ -12,6 +12,7 @@
 #include <math.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "error.h"
 #include "diag.h"
@@ -39,23 +40,33 @@ static int  isflag(char *flag);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_volsynth.c,v 1.3 2003/07/11 00:50:11 greve Exp $";
+static char vcid[] = "$Id: mri_volsynth.c,v 1.4 2003/07/15 20:33:13 greve Exp $";
 char *Progname = NULL;
 
 int debug = 0;
+
 char *volid = NULL;
 char *vol_type;
-int volfmtid;
+int   volfmtid;
 char *volfmt = NULL;
+
+char *tempid = NULL; // Template
+char *temp_type;
+int   tempfmtid;
+char *tempfmt = NULL;
+
 int dim[4];
 float res[4];
 float cras[4];
 float cdircos[3], rdircos[3], sdircos[3];
 char *pdfname = "gaussian";
 char *precision=NULL; /* not used yet */
-MRI *mri, *mrism;
-long seed=53;
+MRI *mri, *mrism, *mritemp;
+long seed = -1; /* < 0 for auto */
+char *seedfile = NULL;
 float fwhm = 0, gstd = 0;
+int nframes = -1;
+
 
 /*---------------------------------------------------------------*/
 int main(int argc, char **argv)
@@ -91,6 +102,20 @@ int main(int argc, char **argv)
   check_options();
   dump_options(stdout);
 
+  if(tempid != NULL){
+    printf("INFO: reading template header\n");
+    mritemp = MRIreadHeader(tempid,tempfmtid);
+    if(mritemp == NULL){
+      printf("ERROR: reading %s header\n",tempid);
+      exit(1);
+    }
+    dim[0] = mritemp->width;
+    dim[1] = mritemp->height;
+    dim[2] = mritemp->depth;
+    if(nframes > 0) dim[3] = nframes;
+    else            dim[3] = mritemp->nframes;
+  }
+
   printf("Synthesizing\n");
   srand48(seed);
   if(strcmp(pdfname,"gaussian")==0)
@@ -105,26 +130,33 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  mri->xsize = res[0];
-  mri->ysize = res[1];
-  mri->zsize = res[2];
-  mri->tr    = res[3];
-  mri->x_r = cdircos[0];
-  mri->x_a = cdircos[1];
-  mri->x_s = cdircos[2];
-  mri->y_r = rdircos[0];
-  mri->y_a = rdircos[1];
-  mri->y_s = rdircos[2];
-  mri->z_r = sdircos[0];
-  mri->z_a = sdircos[1];
-  mri->z_s = sdircos[2];
-  mri->c_r = cras[0];
-  mri->c_a = cras[1];
-  mri->c_s = cras[2];
+  if(tempid != NULL){
+    MRIcopyHeader(mritemp,mri);
+    mri->type = MRI_FLOAT;
+    if(nframes > 0) mri->nframes = nframes;
+  }
+  else {
+    mri->xsize = res[0];
+    mri->ysize = res[1];
+    mri->zsize = res[2];
+    mri->tr    = res[3];
+    mri->x_r = cdircos[0];
+    mri->x_a = cdircos[1];
+    mri->x_s = cdircos[2];
+    mri->y_r = rdircos[0];
+    mri->y_a = rdircos[1];
+    mri->y_s = rdircos[2];
+    mri->z_r = sdircos[0];
+    mri->z_a = sdircos[1];
+    mri->z_s = sdircos[2];
+    mri->c_r = cras[0];
+    mri->c_a = cras[1];
+    mri->c_s = cras[2];
+  }
 
   if(gstd > 0){
     printf("Smoothing\n");
-    MRIgaussianSmooth(mri, gstd, 1, mri);
+    MRIgaussianSmooth(mri, gstd, 1, mri); /* 1 = normalize */
   }
 
   printf("Saving\n");
@@ -167,15 +199,25 @@ static int parse_commandline(int argc, char **argv)
       }
       else volfmtid = getfmtid(volid);
     }
-    else if (!strcmp(option, "--vol_type")){
+    else if (!strcmp(option, "--temp")){
       if(nargc < 1) argnerr(option,1);
-      vol_type = pargv[0];
+      tempid = pargv[0];
       nargsused = 1;
+      if(nth_is_arg(nargc, pargv, 1)){
+	tempfmt = pargv[1]; nargsused ++;
+	tempfmtid = checkfmt(tempfmt);
+      }
+      else tempfmtid = getfmtid(tempid);
     }
     else if ( !strcmp(option, "--dim") ) {
       if(nargc < 4) argnerr(option,4);
       for(i=0;i<4;i++) sscanf(pargv[i],"%d",&dim[i]);
       nargsused = 4;
+    }
+    else if ( !strcmp(option, "--nframes") ) {
+      if(nargc < 1) argnerr(option,1);
+      sscanf(pargv[0],"%d",&nframes);
+      nargsused = 1;
     }
     else if ( !strcmp(option, "--res") ) {
       if(nargc < 4) argnerr(option,4);
@@ -218,6 +260,11 @@ static int parse_commandline(int argc, char **argv)
       sscanf(pargv[0],"%ld",&seed);
       nargsused = 1;
     }
+    else if (!strcmp(option, "--seedfile")){
+      if(nargc < 1) argnerr(option,1);
+      seedfile = pargv[0];
+      nargsused = 1;
+    }
     else if (!strcmp(option, "--pdf")){
       if(nargc < 1) argnerr(option,1);
       pdfname = pargv[0];
@@ -247,7 +294,11 @@ static void print_usage(void)
   printf("\n");
   printf("   --vol volid <fmt> : output volume path id and format\n");
   printf("\n");
-  printf(" Geometry flags\n");
+  printf(" Get geometry from template\n");
+  printf("   --temp templateid <fmt>\n");
+  printf("   --nframes nframes : override template\n");  
+  printf("\n");
+  printf(" Specify geometry explicitly\n");
   printf("   --dim nc nr ns nf  (required)\n");
   printf("   --res dc dr ds df\n");
   printf("   --cdircos x y z\n");
@@ -256,6 +307,8 @@ static void print_usage(void)
   printf("   --c_ras   c_r c_a c_s\n");
   printf("\n");
   printf(" Value distribution flags\n");
+  printf("   --seed seed (default is time-based auto)\n");
+  printf("   --seedfile fname : write seed value to this file\n");
   printf("   --pdf pdfname : <gaussian>, uniform, or const\n");
   printf("\n");
 }
@@ -289,10 +342,27 @@ static void argnerr(char *option, int n)
 /* --------------------------------------------- */
 static void check_options(void)
 {
+  struct timeval tv;
+  FILE *fp;
+
   if(volid == NULL){
     printf("A volume path must be supplied\n");
     exit(1);
   }
+  if(seed < 0){
+    gettimeofday(&tv, NULL);
+    seed = tv.tv_sec;
+  }
+  if(seedfile != NULL){
+    fp = fopen(seedfile,"w");
+    if(fp == NULL){
+      printf("ERROR: cannot open seed file %s\n",seedfile);
+      exit(1);
+    }
+    fprintf(fp,"%ld\n",seed);
+    fclose(fp);
+  }
+
   return;
 }
 /* --------------------------------------------- */
@@ -300,18 +370,24 @@ static void dump_options(FILE *fp)
 {
   fprintf(fp,"volid    %s\n",volid);
   fprintf(fp,"volfmt   %s\n",volfmt);
-  fprintf(fp,"dim      %3d %3d %3d %3d\n",
-	  dim[0],dim[1],dim[2],dim[3]);
-  fprintf(fp,"res      %6.4f %6.4f %6.4f %6.4f\n",
-	  res[0],res[1],res[2],res[3]);
-  fprintf(fp,"c_ras  %6.4f %6.4f %6.4f\n",
-	  cras[0], cras[1], cras[2]);
-  fprintf(fp,"col   dircos  %6.4f %6.4f %6.4f\n",
-	  cdircos[0],cdircos[1],cdircos[2]);
-  fprintf(fp,"row   dircos  %6.4f %6.4f %6.4f\n",
-	  rdircos[0],rdircos[1],rdircos[2]);
-  fprintf(fp,"slice dircos  %6.4f %6.4f %6.4f\n",
-	  sdircos[0],sdircos[1],sdircos[2]);
+  if(tempid == 0){
+    fprintf(fp,"dim      %3d %3d %3d %3d\n",
+	    dim[0],dim[1],dim[2],dim[3]);
+    fprintf(fp,"res      %6.4f %6.4f %6.4f %6.4f\n",
+	    res[0],res[1],res[2],res[3]);
+    fprintf(fp,"c_ras  %6.4f %6.4f %6.4f\n",
+	    cras[0], cras[1], cras[2]);
+    fprintf(fp,"col   dircos  %6.4f %6.4f %6.4f\n",
+	    cdircos[0],cdircos[1],cdircos[2]);
+    fprintf(fp,"row   dircos  %6.4f %6.4f %6.4f\n",
+	    rdircos[0],rdircos[1],rdircos[2]);
+    fprintf(fp,"slice dircos  %6.4f %6.4f %6.4f\n",
+	    sdircos[0],sdircos[1],sdircos[2]);
+  }
+  else{
+    fprintf(fp,"Using %s as template\n",tempid);
+    if(nframes > 0) fprintf(fp,"nframes = %d\n",nframes);
+  }
   fprintf(fp,"fwhm = %g, gstd  = %g\n",fwhm,gstd);
   //fprintf(fp,"precision %s\n",precision);
   fprintf(fp,"seed %ld\n",seed);
