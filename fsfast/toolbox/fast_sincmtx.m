4eb2ab0f3f9286bba096abcbd08821becacd24de
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-07-19 18:28:33 +0000

    Added ability to use hann window.

--- fsfast/toolbox/fast_sincmtx.m
+++ fsfast/toolbox/fast_sincmtx.m
@@ -1,11 +1,11 @@
-function M = fast_sincmtx(len,delta,rownorm)
-% M = fast_sincmtx(len,delta,<rownorm>)
+function M = fast_sincmtx(len,delta,rownorm,hw)
+% M = fast_sincmtx(len,delta,<rownorm>,<hw>)
 % Create a sinc interpolation matrix. Given a vector y,
 % yhat = M*y will be the sinc interpolation of y shifted
 % backwards by delta, where delta is a fration of the 
 % distance between y. If rownorm is set to non-zero, then
 % each row of the matrix M is normalized so that the
-% mean is 1.
+% mean is 1. hw is the width of the hann window.
 
 M = [];
 
@@ -19,7 +19,12 @@ if(isempty(rownorm)) rownorm = 0; end
 M = zeros(len);
 i = 1:len;
 for n = 1:len
-  r = sinc(i - n - delta);
+  x = i - n - delta;
+  if(exist('hw') == 1)
+    r = sinc(x) .* hann(x,hw);
+  else
+    r = sinc(x);
+  end
   if(rownorm) r = r/sum(r); end
   M(n,:) = r;
 end
@@ -32,5 +37,15 @@ function y = sinc(x)
 y      = ones(size(x));
 inz    = find(x ~= 0);
 y(inz) = sin(pi*x(inz)) ./ (pi*x(inz));
+
+return;
+
+%-----------------------------------------------------------%
+function h = hann(x,tau)
+
+alpha = 0.5; % 0.54 for Hamming
+h = zeros(size(x));
+ind = find(abs(x) < tau);
+h(ind) = alpha + (1-alpha)*cos(pi*x(ind)/tau);
 
 return;
