44e29ece59eaa6d01a3201469badde6fe167c510
E'beth Haley <ebeth@nmr.mgh.harvard.edu>
2003-07-21 16:14:11 +0000

    fixed typo lenght -> length

--- matlab/save_mgh.m
+++ matlab/save_mgh.m
@@ -10,7 +10,7 @@ function r = save_mgh(vol, fname, M, mr_parms);
 %
 % See also: load_mgh, vox2ras_0to1
 %
-% $Id: save_mgh.m,v 1.3 2003/05/26 05:49:20 greve Exp $
+% $Id: save_mgh.m,v 1.4 2003/07/21 16:14:11 ebeth Exp $
 %
 
 r = 1;
@@ -24,7 +24,7 @@ if(exist('mr_parms')~=1) mr_parms = []; end
 if(isempty(mr_parms))   mr_parms = [0 0 0 0]; end
 if(length(mr_parms) ~= 4)
   fprintf('ERROR: mr_parms length = %d, must be 4\n', ...
-	  lenght(mr_parms));
+	  length(mr_parms));
   return;
 end
 
