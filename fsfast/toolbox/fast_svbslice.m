d5676ffeca2bd13da6c73c374f09f747b05a4976
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-07-25 00:33:13 +0000

    added ability to write out bhdr file

--- fsfast/toolbox/fast_svbslice.m
+++ fsfast/toolbox/fast_svbslice.m
@@ -1,18 +1,22 @@
-function err = fast_svbslice(y,stem,sliceno,bext)
+function err = fast_svbslice(y,stem,sliceno,bext,bhdrstr)
 % err = fast_svbslice(y,stem,sliceno,<bext>)
 %
 % size(y) = [rows cols frames]
 % sliceno is zero-based
-% if bext is not specificed, it is set to bfloat
+% if bext is not specificed or is null, it is set to bfloat
+% bhdrstr is printed to stem.bhdr
 
 err = 1;
 
-if(nargin ~= 2 & nargin ~= 3)
-  fprintf('err = fast_svbslice(y,stem,sliceno,<bext>)\n');
+if(nargin < 2 & nargin > 4)
+  fprintf('err = fast_svbslice(y,stem,sliceno,<bext>,<bhdrstr>)\n');
   return;
 end
 
-if(exist('bext') ~= 1) bext = 'bfloat'; end
+if(exist('bext') ~= 1) bext = ''; end
+if(isempty(bext)) bext = 'bfloat'; end
+
+if(exist('bhdrstr') ~= 1) bhdrstr = ''; end
 
 if(strcmp(bext,'bfloat') == 0 & strcmp(bext,'bshort') == 0)
   fprintf('ERROR: bext = %s, must be bfloat or bshort\n',bext);
@@ -27,6 +31,15 @@ end
 
 fname = sprintf('%s_%03d.%s',stem,sliceno,bext);
 fmri_svbfile(y,fname);
+
+if(~isempty(bhdrstr))
+  % Should make sure nframes is correct %
+  bhdrfile = sprintf('%s.bhdr',stem);
+  fid = fopen(bhdrfile,'w');
+  fprintf(fid,'%s',bhdrstr);
+  fclose(fid);
+end
+
 
 err = 0;
 
