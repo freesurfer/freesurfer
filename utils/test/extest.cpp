9b2f6380078152a8d7362e28fd1c6f7143384b81
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2003-07-30 18:35:49 +0000

    fread*Ex test

---
+++ utils/test/extest.cpp
@@ -0,0 +1,178 @@
+//
+// extest.cpp
+//
+// test fread*Ex() routines
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <typeinfo>
+
+extern "C" {
+#include "fio.h"
+}
+
+const int MAX = 3;
+
+using namespace std;
+
+template <typename T> bool checkOK(const T &p, const T &q)
+{
+  if (p != q)
+  {
+    cerr << "type is " << typeid(p).name() << ":";
+    cerr << "value differed " << p << " vs. " << q << endl;
+    return false;
+  }
+  else
+  {
+    cout << "type is " << typeid(p).name() << ":";
+    cout << "read " << p << "  orig: " << q << endl;
+    return true;
+  }
+}
+
+template <> bool checkOK(const double &p, const double &q)
+{
+  if ((p-q) > 0.00000000001)
+  {
+    cerr << "type is " << typeid(p).name() << ":";
+    cerr << "value differed " << p << " vs. " << q << endl;
+    return false;
+  }
+  else
+  {
+    cout << "type is " << typeid(p).name() << ":";
+    cout << "read " << p << "  orig: " << q << endl;
+    return true;
+  }
+}
+
+template <> bool checkOK(const float &p, const float &q)
+{
+  if ((p-q) > 0.0000001)
+  {
+    cerr << "type is " << typeid(p).name() << ":";
+    cerr << "value differed " << p << " vs. " << q << endl;
+    return false;
+  }
+  else
+  {
+    cout << "type is " << typeid(p).name() << ":";
+    cout << "read " << p << "  orig: " << q << endl;
+    return true;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  FILE *in, *out;
+  int i[MAX];
+  short s[MAX];
+  float f[MAX];
+  double d[MAX];
+
+  int ii[MAX];
+  short ss[MAX];
+  float ff[MAX];
+  double dd[MAX];
+
+  char *file = "./extest.out";
+
+  out = fopen(file, "w");
+  if (!out)
+  {
+    cerr << "could not open " << file << endl;
+    exit (-1);
+  }
+  i[0] = 1, i[1] = 256, i[2] = 256*256*256;
+  s[0] = 1, s[1] = 256*256, s[2] = 256*256*256-1;
+  f[0] = 1.; f[1] = 1.-0.001; f[2] = 1./3.;
+  d[0] = 1.; d[1] = 1.-0.001; d[2] = 1./3.;
+
+  fwriteInt(i[0], out);
+  fwriteShort(s[0], out);
+  fwriteFloat(f[0], out);
+  fwriteDouble(d[0], out);
+  fwriteDouble(d[1], out);
+  fwriteFloat(f[1], out);
+  fwriteShort(s[1], out);
+  fwriteInt(i[1], out);
+  fwriteDouble(d[2], out);
+  fwriteFloat(f[2], out);
+  fwriteShort(s[2], out);
+  fwriteInt(i[2], out);
+
+  fclose(out);
+  
+  in = fopen(file, "rb");
+  if(freadIntEx(&ii[0], in))
+  {
+    if (!checkOK(i[0], ii[0]))
+      goto finalize;
+  }
+  if (freadShortEx(&ss[0], in))
+  {
+    if (!checkOK(s[0], ss[0]))
+      goto finalize;
+  }
+  if (freadFloatEx(&ff[0], in))
+  {
+    if (!checkOK(f[0], ff[0]))
+      goto finalize;
+  }
+  if (freadDoubleEx(&dd[0], in))
+  {
+    if (!checkOK(d[0], dd[0]))
+      goto finalize;
+  }
+  if (freadDoubleEx(&dd[1], in))
+  {
+    if (!checkOK(d[1], dd[1]))
+      goto finalize;
+  }
+  if (freadFloatEx(&ff[1], in))
+  {
+    if (!checkOK(f[1], ff[1]))
+      goto finalize;
+  }
+      if (freadShortEx(&ss[1], in))
+  {
+    if (!checkOK(s[1], ss[1]))
+      goto finalize;
+  }
+      if (freadIntEx(&ii[1], in))
+  {
+    if (!checkOK(i[1], ii[1]))
+      goto finalize;
+  }
+      if (freadDoubleEx(&dd[2], in))
+  {
+    if (!checkOK(d[2], dd[2]))
+      goto finalize;
+  }
+      if (freadFloatEx(&ff[2], in))
+  {
+    if (!checkOK(f[2], ff[2]))
+      goto finalize;
+  }
+      if (freadShortEx(&ss[2], in))
+  {
+    if (!checkOK(s[2], ss[2]))
+      goto finalize;
+  }
+      if (freadIntEx(&ii[2], in))
+  {
+    if (!checkOK(i[2], ii[2]))
+      goto finalize;
+  }
+  fclose(in);
+  cout << "No error" << endl;
+  return 0;
+
+ finalize:
+  cerr << "Error" << endl;
+  exit(-1);
+}
