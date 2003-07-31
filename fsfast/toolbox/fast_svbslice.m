c012f335298223953b3d919b23e2ac32dff00b1d
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-07-31 15:12:06 +0000

    added ability to save a volume with slice = -1

--- fsfast/toolbox/fast_svbslice.m
+++ fsfast/toolbox/fast_svbslice.m
@@ -5,6 +5,11 @@ function err = fast_svbslice(y,stem,sliceno,bext,bhdrstr)
 % sliceno is zero-based
 % if bext is not specificed or is null, it is set to bfloat
 % bhdrstr is printed to stem.bhdr
+%
+% if sliceno is < 0 then size(y) = [slices rows cols frames]
+% and each slice is saved.
+%
+% $Id: fast_svbslice.m,v 1.3 2003/07/31 15:12:06 greve Exp $
 
 err = 1;
 
@@ -23,14 +28,16 @@ if(strcmp(bext,'bfloat') == 0 & strcmp(bext,'bshort') == 0)
   return;
 end
   
-if(sliceno < 0)
-  fprintf('ERROR: cannot have negative slice number\n');
-  return;
+if(sliceno >= 0)
+  fname = sprintf('%s_%03d.%s',stem,sliceno,bext);
+  fmri_svbfile(y,fname);
+else
+  nslices = size(y,1);
+  for slice = 0:nslices-1
+    fname = sprintf('%s_%03d.%s',stem,slice,bext);
+    fmri_svbfile(squeeze(y(slice+1,:,:,:)),fname);
+  end
 end
-  
-
-fname = sprintf('%s_%03d.%s',stem,sliceno,bext);
-fmri_svbfile(y,fname);
 
 if(~isempty(bhdrstr))
   % Should make sure nframes is correct %
@@ -39,7 +46,6 @@ if(~isempty(bhdrstr))
   fprintf(fid,'%s',bhdrstr);
   fclose(fid);
 end
-
 
 err = 0;
 
