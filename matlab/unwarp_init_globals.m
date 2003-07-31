ce5b2029e7bf80a18d43be770e2b36dc6ba19706
E'beth Haley <ebeth@nmr.mgh.harvard.edu>
2003-07-31 21:47:40 +0000

    changed GRADWARPPATH & TABLE to have base dir of getenv('DEV').

--- matlab/unwarp_init_globals.m
+++ matlab/unwarp_init_globals.m
@@ -1,4 +1,4 @@
-% $Id: unwarp_init_globals.m,v 1.1 2003/07/24 21:19:24 ebeth Exp $
+% $Id: unwarp_init_globals.m,v 1.2 2003/07/31 21:47:40 ebeth Exp $
 % Called by convert_unwarp_resample.m and unwarp_scanners_table.m
 % This can't be the right way to do this.
 
@@ -8,7 +8,7 @@ global GRADWARPPATH;
 global TABLE;
 global QuitOnError;
 
-GRADWARPPATH = '/space/dijon/10/ebeth/gradwarp/data';
+GRADWARPPATH = sprintf('%s/grad_unwarp_tables',getenv('DEV'));
 TABLE = sprintf('%s/table.mat',GRADWARPPATH);
 if (exist('called_by_script') & ~isempty(called_by_script))
   QuitOnError = called_by_script;
