2c14e57e608ecf0265f706790acd5158d5a07f27
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-08-01 21:19:51 +0000

    Routines to read and write the bshort/bfloat .bhdr files.

---
+++ fsfast/toolbox/fast_ldbhdr.m
@@ -0,0 +1,115 @@
+function m = fast_ldbhdr(bhdrfile);
+%
+% m = fast_ldbhdr(bhdrfile)
+%
+% Parses the bhdr header file for bshorts/bfloats and returns the
+% result as an mri structure. See fast_mri_struct. The bhdr file
+% consists of keyword/value pairs.
+%
+% bhdrfile is the name of the bhdr file. If this cannot be opened,
+% then bhdrfile.bhdr is tried (ie, it treats bhdrfile as the stem).
+%
+% top_left_?:     RAS at Top-Left  Edge = T*[0 0 0 1]'
+% top_right_?:    RAS at Top-Right Edge = T*[ncols 0 0 1]'
+% bottom_right_?: RAS at Bot-Right Edge = T*[ncols nrows 0 1]'
+%
+% See also fast_svbhdr and fast_mri_struct.
+%
+% $Id: fast_ldbhdr.m,v 1.1 2003/08/01 21:19:51 greve Exp $
+
+m = [];
+
+if(nargin ~= 1)
+  msg = 'm = fast_ldbhdr(bhdrfile)';
+  qoe(msg); error(msg);
+end
+
+fid = fopen(bhdrfile,'r');
+if(fid == -1)
+  bhdrfilebhdr = sprintf('%s.bhdr',bhdrfile);
+  fid = fopen(bhdrfilebhdr,'r');
+  if(fid == -1)
+    msg = sprintf('Could not open %s or %s',bhdrfile,bhdrfilebhdr);
+    qoe(msg); error(msg);
+  end    
+end
+
+m = fast_mri_struct;
+
+%------- Loop through all the lines -----------%
+nthrow = 1;
+while(1)
+
+  % scroll through any blank lines or comments %
+  while(1)
+    tline = fgetl(fid);
+    if(~isempty(tline) & tline(1) ~= '#') break; end
+  end
+  if(tline(1) == -1) break; end
+
+  % Read the keyword and value %  
+  key = sscanf(tline,'%s',1);
+  val = sscanf(tline,'%*s %f',1);
+
+  % determine where to put it %
+  switch(key)
+   case {'cols:'},          ncols = val;
+   case {'rows:'},          nrows = val;
+   case {'nslices:'},       nslices = val;
+   case {'n_time_points:'}, m.nframes = val;
+   case {'slice_thick:'},   thickness = val;
+   case {'top_left_r:'},    tlr = val;
+   case {'top_left_a:'},    tla = val;
+   case {'top_left_s:'},    tls = val;
+   case {'top_right_r:'},   trr = val;
+   case {'top_right_a:'},   tra = val;
+   case {'top_right_s:'},   trs = val;
+   case {'bottom_right_r:'},brr = val;
+   case {'bottom_right_a:'},bra = val;
+   case {'bottom_right_s:'},brs = val;
+   case {'normal_r:'},      nr = val;
+   case {'normal_a:'},      na = val;
+   case {'normal_s:'},      ns = val;
+   case {'image_te:'},      m.te = val;
+   case {'image_tr:'},      m.tr = val;
+   case {'image_ti:'},      m.ti = val;
+   case {'flip_angle:'},    m.flip_angle = 180*val/pi;
+  end
+  
+end % while (1)
+fclose(fid);
+%---------------------------------------------%
+
+TL = [tlr tla tls]';
+TR = [trr tra trs]';
+BR = [brr bra brs]';
+m.sdc = [nr na ns]'; 
+
+% Compute the Column and Row FOV %
+% Outer edge of voxel to outer edge of voxel
+col_fov = sqrt(sum((TR-TL).^2));
+row_fov = sqrt(sum((TR-BR).^2));
+
+% Compute the Column and Row Resolution %
+col_res = col_fov/ncols;
+row_res = row_fov/nrows;
+
+% Compute the Column and Row Normal Vectors %
+m.cdc = (TR-TL)/col_fov;
+m.rdc = (BR-TR)/row_fov;
+
+m.voldim = [ncols nrows nslices]'; %'
+m.volres = [col_res row_res thickness]';%'
+m.P0 = TL; % Assumes TL is center, not edge
+Mdc = [m.cdc m.rdc m.sdc];
+D = diag(m.volres);
+m.T = [Mdc*D m.P0; 0 0 0 1];
+
+% Fix P0 for when TL, TR, and BR are the edges
+%tmp = m.T*[0.5 0.5 0.5 1]';
+%m.P0 = tmp(1:3);
+%m.T(:,4) = tmp;
+
+m.c = m.T*[m.voldim/2; 1];
+
+return;
---
+++ fsfast/toolbox/fast_svbhdr.m
@@ -0,0 +1,63 @@
+function err = fast_svbhdr(m, bhdrfile)
+% err = fast_svbhdr(m, bhdrfile)
+%
+% m is a fast_mri_struct.
+% It is assumed that zero-based indexing is used.
+%
+% See also fast_ldbhdr and fast_mri_struct.
+%
+% $Id: fast_svbhdr.m,v 1.1 2003/08/01 21:19:51 greve Exp $
+
+err = 1;
+
+if(nargin ~= 2)
+  fprintf('err = fast_svbhdr(fsfmri, bhdrfile)\n');
+  return;
+end
+
+fid = fopen(bhdrfile,'w');
+if(fid == -1)
+  fprintf('ERROR: could not open %s\n',bhdrfile);
+  return;
+end
+  
+
+TL = m.T*[0 0 0 1]'; % Center of first vox
+TR = m.T*[m.voldim(1) 0 0 1]'; % Top-right edge + 0.5 vox
+BR = m.T*[m.voldim(1) m.voldim(2) 0 1]'; % Bot right + 0.5 vox
+
+% Valid for when TL, TR, and BR are the edges
+%TL = m.T*[-0.5 -0.5 -0.5 1]'; % Top-left edge
+%TR = m.T*[m.voldim(1)-0.5 -0.5 -0.5 1]'; % Top-right edge
+%BR = m.T*[m.voldim(1)-0.5 m.voldim(2)-0.5 -0.5 1]'; % Bot right
+
+SliceNorm = m.T(:,3);
+SliceNorm = SliceNorm/sqrt(sum(SliceNorm.^2));
+
+fprintf(fid,'          cols: %d\n',m.voldim(1));
+fprintf(fid,'          rows: %d\n',m.voldim(2));
+fprintf(fid,'       nslices: %d\n',m.voldim(3));
+fprintf(fid,' n_time_points: %d\n',m.nframes);
+fprintf(fid,'   slice_thick: %d\n',m.volres(3));
+fprintf(fid,'    top_left_r: %f\n',TL(1));
+fprintf(fid,'    top_left_a: %f\n',TL(2));
+fprintf(fid,'    top_left_s: %f\n',TL(3));
+fprintf(fid,'   top_right_r: %f\n',TR(1));
+fprintf(fid,'   top_right_a: %f\n',TR(2));
+fprintf(fid,'   top_right_s: %f\n',TR(3));
+fprintf(fid,'bottom_right_r: %f\n',BR(1));
+fprintf(fid,'bottom_right_a: %f\n',BR(2));
+fprintf(fid,'bottom_right_s: %f\n',BR(3));
+fprintf(fid,'      normal_r: %f\n',SliceNorm(1));
+fprintf(fid,'      normal_a: %f\n',SliceNorm(2));
+fprintf(fid,'      normal_s: %f\n',SliceNorm(3));
+fprintf(fid,'      image_te: %f\n',m.te);
+fprintf(fid,'      image_tr: %f\n',m.tr);
+fprintf(fid,'      image_ti: %f\n',m.ti);
+fprintf(fid,'    flip_angle: %f\n',pi*m.flip_angle/180);
+
+fclose(fid);
+
+err = 0;
+
+return;
