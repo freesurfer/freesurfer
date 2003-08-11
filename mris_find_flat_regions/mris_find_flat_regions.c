6b85f06eaa6a29eccc0408deefd4d065d9ae9715
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2003-08-11 20:05:47 +0000

    added mris_find_flat_regions.
    fill hypointensities.
    
    find  regions in  which  the surface is perpindicular to one  of  the
    cardinal axes for measuring  thickness.

---
+++ mris_find_flat_regions/mris_find_flat_regions.c
@@ -0,0 +1,149 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "macros.h"
+#include "error.h"
+#include "diag.h"
+#include "proto.h"
+#include "mrisurf.h"
+#include "mri.h"
+#include "macros.h"
+#include "version.h"
+
+static char vcid[] = "$Id: mris_find_flat_regions.c,v 1.1 2003/08/11 20:05:47 fischl Exp $";
+
+int main(int argc, char *argv[]) ;
+
+static int  get_option(int argc, char *argv[]) ;
+static void print_usage(void) ;
+static void print_help(void) ;
+static void print_version(void) ;
+
+char *Progname ;
+
+static  float  thresh =  0.99 ;
+
+int
+main(int argc, char *argv[])
+{
+  char               **av, fname[STRLEN], *surf_name, *wfile_name ;
+  int                ac, nargs, vno ;
+  MRI_SURFACE        *mris ;
+  VERTEX             *v ;
+
+  /* rkt: check for and handle version tag */
+  nargs = handle_version_option (argc, argv, "$Id: mris_find_flat_regions.c,v 1.1 2003/08/11 20:05:47 fischl Exp $");
+  if (nargs && argc - nargs == 1)
+    exit (0);
+  argc -= nargs;
+
+  Progname = argv[0] ;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
+  {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 2)
+    print_help() ;
+
+  surf_name = argv[1] ;
+  wfile_name = argv[2] ;
+
+  mris = MRISread(surf_name) ;
+  if (!mris)
+    ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
+              Progname, fname) ;
+
+	MRIScomputeMetricProperties(mris) ;
+	
+  for (vno = 0 ; vno < mris->nvertices ; vno++)
+  {
+    v = &mris->vertices[vno] ;
+    if (v->ripflag)
+      continue ;
+		if ((fabs(v->nx)  > thresh) || (fabs(v->ny)  > thresh) || (fabs(v->nz)  > thresh))
+			v->val = 1 ;
+  }
+
+  MRISwriteValues(mris, wfile_name) ;
+
+  exit(0) ;
+  return(0) ;  /* for ansi */
+}
+
+/*----------------------------------------------------------------------
+            Parameters:
+
+           Description:
+----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[])
+{
+  int  nargs = 0 ;
+  char *option ;
+  
+  option = argv[1] + 1 ;            /* past '-' */
+  if (!stricmp(option, "-help"))
+    print_help() ;
+  else if (!stricmp(option, "-version"))
+    print_version() ;
+  else switch (toupper(*option))
+  {
+  case '?':
+  case 'U':
+    print_usage() ;
+    exit(1) ;
+    break ;
+  case 'T':
+    thresh = atof(argv[2]) ;
+    nargs = 1 ;
+    fprintf(stderr, "using threshold = %2.3f\n", thresh) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static void
+print_usage(void)
+{
+  fprintf(stderr, 
+          "usage: %s [options] <surface> <wfile>\n",
+          Progname) ;
+}
+
+static void
+print_help(void)
+{
+  print_usage() ;
+  fprintf(stderr, 
+       "\nThis program computed regions in  which the surface is almost perpindicular to one\n"
+					"of  the cardinal axes, and writes  the results  to  a label file\n") ;
+  fprintf(stderr, "-t <thresh>  "
+          "specify the threshold to use  (default=%2.3f)\n", thresh)  ;
+  exit(1) ;
+}
+
+
+static void
+print_version(void)
+{
+  fprintf(stderr, "%s\n", vcid) ;
+  exit(1) ;
+
+}
