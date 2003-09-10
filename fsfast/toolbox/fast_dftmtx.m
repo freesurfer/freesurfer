55e4bc07f62545822d69fd525e77ab27462ebbfc
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-09-10 03:00:56 +0000

    Modified so that no fftshift is needed.

--- fsfast/toolbox/fast_dftmtx.m
+++ fsfast/toolbox/fast_dftmtx.m
@@ -1,5 +1,5 @@
-function K = fast_dftmtx(N_or_Phase)
-% K = fast_dftmtx(N_or_Phase)
+function [K, phmat] = fast_dftmtx(N_or_Phase)
+% [K phmat] = fast_dftmtx(N_or_Phase)
 % 
 % Computes the matrix that implements an N-point 1D DFT.
 %
@@ -7,29 +7,30 @@ function K = fast_dftmtx(N_or_Phase)
 %   N - the number of points in the DFT (scalar)
 %   Phase - the phase associated with each sample (vector)
 %
-% Specifying N is equivalent to specifying Phase = 2*pi*[0:N-1]';
+% Specifying N is equivalent to specifying Phase = 2*pi*[0:N-1]/N';
 %
-% For a vector f = randn(N,1); then F = K*f; will be the same
-% as fft(f).
+% For a vector f = randn(N,1); then F = fftshift(K*f); will be the
+% same as fft(f). Actually, only the magnitudes will be the same.
 %
-% $Id: fast_dftmtx.m,v 1.1 2003/09/06 01:26:06 greve Exp $
+% $Id: fast_dftmtx.m,v 1.2 2003/09/10 03:00:56 greve Exp $
 
 if(nargin ~= 1)
-  fprintf('K = fast_dftmtx(N_or_Phase)\n');
+  fprintf('[K phmat] = fast_dftmtx(N_or_Phase)\n');
   return;
 end
 
 if(length(N_or_Phase)==1) 
   N = N_or_Phase;
-  ph0 = 2*pi * [0:N-1]';
+  ph0 = 2*pi * [0:N-1]'/N;
 else                      
   N = length(N_or_Phase);
   ph0 = reshape(N_or_Phase,[N 1]);
 end
 
-r = [0:N-1]/N;
+r = [0:N-1];
+r = r - r(N/2 + 1);
+ph0 = ph0 - ph0(N/2+1);
 phmat = (ph0 * r); % outer product
-%phmat = (r' * ph0'); % outer product
 K = exp(-i * phmat);
 
 return;
