36fd959be008e2f6c22a4ee998ace716aaf6b727
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-10-27 05:22:57 +0000

    Handles t<0 better.

--- fsfast/toolbox/fast_spmhrf.m
+++ fsfast/toolbox/fast_spmhrf.m
@@ -15,7 +15,7 @@ function h = fast_spmhrf(t,ttp1,tau1,ttp2,tau2,c)
 % default conditions. In general, 
 %    p = [(tt1+1) (ttp2+1) tau1 tau2 1/c 0]
 % Then, 
-%    h = fast_spm_hrf(TR,p);
+%    h = fast_spmhrf(TR,p);
 % This will yeild the same result when tau1=tau2=1, t = [0:TR:32], and
 %    h = fast_spmhrf(t,ttp1,tau1,ttp2,tau2,c);
 
@@ -38,13 +38,20 @@ end
 a1 = ttp1/tau1;
 a2 = ttp2/tau2;
 
+indlz = find(t<0);
+h(indlz) = 0;
+
 h1 = (t/ttp1).^a1 .* exp(-(t-ttp1)/tau1);
+h1(indlz) = 0;
 h1 = h1/sum(h1);
+
 h2 = (t/ttp2).^a2 .* exp(-(t-ttp2)/tau2);
+h2(indlz) = 0;
 h2 = h2/sum(h2);
 h = h1 - c*h2;
 
 h = h/sum(h);
+
 
 return;
 
