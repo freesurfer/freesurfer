2b3cc98b78ecc64138328261ae72573c6878967c
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-10-27 05:22:24 +0000

    Handles hw=0.

--- fsfast/toolbox/fast_sincmtx.m
+++ fsfast/toolbox/fast_sincmtx.m
@@ -5,25 +5,29 @@ function M = fast_sincmtx(len,delta,rownorm,hw)
 % backwards by delta, where delta is a fration of the 
 % distance between y. If rownorm is set to non-zero, then
 % each row of the matrix M is normalized so that the
-% mean is 1. hw is the width of the hann window.
+% mean is 1. hw is the width of the hann window. The wider
+% the width, the less the effect.
+%
+% $Id: fast_sincmtx.m,v 1.3 2003/10/27 05:22:24 greve Exp $
 
 M = [];
 
-if( nargin ~= 2 & nargin ~= 3 )
+if( nargin < 2 | nargin > 4 )
   fprintf('M = fast_sincmtx(len,delta,<rownorm>)\n');
   return;
 end
 if(exist('rownorm') ~= 1) rownorm = []; end
 if(isempty(rownorm)) rownorm = 0; end
 
+if(exist('hw') ~= 1) hw = []; end
+if(isempty(hw))      hw = 0; end
+
 M = zeros(len);
 i = 1:len;
 for n = 1:len
   x = i - n - delta;
-  if(exist('hw') == 1)
-    r = sinc(x) .* hann(x,hw);
-  else
-    r = sinc(x);
+  if(hw > 0) r = sinc(x) .* hann(x,hw);
+  else       r = sinc(x);
   end
   if(rownorm) r = r/sum(r); end
   M(n,:) = r;
