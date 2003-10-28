b61eb20e962c5ef2d8e0797d9f3e1c9a64dadc4e
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-10-28 04:32:14 +0000

    The rows are reconned by applying Rrow. Includes deghosting.

---
+++ fsfast/toolbox/tdr_recon_rows.m
@@ -0,0 +1,47 @@
+function [rimg, dgbeta] = tdr_recon_rows(kimg,Rrow,PERev)
+% [rimg, dgbeta] = tdr_recon_rows(kimg,Rrow,<PERev>)
+%
+% The rows are reconned by applying Rrow. Includes deghosting.
+%
+% See also tdr_recon_cols.
+% 
+% $Id: tdr_recon_rows.m,v 1.1 2003/10/28 04:32:14 greve Exp $
+
+rimg = [];
+dgbeta = [];
+
+if(nargin ~= 2 & nargin ~= 3)
+  fprintf('[rimg, dgbeta] = tdr_recon_rows(kimg,Rrow,<PERev>)\n');
+  return;
+end
+if(exist('PERev') ~= 1) PERev = 0; end
+
+[nrows ncols nslices nframes] = size(kimg);
+evenrows = [2:2:nrows];
+Fcol = fast_dftmtx(nrows);
+Rcol = inv(Fcol);
+
+rimg = zeros(size(kimg));
+dgbeta = zeros(2,nslices,nframes);
+for frame = 1:nframes
+  for slice = 1:nslices
+    kslice = kimg(:,:,slice,frame);
+    kslice(evenrows,:) = fliplr(kslice(evenrows,:));
+    [rimg(:,:,slice,frame) dgbeta(:,slice,frame)] = ...
+	tdr_deghost(kslice,Rcol,Rrow,PERev);
+  end
+end
+
+return;
+
+
+
+
+
+
+
+
+
+
+
+
