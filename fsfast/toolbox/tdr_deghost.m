8b8d7c2237ac349092b6b3805b4209026dcdb680
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-10-30 19:29:26 +0000

    Now recons cols with ifft instead of Rcol

--- fsfast/toolbox/tdr_deghost.m
+++ fsfast/toolbox/tdr_deghost.m
@@ -1,5 +1,5 @@
-function [kimg2, beta, ncsub] = tdr_deghost(kimg,Rcol,Rrow,perev)
-% [kimg2 beta ncsub] = tdr_deghost(kimg,Rcol,Rrow,<perev>)
+function [kimg2, beta] = tdr_deghost(kimg,Rrow,perev)
+% kimg2 = tdr_deghost(kimg,Rrow,<perev>)
 %
 % Recons the rows of kimg with deghosting.
 %
@@ -11,15 +11,15 @@ function [kimg2, beta, ncsub] = tdr_deghost(kimg,Rcol,Rrow,perev)
 %  (perev=0) or even lines (perev=1) will be used as 
 %  reference. If perev is not passed, perev=0 is assumed.
 %
-% $Id: tdr_deghost.m,v 1.3 2003/10/28 05:15:22 greve Exp $
+% $Id: tdr_deghost.m,v 1.4 2003/10/30 19:29:26 greve Exp $
 %
 
 rsubdel = 3; % region around center
 rthresh = 1; % fraction of mean to use as segmentation thresh
 
 kimg2 = [];
-if(nargin ~= 3 & nargin ~= 4)
-  fprintf('kimg2 = tdr_deghost(kimg,Rcol,Rrow,<perev>)\n');
+if(nargin ~= 2 & nargin ~= 3)
+  fprintf('kimg2 = tdr_deghost(kimg,Rrow,<perev>)\n');
   return;
 end
 
@@ -39,8 +39,15 @@ end
 % Recon separate images based on every-other kspace
 % line. Both will have wrap-around but neither will
 % have ghosting (may be hard to tell the difference).
-img2ref = Rcol(:,refrows)*kimg(refrows,:)*Rrow;
-img2mov = Rcol(:,movrows)*kimg(movrows,:)*Rrow;
+%img2ref = Rcol(:,refrows)*kimg(refrows,:)*Rrow;
+%img2mov = Rcol(:,movrows)*kimg(movrows,:)*Rrow;
+ktmp = kimg*Rrow;
+ktmpref = ktmp;
+ktmpref(movrows,:) = 0;
+img2ref = fftshift(ifft(fftshift(ktmpref,1),[],1),1);
+ktmpmov = ktmp;
+ktmpmov(refrows,:) = 0;
+img2mov = fftshift(ifft(fftshift(ktmpmov,1),[],1),1);
 
 % Choose a range of rows around the center, away from 
 % wrap-around.
@@ -51,12 +58,11 @@ img2mn = (abs(img2ref)+abs(img2mov))/2;
 imgrsubmn = mean(img2mn(rsub,:));
 thresh = rthresh*mean(imgrsubmn);
 csub = find(imgrsubmn > thresh);
-ncsub = length(csub);
 
 % Fit the phase difference at the segmented columns.
 % Model is offset and slope.
 ph = mean(angle(img2ref(rsub,csub)./img2mov(rsub,csub)))';
-X = [ones(ncsub,1) csub'];
+X = [ones(length(csub),1) csub'];
 beta = (inv(X'*X)*X')*ph;
 
 % Generate the estimate of the phase diff at all columns
