2ae5cc76f365662815f95873e8808e0009dfc84f
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-12-01 04:57:02 +0000

    Added ability to read in result of tdr_fidmat2.m. Now performs a recon on the simulated EPI kspace image from the FID.

--- fsfast/toolbox/tdr_rcolmat.m
+++ fsfast/toolbox/tdr_rcolmat.m
@@ -2,12 +2,14 @@
 % columns based on the FID map and according to the time-domain
 % reconstruction method.
 %
-% $Id: tdr_rcolmat.m,v 1.4 2003/11/07 00:42:42 greve Exp $
+% $Id: tdr_rcolmat.m,v 1.5 2003/12/01 04:57:02 greve Exp $
 
 if(0) 
   % Input and output files
-  fidmatfile  = sprintf('%s/D%2d.2.mat',topdir,TE0);
-  rcolmatfile = sprintf('%s/R%2d.2.mat',topdir,TE0);
+  topdir = '/home/greve/sg1/dng072203';
+  TE = 50;
+  fidmatfile  = sprintf('%s/D%2d.1.B.mat',topdir,TE);
+  rcolmatfile = sprintf('%s/R%2d.1.B.mat',topdir,TE);
   
   % Normalize FID with abs value at first FID echo
   dnorm = 1;
@@ -15,6 +17,7 @@ if(0)
   % Segmentation theshold the first echo of the FID Map
   rthreshfid = 0.2;
   
+  % Flag to apply BOLD weighting
   boldweight = 0;
   
   % SVD regularization percentage
@@ -26,17 +29,32 @@ end
 %-----------------------------------------%
 tic; % Start Timer
 
+clear DTE;
 fprintf('Loading fidmat file ... ');
 load(fidmatfile);
 fprintf('Done (%g)\n',toc);
+if(exist('DTE') ~= 1)
+  DTE = D;
+  clear D;
+  fidmatversion = 2;
+else
+  fidmatversion = 1;
+end
+
 [nrows ncols nslices] = size(D0);
 nv = prod([nrows ncols nslices]);
 
-% Compute the Ideal col and row DFT reconstruction matrices
+% Compute the Ideal col DFT reconstruction matrices
 Fcol = fast_dftmtx(nrows);
-%Rcol = inv(Fcol);
-%Frow = fast_dftmtx(ncols);
-%Rrow = transpose(inv(Frow));
+
+if(fidmatversion ~= 1)
+  % Compute row DFT for recon of simulated distorted kepi
+  nkcols = size(kepiref_dist,2);
+  Frow = fast_dftmtx(nkcols);
+  Frow = Frow(:,colkeep);
+  Rrow = inv(Frow'*Frow)*Frow';
+  Rrow = transpose(Rrow);
+end
 
 fidvol1 = abs(D0);
 if(rthreshfid > 0)
@@ -58,8 +76,15 @@ end
 Rtdr = zeros(nrows,nrows,ncols,nslices);
 RtdrCond = zeros(ncols,nslices);
 RtdrDim  = zeros(ncols,nslices);
+epirecon_undist = zeros(nrows,ncols,nslices);
 for sliceno = 1:nslices
   fprintf('sliceno = %d (%g) ----------------\n',sliceno,toc);
+
+  if(fidmatversion ~= 1)
+    % Recon rows of simulated kepi
+    kslice = kepiref_dist(:,:,sliceno);
+    kepi2 = kslice * Rrow;
+  end
 
   for imgcol = 1:ncols
 
@@ -99,15 +124,31 @@ for sliceno = 1:nslices
     Rtdr(:,:,imgcol,sliceno) = RcolTDR;
     RtdrCond(imgcol,sliceno) = cond(RcolTDR);
     RtdrDim(imgcol,sliceno) = FcolTDRdim;
-  
+
+    if(fidmatversion ~= 1)
+      % Recon columns of simulated distored kepi %
+      epirecon_undist(:,imgcol,sliceno) = abs(RcolTDR * kepi2(:,imgcol));
+    end
+    
   end
 
 end % slice
 
 fprintf('Saving to %s\n',rcolmatfile);
-save(rcolmatfile,'fidmatfile','dnorm','regmethod',...
-     'fidvol1mn','fidvol1','TE','TEList','perev',...
-     'svdregpct','tikregfact','Rtdr','RtdrCond','RtdrDim',...
-     'T2s','B0','epiref_dist','epiref_undist','boldweight');
+if(fidmatversion == 1)
+  save(rcolmatfile,'fidmatfile','dnorm','regmethod',...
+       'fidvol1mn','fidvol1','TE','TEList','perev',...
+       'svdregpct','tikregfact','Rtdr','RtdrCond','RtdrDim',...
+       'T2s','B0','epiref_dist','epiref_undist','boldweight',...
+       'rthreshfid','indsubthresh','fidmatversion');
+else
+  save(rcolmatfile,'fidmatfile','dnorm','regmethod',...
+       'fidvol1mn','fidvol1','TE','perev',...
+       'svdregpct','tikregfact','Rtdr','RtdrCond','RtdrDim',...
+       'T2s','B0','epiref_dist','epiref_undist','boldweight',...
+       'sliceorder','pedmat','colkeep','nT2sFit','rthreshfid',...
+       'indsubthresh','kepiref_dist','epirecon_undist',...
+       'fidmatversion');
+end
 fprintf('Done (%g)\n',toc);
 
