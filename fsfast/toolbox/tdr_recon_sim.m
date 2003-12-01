e4378a7aa41b6d2e9ed1ea36c010562a03b5a127
Doug Greve <greve@nmr.mgh.harvard.edu>
2003-12-01 04:53:00 +0000

    Recon simulation using the simulated EPI kspace image from the FID.

---
+++ fsfast/toolbox/tdr_recon_sim.m
@@ -0,0 +1,74 @@
+% tdr_recon_sim.m
+tic;
+
+rcolmat = '/home/greve/sg1/dng072203/R20.1.tik05.mat';
+
+fprintf('Loading ... ');
+load(rcolmat);
+fprintf(' Done (%g)\n',toc);
+
+nstd = 0.00005;
+[nrows ncols nslices] = size(fidvol1);
+nv = prod([nrows ncols nslices]);
+nkcols = size(kepiref_dist,2);
+
+Frow = fast_dftmtx(nkcols);
+Frow = Frow(:,colkeep);
+Rrow = inv(Frow'*Frow)*Frow';
+Rrow = transpose(Rrow);
+
+epi0 = zeros(nrows,ncols,nslices);
+for slice = 1:nslices
+  %fprintf('Slice %d  (%g)\n',slice,toc);
+  kslice0 = kepiref_dist(:,:,slice);
+  kepi02 = kslice0 * Rrow;
+  for col = 1:ncols
+    Rcol = Rtdr(:,:,col,slice);
+    epi0(:,col,slice) = abs(Rcol * kepi02(:,col));
+  end
+end
+
+nsim = 20;
+epi = zeros(nrows,ncols,nslices,nsim);
+for n = 1:nsim
+  fprintf('n=%d, (%g)\n',n,toc);
+  for slice = 1:nslices
+    kslice0 = kepiref_dist(:,:,slice);
+    knoise = nstd * ( randn(nrows,nkcols) + i*randn(nrows,nkcols));
+    kslice = kslice0 + knoise;
+    
+    kepi2  = kslice * Rrow;
+    for col = 1:ncols
+      Rcol = Rtdr(:,:,col,slice);
+      epi(:,col,slice,n)  = abs(Rcol * kepi2(:,col));
+    end
+  end
+
+end
+
+epistd = std(epi,[],4);
+epimn = mean(epi,4);
+mn = mean(reshape1d(epimn));
+r = epistd./epimn;
+ind = find(epimn < mn);
+r(ind) = 0;
+mos = vol2mos(r);            
+
+figure(2)
+imagesc(mos);colorbar;
+
+return
+
+
+swapview('-init','-v1',epi0,'-v2',epi(:,:,:,1));
+
+
+
+
+
+
+
+
+
+
+
