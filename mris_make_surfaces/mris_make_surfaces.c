bd2adb30fd4008017e2babefd82f184b9128fadc
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2003-12-09 17:08:49 +0000

    added TMP2 vertex set.
    added some binaries
    
    fixed help message
    
    used STRLEN instead of 100

--- mris_make_surfaces/mris_make_surfaces.c
+++ mris_make_surfaces/mris_make_surfaces.c
@@ -16,7 +16,7 @@
 #include "mrinorm.h"
 #include "version.h"
 
-static char vcid[] = "$Id: mris_make_surfaces.c,v 1.47 2003/09/05 04:45:43 kteich Exp $";
+static char vcid[] = "$Id: mris_make_surfaces.c,v 1.48 2003/12/09 17:08:49 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -81,7 +81,7 @@ static char *suffix = "" ;
 static char *output_suffix = "" ;
 static char *xform_fname = NULL ;
 
-static char pial_name[100] = "pial" ;
+static char pial_name[STRLEN] = "pial" ;
 static char white_matter_name[STRLEN] = WHITE_MATTER_NAME ;
 
 static int lh_label = LH_LABEL ;
@@ -142,7 +142,7 @@ main(int argc, char *argv[])
   M3D           *m3d ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_make_surfaces.c,v 1.47 2003/09/05 04:45:43 kteich Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mris_make_surfaces.c,v 1.48 2003/12/09 17:08:49 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
