1fb084e9d4f8b87d1e73a2d0aeff33af2162f2a2
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-01-16 22:19:07 +0000

    References TE instead of time to first echo of first line

--- fsfast/toolbox/tdr_pedmatrix.m
+++ fsfast/toolbox/tdr_pedmatrix.m
@@ -1,69 +1,76 @@
-function pedmat = tdr_pedmatrix(echo1ped,echospacing,delsamp,tDwell,nrows,ncols,nechoes)
-% pedmat = tdr_pedmatrix(echo1ped,echospacing,delsamp,tDwell,nrows,ncols,nechoes) 
+function pedmat = tdr_pedmatrix(TE,echospacing,delsamp,tDwell,nrows,ncols,perev)
+% pedmat = tdr_pedmatrix(TE,echospacing,delsamp,tDwell,nrows,ncols,<perev>)
 %
-% Computes the post-excitation delay of each sample in a k-space
-% image, including multiple echos. The k-space image is
-% nrows-by-ncols with even lines going in the same direction as odd
-% lines (ie, if the even lines were reversed during acquisition,
-% then they have been reversed again in the k-space image). If
-% there are multiple echos, then it is assumed that the even echos
-% were collected in reverse order (but have been reversed again in
-% the kspace image).
+% Computes the post-excitation delay (PED) of each sample in a k-space
+% image.  The row/col in the PED matrix corresponds to that of the
+% kspace image without any reversals (readout or phase encode)
+% applied. Any such operations applied to the kspace image should
+% then be applied to the PED matrix. It is assumed that the even
+% lines were acquired in reverse readout.
 %
-% echo1ped - time to the first echo of the first line
+% The perev indicates that k-space was traversed in the reverse
+% phase encode direction. This changes where the center of k-space is
+% in the PED matrix. No reversals or flipping of the PED matrix are
+% applied even when the perev is set.
+%
+% TE - echo time - time at which the center of k-space is traversed.
 % echospacing - time between lines/echoes
 % delsamp - time to first sample after the start of the ramp
-% tDwell - time between ADCs
-% 
-% $Id: tdr_pedmatrix.m,v 1.1 2004/01/16 21:55:05 greve Exp $
+% tDwell - time between ADC samples
+% nrows - number of rows in the raw k-space image
+% ncols - number of columns in the raw k-space image (includes oversampling)
+% perev - k-space was traversed in the reverse direction (PED
+%           matrix is not flipped UD). If perev is not given or is
+%           empty, no reversal is assumed.
+%
+% $Id: tdr_pedmatrix.m,v 1.2 2004/01/16 22:19:07 greve Exp $
 %
 
 pedmat = [];
 
-if(nargin ~= 7)
-  fprintf('pedmat = tdr_pedmatrix(echo1ped,echospacing,delsamp,tDwell,nrows,ncols,nechoes)\n');
+if(nargin ~= 6 & nargin ~= 7)
+  fprintf('pedmat = tdr_pedmatrix(TE,echospacing,delsamp,tDwell,nrows,ncols,<perev>)\n');
   return;
 end
 
-% Time from the RF Pulse to the start of very first rampup
-FirstRampPED = echo1ped - echospacing/2;
+if(exist('perev') ~= 1) perev = []; end
+if(isempty(perev)) perev = 0; end
 
-% Time at which each column in a line is sampled wrt start of Ramp
-tcol0 = tDwell * [0:ncols-1] + delsamp;
-
-% Time at which the ramp of each row starts wrt the start of first Ramp
-trow0 = echospacing * [0:nrows-1]';
-
-% Time to traverse k-space for one image
-Timage = echospacing * nrows;
-
-% Time of first ramp of each kimage
-techo0 = [0:nechoes-1]*Timage;
-
-% Time of first and second major echoes
-% techo1 = trow0(nrows/2+1) + echo1ped;
-%   Should be at pedmat(nrows/2+1,ncols/2+1,1)
-% techo2 = techo1 + echospacing*(nrows-1); % nrows-1 for rev readout
-%   Should be at pedmat(nrows/2+1,ncols/2+1,2)
-
-% Relative time at which each row/col is sampled
-pedmat0 = repmat(trow0,[1 ncols]) + repmat(tcol0,[nrows 1]);
-
-% Reverse Readout
-evenrows = [2:2:nrows];
-pedmat0(evenrows,:) = fliplr(pedmat0(evenrows,:));
-
-for echo = 1:nechoes
-  if(rem(echo,2) ~= 0) 
-    % odd numbered echo
-    pedmat(:,:,echo) = pedmat0;
-  else
-    % even numbered echo - reverse row order
-    pedmat(:,:,echo) = flipud(pedmat0);
-  end
-  pedmat(:,:,echo) = pedmat(:,:,echo) + techo0(echo);
+% Compute the row where center of k-space is traversed%
+if(~perev) % not reversed
+  rcenter = nrows/2 + 1;
+else % reversed
+  rcenter = nrows/2 - 1;
 end
 
-pedmat = pedmat + FirstRampPED;
+% Column center is always here
+ccenter = ncols/2 + 1;
+
+% Time to the echo of (ie, center of) the first line
+L1CenterPED = TE - (echospacing*(rcenter-1));
+
+% Time to the start of the rampup of the first line
+FirstRampPED = L1CenterPED - echospacing/2;
+
+% Time at which each column in a line is sampled wrt start of Ramp
+col0PED = tDwell * [0:ncols-1] + delsamp;
+
+% Time at which the ramp of each row starts wrt the start of first Ramp
+row0PED = echospacing * [0:nrows-1]';
+
+% Relative time at which each row/col is sampled wrt 
+% the start of first Ramp.
+pedmat0 = repmat(row0PED,[1 ncols]) + repmat(col0PED,[nrows 1]);
+
+% Reverse readout for even lines
+evenrows = 2:2:nrows;
+pedmat0(evenrows,:) = fliplr(pedmat0(evenrows,:));
+
+% Relative time at which each row/col is sampled wrt 
+% the center of the RF excitation pulse
+pedmat = pedmat0 + FirstRampPED;
+
+% Note: pedmat(rcenter,ccenter) should equal TE (+/- tDwell/2)
+% pedmat(rcenter,ccenter)-TE
 
 return;
