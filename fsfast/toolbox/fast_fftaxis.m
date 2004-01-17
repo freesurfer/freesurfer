ca7a7984eab93ae32a9448e55e3a54b4e99d9af3
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-01-17 05:33:39 +0000

    Fixed bug where the frequencies were slightly off.

--- fsfast/toolbox/fast_fftaxis.m
+++ fsfast/toolbox/fast_fftaxis.m
@@ -2,18 +2,25 @@ function [fftaxis, deltafreq] = fast_fftaxis(Ntp,TR)
 % [fftaxis, deltafreq] = fast_fftaxis(Ntp,TR)
 %
 % Returns the frequencies at which the fft is computed, 
-% for DC to the nyquist frequency.
+% from DC to the nyquist frequency. There will be Ntp/2 + 1
+% frequencies.
 %
-% $Id: fast_fftaxis.m,v 1.2 2003/12/02 23:07:08 greve Exp $
+% Example:
+% fftaxis_pos = fast_fftaxis(Nf,TR);
+%
+% To get the negative frequencies:
+% fftaxis_neg = -fliplr(fftaxis_pos(2:end-1));
+%
+% $Id: fast_fftaxis.m,v 1.3 2004/01/17 05:33:39 greve Exp $
 
 if(nargin ~= 2) 
   msg = 'USAGE: [fftaxis, deltafreq] = fast_fftaxis(Ntp,TR)';
   qoe(msg); error(msg);
 end
 
-nn = 1:round(Ntp/2);
+nn = 0:round(Ntp/2);
 freqmax = (1/TR)/2;         % Nyquist
-deltafreq = freqmax/(Ntp/2);  % Measured from 0 to Nyquist
-fftaxis = deltafreq*(nn-1);
+deltafreq = freqmax/(Ntp/2); % Measured from 0 to Nyquist
+fftaxis = deltafreq*nn;
 
 return;
