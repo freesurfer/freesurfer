2cb4a76a055214b2752075992abfa1fd219b14c5
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-02-13 00:14:10 +0000

    Added ability to read in and sample arbitrary CDFs. Added seed generator based on time-of-day

--- include/pdf.h
+++ include/pdf.h
@@ -2,8 +2,11 @@
 #ifndef PDF_INC
 #define PDF_INC
 
+unsigned long PDFtodSeed(void);
 double PDFgaussian(void);
 double PDFerlang(int order);
+int PDFloadCDF(char *fname, double **xcdf, double **cdf, int *ncdf);
+double PDFsampleCDF(double *xcdf, double *cdf, int ncdf);
 
 
 #endif //#ifndef PDF_INC
--- utils/pdf.c
+++ utils/pdf.c
@@ -4,6 +4,25 @@
 #include "pdf.h"
 #include "proto.h"
 #include "utils.h"
+#include <sys/time.h>
+
+
+/*----------------------------------------------------------------
+  PDFtodSeed() - generate a random seed based on the time-of-day.
+  The seed will be different from microsecond-to-microsecond. This
+  can be used as input to srand48().
+  -------------------------------------------------------------------*/
+unsigned long PDFtodSeed(void)
+{
+  struct timeval tv;
+  unsigned long seed;
+  gettimeofday(&tv, NULL);
+  //seed = ((unsigned long) 1000000)*tv.tv_sec + tv.tv_usec;
+  seed = (unsigned long)( tv.tv_sec + tv.tv_usec);
+  //printf("%ld %ld\n",tv.tv_sec,tv.tv_usec);
+  return(seed);
+}
+
 
 /*********************************************************
  * Name:    PDFgaussian(void)
@@ -30,7 +49,7 @@ double PDFgaussian(void)
  * Purpose: generates random numbers that obey an erlang
  *          distribution with mean=1 and stddev = mean/sqrt(order):
  *   pdf(x) = r*((r*(x-avg+1))^(r-1)) * exp(-r*(x-avg+1)) / (r-1)!
- * when order=1, this generates an exponential distribution.
+ * when order=1, an exponential distribution is generated.
  ************************************************************/
 double PDFerlang(int order)
 {
@@ -43,3 +62,58 @@ double PDFerlang(int order)
   return(v);
 }
 
+/*----------------------------------------------------------------
+  PDFsampleCDF() - sample a value from the given CDF. The resulting
+  data will be distributed with the PDF that created the CDF. This is
+  a fairly inefficient implementation. See also PDFloadCDF().
+  -------------------------------------------------------------------*/
+double PDFsampleCDF(double *xcdf, double *cdf, int ncdf)
+{
+  double u, x, d, dmin;
+  int n;
+
+  u = drand48();
+
+  // This can be done much more efficiently by searching
+  // an ordered table.
+  dmin = 10;
+  for(n=0; n < ncdf; n++){
+    d = fabs(u-cdf[n]);
+    if(dmin > d){
+      dmin = d;
+      x = xcdf[n];
+    }
+  }
+  return(x);
+}
+
+/*----------------------------------------------------------------
+  PDFloadCDF() - read in a CDF. The file format is that the first
+  line has the number of rows. Each row has two columns. The first
+  column is the x at which the cdf is sampled, the second column
+  is the value of the cdf. See also PDFsampleCDF().
+  ----------------------------------------------------------------*/
+int PDFloadCDF(char *fname, double **xcdf, double **cdf, int *ncdf)
+{
+  FILE *fp;
+  int n;
+
+  fp = fopen(fname,"r");
+  if(fp == NULL){
+    printf("ERROR: cannot open %s\n",fname);
+    return(1);
+  }
+
+  fscanf(fp,"%d",ncdf);
+  //printf("ncdf = %d\n",*ncdf);
+
+  *xcdf = (double *) calloc(*ncdf,sizeof(double));
+  *cdf  = (double *) calloc(*ncdf,sizeof(double));
+
+  for(n=0; n < *ncdf; n++)
+    fscanf(fp,"%lf %lf",(*xcdf+n),(*cdf+n));
+
+  fclose(fp);
+
+  return(0);
+}
