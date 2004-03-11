a96a8920439dd12c7a828f6fa9f2a0dc4945906a
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-03-11 23:02:00 +0000

    ctrpoint read/write

---
+++ include/ctrpoints.h
@@ -0,0 +1,20 @@
+//
+// mrictrpoint.h
+//
+// purpose: read and write control points
+//
+
+typedef struct { Real x; Real y; Real z ;} MPoint;
+
+// reading control points 
+// returning array of MGHPoint
+// if useRealRAS = 1, then it is in scanner RAS
+// if useRealRAS = 0, then it is in surface RAS
+MPoint *MRIreadControlPoints(const char *fname, int *count, int *useRealRAS);
+
+// writing control points 
+// returning array of MGHPoint
+// we will write whether they are in scannerRAS or surfaceRAS
+// Note that use must tell which coordinate system it is.
+int MRIwriteControlPoints(MPoint *pointArray, int count, int useRealRAS, char *fname);
+
