3116e06ec43d065a978b03261bbeed26c51418bf
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-04-06 16:22:53 +0000

    Added docs.

--- fsfast/toolbox/fast_kjw_mtx.m
+++ fsfast/toolbox/fast_kjw_mtx.m
@@ -1,5 +1,23 @@
 function  M = fast_kjw_mtx(R,p)
 % M = fast_kjw_mtx(R, <p>)
+%
+% Computes the matrix that remove bias from residual autocorrelation
+% function using Keith Worsley's fmristats method. The bias is induced
+% by projecting out the task components to form the residual.
+%
+% R is the residual forming matrix
+% p - can't remember what this does, but it's optional
+%
+% Notes:
+%  1. As the number of frames increases, the computation is 
+%     more intense and the correction matrix is more singular.
+%     There is some happy medium where the accuracy of the 
+%     ACF is best, but it depends upon the design among other
+%     things.
+%
+% See also: fast_acorr, fast_yacf_kjw.
+%
+% $Id: fast_kjw_mtx.m,v 1.4 2004/04/06 16:22:53 greve Exp $
 
 if(nargin ~= 1 & nargin ~= 2)
   fprintf('M = fast_kjw_mtx(R, <p>)\n');
@@ -9,33 +27,27 @@ end
 nf = size(R,1);
 if(~exist('p')) p = nf; end
 
-M = zeros(p,p);
-
-tic;
-fprintf('KJW Matrix: Stage 1\n');
+%fprintf('KJW Matrix: Stage 1\n');
 Dl = eye(nf);
 for l = 1:p
-  if(mod(l,5)==0 | l==1) fprintf('l = %d, %g\n',l,toc); end
-  %Dl = diag(ones(nf-(l-1),1),l-1);  
+  %if(mod(l,5)==0 | l==1) fprintf('l = %d, %g\n',l,toc); end
   D(:,:,l) = Dl;
   DpDt(:,:,l) = Dl+Dl'; %'
   Dl = fast_mshift(Dl,[0 1],0);
 end
 
-fprintf('KJW Matrix: Stage 2\n');
+%fprintf('KJW Matrix: Stage 2\n');
 M = zeros(p,p);
 for l = 1:p
-  if(mod(l,5)==0 | l==1) fprintf('l = %d, %g\n',l,toc); end
-  %Dl = diag(ones(p-l,1),l);
+  %if(mod(l,5)==0 | l==1) fprintf('l = %d, %g\n',l,toc); end
   Dl = D(:,:,l);
-  %RDl = fast_mshift(R,[0 l-1],0);
   RDlR = R*Dl*R;
   M(l,1) = trace(RDlR);
   for j = 2:p
     M(l,j) = trace(RDlR*DpDt(:,:,j)); 
   end
 end
-fprintf('KJW Matrix: cond = %g  (%g)\n',cond(M),toc);
+%fprintf('KJW Matrix: cond = %g  (%g)\n',cond(M),toc);
 
 
 return;
--- fsfast/toolbox/fast_yacf_kjw.m
+++ fsfast/toolbox/fast_yacf_kjw.m
@@ -1,5 +1,27 @@
 function [yacf, M] = fast_yacf_kjw(racf,R)
 % yacf = fast_yacf_kjw(racf,R)
+% 
+% Remove bias from residual autocorrelation function using Keith
+% Worsley's fmristats method. The bias is induced by projecting out
+% the task components to form the residual.
+%
+% racf is the autocor function of the residual. This
+%  can be nf X nv. See fast_acorr.m
+% R is the residual forming matrix
+% yacf is an approximation of the autocor function
+%  of the original noise.
+% M is the correction matrix.
+%
+% Notes:
+%  1. As the number of frames increases, the computation is 
+%     more intense and the correction matrix is more singular.
+%     There is some happy medium where the accuracy of the 
+%     ACF is best, but it depends upon the design among other
+%     things.
+%
+% See also: fast_acorr, fast_kjw_mtx.
+%
+% $Id: fast_yacf_kjw.m,v 1.4 2004/04/06 16:22:59 greve Exp $
 
 if(nargin ~= 2)
   fprintf('yacf = fast_yacf_kjw(racf,R)\n');
