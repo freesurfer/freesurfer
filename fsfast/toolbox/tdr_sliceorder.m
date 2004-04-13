8bef2180619389c65bf138e391ed091fa9c356f9
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-04-13 22:49:47 +0000

    Now works with even and odd number of slices (slice order differs on siemens)

--- fsfast/toolbox/tdr_sliceorder.m
+++ fsfast/toolbox/tdr_sliceorder.m
@@ -9,12 +9,17 @@ function so = tdr_sliceorder(nslices,which)
 % which = 2, convert an anat slice number into an acquired
 %  slice number. acqsliceno = so(anatsliceno)
 %
+% Siemens changes the interleaved slice order depending upon
+% whether there are an even or odd number of slices, eg
+%   odd(5):  1 3 5 2 4
+%   even(6): 2 4 6 1 3 5
+%
 % Example: if v is a stack of images in the order of acquisition,
 %  then so = tdr_sliceorder(nslices,2) and v2 = v(:,:,so), then
 %  v2 will be the same stack but with adjacent anatomical slices
 %  next to each other.
 % 
-% $Id: tdr_sliceorder.m,v 1.1 2003/10/20 22:15:53 greve Exp $
+% $Id: tdr_sliceorder.m,v 1.2 2004/04/13 22:49:47 greve Exp $
 
 so = [];
 if(nargin ~= 2)
@@ -28,16 +33,25 @@ if(which ~= 1 & which ~= 2)
 end
 
 if(which == 1)
-  so = [1:2:nslices 2:2:nslices];
+  % converts acq slice to anat slice
+  if(rem(nslices,2)==1) % odd
+    so = [1:2:nslices 2:2:nslices];
+  else % even
+    so = [2:2:nslices 1:2:nslices ];
+  end
   return;
 end
 
-M = floor(nslices/2) + 1;
-c = [1:M; M+1:2*M];
-so = reshape1d(c);
-so = so(1:nslices);
+so0 = tdr_sliceorder(nslices,1);
+[tmp so] = sort(so0);
 
-return;
+return
+
+%M = floor(nslices/2) + 1;
+%c = [1:M; M+1:2*M];
+%so = reshape1d(c);
+%so = so(1:nslices);
+%return;
 
 
 
