50a8f2125145249f853330c1f4a044b48e1d2960
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-04-28 02:17:46 +0000

    % [beta, rvar, t, tsig] = fast_vvglm(y,x,<polyorder>) % % Performs a voxel-by-voxel GLM analysis in which each column of x % and y are treated as their own set of equations to solve, ie, %   y(:,c) = x(:,c)*beta(c) + n

---
+++ fsfast/toolbox/fast_vvglm.m
@@ -0,0 +1,84 @@
+function [beta, rvar, t, tsig] = fast_vvglm(y,x,polyorder)
+% [beta, rvar, t, tsig] = fast_vvglm(y,x,<polyorder>)
+%
+% Performs a voxel-by-voxel GLM analysis in which each column of x
+% and y are treated as their own set of equations to solve, ie,
+%   y(:,c) = x(:,c)*beta(c) + n
+%
+% $Id: fast_vvglm.m,v 1.1 2004/04/28 02:17:46 greve Exp $
+
+beta = [];
+
+if(nargin < 2 | nargin > 3)
+  fprintf('[beta rvar t tsig] = fast_vvglm(y,x,<polyorder>)\n');
+  return;
+end
+
+if(~exist('polyorder','var')) polyorder = []; end
+
+[nfy nvy] = size(y);
+[nfx nvx] = size(y);
+
+if(nfy ~= nfx)
+  fprintf('ERROR: x and y do not have same number of frames\n');
+  return;
+end
+
+if(nvy ~= nvx)
+  fprintf('ERROR: x and y do not have same number of voxels\n');
+  return;
+end
+
+nv = nvx;
+nf = nfx;
+dof = nf - 1;
+
+% Detrend both, does not bias beta values
+if(~isempty(polyorder))
+  Xdt = fast_polytrendmtx(1,nf,1,polyorder);
+  Rdt = eye(nf) - Xdt*inv(Xdt'*Xdt)*Xdt';
+  x = Rdt*x;
+  y = Rdt*y;
+  dof = dof - polyorder;
+end
+
+
+% Now do voxel-by-voxel fit
+sumx2 = sum(x.^2);
+indz = find(sumx2==0);
+sumx2(indz) = 10e10;
+sumxy = sum(x.*y);
+beta = sumxy ./ sumx2;
+yhat = x .* repmat(beta,[nf 1]);
+r = y - yhat;
+rvar = sum(r.^2)/dof;
+
+% Do t-test
+betavar = rvar ./ sumx2;
+t = beta./sqrt(betavar);
+tsig = tTest(dof,t);
+
+% This is to test linear FPRs under null hypothesis
+% [pdf, alpha, nxhist, fpr] = ComputePDF(tsig,.01,1,.01);
+% loglog(alpha,alpha,alpha,fpr)
+
+return;
+
+
+
+
+% This was just to verify that this gives the same results as my
+% other programs. It does.
+ytmp = y(:,1);
+Xtmp = x(:,1);
+[betatmp rvartmp vdof] = fast_glmfit(ytmp,Xtmp);
+[F Fsig] = fast_fratio(betatmp,Xtmp,rvartmp,1);
+
+keyboard
+
+return;
+
+
+
+
+
