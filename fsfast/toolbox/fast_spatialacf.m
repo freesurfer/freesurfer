014079ba8290109e475c2dc4101f07e7160d664b
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-05-03 03:27:33 +0000

    % sacf = fast_spatialacf(fslice,r0,c0,maxlag,<normflag>) % % Computes the spatial (2D) autocorrelation function. The % correlation coefficient is computed as the normalized cross % correlation of the time courses.

---
+++ fsfast/toolbox/fast_spatialacf.m
@@ -0,0 +1,54 @@
+function sacf = fast_spatialacf(fslice,r0,c0,maxlag,normflag)
+% sacf = fast_spatialacf(fslice,r0,c0,maxlag,<normflag>)
+% 
+% Computes the spatial (2D) autocorrelation function. The
+% correlation coefficient is computed as the normalized cross
+% correlation of the time courses.
+%
+% fslice - functional slice [nr nc nf] = size(fslice)
+%
+% If normflag then normalizes. Otherwise assumes that the time
+% courses are already normalized.
+%
+% gauss std = fwhm/2.36
+%
+% $Id: fast_spatialacf.m,v 1.1 2004/05/03 03:27:33 greve Exp $
+%
+% (c) Douglas N. Greve, 2004.
+
+if(nargin < 4 | nargin > 5)
+  fprintf('sacf = fast_spatialacf(fslice,r0,c0,maxlag,<normflag>)\n');
+  return;
+end
+
+[nr nc nf] = size(fslice);
+if(r0-maxlag < 1 | r0+maxlag > nr)
+  fprintf('ERROR: r0/max lag\n');
+  return;
+end
+if(c0-maxlag < 1 | c0+maxlag > nc)
+  fprintf('ERROR: r0/max lag\n');
+  return;
+end
+
+% 2D sptial acf
+sacf = zeros(2*maxlag+1);
+
+v0 = squeeze(fslice(r0,c0,:));
+if(normflag) v0 = v0/sqrt(sum(v0.^2)); end
+
+nthdr = 1;
+for dr = -maxlag : maxlag
+  r = r0 + dr;
+  nthdc = 1;
+  for dc = -maxlag : maxlag
+    c = c0 + dc;
+    v = squeeze(fslice(r,c,:));
+    if(normflag) v = v/sqrt(sum(v.^2)); end
+    sacf(nthdr,nthdc) = v'*v0;
+    nthdc = nthdc + 1;
+  end
+  nthdr = nthdr + 1;
+end
+
+return;
