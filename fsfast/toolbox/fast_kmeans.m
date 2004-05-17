5367a712a441fe15d3addc8eb12ef00b8c9ffe29
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-05-17 02:01:11 +0000

    Allows fixing of one or more classes.

--- fsfast/toolbox/fast_kmeans.m
+++ fsfast/toolbox/fast_kmeans.m
@@ -1,28 +1,49 @@
-function [kmeans, kmap, d2min, niters, yhat] = fast_kmeans(y,nc,kmeans0,nitersmax)
-% [kmeans, kmap, d2min, niters, yhat] = fast_kmeans(y,nc,kmeans0,nitersmax)
+function [kmeans, kmap, d2min, niters, yhat] = fast_kmeans(y,nc,kmeans0,nitersmax,nfix)
+% [kmeans, kmap, d2min, niters, yhat] = fast_kmeans(y,nc,<kmeans0>,<nitersmax>,<nfix>)
 %
 % nc is number of classes (a better name would have been nk)
 % If nitersmax is not specified, uses 100.
 % If kmeans0 is not specified, uses first nc of y.
 % The mean squared error is mean(d2min) = mean(reshape1d(y-yhat).^2)
 %
-% $Id: fast_kmeans.m,v 1.4 2003/04/22 05:35:13 greve Exp $
+% nfix - fix the first nfix class means as specified in
+% kmeans0. nc-nfix class means are adapted.
+%
+% $Id: fast_kmeans.m,v 1.5 2004/05/17 02:01:11 greve Exp $
+%
 
 kmeans = [];
 kmap = [];
 d2min = [];
 niters = 0;
 
-if(nargin < 2 | nargin > 4)
- fprintf('[kmeans kmap d2min niters yhat] = fast_kmeans(y,nc,kmeans0,nitersmax)\n');
+if(nargin < 2 | nargin > 5)
+ fprintf('[kmeans, kmap, d2min, niters, yhat] = fast_kmeans(y,nc,<kmeans0>,<nitersmax>,<nfix>)\n');
  return;
 end
 
-[nf nv] = size(y);
+if(~exist('nfix','var')) nfix = 0; end
+if(isempty(kmeans0) & nfix > 0)
+  fprintf('ERROR: must specify kmeans0 with nfix\n');
+  return;
+end
+if(nfix >= nc)
+  fprintf('ERROR: nfix >= number of classesx\n');
+  return;
+end
 
-if(~exist('nitersmax')) nitersmax = 100; end
-if(~exist('kmeans0'))   kmeans0 = []; end
-if(isempty(kmeans0))    kmeans0 = y(:,1:nc); end
+if(~exist('nitersmax','var')) nitersmax = 100; end
+if(~exist('kmeans0','var'))   kmeans0 = []; end
+
+% If unspecified, init kmeans to first nc examples
+if(isempty(kmeans0))  kmeans0 = y(:,1:nc); end
+
+if(nc ~= size(kmeans0,2))
+  fprintf('ERROR: nc does not equal kmean0\n');
+  return;
+end
+
+[nf nv] = size(y);
 
 tic;
 niters = 0;
@@ -30,24 +51,30 @@ ndiff = nv;
 kmeans = kmeans0;
 while(niters < nitersmax & ndiff ~= 0)
 
-  %d2 = dist2(y,kmeans);
+  % Compute the distances between each input and each class
   for c=1:nc
     yhatc = repmat(kmeans(:,c),[1 nv]) ;
-    % Do weighting here %
-    if(nf > 1)  d2(c,:) = mean( ((y - yhatc).^2), 1 );
-    else        d2(c,:) = (y - yhatc).^2;
+    r = y - yhatc; % residual
+    if(nf > 1)  d2(c,:) = mean(r.^2, 1 );
+    else        d2(c,:) = r.^2;
     end
   end
 
+  % Remap each input to classes
   [d2min kmap] = min(d2,[],1);
 
-  for c=1:nc
+  % Recompute the class means for unfixed classes
+  for c = nfix+1:nc
     ind = find(kmap==c);
-    if(~isempty(ind))  kmeans(:,c) = mean(y(:,ind),2);
-    else               kmeans(:,c) = 0;
+    if(~isempty(ind)) kmeans(:,c) = mean(y(:,ind),2);
+    else
+      % Randomly reassign to one of the cols of y
+      ind = round(nv*rand)+1;
+      kmeans(:,c) = y(:,ind);
     end
   end
 
+  % Check whether anything has changed 
   if(niters ~= 0 )
     ndiff = length(find( (kmap-kmap0) ~= 0));
   end
@@ -55,12 +82,13 @@ while(niters < nitersmax & ndiff ~= 0)
   kmap0 = kmap;
   niters = niters + 1;
   if(1 | mod(niters,10)==0 | niters == 1)
-    fprintf('%3d %5d %14.13f %g\n',niters,ndiff,mean(d2min),toc);
+    %fprintf('%3d %5d %14.13f %g\n',niters,ndiff,mean(d2min),toc);
   end
 
-end
-fprintf('%3d %5d %14.13f %14.13f %g\n',niters,ndiff,...
-	mean(d2min),sqrt(mean(d2min)),toc);
+end % iteration loop
+
+%fprintf('%3d %5d %14.13f %14.13f %g\n',niters,ndiff,...
+%	mean(d2min),sqrt(mean(d2min)),toc);
 
 %------- Create estimate of the input ----------%
 if(nargout == 5)
