7a1585eba5d8c5554aacf06f2cb112bbabfbed68
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-05-27 01:40:06 +0000

    Changed to allow model type specification (ie, ARN+W or just ARN). Assumes R=I if R=[].

--- fsfast/toolbox/fast_arnw_fiterr.m
+++ fsfast/toolbox/fast_arnw_fiterr.m
@@ -1,16 +1,16 @@
-function [err, racfexp] = fast_arnw_fiterr(p,racf,R,w)
-% [err, racfexp] = fast_arnw_fiterr(p,racf,R,w)
+function [err, racfexp] = fast_arnw_fiterr(p,racf,R,w,modeltype)
+% [err, racfexp] = fast_arnw_fiterr(p,racf,<R>,<w>,<modeltype>)
 %
 % error function for fitting an ARN+White noise model while also
 % taking into account the bias introduced by GLM fitting. This
 % function can be used with fminsearch, which uses Nelder-Mead
 % unconstrained non-linear search. 
 %
-% p = [alpha phi1 phi2 ... phiN]; 
-%   phi1-phiN are the ARN parameters consistent with aryule.
-%   Note: these are negative relative to the "standard" 
-%   interpretation. Eg, for an AR1, phi1 = -0.5 corresponds
-%   to an acf = (0.5).^[1:nf]
+% p = [phi1 phi2 ... phiN alpha]; 
+%   phi1-phiN are the ARN parameters.
+%     Eg, for an AR1, phi1 = 0.5 corresponds
+%     to an acf = (0.5).^[1:nf]
+%   alpha is the white noise parameter
 %
 % racf is actual residual ACF (nf-by-1). 
 % R is residual forming matrix (nf-by-nf)
@@ -18,29 +18,44 @@ function [err, racfexp] = fast_arnw_fiterr(p,racf,R,w)
 %   no weighting is performed. In simulations, works best
 %   without weighting. But those are simulations. Consider
 %   weighting with w = 1./[1:nf]';
+% modeltype = 0 or not specified means an ARN+W
+% modeltype = 1 means an ARN with phi = p;
 % 
-% err = sum( abs(racf-racfexp).*w ); % L1 norm
-% 
-% See also: fast_arn2_acf
+% If R = [], then assumes R=I
 %
-% $Id: fast_arnw_fiterr.m,v 1.1 2004/05/24 00:09:29 greve Exp $
+% err = mean( abs(racf-racfexp).*w ); % L1 norm
+% 
+% See also: fast_arnw_acf
+%
+% $Id: fast_arnw_fiterr.m,v 1.2 2004/05/27 01:40:06 greve Exp $
 %
 % (c) Douglas N. Greve, 2004
 %
 
+err = [];
+racfexp = [];
+
 % Number of frames
 nf = length(racf);
 
+if(exist('modeltype','var') & modeltype == 1)
+  % Pure ARN
+  alpha = 0;
+  phi = p;
+else
+  % Pure ARN+White
+  alpha = p(end);
+  phi = p(1:end-1);
+end
+
 % Penalize for alpha being out of range
-alpha = p(1);
 if(alpha < 0 | alpha > 1)
   err = nf;
   return;
 end
 
 % Penalize for poles close to the unit circle
-phi = p(2:end);
-phi = [1 phi(:)'];
+phi = [1 -phi(:)'];
 poles = roots(phi);
 if(~isempty(find(abs(poles) > .95)))
   err = nf;
@@ -48,22 +63,23 @@ if(~isempty(find(abs(poles) > .95)))
 end
 
 % Theoretical autocor function
-nacf = fast_arnw_acf(p,nf);
+nacf = fast_arnw_acf(p(1:end-1),nf,p(end));
 
-% Create the noise covariaance matrix
-Sn = toeplitz(nacf);
-
-% Create the expected residual covariance matrix
-Srexp = R*Sn*R;
-
-% Extract the expected residual ACF
-racfexp = fast_cvm2acor(Srexp);
-
+if(exist('R','var') & ~isempty(R))
+  % Create the noise covariaance matrix
+  Sn = toeplitz(nacf);
+  % Create the expected residual covariance matrix
+  Srexp = R*Sn*R;
+  % Extract the expected residual ACF
+  racfexp = fast_cvm2acor(Srexp);
+else
+  racfexp = nacf;
+end
 % Error is L1 difference between actual and expected
 if(exist('w','var'))
-  err = sum( abs(racf-racfexp).*w );
+  err = mean( abs(racf-racfexp).*w );
 else
-  err = sum( abs(racf-racfexp) );
+  err = mean( abs(racf-racfexp) );
 end
 
 return;
