fb667344f5c21b546486875b6b9519b093359c02
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-06-11 17:19:14 +0000

    % fn = fast_fnorm(f,<fdim>,<demean>) % % functional normalization - divides each voxel by sqrt % sum of the squares across frames. If demean flag is non-zero, % the mean is removed first. % % Assumes functional dimension is 1 unless set by fdim

---
+++ fsfast/toolbox/fast_fnorm.m
@@ -0,0 +1,48 @@
+function fn = fast_fnorm(f,fdim,demean)
+% fn = fast_fnorm(f,<fdim>,<demean>)
+%
+% functional normalization - divides each voxel by sqrt 
+% sum of the squares across frames. If demean flag is non-zero, 
+% the mean is removed first.
+%
+% Assumes functional dimension is 1 unless set by fdim
+%
+% $Id: fast_fnorm.m,v 1.1 2004/06/11 17:19:14 greve Exp $
+
+fn = [];
+
+if(nargin < 1 | nargin > 3)
+  fprintf('fn = fast_fnorm(f,<fdim>,<demean>)\n');
+  return;
+end
+
+if(~exist('fdim','var')) fdim = []; end
+if(isempty('fdim')) fdim = 1; end
+
+if(~exist('demean','var')) demean = []; end
+if(isempty('demean')) demean = 1; end
+
+szf = size(f);
+if(length(szf) < fdim)
+  fprintf('ERROR: fdim = %d, but f only has %d dims\n',length(szf))
+  return;
+end
+
+nf = szf(fdim);
+repmatsz = ones(size(szf));
+repmatsz(fdim) = nf;
+
+fn = f;
+
+if(demean)
+  fmn = mean(f,fdim);
+  fn = fn - repmat(fmn,repmatsz);
+end
+
+fss2 = sqrt(sum(fn.^2,fdim));
+ind = find(fss2==0);
+fss2(ind) = 1e10;
+
+fn = fn./repmat(fss2,repmatsz);
+
+return;
