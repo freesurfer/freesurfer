0826ab73b0a94957d62008e4e8bbc25dad418b54
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-07-19 14:37:12 +0000

    one more xfm to test

--- utils/test/mnitest.cpp
+++ utils/test/mnitest.cpp
@@ -15,39 +15,52 @@ extern "C" {
 }
 
 char *file1="./bruce.xfm";
-char *file2="./david.xfm";
-char *file3="./tosa.xfm";
+char *file2="./bruce2.xfm";
+char *file3="./david.xfm";
+char *file4="./tosa.xfm";
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-  LTA *lta1, *lta2, *lta3;
+  LTA *lta1, *lta2, *lta3, *lta4;
+  cout << "////////////////////////////////////" << endl;
   cout << "Old way of reading -----------------" << endl;
+  cout << "////////////////////////////////////" << endl;
   cout << "bruce.xfm" << endl;
   lta1 = LTAread(file1);
   LTAprint(stdout, lta1);
-  cout << "david.xfm" << endl;
+  cout << "bruce2.xfm" << endl;
   lta2 = LTAread(file2);
   LTAprint(stdout, lta2);
-  cout << "tosa.xfm" << endl;
+  cout << "david.xfm" << endl;
   lta3 = LTAread(file3);
   LTAprint(stdout, lta3);
+  cout << "tosa.xfm" << endl;
+  lta4 = LTAread(file4);
+  LTAprint(stdout, lta4);
   LTAfree(&lta1);
   LTAfree(&lta2);
   LTAfree(&lta3);
+  LTAfree(&lta4);
 
+  cout << "////////////////////////////////////" << endl;
   cout << "New way of reading -----------------" << endl;
+  cout << "////////////////////////////////////" << endl;
   cout << "bruce.xfm" << endl;
   lta1 = LTAreadEx(file1);
   LTAprint(stdout, lta1);
-  cout << "david.xfm" << endl;
+  cout << "bruce2.xfm" << endl;
   lta2 = LTAreadEx(file2);
   LTAprint(stdout, lta2);
-  cout << "tosa.xfm" << endl;
+  cout << "david.xfm" << endl;
   lta3 = LTAreadEx(file3);
   LTAprint(stdout, lta3);
+  cout << "tosa.xfm" << endl;
+  lta4 = LTAreadEx(file4);
+  LTAprint(stdout, lta4);
   LTAfree(&lta1);
   LTAfree(&lta2);
   LTAfree(&lta3);
+  LTAfree(&lta4);
 }
