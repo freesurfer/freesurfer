e2b567976bac3ed2602b5d642339757bd8c57b42
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-08-02 15:46:51 +0000

    when gcc2.96 is used

--- scuba/string_fixed.h
+++ scuba/string_fixed.h
@@ -1,10 +1,7 @@
 #ifndef string_fixed_h
 #define string_fixed_h
 
-#include <ansidecl.h>
-
-
-#if (GCC_VERSION <= 2096)
+#if (__GNUC__ <= 2)
 // pick particular version of alloc.h!
 #include "/usr/include/g++-3/alloc.h"
 #endif
