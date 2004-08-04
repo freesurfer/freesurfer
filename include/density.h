931041bc6cbfa9c4e3c50634d452fae3df999a5c
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2004-08-04 20:39:31 +0000

    added histo_* progs.
    program for computing the joint density between two images (histology
    and block) that have been previously aligned.
    
    program for computing the registration between a histological (Nissl)
    and a block face image.
    
    utilities for computing joint densities from images
    tukey biweight.

---
+++ include/density.h
@@ -0,0 +1,35 @@
+#ifndef DENSITY_H
+#define DENSITY_H
+
+#include <stdio.h>
+#include "image.h"
+#include "const.h"
+#include "mri.h"
+#include "matrix.h"
+
+typedef struct
+{
+	float   min_val1 ;
+	float   min_val2 ;
+	float   max_val1 ;
+	float   max_val2 ;
+	int     *valid1 ;
+	int     *valid2 ;
+	char    fname1[STRLEN] ;
+	char    fname2[STRLEN] ;
+	float   sigma ;
+	int     dof ;
+	float   min_p ;
+	IMAGE   *Ipdf ;
+} DENSITY ;
+
+DENSITY *DensityHistogramEstimate(MRI *mri1, MRI *mri2, int nbins, float sigma, int *valid1, int *valid2) ;
+int      DensityWrite(DENSITY *pdf, char *fname) ;
+double   DensityLogLikelihood(DENSITY *pdf, float val1, float val2) ;
+DENSITY *DensityRead(char *fname) ;
+MRI      *DensityLikelihoodImage(MRI *mri1, MRI *mri2, MRI *mri_ll, MATRIX *m, DENSITY *pdf, MRI *mri_seg, int inverse);
+
+#define val2bin1(pdf,val1)   ((val1-pdf->min_val1) * (pdf->Ipdf->rows /  (pdf->max_val1 - pdf->min_val1)))
+#define val2bin2(pdf,val2)   ((val2-pdf->min_val2) * (pdf->Ipdf->rows /  (pdf->max_val2 - pdf->min_val2)))
+
+#endif
---
+++ include/tukey.h
@@ -0,0 +1,6 @@
+#ifndef TUKEY_H
+#define TUKEY_H
+
+double tukey_biweight(double residual, double C) ;
+
+#endif
