705f741abebfc1b0edfc01c472fe71ddb8e70378
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-09-08 20:48:36 +0000

    added nonerror case

--- utils/test/inftest.cpp
+++ utils/test/inftest.cpp
@@ -15,10 +15,23 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  int res = devIsnan(0.f/0.f);
+  float v = 0.f;
+  int res = devIsnan(0.f/v);
   if (res != 1)
   {
     cerr << "should be nan when 0.f/0.f" << endl;
+    return -1;
+  }
+  res= devIsnan(1.f);
+  if (res != 0)
+  {
+    cerr << "should not be nan when 0.f/0.f" << endl;
+    return -1;
+  }
+  res= devIsinf(1.f);
+  if (res != 0)
+  {
+    cerr << "should not be inf when 1.f" << endl;
     return -1;
   }
   res = devIsinf(HUGE_VALF);
