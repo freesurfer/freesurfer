62994813bb6a345327722bfe087efd02880f847b
Kevin Teich <kteich@nmr.mgh.harvard.edu>
2004-09-16 20:39:53 +0000

    *** empty log message ***

--- scuba/test_RASToVoxel.cpp
+++ scuba/test_RASToVoxel.cpp
@@ -21,6 +21,65 @@ char* Progname = "test_RASToVoxel";
 
 
 
+void TestCoord ( Transform44& rasToVoxel, Point3<float>& ras ) {
+
+
+    Point3<int> idxi;
+    Point3<float> idxf;
+    Point3<float> rasFromIdxICheck;
+    Point3<float> rasFromIdxFCheck;
+    Point3<int> idxiFromRASFromIdxICheck;
+    Point3<float> idxfFromRASFromIdxICheck;
+
+    rasToVoxel.MultiplyVector3( ras.xyz(), idxi.xyz() );
+    rasToVoxel.MultiplyVector3( ras.xyz(), idxf.xyz() );
+    
+    if( idxi.x() != (int) idxf.x() ||
+	idxi.y() != (int) idxf.y() ||
+	idxi.z() != (int) idxf.z() ) {
+      
+      stringstream ssError;
+      ssError << "RAS -> index float didn't match int: "
+	      << ras << " -> " << idxf << ", " << idxi;
+      throw runtime_error( ssError.str() );
+    }
+    
+    rasToVoxel.InvMultiplyVector3( idxf.xyz(), rasFromIdxFCheck.xyz() );
+    
+    if( fabs (rasFromIdxFCheck.x() - ras.x()) > 0.00001 ||
+	fabs (rasFromIdxFCheck.y() - ras.y()) > 0.00001 ||
+	fabs (rasFromIdxFCheck.z() - ras.z()) > 0.00001 ) {
+      
+      stringstream ssError;
+      ssError << "RAS -> index float -> RAS didn't match orig RAS: "
+	      << ras << " -> " << idxf << " -> " << rasFromIdxFCheck;
+      throw runtime_error( ssError.str() );
+    }
+    
+    rasToVoxel.InvMultiplyVector3( idxi.xyz(), rasFromIdxICheck.xyz() );
+    
+    rasToVoxel.MultiplyVector3( rasFromIdxICheck.xyz(), 
+				idxiFromRASFromIdxICheck.xyz() );
+    rasToVoxel.MultiplyVector3( rasFromIdxICheck.xyz(), 
+				idxfFromRASFromIdxICheck.xyz() );
+    
+    if( idxiFromRASFromIdxICheck.x() != 
+	(int) idxfFromRASFromIdxICheck.x() ||
+	idxiFromRASFromIdxICheck.y() != 
+	(int) idxfFromRASFromIdxICheck.y() ||
+	idxiFromRASFromIdxICheck.z() != 
+	(int) idxfFromRASFromIdxICheck.z() ) {
+      
+      stringstream ssError;
+      ssError << "RAS -> index int -> RAS -> index float "
+	      << "didn't match int: "
+	      << ras << " -> " << idxf << " -> " << rasFromIdxICheck
+	      << " -> " << idxiFromRASFromIdxICheck << ", " 
+	      << idxfFromRASFromIdxICheck;
+      throw runtime_error( ssError.str() );
+    }
+}
+
 int main ( int argc, char** argv ) {
 
   cerr << "Beginning test" << endl;
@@ -34,68 +93,27 @@ int main ( int argc, char** argv ) {
 				 0,      0,      0,      1 );
 
     Point3<float> ras;
-    Point3<int> idxi;
-    Point3<float> idxf;
-    Point3<float> rasFromIdxICheck;
-    Point3<float> rasFromIdxFCheck;
-    Point3<int> idxiFromRASFromIdxICheck;
-    Point3<float> idxfFromRASFromIdxICheck;
 
-    for( float z = -10.0; z < 10.0; z += 0.05 ) {
-      for( float y = -10.0; y < 10.0; y += 0.05 ) {
-	for( float x = -10.0; x < 10.0; x += 0.05 ) {
-	
+    for( float z = -10.0; z < 10.0; z += 0.0490005 ) {
+      for( float y = -10.0; y < 10.0; y += 0.0490003 ) {
+	for( float x = -10.0; x < 10.0; x += 0.0490001 ) {
 	  ras.Set( x, y, z );
-	  rasToVoxel.MultiplyVector3( ras.xyz(), idxi.xyz() );
-	  rasToVoxel.MultiplyVector3( ras.xyz(), idxf.xyz() );
-
-	  if( idxi.x() != (int) idxf.x() ||
-	      idxi.y() != (int) idxf.y() ||
-	      idxi.z() != (int) idxf.z() ) {
-	    
-	    stringstream ssError;
-	    ssError << "RAS -> index float didn't match int: "
-		    << ras << " -> " << idxf << ", " << idxi;
-	    throw runtime_error( ssError.str() );
-	  }
-	  
-	  rasToVoxel.InvMultiplyVector3( idxf.xyz(), rasFromIdxFCheck.xyz() );
-
-	  if( fabs (rasFromIdxFCheck.x() - ras.x()) > 0.00001 ||
-	      fabs (rasFromIdxFCheck.y() - ras.y()) > 0.00001 ||
-	      fabs (rasFromIdxFCheck.z() - ras.z()) > 0.00001 ) {
-
-	    stringstream ssError;
-	    ssError << "RAS -> index float -> RAS didn't match orig RAS: "
-		    << ras << " -> " << idxf << " -> " << rasFromIdxFCheck;
-	    throw runtime_error( ssError.str() );
-	  }
-
-	  rasToVoxel.InvMultiplyVector3( idxi.xyz(), rasFromIdxICheck.xyz() );
-
-	  rasToVoxel.MultiplyVector3( rasFromIdxICheck.xyz(), 
-				      idxiFromRASFromIdxICheck.xyz() );
-	  rasToVoxel.MultiplyVector3( rasFromIdxICheck.xyz(), 
-				      idxfFromRASFromIdxICheck.xyz() );
-	  
-	  if( idxiFromRASFromIdxICheck.x() != 
-	      (int) idxfFromRASFromIdxICheck.x() ||
-	      idxiFromRASFromIdxICheck.y() != 
-	      (int) idxfFromRASFromIdxICheck.y() ||
-	      idxiFromRASFromIdxICheck.z() != 
-	      (int) idxfFromRASFromIdxICheck.z() ) {
-	    
-	    stringstream ssError;
-	    ssError << "RAS -> index int -> RAS -> index float "
-		    << "didn't match int: "
-		    << ras << " -> " << idxf << " -> " << rasFromIdxICheck
-		    << " -> " << idxiFromRASFromIdxICheck << ", " 
-		    << idxfFromRASFromIdxICheck;
-	    throw runtime_error( ssError.str() );
-	  }
-
+	  TestCoord( rasToVoxel, ras );
 	}
       }
+    }
+
+    for( int nTrial = 0; nTrial < 1000; nTrial++ ) {
+
+      do {
+	ras.Set( (float)random()/(float)random(),
+		 (float)random()/(float)random(),
+		 (float)random()/(float)random() );
+      } while ( ras.x() < -50.0 || ras.x() > 50.0 ||
+		ras.y() < -50.0 || ras.y() > 50.0 ||
+		ras.z() < -50.0 || ras.z() > 50.0 );
+
+      TestCoord( rasToVoxel, ras );
     }
  
   }
