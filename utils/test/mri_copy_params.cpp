553dffd346934d27e223871b85146215a8d5d2cf
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-09-28 16:22:58 +0000

    update

--- utils/test/mri_copy_params.cpp
+++ utils/test/mri_copy_params.cpp
@@ -23,7 +23,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2)
+  if (argc < 3)
   {
     cout << "Usage  : mri_copy_params <input volume> <like volume> [<output volume>]" << endl;
     cout << "         if the output volume is not given, it will overwrite input volume " << endl;
@@ -34,17 +34,17 @@ int main(int argc, char *argv[])
   string likevol;
   string outvol;
 
-  if (argc == 2)
+  if (argc == 3)
   {
     invol = argv[1];
     likevol = argv[2];
     outvol = argv[1];
   }
-  else if (argc ==3)
+  else if (argc ==4)
   {
     invol = argv[1];
     likevol = argv[2];
-    outvol = argv[1];
+    outvol = argv[3];
   }
   MRI *imri = MRIread(const_cast<char *> (invol.c_str()));
   if (!imri)
@@ -59,11 +59,14 @@ int main(int argc, char *argv[])
     cerr << "could not open " << invol.c_str() << endl;
     return -1;
   }
+  printf("Original Values ... TR: %2.2f msec, TE: %2.2f msec, TI: %2.2f msec, flip angle: %2.2f degrees\n",
+	 imri->tr, imri->te, imri->ti, DEGREES(imri->flip_angle)) ;
   imri->tr = lmri->tr;
   imri->te = lmri->te;
   imri->ti = lmri->ti;
   imri->flip_angle = lmri->flip_angle;
-  
+  printf("Modified Values ... TR: %2.2f msec, TE: %2.2f msec, TI: %2.2f msec, flip angle: %2.2f degrees\n",
+	 imri->tr, imri->te, imri->ti, DEGREES(imri->flip_angle)) ;
   int val = MRIwrite(imri, const_cast<char *>(outvol.c_str()));
   if (val)
   {
