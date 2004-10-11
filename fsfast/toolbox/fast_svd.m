55d45ab109b553b90179aaa1c66fe06264378dd9
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-10-11 23:27:19 +0000

    Now returns all EVs regardless of whether the sing val is 0

--- fsfast/toolbox/fast_svd.m
+++ fsfast/toolbox/fast_svd.m
@@ -6,15 +6,16 @@ function [u,s,v] = fast_svd(y,M)
 % number of eigen components are computed. If M is supplied, then
 % must be M = y*y' or M = y'*y depending upon the size of y.
 %
+% nmin = min(nrows,ncols)
 % u will have dimension nrows by nmin
 % v will have dimension ncols by nmin
 % s will have dimension nmin by nmin
-%     where nmin = min(nrows,ncols)
-%     nmin can also be determined as the number of non-zero singvals
 % 
 % In any case, y = u*s*v';
 %
-% $Id: fast_svd.m,v 1.3 2004/09/23 18:25:39 greve Exp $
+% Note: all vectors are returned even if the corresponding value is 0
+%
+% $Id: fast_svd.m,v 1.4 2004/10/11 23:27:19 greve Exp $
 %
 
 u=[];
@@ -37,12 +38,9 @@ if(nr > nc)
   if(~exist('M','var')) M = y'*y; end
   [v s blah] = svd(M);
   s = sqrt(s);
-  ds = diag(s);
-  ns = length(find(ds > ds(1)/1e6));
-  nn = 1:ns;
-  s = s(nn,nn);
-  v = v(:,nn);
-  u = y*(v*inv(s));
+  % Could do: u = y*v*inv(s)), but inv(s) is just a scale factor
+  % which will be removed during normalization
+  u = y*v;
   uss2 = sqrt(sum(u.^2));
   u = u./repmat(uss2,[nr 1]);
   return;
@@ -53,12 +51,9 @@ end
 if(~exist('M','var')) M = y*y'; end
 [u s blah] = svd(M);
 s = sqrt(s);
-ds = diag(s);
-ns = length(find(ds > ds(1)/1e6));
-nn = 1:ns;
-s = s(nn,nn);
-u = u(:,nn);
-v = y'*(u*inv(s));
+% Could do: v = y'*(u*inv(s)), but inv(s) is just a scale factor
+% which will be removed during normalization
+v = y'*u; 
 vss2 = sqrt(sum(v.^2));
 v = v./repmat(vss2,[nc 1]);
 
