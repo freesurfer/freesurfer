10b287cc7d10a0d51834ee7ecf615d2fb975f606
Yasunari Tosa <tosa@nmr.mgh.harvard.edu>
2004-10-13 16:01:06 +0000

    sample program

---
+++ dummy/dummy.c
@@ -0,0 +1,15 @@
+/**
+ * @file   dummy.c
+ * @author Yasunari Tosa
+ * @date   Wed Oct 13 11:47:18 2004
+ * 
+ * @brief  sample dummy program
+ * 
+ * 
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Just a dummy.\n");
+}
---
+++ dummy/myown.c
@@ -0,0 +1,15 @@
+/**
+ * @file   myown.c
+ * @author Yasunari Tosa
+ * @date   Wed Oct 13 11:48:26 2004
+ * 
+ * @brief  sample test program
+ * 
+ * 
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("just a sample program.\n");
+}
