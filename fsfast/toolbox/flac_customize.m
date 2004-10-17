81eda9d53771b0848225b780bbe937174c05915e
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-10-17 18:33:53 +0000

    Now sessid and nthrun are in flac struct

--- fsfast/toolbox/flac_customize.m
+++ fsfast/toolbox/flac_customize.m
@@ -1,26 +1,50 @@
 function flacnew = flac_customize(flac)
-%  flacnew = flac_customize(flac)
+% flacnew = flac_customize(flac)
 %
-% $Id: flac_customize.m,v 1.1 2004/10/16 05:19:36 greve Exp $
+% Gets number of time points for the given run, loads all stimulus
+% timings, and loads all nonpar matrices. 
+%
+% Caller must set: flac.sess and flac.nthrun
+%
+% See flac_desmtx for how the design matrices are built.
+%
+% $Id: flac_customize.m,v 1.2 2004/10/17 18:33:53 greve Exp $
 
 flacnew = [];
-
 if(nargin ~= 1)
- fprintf('[X, taskind] = flac_customize(flac)\n');
+ fprintf('flacnew = flac_customize(flac)\n');
  return;
 end
+flacnew = flac;
 
-sess = flac.sess;
-run  = flac.run;
-runpath = sprintf('%s/%s/%03d',sess,flac.fsd,run);
+% Construct path names
+fsdpath = sprintf('%s/%s',flac.sess,flac.fsd);
+flacnew.runlist = fast_runlist(fsdpath,flac.runlistfile);
+if(isempty(flacnew.runlist))
+  fprintf('ERROR: cannot find any runs in %s\n',fsdpath);
+  flacnew = [];
+  return; 
+end
+  
+flacnew.nruns = length(flacnew.runlist);
+if(flacnew.nruns < flac.nthrun)
+  fprintf(['ERROR: requested nth run %d is greater than the number of' ...
+	   ' runs %d\n'],flacnew.nthrun,flacnew.nruns);
+  flacnew = [];
+  return; 
+end
+runid = flacnew.runlist(flac.nthrun,:);
+
+runpath = sprintf('%s/%s',fsdpath,runid);
 fstem = sprintf('%s/%s',runpath,flac.funcstem);
 
+% Get the number of time points
 [nslices nrows ncols ntp] = fmri_bvoldim(fstem);
 if(nslices == 0) 
   fprintf('ERROR: attempting to read %s\n',fstem);
+  flacnew = [];
   return; 
 end
-flacnew = flac;
 flacnew.ntp = ntp;
 
 nev = length(flac.ev);
@@ -29,14 +53,18 @@ for nthev = 1:nev
   
   % HRF Regressors
   if(ev.ishrf)  
+    % Just get the stimulus timing
     stfpath = sprintf('%s/%s',runpath,ev.stf);
     st = fast_ldstf(stfpath);
-    if(isempty(st)) flacnew = []; return; end
+    if(isempty(st)) 
+      fprintf('ERROR: reading timing file %s\n',stfpath);
+      flacnew = []; return; 
+    end
     flacnew.ev(nthev).st = st;
-    ev.Xfir = fast_st2fir(st,ntp,flac.TR,ev.psdwin,1);
+    continue;
   end  
   
-  % Non-parametric regressors
+  % Non-parametric regressors - load and demean matrix
   if(strcmp(ev.model,'nonpar'))
     nonparpath = sprintf('%s/%s',runpath,ev.nonparname);
     X = fast_ldbslice(nonparpath);
@@ -63,8 +91,9 @@ for nthev = 1:nev
     Xmn = mean(X,1);
     X = X - repmat(Xmn,[ntp 1]);
     flacnew.ev(nthev).X = X;
+    continue;
   end
-  
+
 end
 
 return;
