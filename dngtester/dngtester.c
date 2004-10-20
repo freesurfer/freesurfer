7871e734952ec2aa424bdbcfe8db3801cd748b49
Doug Greve <greve@nmr.mgh.harvard.edu>
2004-10-20 18:41:56 +0000

    Test program for doug.

---
+++ dngtester/dngtester.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "volcluster.h"
+#include "gsl/gsl_randist.h"
+
+char *Progname = "dngtester";
+
+/*----------------------------------------*/
+int main(int argc, char **argv)
+{
+  printf("hello, whoever\n");
+  return(0);
+}
