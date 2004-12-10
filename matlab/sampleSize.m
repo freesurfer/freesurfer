d86d14949daf3ca6dd4b28375f4d4078f4717dd6
Xiao Han <xhan@nmr.mgh.harvard.edu>
2004-12-10 17:12:02 +0000

    Add choice for two-sided and one-sided

--- matlab/sampleSize.m
+++ matlab/sampleSize.m
@@ -1,17 +1,16 @@
-function N = sampleSize(stdS, effectsize, alpha, targetPOWER);
-% function N = sampleSize(stdS, effectsize, alpha, power);
+function N = sampleSize(stdS, effectsize, alpha, targetPOWER, alternative);
+% function N = sampleSize(stdS, effectsize, alpha, power, alternative);
 % 
-%                author: Xiao Han
+% compute the required sample-size for significance level alpha and power
+% assuming parallel study with two groups of equal-size
 %
-% compute the sample-size for significance level alpha (1-sided) and parallel study
-%
-% stdS is the standard deviation of measurement
+% stdS is the standard deviation of measurement (equal for both groups)
 % effectsize is the effect to look for (difference in means of two groups)
 % alpha is the significance level, default = 0.01
 % power is the desired power, default = 0.8
+% alternative is a string for either 'one-sided' or 'two-sided'; defaul is 'two-sided'
+% tested against http://www.stat.uiowa.edu/~rlenth/Power/, and the results match perfectly
 
-%for two-sided, use
-%alpha = 0.5*alpha ?
 
 if nargin < 2,
     error('Insufficient Inputs');
@@ -20,32 +19,44 @@ end;
 
 if nargin < 3,
    alpha = 0.01;
-   targetPOWER = 0.8;
-elseif nargin < 4
+end;
+
+if nargin < 4
     targetPOWER = 0.8;
 end;
 
-
+if nargin < 5,
+    alternative = 'two-sided';
+end;
+    
+if strcmp(alternative, 'one_sided'),
+    disp('assuming one-sided t-test');
+    alpha = alpha;
+else,
+    disp('assuming two-sided t-test');
+    alpha = 0.5*alpha;
+end;
+    
 N1 = 1;
 N2 = 1000;
 
 %first test whether requiring N > 2*1000
-%i.e., see whether 2*N2 satisfies the required power and significance level
+%i.e., see whether N2 satisfies the required power and significance level
 t_alpha = tinv(1-alpha, N2 + N2 - 2);
-t_delta = effectsize*sqrt(0.5*N2)/stdS;
+noncentrality = effectsize*sqrt(0.5*N2)/stdS;
 
 %if the following is true, the significant level won't be achieved 
-if t_alpha > t_delta,
-    disp(sprintf('Requires more than %d subjects.', 2*N2));
-    N = 2*N2;
-    return;
-end;
+%if t_alpha > noncentrality,
+%    disp(sprintf('Requires more than %d subjects for each group.', N2));
+%    N = N2;
+%    return;
+%end;
 
 %if the following test is true, then the power level won't be achieved
-power_N2 = 1 - tcdf(t_alpha - t_delta, N2 + N2 -2);
+power_N2 = 1 - nctcdf(t_alpha, N2 + N2 -2, noncentrality);
 if power_N2 < targetPOWER,
-    disp(sprintf('Requires more than %d subjects.', 2*N2));
-    N = 2*N2;
+    disp(sprintf('Requires more than %d subjects for each group.', N2));
+    N = N2;
     return; 
 end;
 
@@ -55,10 +66,10 @@ N = ceil((N1+N2)*0.5);
 while N2 - N1 > 1,
         
     t_alpha = tinv(1-alpha, N + N -2);
-    t_delta = effectsize*sqrt(0.5*N)/stdS;
+    noncentrality = effectsize*sqrt(0.5*N)/stdS;
 
     %evaluate power
-    power_N = 1 - tcdf(t_alpha - t_delta, N + N -2);
+    power_N = 1 - nctcdf(t_alpha, N + N -2, noncentrality);
     
     if power_N >= targetPOWER,
         N2 = N;
@@ -69,8 +80,6 @@ while N2 - N1 > 1,
     N = ceil((N1+N2)*0.5);
 end;
 
-%reuires N samples for each group, so total size = 2N
-N = N + N;
-
-disp(sprintf('The required total sample size (two groups) is %d.',N));
+%requires N samples for each group
+disp(sprintf('The required sample size for each group is %d.',N));
 return;
