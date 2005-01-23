b933be2586bc0aa76795d8b859457068241d3fb2
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-01-23 17:00:23 +0000

    Uses abs().

--- fsfast/toolbox/fast_z2p.m
+++ fsfast/toolbox/fast_z2p.m
@@ -3,11 +3,10 @@ function p = fast_z2p(z)
 % converts z values into p values (ie significances)
 %
 
-zsize = size(z);
-z = reshape1d(z);
-
-p = erfc(z/sqrt(2))/2;
-p = reshape(p,zsize);
+p = erfc(abs(z(:))/sqrt(2))/2;
+p = reshape(p,size(z)).*sign(z);
+ind = find(z==0);
+p(ind) = 1;
 
 return;
 
