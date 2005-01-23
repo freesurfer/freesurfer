c20171aec79010de5a83289b36fc98d370c06de4
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-01-23 17:20:16 +0000

    Just changed tabbing to make it look nicer.

--- matlab/read_surf.m
+++ matlab/read_surf.m
@@ -26,30 +26,30 @@ QUAD_FILE_MAGIC_NUMBER =  16777215 ;
 
 fid = fopen(fname, 'rb', 'b') ;
 if (fid < 0)
-	 str = sprintf('could not open curvature file %s.', fname) ;
-	 error(str) ;
+  str = sprintf('could not open curvature file %s.', fname) ;
+  error(str) ;
 end
 magic = fread3(fid) ;
 
-if (magic == QUAD_FILE_MAGIC_NUMBER)
-	 vnum = fread3(fid) ;
-	 fnum = fread3(fid) ;
-	 vertex_coords = fread(fid, vnum*3, 'int16') ./ 100 ; 
-	 if (nargout > 1)
-	 	 for i=1:fnum
-		 	 for n=1:4
-			 		 faces(i,n) = fread3(fid) ;
-			 end
-		 end
-	 end
+if(magic == QUAD_FILE_MAGIC_NUMBER)
+  vnum = fread3(fid) ;
+  fnum = fread3(fid) ;
+  vertex_coords = fread(fid, vnum*3, 'int16') ./ 100 ; 
+  if (nargout > 1)
+    for i=1:fnum
+      for n=1:4
+	faces(i,n) = fread3(fid) ;
+      end
+    end
+  end
 elseif (magic == TRIANGLE_FILE_MAGIC_NUMBER)
-   fgets(fid) ;
-	 fgets(fid) ;
-	 vnum = fread(fid, 1, 'int32') ;
-	 fnum = fread(fid, 1, 'int32') ;
-	 vertex_coords = fread(fid, vnum*3, 'float32') ; 
-	 faces = fread(fid, fnum*3, 'int32') ;
-	 faces = reshape(faces, 3, fnum)' ;
+  fgets(fid) ;
+  fgets(fid) ;
+  vnum = fread(fid, 1, 'int32') ;
+  fnum = fread(fid, 1, 'int32') ;
+  vertex_coords = fread(fid, vnum*3, 'float32') ; 
+  faces = fread(fid, fnum*3, 'int32') ;
+  faces = reshape(faces, 3, fnum)' ;
 end
 
 vertex_coords = reshape(vertex_coords, 3, vnum)' ;
