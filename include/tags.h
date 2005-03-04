2781c13ff82c9d094487de2384b95b0d2139af3d
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2005-03-04 21:18:10 +0000

    added some GCAMORPH stuff

--- include/tags.h
+++ include/tags.h
@@ -1,12 +1,15 @@
 #ifndef TAGS_H
 #define TAGS_H
 
-#define TAG_COLORTABLE   1
-#define TAG_USEREALRAS   2
+#define TAG_COLORTABLE         1
+#define TAG_USEREALRAS         2
 
-#define TAG_GCAMORPH_GEOM  10
-#define TAG_SURF_GEOM      20
+#define TAG_GCAMORPH_GEOM      10
+#define TAG_GCAMORPH_TYPE      11
+#define TAG_GCAMORPH_LABELS    12
 
-#define TAG_MGH_XFORM      30
+#define TAG_SURF_GEOM          20
+
+#define TAG_MGH_XFORM          30
  
 #endif
