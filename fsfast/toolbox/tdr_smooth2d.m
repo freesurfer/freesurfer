8375913d82e3d2796382e46103353e4a062e2c44
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-03-19 00:20:26 +0000

    Added zpadding

--- fsfast/toolbox/tdr_smooth2d.m
+++ fsfast/toolbox/tdr_smooth2d.m
@@ -6,7 +6,7 @@ function [imgsm, G] = tdr_smooth2d(img,sig1,sig2)
 % sig1 - gaussian standard dev for the rows
 % sig2 - gaussian standard dev for the columns
 %
-% $Id: tdr_smooth2d.m,v 1.1 2003/11/06 19:45:37 greve Exp $
+% $Id: tdr_smooth2d.m,v 1.2 2005/03/19 00:20:26 greve Exp $
 
 if(nargin ~= 3)
   fprintf('[imgsm filtimg] = tdr_smooth2d(img,sig1,sig2)\n');
@@ -14,30 +14,24 @@ if(nargin ~= 3)
 end
 
 [nrows ncols] = size(img);
+nrows2 = 2*nrows;
+ncols2 = 2*ncols;
 
-% Create a 2d kernel - same in image and kspace 
-%filtimg = exp(-1/2*(((([1:nrows]-(nrows/2+1))'*ones(1,ncols))/nrows*sig1).^2+((ones(nrows,1)*([1:ncols]-(ncols/2+1)))/ncols*sig2).^2));
-
-x = ([1:nrows] - (nrows/2 + 1))/sig1;
-xx = ones(nrows,1) * x;
-y = ([1:ncols]' - (ncols/2 + 1))/sig2;
-yy = y * ones(1,ncols);
+x = ([1:nrows2] - (nrows2/2 + 1))/sig1;
+xx = ones(nrows2,1) * x;
+y = ([1:ncols2]' - (ncols2/2 + 1))/sig2;
+yy = y * ones(1,ncols2);
 G = exp( -(xx.^2)/2 + -(yy.^2)/2 );
-G = G/max(reshape1d(G));
+G = G/sum(reshape1d(G));
 
-%sfiltimg = fftshift(fftn(ifftshift(filtimg)));
-%figure; imagesc(log(filtimg)); colorbar; axis equal; axis image;
-%figure; imagesc(log(abs(sfiltimg))); colorbar; axis equal; axis image;
+% zero pad
+zpimg = zeros(2*size(img));
+zpimg(1:nrows,1:ncols) = img;
 
-% Conver the image to kspace
-%kimg = fftshift(ifftn(img));
-
-% Smooth in kspace and return to image space
-%imgsm = fftn(ifftshift(kimg.*filtimg));
-
-kimg  = fftn(img);
+kimg  = fftn(zpimg); 
 kG    = fftn(G);
 imgsm = fftshift(ifftn(kimg.*kG));
+imgsm = imgsm(1:nrows,1:ncols); 
 
 if( max(abs(reshape1d(imag(img)))) == 0 )
   % Only take the magnitude if input is totally really
