fecb0f1e9b6fc4ed6c0d268fce18f82ed2a8902a
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2005-04-04 18:37:24 +0000

    added Ggca_nbr_label

--- include/gca.h
+++ include/gca.h
@@ -343,7 +343,7 @@ GCA *GCAcompactify(GCA *gca);
 MRI *GCAreplaceImpossibleLabels(MRI *mri_inputs, GCA *gca, MRI *mri_in_labels, MRI *mri_out_labels, TRANSFORM *transform) ;
 GC1D *alloc_gcs(int nlabels, int flags, int ninputs) ;
 
-extern int Ggca_x, Ggca_y, Ggca_z, Ggca_label ;
+extern int Ggca_x, Ggca_y, Ggca_z, Ggca_label, Ggca_nbr_label ;
 
 
 #define GCA_DEFAULT_NOISE_PARAMETER  1
