38b665bb0a36f58e372ab0c3abfc805a20516582
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2005-06-23 19:27:43 +0000

    __MACTYPES__ defined to fix QT/MNI collision with Point typedef

--- include/mri.h
+++ include/mri.h
@@ -6,6 +6,17 @@
 /* remove unwanted warnings between hips_basic.h vs. volume_io/basic.h */
 #undef ABS
 #undef SIGN
+#ifdef Darwin
+  // The result of not defining __MACTYPES__ is a scuba2 build error 
+  // complaining about QT conflicting with MNI over the Point typedef. 
+  // Notes from the file <mni installation>/include/volume_io/geom_structs.h:
+  /* Th 'Point' typedef is annoying to Mac OS users, since Point has been 
+   * a basic type on Macs since the beginning.  Testing __MACTYPES__ should
+   * work at least with the OS X codebase, I don't know if it existed in
+   * earlier versions of the MacTypes.h header.
+   */
+  #define __MACTYPES__
+#endif
 #include "volume_io.h"
 #include "box.h"
 #include "machine.h"
