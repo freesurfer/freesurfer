e42a8aa22daaa8ce2a4b23846cfe5027298bd255
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2005-06-30 01:03:44 +0000

    made fillven=1 the default

--- mri_edit_wm_with_aseg/mri_edit_wm_with_aseg.c
+++ mri_edit_wm_with_aseg/mri_edit_wm_with_aseg.c
@@ -5,8 +5,8 @@
 //
 // Warning: Do not edit the following four lines.  CVS maintains them.
 // Revision Author: $Author: fischl $
-// Revision Date  : $Date: 2005/04/22 20:45:51 $
-// Revision       : $Revision: 1.2 $
+// Revision Date  : $Date: 2005/06/30 01:03:44 $
+// Revision       : $Revision: 1.3 $
 //
 
 #include <stdio.h>
@@ -39,7 +39,7 @@ static int distance_to_label(MRI *mri_labeled, int label, int x,
 
 char *Progname ;
 
-static int fillven = 0 ;
+static int fillven = 1 ;
 static void usage_exit(int code) ;
 
 
@@ -106,7 +106,7 @@ get_option(int argc, char *argv[])
   else if (!strcmp(option, "fillven"))
   {
 		fillven = atoi(argv[2]) ;
-		printf("%sfilling ventricles\n", fillven ? "not " : "") ;
+		printf("%sfilling ventricles\n", fillven == 0 ? "not " : "") ;
     nargs = 1 ;
   }
   else if (!stricmp(option, "debug_voxel"))
@@ -146,7 +146,7 @@ usage_exit(int code)
 static int
 edit_segmentation(MRI *mri_wm, MRI *mri_seg)
 {
-  int   width, height, depth, x, y, z, label, non, noff, xi, yi, zi,  xk, yk, zk, nchanged, wsize, hlabel ;
+  int   width, height, depth, x, y, z, label, non, noff, xi, yi, zi,  xk, yk, zk, nchanged, wsize, hlabel, slabel, olabel, left;
   MRI   *mri_filled ;
 
   mri_filled =  MRIclone(mri_wm,  NULL);
@@ -222,15 +222,82 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
 					break ;
         case Left_Lateral_Ventricle:
         case Right_Lateral_Ventricle:
+          if ((neighborLabel(mri_seg, x, y, z,1,Left_Cerebral_White_Matter) > 0) &&
+              (neighborLabel(mri_seg, x, y, z,1,Right_Cerebral_White_Matter) > 0) &&
+              (MRIvox(mri_wm, x, y, z) < WM_MIN_VAL))
+					{
+						if (x == Gx && y == Gy && z == Gz)  
+						{
+							printf("filling ventricle adjacent to wm at (%d, %d, %d)\n", x,y,z) ;
+							DiagBreak() ;
+						}
+            MRIvox(mri_wm, x, y, z) = 255 ;
+            MRIvox(mri_filled, x, y, z) = 255 ;
+            non++ ;
+						break ;
+					}
 					if (fillven == 0)
 						break ;
+					if (MRIvox(mri_wm, x, y, z) < WM_MIN_VAL)
+					{
+						MRIvox(mri_wm, x, y, z) = 255 ;
+						MRIvox(mri_filled, x, y, z) = 255 ;
+						non++ ;
+					}
         case Left_Inf_Lat_Vent:
         case Right_Inf_Lat_Vent:
+					xi = (label ==  Left_Inf_Lat_Vent) ?  mri_wm->xi[x+1] :  mri_wm->xi[x-1] ; // lateral
+					olabel = MRIvox(mri_seg, xi, y, z) ;
+
+					/* don't allow cortex to be directly lateral to inf-lat-vent - should be some wm there
+					   also don't allow it to be diagonally connected */
+					if (IS_CORTEX(olabel) && (MRIvox(mri_wm, xi, y, z) < WM_MIN_VAL))
+					{
+						if (xi == Gx && y == Gy && z == Gz)
+							printf("changing label (%d, %d, %d) to wm (gm lateral to inf-lat-vent)\n", xi, y, z);
+            MRIvox(mri_wm, xi, y, z) = 255 ;
+            MRIvox(mri_filled, xi, y, z) = 255 ;
+            non++ ;  
+					}
+
+					yi = mri_wm->yi[y+1] ; // inferior
+					olabel = MRIvox(mri_seg, xi, yi, z) ;
+					if (IS_CORTEX(olabel) && (MRIvox(mri_wm, xi, yi, z) < WM_MIN_VAL))
+					{
+						if (xi == Gx && yi == Gy && z == Gz)
+							printf("changing label (%d, %d, %d) to wm (gm lateral to inf-lat-vent)\n", xi, yi, z);
+            MRIvox(mri_wm, xi, yi, z) = 255 ;
+            MRIvox(mri_filled, xi, yi, z) = 255 ;
+            non++ ;  
+					}
+
+					// check diagonally anterior/posterior and inferior
+					zi = mri_wm->zi[z-1] ; // posterior
+					olabel = MRIvox(mri_seg, x, yi, zi) ;
+					if (IS_CORTEX(olabel) && (MRIvox(mri_wm, x, yi, zi) < WM_MIN_VAL))
+					{
+						if (x == Gx && yi == Gy && zi == Gz)
+							printf("changing label (%d, %d, %d) to wm (gm lateral to inf-lat-vent)\n", x, yi, zi);
+            MRIvox(mri_wm, x, yi, zi) = 255 ;
+            MRIvox(mri_filled, x, yi, zi) = 255 ;
+            non++ ;  
+					}
+					zi = mri_wm->zi[z+1] ; // anterior
+					olabel = MRIvox(mri_seg, x, yi, zi) ;
+					if (IS_CORTEX(olabel) && (MRIvox(mri_wm, x, yi, zi) < WM_MIN_VAL))
+					{
+						if (x == Gx && yi == Gy && zi == Gz)
+							printf("changing label (%d, %d, %d) to wm (gm lateral to inf-lat-vent)\n", x, yi, zi);
+            MRIvox(mri_wm, x, yi, zi) = 255 ;
+            MRIvox(mri_filled, x, yi, zi) = 255 ;
+            non++ ;  
+					}
+
 					hlabel = ((label == Left_Lateral_Ventricle) || (label == Left_Inf_Lat_Vent)) ? Left_Hippocampus : Right_Hippocampus ;
 					if (distance_to_label(mri_seg, hlabel, x, y, z, 0, 1, 0, 10) < 10)
 						continue ;  // don't fill ventricular voxels superior to hippo
-          if ((neighborLabel(mri_seg, x, y, z,1,Left_Cerebral_Cortex) >= 0) &&
-              (neighborLabel(mri_seg, x, y, z,1,Right_Cerebral_Cortex) >= 0) &&
+          if ((neighborLabel(mri_seg, x, y, z,1,Left_Cerebral_Cortex) > 0) &&
+              (neighborLabel(mri_seg, x, y, z,1,Right_Cerebral_Cortex) > 0) &&
               (MRIvox(mri_wm, x, y, z) < WM_MIN_VAL))
           {
 						if (x == Gx && y == Gy && z == Gz)  
@@ -254,12 +321,17 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
           }
 					if (label == Left_Inf_Lat_Vent || label ==  Right_Inf_Lat_Vent)  /* fill inferior wm */
 					{
-						int xi,  olabel ;
+						int xi ;
 	    
 						xi = label ==  Left_Inf_Lat_Vent ?  mri_wm->xi[x-1] :  mri_wm->xi[x+1] ;
 						olabel = MRIvox(mri_seg, xi, y, z) ;
 						/* voxel lateral to this one is not hippocampus   */
-						if (olabel != label && MRIvox(mri_wm, xi, y, z) < WM_MIN_VAL)
+						if ((olabel != label) && (MRIvox(mri_wm, xi, y, z) < WM_MIN_VAL) && !IS_AMYGDALA(olabel) &&
+								((distance_to_label(mri_seg, label ==  Left_Inf_Lat_Vent ? Left_Cerebral_White_Matter :
+																	 Right_Cerebral_White_Matter, xi, y, z, 0, 1, 0, 5) < 3) ||
+								(distance_to_label(mri_seg, label ==  Left_Inf_Lat_Vent ? Left_Cerebral_Cortex :
+																	 Right_Cerebral_Cortex, xi, y, z, 0, 1, 0, 5) < 3)) &&
+								!IS_LAT_VENT(olabel))
 								
 						{
 							if (xi == Gx && y == Gy && z == Gz)  
@@ -295,15 +367,19 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
         case Left_Hippocampus:
         case Right_Hippocampus:
 					{
-						int xi,  olabel ;
+						int xi ;
 	    
 						xi = label == Right_Hippocampus ?  mri_wm->xi[x-1] :  mri_wm->xi[x+1] ;
 						yi = mri_wm->yi[y+1] ;
 						olabel = MRIvox(mri_seg, xi, y, z) ;
 						/* voxel lateral to this one is not hippocampus, and not
-						 superior to hippocampus */
+						 superior to hippocampus, and not far from wm or gm */
 						if (olabel != label && (MRIvox(mri_wm, xi, y, z) < MIN_WM_VAL) &&
-								distance_to_label(mri_seg, label, xi, y, z, 0, 1, 0, 10) >= 10)
+								(distance_to_label(mri_seg, label, xi, y, z, 0, 1, 0, 10) >= 10) &&
+								((distance_to_label(mri_seg, label == Left_Hippocampus ? Left_Cerebral_Cortex : Right_Cerebral_Cortex, xi, y, z, 0, 1, 0, 5) < 3) ||
+								 (distance_to_label(mri_seg, label == Left_Hippocampus ? Left_Cerebral_White_Matter : Right_Cerebral_White_Matter, xi, y, z, 0, 1, 0, 5) < 3)))
+								
+																																									
 						{
 							if (xi == Gx && y == Gy && z == Gz)  
 								DiagBreak() ;
@@ -313,7 +389,19 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
 						}
 	    
 						yi = mri_wm->yi[y+1] ;
+						olabel = MRIvox(mri_seg, xi, yi, z) ;  // diagonally lateral and inferior 
+						if (IS_CORTEX(olabel) && (MRIvox(mri_wm, xi, yi, z) < WM_MIN_VAL))
+						{
+							if (xi == Gx && yi == Gy && z == Gz)  
+								DiagBreak() ;
+							MRIvox(mri_wm, xi, yi, z) = 255 ;
+							MRIvox(mri_filled, xi, yi, z) = 255 ;
+							non++ ;
+						}
+
+
 						label = MRIvox(mri_seg, x,yi, z) ;
+
 						if (((label == Left_Cerebral_Cortex || label == Right_Cerebral_Cortex) ||
 								 (label == Left_Cerebral_White_Matter || label == Right_Cerebral_White_Matter))
 								&& (MRIvox(mri_wm, x, yi, z) < WM_MIN_VAL))
@@ -344,6 +432,12 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
         case Right_Putamen:
         case Left_Pallidum:
         case Right_Pallidum:
+				case Right_Thalamus_Proper:
+				case Left_Thalamus_Proper:
+				case Right_Thalamus:
+				case Left_Thalamus:
+				case Left_VentralDC:
+				case Right_VentralDC:
           if (MRIvox(mri_wm, x, y, z) < WM_MIN_VAL)
           {
             MRIvox(mri_wm, x, y, z) = 255 ;
@@ -351,12 +445,76 @@ edit_segmentation(MRI *mri_wm, MRI *mri_seg)
             non++ ; 
           }
           break ;
+				case Left_Cerebral_White_Matter:
+				case Right_Cerebral_White_Matter:
+					yi = mri_wm->yi[y-1] ;
+					slabel = MRIvox(mri_seg, x, yi, z) ;
+					if (IS_INF_LAT_VENT(slabel) && MRIvox(mri_wm, x, y, z) < WM_MIN_VAL)
+					{
+						if (x == Gx && y == Gy && z == Gz)
+							printf("changing voxel (%d, %d, %d) to WM, due to superior inf-lat-vent\n",
+										 x, y, z) ;
+            MRIvox(mri_wm, x, y, z) = 255 ;
+            MRIvox(mri_filled, x, y, z) = 255 ;
+            non++ ; 
+					}						
+					break ;
         default:
           break ;
         }
       }
     }
   }
+
+
+	/* fill in the borders of the ventricle - 2mm thick. This shouldn't affect the folds
+		 but will prevent small wholes from ventricle into wm
+	*/
+	for (z = 0 ; z < depth ; z++)
+	{
+		for (y = 0 ; y < height ; y++)
+		{
+			for (x = 0 ; x < width ; x++)
+			{
+				if (x == Gx && y == Gy && z == Gz)  
+					DiagBreak() ;
+				label = MRIvox(mri_seg, x, y, z) ;
+				left = 0 ;
+				switch (label)
+				{
+				case Unknown:
+					if (((neighborLabel(mri_seg, x, y, z, 1, Left_Lateral_Ventricle) > 0) &&
+							 (neighborLabel(mri_seg, x, y, z, 1, Left_Cerebral_White_Matter) > 0)) ||
+							((neighborLabel(mri_seg, x, y, z, 1, Right_Lateral_Ventricle) > 0) &&
+							 (neighborLabel(mri_seg, x, y, z, 1, Right_Cerebral_White_Matter) > 0)))
+					{
+						if (x == Gx && y == Gy && z == Gz)
+							DiagBreak() ;
+            MRIvox(mri_wm, x, y, z) = 255 ;
+            MRIvox(mri_filled, x, y, z) = 255 ;
+            non++ ; 
+					}
+					break ;
+
+				case Left_Lateral_Ventricle:
+					left = 1 ;
+				case Right_Lateral_Ventricle:
+					olabel = left ? Left_Cerebral_White_Matter : Right_Cerebral_White_Matter ;
+					if (neighborLabel(mri_seg, x, y, z, 2, olabel) > 0)
+					{
+						if (x == Gx && y == Gy && z == Gz)
+							DiagBreak() ;
+            MRIvox(mri_wm, x, y, z) = 255 ;
+            MRIvox(mri_filled, x, y, z) = 255 ;
+            non++ ; 
+					}
+					break ;
+				default:
+					break ;
+				}
+			}
+		}
+	}
 
   /*
     fill in voxels that were labeled wm by the aseg, but not by  wmfilter, and are
@@ -449,8 +607,10 @@ neighborLabel(MRI *mri, int x, int y, int z, int whalf, int label)
       yi = mri->yi[y+yk] ;
       for (xk = -whalf ; xk <= whalf ; xk++)
       {
+#if 0
         if (abs(xk)+abs(yk)+abs(zk) > 1) /* only 6-connected neighbors */
           continue ;
+#endif
         xi = mri->xi[x+xk] ;
         if (MRIvox(mri, xi, yi, zi) == label)
           return(1) ;
