05e0e6b354a5114409751802b584f63ee14f01be
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-07-12 19:02:47 +0000

    does not take abs() anymore

--- fsfast/toolbox/tdr_ifftslice.m
+++ fsfast/toolbox/tdr_ifftslice.m
@@ -2,9 +2,9 @@ function img = tdr_ifftslice(kimg)
 % img = tdr_fftslice(kimg)
 %
 % Performs a 2D inverse FFT of a stack of slices. The stack
-% can have any number of dimensions.
+% can have any number of dimensions. Does not take abs().
 %
-% $Id: tdr_ifftslice.m,v 1.1 2005/07/12 19:01:40 greve Exp $
+% $Id: tdr_ifftslice.m,v 1.2 2005/07/12 19:02:47 greve Exp $
 
 img = [];
 if(nargin ~= 1)
@@ -13,7 +13,7 @@ if(nargin ~= 1)
 end
 
 kimg = fftshift(fftshift(kimg,1),2);
-img = abs(ifft2(kimg));
+img = ifft2(kimg);
 img = fftshift(fftshift(img,1),2);
 
 
