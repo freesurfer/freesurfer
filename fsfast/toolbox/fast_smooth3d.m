d77b828fc36cc6fdc9cf786ba3dc4cb9b0a785fd
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-08-02 00:13:47 +0000

    3d gaussian smoother. Implemented as a series of 1d smoothings

--- fsfast/toolbox/fast_smooth3d.m
+++ fsfast/toolbox/fast_smooth3d.m
@@ -9,18 +9,51 @@ function [volsm, G] = fast_smooth3d(vol,cfwhm,rfwhm,sfwhm)
 %
 % Note: does not attempt to handle wrap-around
 %
-% $Id: fast_smooth3d.m,v 1.2 2004/12/10 21:53:17 greve Exp $
+% $Id: fast_smooth3d.m,v 1.3 2005/08/02 00:13:47 greve Exp $
 
 if(nargin ~= 4)
   fprintf('[volsm, G] = fast_smooth3d(vol,cfwhm,rfwhm,sfwhm)\n');
   return;
 end
 
-cstd = cfwhm/sqrt(log(256.0));
+if(isfield(vol,'vol')) vol = vol.vol; end
+
 rstd = rfwhm/sqrt(log(256.0));
+cstd = cfwhm/sqrt(log(256.0));
 sstd = sfwhm/sqrt(log(256.0));
 
 [nrows ncols nslices nframes] = size(vol);
+
+% Do the rows:
+if(rstd > 0)
+  vol = reshape(vol,[nrows ncols*nslices*nframes]);
+  vol = fast_smooth1d(vol,rstd);
+  vol = reshape(vol,[nrows ncols nslices nframes]);
+end
+
+% Do the cols:
+if(cstd > 0)
+  vol = permute(vol,[2 1 3 4]);
+  vol = reshape(vol,[ncols nrows*nslices*nframes]);
+  vol = fast_smooth1d(vol,cstd);
+  vol = reshape(vol,[ncols nrows nslices nframes]);
+  vol = permute(vol,[2 1 3 4]);
+end
+
+% Do the slices
+if(sstd > 0)
+  vol = permute(vol,[3 2 1 4]);
+  vol = reshape(vol,[nslices ncols*nrows*nframes]);
+  vol = fast_smooth1d(vol,sstd);
+  vol = reshape(vol,[nslices ncols nrows nframes]);
+  vol = permute(vol,[3 2 1 4]);
+end
+
+volsm = vol;
+
+return
+
+
 volsz = [nrows ncols nslices];
 volsz2 = 2*volsz;
 
