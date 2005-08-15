831a754dacb18697e8ddbf3b207079b1d03697d4
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2005-08-15 14:12:03 +0000

    update version/tag stuff

--- mri_ca_label/mri_ca_label.c
+++ mri_ca_label/mri_ca_label.c
@@ -114,8 +114,10 @@ main(int argc, char *argv[])
 	
 	TAGmakeCommandLineString(argc, argv, cmdline) ;
 
+  make_cmd_version_string (argc, argv, "$Id: mri_ca_label.c,v 1.59 2005/08/15 14:12:03 fischl Exp $", "$Name:  $", cmdline);
+
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_ca_label.c,v 1.58 2005/08/12 17:13:02 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_ca_label.c,v 1.59 2005/08/15 14:12:03 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
