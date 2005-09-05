2c6da51136bc340c8b4e9481ad6c6a1ee1b6128d
Xiao Han <xhan@nmr.mgh.harvard.edu>
2005-09-05 20:18:39 +0000

    corrected a small bug when only one flip angle is used

--- mri_ms_fitparms/mri_ms_fitparms.c
+++ mri_ms_fitparms/mri_ms_fitparms.c
@@ -4,9 +4,9 @@
 // original author: Bruce Fischl
 //
 // Warning: Do not edit the following four lines.  CVS maintains them.
-// Revision Author: $Author: fischl $
-// Revision Date  : $Date: 2005/08/15 14:31:04 $
-// Revision       : $Revision: 1.46 $
+// Revision Author: $Author: xhan $
+// Revision Date  : $Date: 2005/09/05 20:18:39 $
+// Revision       : $Revision: 1.47 $
 //
 ////////////////////////////////////////////////////////////////////
 
@@ -145,10 +145,10 @@ main(int argc, char *argv[])
   int b, segno;
 	char cmdline[CMD_LINE_LEN] ;
 
-  make_cmd_version_string (argc, argv, "$Id: mri_ms_fitparms.c,v 1.46 2005/08/15 14:31:04 fischl Exp $", "$Name:  $", cmdline);
+  make_cmd_version_string (argc, argv, "$Id: mri_ms_fitparms.c,v 1.47 2005/09/05 20:18:39 xhan Exp $", "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_ms_fitparms.c,v 1.46 2005/08/15 14:31:04 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_ms_fitparms.c,v 1.47 2005/09/05 20:18:39 xhan Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -366,6 +366,16 @@ main(int argc, char *argv[])
 	MatrixFree(&m) ;
       }
     }
+
+  if(nvolumes == 1){
+    int j;
+
+    if(use_outside_reg == 0){
+      for (j=0;j<nvolumes;j++) 
+	M_reg[j] = MatrixIdentity(4,(MATRIX *)NULL);
+    }
+  }
+
   if (nvolumes > 1)
   {
     int j, iter ; /* should exit when M_reg's converge. */
