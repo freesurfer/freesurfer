ca81ef97667667b051b1a5c50527a101b708cb72
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-09-08 21:58:22 +0000

    Automatically segments wm based on closest surface point

---
+++ mri_aparc2wmseg/mri_aparc2wmseg.c
@@ -0,0 +1,352 @@
+// mri_aparc2aseg.c 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double round(double x);
+#include <sys/utsname.h>
+#include <unistd.h>
+
+#include "macros.h"
+#include "mrisurf.h"
+#include "mrisutils.h"
+#include "error.h"
+#include "diag.h"
+#include "mri.h"
+#include "mri2.h"
+#include "fio.h"
+#include "version.h"
+#include "label.h"
+#include "matrix.h"
+#include "annotation.h"
+
+#undef X
+
+static int  parse_commandline(int argc, char **argv);
+static void check_options(void);
+static void print_usage(void) ;
+static void usage_exit(void);
+static void print_help(void) ;
+static void print_version(void) ;
+static void argnerr(char *option, int n);
+static void dump_options(FILE *fp);
+static int  singledash(char *flag);
+
+int main(int argc, char *argv[]) ;
+
+static char vcid[] = "$Id: mri_aparc2wmseg.c,v 1.1 2005/09/08 21:58:22 greve Exp $";
+char *Progname = NULL;
+char *SUBJECTS_DIR = NULL;
+char *subject = NULL;
+char *WMSegFile = NULL;
+MRI *ASeg, *mritmp;
+MRI *WMSeg;
+MRIS *lhwhite, *rhwhite;
+MHT *lhwhite_hash, *rhwhite_hash;
+MHT *lhpial_hash, *rhpial_hash;
+VERTEX vtx;
+int  lhwvtx, rhwvtx;
+MATRIX *Vox2RAS, *CRS, *RAS;
+float dlhw, drhw;
+char annotfile[1000];
+
+
+int debug = 0;
+char tmpstr[2000];
+
+
+/*--------------------------------------------------*/
+int main(int argc, char **argv)
+{
+  int nargs, err, asegid, c, r, s, annot, hemioffset;
+  int annotid;
+  struct utsname uts;
+  char *cmdline, cwd[2000];
+
+  /* rkt: check for and handle version tag */
+  nargs = handle_version_option (argc, argv, vcid, "$Name:  $");
+  if (nargs && argc - nargs == 1) exit (0);
+  argc -= nargs;
+  cmdline = argv2cmdline(argc,argv);
+  uname(&uts);
+  getcwd(cwd,2000);
+
+  Progname = argv[0] ;
+  argc --;
+  argv++;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+
+  if(argc == 0) usage_exit();
+
+  parse_commandline(argc, argv);
+  check_options();
+  dump_options(stdout);
+
+  SUBJECTS_DIR = getenv("SUBJECTS_DIR");
+  if(SUBJECTS_DIR==NULL){
+    printf("ERROR: SUBJECTS_DIR not defined in environment\n");
+    exit(1);
+  }
+
+  /* ------ Load subject's lh white surface ------ */
+  sprintf(tmpstr,"%s/%s/surf/lh.white",SUBJECTS_DIR,subject);
+  printf("\nReading lh white surface \n %s\n",tmpstr);
+  lhwhite = MRISread(tmpstr);
+  if(lhwhite == NULL){
+    fprintf(stderr,"ERROR: could not read %s\n",tmpstr);
+    exit(1);
+  }
+
+  printf("\n");
+  printf("Building hash of lh white\n");
+  lhwhite_hash = MHTfillVertexTableRes(lhwhite, NULL,CURRENT_VERTICES,16);
+
+  /* ------ Load lh annotation ------ */
+  sprintf(annotfile,"%s/%s/label/lh.aparc.annot",SUBJECTS_DIR,subject);
+  printf("\nLoading lh annotations from %s\n",annotfile);
+  err = MRISreadAnnotation(lhwhite, annotfile);
+  if(err){
+    printf("ERROR: MRISreadAnnotation() failed %s\n",annotfile);
+    exit(1);
+  }
+
+  /* ------ Load subject's rh surface ------ */
+  sprintf(tmpstr,"%s/%s/surf/rh.white",SUBJECTS_DIR,subject);
+  printf("\nReading rh white surface \n %s\n",tmpstr);
+  rhwhite = MRISread(tmpstr);
+  if(rhwhite == NULL){
+    fprintf(stderr,"ERROR: could not read %s\n",tmpstr);
+    exit(1);
+  }
+  printf("\n");
+  printf("Building hash of rh white\n");
+  rhwhite_hash = MHTfillVertexTableRes(rhwhite, NULL,CURRENT_VERTICES,16);
+
+  /* ------ Load rh annotation ------ */
+  sprintf(annotfile,"%s/%s/label/rh.aparc.annot",SUBJECTS_DIR,subject);
+  printf("\nLoading rh annotations from %s\n",annotfile);
+  err = MRISreadAnnotation(rhwhite, annotfile);
+  if(err){
+    printf("ERROR: MRISreadAnnotation() failed %s\n",annotfile);
+    exit(1);
+  }
+
+  if(lhwhite->ct) printf("Have color table for annotation\n");
+  print_annotation_table(stdout);
+
+  /* ------ Load ASeg ------ */
+  sprintf(tmpstr,"%s/%s/mri/aseg.mgz",SUBJECTS_DIR,subject);
+  if(!fio_FileExistsReadable(tmpstr)){
+    sprintf(tmpstr,"%s/%s/mri/aseg.mgh",SUBJECTS_DIR,subject);
+    if(!fio_FileExistsReadable(tmpstr)){
+      sprintf(tmpstr,"%s/%s/mri/aseg/COR-.info",SUBJECTS_DIR,subject);
+      if(!fio_FileExistsReadable(tmpstr)){
+	printf("ERROR: cannot find aseg\n");
+	exit(1);
+      }
+      else
+	sprintf(tmpstr,"%s/%s/mri/aseg/",SUBJECTS_DIR,subject);
+    }
+  }
+
+  printf("\nLoading aseg from %s\n",tmpstr);
+  ASeg = MRIread(tmpstr);
+  if(ASeg == NULL){
+    printf("ERROR: loading aseg %s\n",tmpstr);
+    exit(1);
+  }
+  mritmp = MRIchangeType(ASeg,MRI_INT,0,0,0);
+  MRIfree(&ASeg);
+  ASeg = mritmp;
+  WMSeg = MRIclone(ASeg,NULL);
+
+  Vox2RAS = MRIxfmCRS2XYZtkreg(ASeg);
+  printf("ASeg Vox2RAS: -----------\n");  
+  MatrixPrint(stdout,Vox2RAS);
+  printf("-------------------------\n");  
+  CRS = MatrixAlloc(4,1,MATRIX_REAL);
+  CRS->rptr[4][1] = 1;
+  RAS = MatrixAlloc(4,1,MATRIX_REAL);
+  RAS->rptr[4][1] = 1;
+
+  // Go through each voxel in the aseg
+  for(c=0; c < ASeg->width; c++){
+    printf("%3d ",c);
+    if(c%20 ==19) printf("\n");
+    for(r=0; r < ASeg->height; r++){
+      for(s=0; s < ASeg->depth; s++){
+
+	// If it's not labeled as white matter in the aseg, skip
+	asegid = MRIgetVoxVal(ASeg,c,r,s,0);
+	if(asegid != 2 && asegid != 41) continue;
+
+	// Convert the CRS to RAS
+	CRS->rptr[1][1] = c;
+	CRS->rptr[2][1] = r;
+	CRS->rptr[3][1] = s;
+	RAS = MatrixMultiply(Vox2RAS,CRS,RAS);
+	vtx.x = RAS->rptr[1][1];
+	vtx.y = RAS->rptr[2][1];
+	vtx.z = RAS->rptr[3][1];
+
+	// Get the index of the closest vertex in the 
+	// lh.white, rh.white
+	lhwvtx = MHTfindClosestVertexNo(lhwhite_hash,lhwhite,&vtx,&dlhw);
+	rhwvtx = MHTfindClosestVertexNo(rhwhite_hash,rhwhite,&vtx,&drhw);
+
+	if( (lhwvtx < 0) && (rhwvtx < 0) ){
+	  printf("ERROR: could not map to any surface.\n");
+	  printf("crs = %d %d %d, ras = %6.4f %6.4f %6.4f \n",
+		 c,r,s,vtx.x,vtx.y,vtx.z);
+	  exit(1);
+	}
+
+	if(lhwvtx < 0) dlhw = 1000000000000000.0;
+	if(rhwvtx < 0) drhw = 1000000000000000.0;
+
+	if(dlhw < drhw){
+	  // Left hemi is closer than the right
+	  annot = lhwhite->vertices[lhwvtx].annotation;
+	  hemioffset = 1000;
+	  if(lhwhite->ct)
+	    annotid = CTABannotationToIndex(lhwhite->ct, annot);
+	  else
+	    annotid = annotation_to_index(annot);
+	}
+	else{
+	  // Right hemi is closer than the left
+	  annot = rhwhite->vertices[rhwvtx].annotation;
+	  hemioffset = 2000;
+	  if(rhwhite->ct)
+	    annotid = CTABannotationToIndex(rhwhite->ct, annot);
+	  else
+	    annotid = annotation_to_index(annot);
+	}
+
+	MRIsetVoxVal(WMSeg,c,r,s,0,annotid+hemioffset);
+      }
+    }
+  }
+
+  printf("Writing output aseg to %s\n",WMSegFile);
+  MRIwrite(WMSeg,WMSegFile);
+
+
+  return(0);
+}
+/*-----------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+
+/* --------------------------------------------- */
+static int parse_commandline(int argc, char **argv)
+{
+  int  nargc , nargsused;
+  char **pargv, *option ;
+
+  if(argc < 1) usage_exit();
+
+  nargc   = argc;
+  pargv = argv;
+  while(nargc > 0){
+
+    option = pargv[0];
+    if(debug) printf("%d %s\n",nargc,option);
+    nargc -= 1;
+    pargv += 1;
+
+    nargsused = 0;
+
+    if (!strcasecmp(option, "--help"))  print_help() ;
+    else if (!strcasecmp(option, "--version")) print_version() ;
+    else if (!strcasecmp(option, "--debug"))   debug = 1;
+    else if (!strcmp(option, "--s")){
+      if(nargc < 1) argnerr(option,1);
+      subject = pargv[0];
+      nargsused = 1;
+    }
+    else if (!strcmp(option, "--wmseg")){
+      if(nargc < 1) argnerr(option,1);
+      WMSegFile = pargv[0];
+      nargsused = 1;
+    }
+    else{
+      fprintf(stderr,"ERROR: Option %s unknown\n",option);
+      if(singledash(option))
+	fprintf(stderr,"       Did you really mean -%s ?\n",option);
+      exit(-1);
+    }
+    nargc -= nargsused;
+    pargv += nargsused;
+  }
+  return(0);
+}
+/* ------------------------------------------------------ */
+static void usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void print_usage(void)
+{
+  printf("USAGE: %s \n",Progname) ;
+  printf("\n");
+  printf("   --s subject \n");
+  printf("   --wmseg wmsegfile\n");
+  printf("\n");
+  printf("   --help      print out information on how to use this program\n");
+  printf("   --version   print out version and exit\n");
+  printf("\n");
+  printf("%s\n", vcid) ;
+  printf("\n");
+}
+/* --------------------------------------------- */
+static void print_help(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void print_version(void)
+{
+  printf("%s\n", vcid) ;
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void argnerr(char *option, int n)
+{
+  if(n==1)
+    fprintf(stderr,"ERROR: %s flag needs %d argument\n",option,n);
+  else
+    fprintf(stderr,"ERROR: %s flag needs %d arguments\n",option,n);
+  exit(-1);
+}
+/* --------------------------------------------- */
+static void check_options(void)
+{
+  if(subject == NULL){
+    printf("ERROR: must specify a subject\n");
+    exit(1);
+  }
+  return;
+}
+
+/* --------------------------------------------- */
+static void dump_options(FILE *fp)
+{
+  return;
+}
+/*---------------------------------------------------------------*/
+static int singledash(char *flag)
+{
+  int len;
+  len = strlen(flag);
+  if(len < 2) return(0);
+
+  if(flag[0] == '-' && flag[1] != '-') return(1);
+  return(0);
+}
+
+
