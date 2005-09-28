0d39a814721281d8a2203b6b1bd5785f180d0a74
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-09-28 18:28:47 +0000

    Utility to check for differences bet volumes

---
+++ mri_voldiff/mri_voldiff.c
@@ -0,0 +1,195 @@
+// mri_voldiff.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double round(double x);
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
+#include "macros.h"
+#include "utils.h"
+#include "mrisurf.h"
+#include "mrisutils.h"
+#include "error.h"
+#include "diag.h"
+#include "mri.h"
+#include "mri2.h"
+#include "fio.h"
+#include "version.h"
+#include "label.h"
+#include "annotation.h"
+#include "fmriutils.h"
+#include "cmdargs.h"
+#include "fsglm.h"
+
+static int  parse_commandline(int argc, char **argv);
+static void check_options(void);
+static void print_usage(void) ;
+static void usage_exit(void);
+static void print_help(void) ;
+static void print_version(void) ;
+static void dump_options(FILE *fp);
+
+int main(int argc, char *argv[]) ;
+
+static char vcid[] = "$Id: mri_voldiff.c,v 1.1 2005/09/28 18:28:47 greve Exp $";
+char *Progname = NULL;
+
+char *vol1File = NULL, *vol2File=NULL;
+MRI *vol1=NULL, *vol2=NULL;
+
+int debug = 0, checkoptsonly = 0;
+char tmpstr[2000];
+
+/*--------------------------------------------------*/
+int main(int argc, char **argv)
+{
+  int nargs,n;
+  struct utsname uts;
+  char *cmdline, cwd[2000];
+
+  /* rkt: check for and handle version tag */
+  nargs = handle_version_option (argc, argv, vcid, "$Name:  $");
+  if (nargs && argc - nargs == 1) exit (0);
+  argc -= nargs;
+  cmdline = argv2cmdline(argc,argv);
+  uname(&uts);
+  getcwd(cwd,2000);
+
+  Progname = argv[0] ;
+  argc --;
+  argv++;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+  if(argc == 0) usage_exit();
+  parse_commandline(argc, argv);
+  check_options();
+  if(checkoptsonly) return(0);
+
+  printf("\n");
+  printf("%s\n",vcid);
+  printf("cwd %s\n",cwd);
+  printf("cmdline %s\n",cmdline);
+  printf("sysname  %s\n",uts.sysname);
+  printf("hostname %s\n",uts.nodename);
+  printf("machine  %s\n",uts.machine);
+
+  dump_options(stdout);
+
+  printf("mri_voldiff done\n");
+  return(0); 
+  exit(0);
+
+}
+/*-----------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+
+/* --------------------------------------------- */
+static int parse_commandline(int argc, char **argv)
+{
+  int  nargc , nargsused, msec, niters;
+  char **pargv, *option ;
+
+  if(argc < 1) usage_exit();
+
+  nargc   = argc;
+  pargv = argv;
+  while(nargc > 0){
+
+    option = pargv[0];
+    if(debug) printf("%d %s\n",nargc,option);
+    nargc -= 1;
+    pargv += 1;
+
+    nargsused = 0;
+
+    if (!strcasecmp(option, "--help"))  print_help() ;
+    else if (!strcasecmp(option, "--version")) print_version() ;
+    else if (!strcasecmp(option, "--debug"))   debug = 1;
+    else if (!strcasecmp(option, "--checkopts"))   checkoptsonly = 1;
+    else if (!strcasecmp(option, "--nocheckopts")) checkoptsonly = 0;
+
+    else if (!strcmp(option, "--v1")){
+      if(nargc < 1) CMDargNErr(option,1);
+      vol1File = fio_fullpath(pargv[0]);
+      nargsused = 1;
+    }
+    else if (!strcmp(option, "--v2")){
+      if(nargc < 1) CMDargNErr(option,1);
+      vol2File = fio_fullpath(pargv[0]);
+      nargsused = 1;
+    }
+    else{
+      fprintf(stderr,"ERROR: Option %s unknown\n",option);
+      if(CMDsingleDash(option))
+	fprintf(stderr,"       Did you really mean -%s ?\n",option);
+      exit(-1);
+    }
+    nargc -= nargsused;
+    pargv += nargsused;
+  }
+  return(0);
+}
+/* ------------------------------------------------------ */
+static void usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void print_usage(void)
+{
+  printf("USAGE: %s \n",Progname) ;
+  printf("\n");
+  printf("   --v1 first input volume \n");
+  printf("   --v2 second input volume \n");
+  printf("\n");
+  printf("   --debug     turn on debugging\n");
+  printf("   --checkopts don't run anything, just check options and exit\n");
+  printf("   --help      print out information on how to use this program\n");
+  printf("   --version   print out version and exit\n");
+  printf("\n");
+  printf("%s\n", vcid) ;
+  printf("\n");
+}
+/* --------------------------------------------- */
+static void print_help(void)
+{
+  print_usage() ;
+  printf("WARNING: this program is not yet tested!\n");
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void print_version(void)
+{
+  printf("%s\n", vcid) ;
+  exit(1) ;
+}
+/* --------------------------------------------- */
+static void check_options(void)
+{
+  if(vol1File == NULL){
+    printf("ERROR: must specify a vol1 file\n");
+    exit(1);
+  }
+  if(vol2File == NULL){
+    printf("ERROR: must specify a vol2 file\n");
+    exit(1);
+  }
+  return;
+}
+
+/* --------------------------------------------- */
+static void dump_options(FILE *fp)
+{
+  int n;
+  fprintf(fp,"vol1    %s\n",vol1File);
+  fprintf(fp,"vol2    %s\n",vol2File);
+  return;
+}
+
+
