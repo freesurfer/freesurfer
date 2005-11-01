1ba8ee84766d233282000ca59297af6653110e50
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-11-01 23:00:31 +0000

    Regrids kspace

---
+++ fsfast/toolbox/tdr_regrid.m
@@ -0,0 +1,58 @@
+function kdatgrid = tdr_regrid(kx,ky,kdat,kgridsize)
+% kdatgrid = tdr_regrid(kx,ky,kdat,kgridsize)
+% 
+% Regrids using nearest neighbor
+%
+% $Id: tdr_regrid.m,v 1.1 2005/11/01 23:00:31 greve Exp $
+
+
+kdatgrid = [];
+
+if(nargin ~= 4)
+  fprintf('kdatgrid = tdr_regrid(kx,ky,kdat,kgridsize)\n');
+  return;
+end
+
+xkgridsize = kgridsize(2);
+ykgridsize = kgridsize(1);
+
+kr2max = max((kx.^2 + ky.^2));
+kxmax = max(kx);
+kxmin = min(kx);
+kymax = max(ky);
+kymin = min(ky);
+
+
+gx0 = round(xkgridsize/2) + 1;
+gy0 = round(ykgridsize/2) + 1;
+
+tmp = [1:xkgridsize];
+tmp = tmp - tmp(gx0);
+tmp = tmp/max(abs(tmp));
+kxg = kxmax * tmp;
+
+tmp = [1:ykgridsize];
+tmp = tmp - tmp(gy0);
+tmp = tmp/max(abs(tmp));
+kyg = kymax * tmp;
+
+kdatgrid = zeros(kgridsize);
+
+for gx = 1:xkgridsize
+  for gy = 1:ykgridsize
+    kr2 = kxg(gx).^2 + kyg(gy).^2;
+    if(kr2 > kr2max) continue; end
+    dx = kx - kxg(gx);
+    dy = ky - kyg(gy);
+    d2 = dx.^2 + dy.^2;
+    [blah nnbr] = min(d2);
+    kdatgrid(gy,gx) = kdat(nnbr);
+  end
+end
+
+return;
+
+
+
+
+
