74889004339db2816ee52e5d10523c1c14fa89c6
Florent Segonne <segonne@nmr.mgh.harvard.edu>
2005-11-21 19:42:53 +0000

    *** empty log message ***

--- mri_mc/mri_pretess.c
+++ mri_mc/mri_pretess.c
@@ -468,7 +468,129 @@ static int mriRemoveCornerConfiguration(MRI *mri_seg, MRI *mri_orig, int label){
 		fprintf(stderr,"\npass %3d (+-+): %3d found - %3d modified     |    TOTAL: %3d",npass,nfound,nmodified,ntotal);
 	}
 
+	return ntotal;
+}
 
+static int mriRemoveBackgroundCornerConfiguration(MRI *mri_seg, MRI *mri_orig, int label){
+	static int niter=0;
+	int i,j,k;
+	int ntotal=0,nmodified,nfound,npass;
+	
+	niter++;
+	fprintf(stderr,"\nIteration Number : %d",niter);
+
+	/* dealing with i+1,j+1,k+1 */
+	nfound=1;
+	nmodified=0;
+	npass=0;
+	while(nfound){
+		nfound=0;
+		npass++;
+		for(k = 0 ; k < mri_seg->depth-1; k++)
+			for(j = 0 ; j < mri_seg->height-1 ; j++)
+				for(i = 0 ; i < mri_seg->width-1 ; i++){
+					if(MRIvox(mri_seg,i,j,k)==label) continue;
+					if(MRIvox(mri_seg,i+1,j+1,k+1)==label) continue;
+						 /* find problematic configuration */
+						 if((MRIvox(mri_seg,i+1,j,k)==label) && (MRIvox(mri_seg,i+1,j+1,k)==label) && 
+								(MRIvox(mri_seg,i,j+1,k)==label) && (MRIvox(mri_seg,i,j+1,k+1)==label) &&
+								(MRIvox(mri_seg,i+1,j,k)==label) && 
+								(MRIvox(mri_seg,i+1,j,k+1)==label)){
+							 if(MRIvox(mri_orig,i,j,k)>MRIvox(mri_orig,i+1,j+1,k+1))
+								 MRIvox(mri_seg,i,j,k)=label;
+							 else
+								 MRIvox(mri_seg,i+1,j+1,k+1)=label;
+							 nfound++;
+						 }
+				}
+		nmodified += nfound;
+		ntotal += nfound;
+		fprintf(stderr,"\npass %3d (++): %3d found - %3d modified     |    TOTAL: %3d",npass,nfound,nmodified,ntotal);
+	}
+		/* dealing with i+1,j+1,k-1 */
+	nfound=1;
+	nmodified=0;
+	npass=0;
+	while(nfound){
+		nfound=0;
+		npass++;
+		for(k = 1 ; k < mri_seg->depth; k++)
+			for(j = 0 ; j < mri_seg->height-1 ; j++)
+				for(i = 0 ; i < mri_seg->width-1 ; i++){
+					if(MRIvox(mri_seg,i,j,k)==label) continue;
+					if(MRIvox(mri_seg,i+1,j+1,k-1)==label) continue;
+						 /* find problematic configuration */
+						 if((MRIvox(mri_seg,i+1,j,k)==label) && (MRIvox(mri_seg,i+1,j+1,k)==label) && 
+								(MRIvox(mri_seg,i,j+1,k)==label) && (MRIvox(mri_seg,i,j+1,k-1)==label) &&
+								(MRIvox(mri_seg,i+1,j,k)==label) && 
+								(MRIvox(mri_seg,i+1,j,k-1)==label)){
+							 if(MRIvox(mri_orig,i,j,k)>MRIvox(mri_orig,i+1,j+1,k-1))
+								 MRIvox(mri_seg,i,j,k)=label;
+							 else
+								 MRIvox(mri_seg,i+1,j+1,k-1)=label;
+							 nfound++;
+						 }
+				}
+		nmodified += nfound;
+		ntotal += nfound;
+		fprintf(stderr,"\npass %3d (+-): %3d found - %3d modified     |    TOTAL: %3d",npass,nfound,nmodified,ntotal);
+	}
+	/* dealing with i+1,j-1,k-1 */
+	nfound=1;
+	nmodified=0;
+	npass=0;
+	while(nfound){
+		nfound=0;
+		npass++;
+		for(k = 1 ; k < mri_seg->depth; k++)
+			for(j = 1 ; j < mri_seg->height ; j++)
+				for(i = 0 ; i < mri_seg->width-1 ; i++){
+					if(MRIvox(mri_seg,i,j,k)==label) continue;
+					if(MRIvox(mri_seg,i+1,j-1,k-1)==label) continue;
+						 /* find problematic configuration */
+						 if((MRIvox(mri_seg,i+1,j,k)==label) && (MRIvox(mri_seg,i+1,j-1,k)==label) && 
+								(MRIvox(mri_seg,i,j-1,k)==label) && (MRIvox(mri_seg,i,j-1,k-1)==label) &&
+								(MRIvox(mri_seg,i+1,j,k)==label) && 
+								(MRIvox(mri_seg,i+1,j,k-1)==label)){
+							 if(MRIvox(mri_orig,i,j,k)>MRIvox(mri_orig,i+1,j-1,k-1))
+								 MRIvox(mri_seg,i,j,k)=label;
+							 else
+								 MRIvox(mri_seg,i+1,j-1,k-1)=label;
+							 nfound++;
+						 }
+				}
+		nmodified += nfound;
+		ntotal += nfound;
+		fprintf(stderr,"\npass %3d (--): %3d found - %3d modified     |    TOTAL: %3d",npass,nfound,nmodified,ntotal);
+	}
+		/* dealing with i+1,j-1,k+1 */
+	nfound=1;
+	nmodified=0;
+	npass=0;
+	while(nfound){
+		nfound=0;
+		npass++;
+		for(k = 0 ; k < mri_seg->depth-1; k++)
+			for(j = 1 ; j < mri_seg->height ; j++)
+				for(i = 0 ; i < mri_seg->width-1 ; i++){
+					if(MRIvox(mri_seg,i,j,k)==label) continue;
+					if(MRIvox(mri_seg,i+1,j-1,k+1)==label) continue;
+						 /* find problematic configuration */
+						 if((MRIvox(mri_seg,i+1,j,k)==label) && (MRIvox(mri_seg,i+1,j-1,k)==label) && 
+								(MRIvox(mri_seg,i,j-1,k)==label) && (MRIvox(mri_seg,i,j-1,k+1)==label) &&
+								(MRIvox(mri_seg,i+1,j,k)==label) && 
+								(MRIvox(mri_seg,i+1,j,k+1)==label)){
+							 if(MRIvox(mri_orig,i,j,k)>MRIvox(mri_orig,i+1,j-1,k+1))
+								 MRIvox(mri_seg,i,j,k)=label;
+							 else
+								 MRIvox(mri_seg,i+1,j-1,k+1)=label;
+							 nfound++;
+						 }
+				}
+		nmodified += nfound;
+		ntotal += nfound;
+		fprintf(stderr,"\npass %3d (-+): %3d found - %3d modified     |    TOTAL: %3d",npass,nfound,nmodified,ntotal);
+	}
 
 	return ntotal;
 }
@@ -476,7 +598,7 @@ static int mriRemoveCornerConfiguration(MRI *mri_seg, MRI *mri_orig, int label){
 int main(int argc, char *argv[])
 {
 	MRI *mri_seg,*mri_orig;
-	int niter=10,ntotal=0,nmodified;
+	int niter=10,ntotal=0,nmodified,i,j,k,nvoxels;
 	int label;
 
 	Progname=argv[0];
@@ -496,11 +618,18 @@ int main(int argc, char *argv[])
 		nmodified=0;
 		nmodified += mriRemoveEdgeConfiguration(mri_seg,mri_orig,label);
 		if(0) nmodified += mriRemoveCornerConfiguration(mri_seg,mri_orig,label);
+		nmodified += mriRemoveBackgroundCornerConfiguration(mri_seg,mri_orig,label);
 		if(nmodified==0) break;
 		ntotal += nmodified;
 	}
 
-	fprintf(stderr,"\n\nTotal Number of Modified Voxels = %d\n",ntotal);
+	nvoxels=0;
+	for(k=0;k<mri_seg->depth;k++)
+		for(j=0;j<mri_seg->height;j++)
+			for(i=0;i<mri_seg->width;i++)
+				if(MRIvox(mri_seg,i,j,k)==label) nvoxels++;
+
+	fprintf(stderr,"\n\nTotal Number of Modified Voxels = %d (out of %d: %f)\n",ntotal,nvoxels,100.0*ntotal/nvoxels);
 
 	fprintf(stderr,"\nWritting out volume...");
 	MRIwrite(mri_seg,argv[4]);
