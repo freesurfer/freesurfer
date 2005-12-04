4a6812569a35631d86e68e8534ef9a2d9cae6813
Doug Greve <greve@nmr.mgh.harvard.edu>
2005-12-04 22:38:25 +0000

    Program to do monte carlo simulations on random fields

---
+++ mri_mcsim/mri_mcsim.c
@@ -0,0 +1,17 @@
+/**
+ * @file   dummy.c
+ * @author Yasunari Tosa
+ * @date   Wed Oct 13 11:47:18 2004
+ * 
+ * @brief  sample dummy program
+ * 
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Just a dummy.\n");
+  return 0;
+}
