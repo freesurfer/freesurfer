3dc8224c938e44a74ec5858ae068ef44e6cb91d0
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-01-10 17:07:53 +0000

    Beefed up usage docs

--- mri_normalize/mri_normalize.c
+++ mri_normalize/mri_normalize.c
@@ -63,10 +63,10 @@ main(int argc, char *argv[])
 
 	char cmdline[CMD_LINE_LEN] ;
 	
-  make_cmd_version_string (argc, argv, "$Id: mri_normalize.c,v 1.41 2005/12/03 21:56:41 fischl Exp $", "$Name:  $", cmdline);
+  make_cmd_version_string (argc, argv, "$Id: mri_normalize.c,v 1.42 2006/01/10 17:07:53 greve Exp $", "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_normalize.c,v 1.41 2005/12/03 21:56:41 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_normalize.c,v 1.42 2006/01/10 17:07:53 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -304,13 +304,13 @@ get_option(int argc, char *argv[])
   }
   else if (!stricmp(option, "monkey"))
   {
-		no1d = 1 ;
+    no1d = 1 ;
     num_3d_iter = 1 ;
     printf("disabling 1D normalization and setting niter=1, make sure to use -f to specify control points\n") ;
   }
   else if (!stricmp(option, "nosnr"))
   {
-		nosnr = 1 ;
+    nosnr = 1 ;
     printf("disabling SNR normalization\n") ;
   }
   else if (!stricmp(option, "snr"))
@@ -320,8 +320,8 @@ get_option(int argc, char *argv[])
   }
   else if (!stricmp(option, "sigma"))
   {
-		bias_sigma = atof(argv[2]) ;
-		nargs = 1 ;
+    bias_sigma = atof(argv[2]) ;
+    nargs = 1 ;
     printf("using Gaussian smoothing of bias field, sigma=%2.3f\n", bias_sigma) ;
   }
   else if (!stricmp(option, "conform"))
@@ -331,9 +331,9 @@ get_option(int argc, char *argv[])
   }
   else if (!stricmp(option, "aseg") || !stricmp(option, "segmentation"))
   {
-		aseg_fname = argv[2] ;
+    aseg_fname = argv[2] ;
     nargs = 1  ;
-		fprintf(stderr, "using segmentation for initial intensity normalization\n") ;
+    fprintf(stderr, "using segmentation for initial intensity normalization\n") ;
   }
   else if (!stricmp(option, "gentle"))
   {
@@ -344,90 +344,90 @@ get_option(int argc, char *argv[])
   {
     file_only = 1 ;
     control_point_fname = argv[2] ;
-		no1d = 1 ;
+    no1d = 1 ;
     nargs = 1 ;
     fprintf(stderr, "using control points from file %s...\n", 
            control_point_fname) ;
     fprintf(stderr, "only using file control points...\n") ;
   }
   else switch (toupper(*option))
-  {
-	case 'D':
-		Gx = atoi(argv[2]) ;
-		Gy = atoi(argv[3]) ;
-		Gz = atoi(argv[4]) ;
-		nargs = 3 ;
-		printf("debugging voxel (%d, %d, %d)\n", Gx, Gy, Gz) ;
-		break ;
-	case 'V':
-		Gvx = atoi(argv[2]) ;
-		Gvy = atoi(argv[3]) ;
-		Gvz = atoi(argv[4]) ;
-		nargs = 3 ;
-		printf("debugging alternative voxel (%d, %d, %d)\n", Gvx, Gvy, Gvz) ;
-		break ;
-	case 'P':
-		prune = atoi(argv[2]) ;
-		nargs = 1 ;
-		printf("turning control point pruning %s\n", prune > 0 ? "on" : "off") ;
-		if (prune == 0)
-			prune = -1 ;
-		break ;
-	case 'R':
-		read_flag = 1 ;
-    nargs = 2 ;
-    control_volume_fname = argv[2] ;
-    bias_volume_fname = argv[3] ;
-    printf("reading bias field from %s and ctrl points from %s\n", bias_volume_fname, control_volume_fname) ;
-    break ;
-  case 'W':
-    control_volume_fname = argv[2] ;
-    bias_volume_fname = argv[3] ;
-    nargs = 2 ;
-    printf("writing ctrl pts to   %s\n", control_volume_fname) ;
-    printf("writing bias field to %s\n", bias_volume_fname) ;
-    break ;
-  case 'F':
-    control_point_fname = argv[2] ;
-    nargs = 1 ;
-    fprintf(stderr, "using control points from file %s...\n", 
-           control_point_fname) ;
-    break ;
-  case 'A':
-    intensity_above = atof(argv[2]) ;
-    fprintf(stderr, "using control point with intensity %2.1f above target.\n",
-            intensity_above) ;
-    nargs = 1 ;
-    break ;
-  case 'B':
-    intensity_below = atof(argv[2]) ;
-    fprintf(stderr, "using control point with intensity %2.1f below target.\n",
-            intensity_below) ;
-    nargs = 1 ;
-    break ;
-  case 'G':
-    mni.max_gradient = atof(argv[2]) ;
-    fprintf(stderr, "using max gradient = %2.3f\n", mni.max_gradient) ;
-    nargs = 1 ;
-    break ;
+    {
+    case 'D':
+      Gx = atoi(argv[2]) ;
+      Gy = atoi(argv[3]) ;
+      Gz = atoi(argv[4]) ;
+      nargs = 3 ;
+      printf("debugging voxel (%d, %d, %d)\n", Gx, Gy, Gz) ;
+      break ;
+    case 'V':
+      Gvx = atoi(argv[2]) ;
+      Gvy = atoi(argv[3]) ;
+      Gvz = atoi(argv[4]) ;
+      nargs = 3 ;
+      printf("debugging alternative voxel (%d, %d, %d)\n", Gvx, Gvy, Gvz) ;
+      break ;
+    case 'P':
+      prune = atoi(argv[2]) ;
+      nargs = 1 ;
+      printf("turning control point pruning %s\n", prune > 0 ? "on" : "off") ;
+      if (prune == 0)
+	prune = -1 ;
+      break ;
+    case 'R':
+      read_flag = 1 ;
+      nargs = 2 ;
+      control_volume_fname = argv[2] ;
+      bias_volume_fname = argv[3] ;
+      printf("reading bias field from %s and ctrl points from %s\n", bias_volume_fname, control_volume_fname) ;
+      break ;
+    case 'W':
+      control_volume_fname = argv[2] ;
+      bias_volume_fname = argv[3] ;
+      nargs = 2 ;
+      printf("writing ctrl pts to   %s\n", control_volume_fname) ;
+      printf("writing bias field to %s\n", bias_volume_fname) ;
+      break ;
+    case 'F':
+      control_point_fname = argv[2] ;
+      nargs = 1 ;
+      fprintf(stderr, "using control points from file %s...\n", 
+	      control_point_fname) ;
+      break ;
+    case 'A':
+      intensity_above = atof(argv[2]) ;
+      fprintf(stderr, "using control point with intensity %2.1f above target.\n",
+	      intensity_above) ;
+      nargs = 1 ;
+      break ;
+    case 'B':
+      intensity_below = atof(argv[2]) ;
+      fprintf(stderr, "using control point with intensity %2.1f below target.\n",
+	      intensity_below) ;
+      nargs = 1 ;
+      break ;
+    case 'G':
+      mni.max_gradient = atof(argv[2]) ;
+      fprintf(stderr, "using max gradient = %2.3f\n", mni.max_gradient) ;
+      nargs = 1 ;
+      break ;
 #if 0
-  case 'V':
-    verbose = !verbose ;
-    break ;
+    case 'V':
+      verbose = !verbose ;
+      break ;
 #endif
-  case 'N':
-    num_3d_iter = atoi(argv[2]) ;
-    nargs = 1 ;
-    fprintf(stderr, "performing 3d normalization %d times\n", num_3d_iter) ;
-    break ;
-  case '?':
-  case 'U':
-    usage_exit(0) ;
-    break ;
-  default:
-    fprintf(stderr, "unknown option %s\n", argv[1]) ;
-    exit(1) ;
-    break ;
+    case 'N':
+      num_3d_iter = atoi(argv[2]) ;
+      nargs = 1 ;
+      fprintf(stderr, "performing 3d normalization %d times\n", num_3d_iter) ;
+      break ;
+    case '?':
+    case 'U':
+      usage_exit(0) ;
+      break ;
+    default:
+      fprintf(stderr, "unknown option %s\n", argv[1]) ;
+      exit(1) ;
+      break ;
   }
 
   return(nargs) ;
@@ -435,20 +435,30 @@ get_option(int argc, char *argv[])
 static void
 usage_exit(int code)
 {
-	printf("usage: %s [input directory] [output directory]\n\n", Progname) ;
-	printf("\t-no1d              disable 1d normalization\n");
-	printf("\t-conform           interpolate and embed volume to be 256^3\n");
-	printf("\t-gentle            perform kinder gentler normalization\n");
-	printf("\t-f <path to file>  use control points file (usually control.dat)\n");
-	printf("\t-w <mri_vol c> <mri_vol b> write ctrl point(c) and bias field(b) volumes\n");
-	printf("\t-a <float a>       use control point with intensity a above target (default=%2.1f)\n", intensity_above);
-	printf("\t-b <float b>       use control point with intensity b below target (default=%2.1f)\n", intensity_below);
-	printf("\t-g <float g>       use max intensity/mm gradient g (default=%2.3f)\n", mni.max_gradient);
-	printf("\t-v                 verbose\n");
-	printf("\t-n <int n>         use n 3d normalization iterations (default=%d)\n", num_3d_iter);
-	printf("\t-u                 print usage\n");
-	printf("-prune <boolean>     turn pruning of control points on/off (default=off). Useful if white is expanding into gm\n") ;
-	exit(code);
+  printf("%s input output\n\n", Progname) ;
+  printf("  -no1d              disable 1d normalization\n");
+  printf("  -conform           interpolate and embed volume to be 256^3\n");
+  printf("  -gentle            perform kinder gentler normalization\n");
+  printf("  -f <path to file>  use control points file (usually control.dat)\n");
+  printf("  -w <mri_vol c> <mri_vol b> write ctrl point(c) and bias field(b) volumes\n");
+  printf("  -a <float a>       use control point with intensity a above target (default=%2.1f)\n", intensity_above);
+  printf("  -b <float b>       use control point with intensity b below target (default=%2.1f)\n", intensity_below);
+  printf("  -g <float g>       use max intensity/mm gradient g (default=%2.3f)\n", mni.max_gradient);
+  printf("  -v                 verbose\n");
+  printf("  -v Gvx Gvy Gvz : for debuggin\n");
+  printf("  -n <int n>         use n 3d normalization iterations (default=%d)\n", num_3d_iter);
+  printf("  -u                 print usage\n");
+  printf("  -prune <boolean>     turn pruning of control points on/off (default=off). Useful if white is expanding into gm\n") ;
+  printf("  -MASK maskfile \n");
+  printf("  -monkey : turns of 1d, sets num_3d_iter=1\n");
+  printf("  -nosnr : disable snr normalization\n");
+  printf("  -sigma sigma : smooth bias field\n");
+  printf("  -aseg aseg\n");
+  printf("  -d Gx Gy Gz : for debuggin\n");
+  printf("  -r controlpoints biasfield : for reading\n");
+  printf("  \n");
+
+  exit(code);
 }
 
 static MRI *
