42379604f6096d89bbc7dd6e288fe2fda7ca8ed9
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2006-01-17 22:29:19 +0000

    no code changes: emacs reformatted, untabified, and trailing whitespace deleted, for readability

--- include/transform.h
+++ include/transform.h
@@ -1,10 +1,10 @@
 /***********************************************************************/
 /* transform.h                                                         */
 /*                                                                     */
-/* Warning: Do not edit the following four lines.  CVS maintains them. */
-/* Revision Author: $Author: fischl $                                           */
-/* Revision Date  : $Date: 2005/12/30 19:16:37 $                                             */
-/* Revision       : $Revision: 1.47 $                                         */
+/* Warning: Do not edit the following three lines. CVS maintains them. */
+/* Revision Author: $Author: nicks $                                  */
+/* Revision Date  : $Date: 2006/01/17 22:29:19 $                       */
+/* Revision       : $Revision: 1.48 $                                  */
 /*                                                                     */
 /***********************************************************************/
 
@@ -17,17 +17,18 @@ typedef enum { MINC, TKREG, GENERIC, UNKNOWN=-1 } TransformType;
 
 typedef struct
 {
-  int           valid;   // whether this is a valid info or not (1 valid, 0 not valid)
+  int           valid;   /* whether this is a
+                            valid info or not (1 valid, 0 not valid) */
   int           width ;
   int           height ;
-  int           depth ;     
-  float         xsize ;     
-  float         ysize ;     
-  float         zsize ;     
-  float         x_r, x_a, x_s; 
-  float         y_r, y_a, y_s; 
-  float         z_r, z_a, z_s; 
-  float         c_r, c_a, c_s; 
+  int           depth ;
+  float         xsize ;
+  float         ysize ;
+  float         zsize ;
+  float         x_r, x_a, x_s;
+  float         y_r, y_a, y_s;
+  float         z_r, z_a, z_s;
+  float         c_r, c_a, c_s;
   char          fname[512];  // volume filename
 } VOL_GEOM, VG;
 
@@ -43,7 +44,7 @@ typedef struct
   TransformType type;        /* record transform type       */
   VOL_GEOM   src;            /* src for the transform       */
   VOL_GEOM   dst;            /* dst for the transform       */
-	int        label ;         // if this xform only applies to a specific label
+  int        label ;         // if this xform only applies to a specific label
 } LINEAR_TRANSFORM, LT ;
 
 typedef struct
@@ -64,7 +65,8 @@ typedef struct
   void       *xform ;
 } TRANSFORM ;
 
-void mincGetVolInfo(char *infoline, char *infoline2, VOL_GEOM *vgSrc, VOL_GEOM *vgDst);
+void mincGetVolInfo(char *infoline, char *infoline2,
+                    VOL_GEOM *vgSrc, VOL_GEOM *vgDst);
 
 int      LTAfree(LTA **plta) ;
 LTA      *LTAreadInVoxelCoords(char *fname, MRI *mri) ;
@@ -74,37 +76,43 @@ int      LTAwrite(LTA *lta, char *fname) ;
 LTA      *LTAalloc(int nxforms, MRI *mri) ;
 int      LTAdivide(LTA *lta, MRI *mri) ;
 MRI      *LTAtransform(MRI *mri_src, MRI *mri_dst, LTA *lta) ;
-MRI      *LTAtransformInterp(MRI *mri_src, MRI *mri_dst, LTA *lta, int interp) ;
+MRI      *LTAtransformInterp
+(MRI *mri_src, MRI *mri_dst, LTA *lta, int interp) ;
 MATRIX   *LTAtransformAtPoint(LTA *lta, float x, float y,float z,MATRIX *m_L);
 int      LTAworldToWorld(LTA *lta, float x, float y, float z,
                          float *px, float *py, float *pz);
 int      LTAworldToWorldEx(LTA *lta, float x, float y, float z,
-			   float *px, float *py, float *pz);
+                           float *px, float *py, float *pz);
 int      LTAinverseWorldToWorld(LTA *lta, float x, float y, float z,
-				float *px, float *py, float *pz);
+                                float *px, float *py, float *pz);
 int      LTAinverseWorldToWorldEx(LTA *lta, float x, float y, float z,
-				  float *px, float *py, float *pz);
+                                  float *px, float *py, float *pz);
 VECTOR   *LTAtransformPoint(LTA *lta, VECTOR *v_X, VECTOR *v_Y) ;
 VECTOR   *LTAinverseTransformPoint(LTA *lta, VECTOR *v_X, VECTOR *v_Y) ;
 double   LTAtransformPointAndGetWtotal(LTA *lta, VECTOR *v_X, VECTOR *v_Y) ;
-MATRIX   *LTAinverseTransformAtPoint(LTA *lta, float x, float y, float z, 
+MATRIX   *LTAinverseTransformAtPoint(LTA *lta, float x, float y, float z,
                                      MATRIX *m_L) ;
 MATRIX   *LTAworldTransformAtPoint(LTA *lta, float x, float y,float z,
                                    MATRIX *m_L);
 int      LTAtoVoxelCoords(LTA *lta, MRI *mri) ; // don't use this
 
-/* the following routine reorient and reposition the volume by editing the direction cosines and c_ras values
+/* the following routine reorient and
+   reposition the volume by editing the direction cosines and c_ras values
    it does not perform resampling */
 MRI *MRITransformedCentered(MRI *src, MRI *orig_dst, LTA *lta);
 
 LTA *LTAinvert(LTA *lta); // fill inverse part of LTA
-int LTAmodifySrcDstGeom(LTA *lta, MRI *src, MRI *dst);  // src and dst can be null.  only those non-null used to modifiy geom
-LTA *LTAchangeType(LTA *lta, int ltatype);  // must have both src and dst vol_geom valid
+int LTAmodifySrcDstGeom(LTA *lta, MRI *src, MRI *dst);  /* src and dst can be
+                                                           null. only those
+                                                           non-null used to
+                                                           modify geom */
+LTA *LTAchangeType(LTA *lta, int ltatype);  /* must have both src and
+                                               dst vol_geom valid */
 
 // new routines to retrieve src and dst volume info for transform
 LTA      *LTAreadEx(const char *fname);
-int      LTAwriteEx(const LTA *lta, const char *fname); 
-int      LTAprint(FILE *fp, const LTA *lta); 
+int      LTAwriteEx(const LTA *lta, const char *fname);
+int      LTAprint(FILE *fp, const LTA *lta);
 
 #define LINEAR_VOX_TO_VOX       0
 #define LINEAR_VOXEL_TO_VOXEL   LINEAR_VOX_TO_VOX
@@ -125,19 +133,25 @@ int      LTArasToVoxelXform(LTA *lta, MRI *mri_src, MRI *mri_dst) ;
 int      LTAvoxelTransformToCoronalRasTransform(LTA *lta) ;
 
 int FixMNITal(float  xmni, float  ymni, float  zmni,
-        float *xtal, float *ytal, float *ztal);
+              float *xtal, float *ytal, float *ztal);
 MATRIX *DevolveXFM(char *subjid, MATRIX *XFM, char *xfmname);
-MATRIX *DevolveXFMWithSubjectsDir(char *subjid, MATRIX *XFM, char *xfmname, char *sdir);
+MATRIX *DevolveXFMWithSubjectsDir(char *subjid,
+                                  MATRIX *XFM,
+                                  char *xfmname,
+                                  char *sdir);
 
 TRANSFORM *TransformRead(char *fname) ;
 int       TransformWrite(TRANSFORM *transform, char *fname) ;
 TRANSFORM *TransformIdentity(void) ;
 int       TransformFree(TRANSFORM **ptrans) ;
-int       TransformSample(TRANSFORM *transform, float xv, float yv, float zv, float *px, float *py, float *pz) ;
-int       TransformSampleInverse(TRANSFORM *transform, int xv, int yv, int zv, 
+int       TransformSample(TRANSFORM *transform,
+                          float xv, float yv, float zv,
+                          float *px, float *py, float *pz) ;
+int       TransformSampleInverse(TRANSFORM *transform, int xv, int yv, int zv,
                                  float *px, float *py, float *pz) ;
-int       TransformSampleInverseVoxel(TRANSFORM *transform, int width, int height, int depth,
-                                      int xv, int yv, int zv, 
+int       TransformSampleInverseVoxel(TRANSFORM *transform,
+                                      int width, int height, int depth,
+                                      int xv, int yv, int zv,
                                       int *px, int *py, int *pz) ;
 TRANSFORM *TransformAlloc(int type, MRI *mri) ;
 TRANSFORM *TransformCopy(TRANSFORM *tsrc, TRANSFORM *tdst) ;
@@ -145,9 +159,12 @@ TRANSFORM *TransformCopy(TRANSFORM *tsrc, TRANSFORM *tdst) ;
 int       TransformInvert(TRANSFORM *transform, MRI *mri) ;
 int       TransformSwapInverse(TRANSFORM *transform) ;
 MRI       *TransformApply(TRANSFORM *transform, MRI *mri_src, MRI *mri_dst) ;
-MRI       *TransformCreateDensityMap(TRANSFORM *transform, MRI *mri_src, MRI *mri_dst) ;
-MRI       *TransformApplyType(TRANSFORM *transform, MRI *mri_src, MRI *mri_dst, int interp_type) ;
-MRI       *TransformApplyInverse(TRANSFORM *transform, MRI *mri_src, MRI *mri_dst) ;
+MRI       *TransformCreateDensityMap(TRANSFORM *transform,
+                                     MRI *mri_src, MRI *mri_dst) ;
+MRI       *TransformApplyType(TRANSFORM *transform,
+                              MRI *mri_src, MRI *mri_dst, int interp_type) ;
+MRI       *TransformApplyInverse(TRANSFORM *transform,
+                                 MRI *mri_src, MRI *mri_dst) ;
 
 MATRIX* TransformCompose(TRANSFORM *transform);
 
@@ -165,15 +182,18 @@ MATRIX *vg_r_to_i(const VOL_GEOM *vg);
 
 int TransformCopyVolGeomToMRI(TRANSFORM *transform, MRI *mri);
 
-int  vg_isEqual(const VOL_GEOM *vg1, const VOL_GEOM *vg2); // return 1 if equal return 0 if not equal
+int vg_isEqual(const VOL_GEOM *vg1, 
+               const VOL_GEOM *vg2); /* return 1 if equal 
+                                        return 0 if not equal */
 void vg_print(const VOL_GEOM *vg);
- 
-int LTAvoxelXformToRASXform(const MRI *src, const MRI *dst, LT *voxTran, LT *rasTran);
+
+int LTAvoxelXformToRASXform(const MRI *src, const MRI *dst,
+                            LT *voxTran, LT *rasTran);
 
 // well uses LTA
 MATRIX *surfaceRASFromSurfaceRAS_(MRI *dst, MRI *src, LTA *lta);
 MRI   *MRITransformedCentered(MRI *src, MRI *orig_dst, LTA *lta) ;
 int TransformSampleReal(TRANSFORM *transform,
-												float xv, float yv, float zv, 
-												float *px, float *py, float *pz) ;
+                        float xv, float yv, float zv,
+                        float *px, float *py, float *pz) ;
 #endif
