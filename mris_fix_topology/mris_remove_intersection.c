558f49de442ac15d3ff05429ee0e2aec231150ce
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2006-01-23 22:18:19 +0000

    no code changes: emacs reformatted, untabified, trailing whitespace deleted; also updated help text

--- mris_fix_topology/mris_remove_intersection.c
+++ mris_fix_topology/mris_remove_intersection.c
@@ -17,7 +17,8 @@
 #include "timer.h"
 #include "version.h"
 
-static char vcid[]="$Id: mris_remove_intersection.c,v 1.1 2006/01/21 03:25:01 fischl Exp $";
+static char vcid[]=
+"$Id: mris_remove_intersection.c,v 1.2 2006/01/23 22:18:19 nicks Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -29,21 +30,25 @@ static void print_version(void) ;
 
 char *Progname ;
 
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   char         **av, *in_surf_fname, *out_fname ;
   int          ac, nargs, msec ;
   MRI_SURFACE  *mris ;
-  struct timeb  then ;
+  struct timeb then ;
 
-	char cmdline[CMD_LINE_LEN] ;
-	
-  make_cmd_version_string (argc, argv, "$Id: mris_remove_intersection.c,v 1.1 2006/01/21 03:25:01 fischl Exp $", "$Name:  $", cmdline);
+  char cmdline[CMD_LINE_LEN] ;
+
+  make_cmd_version_string
+    (argc, argv,
+     "$Id: mris_remove_intersection.c,v 1.2 2006/01/23 22:18:19 nicks Exp $",
+     "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_remove_intersection.c,v 1.1 2006/01/21 03:25:01 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option
+    (argc, argv,
+     "$Id: mris_remove_intersection.c,v 1.2 2006/01/23 22:18:19 nicks Exp $",
+     "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -58,11 +63,11 @@ main(int argc, char *argv[])
   ac = argc ;
   av = argv ;
   for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++)
-  {
-    nargs = get_option(argc, argv) ;
-    argc -= nargs ;
-    argv += nargs ;
-  }
+    {
+      nargs = get_option(argc, argv) ;
+      argc -= nargs ;
+      argv += nargs ;
+    }
 
   if (argc < 3)
     usage_exit() ;
@@ -70,58 +75,59 @@ main(int argc, char *argv[])
   in_surf_fname = argv[1] ;
   out_fname = argv[2] ;
 
-
   mris = MRISread(in_surf_fname) ;
   if (!mris)
     ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",
               Progname, in_surf_fname) ;
 
-	MRISaddCommandLine(mris, cmdline) ;
+  MRISaddCommandLine(mris, cmdline) ;
 
-	//	MRISsetNeighborhoodSize(mris, 2) ;
-	MRISremoveIntersections(mris) ;
+  // MRISsetNeighborhoodSize(mris, 2) ;
+  MRISremoveIntersections(mris) ;
 
-	printf("writing corrected surface to %s\n", out_fname) ;
-	MRISwrite(mris, out_fname) ;
+  printf("writing corrected surface to %s\n", out_fname) ;
+  MRISwrite(mris, out_fname) ;
   msec = TimerStop(&then) ;
   fprintf(stderr, "intersection removal took %2.2f hours\n",
           (float)msec/(1000.0f*60.0f*60.0f));
+
   exit(0) ;
+
   return(0) ;  /* for ansi */
 }
 
 /*----------------------------------------------------------------------
-            Parameters:
+  Parameters:
 
-           Description:
-----------------------------------------------------------------------*/
+  Description:
+  ----------------------------------------------------------------------*/
 static int
 get_option(int argc, char *argv[])
 {
   int  nargs = 0 ;
   char *option ;
-  
+
   option = argv[1] + 1 ;            /* past '-' */
   if (!stricmp(option, "-help"))
     print_help() ;
   else if (!stricmp(option, "-version"))
     print_version() ;
   else switch (toupper(*option))
-  {
-  case 'V':
-    Gdiag_no = atoi(argv[2]) ;
-    nargs = 1 ;
-    break ;
-  case '?':
-  case 'U':
-    print_usage() ;
-    exit(1) ;
-    break ;
-  default:
-    fprintf(stderr, "unknown option %s\n", argv[1]) ;
-    exit(1) ;
-    break ;
-  }
+    {
+    case 'V':
+      Gdiag_no = atoi(argv[2]) ;
+      nargs = 1 ;
+      break ;
+    case '?':
+    case 'U':
+      print_usage() ;
+      exit(1) ;
+      break ;
+    default:
+      fprintf(stderr, "unknown option %s\n", argv[1]) ;
+      exit(1) ;
+      break ;
+    }
 
   return(nargs) ;
 }
@@ -136,8 +142,8 @@ usage_exit(void)
 static void
 print_usage(void)
 {
-  fprintf(stderr, 
-          "usage: %s [options] <surface file> <patch file name> <output patch>"
+  fprintf(stderr,
+          "usage: %s [options] <surface in-file> <corrected surface out-file>"
           "\n", Progname) ;
 }
 
@@ -145,9 +151,9 @@ static void
 print_help(void)
 {
   print_usage() ;
-  fprintf(stderr, 
-       "\nThis program will add a template into an average surface.\n");
-  fprintf(stderr, "\nvalid options are:\n\n") ;
+  fprintf(stderr,
+          "\nThis program will remove intersecting vertices, if any, "
+          "from the surface.\n");
   exit(1) ;
 }
 
