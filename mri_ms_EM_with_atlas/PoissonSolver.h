1c3556a90aa8045de302d3c9a543d08e9f44e5bf
Xiao Han <xhan@nmr.mgh.harvard.edu>
2006-02-01 22:00:21 +0000

    First Checkin.

---
+++ mri_ms_EM_with_atlas/PoissonSolver.h
@@ -0,0 +1,12 @@
+/* CVS Version Info
+   $Id: PoissonSolver.h,v 1.1 2006/02/01 22:01:22 xhan Exp $
+*/
+
+#ifndef POISSON_SOLVER_H
+#define POISSON_SOLVER_H
+
+#include "mri.h"
+
+void multigrid(MRI *u, MRI *f, MRI *hu, int XN, int YN, int ZN);
+
+#endif
---
+++ mri_ms_EM_with_atlas/subroutines.h
@@ -0,0 +1,34 @@
+#ifndef SUBROUTINES_H
+#define SUBROUTINES_H
+
+#include "mri.h"
+
+typedef struct {
+  int x,y,z;
+} POINTI;
+
+
+void RemoveHoles(MRI *orivol);
+void  GrassFire(MRI *orivol, MRI *Label, int label, POINTI *Pt, 
+		int *curSize, int *minX, int *maxX, int *minY, int *maxY,
+		int *minZ, int *maxZ);
+void  GrassFire6(MRI *orivol, MRI *Label, int label, POINTI *Pt, 
+		int *curSize, int *minX, int *maxX, int *minY, int *maxY,
+		int *minZ, int *maxZ);
+
+void  GrassFire18(MRI *orivol, MRI *Label, int label, POINTI *Pt, 
+		int *curSize, int *minX, int *maxX, int *minY, int *maxY,
+		  int *minZ, int *maxZ);
+void GetLargestCC18(MRI *orivol);
+void GetLargestCC6(MRI *orivol);
+MRI * Dilation6(MRI *ori, MRI *out, int);
+MRI * Erosion6(MRI *ori, MRI *out, int);
+MRI * Dilation26(MRI *ori, MRI *out, int);
+MRI * Erosion26(MRI *ori, MRI *out, int);
+MRI * BinaryOpen6(MRI *ori, MRI *out, int R);
+MRI * BinaryOpen26(MRI *ori, MRI *out, int R);
+MRI * BinaryClose6(MRI *ori, MRI *out, int R);
+MRI * BinaryClose26(MRI *ori, MRI *out, int R);
+
+
+#endif
