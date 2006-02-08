92ec9d79c58290e250649684badc100fbfaec317
Xiao Han <xhan@nmr.mgh.harvard.edu>
2006-02-08 20:52:24 +0000

    corrected longitudinal with -align

--- mri_ca_register/mri_ca_register.c
+++ mri_ca_register/mri_ca_register.c
@@ -4,9 +4,9 @@
 // by Bruce Fischl
 //
 // Warning: Do not edit the following four lines.  CVS maintains them.
-// Revision Author: $Author: fischl $
-// Revision Date  : $Date: 2006/02/03 17:59:56 $
-// Revision       : $Revision: 1.42 $
+// Revision Author: $Author: xhan $
+// Revision Date  : $Date: 2006/02/08 20:52:24 $
+// Revision       : $Revision: 1.44 $
 
 
 #include <math.h>
@@ -151,7 +151,7 @@ main(int argc, char *argv[])
   DiagInit(NULL, NULL, NULL) ;
   ErrorInit(NULL, NULL, NULL) ;
 
-  nargs = handle_version_option (argc, argv, "$Id: mri_ca_register.c,v 1.42 2006/02/03 17:59:56 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_ca_register.c,v 1.44 2006/02/08 20:52:24 xhan Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -711,6 +711,10 @@ main(int argc, char *argv[])
 	  trans = (TRANSFORM *)calloc(1, sizeof(TRANSFORM)) ;
 	  trans->type = TransformFileNameType(xform_name);
 	  trans->xform = (void *)gcam;
+
+	  //The following inversion is necessary; but do I need to release the memory for the inverse transform after the mapRenormalize is done -xhan?
+	  TransformInvert(trans, mri_inputs);
+
 	  //      GCAmapRenormalize(gcam->gca, mri_inputs, trans) ;
 	  GCAmapRenormalizeWithAlignment(gcam->gca, mri_inputs, trans, parms.log_fp, parms.base_name, &lta) ;
 	  free(trans);
@@ -734,7 +738,7 @@ main(int argc, char *argv[])
 	  MRIfree(&mri_seg) ; MRIfree(&mri_aligned) ;
 	}
 #endif
-      if (reinit)
+      if (reinit && (xform_name != NULL))
 	GCAMreinitWithLTA(gcam, lta, mri_inputs, &parms) ;
       if (DIAG_VERBOSE_ON)
 	{
