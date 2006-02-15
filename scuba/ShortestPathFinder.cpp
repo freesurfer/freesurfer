a5c8b4376c0fbee83f3d53ed7720e3a04a3c0ee4
Kevin Teich <kteich@nmr.mgh.harvard.edu>
2006-02-15 22:08:46 +0000

    tabs

--- scuba/ShortestPathFinder.cpp
+++ scuba/ShortestPathFinder.cpp
@@ -46,146 +46,146 @@ void
 ShortestPathFinder::FindPath ( Point2<int>& iStartPoint,
 			       Point2<int>& iEndPoint,
 			       std::list<Point2<int> >& ioPoints ) {
-
-    int aDirectionOffsets[9][2] = { { 0, 0},
-				    {-1, 0}, {-1, 1}, {0, 1}, { 1, 1},
-				    { 1, 0}, { 1,-1}, {0,-1}, {-1,-1} };
-    float aFactor[9] = { 0, 1, 1.4, 1, 1.4, 1, 1.4, 1, 1.4 };
-
-    // Set begin and ending points.
-    Point2<int> begin( iStartPoint );
-    Point2<int> end( iEndPoint );
-
-    // Set initial cost to zero and insert it in the queue.
-    maCost->Set( begin, 0 );
-    mQueue->Insert( begin, 0 );      
-
-    // While end point not done, keep checking out neighbors of current point
-    float currentCost = 0;
-    while( !maDone->Get( end ) ) {
-
-      // Get min vertex from Q 
-      listElement* min = mQueue->GetListElement( (int)currentCost );
-      if( NULL == min ) {
-	cerr << "GetListElement returned NULL" << endl;
-	return;
-      }
-      Point2<int> current( min->Coord );
-
-      // Update the current path cost
-      currentCost = maCost->Get( current );
-
-      // Mark this point done.
-      maDone->Set( current, true );
-
-      // remove it from Q
-      mQueue->Remove( min );
+  
+  int aDirectionOffsets[9][2] = { { 0, 0},
+				  {-1, 0}, {-1, 1}, {0, 1}, { 1, 1},
+				  { 1, 0}, { 1,-1}, {0,-1}, {-1,-1} };
+  float aFactor[9] = { 0, 1, 1.4, 1, 1.4, 1, 1.4, 1, 1.4 };
+  
+  // Set begin and ending points.
+  Point2<int> begin( iStartPoint );
+  Point2<int> end( iEndPoint );
+  
+  // Set initial cost to zero and insert it in the queue.
+  maCost->Set( begin, 0 );
+  mQueue->Insert( begin, 0 );      
+  
+  // While end point not done, keep checking out neighbors of current point
+  float currentCost = 0;
+  while( !maDone->Get( end ) ) {
+    
+    // Get min vertex from Q 
+    listElement* min = mQueue->GetListElement( (int)currentCost );
+    if( NULL == min ) {
+      cerr << "GetListElement returned NULL" << endl;
+      return;
+    }
+    Point2<int> current( min->Coord );
+    
+    // Update the current path cost
+    currentCost = maCost->Get( current );
+    
+    // Mark this point done.
+    maDone->Set( current, true );
+    
+    // remove it from Q
+    mQueue->Remove( (listElement**)&min );
+    
+    // Check out neighbors.
+    for( int direction = 1; direction < 9; direction++ ) {
       
-      // Check out neighbors.
-      for( int direction = 1; direction < 9; direction++ ) {
-
-	Point2<int> neighbor( current.x() + aDirectionOffsets[direction][0],
-			      current.y() + aDirectionOffsets[direction][1]);
+      Point2<int> neighbor( current.x() + aDirectionOffsets[direction][0],
+			    current.y() + aDirectionOffsets[direction][1]);
+      
+      if( neighbor.x() >= 0 && neighbor.x() < mzX &&
+	  neighbor.y() >= 0 && neighbor.y() < mzY ) {
 	
-	if( neighbor.x() >= 0 && neighbor.x() < mzX &&
-	    neighbor.y() >= 0 && neighbor.y() < mzY ) {
-
-	  // Get the cost of this edge = value of weight volume *
-	  // factor.  We add 0.001 to the edge cost because if it's
-	  // 0, there's no preference for straight lines, since
-	  // diagonal lines will have the same cost, which in some
-	  // cases makes a really weird looking line.
-	  float newCost = currentCost + 
-	    ( (this->GetEdgeCost( neighbor ) * mEdgeBias + 0.001) * 
-	      aFactor[direction] * (1.0 - mEdgeBias) );
-
-          // If path from current point shorter than old path
-          if( newCost < maCost->Get( neighbor ) ) {
-
-	    // lower the cumulative cost to reach this neighbor
-	    maCost->Set( neighbor, newCost );
-
-	    // store new short path direction
-	    maDir->Set( neighbor, direction );
-	    
-	    // remove this neighbor from Q if in it
-	    mQueue->Remove( neighbor );
-	    
-	    // then put it in the proper place in Q for its new cost
-	    mQueue->Insert( neighbor, (int)newCost );
-	  }
-        }
-      }
-      
-#if 0
-      if( mDebug ) {
-	cerr << "after pulling" << current << endl;
-	for( int nY = 0; nY < mzY; nY++ ) {
-	  for( int nX = 0; nX < mzX; nX++ ) {
-	    if( nX == current.x() && nY == current.y() ) {
-	      cerr.width( 4 );
-	      cerr.fill( '-' );
-	      cerr << maCost->Get( nX, nY ) << "-";
-	    } else {
-	      cerr.width( 4 );
-	      cerr.fill( ' ' );
-	      cerr << maCost->Get( nX, nY ) << " ";
-	    }
-	  }  
-	  cerr << endl;
+	// Get the cost of this edge = value of weight volume *
+	// factor.  We add 0.001 to the edge cost because if it's
+	// 0, there's no preference for straight lines, since
+	// diagonal lines will have the same cost, which in some
+	// cases makes a really weird looking line.
+	float newCost = currentCost + 
+	  ( (this->GetEdgeCost( neighbor ) * mEdgeBias + 0.001) * 
+	    aFactor[direction] * (1.0 - mEdgeBias) );
+	
+	// If path from current point shorter than old path
+	if( newCost < maCost->Get( neighbor ) ) {
+	  
+	  // lower the cumulative cost to reach this neighbor
+	  maCost->Set( neighbor, newCost );
+	  
+	  // store new short path direction
+	  maDir->Set( neighbor, direction );
+	  
+	  // remove this neighbor from Q if in it
+	  mQueue->Remove( neighbor );
+	  
+	  // then put it in the proper place in Q for its new cost
+	  mQueue->Insert( neighbor, (int)newCost );
 	}
-	cerr << endl;
       }
-#endif
     }
-
-    // Start at the end and follow the direction table back to the
-    // beginning, adding points on the way.
-    ioPoints.clear();
-    Point2<int> current( end );
-    Point2<int> next;
-    while( current.x() != begin.x() ||
-	   current.y() != begin.y() ) {
-
-      ioPoints.push_front( current );
-
-      next.Set( current.x() - aDirectionOffsets[maDir->Get(current)][0], 
-		current.y() - aDirectionOffsets[maDir->Get(current)][1]);
-
-      current.Set( next.x(), next.y() );
-    }
-
+    
 #if 0
     if( mDebug ) {
-      cerr << "final cost array and path" << endl;
+      cerr << "after pulling" << current << endl;
       for( int nY = 0; nY < mzY; nY++ ) {
 	for( int nX = 0; nX < mzX; nX++ ) {
-
-	  bool bInPath = false;
-	  list<Point2<int> >::iterator tPoint;
-	  for( tPoint = ioPoints.begin(); tPoint != ioPoints.end();
-	       ++tPoint ) {
-	    if( nX == (*tPoint).x() && nY == (*tPoint).y() ) {
-	      bInPath = true;
-	      break;
-	    }
-	  }
-	  if ( bInPath ) {
-	    cerr.width( 7 );
-	    cerr.precision( 4 );
+	  if( nX == current.x() && nY == current.y() ) {
+	    cerr.width( 4 );
 	    cerr.fill( '-' );
 	    cerr << maCost->Get( nX, nY ) << "-";
 	  } else {
-	    cerr.width( 7 );
-	    cerr.precision( 4 );
+	    cerr.width( 4 );
 	    cerr.fill( ' ' );
 	    cerr << maCost->Get( nX, nY ) << " ";
 	  }
-	}      
+	}  
 	cerr << endl;
       }
       cerr << endl;
     }
+#endif
+  }
+  
+  // Start at the end and follow the direction table back to the
+  // beginning, adding points on the way.
+  ioPoints.clear();
+  Point2<int> current( end );
+  Point2<int> next;
+  while( current.x() != begin.x() ||
+	 current.y() != begin.y() ) {
+    
+    ioPoints.push_front( current );
+    
+    next.Set( current.x() - aDirectionOffsets[maDir->Get(current)][0], 
+	      current.y() - aDirectionOffsets[maDir->Get(current)][1]);
+    
+    current.Set( next.x(), next.y() );
+  }
+  
+#if 0
+  if( mDebug ) {
+    cerr << "final cost array and path" << endl;
+    for( int nY = 0; nY < mzY; nY++ ) {
+      for( int nX = 0; nX < mzX; nX++ ) {
+	
+	bool bInPath = false;
+	list<Point2<int> >::iterator tPoint;
+	for( tPoint = ioPoints.begin(); tPoint != ioPoints.end();
+	     ++tPoint ) {
+	  if( nX == (*tPoint).x() && nY == (*tPoint).y() ) {
+	    bInPath = true;
+	    break;
+	  }
+	}
+	if ( bInPath ) {
+	  cerr.width( 7 );
+	  cerr.precision( 4 );
+	  cerr.fill( '-' );
+	  cerr << maCost->Get( nX, nY ) << "-";
+	} else {
+	  cerr.width( 7 );
+	  cerr.precision( 4 );
+	  cerr.fill( ' ' );
+	  cerr << maCost->Get( nX, nY ) << " ";
+	}
+      }      
+      cerr << endl;
+    }
+    cerr << endl;
+  }
 #endif
 }
 
@@ -227,7 +227,7 @@ circularQueue::Insert ( Point2<int>& iLocation, int iCost ) {
   // insert el at the top of the list from the bucket
   el->Next = this->Circle[nBucket].Next;
   if (el->Next == NULL) {
-    cout << "ERROR.  bucket is NULL, not linked to self." << endl;
+    cout << "ERROR. bucket is NULL, not linked to self." << endl;
   }
   this->Circle[nBucket].Next->Prev = el;      
   this->Circle[nBucket].Next = el;
@@ -238,16 +238,17 @@ void
 circularQueue::Remove ( Point2<int>& iLocation ) {
 
   listElement *el = this->A->Element( iLocation );
-  this->Remove( el );      
+  this->Remove( &el );
 }
 
 void
-circularQueue::Remove( listElement *el ) {
+circularQueue::Remove( listElement **iElementToDelete ) {
 
   // if el is in linked list
+  listElement* el = *iElementToDelete;
   if (el->Prev != NULL) {
     if (el->Next == NULL) {
-      cout <<"ERROR.  el->Next is NULL."<< endl;
+      cout <<"ERROR. el->Next is NULL."<< endl;
       return;
     }
 
@@ -256,7 +257,6 @@ circularQueue::Remove( listElement *el ) {
     
     // clear el's pointers
     el->Prev = el->Next = NULL;
-
   }
 
   return;
@@ -269,11 +269,11 @@ circularQueue::GetListElement ( int iCost ) {
 
   // return the last one in the linked list.
   if (this->Circle[nBucket].Prev == NULL) {
-    cout << "ERROR in vtkImageLiveWire.  Unlinked list." << endl;
+    cout << "ERROR. Unlinked list." << endl;
     return NULL;
   }
   if (this->Circle[nBucket].Next == &this->Circle[nBucket]) {
-    cout << "ERROR in vtkImageLiveWire.  Empty linked list." << endl;
+    cout << "ERROR. Empty linked list." << endl;
     return NULL;
   }
 
@@ -301,20 +301,20 @@ circularQueue::FindMinBucket( int iCost ) {
   while( this->Circle[nBucket].Next == &this->Circle[nBucket] &&
 	 count <= this->C ) {
 
-      // search around the Q for the next vertex
-      cost++;
-      nBucket = this->GetBucket( cost );
-      count++;
-    }
-
+    // search around the Q for the next vertex
+    cost++;
+    nBucket = this->GetBucket( cost );
+    count++;
+  }
+  
   // have we looped all the way around?
   if (count > this->C) {
-      cout << "ERROR.  Empty Q." << endl;
+    cout << "ERROR. Empty Q." << endl;
   }
-
+  
   if (this->Circle[nBucket].Prev == &this->Circle[nBucket]) {
-    cout <<"ERROR in vtkImageLiveWire.  Prev not linked to bucket." << endl;
+    cout <<"ERROR. Prev not linked to bucket." << endl;
   }
-
+  
   return nBucket;
 }
