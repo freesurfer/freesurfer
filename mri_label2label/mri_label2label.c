5cc13f1e7b2abd59f6251f0abdb335f49fbb3e59
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-02-18 00:59:30 +0000

    Working on ipsi-contra mapping.

--- mri_label2label/mri_label2label.c
+++ mri_label2label/mri_label2label.c
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------
   Name: mri_label2label.c
-  $Id: mri_label2label.c,v 1.21 2004/11/16 00:35:37 greve Exp $
+  $Id: mri_label2label.c,v 1.22 2006/02/18 00:59:30 greve Exp $
   Author: Douglas Greve
   Purpose: Converts a label in one subject's space to a label
   in another subject's space using either talairach or spherical
@@ -59,7 +59,7 @@ static int  nth_is_arg(int nargc, char **argv, int nth);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_label2label.c,v 1.21 2004/11/16 00:35:37 greve Exp $";
+static char vcid[] = "$Id: mri_label2label.c,v 1.22 2006/02/18 00:59:30 greve Exp $";
 char *Progname = NULL;
 
 char  *srclabelfile = NULL;
@@ -125,7 +125,7 @@ int main(int argc, char **argv)
   int nargs;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_label2label.c,v 1.21 2004/11/16 00:35:37 greve Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_label2label.c,v 1.22 2006/02/18 00:59:30 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -232,8 +232,13 @@ int main(int argc, char **argv)
     
     /*** Load the source registration surface ***/
     if(strcmp(srcsubject,"ico")){
-      sprintf(tmpstr,"%s/%s/surf/%s.%s",SUBJECTS_DIR,srcsubject,
-	      srchemi,srcsurfreg); 
+      if(strcmp(srchemi,trghemi)==0)
+	sprintf(tmpstr,"%s/%s/surf/%s.%s",SUBJECTS_DIR,srcsubject,
+		srchemi,srcsurfreg); 
+      else
+	sprintf(tmpstr,"%s/%s/surf/%s.%s.%s",SUBJECTS_DIR,srcsubject,
+		srchemi,trghemi,srcsurfreg); 
+
       printf("Reading source registration \n %s\n",tmpstr);
       SrcSurfReg = MRISread(tmpstr);
       if(SrcSurfReg == NULL){
