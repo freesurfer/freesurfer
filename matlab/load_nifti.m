0e48b2911d7892dae7a9d2e8cffa62133f9922d3
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-04-12 15:52:00 +0000

    Routines to read and write nifti.

---
+++ matlab/load_nifti.m
@@ -0,0 +1,116 @@
+function hdr = load_nifti(niftifile,hdronly)
+% hdr = load_nifti(niftifile,hdronly)
+%
+% Loads nifti header and volume. The volume is stored
+% in hdr.vol. Columns and rows are not swapped.
+%
+% Handles compressed nifti (nii.gz) by issuing a unix command to
+% uncompress the file to a temporary file, which is then deleted.
+%
+% Dimensions are in mm and msec
+% hdr.pixdim(1) = physical size of first dim (eg, 3.125 mm or 2000 ms)
+% hdr.pixdim(2) = ...
+% 
+% The sform and qform matrices are stored in hdr.sform and hdr.qform.
+%
+% hdr.vox2ras is the vox2ras matrix based on sform (if valid), then
+% qform.
+%
+% See also: load_nifti_hdr.m
+%
+% $Id: load_nifti.m,v 1.6.2.1 2006/04/12 15:52:00 greve Exp $
+
+hdr = [];
+
+if(nargin < 1 | nargin > 2)
+  fprintf('hdr = load_nifti(niftifile,<hdronly>)\n');
+  return;
+end
+
+if(~exist('hdronly','var')) hdronly = []; end
+if(isempty(hdronly)) hdronly = 0; end
+
+% unzip if it is compressed 
+ext = niftifile((strlen(niftifile)-2):strlen(niftifile));
+if(strcmpi(ext,'.gz'))
+  gzipped =  round(rand(1)*10000000);
+  ind = findstr(niftifile, '.');
+  new_niftifile = sprintf('/tmp/tmp%d.nii', gzipped);
+  fprintf('Uncompressing %s to %s\n',niftifile,new_niftifile);
+  unix(sprintf('zcat %s > %s', niftifile, new_niftifile)) ;
+  niftifile = new_niftifile ;
+else
+  gzipped = -1 ;
+end
+
+hdr = load_nifti_hdr(niftifile);
+if(isempty(hdr)) 
+  if(gzipped >=0) unix(sprintf('rm %s', niftifile)); end
+  return; 
+end
+
+% If only header is desired, return now
+if(hdronly) 
+  if(gzipped >=0) unix(sprintf('rm %s', niftifile)); end
+  return; 
+end
+
+% Open to read the pixel data
+fp = fopen(niftifile,'r',hdr.endian);
+
+% Get past the header
+if(hdr.vox_offset ~= 348)
+  fprintf('hdr.vox_offset = %d, which is not the size of the hdr (348)\n',...
+	  hdr.vox_offset);
+  fprintf('Just thought I would let you know ...\n');
+end
+fseek(fp,round(hdr.vox_offset),'bof');
+
+switch(hdr.datatype)
+ case   2, [hdr.vol nitemsread] = fread(fp,inf,'char');
+ case   4, [hdr.vol nitemsread] = fread(fp,inf,'short');
+ case   8, [hdr.vol nitemsread] = fread(fp,inf,'int');
+ case  16, [hdr.vol nitemsread] = fread(fp,inf,'float');
+ case  64, [hdr.vol nitemsread] = fread(fp,inf,'double');
+ case 512, [hdr.vol nitemsread] = fread(fp,inf,'ushort');
+ case 768, [hdr.vol nitemsread] = fread(fp,inf,'uint');
+ otherwise,
+  fprintf('ERROR: data type %d not supported',hdr.datatype);
+  hdr = [];
+  return;
+end
+
+fclose(fp);
+if(gzipped >=0) 
+  fprintf('Deleting temporary uncompressed file %s\n',niftifile);
+  unix(sprintf('rm %s', niftifile)); 
+end
+
+% Get total number of voxels
+dim = hdr.dim(2:end);
+ind0 = find(dim==0);
+dim(ind0) = 1;
+nvoxels = prod(dim);
+
+% Check that that many voxels were read in
+if(nitemsread ~= nvoxels) 
+  fprintf('ERROR: %s, read in %d voxels, expected %d\n',...
+	  niftifile,nitemsread,nvoxels);
+  hdr = [];
+  return;
+end
+
+hdr.vol = reshape(hdr.vol, dim');
+if(hdr.scl_slope ~= 0)
+  fprintf('Rescaling NIFTI: slope = %g, intercept = %g\n',...
+	  hdr.scl_slope,hdr.scl_inter);
+  %fprintf('    Good luck, this has never been tested ... \n');
+  hdr.vol = hdr.vol * hdr.scl_slope  + hdr.scl_inter;
+end
+
+return;
+
+
+
+
+
---
+++ matlab/save_nifti.m
@@ -0,0 +1,140 @@
+function err = save_nifti(hdr,niftifile)
+% err = save_nifti(hdr,niftifile)
+%
+% Only uncompressed (so far)
+% pixel data should be in hdr.vol
+
+err = 1;
+if(nargin ~= 2)
+  fprintf('err = save_nifti(hdr,niftifile)\n');
+  return;
+end
+
+ext = niftifile((strlen(niftifile)-2):strlen(niftifile));
+if(strcmpi(ext,'.gz'))
+  gzip_needed = 1;
+  niftifile = niftifile(1:strlen(niftifile)-3);
+  fprintf('First, saving to %s before compressing\n',niftifile);
+else
+  gzip_needed = 0;
+end
+
+
+fp = fopen(niftifile,'w');
+if(fp == -1)
+  fprintf('ERROR: could not open %s\n',niftifile);
+  return;
+end
+
+hdr.data_type = [hdr.data_type(:)' repmat(' ',[1 10])];
+hdr.data_type = hdr.data_type(1:10);
+
+hdr.db_name = [hdr.data_type(:)' repmat(' ',[1 18])];
+hdr.db_name = hdr.db_name(1:18);
+
+hdr.dim = ones(1,8);
+hdr.dim(1) = 4;
+hdr.dim(2) = size(hdr.vol,1);
+hdr.dim(3) = size(hdr.vol,2);
+hdr.dim(4) = size(hdr.vol,3);
+hdr.dim(5) = size(hdr.vol,4);
+
+hdr.pixdim = [hdr.pixdim(:)' repmat(0,[1 8])];
+hdr.pixdim = hdr.pixdim(1:8);
+
+hdr.descrip = [hdr.descrip(:)' repmat(' ',[1 80])];
+hdr.descrip = hdr.descrip(1:80);
+
+hdr.aux_file = [hdr.aux_file(:)' repmat(' ',[1 24])];
+hdr.aux_file = hdr.aux_file(1:24);
+
+hdr.intent_name = [hdr.intent_name(:)' repmat(' ',[1 16])];
+hdr.intent_name = hdr.intent_name(1:16);
+
+hdr.magic = [hdr.magic(:)' repmat(0,[1 4])];
+hdr.magic = hdr.magic(1:4);
+
+if(isempty(hdr.regular))  hdr.regular  = ' '; end
+if(isempty(hdr.dim_info)) hdr.dim_info = ' '; end
+if(isempty(hdr.slice_code)) hdr.slice_code = ' '; end
+if(isempty(hdr.xyzt_units)) hdr.xyzt_units = ' '; end % should be err
+
+hdr.vox_offset = 348; 
+fwrite(fp,348,'int');
+fwrite(fp,hdr.data_type,    'char');
+fwrite(fp,hdr.db_name,      'char');
+fwrite(fp,hdr.extents,      'int');
+fwrite(fp,hdr.session_error,'short');
+fwrite(fp,hdr.regular,      'char');
+fwrite(fp,hdr.dim_info,     'char');
+fwrite(fp,hdr.dim,          'short');
+fwrite(fp,hdr.intent_p1,    'float');
+fwrite(fp,hdr.intent_p2,    'float');
+fwrite(fp,hdr.intent_p3,    'float');
+fwrite(fp,hdr.intent_code,  'short');
+fwrite(fp,hdr.datatype,     'short');
+fwrite(fp,hdr.bitpix,       'short');
+fwrite(fp,hdr.slice_start,  'short');
+fwrite(fp,hdr.pixdim,       'float');
+fwrite(fp,hdr.vox_offset,   'float');
+fwrite(fp,hdr.scl_slope,    'float');
+fwrite(fp,hdr.scl_inter,    'float');
+fwrite(fp,hdr.slice_end,    'short');
+fwrite(fp,hdr.slice_code,   'char');
+fwrite(fp,hdr.xyzt_units,   'char');
+fwrite(fp,hdr.cal_max,      'float');
+fwrite(fp,hdr.cal_min,      'float');
+fwrite(fp,hdr.slice_duration,'float');
+fwrite(fp,hdr.toffset,       'float');
+fwrite(fp,hdr.glmax,         'int');
+fwrite(fp,hdr.glmin,         'int');
+fwrite(fp,hdr.descrip,       'char');
+fwrite(fp,hdr.aux_file,      'char');
+fwrite(fp,hdr.qform_code,    'short');
+fwrite(fp,hdr.sform_code,    'short');
+fwrite(fp,hdr.quatern_b,     'float');
+fwrite(fp,hdr.quatern_c,     'float');
+fwrite(fp,hdr.quatern_d,     'float');
+fwrite(fp,hdr.quatern_x,     'float');
+fwrite(fp,hdr.quatern_y,     'float');
+fwrite(fp,hdr.quatern_z,     'float');
+fwrite(fp,hdr.srow_x,        'float');
+fwrite(fp,hdr.srow_y,        'float');
+fwrite(fp,hdr.srow_z,        'float');
+fwrite(fp,hdr.intent_name,   'char');
+fwrite(fp,hdr.magic,         'char');
+
+npix = prod(size(hdr.vol));
+switch(hdr.datatype)
+ case   2, nitemswritten = fwrite(fp,hdr.vol,'char');
+ case   4, nitemswritten = fwrite(fp,hdr.vol,'short');
+ case   8, nitemswritten = fwrite(fp,hdr.vol,'int');
+ case  16, nitemswritten = fwrite(fp,hdr.vol,'float');
+ case  64, nitemswritten = fwrite(fp,hdr.vol,'double');
+ case 512, nitemswritten = fwrite(fp,hdr.vol,'ushort');
+ case 768, nitemswritten = fwrite(fp,hdr.vol,'uint');
+ otherwise,
+  fprintf('WARNING: data type %d not supported, but writing as float',...
+	  hdr.datatype);
+  nitemswritten = fwrite(fp,hdr.vol,'float');
+  return;
+end
+fclose(fp);
+
+if(npix ~= nitemswritten)
+  fprintf('ERROR: tried to write %d, but only wrote %d',npix,nitemswritten);
+  return;
+end
+
+if(gzip_needed)
+  cmd = sprintf('gzip -f %s', niftifile);
+  fprintf('Compressing with\n');
+  fprintf('   %s\n',cmd);
+  unix(cmd);
+end
+
+
+err = 0;
+return;
+
+
