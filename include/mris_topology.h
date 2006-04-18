802ac03b7e2690332638c865aa1f992fe3c5dbf7
Florent Segonne <segonne@nmr.mgh.harvard.edu>
2006-04-18 09:40:45 +0000

    n_vertices+n_faces

--- include/mris_topology.h
+++ include/mris_topology.h
@@ -9,11 +9,35 @@ extern "C" {
 
 #include "mrisurf.h"
 
-	//function
-	void MRIScutSurface(MRIS *surf_in);
+	//functions
+	bool MRIScorrectDefect(MRIS *mris, int defect_number);
+	int MRISgetEulerNumber(const MRIS *mris, const int *list_of_faces, int nfs);
+	MRIP* MRIPextractFromMRIS(MRIS *mris, int defect_number);
+	void MRISinitSurface(MRIS *mris);
 
 #ifdef __cplusplus
 }
+#endif
+
+
+
+// C++ portion starts here
+#ifdef __cplusplus
+
+extern "C" {
+#include "mrisurf.h"
+#include "error.h"
+}
+#include "topology/surface.h"
+
+MRIP *MRIPalloc(int nvertices, int nfaces);
+void MRIPfree(MRIP **mrip);
+MRIP *MRIPclone(MRIP *src);
+
+Surface *MRIStoSurface(MRIS *mris);
+void SurfaceToMRIP(Surface &surface, MRIS &mris);
+bool MRISaddMRIP(MRIS *mris_dst, MRIP *mrip);
+
 #endif
 
 
