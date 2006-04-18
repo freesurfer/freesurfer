d73aa2ab36c7ff33260b06f76a15ce34e29a6c7c
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-04-18 19:51:16 +0000

    Fixed bug where it would die if the offset of one stimulus abuttted the onset o fthe next.

--- fsfast/toolbox/fast_st2fir.m
+++ fsfast/toolbox/fast_st2fir.m
@@ -19,7 +19,7 @@ function Xfir = fast_st2fir(st,ntp,TR,psdwin,usew)
 %  5. Does not force dpsd to be an integer divisor of TR,
 %     but it is a good idea.
 %
-% $Id: fast_st2fir.m,v 1.4 2006/04/18 19:34:51 greve Exp $
+% $Id: fast_st2fir.m,v 1.5 2006/04/18 19:51:16 greve Exp $
 
 Xfir = [];
 
@@ -30,10 +30,21 @@ end
 if(~exist('usew','var')) usew = []; end
 if(isempty(usew)) usew = 0; end
 
-% If weights arent spec, set them to 1
+% If weights are not specified, set them to 1
 if(size(st,2) < 3 | ~usew) st(:,3) = 1; end
 
-% Make sure that presentations are not overlapping
+npres   = size(st,1); % number of presentations
+psdmin  = psdwin(1);  % start of PSD window
+psdmax  = psdwin(2);  % end of PSD window
+dpsd    = psdwin(3);  % increment of PSD window
+npsdwin = round((psdmax-psdmin)/dpsd);
+
+% The following two pieces of code prevent the case where stimuli
+% of the same type are presented in a overlapping manner. It is,
+% of course, ok to have the presentations of different stimulus
+% types overlap (or even be simultaneous).
+
+% Make sure that presentations are not simultaneous
 st = sortrows(st); % sort in order of onset time
 d = diff(st(:,1));
 ind = find(d == 0);
@@ -41,19 +52,16 @@ if(~isempty(ind))
   fprintf('ERROR: fast_st2fir: two or more presentations are simultaneous\n');
   return;
 end
+
+% Check whether the offset of one stimulus is past the onset of the
+% next. Allows them to overlay by less than dpsd/2.
 tonset  = st(:,1);
 toffset = st(:,1) + st(:,2);
-ind = find(toffset(1:end-1) > tonset(2:end));
+ind = find(toffset(1:end-1) > (tonset(2:end) + dpsd/2));
 if(~isempty(ind))
   fprintf('ERROR: fast_st2fir: two or more presentations overlap\n');
   return;
 end
-
-npres   = size(st,1); % number of presentations
-psdmin  = psdwin(1);  % start of PSD window
-psdmax  = psdwin(2);  % end of PSD window
-dpsd    = psdwin(3);  % increment of PSD window
-npsdwin = round((psdmax-psdmin)/dpsd);
 
 % Alloc and set to 0
 Xfir = zeros(ntp,npsdwin);
