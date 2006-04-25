bd862528b5146420814e18eb0516ab096017a37b
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-04-25 05:04:32 +0000

    Now computes mode without calling mode

--- fsfast/toolbox/fast_raygausinit.m
+++ fsfast/toolbox/fast_raygausinit.m
@@ -2,14 +2,19 @@ function params = fast_raygausinit(y)
 % params = fast_raygausinit(y)
 % params = [alpha Rmu Gmu Gstd];
 %
-% $Id: fast_raygausinit.m,v 1.1 2006/04/25 01:31:40 greve Exp $
+% $Id: fast_raygausinit.m,v 1.2 2006/04/25 05:04:32 greve Exp $
 
 if(nargin ~= 1)
   fprintf('params = fast_raygausinit(y)\n');
   return;
 end
 
-md = mode(y);
+%md = mode(y);
+ysort = sort(y);
+ny = length(y);
+md = ysort(round(ny/2));
+
+
 indray = find(y < md);
 indgaus = find(y >= md);
 alpha = .5;
