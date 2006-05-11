50e192b01e96ee3760de2277df491c7dfaafe01f
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2006-05-11 22:57:41 +0000

    moved NRC functions hiding in sig.h to nr_wrapper.h

--- include/sig.h
+++ include/sig.h
@@ -1,17 +1,10 @@
 #ifndef SIG_H
 #define SIG_H
 
-double sigt(double t, int df) ;
-float betacf(float a, float b, float x) ;
-float gammln(float xx) ;
-float betai(float a, float b, float x) ;
-float sigchisq(double chisq, int df) ;
-void gser(float *gamser,float a,float x, float *gln) ;
-void gcf(float *gammcf, float a, float x, float *gln) ;
-float gammp(float a,float x) ;
-
 double fdr2vwth(double *p, int np, double fdr);
 double vwth2fdr(double *p, int np, double vwth);
 int doublecompar(const void *v1, const void *v2);
+double sigt(double t, int df) ;
+float sigchisq(double chisq, int df) ;
 
 #endif
