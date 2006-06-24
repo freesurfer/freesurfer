7157bd352b4897f0b16f6b163cee5294d1343555
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-06-24 14:03:47 +0000

    Now seems to work.

--- fsfast/toolbox/rft_zcluster_cdf.m
+++ fsfast/toolbox/rft_zcluster_cdf.m
@@ -2,8 +2,8 @@ function cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)
 % cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)
 %
 % Prob that a cluster >= csize defined by threshold vthresh will be
-% found in a D-dim z-field with fwhm smoothness of ssize search space
-% 
+% found in a D-dim z-field with fwhm smoothness of ssize search space.
+% vthresh is the voxel-wise z thresh
 
 cdf = [];
 if(nargin ~= 5)
@@ -12,15 +12,23 @@ if(nargin ~= 5)
 end
 
 W = fwhm/sqrt(4*log(2));
-dLh = W.^(-D);
+dLh = W.^(-D); % Same as FSL?
 %dLh = 0.493971;
 
+if(0)
 Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (vthresh.^(D-1)) .* ...
      exp(-(vthresh.^2)/2); 
+else
+% This appears to be the FSL equation from the techrep
+Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (vthresh.^(D-1) - 1) .* ...
+     exp(-(vthresh.^2)/2); 
+end
 
 beta = ( ((gamma(D/2+1) .* Em)) ./ (ssize.*mri_zcdf(-vthresh))).^(2./D);
 
-cdf = exp(-beta.*(csize.^(2./D)));
+% Prob than n >= k, ie, the number of voxels in a cluster >= csize
+Pnk = exp(-beta.*(csize.^(2./D)));
+cdf = 1 - exp(-Em.*Pnk);
 
 %keyboard
 
