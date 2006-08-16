355c34ca01b3dc17b9750b1d27398e5d20cdb947
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-08-16 03:17:13 +0000

    Major changes

--- matlab/bmmroc.m
+++ matlab/bmmroc.m
@@ -1,16 +1,42 @@
-function [fpr,tpr,auc] = bmmroc(pA,pI,lambda,ntrials,thresh)
-% [fpr tpr auc] = bmmroc(pA,pI,lambda,ntrials,thresh);
+function [fpr,tpr,auc,fdr,nthresh] = bmmroc(pA,pI,lambda,ntrials,nthresh)
+% [fpr tpr auc fdr nthresh] = bmmroc(pA,pI,lambda,ntrials,<nthresh>)
 %
 % Computes the ROC (ie, TPR and FPR) based on a binomial mixture
 % model. 
 %
-% $Id: bmmroc.m,v 1.1 2006/08/15 23:42:51 greve Exp $
+% pA - probability that a truly active voxel is declared positive
+% pI - probability that a truly inactive voxel is declared positive
+% lambda - mixure of active to total number of voxels
+% ntrials 
+% nthresh - threshold used to compute positives. Can be a vector. 
+%  default is [0:ntrials].
+%
+% auc - area under the [ROC] curv
+% fdr - false dicovery rate as a function of threshold
+%
+% $Id: bmmroc.m,v 1.2 2006/08/16 03:17:13 greve Exp $
 
-Nthresh = round(ntrials*thresh);
-fpr = 1-binomialcdf(Nthresh,ntrials,pI);
-tpr = 1-binomialcdf(Nthresh,ntrials,pA);
-%auc = trapz(fpr,tpr);
-auc = 0;
+if(nargin < 4 | nargin > 5)
+  fprintf('[fpr tpr auc] = bmmroc(pA,pI,lambda,ntrials,<nthresh>)\n');
+  return;
+end
+
+if(~exist('nthresh','var')) nthresh = []; end
+if(isempty(nthresh)) nthresh = [0:ntrials]; end
+
+fpr = 1-binomialcdf(nthresh,ntrials,pI);
+tpr = 1-binomialcdf(nthresh,ntrials,pA);
+
+% Area under the ROC curve
+auc = abs(trapz(fpr,tpr)); % needs abs for reversal
+
+% False dicovery rate
+den = ((1-lambda)*fpr+lambda*tpr);
+ind = find(den == 0);
+den(ind) = 1;
+fdr = fpr./den;
+ind = find(fdr<0 | fdr>1);
+fdr(ind) = 0;
 
 return;
 
