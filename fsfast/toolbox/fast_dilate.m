5d9f31a84dcdba5901d53c9f74d3f053cbfc29ac
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-09-07 02:32:55 +0000

    Major changes. Now uses mshift, which makes it much faster.

--- fsfast/toolbox/fast_dilate.m
+++ fsfast/toolbox/fast_dilate.m
@@ -12,12 +12,12 @@ function md = fast_dilate(m,ndilations,erodeflag,flag2d)
 % flag2d=1, then 3x3x1. Dilation and erosion are reversible only
 % if there are no edge effects.
 %
-% $Id: fast_dilate.m,v 1.1 2005/02/17 18:19:16 greve Exp $
+% $Id: fast_dilate.m,v 1.2 2006/09/07 02:32:55 greve Exp $
 
 md = [];
 
 if(nargin < 1 | nargin > 4)
-  fprintf('md = fast_dilate(m,<ndilations>,<erodeflag>,<flag2d>)\n');
+  fprintf('md = di1(m,<ndilations>,<erodeflag>,<flag2d>)\n');
   return;
 end
 
@@ -30,7 +30,6 @@ if(isempty(erodeflag)) erodeflag = 0; end
 if(~exist('flag2d','var')) flag2d = []; end
 if(isempty(flag2d)) flag2d = 0; end
 
-
 if(erodeflag)
   md = ~fast_dilate(~m,ndilations,0,flag2d);
   return;
@@ -42,35 +41,23 @@ end
 md = m;
 
 if(flag2d) dsrange = 0;
-else       dsrange = 1;
+else       dsrange = [-1:1];
 end
 
-
-for r = 1:size(m,1)
-  for c = 1:size(m,2)
-    for s = 1:size(m,3)
-      if(md(r,c,s)) continue; end
-
-      for dr = -1:1
-	rn = r+dr;
-	if(rn < 1 | rn > size(m,1)) continue; end
-	for dc = -1:1
-	  cn = c+dc;
-	  if(cn < 1 | cn > size(m,2)) continue; end
-	  for ds = -dsrange:dsrange
-	    sn = s+ds;
-	    if(sn < 1 | sn > size(m,3)) continue; end
-	    if(m(rn,cn,sn))
-	      md(r,c,s) = 1;
-	      continue;
-	    end
-	  end % ds
-	end % dc
-      end % dr
-    
+md = m;
+tic;
+nth = 1;
+%fprintf('--------------------\n');
+for dc = -1:1
+  for dr = -1:1
+    for ds = dsrange
+      if(dc==0 & dr==0 & ds == 0) continue; end
+      vshift = [dc dr ds];
+      %fprintf('%2d %2d %2d %2d  %g\n',nth,vshift,toc);
+      md = md | fast_mshift(m,vshift);
+      nth = nth + 1;
     end
   end
 end
 
 return;
-
