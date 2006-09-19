155118e3b95e425491a6086ff73d04e841ce9997
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2006-09-19 17:28:52 +0000

    script for generating registration movie.

--- mris_register/make_mris_register_movie.tcl
+++ mris_register/make_mris_register_movie.tcl
@@ -1,29 +1,46 @@
 set suffix ec.reg
+set suffix ec.tal.reg
+set suffix reg
+#rotate_brain_y 190
+set suffix sphere.funcreg
+set suffix sphere.noinf.ec.reg
+set suffix sphere.ec.one.reg
+set suffix sphere.ec.one.test.reg
+set suffix sphere.ec.reg
+set suffix sphere.new.reg
 set surf_dir $env(SUBJECTS_DIR)/$subject/surf
 set_current_vertex_set 0
 set fname $surf_dir/$hemi.sphere
 read_surface_vertex_set 0 $fname
+set target 1
+set curv sno1_target_blur4.00
+set curv $hemi.target${target}
 set curv $hemi.target
 read_binary_curv
 redraw
 set_current_vertex_set 0
-set rgb $hemi.$subject.sphere.$suffix.target1.rgb
+set rgb $hemi.$subject.$suffix.target1.rgb
 save_rgb
+#set sulc inflated.H
+#set cslope 17
+#set sulc $hemi.0074blur2.00
+#set sulc sno1_blur4.00
 read_binary_sulc
-#set curv $hemi.blur4
-#read_binary_curv
-set min_p 0
-set max_p 57
-set fname $surf_dir/$hemi.sphere.${suffix}[format "%04d" 0]
+set min_p 90
+set max_p 105
+set fname $surf_dir/$hemi.${suffix}
+set fname $surf_dir/$hemi.${suffix}[format "%04d" 0]
 read_surface_vertex_set 1 $fname
-for {set pno $min_p } { $pno <= $max_p} {incr pno 1} {
+mark_vertex 69625 1
+for {set pno $min_p } { $pno <= $max_p} {incr pno 5} {
 		set_current_vertex_set 1
-		set fname $surf_dir/$hemi.sphere.${suffix}[format "%04d" $pno]
+		set fname $surf_dir/$hemi.${suffix}[format "%04d" $pno]
 		read_surface_vertex_set 1 $fname
-		set rgb $hemi.$subject.sphere.${suffix}[format "%04d" $pno].rgb
+		set rgb $hemi.$subject.${suffix}[format "%04d" $pno].rgb
 		redraw
 		set_current_vertex_set 1
 		redraw
 		save_rgb
 		puts "finished saving rgb $rgb"
 }
+exit
