226a5ae30d45e9d4ecd8164f82ecef66b691f5e0
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2006-10-06 18:16:10 +0000

    added GCAcomputeLogImageProbability to compute the likelihood of an image given a transform and an atlas. Will use this to pick out the best skull stripped image.
    added GCAcomputeLogImageProbability

--- include/gca.h
+++ include/gca.h
@@ -170,6 +170,7 @@ int  GCAnodeToVoxel(GCA *gca, MRI *mri, int xn, int yn, int zn, int *pxv,
                     int *pyv, int *pzv) ;
 int  GCApriorToVoxel(GCA *gca, MRI *mri, int xn, int yn, int zn, int *pxv, 
                     int *pyv, int *pzv) ;
+double GCAimageLogLikelihood(GCA *gca, MRI *mri_inputs, TRANSFORM *transform) ;
 float GCAcomputeLogImageProbability(GCA *gca, MRI *mri_inputs, MRI *mri_labels,
                                     TRANSFORM *transform) ;
 float  GCAcomputeLogSampleProbability(GCA *gca, GCA_SAMPLE *gcas, 
