52fc6483e837492bdc8dce7e81598597b180c127
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-10-19 16:59:43 +0000

    Converts a z map to a -log10(p) map.

---
+++ mri_z2p/mri_z2p.c
@@ -0,0 +1,236 @@
+
+/*! 
+\file mri_z2p.c
+\brief Converts a z to a -log10(p)
+\author Douglas Greve
+*/
+
+
+
+/*
+  BEGINHELP
+
+  ENDHELP
+*/
+
+/*
+  BEGINUSAGE
+
+  ENDUSAGE
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
+#include "macros.h"
+#include "utils.h"
+#include "mrisurf.h"
+#include "mrisutils.h"
+#include "error.h"
+#include "diag.h"
+#include "mri.h"
+#include "mri2.h"
+#include "fio.h"
+#include "version.h"
+#include "label.h"
+#include "matrix.h"
+#include "annotation.h"
+#include "fmriutils.h"
+#include "cmdargs.h"
+#include "fsglm.h"
+#include "pdf.h"
+#include "fsgdf.h"
+#include "timer.h"
+#include "matfile.h"
+#include "volcluster.h"
+#include "surfcluster.h"
+
+static int  parse_commandline(int argc, char **argv);
+static void check_options(void);
+static void print_usage(void) ;
+static void usage_exit(void);
+static void print_help(void) ;
+static void print_version(void) ;
+static void dump_options(FILE *fp);
+int main(int argc, char *argv[]) ;
+
+static char vcid[] = "$Id: mri_z2p.c,v 1.1 2006/10/19 16:59:43 greve Exp $";
+char *Progname = NULL;
+char *cmdline, cwd[2000];
+int debug=0;
+int checkoptsonly=0;
+struct utsname uts;
+
+char *TempVolFile=NULL;
+char *subject, *hemi, *SUBJECTS_DIR;
+
+/*---------------------------------------------------------------*/
+int main(int argc, char *argv[])
+{
+  int nargs;
+
+  nargs = handle_version_option (argc, argv, vcid, "$Name:  $");
+  if (nargs && argc - nargs == 1) exit (0);
+  argc -= nargs;
+  cmdline = argv2cmdline(argc,argv);
+  uname(&uts);
+  getcwd(cwd,2000);
+
+  Progname = argv[0] ;
+  argc --;
+  argv++;
+  ErrorInit(NULL, NULL, NULL) ;
+  DiagInit(NULL, NULL, NULL) ;
+  if(argc == 0) usage_exit();
+  parse_commandline(argc, argv);
+  check_options();
+  if(checkoptsonly) return(0);
+  dump_options(stdout);
+
+  SUBJECTS_DIR = getenv("SUBJECTS_DIR");
+  if(SUBJECTS_DIR == NULL){
+    printf("ERROR: SUBJECTS_DIR not defined in environment\n");
+    exit(1);
+  }
+
+  return 0;
+}
+/* ------ Doxygen markup starts on the line below ---- */
+/*! 
+\fn int parse_commandline(int argc, char **argv)
+\brief Parses the command-line arguments
+\param argc - number of command line arguments
+\param argv - pointer to a character pointer
+*/
+/* ------ Doxygen markup ends on the line above ---- */
+static int parse_commandline(int argc, char **argv)
+{
+  int  nargc , nargsused;
+  char **pargv, *option ;
+
+  if(argc < 1) usage_exit();
+
+  nargc   = argc;
+  pargv = argv;
+  while(nargc > 0){
+
+    option = pargv[0];
+    if(debug) printf("%d %s\n",nargc,option);
+    nargc -= 1;
+    pargv += 1;
+
+    nargsused = 0;
+
+    if (!strcasecmp(option, "--help"))  print_help() ;
+    else if (!strcasecmp(option, "--version")) print_version() ;
+    else if (!strcasecmp(option, "--debug"))   debug = 1;
+    else if (!strcasecmp(option, "--checkopts"))   checkoptsonly = 1;
+    else if (!strcasecmp(option, "--nocheckopts")) checkoptsonly = 0;
+
+    else if (!strcasecmp(option, "--temp-vol")){
+      if(nargc < 1) CMDargNErr(option,1);
+      TempVolFile = pargv[0];
+      nargsused = 1;
+    }
+    else{
+      fprintf(stderr,"ERROR: Option %s unknown\n",option);
+      if(CMDsingleDash(option))
+	fprintf(stderr,"       Did you really mean -%s ?\n",option);
+      exit(-1);
+    }
+    nargc -= nargsused;
+    pargv += nargsused;
+  }
+  return(0);
+}
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void usage_exit(void)
+\brief Prints usage and exits
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void usage_exit(void)
+{
+  print_usage() ;
+  exit(1) ;
+}
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void print_usage(void)
+\brief Prints usage and returns (does not exit)
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void print_usage(void)
+{
+  printf("USAGE: %s \n",Progname) ;
+  printf("\n");
+  printf("   --temp-vol volfile : template volume \n");
+  printf("\n");
+  printf("   --debug     turn on debugging\n");
+  printf("   --checkopts don't run anything, just check options and exit\n");
+  printf("   --help      print out information on how to use this program\n");
+  printf("   --version   print out version and exit\n");
+  printf("\n");
+  printf("%s\n", vcid) ;
+  printf("\n");
+}
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void print_help(void)
+\brief Prints help and exits
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void print_help(void)
+{
+  print_usage() ;
+  printf("WARNING: this program is not yet tested!\n");
+  exit(1) ;
+}
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void print_version(void)
+\brief Prints version and exits
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void print_version(void)
+{
+  printf("%s\n", vcid) ;
+  exit(1) ;
+}
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void check_options(void)
+\brief Checks command-line options
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void check_options(void)
+{
+  return;
+}
+
+/* -- Doxygen markup starts on the line below (this line not needed for Doxygen) -- */
+/*! 
+\fn static void dump_options(FILE *fp)
+\brief Prints command-line options to the given file pointer
+\param FILE *fp - file pointer
+*/
+/* ------ Doxygen markup ends on the line above  (this line not needed for Doxygen) -- */
+static void dump_options(FILE *fp)
+{
+  fprintf(fp,"\n");
+  fprintf(fp,"%s\n",vcid);
+  fprintf(fp,"cwd %s\n",cwd);
+  fprintf(fp,"cmdline %s\n",cmdline);
+  fprintf(fp,"sysname  %s\n",uts.sysname);
+  fprintf(fp,"hostname %s\n",uts.nodename);
+  fprintf(fp,"machine  %s\n",uts.machine);
+  fprintf(fp,"user     %s\n",VERuser());
+
+  return;
+}
