aa045df0203296cd896d981f432e942c1ec7fb72
Dennis Jen <dsjen@nmr.mgh.harvard.edu>
2006-10-25 18:20:04 +0000

    Updated fs_lbfgs to vxl 1.6.0 and changed configure.in to link to v3p_netlib (new library in vxl 1.6.0).

--- utils/fs_lbfgs.cpp
+++ utils/fs_lbfgs.cpp
@@ -1,3 +1,4 @@
+// This is core/vnl/algo/vnl_lbfgs.cxx
 #ifdef VCL_NEEDS_PRAGMA_INTERFACE
 #pragma implementation
 #endif
@@ -13,12 +14,8 @@
 #include <vcl_cmath.h>
 #include <vcl_iostream.h>
 #include <vcl_iomanip.h> // for setw (replaces cout.form())
+
 #include <vnl/algo/vnl_netlib.h> // lbfgs_()
-
-extern "C" {
-#include <lbfgs.h> // from netlib, for lb3_ data struct
-}
-
 
 //: Default constructor.
 // memory is set to 5, line_search_accuracy to 0.9.
@@ -29,7 +26,6 @@ fs_lbfgs::fs_lbfgs():
   init_parameters();
 }
 
-
 //: Constructor. f is the cost function to be minimized.
 // Calls init_parameters
 fs_lbfgs::fs_lbfgs(vnl_cost_function& f):
@@ -37,7 +33,6 @@ fs_lbfgs::fs_lbfgs(vnl_cost_function& f):
 {
   init_parameters();
 }
-
 
 //: Called by constructors.
 // Memory is set to 5, line_search_accuracy to 0.9, default_step_length to 1.
@@ -51,12 +46,18 @@ void fs_lbfgs::init_parameters()
 bool fs_lbfgs::minimize(vnl_vector<double>& x)
 {
   /* Local variables */
-  /*     The driver for fs_lbfgs must always declare LB2 as EXTERNAL */
+  /*     The driver for vnl_lbfgs must always declare LB2 as EXTERNAL */
 
-  int n = f_->get_number_of_unknowns();
-  int m = memory; // The number of basis vectors to remember.
+  long n = f_->get_number_of_unknowns();
+  long m = memory; // The number of basis vectors to remember.
 
-  int iprint[2] = {1, 0};
+  // Create an instance of the lbfgs global data to pass as an
+  // argument.  It must persist through all calls in this
+  // minimization.
+  v3p_netlib_lbfgs_global_t lbfgs_global;
+  v3p_netlib_lbfgs_init(&lbfgs_global);
+
+  long iprint[2] = {1, 0};
   vnl_vector<double> g(n);
 
   // Workspace
@@ -65,24 +66,14 @@ bool fs_lbfgs::minimize(vnl_vector<double>& x)
   vnl_vector<double> w(n * (2*m+1)+2*m);
 
   if (verbose_)
-    vcl_cerr << "ERROR fs_lbfgs: n = "
-             << n
-             <<", memory = "
-             << m
-             <<", Workspace = "
-             << w.size()
-             << "[ "
-             << ( w.size() / 128.0 / 1024.0)
-             <<" MB], ErrorScale = "
-             << f_->reported_error(1)
-             <<", xnorm = "
-             << x.magnitude()
-             << vcl_endl;
+    vcl_cerr << "vnl_lbfgs: n = "<< n <<", memory = "<< m <<", Workspace = "
+             << w.size() << "[ "<< ( w.size() / 128.0 / 1024.0) <<" MB], ErrorScale = "
+             << f_->reported_error(1) <<", xnorm = "<< x.magnitude() << vcl_endl;
 
   bool we_trace = (verbose_ && !trace);
-  
+
   if (we_trace)
-    vcl_cerr << "ERROR fs_lbfgs: ";
+    vcl_cerr << "vnl_lbfgs: ";
 
   double best_f = 0;
   vnl_vector<double> best_x;
@@ -90,29 +81,24 @@ bool fs_lbfgs::minimize(vnl_vector<double>& x)
   bool ok;
   this->num_evaluations_ = 0;
   this->num_iterations_ = 0;
-  int iflag = 0;
-  
+  long iflag = 0;
   while (true) {
-
-    // We do not wish to provide the diagonal
-    // matrices Hk0, and therefore set DIAGCO to FALSE.
-    logical diagco = false;
+    // We do not wish to provide the diagonal matrices Hk0, and therefore set DIAGCO to FALSE.
+    v3p_netlib_logical diagco = false;
 
     // Set these every iter in case user changes them to bail out
     double eps = gtol; // Gradient tolerance
     double local_xtol = 1e-16;
-    lb3_.gtol = line_search_accuracy; // set to 0.1 for huge
-                                      //problems or cheap functions
-    lb3_.stpawf = default_step_length;
+    lbfgs_global.gtol = line_search_accuracy; // set to 0.1 for huge problems or cheap functions
+    lbfgs_global.stpinit = default_step_length;
 
     // Call function
     double f;
     f_->compute(x, &f, &g);
     if (this->num_evaluations_ == 0) {
       this->start_error_ = f;
-      best_f = f;      
+      best_f = f;
     } else if (f < best_f) {
-      
       best_x = x;
       best_f = f;
             
@@ -123,45 +109,36 @@ bool fs_lbfgs::minimize(vnl_vector<double>& x)
            <<vcl_setw(20)<<b<<' '<<vcl_setw(20)<<c<<' '<<vcl_setw(20)<<d<<'\n'
 
     if (check_derivatives_)
+    {
+      vcl_cerr << "vnl_lbfgs: f = " << f_->reported_error(f) << ", computing FD gradient\n";
+      vnl_vector<double> fdg = f_->fdgradf(x);
+      if (verbose_)
       {
-        vcl_cerr << "ERROR fs_lbfgs: f = "
-                 << f_->reported_error(f)
-                 << ", computing FD gradient\n";
-        vnl_vector<double> fdg = f_->fdgradf(x);
-        if (verbose_)
-          {
-            int l = n;
-            int limit = 100;
-            int limit_tail = 10;
-            if (l > limit + limit_tail) {
-              vcl_cerr << " [ Showing only first "
-                       <<limit
-                       << " components ]\n";
-              l = limit;
-            }
-            print_("i","x","g","fdg","dg");
-            print_("-","-","-","---","--");
-            for (int i = 0; i < l; ++i)
-              print_(i, x[i], g[i], fdg[i], g[i]-fdg[i]);
-            if (n > limit) {
-              vcl_cerr << "   ...\n";
-              for (int i = n - limit_tail; i < n; ++i)
-                print_(i, x[i], g[i], fdg[i], g[i]-fdg[i]);
-            }
-          }
-        vcl_cerr << "   ERROR = "
-                 << (fdg - g).squared_magnitude() / vcl_sqrt(double(n))
-                 << "\n";
+        int l = n;
+        int limit = 100;
+        int limit_tail = 10;
+        if (l > limit + limit_tail) {
+          vcl_cerr << " [ Showing only first " <<limit<< " components ]\n";
+          l = limit;
+        }
+        print_("i","x","g","fdg","dg");
+        print_("-","-","-","---","--");
+        for (int i = 0; i < l; ++i)
+          print_(i, x[i], g[i], fdg[i], g[i]-fdg[i]);
+        if (n > limit) {
+          vcl_cerr << "   ...\n";
+          for (int i = n - limit_tail; i < n; ++i)
+            print_(i, x[i], g[i], fdg[i], g[i]-fdg[i]);
+        }
       }
+      vcl_cerr << "   ERROR = " << (fdg - g).squared_magnitude() / vcl_sqrt(double(n)) << "\n";
+    }
 
     iprint[0] = trace ? 1 : -1; // -1 no o/p, 0 start and end, 1 every iter.
     iprint[1] = 0; // 1 prints X and G
-
-    lbfgs_(&n, &m, x.data_block(),
-           &f, g.data_block(),
-           &diagco, diag.data_block(),
-           iprint, &eps, &local_xtol, w.data_block(), &iflag);
-
+    v3p_netlib_lbfgs_(
+      &n, &m, x.data_block(), &f, g.data_block(), &diagco, diag.data_block(),
+      iprint, &eps, &local_xtol, w.data_block(), &iflag, &lbfgs_global);
     ++this->num_iterations_;
 
     if (we_trace)
@@ -171,11 +148,7 @@ bool fs_lbfgs::minimize(vnl_vector<double>& x)
       // Successful return
       this->end_error_ = f;
       ok = true;
-
-      // this is an DJ addition for when the best_x is not initialized yet
-      if( best_x.size() != 0 ) {
-        x = best_x;
-      }
+      x = best_x;
       break;
     }
 
@@ -205,7 +178,6 @@ bool fs_lbfgs::minimize(vnl_vector<double>& x)
 
       break;
     }
-
   }
   if (we_trace) vcl_cerr << "done\n";
 
