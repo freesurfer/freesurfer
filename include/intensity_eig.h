36132f2ffa7f73c114411002ae514137ca872e0e
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2006-10-31 20:59:47 +0000

    eigensystem of spgr/mp-rage intensity covariance across structures.

---
+++ include/intensity_eig.h
@@ -0,0 +1,5 @@
+#define NUM_INT_EIG_LABELS 32
+extern int intensity_eig_labels[NUM_INT_EIG_LABELS] ;
+extern double intensity_eig_values[NUM_INT_EIG_LABELS] ;
+extern double intensity_eig_vectors[NUM_INT_EIG_LABELS][NUM_INT_EIG_LABELS] ;
+extern double intensity_means[NUM_INT_EIG_LABELS] ;
