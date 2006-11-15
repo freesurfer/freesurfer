baaf46d016f23de4d6c3ea23bbeb32d23646508c
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2006-11-15 02:07:06 +0000

    removed GPL from some of our scripts

--- scripts/progressbar.tcl
+++ scripts/progressbar.tcl
@@ -3,20 +3,6 @@
 #
 # @(#)progressbar.tcl v1.3 00/04/28 (c) 2000 Alexander Schoepe
 #
-# Progressbar is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-#
-# Progressbar is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Progressbar; see the file COPYING.  If not, write to
-# the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-#
 # Special thanks to Bryan Oakley his mclistbox was a very good example
 # how to build a widget - GREAT SOURCE!
 # Futhermore special thanks to Joerg Mehring who ask me just miniutes
--- scripts/unpack_ima.tcl
+++ scripts/unpack_ima.tcl
@@ -6,25 +6,20 @@
 #----------------------------------------------------------------#
 
 
-# this script looks at the headers of ima files in a predetermined archive directory. The default archive directory is over-ridden by the environment variable ARCHIVE_DIR. the user selects a session and the path to that session is proveded to the script that copies the relevant files via nfs to the local machine and unpacks them locally into b shorts.
+# this script looks at the headers of ima files in a predetermined 
+# archive directory. The default archive directory is over-ridden 
+# by the environment variable ARCHIVE_DIR. the user selects a session 
+# and the path to that session is proveded to the script that copies 
+# the relevant files via nfs to the local machine and unpacks them 
+# locally into b shorts.
 
 # Copyright (C) 2001 Tony Harris tony@nmr.mgh.harvard.edu
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# http://www.gnu.org/copyleft/gpl.html
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 
-#--------------------------------------------------------------------------------------#
+#----------------------------------------------------------------#
 #Structure of this script:
 
 #global variables
@@ -38,7 +33,7 @@
 #widgets
 #bindings
 #main
-#--------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------#
 
 # Alphabetical listing of functions (updated 07 Aug 2001) :
 
