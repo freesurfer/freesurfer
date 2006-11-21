4dae6d28f7acb193386518fecd20ce25b7a5d662
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-11-21 21:28:52 +0000

    Bias compensation for using residual acf

---
+++ fsfast/toolbox/fast_rfm2nrho1.m
@@ -0,0 +1,59 @@
+function [beta, rrho1, nrho1, nrho1hat] = fast_rfm2nrho1(R,nrho1)
+% [beta rrho1 nrho1 nrho1hat] = fast_rfm2nrho1(R,<nrho1>)
+%
+% Computes the correction factor to an AR1 value for the given
+% residual forming matrix R. Works by synthesizing a noise
+% covariance matrix for a series of true AR1 values (nrho1).
+% If nrho1 is not supplied, then uses nrho1 = [-.5:.25:.5].
+%
+% rrho1 are the expected AR1 values when computed from the 
+% residual for each of the true AR1 values nrho1.
+%
+% beta are the two best-fit linear components relating rrho1 and
+% nrho1, ie, nrho1hat = [1 rrho1]*beta, or equivalently nrho1hat =
+% beta(1) + rho1*beta(2). This is the way to correct for the bias
+% induced by estimating the AR1 from the resiual.
+%
+% This is sort of an empirical version of Keith's method (but a
+% little more accurate). The relationship between rrho1 and nrho1
+% is pretty linear over that range, at least for event-related
+% designs.
+% 
+% $Id: fast_rfm2nrho1.m,v 1.1 2006/11/21 21:28:52 greve Exp $
+
+beta = [];
+if(nargin ~= 1)
+  fprintf('[beta rrho1 nrho1 nrho1hat] = fast_rfm2nrho1(R,<nrho1>)\n');
+  return;
+end
+
+ntp = size(R,1);
+nn = [0:ntp-1];
+
+if(~exist('nrho1','var'))  nrho1 = [-.5:.25:.5]'; end
+nrho1 = nrho1(:);
+
+nthrho = 1;
+tic;
+for rho = nrho1'
+  acf = rho.^nn;
+  M = toeplitz(acf);
+  D = R*M*R;
+  rrho1(nthrho,1) = mean(diag(D,1));
+  %nrho1kjw = fast_yacf_kjw([1 rrho1(nthrho,1)],R);
+  fprintf('%2d  %g  %g    (t=%g)\n',nthrho,rho,rrho1(nthrho),toc);
+  nthrho = nthrho + 1;  
+end
+
+nlist = length(nrho1);
+
+X = [ones(nlist,1) rrho1];
+beta = inv(X'*X)*X'*nrho1;
+nrho1hat = X*beta;
+
+return;
+
+
+
+
+
