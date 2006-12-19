48ed618760d47f0d51a0c44ee6be24325823f706
Doug Greve <greve@nmr.mgh.harvard.edu>
2006-12-19 20:59:57 +0000

    Changed order, added mge te20 and te50

--- fsfast/toolbox/fbirn_sitelist.m
+++ fsfast/toolbox/fbirn_sitelist.m
@@ -1,18 +1,32 @@
 function sitelist = fbirn_sitelist
 % sitelist = fbirn_sitelist
+% changed order on 12/19/06
 
 sitelist = '';
-sitelist = strvcat(sitelist,'dunc');    % 1
-sitelist = strvcat(sitelist,'iowa');    % 2
-sitelist = strvcat(sitelist,'nm');      % 3
-sitelist = strvcat(sitelist,'uci');     % 4
-sitelist = strvcat(sitelist,'ucsd');    % 5
-sitelist = strvcat(sitelist,'bwh');     % 6
-
-sitelist = strvcat(sitelist,'mgh');     % 7
-sitelist = strvcat(sitelist,'min');     % 8
-sitelist = strvcat(sitelist,'stan');    % 9
-
-sitelist = strvcat(sitelist,'dunc4t');  % 10
+sitelist = strvcat(sitelist,'bwh');     % 1
+sitelist = strvcat(sitelist,'dunc');    % 2
+sitelist = strvcat(sitelist,'dunc4t');  % 3
+sitelist = strvcat(sitelist,'iowa');    % 4
+sitelist = strvcat(sitelist,'mgh');     % 5
+sitelist = strvcat(sitelist,'min');     % 6
+sitelist = strvcat(sitelist,'nm');      % 7
+sitelist = strvcat(sitelist,'stan');    % 8
+sitelist = strvcat(sitelist,'uci');     % 9
+sitelist = strvcat(sitelist,'ucsd');    % 10
+sitelist = strvcat(sitelist,'mgh-te20'); % 11
+sitelist = strvcat(sitelist,'mgh-te50'); % 12
 
 return
+
+% This was the original list
+% sitelist = '';
+% sitelist = strvcat(sitelist,'dunc');    % 1
+% sitelist = strvcat(sitelist,'iowa');    % 2
+% sitelist = strvcat(sitelist,'nm');      % 3
+% sitelist = strvcat(sitelist,'uci');     % 4
+% sitelist = strvcat(sitelist,'ucsd');    % 5
+% sitelist = strvcat(sitelist,'bwh');     % 6
+% sitelist = strvcat(sitelist,'mgh');     % 7
+% sitelist = strvcat(sitelist,'min');     % 8
+% sitelist = strvcat(sitelist,'stan');    % 9
+% sitelist = strvcat(sitelist,'dunc4t');  % 10
