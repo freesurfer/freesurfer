bf2b59aec0f3fe747d8a9924fe14a1b47dd14bf8
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-01-23 21:42:38 +0000

    Now uses 0777 mode when running mkdir()

--- mri_annotation2label/mri_annotation2label.c
+++ mri_annotation2label/mri_annotation2label.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:04 $
- *    $Revision: 1.11 $
+ *    $Author: greve $
+ *    $Date: 2007/01/23 21:42:38 $
+ *    $Revision: 1.12 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -28,7 +28,7 @@
 
 /*----------------------------------------------------------
   Name: mri_annotation2label.c
-  $Id: mri_annotation2label.c,v 1.11 2006/12/29 02:09:04 nicks Exp $
+  $Id: mri_annotation2label.c,v 1.12 2007/01/23 21:42:38 greve Exp $
   Author: Douglas Greve
   Purpose: Converts an annotation to a labels.
 
@@ -62,7 +62,7 @@ static int  singledash(char *flag);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_annotation2label.c,v 1.11 2006/12/29 02:09:04 nicks Exp $";
+static char vcid[] = "$Id: mri_annotation2label.c,v 1.12 2007/01/23 21:42:38 greve Exp $";
 char *Progname = NULL;
 
 char  *subject   = NULL;
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
   int nargs;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_annotation2label.c,v 1.11 2006/12/29 02:09:04 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_annotation2label.c,v 1.12 2007/01/23 21:42:38 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -114,7 +114,7 @@ int main(int argc, char **argv) {
 
   if (outdir != NULL) {
     // Create the output directory
-    err = mkdir(outdir,(mode_t)-1);
+    err = mkdir(outdir,0777);
     if (err != 0 && errno != EEXIST) {
       printf("ERROR: creating directory %s\n",outdir);
       perror(NULL);
