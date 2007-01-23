5834bbe02a926189a4b8f9a5278258df01826271
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2007-01-23 01:33:41 +0000

    added mri_interpolate
    fixed mris_wm_volume problem.
    
    program for interpolating zero locations with a voronoi diagram and
    soap bubble smoothing.

---
+++ mri_interpolate/mri_interpolate.c
@@ -0,0 +1,176 @@
+/**
+ * @file  mri_interpolate.c
+ * @brief REPLACE_WITH_ONE_LINE_SHORT_DESCRIPTION
+ *
+ * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
+ */
+/*
+ * Original Author: Bruce Fischl
+ * CVS Revision Info:
+ *    $Author: fischl $
+ *    $Date: 2007/01/23 01:33:41 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright (C) 2002-2007,
+ * The General Hospital Corporation (Boston, MA). 
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+ *
+ */
+
+
+//
+// mri_interpolate
+// written by Bruce Fischl
+//
+// Warning: Do not edit the following four lines.  CVS maintains them.
+// Revision Author: $Author: fischl $
+// Revision Date  : $Date: 2007/01/23 01:33:41 $
+// Revision       : $Revision: 1.1 $
+//
+////////////////////////////////////////////////////////////////////
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "mri.h"
+#include "matrix.h"
+#include "proto.h"
+#include "macros.h"
+#include "error.h"
+#include "timer.h"
+#include "diag.h"
+#include "mrimorph.h"
+#include "utils.h"
+#include "gca.h"
+#include "tags.h"
+#include "cma.h"
+#include "mrinorm.h"
+#include "version.h"
+
+char *Progname ;
+
+static void usage_exit(int code) ;
+static int get_option(int argc, char *argv[]) ;
+
+static int navgs = 50 ;
+
+/*
+   command line consists of three inputs:
+
+   argv[1]  - input volume
+   argv[2]  - output volume
+*/
+
+
+int
+main(int argc, char *argv[]) {
+  char         *in_fname, *out_fname, **av ;
+  MRI          *mri_in, *mri_ctrl, *mri_out;
+  int          ac, nargs,x, y,z ;
+  char         cmdline[CMD_LINE_LEN] ;
+  Real         val;
+
+  make_cmd_version_string(argc, argv,
+                          "$Id: mri_interpolate.c,v 1.1 2007/01/23 01:33:41 fischl Exp $",
+                          "$Name:  $",cmdline);
+  if (nargs && argc - nargs == 1)
+    exit (0);
+  argc -= nargs;
+
+  setRandomSeed(-1L) ;
+  Progname = argv[0] ;
+
+  DiagInit(NULL, NULL, NULL) ;
+  ErrorInit(NULL, NULL, NULL) ;
+
+  ac = argc ;
+  av = argv ;
+  for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++) {
+    nargs = get_option(argc, argv) ;
+    argc -= nargs ;
+    argv += nargs ;
+  }
+
+  if (argc < 2)
+    ErrorExit
+    (ERROR_BADPARM,
+     "usage: %s [<options>] <in volume> <out volume>\n", Progname) ;
+
+  in_fname = argv[1] ;
+  out_fname = argv[2] ;
+
+  MRIaddCommandLine(mri_in, cmdline) ;
+
+  mri_ctrl = MRIclone(mri_in,NULL);
+  for (x = 0 ; x < mri_in->width; x++)
+    for (y = 0 ; y < mri_in->height; y++)
+      for (z = 0 ; z < mri_in->depth; z++)
+      {
+        val = MRIgetVoxVal(mri_in, x, y, z, 0);
+        if (!FZERO(val))
+          MRIsetVoxVal(mri_ctrl,x,y,z, 0, CONTROL_MARKED);
+      }
+  mri_out = MRIbuildVoronoiDiagram(mri_in, mri_ctrl, NULL) ;
+  MRIsoapBubble(mri_out, mri_ctrl, mri_out, navgs) ;
+  MRIwrite(mri_out,out_fname);
+  exit(0) ;
+  return(0) ;
+}
+
+
+/*----------------------------------------------------------------------
+  Parameters:
+
+  Description:
+  ----------------------------------------------------------------------*/
+static int
+get_option(int argc, char *argv[]) {
+  int  nargs = 0 ;
+  char *option ;
+
+  option = argv[1] + 1 ;            /* past '-' */
+  StrUpper(option) ;
+  if (!strcmp(option, "DEBUG_VOXEL")) {
+    Gx = atoi(argv[2]) ;
+    Gy = atoi(argv[3]) ;
+    Gz = atoi(argv[4]) ;
+    nargs = 3 ;
+    printf("debugging voxel (%d, %d, %d)\n", Gx, Gy, Gz) ;
+  } else switch (*option) {
+  case 'A':
+    navgs = atoi(argv[2]);
+    nargs = 1;
+    printf("using %d soap bubble averages\n",navgs);
+    break;
+  case 'V':
+    Gdiag_no = atoi(argv[2]) ;
+    nargs = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    usage_exit(0) ;
+    break ;
+  default:
+    printf("unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
+  return(nargs) ;
+}
+
+static void
+usage_exit(int code) {
+  printf("usage: %s <in volume> <out volume>\n",Progname) ;
+  exit(code) ;
+}
