9c19fab92c54518366c8bd36e10c8238b5794b68
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-02-02 04:53:40 +0000

    Converts spatial ar1 to fwhm

---
+++ fsfast/toolbox/fast_ar12fwhm.m
@@ -0,0 +1,36 @@
+function fwhm = fast_ar12fwhm(ar1,d)
+% fwhm = fast_ar12fwhm(ar1,d)
+%
+% Converts an AR1 to FWHM. 
+% d is the voxel size. FWHM will be in units of d.
+%
+% $Id: fast_ar12fwhm.m,v 1.1 2007/02/02 04:53:40 greve Exp $
+
+% Original Author: Doug Greve
+% CVS Revision Info:
+%    $Author: greve $
+%    $Date: 2007/02/02 04:53:40 $
+%    $Revision: 1.1 $
+%
+% Copyright (C) 2002-2007,
+% The General Hospital Corporation (Boston, MA). 
+% All rights reserved.
+%
+% Distribution, usage and copying of this software is covered under the
+% terms found in the License Agreement file named 'COPYING' found in the
+% FreeSurfer source code root directory, and duplicated here:
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+%
+% General inquiries: freesurfer@nmr.mgh.harvard.edu
+% Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+%
+
+ar1 = [];
+if(nargin ~= 1 & nargin ~= 2)
+  fprintf('ar1 = fast_ar12fwhm(ar1,d)\n');
+  return;
+end
+
+fwhm = sqrt(log(256.0))*d./sqrt(-4*log(ar1));
+
+return
