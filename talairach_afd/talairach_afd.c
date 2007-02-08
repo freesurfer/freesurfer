a59c7a73c412e67d4e57cb738f1a7972d27e7f52
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2007-02-08 18:32:28 +0000

    simple cleanup; dont need myown.c

--- talairach_afd/talairach_afd.c
+++ talairach_afd/talairach_afd.c
@@ -1,3 +1,32 @@
+/**
+ * @file  talairach_afd.c
+ * @brief Automatically detect Talairach alignment failures.
+ *
+ * Detect bad Talairach alignment by checking subjects Talairach transform
+ * against a range of known good values.
+ * See: dev/docs/Automatic_Failure_Detection.doc
+ */
+/*
+ * Original Author: Laurence Wastiaux
+ * CVS Revision Info:
+ *    $Author: nicks $
+ *    $Date: 2007/02/08 18:32:28 $
+ *    $Revision: 1.2 $
+ *
+ * Copyright (C) 2002-2007,
+ * The General Hospital Corporation (Boston, MA).
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -19,7 +48,8 @@
 
 //#define VERBOSE 1
 
-static char vcid[] = "$Id: talairach_afd.c,v 1.1 2007/02/08 17:05:21 wastiaux Exp $";
+static char vcid[] =
+"$Id: talairach_afd.c,v 1.2 2007/02/08 18:32:28 nicks Exp $";
 static int get_option(int argc, char *argv[]) ;
 static void usage(int exit_value) ;
 static char *subject_name = NULL;
@@ -58,7 +88,10 @@ main(int argc, char *argv[])
   txfm=NULL;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: talairach_afd.c,v 1.1 2007/02/08 17:05:21 wastiaux Exp $", "$Name:  $");
+  nargs = handle_version_option
+    (argc, argv,
+     "$Id: talairach_afd.c,v 1.2 2007/02/08 18:32:28 nicks Exp $",
+     "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -80,7 +113,7 @@ main(int argc, char *argv[])
 
   if (argc < 1)
     usage(1) ;
-  if (argc ==1 && threshold == -1e10){
+  if (argc ==1 && threshold == -1e10) {
     printf("default p-values threshold at 0.01\n");
     threshold=0.01;
   }
@@ -90,7 +123,7 @@ main(int argc, char *argv[])
   }
 
   fsenv=getenv("FREESURFER_HOME");
-  if(!fsenv){
+  if(!fsenv) {
     printf("FREESURFER_HOME not defined\n");
     exit(1);
   }
@@ -101,7 +134,6 @@ main(int argc, char *argv[])
   sprintf(xfm, "%s/mri/transforms/talairach.xfm", subject_name);
   sprintf(probasf, "%s/TalairachingProbas.adf", fsafd);
   sname=fio_basename(subject_name, NULL);
-
 
   /*----- load 1x9 mean vector computed from training set -----*/
   mu = ReadMeanVect(tmf);
@@ -114,13 +146,13 @@ main(int argc, char *argv[])
 
   h = HISTObins(21, 0,1);
   HISTOcount(h, ts_probas, nsamples);
-  
+
   for (b = 0 ; b < h->nbins ; b++)
     h->counts[b] = (float)h->counts[b]/(float)nsamples ;
-  
+
   /*---Load Talairach.xfm---*/
   txfm = Load_xfm(xfm);
-  
+
   /*--- compute mvnpdf ---*/
   p = mvnpdf(txfm, mu, sigma);
 
@@ -135,33 +167,31 @@ main(int argc, char *argv[])
       printf("Talairach Transform: %s OK (p=%f, pval=%f)\n",sname, p, pval);
   }
 
-
-
 #ifdef VERBOSE
   int i,j;
- printf("FREESURFER_HOME=%s\n", fsenv);
- printf("fsafdDir=%s\n", fsafd);
- 
- printf("mu:\n");
- for (i=1;i<=9;i++){
-   printf("%f  ", mu->rptr[1][i]);
- }
- printf("\n\n");
- printf("sigma:\n");
- for(i=1;i<=9;i++){
-   for(j=1;j<=9;j++){
-     printf("%f  ", sigma->rptr[i][j]);
-   }
-   printf("\n");
- }
- printf("\n\n");
- printf("xfm:\n");
- for(i=1;i<=9;i++){
-   printf("%f  ", txfm->rptr[1][i]);
- }
- printf("\n\n");
+  printf("FREESURFER_HOME=%s\n", fsenv);
+  printf("fsafdDir=%s\n", fsafd);
 
- printf("proba = %f\n", p);
+  printf("mu:\n");
+  for (i=1;i<=9;i++){
+    printf("%f  ", mu->rptr[1][i]);
+  }
+  printf("\n\n");
+  printf("sigma:\n");
+  for(i=1;i<=9;i++){
+    for(j=1;j<=9;j++){
+      printf("%f  ", sigma->rptr[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+  printf("xfm:\n");
+  for(i=1;i<=9;i++){
+    printf("%f  ", txfm->rptr[1][i]);
+  }
+  printf("\n\n");
+
+  printf("proba = %f\n", p);
 
 #endif
 
@@ -171,16 +201,18 @@ main(int argc, char *argv[])
   seconds = seconds % 60 ;
 #ifdef VERBOSE
   fprintf(stderr, "Talairach failure detection took %d minutes"
-					" and %d seconds.\n", minutes, seconds) ;
+          " and %d seconds.\n", minutes, seconds) ;
 #endif
   exit(0) ;
   return(0) ;
 }
-/*----------------------------------------------------------------------
-            Parameters:
 
-           Description:
-----------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------
+  Parameters:
+
+  Description:
+  ----------------------------------------------------------------------*/
 static void print_version(void)
 {
   fprintf(stdout, "%s\n", vcid) ;
@@ -191,53 +223,53 @@ static int
 get_option(int argc, char *argv[])
 {
   int  nargs = 0 ;
-  char *option; 
-  
+  char *option;
+
   option = argv[1] + 1 ;            /* past '-' */
   if (!stricmp(option, "help"))
     usage(1) ;
   else if (!stricmp(option, "version"))
     print_version() ;
   if (!stricmp(option, "subj"))
-    {
-      subject_name = argv[2];
-      nargs = 1;
-      if(!flag_failed)
-	fprintf(stderr, "Subject: %s\n", subject_name);
-    }
-  else if (!stricmp(option, "T")
-           || !stricmp(option, "threshold")
-           ){
+  {
+    subject_name = argv[2];
+    nargs = 1;
+    if(!flag_failed)
+      fprintf(stderr, "Subject: %s\n", subject_name);
+  }
+  else if (!stricmp(option, "T") || !stricmp(option, "threshold")){
     threshold = (float)atof(argv[2]);
     nargs = 1;
     if(!flag_failed)
       fprintf(stderr, "Threshold p-values at %g\n", threshold);
   }
   else if (!stricmp(option, "F"))
-    {
-      flag_failed = (float)atof(argv[2]);
-      nargs = 1;
-    }
+  {
+    flag_failed = (float)atof(argv[2]);
+    nargs = 1;
+  }
   else switch (toupper(*option))
-    {
-    case 'T':
-    case '?':
-    case 'U':
-      usage(0) ;
-      break ;
-    default:
-      fprintf(stderr, "unknown option %s\n", argv[1]) ;
-      exit(1) ;
-      break ;
-    }
-  
+  {
+  case 'T':
+  case '?':
+  case 'U':
+    usage(0) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
+
   return(nargs) ;
 }
-/*----------------------------------------------------------------------
-            Parameters:
 
-           Description:
-----------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------
+  Parameters:
+
+  Description:
+  ----------------------------------------------------------------------*/
 static void
 usage(int exit_value)
 {
@@ -245,31 +277,34 @@ usage(int exit_value)
 
   fout = (exit_value ? stderr : stdout);
 
-  fprintf(fout, 
-          "Usage: %s [options] <subject_name> <p-value threshold>\n", 
+  fprintf(fout,
+          "Usage: %s [options] <subject_name> <p-value threshold>\n",
           Progname);
-  fprintf(fout, 
+  fprintf(fout,
           "This program detects talairaching failures.\n") ;
 
   fprintf(fout, "Options:\n") ;
-  fprintf(fout, 
+  fprintf(fout,
           "   -subj %%s    subject's name \n");
-  fprintf(fout, 
+  fprintf(fout,
           "   -T #        threshold the p-values at # "
-          "(i.e., Talairach transforms for subjects with p-values <= T are considered as very unlikely) \n");
-  fprintf(fout, 
-          "   -F 0/1      if 1, a message will appear only if subject has an unlikely talairach transform\n");
+          "(i.e., Talairach transforms for subjects with p-values "
+          "<= T are considered as very unlikely) \n");
+  fprintf(fout,
+          "   -F 0/1      if 1, a message will appear only if subject "
+          "has an unlikely talairach transform\n");
   fprintf(fout, "\n");
-  
+
   exit(exit_value) ;
 }
 
-/*----------------------------------------------------------------------
-            Parameters:char *fname 
 
-           Description:reads the 9 componants of Mean Vector mu 
-	   from "$FREESURFER_HOME/fsafd/TalairachingMean.adf"
-----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+  Parameters:char *fname
+
+  Description:reads the 9 componants of Mean Vector mu
+  from "$FREESURFER_HOME/fsafd/TalairachingMean.adf"
+  ----------------------------------------------------------------------*/
 VECTOR *
 ReadMeanVect(char *fname)
 {
@@ -277,8 +312,7 @@ ReadMeanVect(char *fname)
   char tag[1000], tmpstr[1000];
   int nth, r;
   VECTOR *v;
-  
-  
+
   v = MatrixAlloc(1,9, MATRIX_REAL) ;
 
   fp = fopen(fname,"r");
@@ -286,16 +320,17 @@ ReadMeanVect(char *fname)
     printf("ERROR: main(): could not open %s\n",fname);
     exit(1);
   }
-  
+
   nth=1;
   while(1) {
     r = fscanf(fp,"%s",tag);
     if(r==EOF) break;
-    if(!strcmp(tag,"#")){  // line starts with #: header info -> skip the whole line
+    if(!strcmp(tag,"#")){  // line starts with #:
+                           // header info -> skip the whole line
       fgets(tmpstr,1000,fp);
     }
     else{
-      v->rptr[1][nth]=(float)atof(tag); 
+      v->rptr[1][nth]=(float)atof(tag);
       nth++;
     }
   }
@@ -303,17 +338,18 @@ ReadMeanVect(char *fname)
     printf("ReadMeanVect():9 componants could not be loaded\n");
     exit(1);
   }
-  
+
   fclose(fp);
   return(v);
 }
 
-/*----------------------------------------------------------------------
-            Parameters:char *fname 
 
-           Description:reads the 9x9 componants of covariance matrix sigma[9][9] 
-	   from "$FREESURFER_HOME/fsafd/TalairachingCovariance.adf"
-----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+  Parameters:char *fname
+
+  Description:reads the 9x9 componants of covariance matrix sigma[9][9]
+  from "$FREESURFER_HOME/fsafd/TalairachingCovariance.adf"
+  ----------------------------------------------------------------------*/
 MATRIX *
 ReadCovMat(char *fname)
 {
@@ -321,26 +357,27 @@ ReadCovMat(char *fname)
   char tag[1000], tmpstr[1000];
   int nth, r, i, j;
   MATRIX *s;
-  
+
   s = MatrixAlloc(9,9, MATRIX_REAL) ;
-  
+
   fp = fopen(fname,"r");
   if(fp == NULL){
     printf("ERROR: main(): could not open %s\n",fname);
     exit(1);
   }
-  
+
   nth=0;
   while(1) {
     r = fscanf(fp,"%s",tag);
     if(r==EOF) break;
-    if(!strcmp(tag,"#")){  // line starts with #: header info -> skip the whole line
+    if(!strcmp(tag,"#")){  // line starts with #:
+                           // header info -> skip the whole line
       fgets(tmpstr,1000,fp);
     }
     else{
       i=nth/9;
       j=nth-(i*9);
-      s->rptr[i+1][j+1]=(float)atof(tag); 
+      s->rptr[i+1][j+1]=(float)atof(tag);
       nth++;
     }
   }
@@ -348,30 +385,30 @@ ReadCovMat(char *fname)
     printf("ReadCovMAt():9x9 componants could not be loaded\n");
     exit(1);
   }
-  
+
   fclose(fp);
   return(s);
 }
 
-/*----------------------------------------------------------------------
-            Parameters:char *fname 
 
-           Description:Load xfm matrix
-----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+  Parameters:char *fname
+
+  Description:Load xfm matrix
+  ----------------------------------------------------------------------*/
 VECTOR *
 Load_xfm(char *fname)
 {
   FILE *fp;
   char *sdir;
-  char line[MAX_LINE_LEN], tmp_name[1000]; 
+  char line[MAX_LINE_LEN], tmp_name[1000];
   char tag[1000];
   int nth, r, i, j;
   float M[3][4];
   VECTOR *v;
- 
-  
+
   v = MatrixAlloc(1,9, MATRIX_REAL) ;
-   
+
   fp = fopen(fname,"r");
   if(!fp){
     // try to find subject in SUBJECTS_DIR
@@ -380,12 +417,13 @@ Load_xfm(char *fname)
       sprintf(tmp_name,"%s/%s", sdir, fname);
       fp = fopen(tmp_name,"r");
       if(!fp){
-	printf("ERROR: main(): could not open %s\n",fname);
-	exit(1);
+        printf("ERROR: main(): could not open %s\n",fname);
+        exit(1);
       }
     }
     else{
-      printf("ERROR: main(): SUBJECTS_DIR is not defined,  could not open %s\n",fname);
+      printf("ERROR: main(): SUBJECTS_DIR is not defined, "
+             "could not open %s\n",fname);
       exit(1);
     }
   }
@@ -396,11 +434,11 @@ Load_xfm(char *fname)
   }
   nth=0;
   while(1) {
-    r = fscanf(fp,"%s",tag); 
+    r = fscanf(fp,"%s",tag);
     if(r==EOF) break;
     i=nth/4;
     j=nth-(i*4);
-    M[i][j]=(float)atof(tag); 
+    M[i][j]=(float)atof(tag);
     nth++;
   }
   if(nth!=12){
@@ -412,16 +450,18 @@ Load_xfm(char *fname)
       v->rptr[1][i*3+j+1]=M[i][j];
     }
   }
+
   fclose(fp);
   return(v);
 }
 
-/*----------------------------------------------------------------------
-            Parameters:char *fname 
 
-           Description:Load probas computed from the training data set
-	   and contained in probas table 
-----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+  Parameters:char *fname
+
+  Description:Load probas computed from the training data set
+  and contained in probas table
+  ----------------------------------------------------------------------*/
 double *
 LoadProbas(char *fname, int *nb_samples)
 {
@@ -429,27 +469,28 @@ LoadProbas(char *fname, int *nb_samples)
   char tag[1000], tmpstr[1000], c[1000];
   int  nth, r,  len;
   double *p=NULL;
-  
+
   fp = fopen(fname,"r");
   if(fp == NULL){
     printf("ERROR: main(): could not open %s\n",fname);
     exit(1);
   }
-  
+
   nth=0;
 
   while(1) {
     r = fscanf(fp,"%s",tag);
     if(r==EOF) break;
-    if(!strcmp(tag,"#")){  // line starts with #: header info -> skip the whole line
-      fgets(tmpstr,1000,fp);     	
-      if(strcmp(tmpstr, " nrows")>0){	
-	sscanf(tmpstr, "%s %d", c, &len);
-	break;
+    if(!strcmp(tag,"#")){  // line starts with #:
+                           // header info -> skip the whole line
+      fgets(tmpstr,1000,fp);
+      if(strcmp(tmpstr, " nrows")>0){
+        sscanf(tmpstr, "%s %d", c, &len);
+        break;
       }
     }
   }
- 
+
   p=(double *)calloc(len, sizeof(double));
   if(!p){
     printf("LoadProbas(): 1-could not allocate memory for probas vector\n");
@@ -465,31 +506,32 @@ LoadProbas(char *fname, int *nb_samples)
   while(1) {
     r = fscanf(fp,"%s",tag);
     if(r==EOF) break;
-    if(!strcmp(tag,"#")){  // line starts with #: header info -> skip the whole line
-      fgets(tmpstr,1000,fp);  
+    if(!strcmp(tag,"#")){  // line starts with #:
+                           // header info -> skip the whole line
+      fgets(tmpstr,1000,fp);
     }
     else{
-      p[nth] = (double)atof(tag); 
+      p[nth] = (double)atof(tag);
       nth++;
     }
   }
 
   if(nth!=len){
-    printf("LoadProbas(): %d componants could not be loaded\n", len); 
+    printf("LoadProbas(): %d componants could not be loaded\n", len);
     exit(1);
   }
   *(nb_samples)=len;
-  
+
   fclose(fp);
   return(p);
 
 }
 
 /*----------------------------------------------------------------------
-            Parameters:vector t, mean vector mu, covariance matrix sigma 
+  Parameters:vector t, mean vector mu, covariance matrix sigma
 
-           Description:computes p = mvnpdf(t, mu, sigma)
-----------------------------------------------------------------------*/
+  Description:computes p = mvnpdf(t, mu, sigma)
+  ----------------------------------------------------------------------*/
 
 float
 mvnpdf(VECTOR *t, VECTOR *v_mu, MATRIX *m_sigma)
@@ -505,7 +547,7 @@ mvnpdf(VECTOR *t, VECTOR *v_mu, MATRIX *m_sigma)
   }
 
   d = t->cols;
- 
+
   sub = VectorSubtract(t,v_mu,NULL);
   m_sinv = MatrixInverse(m_sigma, NULL);
   t_sub = VectorTranspose(sub, NULL);
@@ -522,12 +564,12 @@ mvnpdf(VECTOR *t, VECTOR *v_mu, MATRIX *m_sigma)
   return(num);
 }
 
+
 /*----------------------------------------------------------------------
-            Parameters: 
+  Parameters:
 
-           Description:computes area under the curve for b<=nbin
-----------------------------------------------------------------------*/
-
+  Description:computes area under the curve for b<=nbin
+  ----------------------------------------------------------------------*/
 float
 ComputeArea(HISTO *h, int nbin)
 {
@@ -539,8 +581,6 @@ ComputeArea(HISTO *h, int nbin)
     a = (h->counts[i] + h->counts[i-1]) /2;
     s += a;
   }
-  
+
   return(s);
 }
-
-  
