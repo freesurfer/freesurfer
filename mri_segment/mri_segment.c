35d5ef94070f63bf9e4c98204eefb64aba4d9cad
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2007-02-14 19:59:27 +0000

    don't allow pathological values for gray_hi

--- mri_segment/mri_segment.c
+++ mri_segment/mri_segment.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:08 $
- *    $Revision: 1.32 $
+ *    $Author: fischl $
+ *    $Date: 2007/02/14 19:59:27 $
+ *    $Revision: 1.33 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -32,13 +32,13 @@
 //
 // Warning: Do not edit the following four lines.  CVS maintains them.
 //
-// ID             : $Id: mri_segment.c,v 1.32 2006/12/29 02:09:08 nicks Exp $
-// Revision Author: $Author: nicks $
-// Revision Date  : $Date: 2006/12/29 02:09:08 $
-// Revision       : $Revision: 1.32 $
+// ID             : $Id: mri_segment.c,v 1.33 2007/02/14 19:59:27 fischl Exp $
+// Revision Author: $Author: fischl $
+// Revision Date  : $Date: 2007/02/14 19:59:27 $
+// Revision       : $Revision: 1.33 $
 //
 ////////////////////////////////////////////////////////////////////
-char *MRI_SEGMENT_VERSION = "$Revision: 1.32 $";
+char *MRI_SEGMENT_VERSION = "$Revision: 1.33 $";
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -131,7 +131,7 @@ main(int argc, char *argv[]) {
   TAGmakeCommandLineString(argc, argv, cmdline) ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_segment.c,v 1.32 2006/12/29 02:09:08 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_segment.c,v 1.33 2007/02/14 19:59:27 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -207,9 +207,9 @@ main(int argc, char *argv[]) {
 
     if (!gray_hi_set) {
       gray_hi = gray_mean + 2*gray_sigma ;
-#if 0
-      if (gray_hi >= white_mean-white_sigma)
-        gray_hi = white_mean-white_sigma ;
+#if 1
+      if (gray_hi >= white_mean)
+        gray_hi = white_mean-1 ;
 #endif
     }
     fprintf(stderr, "setting bottom of white matter range to %2.1f\n",wm_low);
