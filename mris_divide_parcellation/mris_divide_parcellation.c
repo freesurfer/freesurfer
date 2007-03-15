51e1f9c19b5837914c52f3b586bf06fb89ecefcf
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-03-15 19:25:08 +0000

    Added docs

--- mris_divide_parcellation/mris_divide_parcellation.c
+++ mris_divide_parcellation/mris_divide_parcellation.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:10 $
- *    $Revision: 1.6 $
+ *    $Author: greve $
+ *    $Date: 2007/03/15 19:25:08 $
+ *    $Revision: 1.7 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -28,12 +28,84 @@
 
 
 /*!
-\author Douglas Greve
+\author Bruce Fischl
 
 */
 
+/*
+BEGINUSAGE --------------------------------------------------------------
 
-// $Id: mris_divide_parcellation.c,v 1.6 2006/12/29 02:09:10 nicks Exp $
+mris_divide_parcellation [options] subject hemi sourceannot [splitfile|areathresh] outannot
+
+ENDUSAGE --------------------------------------------------------------
+
+BEGINHELP --------------------------------------------------------------
+
+This program divides one or more parcellations into divisions
+perpendicular to the long axis of the label.  The number of divisions
+can be specified in one of two ways, depending upon the nature of the
+fourth argument.
+
+First, a splitfile can be specified as the fourth argument. The
+splitfile is a text file with two columns. The first column is the
+name of the label in the source annotation, and the second column is
+the number of units that label should be divided into. The names of
+the labels depends upon the source parcellation.  For aparc.annot and
+aparc.a2005.annot, the names can be found in
+$FREESURFER_HOME/FreeSurferColorLUT.txt.  For aparc.annot, the labels
+are between the ranges of 1000-1034.  For aparc.a2005s.annot, the
+labels are between the ranges of 1100-1181.  The name for the label is
+the name of the segmentation without the 'ctx-lh'. Note that the name
+included in the splitfile does not indicate the hemisphere. For
+example, 1023 is 'ctx-lh-posteriorcingulate'.  You should put
+'posteriorcingulate' in the splitfile. Eg, to divide it into three
+segments, the following line should appear in the splitfile:
+
+posteriorcingulate 3
+
+Only labels that should be split need be specified in the splitfile.
+
+The second method is to specify an area threshold (in mm^2) as the
+fourth argument, in which case each label is divided until each
+subdivision is smaller than the area threshold.
+
+The output label name will be the original name with _divN appended,
+where N is the division number. N will go from 2 to the number of
+divisions. The first division has the same name as the original label.
+
+
+EXAMPLE:
+
+cd $SUBJECTS_DIR/mysubj001/label
+
+Create a split file to divide the superior frontal gyrus into 4
+segements and the precentral gyrus into 3 segments:
+
+echo superiorfrontal 4 >  splittable.txt
+echo precentral      3 >> splittable.txt
+
+Run program:
+
+mris_divide_parcellation mysubj001 rh aparc.annot splittable.txt rh.aparc.split-sfg+pc
+
+This reads in rh.aparc.annot, splits the SFG and PC, and and saves the
+result to rh.aparc.split-sfg+pc
+
+View
+
+tksurfer mysubj001 rh inflated -annot aparc.split-sfg+pc
+
+The SFG divisions will have the following names: superiorfrontal,
+superiorfrontal_div2, superiorfrontal_div3, superiorfrontal_div4. The
+PC divisions will be precentral, precentral_div2, precentral_div3.
+
+
+
+ENDHELP ----------------------------------------------------------------
+
+*/
+
+// $Id: mris_divide_parcellation.c,v 1.7 2007/03/15 19:25:08 greve Exp $
 
 
 
@@ -63,7 +135,7 @@ static void print_help(void) ;
 static void print_version(void) ;
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mris_divide_parcellation.c,v 1.6 2006/12/29 02:09:10 nicks Exp $";
+static char vcid[] = "$Id: mris_divide_parcellation.c,v 1.7 2007/03/15 19:25:08 greve Exp $";
 char *Progname = NULL;
 
 static char sdir[STRLEN] = "" ;
@@ -81,8 +153,7 @@ int main(int argc, char *argv[]) {
   float       area_thresh ;
 
   nargs = handle_version_option (argc, argv, vcid, "$Name:  $");
-  if (nargs && argc - nargs == 1)
-    exit (0);
+  if (nargs && argc - nargs == 1)  exit (0);
   argc -= nargs;
 
   ErrorInit(NULL, NULL, NULL) ;
@@ -97,8 +168,7 @@ int main(int argc, char *argv[]) {
     argv += nargs ;
   }
 
-  if (argc < 6)
-    usage_exit() ;
+  if (argc < 6)  usage_exit() ;
   subject = argv[1] ;
   hemi = argv[2] ;
   annot_name = argv[3] ;
@@ -213,27 +283,85 @@ get_option(int argc, char *argv[]) {
 static void
 usage_exit(void) {
   print_usage() ;
-  print_help() ;
   exit(1) ;
 }
 
 static void
 print_usage(void) {
-  printf("%s [options] <subject> <hemi> <input annot> <area> <output annot>\n",Progname) ;
+  printf("\n");
+  printf("mris_divide_parcellation [options] subject hemi sourceannot [splitfile|areathresh] outannot\n");
+  printf("\n");
   printf("\n");
   printf("options\n");
+  printf( "  -help\n");
   printf( "  -scale <scale>   specify offset scaling for rgb values (default=20)\n");
-  printf( "  -l     <label name>  only process the label <label name> (not implemented yet)\n");
+  printf( "  -l <label name>  only process the label <label name> (not implemented yet)\n");
 }
 
 static void
 print_help(void) {
   print_usage() ;
-  printf("\nThis program divides a parcellation either into a specified #\n"
-         "of units or until all units are below an area threshold\n") ;
-  printf("If <area thresh> is non-numeric, it is interpreted as a file name.\n"
-         "The file is parsed for <name> <num> pairs that specify how many\ndivisions each parcellation "
-         "unit should be split into \n(only units that should be split need be specified.\n") ;
+printf("\n");
+printf("This program divides one or more parcellations into divisions\n");
+printf("perpendicular to the long axis of the label.  The number of divisions\n");
+printf("can be specified in one of two ways, depending upon the nature of the\n");
+printf("fourth argument.\n");
+printf("\n");
+printf("First, a splitfile can be specified as the fourth argument. The\n");
+printf("splitfile is a text file with two columns. The first column is the\n");
+printf("name of the label in the source annotation, and the second column is\n");
+printf("the number of units that label should be divided into. The names of\n");
+printf("the labels depends upon the source parcellation.  For aparc.annot and\n");
+printf("aparc.a2005.annot, the names can be found in\n");
+printf("$FREESURFER_HOME/FreeSurferColorLUT.txt.  For aparc.annot, the labels\n");
+printf("are between the ranges of 1000-1034.  For aparc.a2005s.annot, the\n");
+printf("labels are between the ranges of 1100-1181.  The name for the label is\n");
+printf("the name of the segmentation without the 'ctx-lh'. Note that the name\n");
+printf("included in the splitfile does not indicate the hemisphere. For\n");
+printf("example, 1023 is 'ctx-lh-posteriorcingulate'.  You should put\n");
+printf("'posteriorcingulate' in the splitfile. Eg, to divide it into three\n");
+printf("segments, the following line should appear in the splitfile:\n");
+printf("\n");
+printf("posteriorcingulate 3\n");
+printf("\n");
+printf("Only labels that should be split need be specified in the splitfile.\n");
+printf("\n");
+printf("The second method is to specify an area threshold (in mm^2) as the\n");
+printf("fourth argument, in which case each label is divided until each\n");
+printf("subdivision is smaller than the area threshold.\n");
+printf("\n");
+printf("The output label name will be the original name with _divN appended,\n");
+printf("where N is the division number. N will go from 2 to the number of\n");
+printf("divisions. The first division has the same name as the original label.\n");
+printf("\n");
+printf("\n");
+printf("EXAMPLE:\n");
+printf("\n");
+printf("cd $SUBJECTS_DIR/mysubj001/label\n");
+printf("\n");
+printf("Create a split file to divide the superior frontal gyrus into 4\n");
+printf("segements and the precentral gyrus into 3 segments:\n");
+printf("\n");
+printf("echo superiorfrontal 4 >  splittable.txt\n");
+printf("echo precentral      3 >> splittable.txt\n");
+printf("\n");
+printf("Run program:\n");
+printf("\n");
+printf("mris_divide_parcellation mysubj001 rh aparc.annot splittable.txt rh.aparc.split-sfg+pc\n");
+printf("\n");
+printf("This reads in rh.aparc.annot, splits the SFG and PC, and and saves the\n");
+printf("result to rh.aparc.split-sfg+pc\n");
+printf("\n");
+printf("View\n");
+printf("\n");
+printf("tksurfer mysubj001 rh inflated -annot aparc.split-sfg+pc\n");
+printf("\n");
+printf("The SFG divisions will have the following names: superiorfrontal,\n");
+printf("superiorfrontal_div2, superiorfrontal_div3, superiorfrontal_div4. The\n");
+printf("PC divisions will be precentral, precentral_div2, precentral_div3.\n");
+printf("\n");
+printf("\n");
+printf("\n");
   exit(1) ;
 }
 
