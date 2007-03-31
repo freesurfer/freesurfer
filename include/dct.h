ce2cf49a3ac93ae59729af43863f0d129fb44c4e
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2007-03-31 20:19:44 +0000

    discrete cosine transform.

---
+++ include/dct.h
@@ -0,0 +1,87 @@
+/**
+ * @file  dct.h
+ * @brief structures and prototypes for discrete cosine transform
+ *
+ * 
+ *  structures and prototypes for discrete cosine transform
+ */
+/*
+ * Original Author: Bruce Fischl
+ * CVS Revision Info:
+ *    $Author: fischl $
+ *    $Date: 2007/03/31 20:19:44 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright (C) 2002-2007,
+ * The General Hospital Corporation (Boston, MA). 
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+ *
+ */
+
+
+//
+// dct.h
+//
+// written by Bruce Fischl
+// Nov. 9th ,2000
+// 
+// Warning: Do not edit the following four lines.  CVS maintains them.
+// Revision Author: $Author: fischl $
+// Revision Date  : $Date: 2007/03/31 20:19:44 $
+// Revision       : $Revision: 1.1 $
+//
+////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "transform.h"
+#include "matrix.h"
+#include "voxlist.h"
+
+#ifndef DCT_H
+#define DCT_H
+
+typedef struct
+{
+  int      ncoef ;
+  VOL_GEOM *VG ;
+  LTA      *lta ;
+  MRI      *mri_source ;
+  MATRIX   *m_x_basis ;
+  MATRIX   *m_y_basis ;
+  MATRIX   *m_z_basis ;
+  VECTOR   *v_xk ;   // vector of x coefs
+  VECTOR   *v_yk ;   // vector of y coefs
+  VECTOR   *v_zk ;   // vector of z coefs
+  double   *x ;      // forward mapping of x
+  double   *y ;      // forward mapping of y
+  double   *z ;      // forward mapping of z
+  double   *x_inv ;  // inverse of x indices
+  double   *y_inv ;  // inverse of y indices
+  double   *z_inv ;  // inverse of z indices
+  double   res ;     // resolution of inverse transform tables
+  double   b ;       // intensity scale factor
+} DISCRETE_COSING_TRANSFORM, DCT ;
+
+DCT   *DCTalloc(int ncoef, MRI *mri_source) ;
+int   DCTfree(DCT **pdct) ;
+int   DCTcreateMatrix(DCT *dct, MRI *mri, int skip) ;
+DCT   *DCTcopy(DCT *dct_src, DCT *dct_dst) ;
+MRI   *DCTapply(DCT *dct, MRI *mri_src, MRI *mri_template, MRI *mri_dst, int sample_type) ;
+MRI   *DCTapplyInverse(DCT *dct, MRI *mri_src, MRI *mri_dst, int sample_type) ;
+int   DCTtransformVoxlist(DCT *dct, VOXEL_LIST *vl) ;
+int   DCTinverseTransformVoxlist(DCT *dct, VOXEL_LIST *vl) ;
+int   DCTtransformPoint(DCT *dct, int x, int y, int z, double *px, double *py,  double *pz) ;
+int   DCTdump(DCT *dct, FILE *fp) ;
+int   DCTupdate(DCT *dct) ;
+int   DCTinverseTransformPoint(DCT *dct, double x, double y, double z, double *px, double *py,  double *pz) ;
+
+#endif
