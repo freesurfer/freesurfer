ba8fa10ee14770527a937e334beb9094a9129f16
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-04-04 16:53:32 +0000

    Added ability to set values in and out of the bbox

--- mri_volsynth/mri_volsynth.c
+++ mri_volsynth/mri_volsynth.c
@@ -8,8 +8,8 @@
  * Original Author: Douglas N. Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2007/04/03 04:12:52 $
- *    $Revision: 1.25 $
+ *    $Date: 2007/04/04 16:53:32 $
+ *    $Revision: 1.26 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -32,7 +32,7 @@
   email:   analysis-bugs@nmr.mgh.harvard.edu
   Date:    2/27/02
   Purpose: Synthesize a volume.
-  $Id: mri_volsynth.c,v 1.25 2007/04/03 04:12:52 greve Exp $
+  $Id: mri_volsynth.c,v 1.26 2007/04/04 16:53:32 greve Exp $
 */
 
 #include <stdio.h>
@@ -74,7 +74,7 @@ static int  isflag(char *flag);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_volsynth.c,v 1.25 2007/04/03 04:12:52 greve Exp $";
+static char vcid[] = "$Id: mri_volsynth.c,v 1.26 2007/04/04 16:53:32 greve Exp $";
 char *Progname = NULL;
 
 int debug = 0;
@@ -113,6 +113,8 @@ MRI *offset;
 char *sum2file = NULL;
 int NoOutput = 0;
 MRI_REGION boundingbox;
+double ValueA = 1;
+double ValueB = 0;
 
 /*---------------------------------------------------------------*/
 int main(int argc, char **argv) 
@@ -268,7 +270,7 @@ int main(int argc, char **argv)
     }
   } else if (strcmp(pdfname,"boundingbox")==0) {
     printf("Setting bounding box \n");
-    mri = MRIsetBoundingBox(mritemp,&boundingbox,1,0);
+    mri = MRIsetBoundingBox(mritemp,&boundingbox,ValueA,ValueB);
     if(!mri) exit(1);
   } else {
     printf("ERROR: pdf %s unrecognized, must be gaussian, uniform, const, or delta\n",
@@ -456,6 +458,14 @@ static int parse_commandline(int argc, char **argv) {
       if (nargc < 1) argnerr(option,1);
       sscanf(pargv[0],"%d",&numdof);
       nargsused = 1;
+    } else if (!strcmp(option, "--val-a")) {
+      if (nargc < 1) argnerr(option,1);
+      sscanf(pargv[0],"%lf",&ValueA);
+      nargsused = 1;
+    } else if (!strcmp(option, "--val-b")) {
+      if (nargc < 1) argnerr(option,1);
+      sscanf(pargv[0],"%lf",&ValueB);
+      nargsused = 1;
     } else if (!strcmp(option, "--dof-den") || !strcmp(option, "--dof")) {
       if (nargc < 1) argnerr(option,1);
       sscanf(pargv[0],"%d",&dendof);
@@ -525,7 +535,7 @@ static void print_usage(void) {
   printf("   --seedfile fname : write seed value to this file\n");
   printf("   --pdf pdfname : <gaussian>, uniform, const, delta, \n");
   printf("      sphere, z, t, F, chi2, voxcrs\n");
-  printf("   --bb c r s dc dr ds : bounding box \n");
+  printf("   --bb c r s dc dr ds : bounding box (In=ValA, Out=ValB)\n");
   printf("   --gmean mean : use mean for gaussian (def is 0)\n");
   printf("   --gstd  std  : use std for gaussian standard dev (def is 1)\n");
   printf("   --delta-crsf col row slice frame : 0-based\n");
@@ -535,6 +545,8 @@ static void print_usage(void) {
   printf("   --dof-num numdof : numerator dof for F\n");
   printf("   --dof-den dendof : denomenator dof for F\n");
   printf("   --rescale : rescale z, t, F, or chi2 after smoothing\n");
+  printf("   --val-a value : set ValA (default 1)\n");
+  printf("   --val-b value : set ValB (default 0)\n");
   printf("\n");
   printf(" Other arguments\n");
   printf("   --fwhm fwhm_mm : smooth by FWHM mm\n");
