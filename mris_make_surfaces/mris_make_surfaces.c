ea116f413c91190c6252b15e093fb3d6f937cd9f
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-04-06 23:57:14 +0000

    Fixed init warnings

--- mris_make_surfaces/mris_make_surfaces.c
+++ mris_make_surfaces/mris_make_surfaces.c
@@ -11,9 +11,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: fischl $
- *    $Date: 2007/04/06 20:26:38 $
- *    $Revision: 1.91 $
+ *    $Author: greve $
+ *    $Date: 2007/04/06 23:57:14 $
+ *    $Revision: 1.92 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -54,7 +54,7 @@
 #include "label.h"
 
 static char vcid[] =
-  "$Id: mris_make_surfaces.c,v 1.91 2007/04/06 20:26:38 fischl Exp $";
+  "$Id: mris_make_surfaces.c,v 1.92 2007/04/06 23:57:14 greve Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -221,13 +221,13 @@ main(int argc, char *argv[]) {
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mris_make_surfaces.c,v 1.91 2007/04/06 20:26:38 fischl Exp $",
+   "$Id: mris_make_surfaces.c,v 1.92 2007/04/06 23:57:14 greve Exp $",
    "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mris_make_surfaces.c,v 1.91 2007/04/06 20:26:38 fischl Exp $",
+           "$Id: mris_make_surfaces.c,v 1.92 2007/04/06 23:57:14 greve Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -1974,7 +1974,7 @@ static int
 fix_midline(MRI_SURFACE *mris, MRI *mri_aseg, MRI *mri_brain, char *hemi,
             int which)
 {
-  int      vno, label, contra_wm_label, nvox, total_vox, adjacent ;
+  int      vno, label, contra_wm_label, nvox=0, total_vox=0, adjacent=0 ;
   VERTEX   *v ;
   double   xv, yv, zv, val, xs, ys, zs, d ;
 
