41ca53b97dc9b018c20722656e012b1dc4f3b7d6
Rudolph Pienaar <rudolph@nmr.mgh.harvard.edu>
2007-04-16 20:27:46 +0000

    Added a final MRIScomputeMetricProperties in line 1093 at the end of MRISmeasureCorticalGrayMatterVolume(). Without this, 'mris_anatomical_stats' will always use the PIAL surface for all its curvature reports, irrespective of whatever surface is passed on the command line.

--- mris_anatomical_stats/mris_anatomical_stats.c
+++ mris_anatomical_stats/mris_anatomical_stats.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:10 $
- *    $Revision: 1.42 $
+ *    $Author: rudolph $
+ *    $Date: 2007/04/16 20:27:46 $
+ *    $Revision: 1.43 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -46,7 +46,7 @@
 #include "colortab.h"
 
 static char vcid[] =
-  "$Id: mris_anatomical_stats.c,v 1.42 2006/12/29 02:09:10 nicks Exp $";
+  "$Id: mris_anatomical_stats.c,v 1.43 2007/04/16 20:27:46 rudolph Exp $";
 
 int main(int argc, char *argv[]) ;
 static int  get_option(int argc, char *argv[]) ;
@@ -116,7 +116,7 @@ main(int argc, char *argv[]) {
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mris_anatomical_stats.c,v 1.42 2006/12/29 02:09:10 nicks Exp $",
+           "$Id: mris_anatomical_stats.c,v 1.43 2007/04/16 20:27:46 rudolph Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -1090,6 +1090,7 @@ MRISmeasureCorticalGrayMatterVolume(MRI_SURFACE *mris) {
   else
     mean = 0 ;
   MRISrestoreVertexPositions(mris, TMP_VERTICES) ;
+  MRIScomputeMetricProperties(mris);
   return(total) ;
 }
 
