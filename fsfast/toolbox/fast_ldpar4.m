f3367f9d3431c54533772bfdb88f7eb4b4c548b7
Doug Greve <greve@nmr.mgh.harvard.edu>
2007-06-14 01:45:18 +0000

    Can now read par as 2, 3, or 4 cols

--- fsfast/toolbox/fast_ldpar4.m
+++ fsfast/toolbox/fast_ldpar4.m
@@ -15,6 +15,12 @@ function par4 = fast_ldpar4(par4file)
 % par4 dimensionality: 4 x nPresentations
 %
 %
+% a. Two cols:   onset time, condition number
+% b. Three cols: onset time, condition number, duration
+% c. Four cols:  onset time, condition number, duration, stringid
+% d. Four cols:  onset time, condition number, duration, weight
+% e. Three cols: onset time, condition number, duration
+% f. Three cols: onset time, condition number, stringid
 
 %
 % fast_ldpar4
@@ -22,8 +28,8 @@ function par4 = fast_ldpar4(par4file)
 % Original Author: Doug Greve
 % CVS Revision Info:
 %    $Author: greve $
-%    $Date: 2007/06/07 22:12:09 $
-%    $Revision: 1.2 $
+%    $Date: 2007/06/14 01:45:18 $
+%    $Revision: 1.3 $
 %
 % Copyright (C) 2002-2007,
 % The General Hospital Corporation (Boston, MA). 
@@ -53,7 +59,7 @@ if(fp == -1)
   return;
 end
 
-nthrow = 1;
+nthrow = 0;
 while(1)
   % scroll through any blank lines or comments 
   % comments are lines that begin with #
@@ -65,35 +71,71 @@ while(1)
 
   % Get count of number of items in the line
   [items count] = sscanf(tline,'%s');
-  if(count < 4)
+  if(count < 2)
     fprintf('ERROR: %s is not correctly formatted. ',par4file);
-    fprintf('Line %d only has %d items\n',nthrow,count);
+    fprintf('Line %d only has %d items\n',nthrow+1,count);
+    fclose(fp);
+    par4 = [];
+    return;
+  end
+  if(nthrow == 0) prevcount = count; end
+  if(count ~= prevcount)
+    fprintf('ERROR: %s is not correctly formatted. ',par4file);
+    fprintf('Line %d has %d items, 1st line had %d\n',...
+	    nthrow+1,count,prevcount);
+    fclose(fp);
+    par4 = [];
+    return;
+  end
+  nthrow = nthrow + 1;
+  
+  % First column - onset time
+  item = sscanfitem(tline,1);
+  tonset = sscanf(item,'%f');
+  par4(nthrow,1) = tonset;
+  
+  % Second column - stimulus id
+  item = sscanfitem(tline,2);
+  condid = sscanf(item,'%d');
+  par4(nthrow,2) = condid;
+
+  if(count < 3) continue; end % Only two cols
+  
+  % Third column
+  item = sscanfitem(tline,3);
+  duration = sscanf(item,'%f');
+  if(isempty(duration)) continue; end % 3rd col is a string
+  par4(nthrow,3) = duration;
+  
+  % optseq used to make the last duration a large negative number
+  if(duration < 0)
+    fprintf('ERROR: %s, row %d has a negative duration.\n',...
+	    par4file,nthrow);
     fclose(fp);
     par4 = [];
     return;
   end
 
-  for c = 1:4
-    item = sscanfitem(tline,c);
-    tmp = sscanf(item,'%f');
-    if(isempty(tmp))
-      fprintf('ERROR: %s is not correctly formatted. ',par4file);
-      fprintf('Line %d, item %d appears to be a string\n',nthrow,c);
-      fprintf('%s\n',tline);
-      fclose(fp);
-      par4 = [];
-      return;
-    end
-    par4(nthrow,c) = tmp;
-  end
-
-  nthrow = nthrow + 1;
+  if(count < 4) continue; end % Only three cols
   
+  % Fourth column
+  item = sscanfitem(tline,4);
+  weight = sscanf(item,'%f');
+  if(isempty(weight)) continue; end % 4th col is a string
+  % If it gets here, assume it is the weight
+  par4(nthrow,4) = weight;
+
 end % while (1)
 
 fclose(fp);
 
-
+if(size(par4,2) == 2 )
+  fprintf('Loaded %s as a par2 file\n',par4file);
+elseif(size(par4,2) == 3 )
+  fprintf('Loaded %s as a par3 file\n',par4file);
+else  
+  fprintf('Loaded %s as a par4 file\n',par4file);
+end
 
 return;
 
