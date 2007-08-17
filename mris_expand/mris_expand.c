a4cc9e235fe57c80f403ee5b93c08cc116ac5f45
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2007-08-17 17:08:07 +0000

    removed  conflict indicators.

--- mris_expand/mris_expand.c
+++ mris_expand/mris_expand.c
@@ -1,7 +1,6 @@
 /**
  * @file  mris_expand.c
  * @brief expand a surface outwards by a specified amount
-=======
  * Expands a surface (typically ?h.white) outwards while maintaining smoothness
  * and self-intersection constraints.
  */
@@ -9,8 +8,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2007/08/17 17:07:43 $
- *    $Revision: 1.8 $
+ *    $Date: 2007/08/17 17:08:07 $
+ *    $Revision: 1.9 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA).
@@ -72,7 +71,7 @@ main(int argc, char *argv[])
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
     (argc, argv,
-     "$Id: mris_expand.c,v 1.8 2007/08/17 17:07:43 fischl Exp $",
+     "$Id: mris_expand.c,v 1.9 2007/08/17 17:08:07 fischl Exp $",
      "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
