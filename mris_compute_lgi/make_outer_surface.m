0d495fba7c9dea83d5f42481ba326e2873e504c3
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2007-12-07 21:29:01 +0000

    added maries fix where it blurs the filled vol before tesselation

--- mris_compute_lgi/make_outer_surface.m
+++ mris_compute_lgi/make_outer_surface.m
@@ -20,51 +20,64 @@ function make_outer_surface (filled_volume, se_diameter, output_surface)
 % Example: make_outer_surface('lh.pial.mgz',15,'lh.outer-pial')
 
 
-fprintf('reading filled volume...');
-vol=MRIread(filled_volume);
-fprintf('done.\n');
-volume=vol.vol;
+    fprintf('reading filled volume...\n');
+    vol=MRIread(filled_volume);
+    volume=vol.vol;
+    volume(volume==1)=255;
+    fprintf('closing volume...\n');
     
-fprintf('closing volume...\n');
-se=strel('ball',se_diameter,se_diameter);
-BW2=imclose(volume,se);
-thresh = max(BW2(:))/2;
-i=find(BW2<=thresh);
-BW2(i)=0;
-i=find(BW2>thresh);
-BW2(i)=255;
+% first apply a very soft gaussian filter, with sigma = 1mm, in order to
+% facilitate the closing
+Gaussian = fspecial('gaussian',[2 2],1);
+image_f=zeros(256,256,256);
+for slice=1:256
+    temp = double(reshape(volume(:,:,slice),256,256));
+    image_f(:,:,slice) = conv2(temp,Gaussian,'same');
+end
+image2=zeros(size(image_f));
+image2(image_f<=25)=0;
+image2(image_f>25)=255;
+       
+    se=strel('ball',se_diameter,se_diameter);
+    BW2=imclose(image2,se);
+    thresh = max(BW2(:))/2;
+    i=find(BW2<=thresh);
+    BW2(i)=0;
+    i=find(BW2>thresh);
+    BW2(i)=255;
     
-[f,v] = isosurface(BW2,100);
+    [f,v] = isosurface(BW2,100);
 
-v2=[129-v(:,1) v(:,3)-129 129-v(:,2)]; % in order to cope with the different orientation 
-v=v2;
+    v2=[129-v(:,1) v(:,3)-129 129-v(:,2)]; % in order to cope with the different orientation 
+    v=v2;
 
-fprintf('morphological closing done.\n');
-fprintf('writing outer surface...\n');
+    fprintf('morphological closing done.\n');
+    fprintf('writing outer surface...\n');
     
-fname=output_surface;
-vert = v;
-face = f - 1;
-vnum = size(vert,1);  
-fnum = size(face,1);  
+    fname=output_surface;
+    vert = v;
+    face = f - 1;
+    vnum = size(vert,1);  
+    fnum = size(face,1);  
     
-% open it as a big-endian file
-fid = fopen(fname, 'wb', 'b');
-TRIANGLE_FILE_MAGIC_NUMBER = 16777214;
-fwrite3(fid, TRIANGLE_FILE_MAGIC_NUMBER);
+    % open it as a big-endian file
+    fid = fopen(fname, 'wb', 'b');
+    TRIANGLE_FILE_MAGIC_NUMBER = 16777214;
+    fwrite3(fid, TRIANGLE_FILE_MAGIC_NUMBER);
     
-% Ouput a couple of text lines with creation date
-str = sprintf('created from matlab on %s\n',datestr(now));
-fwrite(fid, str,'char');
-fwrite(fid, vnum,'int32');
-fwrite(fid, fnum,'int32');
+    % Ouput a couple of text lines with creation date
+    str = sprintf('created from matlab on %s\n',datestr(now));
+    fwrite(fid, str,'char');
+    fwrite(fid, vnum,'int32');
+    fwrite(fid, fnum,'int32');
 
-% reshape vert into column array and write
-vert = reshape(vert',size(vert,1)*size(vert,2),1);
-fwrite(fid, vert,'float32');
+    % reshape vert into column array and write
+    vert = reshape(vert',size(vert,1)*size(vert,2),1);
+    fwrite(fid, vert,'float32');
 
-% reshape face into column array and write
-face = reshape(face',size(face,1)*size(face,2),1);
-fwrite(fid, face,'int32');
-fclose(fid) ;
-
+    % reshape face into column array and write
+    face = reshape(face',size(face,1)*size(face,2),1);
+    fwrite(fid, face,'int32');
+    fclose(fid) ;
+return
+end
