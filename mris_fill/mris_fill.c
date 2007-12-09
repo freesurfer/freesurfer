98d088add10f0db32c9191f959c7285bc835f5a5
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2007-12-09 22:45:02 +0000

    from dev: added -C (conform) routine to support mris_compute_lgi

--- mris_fill/mris_fill.c
+++ mris_fill/mris_fill.c
@@ -8,9 +8,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: fischl $
- *    $Date: 2007/01/04 15:59:46 $
- *    $Revision: 1.2 $
+ *    $Author: nicks $
+ *    $Date: 2007/12/09 22:45:02 $
+ *    $Revision: 1.2.2.1 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -25,7 +25,6 @@
  * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
  *
  */
-
 
 
 #include <stdio.h>
@@ -43,8 +42,8 @@
 #include "macros.h"
 #include "version.h"
 
-static char vcid[] = "$Id: mris_fill.c,v 1.2 2007/01/04 15:59:46 fischl Exp $";
-
+static const char vcid[] = 
+"$Id: mris_fill.c,v 1.2.2.1 2007/12/09 22:45:02 nicks Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -54,12 +53,14 @@ static void print_usage(void) ;
 static void print_help(void) ;
 static void print_version(void) ;
 
-char *Progname ;
+const char *Progname ;
 
 static double resolution = .25 ;
+static int conform = 0 ;
 
 int
-main(int argc, char *argv[]) {
+main(int argc, char *argv[]) 
+{
   char          **av, *out_fname, *in_fname ;
   int           ac, nargs ;
   MRI_SURFACE   *mris ;
@@ -69,11 +70,14 @@ main(int argc, char *argv[]) {
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mris_fill.c,v 1.2 2007/01/04 15:59:46 fischl Exp $", "$Name:  $",
+   "$Id: mris_fill.c,v 1.2.2.1 2007/12/09 22:45:02 nicks Exp $", "$Name:  $",
    cmdline);
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_fill.c,v 1.2 2007/01/04 15:59:46 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option 
+    (argc, argv, 
+     "$Id: mris_fill.c,v 1.2.2.1 2007/12/09 22:45:02 nicks Exp $", 
+     "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -99,8 +103,29 @@ main(int argc, char *argv[]) {
   fprintf(stderr, "reading surface from %s...\n", in_fname) ;
   mris = MRISread(in_fname) ;
   if (!mris)
-    ErrorExit(ERROR_NOFILE, "%s: could not read surface file %s",Progname, in_fname) ;
+    ErrorExit(ERROR_NOFILE, 
+              "%s: could not read surface file %s",
+              Progname, in_fname) ;
+
   mri_interior = MRISfillInterior(mris, resolution, NULL) ;
+
+  if (conform)
+  {
+    MRI *mri_tmp, *mri_tmp2 ;
+    mri_tmp = MRIalloc(256, 256, 256, MRI_UCHAR) ;
+    MRIsetResolution(mri_tmp, 1.0, 1.0, 1.0) ;
+    mri_tmp->xstart = mri_tmp->ystart = mri_tmp->zstart = -mri_tmp->width/2;
+    mri_tmp->xend =   mri_tmp->yend =   mri_tmp->zend =   mri_tmp->width/2;
+    mri_tmp->x_r = -1.0;  mri_tmp->x_a =  0.0; mri_tmp->x_s =  0.0;
+    mri_tmp->y_r =  0.0;  mri_tmp->y_a =  0.0; mri_tmp->y_s = -1.0;
+    mri_tmp->z_r =  0.0;  mri_tmp->z_a =  1.0;  mri_tmp->z_s =  0.0;
+    mri_tmp->c_r = mris->vg.c_r ;
+    mri_tmp->c_a = mris->vg.c_a ;
+    mri_tmp->c_s = mris->vg.c_s ;
+    mri_tmp2 = MRIresample(mri_interior, mri_tmp, SAMPLE_NEAREST);
+    MRIfree(&mri_interior) ; MRIfree(&mri_tmp) ;
+    mri_interior = mri_tmp2 ;
+  }
   MRIaddCommandLine(mri_interior, cmdline) ;
   fprintf(stderr, "writing filled volume to %s...\n", out_fname) ;
   MRIwrite(mri_interior, out_fname) ;
@@ -124,37 +149,44 @@ get_option(int argc, char *argv[]) {
   else if (!stricmp(option, "-version")){
     print_version() ;
   } else switch (toupper(*option)) {
-    case 'R':
-      resolution = (double)atof(argv[2]) ;
-      nargs = 1 ;
-      printf("setting resolution for intermediate calculations to %2.4f\n", resolution) ;
-      break ;
-    case 'V':
-      Gdiag_no = atoi(argv[2]) ;
-      nargs = 1 ;
-      break ;
-    case '?':
-    case 'U':
-      print_usage() ;
-      exit(1) ;
-      break ;
-    default:
-      fprintf(stderr, "unknown option %s\n", argv[1]) ;
-      exit(1) ;
-      break ;
-    }
+  case 'R':
+    resolution = (double)atof(argv[2]) ;
+    nargs = 1 ;
+    printf("setting resolution for intermediate calculations to %2.4f\n", 
+           resolution) ;
+    break ;
+  case 'C':
+    conform = 1 ;
+    printf("conforming output volume\n") ;
+    break ;
+  case 'V':
+    Gdiag_no = atoi(argv[2]) ;
+    nargs = 1 ;
+    break ;
+  case '?':
+  case 'U':
+    print_usage() ;
+    exit(1) ;
+    break ;
+  default:
+    fprintf(stderr, "unknown option %s\n", argv[1]) ;
+    exit(1) ;
+    break ;
+  }
 
   return(nargs) ;
 }
 
 static void
-usage_exit(void) {
+usage_exit(void) 
+{
   print_help() ;
   exit(1) ;
 }
 
 static void
-print_usage(void) {
+print_usage(void) 
+{
   printf("usage: %s [options] <input surface> <output volume>\n",
          Progname) ;
 }
@@ -168,13 +200,13 @@ print_help(void)
   printf("\nvalid options are:\n\n") ;
   printf("\t-r <resolution>: set the resolution of the output volume"
          " (default = %2.3f mm/voxel)\n", resolution) ;
+  printf("\t-c               'conform' the volume before writing\n") ;
   exit(1) ;
 }
 
 static void
-print_version(void) {
+print_version(void) 
+{
   fprintf(stderr, "%s\n", vcid) ;
   exit(1) ;
 }
-
-
