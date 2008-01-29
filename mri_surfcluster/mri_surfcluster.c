193d7c6af9057fdcfb525f1d24e1ad006817d8a9
Doug Greve <greve@nmr.mgh.harvard.edu>
2008-01-29 22:56:00 +0000

    Fix bug with --frame

--- mri_surfcluster/mri_surfcluster.c
+++ mri_surfcluster/mri_surfcluster.c
@@ -8,8 +8,8 @@
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2007/07/31 00:34:19 $
- *    $Revision: 1.39 $
+ *    $Date: 2008/01/29 22:56:00 $
+ *    $Revision: 1.40 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -32,7 +32,7 @@
   email:   analysis-bugs@nmr.mgh.harvard.edu
   Date:    2/27/02
   Purpose: Finds clusters on the surface.
-  $Id: mri_surfcluster.c,v 1.39 2007/07/31 00:34:19 greve Exp $
+  $Id: mri_surfcluster.c,v 1.40 2008/01/29 22:56:00 greve Exp $
 */
 
 #include <stdio.h>
@@ -80,14 +80,14 @@ static int  stringmatch(char *str1, char *str2);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_surfcluster.c,v 1.39 2007/07/31 00:34:19 greve Exp $";
+static char vcid[] = "$Id: mri_surfcluster.c,v 1.40 2008/01/29 22:56:00 greve Exp $";
 char *Progname = NULL;
 
 char *subjectdir = NULL;
 char *hemi = NULL;
 
 char *srcid = NULL;
-char *srcfmt  = "paint";
+char *srcfmt  = "";
 int   srcfmtid = MRI_VOLUME_TYPE_UNKNOWN;
 char *srcsurfid = "white";
 char *srcsubjid = NULL;
@@ -189,7 +189,7 @@ int main(int argc, char **argv) {
   double cmaxsize;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_surfcluster.c,v 1.39 2007/07/31 00:34:19 greve Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_surfcluster.c,v 1.40 2008/01/29 22:56:00 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -1169,7 +1169,7 @@ static void print_help(void) {
     "summary file is shown below.\n"
     "\n"
     "Cluster Growing Summary (mri_surfcluster)\n"
-    "$Id: mri_surfcluster.c,v 1.39 2007/07/31 00:34:19 greve Exp $\n"
+    "$Id: mri_surfcluster.c,v 1.40 2008/01/29 22:56:00 greve Exp $\n"
     "Input :      minsig-0-lh.w\n"
     "Frame Number:      0\n"
     "Minimum Threshold: 5\n"
