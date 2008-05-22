e0c0bca42f0b2c6c2933211c51f8cce8313bb6cf
Doug Greve <greve@nmr.mgh.harvard.edu>
2008-05-22 20:20:57 +0000

    Fixed docs

--- mri_probedicom/mri_probedicom.c
+++ mri_probedicom/mri_probedicom.c
@@ -8,8 +8,8 @@
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2007/08/30 19:58:36 $
- *    $Revision: 1.15.2.1 $
+ *    $Date: 2008/05/22 20:20:57 $
+ *    $Revision: 1.15.2.2 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -61,7 +61,7 @@
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_probedicom.c,v 1.15.2.1 2007/08/30 19:58:36 greve Exp $";
+static char vcid[] = "$Id: mri_probedicom.c,v 1.15.2.2 2008/05/22 20:20:57 greve Exp $";
 char *Progname = NULL;
 
 static int  parse_commandline(int argc, char **argv);
@@ -131,7 +131,7 @@ int main(int argc, char **argv) {
   int nargs;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_probedicom.c,v 1.15.2.1 2007/08/30 19:58:36 greve Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_probedicom.c,v 1.15.2.2 2008/05/22 20:20:57 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -446,8 +446,8 @@ static void print_help(void) {
     "      pixel spacing      28 30\n"
     "      rows               28 10\n"
     "      cols               28 11\n"
-    "      image orientation  20 32\n"
-    "      image position     20 1041\n"
+    "      image position     20 32\n"
+    "      image orientation  20 37\n"
     "\n"
     "  --d directive\n"
     "\n"
