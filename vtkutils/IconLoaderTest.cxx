60ceb9ed367e720e8aa548d87eeed65caf72d0e1
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2008-06-17 21:25:42 +0000

    works on Linux

--- vtkutils/IconLoaderTest.cxx
+++ vtkutils/IconLoaderTest.cxx
@@ -7,8 +7,8 @@
  * Original Author: Nick Schmansky
  * CVS Revision Info:
  *    $Author: nicks $
- *    $Date: 2008/06/17 20:43:19 $
- *    $Revision: 1.1.2.1 $
+ *    $Date: 2008/06/17 21:25:42 $
+ *    $Revision: 1.1.2.2 $
  *
  * Copyright (C) 2008,
  * The General Hospital Corporation (Boston, MA).
@@ -27,40 +27,82 @@
 #include <iostream>
 #include <stdexcept>
 
+#include "vtkObjectFactory.h"
+#include "vtkKWApplication.h"
 #include "IconLoader.h"
+#include "IconLoaderTest.h"
+
+extern "C" {
+
+#include "unistd.h" // getcwd
+#include "tix.h"
+  
+  extern int Qdeclib_SafeInit( Tcl_Interp* iInterp );
+  extern int Blt_Init( Tcl_Interp* iInterp );
+
+}
 
 using namespace std;
 
-class IconLoaderTest
-{
-public:
+vtkStandardNewMacro( IconLoaderTest );
 
-  static int Test () {
-    // Init the icon loader with the app and load our icons.
+IconLoaderTest::IconLoaderTest () :
+  vtkKWApplication() {
+
+  // Init the icon loader with the app and load our icons.
+  try {
+    IconLoader::Initialize( this );
+    
     try {
-      IconLoader::Initialize( NULL );
-      
-      try {
-        IconLoader::LoadIconsFromFile( "./IconLoaderTestIcons.txt" );
-      }
-      catch(...) {
-        char* pfnFreesurferDir = getenv( "FREESURFER_HOME" );
-        if( NULL != pfnFreesurferDir ) {
-          string fnIcons =
-            string(pfnFreesurferDir) + "/lib/resource/QdecIcons.txt";
-          IconLoader::LoadIconsFromFile( fnIcons.c_str() );
-        }
+      IconLoader::LoadIconsFromFile( "./IconLoaderTestIcons.txt" );
+    }
+    catch(...) {
+      char* pfnFreesurferDir = getenv( "FREESURFER_HOME" );
+      if( NULL != pfnFreesurferDir ) {
+        string fnIcons =
+          string(pfnFreesurferDir) + "/lib/resource/QdecIcons.txt";
+        IconLoader::LoadIconsFromFile( fnIcons.c_str() );
       }
     }
-    catch( exception& e ) {
-      cerr << "Error loading icons: " << e.what() << endl;
-      return 1;
-    }
-    return 0;
-  };
+  }
+  catch( exception& e ) {
+    cerr << "Error loading icons: " << e.what() << endl;
+  }
+  cout << "Success loading icons" << endl;
 };
+
+IconLoaderTest::~IconLoaderTest () {
+
+  IconLoader::ShutDown();
+}
 
 int main ( int argc, char** argv ) {
 
-  return IconLoaderTest::Test();
+  // Initialize Tcl.
+  Tcl_Interp* interp = vtkKWApplication::InitializeTcl( argc, argv, &cerr );
+  if ( !interp ) {
+    cerr << "Error initializing Tcl." << endl;
+    return 1;
+  }
+
+  // Init Tix manually.
+  int rTcl = Tix_Init( interp );
+  if ( TCL_OK != rTcl ) {
+    const char* sResult = Tcl_GetStringResult( interp );
+    cerr <<  "Tix_Init returned not TCL_OK: " << sResult << endl;
+    return 1;
+  }
+
+  // Init Blt manually.
+  rTcl = Blt_Init( interp );
+  if ( TCL_OK != rTcl ) {
+    const char* sResult = Tcl_GetStringResult( interp );
+    cerr <<  "Blt_Init returned not TCL_OK: " << sResult << endl;
+    return 1;
+  }
+
+  IconLoaderTest* app = IconLoaderTest::New();
+  app->Delete();
+
+  return 0;
 }
