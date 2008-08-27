e9625f98da25aeb2ea4461bad2eedd7d36a5ee7b
Doug Greve <greve@nmr.mgh.harvard.edu>
2008-08-27 19:09:51 +0000

    Updated to work with FSL 4.1.0. Copied change from dev.

--- tkregister2/tkregister2.c
+++ tkregister2/tkregister2.c
@@ -7,9 +7,9 @@
 /*
  * Original Authors: Martin Sereno and Anders Dale, 1996; Doug Greve, 2002
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2008/03/10 14:02:35 $
- *    $Revision: 1.86.2.2 $
+ *    $Author: greve $
+ *    $Date: 2008/08/27 19:09:51 $
+ *    $Revision: 1.86.2.3 $
  *
  * Copyright (C) 2002-2007, CorTechs Labs, Inc. (La Jolla, CA) and
  * The General Hospital Corporation (Boston, MA).
@@ -35,7 +35,7 @@
 
 #ifndef lint
 static char vcid[] =
-"$Id: tkregister2.c,v 1.86.2.2 2008/03/10 14:02:35 nicks Exp $";
+"$Id: tkregister2.c,v 1.86.2.3 2008/08/27 19:09:51 greve Exp $";
 #endif /* lint */
 
 #ifdef HAVE_TCL_TK_GL
@@ -1022,9 +1022,13 @@ static int parse_commandline(int argc, char **argv) {
     else if (!strcasecmp(option, "--mgz"))  ; // for backwards compat
     else if (stringmatch(option, "--fsl-targ")) {
       sprintf(tmpstr,"%s/etc/standard/avg152T1",getenv("FSLDIR"));
+      if(! fio_FileExistsReadable(tmpstr))
+	sprintf(tmpstr,"%s/data/standard/avg152T1",getenv("FSLDIR"));
       targ_vol_id = IDnameFromStem(tmpstr); // For FSL 4.0
     } else if (stringmatch(option, "--fsl-targ-lr")) {
       sprintf(tmpstr,"%s/etc/standard/avg152T1_LR-marked",getenv("FSLDIR"));
+      if(! fio_FileExistsReadable(tmpstr))
+	sprintf(tmpstr,"%s/data/standard/avg152T1_LR-marked",getenv("FSLDIR"));
       targ_vol_id = IDnameFromStem(tmpstr); // For FSL 4.0
     } else if (!strcasecmp(option, "--lh-only")) lhsurf_only=1 ;
     else if (!strcasecmp(option, "--rh-only")) rhsurf_only=1 ;
@@ -1148,6 +1152,8 @@ static int parse_commandline(int argc, char **argv) {
       if (nargc < 1) argnerr(option,1);
       //pargv[0] is featdir
       sprintf(tmpstr,"%s/etc/standard/avg152T1",getenv("FSLDIR"));
+      if(! fio_FileExistsReadable(tmpstr))
+	sprintf(tmpstr,"%s/data/standard/avg152T1",getenv("FSLDIR"));
       targ_vol_id = IDnameFromStem(tmpstr); // For FSL 4.0
       sprintf(tmpstr,"%s/example_func",pargv[0]);
       mov_vol_id = IDnameFromStem(tmpstr);
@@ -4479,7 +4485,7 @@ int main(argc, argv)   /* new main */
   nargs =
     handle_version_option
     (argc, argv,
-     "$Id: tkregister2.c,v 1.86.2.2 2008/03/10 14:02:35 nicks Exp $", "$Name:  $");
+     "$Id: tkregister2.c,v 1.86.2.3 2008/08/27 19:09:51 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
