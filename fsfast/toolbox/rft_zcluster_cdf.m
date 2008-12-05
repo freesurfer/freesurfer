6b6c48e11cdff5fca8ec9abe4683200b648bf67d
Doug Greve <greve@nmr.mgh.harvard.edu>
2008-12-05 20:37:16 +0000

    I think this actually works now.

--- fsfast/toolbox/rft_zcluster_cdf.m
+++ fsfast/toolbox/rft_zcluster_cdf.m
@@ -1,19 +1,26 @@
-function cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)
-% cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)
+function cdf = rft_zcluster_cdf(csize,zthresh,fwhm,ssize,D)
+% cdf = rft_zcluster_cdf(csize,zthresh,fwhm,ssize,D)
 %
-% Prob that a cluster >= csize defined by threshold vthresh will be
+% Prob that a cluster >= csize defined by threshold zthresh will be
 % found in a D-dim z-field with fwhm smoothness of ssize search space.
-% vthresh is the voxel-wise z thresh
-
+% zthresh is the voxel-wise z thresh. ssize and csize are measured
+% in non-resel units. csize, fwhm, and ssize are measured in the
+% same units.
+%
+% Based on Friston, Holmes, Pline, Price, and Frith. Detecting
+% Activations in PET and fMRI: Levels of Inference and Power.
+% Neuroimage 40, 223-235 (1996).
+% 
+% $Id: rft_zcluster_cdf.m,v 1.4 2008/12/05 20:37:16 greve Exp $
 
 %
 % rft_zcluster_cdf.m
 %
 % Original Author: Doug Greve
 % CVS Revision Info:
-%    $Author: nicks $
-%    $Date: 2007/01/10 22:02:34 $
-%    $Revision: 1.3 $
+%    $Author: greve $
+%    $Date: 2008/12/05 20:37:16 $
+%    $Revision: 1.4 $
 %
 % Copyright (C) 2002-2007,
 % The General Hospital Corporation (Boston, MA). 
@@ -30,28 +37,81 @@ function cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)
 
 cdf = [];
 if(nargin ~= 5)
-  fprintf('cdf = rft_zcluster_cdf(csize,vthresh,fwhm,ssize,D)\n');
+  fprintf('cdf = rft_zcluster_cdf(csize,zthresh,fwhm,ssize,D)\n');
   return;
 end
+
+% Variables names and equations are from Friston, et al.
+
+% Height threshold meausred in z-units
+u = zthresh; 
+
+% Equivalent p-value threshold. Note that the paper uses phi(-u),
+% but the results dont work out that way.
+phiu = fast_z2p(u); 
+
+k = csize; % cluster size to test (actual units, not resels)
+S = ssize; % search space (actual units, not resels)
+
+W = fwhm/sqrt(4*log(2));
+
+% Expected number of clusters (Eq 2)
+Em = exp(-(u.^2)/2) .* u.^(D-1) * (2*pi).^(-(D+1)/2) .* S ./ (W.^D);
+
+% Equation 3
+beta = (gamma(D/2+1).*Em./(S.*phiu)).^(2/D);
+
+% Prob that number of voxels in a cluster (n) exceeds k (Bet Eq 2 and 3)
+Pnk = exp(-beta.*(k.^(2/D)));
+
+% Prob of cluster of size k
+cdf = 1 - exp(-Em.*Pnk);
+
+
+return;
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These were old attempts. Above seems to disagree with the FSL
+% implementation but agrees with KJW's stat_threshold.m and seems
+% to agree with actual simulations.
 
 W = fwhm/sqrt(4*log(2));
 dLh = W.^(-D); % Same as FSL?
 %dLh = 0.493971;
 
-if(0)
-Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (vthresh.^(D-1)) .* ...
-     exp(-(vthresh.^2)/2); 
+if(1)
+Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (zthresh.^(D-1)) .* ...
+     exp(-(zthresh.^2)/2); 
 else
 % This appears to be the FSL equation from the techrep
-Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (vthresh.^(D-1) - 1) .* ...
-     exp(-(vthresh.^2)/2); 
+Em = ssize .* ((2*pi).^(-(D+1)/2)) .* dLh .* (zthresh.^(D-1) - 1) .* ...
+     exp(-(zthresh.^2)/2); 
 end
 
-beta = ( ((gamma(D/2+1) .* Em)) ./ (ssize.*mri_zcdf(-vthresh))).^(2./D);
+beta = ( ((gamma(D/2+1) .* Em)) ./ (ssize.*mri_zcdf(zthresh))).^(2./D);
 
 % Prob than n >= k, ie, the number of voxels in a cluster >= csize
 Pnk = exp(-beta.*(csize.^(2./D)));
 cdf = 1 - exp(-Em.*Pnk);
+
+%------------------------------------------------------------------
+%zthresh = fast_p2z(zthresh);
+zthresh = zthresh;
+pthresh = fast_z2p(zthresh);
+rhoD = zthresh .* exp(-(zthresh.^2)/2) / ((2*pi)^1.5); % rho2 for surf
+resels = ssize./(fwhm.^D);
+invol = resels .* (4*log(2)).^(D/2);
+EL = invol.*rhoD;
+cons = (((gamma(D/2+1)*((4*log(2))^(D/2))/(fwhm^D)))*rhoD)/pthresh;
+pS = exp(-(csize*cons).^(2/D));
+P_val_extent = 1-exp(-pS*EL);
+cdf2 = P_val_extent;
+
+keyboard
+
+%------------------------------------------------------------------
 
 %keyboard
 
