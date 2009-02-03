70ef33e88df575848b122a43d8fa1925873702c1
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2009-02-03 22:20:27 +0000

    mods to use local glut source build of libglut.a: fixes segfault problem in tkmedit when middle-mouse-button is clicked while scrolling

--- tksurfer/tksurfer.c
+++ tksurfer/tksurfer.c
@@ -11,9 +11,9 @@
 /*
  * Original Author: Martin Sereno and Anders Dale, 1996
  * CVS Revision Info:
- *    $Author: greve $
- *    $Date: 2009/01/28 17:37:36 $
- *    $Revision: 1.276.2.14 $
+ *    $Author: nicks $
+ *    $Date: 2009/02/03 22:20:30 $
+ *    $Revision: 1.276.2.15 $
  *
  * Copyright (C) 2002-2009, CorTechs Labs, Inc. (La Jolla, CA) and
  * The General Hospital Corporation (Boston, MA).
@@ -146,7 +146,7 @@ int stricmp(const char* const str1, const char* const  str2)
 #endif // Windows_NT
 #include <unistd.h>
 #include <GL/gl.h>
-#include "GL/glut.h"
+#include "glut.h"
 #include "typedefs.h"
 #include "mgh_matrix.h"
 #include "label.h"
@@ -20724,7 +20724,7 @@ int main(int argc, char *argv[])   /* new main */
   nargs =
     handle_version_option
     (argc, argv,
-     "$Id: tksurfer.c,v 1.276.2.14 2009/01/28 17:37:36 greve Exp $", "$Name:  $");
+     "$Id: tksurfer.c,v 1.276.2.15 2009/02/03 22:20:30 nicks Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
