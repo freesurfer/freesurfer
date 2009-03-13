3fb8533748647e773481d2947a82c1293bc44494
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2009-03-13 16:26:26 +0000

    initialized nremoved properly.

--- mri_normalize/mri_normalize.c
+++ mri_normalize/mri_normalize.c
@@ -13,8 +13,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2009/03/13 14:00:09 $
- *    $Revision: 1.57 $
+ *    $Date: 2009/03/13 16:26:26 $
+ *    $Revision: 1.58 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -113,14 +113,14 @@ main(int argc, char *argv[]) {
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mri_normalize.c,v 1.57 2009/03/13 14:00:09 fischl Exp $",
+   "$Id: mri_normalize.c,v 1.58 2009/03/13 16:26:26 fischl Exp $",
    "$Name:  $",
    cmdline);
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mri_normalize.c,v 1.57 2009/03/13 14:00:09 fischl Exp $",
+           "$Id: mri_normalize.c,v 1.58 2009/03/13 16:26:26 fischl Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -610,7 +610,7 @@ MRIremoveWMOutliers(MRI *mri_src, MRI *mri_src_ctrl, MRI *mri_dst_ctrl,
   MRI       *mri_bin, *mri_dist, *mri_dist_sup ;
   float     max, thresh, val;
   HISTOGRAM *histo, *hsmooth ;
-  int       wm_peak, x, y, z, nremoved, whalf = 5 ;
+  int       wm_peak, x, y, z, nremoved = 0, whalf = 5 ;
 
   if (Gdiag & DIAG_WRITE && DIAG_VERBOSE_ON)
     MRIwrite(mri_src_ctrl, "sc.mgz") ;
