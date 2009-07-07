acaaee09473ed551e11c331c7cb12f828b6ce5d2
Doug Greve <greve@nmr.mgh.harvard.edu>
2009-07-07 02:01:54 +0000

    Copied files from dev. Fixed CSDread() fputc() error (now uses ungetc()). This error causes an error with surf and vol cluster with abs() simulations (in combo with an error in mri_glmfit)

--- utils/volcluster.c
+++ utils/volcluster.c
@@ -8,8 +8,8 @@
  * Original Author: Doug Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2009/01/15 23:13:45 $
- *    $Revision: 1.40.2.4 $
+ *    $Date: 2009/07/07 02:01:54 $
+ *    $Revision: 1.40.2.5 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -1502,7 +1502,7 @@ CLUSTER_SIM_DATA *CSDread(char *csdfile)
       // Check for max stat on this line
       c = fgetc(fp);
       while (c == ' ') c = fgetc(fp);
-      fputc(c,fp);
+      ungetc(c,fp);
       if (c != '\n')
       {
         fscanf(fp,"%lf",&d);
