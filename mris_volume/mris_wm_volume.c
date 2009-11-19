2a86f6f84e935b00a6ad77cb8da0124eaac58d70
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2009-11-19 19:02:39 +0000

    moved prototype to mrisurf.h where it belonged.

--- mris_volume/mris_wm_volume.c
+++ mris_volume/mris_wm_volume.c
@@ -10,8 +10,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2007/07/29 16:44:06 $
- *    $Revision: 1.5 $
+ *    $Date: 2009/11/19 19:02:39 $
+ *    $Revision: 1.6 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -45,10 +45,6 @@
 #include "label.h"
 #include "cma.h"
 
-
-double MRIScomputeWhiteVolume(MRI_SURFACE *mris, 
-                              MRI *mri_aseg, 
-                              double resolution) ;
 int main(int argc, char *argv[]) ;
 static int get_option(int argc, char *argv[]) ;
 
@@ -75,7 +71,7 @@ main(int argc, char *argv[])
   /* rkt: check for and handle version tag */
   nargs = handle_version_option 
     (argc, argv, 
-     "$Id: mris_wm_volume.c,v 1.5 2007/07/29 16:44:06 fischl Exp $", 
+     "$Id: mris_wm_volume.c,v 1.6 2009/11/19 19:02:39 fischl Exp $", 
      "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
