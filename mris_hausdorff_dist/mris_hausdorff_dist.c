84a0a4f8ac7f2f452f7758e9453fbd5c2dfa2987
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2009-11-19 18:36:10 +0000

    added support for binarization (-b)

--- mris_hausdorff_dist/mris_hausdorff_dist.c
+++ mris_hausdorff_dist/mris_hausdorff_dist.c
@@ -7,8 +7,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2008/05/22 23:51:10 $
- *    $Revision: 1.1 $
+ *    $Date: 2009/11/19 18:36:10 $
+ *    $Revision: 1.2 $
  *
  * Copyright (C) 2004-2007,
  * The General Hospital Corporation (Boston, MA).
@@ -44,7 +44,7 @@
 #include "MARS_DT_Boundary.h"
 
 static char vcid[] = 
-"$Id: mris_hausdorff_dist.c,v 1.1 2008/05/22 23:51:10 fischl Exp $";
+"$Id: mris_hausdorff_dist.c,v 1.2 2009/11/19 18:36:10 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 static int  get_option(int argc, char *argv[]) ;
@@ -55,6 +55,7 @@ static void print_version(void) ;
 
 const char *Progname ;
 static char *annot_name = NULL ;
+static float binarize = 0 ;
 
 int
 main(int argc, char *argv[])
@@ -69,7 +70,7 @@ main(int argc, char *argv[])
   /* rkt: check for and handle version tag */
   nargs = handle_version_option 
     (argc, argv, 
-     "$Id: mris_hausdorff_dist.c,v 1.1 2008/05/22 23:51:10 fischl Exp $", 
+     "$Id: mris_hausdorff_dist.c,v 1.2 2009/11/19 18:36:10 fischl Exp $", 
      "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -151,7 +152,12 @@ main(int argc, char *argv[])
       ErrorExit(ERROR_NOFILE, "%s: could not read label %s",
                 Progname, argv[3]) ;
     
-    
+
+    if (binarize > 0)
+    {
+      LabelThreshold(area1, binarize) ;
+      LabelThreshold(area2, binarize) ;
+    }
     MRIScomputeMetricProperties(mris) ;
     MRISdistanceTransform(mris, area1, DTRANS_MODE_SIGNED) ;
     MRIScopyValToVal2(mris) ;
@@ -188,6 +194,10 @@ get_option(int argc, char *argv[])
     print_version() ;
   else switch (toupper(*option))
     {
+    case 'B':
+      binarize = atof(argv[2]) ;
+      nargs = 1 ;
+      break ;
     case 'A':
       annot_name = argv[2] ;
       nargs = 1 ;
