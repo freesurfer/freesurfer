60db21f69661c162ca0b44946ab702f7579039a0
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2009-11-19 19:07:47 +0000

    added support for interior distances, and avoiding ventricles.

--- mri_distance_transform/mri_distance_transform.cpp
+++ mri_distance_transform/mri_distance_transform.cpp
@@ -8,8 +8,8 @@
  * Original Author: Florent Segonne 
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2008/09/12 16:16:12 $
- *    $Revision: 1.6 $
+ *    $Date: 2009/11/19 19:07:47 $
+ *    $Revision: 1.7 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -43,32 +43,41 @@ extern "C" {
 #include "version.h"
 #include "timer.h"
 #include "cma.h"
+
 }
 #include "fastmarching.h"
 
 char *Progname ;
 
+static float remove_csf_from_paths(MRI *mri_distance, MRI *mri_area, 
+                                   MRI *mri_csf) ;
 static int get_option(int argc, char *argv[]) ;
 static int MRIexpandDistancesIntoGrayMatter(MRI *mri_distance, MRI *mri_aseg, MRI *mri_white, int target_label) ;
 
+static MRI *mri_area = NULL ;
 static MRI *mri_white = NULL, *mri_aseg = NULL ;
 static char *surf_name = NULL ;
+static char *csf_name = NULL ;
 static float normalize = -1.0 ;
 static float wthresh = -1 ;
 static MRI *mri_thresh = NULL ;
 static float anterior_dist = -1 ;
 static float posterior_dist = -1 ;
 
+static float binarize = 0.0 ;
+
 static int ndilations = 0 ;
 MRI *MRIthresholdPosterior(MRI *mri_src, MRI *mri_dst, float posterior_dist) ;
 MRI *MRIthresholdAnterior(MRI *mri_src, MRI *mri_dst, float anterior_dist) ;
 
 int main(int argc, char *argv[]) {
-  MRI *mri,*mri_distance;
-  int label,mode;
-  float max_distance;
-  int   ac, nargs ;
-  char  **av ;
+  MRI         *mri,*mri_distance;
+  int         label,mode;
+  float       max_distance;
+  int         ac, nargs ;
+  char        **av ;
+  MRI_SURFACE *mris = NULL ;
+  MRI         *mri_orig_white = NULL, *mri_csf = NULL ;
 
   max_distance=10;
   mode=1;
@@ -96,6 +105,8 @@ int main(int argc, char *argv[]) {
   max_distance=atof(argv[3]);
   mode=atoi(argv[4]);
 
+  if (binarize > 0)
+    MRIbinarize(mri, mri, binarize, 0, label) ;
   if (ndilations > 0)
       MRIdilateLabel(mri, mri, label, ndilations) ;
   if (posterior_dist > 0)
@@ -116,7 +127,6 @@ int main(int argc, char *argv[]) {
 
   if (surf_name)
     {
-      MRI_SURFACE *mris ;
       int         x, y, z ;
 
       mris = MRISread(surf_name) ;
@@ -124,6 +134,23 @@ int main(int argc, char *argv[]) {
         ErrorExit(ERROR_NOFILE, "%s: could not read surface from %s", Progname, surf_name) ;
       mri_white = MRIclone(mri, NULL) ;
       MRISfillInterior(mris, mri_white->xsize, mri_white) ;
+      mri_orig_white = MRIcopy(mri_white, NULL) ; // before turning csf off
+
+      if (mri_aseg)  // turn off non-wm/cortex labels
+      {
+        int label ;
+        for (x = 0 ; x < mri_aseg->width ; x++)
+          for (y = 0 ; y < mri_aseg->height ; y++)
+            for (z = 0 ; z < mri_aseg->depth ; z++)
+            {
+              if (nint(MRIgetVoxVal(mri_white, x, y, z, 0)) == 0)
+                continue ;
+              label = nint(MRIgetVoxVal(mri_aseg, x, y, z, 0)) ;
+              if (!IS_CORTEX(label) && !IS_WHITE_CLASS(label))
+                MRIsetVoxVal(mri_white, x, y, z, 0, 0) ; // turn it off
+            }
+      }
+
       if (normalize > 0)
       {
 #if 0
@@ -141,12 +168,32 @@ int main(int argc, char *argv[]) {
         MRIwrite(mri_white, "w.mgz") ;
       if (mri_thresh != NULL)  // remove voxels that are in the ventricles
       {
+        int  csf_vox = 0 ;
+
+        mri_csf = MRIclone(mri_thresh, NULL) ;
         for (x = 0 ; x < mri_thresh->width ; x++)
           for (y = 0 ; y < mri_thresh->height ; y++)
             for (z = 0 ; z < mri_thresh->depth ; z++)
-              if (MRIgetVoxVal(mri_thresh, x, y, z, 0) < wthresh)
-                MRIsetVoxVal(mri_white, x, y, z, 0, 0) ;
-        MRIfree(&mri_thresh) ;
+              {
+                if (MRIgetVoxVal(mri_thresh, x, y, z, 0) < wthresh)
+                  {
+                    if ((int)MRIgetVoxVal(mri_white, x, y, z, 0) > 0)
+                    {
+                      MRIsetVoxVal(mri_csf, x, y, z, 0, 1) ;
+                      MRIsetVoxVal(mri_white, x, y, z, 0, 0) ;
+                      csf_vox++ ;
+                    }
+                  }
+              }
+        if (csf_name)
+          {
+            FILE *fp ;
+
+            printf("writing csf vox (%d) to %s\n", csf_vox, csf_name) ;
+            fp = fopen(csf_name, "w") ;
+            fprintf(fp, "%2.1f\n", csf_vox) ;
+            fclose(fp) ;
+          }
       }
 
       mri_aseg = MRIdilate(mri_white, NULL) ;
@@ -173,8 +220,62 @@ int main(int argc, char *argv[]) {
 
   if (normalize > 0)
     MRIscalarMul(mri_distance, mri_distance, 1.0/normalize) ;
-  MRIwrite(mri_distance,argv[5]);
+  if (mri_area)
+    {
+      float csf_vox ;
 
+      if (!mri_thresh || !mris)
+        ErrorExit(ERROR_BADPARM, 
+                  "%s: must specify -wsurf, -wthresh and -csf with -label",
+                  Progname) ;
+      MRIbinarize(mri_area, mri_area, 1, 0, 1) ;
+      MRIwrite(mri_csf, "csf.mgz") ;
+      csf_vox = remove_csf_from_paths(mri_distance, mri_area, mri_csf) ;
+      if (csf_name)
+        {
+#if 0
+          int      x, y, z ;
+          double   label_dist, dist ;
+#endif
+          FILE *fp ;
+
+#if 0
+          printf("checking for csf in regions closer than %2.1fmm\n", label_dist);
+          
+          /*
+            count the # of voxels that are csf in the interior of the white
+            matter, that are also closer to the source of the distance
+            transform than a specified label (to ignore distant csf)
+          */
+          label_dist = MRImeanInLabel(mri_distance, mri_area, 1) ;
+          csf_vox = 0 ;
+          for (x = 0 ; x < mri_thresh->width ; x++)
+            for (y = 0 ; y < mri_thresh->height ; y++)
+              for (z = 0 ; z < mri_thresh->depth ; z++)
+                {
+                  if ((int)MRIgetVoxVal(mri_orig_white, x, y, z, 0) == 0)
+                    continue ;  // not in interior of white surface
+                  if (MRIgetVoxVal(mri_thresh, x, y, z, 0) < wthresh)
+                    {
+                      dist = MRIvoxelMin(mri_distance, x, y, z, 3) ;
+                      if (dist < label_dist)
+                        csf_vox++ ;
+                    }
+                }
+#endif
+          printf("writing csf vox (%2.0f) to %s\n", csf_vox, csf_name) ;
+          fp = fopen(csf_name, "w") ;
+          fprintf(fp, "%2.1f\n", csf_vox) ;
+          fclose(fp) ;
+        }
+    }
+  
+  MRIwrite(mri_distance,argv[5]);
+  
+  if (mri_thresh)
+    MRIfree(&mri_thresh) ;
+  if (mris)
+    MRISfree(&mris) ;
   MRIfree(&mri);
   MRIfree(&mri_distance);
 
@@ -225,6 +326,25 @@ get_option(int argc, char *argv[]) {
     nargs = 1 ;
     printf("using anterior-most %2.3f mm of label only\n", anterior_dist) ;
   }
+  else if (!stricmp(option, "aseg"))
+  {
+    mri_aseg = MRIread(argv[2]) ;
+    if (mri_aseg == NULL)
+      ErrorExit(ERROR_NOFILE, "%s: could not read aseg volume from %s", Progname, argv[2]) ;
+    nargs = 1 ;
+    printf("using aseg volume %s\n", argv[2]) ;
+  }
+  else if (!stricmp(option, "label"))
+  {
+    mri_area = MRIread(argv[2]) ;
+    if (mri_area == NULL)
+      ErrorExit(ERROR_NOFILE, "%s: could not read label volume from %s", 
+                Progname,
+                argv[2]) ;
+    nargs = 1 ;
+    printf("computing CSF volume bordering regions closer than label %s\n",
+           argv[2]);
+  }
   else if (!stricmp(option, "posterior"))
   {
     posterior_dist = atof(argv[2]) ;
@@ -235,6 +355,11 @@ get_option(int argc, char *argv[]) {
     {
       nargs = 1 ;
       surf_name = argv[2] ;
+    }
+  else if (!stricmp(option, "csf"))
+    {
+      nargs = 1 ;
+      csf_name = argv[2] ;
     }
   else if (!stricmp(option, "normalize"))
     {
@@ -248,7 +373,7 @@ get_option(int argc, char *argv[]) {
       if (mri_thresh == NULL)
         ErrorExit(ERROR_NOFILE, "%s: could not read threshold volume %s",Progname, argv[2]) ;
       wthresh = atof(argv[3]) ;
-      printf("threshing interior volume %s at %2.0f  to remove ventricles from mask\n", argv[2], wthresh) ;
+      printf("threshing interior volume %s at %2.0f to remove ventricles from mask\n", argv[2], wthresh) ;
     }
   else if (!stricmp(option, "dilate"))
     {
@@ -256,6 +381,12 @@ get_option(int argc, char *argv[]) {
       nargs = 1;
       printf("performing %d dilations on labeled volume before computing distance transform\n", 
              ndilations) ;
+    }
+  else if (!stricmp(option, "b"))
+    {
+      binarize = atof(argv[2]) ;
+      nargs = 1;
+      printf("binarizing input data with thresh = %2.1f\n", binarize) ;
     }
   return(nargs) ;
 }
@@ -449,5 +580,93 @@ MRIthresholdPosterior(MRI *mri_src, MRI *mri_dst, float posterior_dist)
 
   MatrixFree(&m_vox2ras) ; VectorFree(&v_vox) ; VectorFree(&v_ras) ;
   return(mri_dst) ;
+}
+
+static float
+remove_csf_from_paths(MRI *mri_distance, MRI *mri_area, MRI *mri_csf)
+{
+  int   x, y, z, steps, xk, yk, zk, xi, yi, zi, csf_nbr,total_label_vox;
+  float xf, yf, zf, mean_csf_len, csf_len, total_csf_len ;
+  Real  dx, dy, dz, distance ;
+  MRI   *mri_orig_csf ;
+
+  mri_orig_csf = MRIcopy(mri_csf, NULL) ;
+
+  total_label_vox = 0;
+  total_csf_len = 0.0 ;
+  for (x = 0 ; x < mri_area->width ; x++)
+    for (y = 0 ; y < mri_area->height ; y++)
+      for (z = 0 ; z < mri_area->depth ; z++)
+      {
+        if ((int)MRIgetVoxVal(mri_area, x, y, z, 0) == 0)
+          continue ;
+        total_label_vox++ ;
+        /*
+          now trace path down distance transform gradient until we get to
+          the interior (dtrans <= 0) and count the # of adjacent csf
+          voxels.
+        */
+#define STEP_SIZE 0.1
+#define MAX_STEPS (1000/STEP_SIZE)
+        xf = x ; yf = y ; zf = z ;
+        steps = 0 ;
+        csf_len = 0.0 ;
+        do
+        {
+          csf_nbr = 0 ;
+          for (xk = -1 ; xk <= 1 && !csf_nbr ; xk++)
+          {
+            xi = mri_csf->xi[nint(xf+xk)] ;
+            for (yk = -1 ; yk <= 1 && !csf_nbr ; yk++)
+            {
+              yi = mri_csf->yi[nint(yf+yk)] ;
+              for (zk = -1 ; zk <= 1 && !csf_nbr ; zk++)
+              {
+                zi = mri_csf->zi[nint(zf+zk)] ;
+                if (fabs(xk)+ fabs(yk) + fabs(zk) > 1)
+                  continue ; // only 6-connected nbrs
+                if ((int)MRIgetVoxVal(mri_csf, xi, yi, zi, 0) != 0)
+                {
+                  // don't count it twice
+                  //                  MRIsetVoxVal(mri_csf, xi, yi, zi, 0, 0) ; 
+                  csf_nbr = 1 ;
+                }
+              }
+            }
+          }
+          if (csf_nbr)
+            csf_len += STEP_SIZE  ;
+
+          // take next step
+          MRIsampleVolumeGradient(mri_distance, xf, yf, zf,
+                                  &dx, &dy, &dz) ;
+          xf -= STEP_SIZE *dx ; yf -= STEP_SIZE *dy ; zf -= STEP_SIZE *dz ;
+          MRIsampleVolume(mri_distance, xf, yf, zf, &distance) ;
+        } while (distance > 0 && steps++ < MAX_STEPS) ;
+        if (steps >= MAX_STEPS) // couldn't get to label??
+        {
+          total_label_vox-- ;
+          continue ;
+        }
+        distance = MRIgetVoxVal(mri_distance, x, y, z, 0) ;
+        if (steps*STEP_SIZE > distance*1.1)
+        {
+          total_label_vox-- ;
+          DiagBreak();
+          continue ; // too suboptimal a path
+        }
+        if (csf_len > 100)
+          DiagBreak() ;
+        // correct for suboptimal paths
+        total_csf_len += (csf_len * distance / (steps*STEP_SIZE));
+        //        MRIsetVoxVal(mri_distance, x, y, z, 0, distance-csf_len) ;
+        /*        MRIcopy(mri_orig_csf, mri_csf) ; */ // restore csf labels
+      }
+  MRIfree(&mri_orig_csf) ;
+  if (total_label_vox > 0)
+    mean_csf_len = total_csf_len / total_label_vox ;
+  else
+    mean_csf_len = 0.0 ;
+  return(mean_csf_len) ;
 }
 
