c3240f106f7989c336cf7930b4fb26c10b0ad2b9
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2010-02-02 16:04:01 +0000

    added allisons handy script

---
+++ scripts/allisons_surfaces.tcl
@@ -0,0 +1,12 @@
+SetZoomLevel 2
+RedrawScreen
+
+
+SetSurfaceLineColor 0 0 0 0 1
+SetSurfaceLineColor 1 0 0 0 1
+SetSurfaceLineWidth 0 0 2
+SetSurfaceLineWidth 0 2 2
+SetSurfaceLineWidth 1 0 2
+SetSurfaceLineWidth 1 2 2
+SetDisplayFlag 5 0
+SetCursorColor .5 0 .5
