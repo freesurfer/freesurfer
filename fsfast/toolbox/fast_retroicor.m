b0ac82dabc609bed6c896f0d1350f7761225f4e2
Doug Greve <greve@nmr.mgh.harvard.edu>
2010-02-06 22:00:29 +0000

    Added trough

--- fsfast/toolbox/fast_retroicor.m
+++ fsfast/toolbox/fast_retroicor.m
@@ -1,16 +1,16 @@
-function [X M] = fast_retroicor(t,indpeak,M,peakamp)
-% [X M] = fast_retroicor(t,indpeak,<M>,<peakamp>)
+function [X M] = fast_retroicor(t,indpeak,M,peakamp,troughamp)
+% [X M] = fast_retroicor(t,indpeak,<M>,<peakamp>,<troughamp>)
 %
 % Based on Glover, Li, and Ress, Image-Based Method for Retrospective
 % Correction of Physiological Motion Effects in fMRI: RETROICOR. MRM,
 % 2000, 44:162-167.
 %
-% $Id: fast_retroicor.m,v 1.2 2010/01/21 23:59:43 greve Exp $
+% $Id: fast_retroicor.m,v 1.3 2010/02/06 22:00:29 greve Exp $
 
 X = [];
 
-if(nargin < 2 | nargin > 4)
-  fprintf('X = fast_retroicor(t,indpeak,<M>,peakamp)\n');
+if(nargin < 2 | nargin > 5)
+  fprintf('[X M] = fast_retroicor(t,indpeak,<M>,<peakamp>,<troughamp>)');
   return;
 end
 
@@ -26,7 +26,12 @@ if(isempty(M))
   M = floor(mean(idelta)/4);
 end
 if(~exist('peakamp','var')) peakamp = []; end
-if(isempty(peakamp)) peakamp = ones(npeaks); end
+if(isempty(peakamp)) peakamp = ones(npeaks,1); end
+if(~exist('troughamp','var')) troughamp = []; end
+if(isempty(troughamp)) troughamp = -ones(npeaks,1); end
+
+baseline = (peakamp + troughamp)/2;
+amp = (peakamp - troughamp)/2;
 
 X = zeros(nt,2*M);
 
@@ -40,9 +45,9 @@ for nthpeak = 1:npeaks-1
   phik = 2*pi*tk/dt;
   for h = 1:M
     c = 1 + 2*(h-1);
-    X(k1:k2,c) = peakamp(nthpeak)*cos(h*phik);
+    X(k1:k2,c) = amp(nthpeak)*cos(h*phik) + baseline(nthpeak);
     s = 2 + 2*(h-1);
-    X(k1:k2,s) = peakamp(nthpeak)*sin(h*phik);
+    X(k1:k2,s) = amp(nthpeak)*sin(h*phik) + baseline(nthpeak);
   end
 end
 
