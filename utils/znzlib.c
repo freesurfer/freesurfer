c0de6fff7f9e8746398990fcd33908e0f2d7402a
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2010-02-09 23:21:01 +0000

    no code change: fixed file header info

--- utils/znzlib.c
+++ utils/znzlib.c
@@ -1,33 +1,3 @@
-/**
- * @file  znzlib.c
- * @brief REPLACE_WITH_ONE_LINE_SHORT_DESCRIPTION
- *
- * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
- */
-/*
- * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
- * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 01:49:41 $
- *    $Revision: 1.5 $
- *
- * Copyright (C) 2002-2007,
- * The General Hospital Corporation (Boston, MA). 
- * All rights reserved.
- *
- * Distribution, usage and copying of this software is covered under the
- * terms found in the License Agreement file named 'COPYING' found in the
- * FreeSurfer source code root directory, and duplicated here:
- * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
- *
- * General inquiries: freesurfer@nmr.mgh.harvard.edu
- * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
- *
- */
-
-
-#ifndef HAVE_NIFTI_LIBS
-
 /** \file znzlib.c
     \brief Low level i/o interface to compressed and noncompressed files.
     Written by Mark Jenkinson, FMRIB
@@ -50,13 +20,6 @@
     NB: seeks for writable files with compression are quite restricted
 
 */
-
-#ifdef Darwin
-// prevent declaration conflicts with swab:
-#define _POSIX_C_SOURCE 1
-#endif
-#include "znzlib.h"
-
 /*
   znzlib.c  (zipped or non-zipped library)
 
@@ -72,6 +35,13 @@
 
 */
 
+#ifndef HAVE_NIFTI_LIBS
+
+#ifdef Darwin
+// prevent declaration conflicts with swab:
+#define _POSIX_C_SOURCE 1
+#endif
+#include "znzlib.h"
 
 /* Note extra argument (use_compression) where
    use_compression==0 is no compression
