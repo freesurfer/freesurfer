34f9e4a6501895c504170fa9ffbeb83b95079291
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2010-02-10 15:32:13 +0000

    fixed uninitialized variable.

--- mris_deform/mris_deform.c
+++ mris_deform/mris_deform.c
@@ -10,8 +10,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2010/02/10 14:03:38 $
- *    $Revision: 1.16 $
+ *    $Date: 2010/02/10 15:32:13 $
+ *    $Revision: 1.17 $
  *
  * Copyright (C) 2002-2009,
  * The General Hospital Corporation (Boston, MA). 
@@ -259,7 +259,7 @@ main(int argc, char *argv[]) {
   dp.min_ig_width = .75 ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_deform.c,v 1.16 2010/02/10 14:03:38 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mris_deform.c,v 1.17 2010/02/10 15:32:13 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -3058,7 +3058,7 @@ find_min_rms(double *kernel, double *intensity_profile, int profile_len,
                                           int nsamples, char *fname,double step, 
                                           double in_dist, double *errors))
 {
-  double    min_rms, sg_intensity_offset, best_sg_intensity_offset = 0, rms, ig_intensity_offset, best_ig_intensity_offset,
+  double    min_rms, sg_intensity_offset, best_sg_intensity_offset = 0, rms, ig_intensity_offset, best_ig_intensity_offset = 0.0,
     stria_intensity_offset, best_stria_intensity_offset = 0.0 ; 
   int       i, stria_len = nint((dp->stria_width+STRIA_OFFSET) / dp->step) ;
 
