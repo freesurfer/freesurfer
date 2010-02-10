c0cd69849d883c1b902c90594694adba3a165257
Krish Subramaniam <krish@nmr.mgh.harvard.edu>
2010-02-10 22:56:28 +0000

    fixed uninitialized var warning

--- mris_deform/mris_deform.c
+++ mris_deform/mris_deform.c
@@ -9,9 +9,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2010/02/10 22:31:47 $
- *    $Revision: 1.20 $
+ *    $Author: krish $
+ *    $Date: 2010/02/10 22:56:28 $
+ *    $Revision: 1.21 $
  *
  * Copyright (C) 2002-2009,
  * The General Hospital Corporation (Boston, MA). 
@@ -259,7 +259,7 @@ main(int argc, char *argv[]) {
   dp.min_ig_width = .75 ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_deform.c,v 1.20 2010/02/10 22:31:47 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mris_deform.c,v 1.21 2010/02/10 22:56:28 krish Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -1686,7 +1686,7 @@ static int
 mark_sg_index_okay(double *intensity_profile, int *sg_index_okay, double step, int len)
 {
   int    index, i, j ;
-  double valley_val, peak_val, previous_peak_val ;
+  double valley_val, peak_val=0, previous_peak_val=0 ;
   int    valley_index, peak_index, previous_peak ;
   
   for (index = 0 ; index < len ; index++)
