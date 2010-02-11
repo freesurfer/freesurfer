e79e9ba0dba805855189b0f37d1eb92d96df3771
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-02-11 20:57:01 +0000

    Starting to think about how to implement GCAmorph on the GPU

---
+++ include/gcamorphgpu.hpp
@@ -0,0 +1,52 @@
+/**
+ * @file  gcamorphgpu.hpp
+ * @brief Holds GCA morph data on the GPU
+ *
+ * 
+ */
+/*
+ * Original Author: Richard Edgar
+ * CVS Revision Info:
+ *    $Author: rge21 $
+ *    $Date: 2010/02/11 20:57:01 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright (C) 2002-2008,
+ * The General Hospital Corporation (Boston, MA). 
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+
+#ifndef GCA_MORPH_GPU_H
+#define GCA_MORPH_GPU_H
+
+
+
+// ==================================================================
+
+namespace GPU {
+
+  namespace Classes {
+
+    //! Class to hold a GCA morph on the GPU
+    class GCAmorphGPU {
+    public:
+
+    private:
+
+    };
+
+  }
+}
+
+
+
+#endif
