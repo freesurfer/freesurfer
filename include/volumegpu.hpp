47665442b2cdad1909400d90439ba02f61ae7d9b
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-02-12 20:56:44 +0000

    Filling in more functionality

--- include/volumegpu.hpp
+++ include/volumegpu.hpp
@@ -9,8 +9,8 @@
  * Original Author: Richard Edgar
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/02/12 20:33:19 $
- *    $Revision: 1.1 $
+ *    $Date: 2010/02/12 20:56:44 $
+ *    $Revision: 1.2 $
  *
  * Copyright (C) 2002-2008,
  * The General Hospital Corporation (Boston, MA). 
@@ -59,12 +59,41 @@ namespace GPU {
 	this->Release();
       }
 
+      // -------------------------------------------
+      // Data accessors
+      
+      //! Return the allocated dimensions
+      const dim3 GetDims( void ) const {
+	return( this->dims );
+      }
+
+      //! Return information about the file version
+      const char* VersionString( void ) const {
+	return "$Id: volumegpu.hpp,v 1.2 2010/02/12 20:56:44 rge21 Exp $";
+      }
+      
+      //! Return pointer to the cudaArray
+      const cudaArray* GetArray( void ) const {
+	return( this->dca_data );
+      }
+
+      //! Checks to see if CUDA array is allocated
+      bool HasArray( void ) const {
+	return( this->dca_data != NULL );
+      }
 
       // -------------------------------------------
       // Memory management
 
       //! Allocates storage of the given dimensions
       void Allocate( const dim3& myDims ) {
+	/*!
+	  Allocates GPU memory to hold a volume of the
+	  given size.
+	  This memory may include padding for GPU
+	  memory alignment requirements
+	  @params[in] myDims The dimensions required
+	*/
 	
 	// Check if we can re-use current memory
 	if( myDims == this->dims ) {
@@ -105,7 +134,16 @@ namespace GPU {
 	  exit( EXIT_FAILURE );
 	}
 
-	this->ReleaseArray();
+	// See if array is already allocated
+	if( this->HasArray() ) {
+	  /*
+	    We don't need to do anything.
+	    The current dimensions can only be set
+	    via the Allocate method, and this will
+	    release everything is there's a mismatch
+	  */
+	  return;
+	}
 
 	cudaChannelFormatDesc cd = cudaCreateChannelDesc<T>();
 
@@ -113,7 +151,6 @@ namespace GPU {
 					   &cd,
 					   this->extent ) );
       }
-
 
 
       //! Releases all data on GPU
@@ -136,6 +173,50 @@ namespace GPU {
 	}
       }
 
+      //! Supplies the size of buffer required on the host
+      size_t BufferSize( void ) const {
+	unsigned int nElements;
+
+	nElements = this->dims.x * this->dims.y * this->dims.z;
+
+	return( nElements * sizeof(T) );
+      }
+
+      // -------------------------------------------
+      // Data transfer
+
+      //! Copies data into a CUDA array for texturing
+      void SendArray( const cudaStream_t myStream = 0 ) {
+	/*!
+	  Method to copy the data currently on the GPU
+	  into a CUDA array, which can then be used in a
+	  texture.
+	  Copy is done asynchronously within the (optionally)
+	  given stream
+	*/
+	if( this->d_data.ptr == NULL ) {
+	  std::cerr << __FUNCTION__
+		    << ": GPU data not available!"
+		    << std::endl;
+	}
+	
+	if( this->dca_data == NULL ) {
+	  std::cerr << __FUNCTION__
+		    << ": CUDA array not allocated!"
+		    << std::endl;
+	  exit( EXIT_FAILURE );
+	}
+	
+	cudaMemcpy3DParms cp = {0};
+	
+	cp.srcPtr = this->d_data;
+	cp.dstArray = this->dca_data;
+	cp.extent = this->extent;
+	cp.kind = cudaMemcpyDeviceToDevice;
+	
+	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &cp, myStream ) );
+      }
+      
 
       // ============================================
     protected:
@@ -162,6 +243,59 @@ namespace GPU {
 	  derived classes
 	*/
 	return( ix + ( this->dims.x * ( iy + ( this->dims.y * iz ) ) ) );
+      }
+
+      // -------------------------------------------
+      // Data transfer
+
+      //! Send a buffer to the GPU
+      void SendBuffer( const T* const h_buffer,
+		       const cudaStream_t stream = 0 ) {
+	/*!
+	  Sends the given contiguous buffer on the
+	  host to the GPU.
+	  The data in the buffer must be ordered by
+	  the Index1D method.
+	  The copy is done asynchronously in the optionally
+	  defined stream.
+	  Accordingly, this routine may return before the
+	  copy is complete - the user is responsible for
+	  stream management.
+	*/
+
+	cudaMemcpy3DParms copyParams = {0};
+
+	copyParams.srcPtr = make_cudaPitchedPtr( (void*)h_buffer,
+						 this->dims.x*sizeof(T),
+						 this->dims.x,
+						 this->dims.y );
+	copyParams.dstPtr = this->d_data;
+	copyParams.extent = this->extent;
+	copyParams.kind = cudaMemcpyHostToDevice;
+
+	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &copyParams, stream ) );
+      }
+
+      //! Receive a buffer from the GPU
+      void RecvBuffer( T* const h_buffer,
+		       const cudaStream_t stream = 0 ) {
+	/*!
+	  Retrieves data from the GPU into the
+	  contiguous buffer on the host.
+	  The copy is done asynchronously, so the
+	  user is responsible for stream management
+	*/
+	
+	cudaMemcpy3DParms cpyPrms = {0};
+	cpyPrms.srcPtr = this->d_data;
+	cpyPrms.dstPtr = make_cudaPitchedPtr( (void*)h_buffer,
+					      this->dims.x*sizeof(T),
+					      this->dims.x,
+					      this->dims.y );
+	cpyPrms.extent = this->extent;
+	cpyPrms.kind = cudaMemcpyDeviceToHost;
+
+	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &cpyPrms, stream ) );
       }
 
       // ============================================
