52df6357ec75ed361fb81456970ce0e4c98f62b3
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-02-16 15:14:06 +0000

    Filling out the auxilliary class, and rethinking public/private on some members

--- include/volumegpu.hpp
+++ include/volumegpu.hpp
@@ -9,8 +9,8 @@
  * Original Author: Richard Edgar
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/02/12 20:56:44 $
- *    $Revision: 1.2 $
+ *    $Date: 2010/02/16 15:14:06 $
+ *    $Revision: 1.3 $
  *
  * Copyright (C) 2002-2008,
  * The General Hospital Corporation (Boston, MA). 
@@ -39,6 +39,103 @@ namespace GPU {
 
   namespace Classes {
 
+    //! Templated ancilliary class for use in kernel calls
+    template<typename T>
+    class VolumeArgGPU {
+    public:
+
+      //! Out of range value
+      static const T kOutOfRangeVal = 0;
+
+      //! Padded data size
+      dim3 dims;
+      //! Extent of allocated 3D array
+      cudaExtent extent;
+      //! Pointer to the allocated memory
+      cudaPitchedPtr data;
+
+      // --------------------------------------
+      // Constructors
+      VolumeArgGPU( void ) : dims(make_uint3(0,0,0)),
+			     extent(make_cudaExtent(0,0,0)),
+			     data(make_cudaPitchedPtr(NULL,0,0,0)) {};
+      
+      // --------------------------------------
+      // Subscripting operators
+      
+      //! Unsafe RHS subscripting routine
+      __device__ T UnsafeAt( const unsigned int ix,
+			     const unsigned int iy,
+			     const unsigned int iz ) const {
+	const char* data = reinterpret_cast<const char*>(this->data.ptr);
+	// Rows are pitch apart
+	size_t pitch = this->data.pitch;
+	// Slices are slicePitch apart
+	size_t slicePitch = pitch * this->extent.height;
+	
+	const char* slice = data + ( iz * slicePitch );
+	const char* row = slice + ( iy * pitch );
+	
+	return( reinterpret_cast<const T*>(row)[ix] );
+      }
+
+      //! RHS Subscripting operator
+      __device__ T operator() ( const unsigned int ix,
+				const unsigned int iy,
+				const unsigned int iz ) const {
+	/*!
+	  This version of the subscripting operator is safe.
+	  It bounds checks the requested dimensions,
+	  and returns kOutofRangeVal if out of range
+	*/
+	if( this->InVolume( ix, iy, iz ) ) {
+	  return( this->UnsafeAt( ix, iy, iz ) );
+	} else {
+	  return( this->kOutOfRangeVal );
+	}
+      }
+      
+      
+      //! LHS subscripting operator
+      __device__ T& operator() ( const unsigned int ix,
+				 const unsigned int iy,
+				 const unsigned int iz ) {
+	char* data = reinterpret_cast<char*>(this->data.ptr);
+	size_t pitch = this->data.pitch;
+	size_t slicePitch = pitch * this->extent.height;
+    
+	char* slice = data + ( iz * slicePitch );
+	char* row = slice + ( iy * pitch );
+	
+	return( reinterpret_cast<T*>(row)[ix] );
+      }
+
+
+      // --------------------------------------
+      
+      //! Checks if given co-ordinate is in the volume
+      __device__ bool InVolume( const unsigned int ix,
+				const unsigned int iy,
+				const unsigned int iz ) const {
+	bool res = ( ix < (this->dims.x) );
+	res = res && ( iy < (this->dims.y) );
+	res = res && ( iz < (this->dims.z) );
+
+	return( res );
+      }
+
+      //! Checks is given location is in volume within given tolerance
+      __device__ bool InFuzzyVolume( const float3& r,
+				     const float tol ) const {
+	bool res = ( (r.x>-tol) && (r.x<(this->dims.x+tol)) );
+	res = res && (r.y>-tol) && (r.y<(this->dims.y+tol));
+	res = res && (r.z>-tol) && (r.z<(this->dims.z+tol));
+
+	return( res );
+      }
+    };
+
+
 
     //! Templated class to hold volume data on the GPU
     template<typename T>
@@ -60,6 +157,20 @@ namespace GPU {
       }
 
       // -------------------------------------------
+      // Conversion operator
+
+      //! Converts volume to a kernel argument
+      operator VolumeArgGPU<T>( void ) {
+	VolumeArgGPU<T> vag;
+
+	vag.dims = this->dims;
+	vag.extent = this->extent;
+	vag.data = this->d_data;
+	
+	return( vag );
+      }
+
+      // -------------------------------------------
       // Data accessors
       
       //! Return the allocated dimensions
@@ -69,7 +180,7 @@ namespace GPU {
 
       //! Return information about the file version
       const char* VersionString( void ) const {
-	return "$Id: volumegpu.hpp,v 1.2 2010/02/12 20:56:44 rge21 Exp $";
+	return "$Id: volumegpu.hpp,v 1.3 2010/02/16 15:14:06 rge21 Exp $";
       }
       
       //! Return pointer to the cudaArray
@@ -185,6 +296,60 @@ namespace GPU {
       // -------------------------------------------
       // Data transfer
 
+      //! Send a buffer to the GPU
+      void SendBuffer( const T* const h_buffer,
+		       const cudaStream_t stream = 0 ) {
+	/*!
+	  Sends the given contiguous buffer on the
+	  host to the GPU.
+	  The data in the buffer must be ordered by
+	  the Index1D method.
+	  The copy is done asynchronously in the optionally
+	  defined stream.
+	  Accordingly, this routine may return before the
+	  copy is complete - the user is responsible for
+	  stream management.
+	*/
+
+	cudaMemcpy3DParms copyParams = {0};
+
+	copyParams.srcPtr = make_cudaPitchedPtr( (void*)h_buffer,
+						 this->dims.x*sizeof(T),
+						 this->dims.x,
+						 this->dims.y );
+	copyParams.dstPtr = this->d_data;
+	copyParams.extent = this->extent;
+	copyParams.kind = cudaMemcpyHostToDevice;
+
+	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &copyParams, stream ) );
+      }
+
+
+
+      //! Receive a buffer from the GPU
+      void RecvBuffer( T* const h_buffer,
+		       const cudaStream_t stream = 0 ) {
+	/*!
+	  Retrieves data from the GPU into the
+	  contiguous buffer on the host.
+	  The copy is done asynchronously, so the
+	  user is responsible for stream management
+	*/
+	
+	cudaMemcpy3DParms cpyPrms = {0};
+	cpyPrms.srcPtr = this->d_data;
+	cpyPrms.dstPtr = make_cudaPitchedPtr( (void*)h_buffer,
+					      this->dims.x*sizeof(T),
+					      this->dims.x,
+					      this->dims.y );
+	cpyPrms.extent = this->extent;
+	cpyPrms.kind = cudaMemcpyDeviceToHost;
+
+	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &cpyPrms, stream ) );
+      }
+
+
+
       //! Copies data into a CUDA array for texturing
       void SendArray( const cudaStream_t myStream = 0 ) {
 	/*!
@@ -218,18 +383,6 @@ namespace GPU {
       }
       
 
-      // ============================================
-    protected:
-      //! Dimensions of the volume
-      dim3 dims;
-      
-      //! Extent of the allocated 3D array
-      cudaExtent extent;
-      //! Pointer to the allocated device memory
-      cudaPitchedPtr d_data;
-      //! CUDA array pointer for texturing
-      cudaArray *dca_data;
-
       // --------------------------------------
 
       //! Provides 1D indexing in to contiguous host memory
@@ -245,61 +398,19 @@ namespace GPU {
 	return( ix + ( this->dims.x * ( iy + ( this->dims.y * iz ) ) ) );
       }
 
-      // -------------------------------------------
-      // Data transfer
-
-      //! Send a buffer to the GPU
-      void SendBuffer( const T* const h_buffer,
-		       const cudaStream_t stream = 0 ) {
-	/*!
-	  Sends the given contiguous buffer on the
-	  host to the GPU.
-	  The data in the buffer must be ordered by
-	  the Index1D method.
-	  The copy is done asynchronously in the optionally
-	  defined stream.
-	  Accordingly, this routine may return before the
-	  copy is complete - the user is responsible for
-	  stream management.
-	*/
-
-	cudaMemcpy3DParms copyParams = {0};
-
-	copyParams.srcPtr = make_cudaPitchedPtr( (void*)h_buffer,
-						 this->dims.x*sizeof(T),
-						 this->dims.x,
-						 this->dims.y );
-	copyParams.dstPtr = this->d_data;
-	copyParams.extent = this->extent;
-	copyParams.kind = cudaMemcpyHostToDevice;
-
-	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &copyParams, stream ) );
-      }
-
-      //! Receive a buffer from the GPU
-      void RecvBuffer( T* const h_buffer,
-		       const cudaStream_t stream = 0 ) {
-	/*!
-	  Retrieves data from the GPU into the
-	  contiguous buffer on the host.
-	  The copy is done asynchronously, so the
-	  user is responsible for stream management
-	*/
-	
-	cudaMemcpy3DParms cpyPrms = {0};
-	cpyPrms.srcPtr = this->d_data;
-	cpyPrms.dstPtr = make_cudaPitchedPtr( (void*)h_buffer,
-					      this->dims.x*sizeof(T),
-					      this->dims.x,
-					      this->dims.y );
-	cpyPrms.extent = this->extent;
-	cpyPrms.kind = cudaMemcpyDeviceToHost;
-
-	CUDA_SAFE_CALL_ASYNC( cudaMemcpy3DAsync( &cpyPrms, stream ) );
-      }
-
       // ============================================
     private:
+      //! Dimensions of the volume
+      dim3 dims;
+      
+      //! Extent of the allocated 3D array
+      cudaExtent extent;
+      //! Pointer to the allocated device memory
+      cudaPitchedPtr d_data;
+      //! CUDA array pointer for texturing
+      cudaArray *dca_data;
+
+
       // -------------------------------------------
       // Prevent copying
 
