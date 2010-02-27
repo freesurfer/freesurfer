49523c51885ef9cfe98f818a82b6fa39106df51c
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2010-02-27 00:39:52 +0000

    FIX: compiler warning

--- utils/MRIio_old.c
+++ utils/MRIio_old.c
@@ -8,10 +8,10 @@
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
  *    $Author: nicks $
- *    $Date: 2006/12/29 01:49:30 $
- *    $Revision: 1.2 $
+ *    $Date: 2010/02/27 00:39:52 $
+ *    $Revision: 1.3 $
  *
- * Copyright (C) 2002-2007,
+ * Copyright (C) 2002-2010,
  * The General Hospital Corporation (Boston, MA). 
  * All rights reserved.
  *
@@ -21,7 +21,6 @@
  * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
  *
  * General inquiries: freesurfer@nmr.mgh.harvard.edu
- * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
  *
  */
 
@@ -41,7 +40,7 @@ static void MGHprint_error(char *str) ;
 static void
 MGHprint_error(char *str)
 {
-  printf(str);
+  printf("%s",str);
   exit(0);
 }
 
