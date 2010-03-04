f3f62b3865ac0ca4904b191b3f6fd13fe5343d1a
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-03-04 15:51:15 +0000

    We're changing the test API slightly

--- include/testgpu.h
+++ include/testgpu.h
@@ -11,8 +11,8 @@
  * Original Author: Richard Edgar
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/03/02 20:36:42 $
- *    $Revision: 1.3 $
+ *    $Date: 2010/03/04 15:51:15 $
+ *    $Revision: 1.4 $
  *
  * Copyright (C) 2002-2008,
  * The General Hospital Corporation (Boston, MA). 
@@ -39,7 +39,7 @@ extern "C" {
 
   void GCAMorphSendBefore( const GCAM* src );
   void GCAMorphSendAfter( const GCAM* src );
-  void GCAMorphCompareBeforeAfter( void );
+  void GCAMorphCompareBeforeAfter( GCAM* dst );
 #if defined(__cplusplus)
 };
 #endif
