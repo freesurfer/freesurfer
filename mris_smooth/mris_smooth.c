a77dfe130fed947acd06b5e953988d7ca703cc13
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2010-03-04 15:40:29 +0000

    added parameter to MRISvertexSpacingStats.

--- mris_smooth/mris_smooth.c
+++ mris_smooth/mris_smooth.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: greve $
- *    $Date: 2010/03/01 23:11:14 $
- *    $Revision: 1.24 $
+ *    $Author: fischl $
+ *    $Date: 2010/03/04 15:40:29 $
+ *    $Revision: 1.25 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -44,7 +44,7 @@
 #include "version.h"
 
 static char vcid[] =
-  "$Id: mris_smooth.c,v 1.24 2010/03/01 23:11:14 greve Exp $";
+  "$Id: mris_smooth.c,v 1.25 2010/03/04 15:40:29 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -87,13 +87,13 @@ main(int argc, char *argv[]) {
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mris_smooth.c,v 1.24 2010/03/01 23:11:14 greve Exp $",
+   "$Id: mris_smooth.c,v 1.25 2010/03/04 15:40:29 fischl Exp $",
    "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mris_smooth.c,v 1.24 2010/03/01 23:11:14 greve Exp $",
+           "$Id: mris_smooth.c,v 1.25 2010/03/04 15:40:29 fischl Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -423,7 +423,8 @@ MRIShistoThresholdGaussianCurvatureToMarked(MRI_SURFACE *mris, double pct) {
   int        vno, num, b, bin_no, bin_thresh = 0, vno_min, vno_max, skipped, nvertices ;
   VERTEX     *v ;
 
-  dmean = MRIScomputeVertexSpacingStats(mris, &dsigma,&dmin, &dmax, &vno_min, &vno_max);
+  dmean = MRIScomputeVertexSpacingStats(mris, &dsigma,&dmin, &dmax, &vno_min, &vno_max, 
+                                        CURRENT_VERTICES);
   min_curv = 1e8 ;
   max_curv = -min_curv ;
 
