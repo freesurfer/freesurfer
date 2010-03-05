ac71034b4232ae222dd22b30894f973773f5db17
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-03-05 18:48:47 +0000

    Starting to think about gcamLogLikelihoodEnergy for the GPU Creating a new file to hold this

---
+++ utils/gcamorphenergygpu.cu
@@ -0,0 +1,42 @@
+/**
+ * @file  gcamorphenergygpu.cu
+ * @brief Holds routines to compute GCAmorph energies on the GPU
+ *
+ * 
+ */
+/*
+ * Original Author: Richard Edgar
+ * CVS Revision Info:
+ *    $Author: rge21 $
+ *    $Date: 2010/03/05 18:48:47 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright (C) 2002-2008,
+ * The General Hospital Corporation (Boston, MA). 
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+#include "chronometer.hpp"
+
+#include "gcamorphgpu.hpp"
+
+
+
+
+// ==============================================================
+
+namespace GPU {
+  namespace Classes {
+
+
+
+  }
+}
