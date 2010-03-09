20dbb4ea94f804a29774acee3aa5a6d95d7c7b27
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-03-09 17:45:42 +0000

    Starting to add actual output

--- utils/gcamorphtestutils.cpp
+++ utils/gcamorphtestutils.cpp
@@ -9,8 +9,8 @@
  * Original Author: Richard Edgar
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/03/09 16:49:35 $
- *    $Revision: 1.1 $
+ *    $Date: 2010/03/09 17:45:42 $
+ *    $Revision: 1.2 $
  *
  * Copyright (C) 2002-2008,
  * The General Hospital Corporation (Boston, MA). 
@@ -34,6 +34,8 @@
 
 #include "gcamorphtestutils.h"
 
+const unsigned int nDims = 3;
+enum dimID{ iX, iY, iZ };
 
 // ======================================================================
 
@@ -60,12 +62,19 @@ void WriteGCAMforMetricProperties( const GCAM* src, const char* fName ) {
     Currently uses the C API, since the C++ one doesn't seem to be available
   */
 
+  enum myVars{ rx, ry, rz, origArea, invalid, area, area1, area2 };
+
+  if( src->ninputs != 1 ) {
+    std::cerr << __FUNCTION__ << ": Must have ninputs=1!" << std::endl;
+    exit( EXIT_FAILURE );
+  }
+
   // Construct the filename
   std::string fileName( fName );
 
   fileName += ".nc";
 
-  std::cout << __FUNCTION__ << ": Writing file " << fileName << std::endl;
+  std::cout << __FUNCTION__ << ": Writing file " << fileName << " ... ";
 
   // Reference for the file
   int ncid;
@@ -74,11 +83,61 @@ void WriteGCAMforMetricProperties( const GCAM* src, const char* fName ) {
   NC_SAFE_CALL( nc_create( fileName.c_str(), NC_CLOBBER, &ncid ) );
 
 
+  // Set up the dimension references
+  int dimIDs[nDims];
+  NC_SAFE_CALL( nc_def_dim( ncid, "x", src->width, &dimIDs[iX] ) );
+  NC_SAFE_CALL( nc_def_dim( ncid, "y", src->height, &dimIDs[iY] ) );
+  NC_SAFE_CALL( nc_def_dim( ncid, "z", src->depth, &dimIDs[iZ] ) );
+
+  
+
+  // Set up the variables
+  int varIDs[8];
+  NC_SAFE_CALL( nc_def_var( ncid, "r_x",
+			    NC_DOUBLE, nDims, dimIDs, &varIDs[rx] ) );
+  NC_SAFE_CALL( nc_def_var( ncid, "r_y",
+			    NC_DOUBLE, nDims, dimIDs, &varIDs[ry] ) );
+  NC_SAFE_CALL( nc_def_var( ncid, "r_z",
+			    NC_FLOAT, nDims, dimIDs, &varIDs[rz] ) );
+
+  NC_SAFE_CALL( nc_def_var( ncid, "origArea",
+			    NC_FLOAT, nDims, dimIDs, &varIDs[origArea] ) );
+  NC_SAFE_CALL( nc_def_var( ncid, "area",
+			    NC_FLOAT, nDims, dimIDs, &varIDs[area] ) );
+  NC_SAFE_CALL( nc_def_var( ncid, "area1",
+			    NC_FLOAT, nDims, dimIDs, &varIDs[area2] ) );
+  NC_SAFE_CALL( nc_def_var( ncid, "area2",
+			    NC_FLOAT, nDims, dimIDs, &varIDs[area2] ) );
 
 
+  NC_SAFE_CALL( nc_def_var( ncid, "invalid",
+			    NC_CHAR, nDims, dimIDs, &varIDs[invalid] ) );
 
 
+  // Make the end of the 'definition' region
+  NC_SAFE_CALL( nc_enddef( ncid ) );
+
+
+  // Write the data (in a very ugly way)
+  for( int i=0; i<src->width; i++ ) {
+    for( int j=0; j<src->height; j++ ) {
+      for( int k=0; k<src->depth; k++ ) {
+	const GCA_MORPH_NODE& gcamn = src->nodes[i][j][k];
+	
+	const size_t count[nDims] = { 1, 1, 1};
+	const size_t loc[nDims] = { i, j, k };
+	
+	
+
+	nc_put_vara_double( ncid, varIDs[rx], loc, count, &gcamn.x );
+	nc_put_vara_double( ncid, varIDs[ry], loc, count, &gcamn.y );
+	nc_put_vara_double( ncid, varIDs[ry], loc, count, &gcamn.z );
+      }
+    }
+  }
 
   // Close the file
   NC_SAFE_CALL( nc_close( ncid ) );
+
+  std::cout << "complete" << std::endl;
 }
