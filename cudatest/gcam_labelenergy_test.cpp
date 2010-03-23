c62f35ebf72d1c878894b39cc2b64c1f0a83dc19
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-03-23 17:52:10 +0000

    Adding a test of the labelenergy routine

---
+++ cudatest/gcam_labelenergy_test.cpp
@@ -0,0 +1,125 @@
+#include <cstdlib>
+#include <cstdio>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+using namespace std;
+
+#include <boost/program_options.hpp>
+namespace bpo = boost::program_options;
+
+#include "gcamorph.h"
+#include "gcamorphtestutils.hpp"
+
+#include "chronometer.hpp"
+
+
+#ifdef FS_CUDA
+#include "devicemanagement.h"
+#endif
+
+
+
+// ==========================================================
+
+const string gcamFileDefault = "gcamInput";
+#ifdef FS_CUDA
+const string outFileDefault = "energyLabel.gpu";
+#else
+const string outFileDefault = "energyLabel.cpu";
+#endif
+
+
+string gcamFilename;
+string outFilename;
+
+
+const char* Progname = "gcam_labelenergy_test";
+
+
+
+
+// ==========================================================
+
+void ReadCommandLine( int ac, char* av[] ) {
+
+  try {
+    bpo::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "Produce help message" )
+      ("gcam", bpo::value<string>(&gcamFilename)->default_value(gcamFileDefault), "Input gcam filename (.nc will be appended)" )
+      ("output", bpo::value<string>(&outFilename)->default_value(outFileDefault), "Output filename" )
+      ;
+
+    
+    bpo::variables_map vm;
+    bpo::store( bpo::parse_command_line( ac, av, desc ), vm );
+    bpo::notify( vm );
+    
+    if( vm.count( "help" ) ) {
+      cout << desc << endl;
+      exit( EXIT_SUCCESS );
+    }
+  }
+  catch( exception& e ) {
+    cerr << "Error: " << e.what() << endl;
+    exit( EXIT_FAILURE );
+  }
+  catch( ... ) {
+    cerr << "Unknown exception" << endl;
+    exit( EXIT_FAILURE );
+  }
+}
+
+
+
+// ==========================================================
+
+int main( int argc, char *argv[] ) {
+
+  SciGPU::Utilities::Chronometer tTotal;
+  GCAMorphUtils myUtils;
+
+  cout << "GCAM Jacobian Energy Tester" << endl;
+  cout << "===========================" << endl << endl;
+
+#ifdef FS_CUDA
+  AcquireCUDADevice();
+#else
+  cout << "CPU Version" << endl;
+#endif
+  cout << endl;
+
+  ReadCommandLine( argc, argv );
+
+  // ============================================
+
+  // Read the input file
+
+  GCAM* gcam = NULL;
+  myUtils.Read( &gcam, gcamFilename );
+  
+  // ============================================
+  // Perform the calculation
+  double labelenergy = gcamLabelEnergy( gcam, NULL, 0 );
+
+
+  // ============================================
+  // Produce output
+  cout << "Label Energy = " << labelenergy << endl;
+
+  cout << "Writing file " << outFilename << endl;
+
+  ofstream oFile;
+  oFile.open( outFilename.c_str(), ios::binary | ios::out | ios::trunc );
+
+  oFile.write( reinterpret_cast<const char*>(&labelenergy),
+	       sizeof(labelenergy) );
+  oFile.close();
+
+  // ====================================
+  // Release
+  GCAMfree( &gcam );
+}
