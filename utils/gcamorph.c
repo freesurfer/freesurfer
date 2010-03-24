c0ac7bbb9527c4446dbbb2c41a9627f304b4bc72
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-03-24 16:46:29 +0000

    Hacking GPU version of gcamComputeSSE into place Have to do something nasty to shut up 'unused function' errors from the compiler

--- utils/gcamorph.c
+++ utils/gcamorph.c
@@ -11,8 +11,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/03/24 14:50:00 $
- *    $Revision: 1.182 $
+ *    $Date: 2010/03/24 16:46:29 $
+ *    $Revision: 1.183 $
  *
  * Copyright (C) 2002-2010,
  * The General Hospital Corporation (Boston, MA). 
@@ -5233,15 +5233,22 @@ GCAMcomputeRMS(GCA_MORPH *gcam, MRI *mri, GCA_MORPH_PARMS *parms)
 static double
 gcamComputeSSE(GCA_MORPH *gcam, MRI *mri, GCA_MORPH_PARMS *parms)
 {
-  double sse, ms_sse, l_sse, s_sse, ls_sse, j_sse, d_sse, a_sse, 
-    nvox, label_sse, map_sse, dtrans_sse, 
-    binary_sse, area_intensity_sse, spring_sse, exp_sse ;
+  double sse;
 
 #ifdef FS_CUDA
-  float tmp;
+  sse = gcamComputeSSEonGPU( gcam, mri, parms );
 
-  tmp = gcamComputeSSEonGPU( gcam, mri, parms );
+  // What follows is an ugly hack to silence a whiny compiler
+  const int flagOff = 0;
+  
+  // It issues warnings for unused static functions, which become errors
+  if( flagOff ) {
+    // Hence, we put the functions inside a 'dead' if block
 #endif
+  
+  double ms_sse, l_sse, s_sse, ls_sse, j_sse, d_sse, a_sse;
+  double nvox, label_sse, map_sse, dtrans_sse;
+  double binary_sse, area_intensity_sse, spring_sse, exp_sse;
 
   if (!DZERO(parms->l_area_intensity))
     parms->nlt = gcamCreateNodeLookupTable(gcam, parms->mri, parms->nlt) ;
@@ -5325,6 +5332,10 @@ gcamComputeSSE(GCA_MORPH *gcam, MRI *mri, GCA_MORPH_PARMS *parms)
   sse =
     spring_sse+area_intensity_sse+binary_sse+l_sse + ms_sse + 
     s_sse + j_sse + d_sse + a_sse + label_sse + map_sse + exp_sse + dtrans_sse ;
+#ifdef FS_CUDA
+  // End of ugly hack to stop compiler whining about unused static functions
+  }
+#endif
   return(sse) ;
 }
 
