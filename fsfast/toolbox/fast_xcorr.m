87541d85597f438ade4ba89aa4195d50cf2de729
Doug Greve <greve@nmr.mgh.harvard.edu>
2010-03-31 16:33:22 +0000

    Computes cross-correlation function

---
+++ fsfast/toolbox/fast_xcorr.m
@@ -0,0 +1,28 @@
+function rho = fast_xcorr(x,y,demean)
+% rho = fast_xcorr(x,y,demean)
+% Default is to demean. Set demean=0 to turn off.
+%   rho = sum(x.*y)./sqrt(sum(x.^2) .* sum(y.^2));
+% $Id: fast_xcorr.m,v 1.1 2010/03/31 16:33:22 greve Exp $
+
+rho = [];
+if(nargin < 2 | nargin > 3)
+  fprintf('rho = fast_xcorr(x,y,demean)\n');
+  return;
+end
+
+if(nargin == 2) demean = 1; end
+
+[nt nv] = size(x);
+
+if(demean)
+  x = x - repmat(mean(x),[nt 1]);
+  y = y - repmat(mean(y),[nt 1]);
+end
+
+rho = sum(x.*y)./sqrt(sum(x.^2) .* sum(y.^2));
+
+return;
+
+
+
+
