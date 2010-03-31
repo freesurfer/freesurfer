d9377a6e18a8f658bb42276da191983a1304adf0
Doug Greve <greve@nmr.mgh.harvard.edu>
2010-03-31 16:31:57 +0000

    Cardiac response function from Chang 2009

---
+++ fsfast/toolbox/fast_crf.m
@@ -0,0 +1,29 @@
+function crf = fast_crf(t)
+% crf = fast_crf(t)
+%
+% Cardiac Response Function from Chang, et al, NI 44 (2009),
+% 857-869, Equation 5 and Figure 6.
+%
+% $Id: fast_crf.m,v 1.1 2010/03/31 16:31:57 greve Exp $
+
+if(nargin ~= 1)
+  fprintf('crf = fast_crf(t)\n');
+  return;
+end
+
+e = -0.5*((t-12).^2)/9;
+
+crf = 0.6 * (t.^2.7) .* exp(-t/1.6) - 16*exp(e)/sqrt(2*pi*9);
+
+return;
+
+
+
+
+
+
+
+
+
+
+
