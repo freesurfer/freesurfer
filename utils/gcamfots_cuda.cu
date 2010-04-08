165fd606c92f9ca8debe6656ede4e4f225a2479e
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-04-08 13:35:33 +0000

    Starting to fill in gcamFindOptimalTimestep for the GPU Only partially complete Compiles, but obviously not tested

--- utils/gcamfots_cuda.cu
+++ utils/gcamfots_cuda.cu
@@ -8,11 +8,11 @@
   * (2002) Neuron, 33:341-355.
  */
 /*
- * Original Author: Bruce Fischl
+ * Original Author: Richard Edgar
  * CVS Revision Info:
  *    $Author: rge21 $
- *    $Date: 2010/04/01 18:37:26 $
- *    $Revision: 1.1 $
+ *    $Date: 2010/04/08 13:35:33 $
+ *    $Revision: 1.2 $
  *
  * Copyright (C) 2002-2010,
  * The General Hospital Corporation (Boston, MA). 
@@ -27,6 +27,8 @@
  *
  */
 
+#include "macros.h"
+
 #include "mriframegpu.hpp"
 #include "gcamorphgpu.hpp"
 #include "gcamorphenergy.hpp"
@@ -37,8 +39,88 @@ float FindOptimalTimestep( GPU::Classes::GCAmorphGPU& gcam,
 			   GCA_MORPH_PARMS *parms,
 			   const GPU::Classes::MRIframeGPU<T>& mri ) {
 
-  float min_dt = 0;
+  GPU::Algorithms::GCAmorphEnergy gcamEnergy;
 
+  float min_dt = 0;
+  float start_rms, min_rms, rms;
+  float orig_dt, start_dt, max_dt;
+  int bad, prev_neg;
+  int suppressed = 0;
+
+  gcam.ClearMomentum();
+
+  start_rms = min_rms = gcamEnergy.ComputeRMS( gcam, mri, parms );
+
+  // Find right order of magnitude for time step
+  orig_dt = parms->dt;
+  if( DZERO( orig_dt ) ) {
+    orig_dt = 1e-6;
+    std::cerr << "orig_dt = 0 in FindOptimalTimestep" << std::endl;
+  }
+
+  // Define a pretty broad search range initially
+  start_dt = (sqrt(parms->navgs)+1)*orig_dt / (10*16.0*16.0);
+  max_dt =   (sqrt(parms->navgs)+1)*orig_dt * (10*16.0*16.0);
+
+
+  int i = 0;
+  do {
+
+    if( i++ > 0 ) {
+      start_dt *= 0.01;
+    }
+
+    if( DEQUAL( start_dt, 0 ) ) {
+      break;
+    }
+
+    bad = 0;
+    prev_neg = 0;
+
+    for( parms->dt = start_dt; parms->dt <= max_dt; parms->dt *= 4 ) {
+      
+      gcam.ApplyGradient( parms );
+      rms = gcamEnergy.ComputeRMS( gcam, mri, parms );
+      gcam.UndoGradient();
+
+      if( (gcam.neg > 0) && 0 ) {
+	std::cerr << __FUNCTION__
+		  << ": How did you get here?" << std::endl;
+	std::cerr << "Exiting on line " << __LINE__ << std::endl;
+	exit( EXIT_FAILURE );
+      } else {
+	prev_neg = suppressed = 0;
+      }
+
+
+      if( (gcam.neg>0) && DEQUAL(parms->dt, start_dt) ) {
+        start_dt *= 0.1;
+        parms->dt /= 4;
+        continue ;
+      }
+
+      if( (gcam.neg>0) && parms->noneg == True ) {
+	break;
+      }
+
+      if( rms < min_rms ) {
+	min_rms = rms;
+	min_dt = parms->dt;
+      }
+
+      if( DEQUAL( min_dt, max_dt ) ) {
+	max_dt *= 10;
+      }
+    }
+
+    if( i > 10 ) {
+      break;
+    }
+
+    max_dt = start_dt*10 ; /* will only iterate if min was at start_dt */
+    
+
+  } while( DEQUAL( min_dt, start_dt ) );
 
 
   return( min_dt );
