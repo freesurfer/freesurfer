7bfcfcc1a64787cddf216c47c85b94a270e0cffb
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-04-16 16:53:55 +0000

    Separating out the float, short and uchar tests Will make fine tuning of testing much easier

--- test_tm/regressiontests/convgauss/gauss.lua
+++ test_tm/regressiontests/convgauss/gauss.lua
@@ -3,13 +3,56 @@
 module( ..., package.seeall )
 
 
+require( "tools.freesurfer" )
+
+-- Make access to the FreeSurfer module quicker
+local FS = tools.freesurfer
+
+
+
 -- List of gaussian widths to use
 sigmaVals = { 0.5, 2, 6.383, 12.766, 4, 15, 21, 29.2 }
+
+-- Function to return the list of files to use
+function GetFiles( pattern )
+   return FS.GetFiles( FS.MRIdir(), pattern )
+end
+
+
+
+-- Function to turn an input filename and gaussian sigma into a test name
+local function testName( input, sigma )
+   local inputItems = FS.split( input, "-" )
+
+   return inputItems[1].."-"..inputItems[2].."-"..sigma
+end
+
+
+
+-- Function to generate a list of tests
+function testGen( inputs, sigmas, tol )
+   local testTable = {}
+
+   -- inputs is a table of input file names
+   -- sigmas is a table of gaussian sigmas
+   -- tol is the tolerance for mri_diff
+   -- Generates nInputs*nSigmas tests
+
+   for i,input in ipairs(inputs) do
+      for s,sigma in ipairs(sigmas) do
+	 local tName = testName( input, sigma )
+	 table.insert( testTable, { id=tName, tol=tol, input=FS.MRIdir()..input, sigma=sigma } )
+      end
+   end
+
+   return testTable
+end
+
 
 
 -- Script to run
 script = [[
-      export MAINCMDS=" --input=$(projectDir)/inputs/$(input) --sigma=$(sigma) --repeats=1"
+      export MAINCMDS=" --input=$(input) --sigma=$(sigma) --repeats=1"
       export CPUOUT="$(outputDir)/$(id).cpu.mgz"
       export GPUOUT="$(outputDir)/$(id).gpu.mgz"
 
