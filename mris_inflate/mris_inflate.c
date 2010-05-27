8dc93398842dbbfe510d411086c6f19c39dc99f9
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2010-05-27 21:35:00 +0000

    ENH: output which GPU device is being used

--- mris_inflate/mris_inflate.c
+++ mris_inflate/mris_inflate.c
@@ -11,9 +11,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: greve $
- *    $Date: 2010/03/03 18:10:53 $
- *    $Revision: 1.39 $
+ *    $Author: nicks $
+ *    $Date: 2010/05/27 21:35:00 $
+ *    $Revision: 1.40 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA).
@@ -46,8 +46,12 @@
 #include "macros.h"
 #include "version.h"
 
+#ifdef FS_CUDA
+#include "devicemanagement.h"
+#endif // FS_CUDA
+
 static char vcid[] =
-  "$Id: mris_inflate.c,v 1.39 2010/03/03 18:10:53 greve Exp $";
+  "$Id: mris_inflate.c,v 1.40 2010/05/27 21:35:00 nicks Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -86,13 +90,18 @@ main(int argc, char *argv[])
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mris_inflate.c,v 1.39 2010/03/03 18:10:53 greve Exp $",
+   "$Id: mris_inflate.c,v 1.40 2010/05/27 21:35:00 nicks Exp $",
    "$Name:  $", cmdline);
+
+#ifdef FS_CUDA
+  // Force CUDA initialisation
+  AcquireCUDADevice();
+#endif // FS_CUDA
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mris_inflate.c,v 1.39 2010/03/03 18:10:53 greve Exp $",
+           "$Id: mris_inflate.c,v 1.40 2010/05/27 21:35:00 nicks Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
