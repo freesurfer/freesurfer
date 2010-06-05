dd62844b30af3aac719987fe4fb8f25d8cf5a248
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2010-06-05 08:23:37 +0000

    fixed uninitialized var

--- histo_synthesize/histo_synthesize.c
+++ histo_synthesize/histo_synthesize.c
@@ -8,8 +8,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2010/06/04 18:34:38 $
- *    $Revision: 1.2 $
+ *    $Date: 2010/06/05 08:23:37 $
+ *    $Revision: 1.3 $
  *
  * Copyright (C) 2002-2007,
  * The General Hospital Corporation (Boston, MA). 
@@ -92,7 +92,7 @@ main(int argc, char *argv[]) {
   MRI          *mri, *histo, *hsynth ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: histo_synthesize.c,v 1.2 2010/06/04 18:34:38 fischl Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: histo_synthesize.c,v 1.3 2010/06/05 08:23:37 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -327,6 +327,7 @@ HISTOsynthesize(MRI *mri, MRI *histo, int test_slice, int train_slice, MRI *hsyn
     MRIsetValues(hsynth, 4080) ;
     box_test.x = mri->width; box_test.y = mri->height ; box_test.z = mri->depth ;
     box_test.dx = 0 ; box_test.dy = 0 ; box_test.dz = 0 ;
+    z = test_slice ;
     for (x = 0 ; x < mri->width ; x++)
     {
       for (y = 0 ; y < mri->height ; y++)
