9bc8f8530e8c7686e13e5b401e2ac17135fab4d4
Doug Greve <greve@nmr.mgh.harvard.edu>
2010-07-01 20:28:33 +0000

    Fixed bug

--- matlab/save_nifti.m
+++ matlab/save_nifti.m
@@ -10,7 +10,7 @@ function err = save_nifti(hdr,niftifile)
 % reshaped to 27307x1x6xnframes. This is for handling the 7th
 % order icosahedron used by FS group analysis.
 %
-% $Id: save_nifti.m,v 1.10 2010/07/01 17:31:19 greve Exp $
+% $Id: save_nifti.m,v 1.11 2010/07/01 20:28:33 greve Exp $
 
 %
 % save_nifti.m
@@ -18,8 +18,8 @@ function err = save_nifti(hdr,niftifile)
 % Original Author: Doug Greve
 % CVS Revision Info:
 %    $Author: greve $
-%    $Date: 2010/07/01 17:31:19 $
-%    $Revision: 1.10 $
+%    $Date: 2010/07/01 20:28:33 $
+%    $Revision: 1.11 $
 %
 % Copyright (C) 2002-2007,
 % The General Hospital Corporation (Boston, MA). 
@@ -51,10 +51,9 @@ end
 
 % Check for ico7
 sz = size(hdr.vol);
-nspatial = prod(sz(1:3));
-if(nspatial == 163842)
+if(sz(1) == 163842)
   fprintf('save_nifti: ico7 reshaping\n');
-  dim = [27307 1 6 hdr.dim(4)];
+  dim = [27307 1 6 size(hdr.vol,4)];
   hdr.vol = reshape(hdr.vol, dim);
 end
 
