e8f3f7be24e2139be6cbb0bac176691b41a2e126
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2010-07-30 19:48:22 +0000

    Adding class for affine transformations These are very common, so allocating and deallocating 4x4 matrices all other the place is rather inefficient As a bonus, this class uses SSE when set to be of type float

---
+++ include/affine.hpp
@@ -0,0 +1,310 @@
+/**
+ * @file  affine.hpp
+ * @brief Affine transformations
+ *
+ */
+/*
+ * Original Authors: Richard Edgar
+ * CVS Revision Info:
+ *    $Author: rge21 $
+ *    $Date: 2010/07/30 19:48:22 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright (C) 2002-2010,
+ * The General Hospital Corporation (Boston, MA).
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+#include <xmmintrin.h>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+
+#include "matrix.h"
+
+
+namespace Freesurfer {
+
+  const unsigned int kAffineVectorSize = 4;
+  const unsigned int kAffineMatrixSize = kAffineVectorSize * kAffineVectorSize;
+
+  // ===============================================================
+
+  //! Affine vector class
+  template<typename T>
+  class AffineVector {
+
+  public:
+    // Indexing enum
+    enum ComponentIndices{ iX=0, iY=1, iZ=2, iW=3 };
+
+    // ----------------------------------------------------
+    // No constructors or destructors
+
+
+    // ----------------------------------------------------
+    // Assignment operator
+    
+    AffineVector<T>& operator=( const AffineVector<T>& src ) {
+      memcpy( &(this->vec[0]),
+	      &(src.vec[0]),
+	      kAffineVectorSize * sizeof(T) );
+      return( *this );
+    }
+
+
+    // ----------------------------------------------------
+    // Get and set
+    
+    template<typename U>
+    void Set( const U& x, const U& y, const U& z ) {
+      this->vec[iX] = x;
+      this->vec[iY] = y;
+      this->vec[iZ] = z;
+      this->vec[iW] = 1;
+    }
+
+    template<typename U>
+    void Get( U& x, U& y, U& z ) const {
+      x = this->vec[iX];
+      y = this->vec[iY];
+      z = this->vec[iZ];
+    }
+
+    void GetFloor( int& x, int& y, int& z ) const {
+      x = static_cast<int>( floor( this->vec[iX] ) );
+      y = static_cast<int>( floor( this->vec[iY] ) );
+      z = static_cast<int>( floor( this->vec[iZ] ) );
+    }
+      
+
+    // ----------------------------------------------------
+    // I/O
+
+    void Print( std::ostream& os = std::cout ) const {
+      os << "("
+	 << std::setw(9) << std::setprecision(3) << this->vec[iX]
+	 << ","
+	 << std::setw(9) << std::setprecision(3) << this->vec[iY]
+	 << ","
+	 << std::setw(9) << std::setprecision(3) << this->vec[iZ]
+	 << " )";      
+    }
+
+    // ----------------------------------------------------
+
+    //! The actual vector, aligned on 16 byte boundary
+    T vec[kAffineVectorSize] __attribute__ ((aligned (16)));
+
+
+  };
+
+
+  // Specialise float copy to use SSE
+  template<>
+  AffineVector<float>&
+  AffineVector<float>::operator=( const AffineVector<float>& src ) {
+    __m128 tmp = _mm_load_ps( &(src.vec[0]) );
+    _mm_store_ps( &(this->vec[0]), tmp );
+    return( *this );
+  }
+
+
+  // ================================================================
+
+  //! Affine matrix class
+  template<typename T>
+  class AffineMatrix {
+
+  public:
+    // ----------------------------------------------------
+    // No constructors or destructors
+
+
+    // ----------------------------------------------------
+    // Assignment operator
+    AffineMatrix<T>& operator=( const AffineMatrix<T>& src ) {
+      memcpy( &(this->mat[0]),
+	      &(src.mat[0]),
+	      kAffineMatrixSize*sizeof(T) );
+      return( *this );
+    }
+
+
+    // ----------------------------------------------------
+    // Indexing operators
+
+    T operator()( const unsigned int i, const unsigned int j ) const {
+      return( this->mat[i+j*kAffineVectorSize] );
+    }
+
+    T& operator()( const unsigned int i, const unsigned int j ) {
+      return( this->mat[i+j*kAffineVectorSize] );
+    }
+
+    // ----------------------------------------------------
+    // Setting
+    
+    void Set( const MATRIX* src ) {
+      
+      for( unsigned int i=0; i<kAffineVectorSize; i++ ) {
+	for( unsigned int j=0; j<kAffineVectorSize; j++ ) {
+	  (*this)(i,j) = src->data[j+(i*kAffineVectorSize)];
+	}
+      }
+    }
+
+    void Clear( void ) {
+      memset( &(this->mat[0]), 0, kAffineMatrixSize*sizeof(T) );
+    }
+
+    void Identity( void ) {
+      this->Clear();
+      this->mat[0]  = 1;
+      this->mat[5]  = 1;
+      this->mat[10] = 1;
+      this->mat[15] = 1;
+    }
+
+    // ---------------------------------------------------
+    // Mathematical operations
+
+    //! Matrix-Vector multiplication
+    AffineVector<T> operator*( const AffineVector<T>& v ) const {
+      AffineVector<T> res;
+
+      for( unsigned int i=0; i<kAffineVectorSize; i++ ) {
+	T tmp = 0;
+	for( unsigned int j=0; j<kAffineVectorSize; j++ ) {
+	  tmp += (*this)(i,j) * v.vec[j];
+	}
+	res.vec[i] = tmp;
+      }
+
+      return( res );
+    }
+
+    //! Matrix-Matrix multiplication
+    AffineMatrix<T> operator*( const AffineMatrix<T>& m ) const {
+      AffineMatrix<T> res;
+      
+      for( unsigned int i=0; i<kAffineVectorSize; i++ ) {
+	for( unsigned int j=0; j<kAffineVectorSize; j++ ) {
+	  T tmp = 0;
+	  for( unsigned int k=0; k<kAffineVectorSize; k++ ) {
+	    tmp += (*this)(i,k) * m(k,j);
+	  }
+	  res(i,j) = tmp;
+	}
+      }
+
+      return( res );
+    }
+
+    // ----------------------------------------------------
+    // I/O
+    
+    void Print( std::ostream& os = std::cout ) const {
+    
+      for( unsigned int i=0; i<kAffineVectorSize; i++ ) {
+	for( unsigned int j=0; j<kAffineVectorSize; j++ ) {
+	  os << std::setw(9) << std::setprecision(3) << (*this)( i, j );
+	}
+	os << std::endl;
+      }
+      
+    }
+
+
+    //! The actual matrix, column major and 16 byte aligned
+    T mat[kAffineMatrixSize] __attribute__ ((aligned (16)));
+
+  };
+
+  
+  //! Specialise matrix-vector for float and use SSE
+  template<>
+  AffineVector<float>
+  AffineMatrix<float>::operator*( const AffineVector<float>& v ) const {
+
+    __m128 resLine, col, val;
+
+    // Use x component of input vector
+    val = _mm_set1_ps( v.vec[0] );
+    col = _mm_load_ps( &(this->mat[0]) );
+    resLine = _mm_mul_ps( col, val );
+
+    // Use y component of input vector
+    val = _mm_set1_ps( v.vec[1] );
+    col = _mm_load_ps( &(this->mat[4] ) );
+    resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+
+    // Use z component of input vector
+    val = _mm_set1_ps( v.vec[2] );
+    col = _mm_load_ps( &(this->mat[8] ) );
+    resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+    
+    // And the w
+    val = _mm_set1_ps( v.vec[3] );
+    col = _mm_load_ps( &(this->mat[12] ) );
+    resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+
+    AffineVector<float> res;
+    _mm_store_ps( &(res.vec[0]), resLine );
+
+    return( res );
+  }
+
+  //! Specialise matrix-matrix for float and use SES
+  template<>
+  AffineMatrix<float>
+  AffineMatrix<float>::operator*( const AffineMatrix<float>& m ) const {
+
+    __m128 resLine, col, val;
+    AffineMatrix<float> res;
+    const float *mLoc = &m.mat[0];
+    const float *thisLoc = &(this->mat[0]);
+    float *resLoc = &(res.mat[0]);
+
+    // Loop over columns
+    for( unsigned int j=0; j<kAffineVectorSize; j++ ) {
+      val = _mm_set1_ps( *mLoc );
+      col = _mm_load_ps( thisLoc );
+      resLine = _mm_mul_ps( col, val );
+      mLoc += 1;
+      thisLoc += kAffineVectorSize;
+
+      val = _mm_set1_ps( *mLoc );
+      col = _mm_load_ps( thisLoc );
+      resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+      mLoc += 1;
+      thisLoc += kAffineVectorSize;
+
+      val = _mm_set1_ps( *mLoc );
+      col = _mm_load_ps( thisLoc );
+      resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+      mLoc += 1;
+      thisLoc += kAffineVectorSize;
+
+      val = _mm_set1_ps( *mLoc );
+      col = _mm_load_ps( thisLoc );
+      resLine = _mm_add_ps( _mm_mul_ps( col, val), resLine );
+      mLoc += 1;
+      thisLoc = &(this->mat[0]); // Reset for next iteration
+
+      _mm_store_ps( resLoc, resLine );
+      resLoc += kAffineVectorSize;
+    }
+    return( res );
+
+  }
+
+}
