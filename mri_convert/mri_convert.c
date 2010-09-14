aa60dd525d4a3d8a9ac9bf993da5937d93b2e44e
Doug Greve <greve@nmr.mgh.harvard.edu>
2010-09-14 21:03:12 +0000

    Fixed --reorder (missing else if)

--- mri_convert/mri_convert.c
+++ mri_convert/mri_convert.c
@@ -6,9 +6,9 @@
 /*
  * Original Author: Bruce Fischl (Apr 16, 1997)
  * CVS Revision Info:
- *    $Author: gregt $
- *    $Date: 2010/08/12 17:11:35 $
- *    $Revision: 1.170 $
+ *    $Author: greve $
+ *    $Date: 2010/09/14 21:03:12 $
+ *    $Revision: 1.171 $
  *
  * Copyright (C) 2002-2010,
  * The General Hospital Corporation (Boston, MA). 
@@ -195,7 +195,7 @@ int main(int argc, char *argv[]) {
 
   make_cmd_version_string
     (argc, argv,
-     "$Id: mri_convert.c,v 1.170 2010/08/12 17:11:35 gregt Exp $", 
+     "$Id: mri_convert.c,v 1.171 2010/09/14 21:03:12 greve Exp $", 
      "$Name:  $",
      cmdline);
 
@@ -302,7 +302,7 @@ int main(int argc, char *argv[]) {
     handle_version_option
     (
       argc, argv,
-      "$Id: mri_convert.c,v 1.170 2010/08/12 17:11:35 gregt Exp $", 
+      "$Id: mri_convert.c,v 1.171 2010/09/14 21:03:12 greve Exp $", 
       "$Name:  $"
       );
   if (nargs && argc - nargs == 1)
@@ -310,13 +310,12 @@ int main(int argc, char *argv[]) {
   argc -= nargs;
 
   for(i = 1;i < argc;i++) {
-    if(strcmp(argv[i], "-version2") == 0)
-      exit(97);
-    if(strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--reorder") == 0) {
+    if(strcmp(argv[i], "-version2") == 0) exit(97);
+    else if(strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--reorder") == 0) {
       get_ints(argc, argv, &i, reorder_vals, 3);
       reorder_flag = TRUE;
     }
-    if(strcmp(argv[i], "-r4") == 0 || strcmp(argv[i], "--reorder4") == 0) {
+    else if(strcmp(argv[i], "-r4") == 0 || strcmp(argv[i], "--reorder4") == 0) {
       get_ints(argc, argv, &i, reorder4_vals, 4);
       reorder4_flag = TRUE;
     }
@@ -1337,7 +1336,7 @@ int main(int argc, char *argv[]) {
             "= --zero_ge_z_offset option ignored.\n");
   }
 
-  printf("$Id: mri_convert.c,v 1.170 2010/08/12 17:11:35 gregt Exp $\n");
+  printf("$Id: mri_convert.c,v 1.171 2010/09/14 21:03:12 greve Exp $\n");
   printf("reading from %s...\n", in_name_only);
 
   if (in_volume_type == OTL_FILE) {
