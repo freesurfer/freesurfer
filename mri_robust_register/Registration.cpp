fb0ec1620195a2b9782ac5fa5603a77e6d5904a0
Martin Reuter <mreuter@nmr.mgh.harvard.edu>
2010-11-12 22:54:16 +0000

    added treatment for --satit if low sat value still yields no outliers (simply terminate), e.g. if all mov volumes are the same

--- mri_robust_register/Registration.cpp
+++ mri_robust_register/Registration.cpp
@@ -8,8 +8,8 @@
  * Original Author: Martin Reuter
  * CVS Revision Info:
  *    $Author: mreuter $
- *    $Date: 2010/11/11 22:32:17 $
- *    $Revision: 1.50 $
+ *    $Date: 2010/11/12 22:54:16 $
+ *    $Revision: 1.51 $
  *
  * Copyright (C) 2008-2009
  * The General Hospital Corporation (Boston, MA).
@@ -886,6 +886,18 @@ double Registration::findSaturation (MRI * mriS, MRI* mriT, const vnl_matrix < d
 		 {
 		   satmax = sat;
 			 wmax = wcheck;
+		 }
+		 
+		 // if sat low (sensitive) and still not many outliers
+		 // quit, to prevent long (infinite) search
+		 // e.g. if source and target are same image
+		 if (sat < 6 && wcheck < 0.04)
+		 {
+		    satmax = sat;
+				satmin = sat;
+				wmax= wcheck;
+				wmin= wcheck;
+				break;
 		 }
 	}
 	
