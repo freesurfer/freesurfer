84754a92f1e1e0e4899b4e099cc8affa7f36b302
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-01-31 19:44:53 +0000

    deleted extra copies of source

--- svm-lib/svm-pr-loqo.c
+++ svm-lib/svm-pr-loqo.c
@@ -1,31 +1,3 @@
-/**
- * @file  svm-pr-loqo.c
- * @brief REPLACE_WITH_ONE_LINE_SHORT_DESCRIPTION
- *
- * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
- */
-/*
- * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
- * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:17 $
- *    $Revision: 1.2 $
- *
- * Copyright (C) 2002-2007,
- * The General Hospital Corporation (Boston, MA). 
- * All rights reserved.
- *
- * Distribution, usage and copying of this software is covered under the
- * terms found in the License Agreement file named 'COPYING' found in the
- * FreeSurfer source code root directory, and duplicated here:
- * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
- *
- * General inquiries: freesurfer@nmr.mgh.harvard.edu
- * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
- *
- */
-
-
 /*
  * File:        pr_loqo.c
  * Purpose:     solves quadratic programming problem for pattern recognition
