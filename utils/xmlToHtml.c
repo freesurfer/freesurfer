e369219d4487e43f2e768e1df4a00f2dfefd4782
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-01-31 20:01:51 +0000

    missing aseg_edit_svm and xmlToHtml

--- utils/xmlToHtml.c
+++ utils/xmlToHtml.c
@@ -1,7 +1,30 @@
-//xmlToWiki.c
-//Reads an .xml file and outputs it in a wiki format
+/**
+ * @file  xmlToHtml.c
+ * @brief Reads an .xml file and outputs it in a wiki format
+ *
+ */
+/*
+ * Original Author: Greg Terrono
+ * CVS Revision Info:
+ *    $Author: nicks $
+ *    $Date: 2011/01/31 20:01:52 $
+ *    $Revision: 1.2 $
+ *
+ * Copyright (C) 2010-2011,
+ * The General Hospital Corporation (Boston, MA). 
+ * All rights reserved.
+ *
+ * Distribution, usage and copying of this software is covered under the
+ * terms found in the License Agreement file named 'COPYING' found in the
+ * FreeSurfer source code root directory, and duplicated here:
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ *
+ * General inquiries: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
 //-------------------------------------------------------------------
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,7 +54,7 @@ int main(int argc, char *argv[])
   if (argc==1)
   {
     fprintf(stderr, "No file name passed.");
-    return;
+    return -1;
   }
   //Opens the .xml file given as an argument
   doc = xmlParseFile(argv[1]);
@@ -39,38 +62,41 @@ int main(int argc, char *argv[])
   if (doc == NULL )
   {
     fprintf(stderr,"Document not parsed successfully. \n");
-    return;
-	}
+    return -1;
+  }
 
   cur = xmlDocGetRootElement(doc);
-	
+
   //Catches the error of passing a blank document
   if (cur == NULL)
   {
     fprintf(stderr,"Empty document.\n");
-		xmlFreeDoc(doc);
-		return;
-	}
+    xmlFreeDoc(doc);
+    return -1;
+  }
 
   //Catches the error of passing a document with a different root tag
-  if (!tagNameIs("help",cur)) 
+  if (!tagNameIs("help",cur))
   {
-		fprintf(stderr,"Document id of the wrong type,the root node is not help.\n");
-		xmlFreeDoc(doc);
-		return;
-	}
-  
+    fprintf(stderr,"Document id of the wrong type, "
+            "the root node is not help.\n");
+    xmlFreeDoc(doc);
+    return -1;
+  }
+
   if (argc==2)
     printf("No output specified.\nOutput will be sent to output.html.\n");
   FILE *outFile;
   outFile=fopen(argc>2?argv[2]:"output.html", "w");
 
   cur=cur->xmlChildrenNode;
-  
-  fprintf(outFile,"<html>\n<body>\n<basefont face=\"sans-serif\"></basefont>\n");
+
+  fprintf(outFile,
+          "<html>\n<body>\n<basefont face=\"sans-serif\"></basefont>\n");
+
   int exampleNum=1;
   //Outputs the file
-  while (cur != NULL) 
+  while (cur != NULL)
   {
     //Outputs the arugments of the file
     if (tagNameIs("arguments",cur)||tagNameIs("outputs",cur))
@@ -90,26 +116,33 @@ int main(int argc, char *argv[])
           int first=1, justIntro=0, justArg=0;
           // fprintf (outFile,"\n<table border=\"3\">\n");
           while (1)
-          { 
+          {
             if (!tagNameIs("text",argumentElement))
             {
               if ((first||justIntro)&&!tagNameIs("intro",argumentElement))
-                fprintf (outFile,"\n<table border=\"3\">\n<tr><th>%s</th><th>Explanation</th></tr>\n",(tagNameIs("arguments",cur))?"Argument":"Output");
-              
+                fprintf (outFile,
+                         "\n<table border=\"3\">\n<tr><th>%s</th><th>"
+                         "Explanation</th></tr>\n",
+                         (tagNameIs("arguments",cur))?"Argument":"Output");
+
               if (justArg)
               {
-                if (tagNameIs("argument",argumentElement)||tagNameIs("output",argumentElement))
+                if (tagNameIs("argument",argumentElement)||
+                    tagNameIs("output",argumentElement))
                   fprintf(outFile, "<td>&nbsp;</td>\n");
                 justArg=0;
               }
               else if (tagNameIs("explanation",argumentElement))
                 fprintf(outFile, "</tr>\n<tr>\n<td>&nbsp;</td>\n");
-              if (tagNameIs("argument",argumentElement)||tagNameIs("output",argumentElement))
+              if (tagNameIs("argument",argumentElement)||
+                  tagNameIs("output",argumentElement))
                 justArg=1;
               if (tagNameIs("intro",argumentElement))
               {
                 char *c, *intro;
-                c= (char *)xmlNodeListGetString(doc, argumentElement->xmlChildrenNode, 1);
+                c= (char *)xmlNodeListGetString
+                  (doc,
+                   argumentElement->xmlChildrenNode, 1);
                 if (first)
                 {
                   intro= malloc(strlen("\n<h3>")+strlen(c)+1);
@@ -123,14 +156,16 @@ int main(int argc, char *argv[])
                 }
                 intro=strcat(intro,c);
                 free(c);
-                int i=1;
-                while (tagNameIs("intro",argumentElement->next)||tagNameIs("text",argumentElement->next))
+                while (tagNameIs("intro",argumentElement->next)||
+                       tagNameIs("text",argumentElement->next))
                 {
                   if (!tagNameIs("text",argumentElement->next))
                   {
                     char *content;
-                    content= (char *)xmlNodeListGetString(doc, argumentElement->next->xmlChildrenNode, 1);
-                    char *secondHalf=malloc(strlen("<br />") +strlen(content)+ 1);
+                    content= (char *)xmlNodeListGetString
+                      (doc, argumentElement->next->xmlChildrenNode, 1);
+                    char *secondHalf=
+                      malloc(strlen("<br />") +strlen(content)+ 1);
                     strcpy(secondHalf,"<br />");
                     secondHalf=strcat(secondHalf,content);
                     char *temp= malloc(strlen(intro)+strlen(secondHalf)+1);
@@ -154,13 +189,14 @@ int main(int argc, char *argv[])
               {
                 if (first)
                   first=0;
-                else if ((tagNameIs("argument",argumentElement)||tagNameIs("output",argumentElement))&&!justIntro)
-                    fprintf(outFile, "</tr>\n");
-                
+                else if ((tagNameIs("argument",argumentElement)||
+                          tagNameIs("output",argumentElement))&&!justIntro)
+                  fprintf(outFile, "</tr>\n");
+
                 justIntro=0;
                 printTableContents(doc,argumentElement,outFile);
               }
-            } 
+            }
             argumentElement= argumentElement->next;
             if (argumentElement==NULL)
             {
@@ -183,15 +219,13 @@ int main(int argc, char *argv[])
         fprintf(outFile," %d",exampleNum++);
       fprintf(outFile,"</h1>");
       printContents(doc,cur,outFile);
-		}
+    }
     cur=cur->next;
   }
 
   fprintf(outFile,"</html>\n</body>\n");
 
-
-
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
 //Changes a string to unpper case
@@ -208,7 +242,7 @@ void toUpperCase(char *c)
 void printName(xmlNodePtr cur, FILE *f)
 {
   xmlChar *n;
-  n=cur->name;
+  n=(xmlChar*)cur->name;
   toUpperCase((char*)n);
   replaceUnderscore((char*)n);
   fprintf(f,"\n\n<h1>%s",n);
@@ -227,9 +261,9 @@ void printContents(xmlDocPtr doc, xmlNodePtr cur,FILE *f)
 {
   xmlChar *contents;
   contents = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-  int i;
+  unsigned int i;
   fprintf (f,"\n<p>");
-  for(i=0; i<strlen(contents); i++)
+  for (i=0; i<strlen((char*)contents); i++)
   {
     if (*(contents+i)=='\n')
       fprintf (f,"<br />");
@@ -253,31 +287,31 @@ void printTableContents(xmlDocPtr doc, xmlNodePtr cur,FILE *f)
 {
   xmlChar *contents;
   contents = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-  int i;
+  unsigned int i;
   if (tagNameIs("argument",cur)||tagNameIs("output",cur))
     fprintf(f,"<tr>\n");
   if (tagNameIs("intro",cur))
     fprintf(f, "<h3><br />");
   else
     fprintf (f,"<td>");
-    for(i=0; i<strlen(contents); i++)
-    {
-      if (*(contents+i)=='\n')
-        fprintf(f,"<br />");
-      else if (*(contents+i)=='<')
-        fprintf(f,"&lt;");
-      else if (*(contents+i)=='>')
-        fprintf(f,"&gt;");
-      else if (*(contents+i)=='&')
-        fprintf(f,"&amp;");
-      else if (*(contents+i)=='\"')
-        fprintf(f, "\\\"");
-      else
-        fprintf(f,"%c",*(contents+i));
-    }
+  for (i=0; i<strlen((char*)contents); i++)
+  {
+    if (*(contents+i)=='\n')
+      fprintf(f,"<br />");
+    else if (*(contents+i)=='<')
+      fprintf(f,"&lt;");
+    else if (*(contents+i)=='>')
+      fprintf(f,"&gt;");
+    else if (*(contents+i)=='&')
+      fprintf(f,"&amp;");
+    else if (*(contents+i)=='\"')
+      fprintf(f, "\\\"");
+    else
+      fprintf(f,"%c",*(contents+i));
+  }
   if (!tagNameIs("intro",cur))
     fprintf (f,"</td>");
-  
+
   xmlFree(contents);
 }
 
