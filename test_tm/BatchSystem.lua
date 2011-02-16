e6c04c2861323338c3c8e076dbfec21ce4adaadc
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2011-02-16 17:21:55 +0000

    Making test names slightly more descriptive

--- test_tm/BatchSystem.lua
+++ test_tm/BatchSystem.lua
@@ -2,7 +2,7 @@ BatchSystems = {
    PBS = {
       launchpad = {
          submitHeader= [[
-               #PBS -N $(JOBNAME)
+               #PBS -N tm-$(JOBNAME)
                #PBS -l walltime="$(TIME)"
                #PBS -l nodes=1:GPU:ppn=8
                #PBS -o std.out
