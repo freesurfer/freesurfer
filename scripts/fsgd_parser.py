54a7b894ea107283d85b89fe77b1a51c3167a36c
Martin Reuter <mreuter@nmr.mgh.harvard.edu>
2011-02-17 22:39:36 +0000

    initial commit: a class to parse an fsgd file (at least most of it) to be used for longitudinal processing

---
+++ scripts/fsgd_parser.py
@@ -0,0 +1,78 @@
+# Original author - Martin Reuter
+# $Id: fsgd_parser.py,v 1.1 2011/02/17 22:39:36 mreuter Exp $
+import os
+import logging
+#from misc import *
+#from subject_info import *
+from datastruct_utils import *
+
+## logging for both stats2table
+#
+#ch = logging.StreamHandler()
+##create logger
+#aseglogger = logging.getLogger("asegstats2table")
+#aseglogger.setLevel(logging.INFO)
+#aseglogger.addHandler(ch)
+#
+#aparclogger = logging.getLogger("aparcstats2table")
+#aparclogger.setLevel(logging.INFO)
+#aparclogger.addHandler(ch)
+
+class BadFileError(Exception):
+    def __init__(self, filename):
+        self.filename = filename
+       
+    def __str__(self):
+        return self.filename
+
+"""
+This is the class which parses the fsgd files. 
+"""
+class FsgdParser:
+
+    fp = None # filepointer
+    subjects_tp_map = StableDict()
+    variables = []
+    defaultvar = ""
+
+    # constructor just needs a fsgd filename
+    # load it and report error
+    def __init__(self, filename):
+        self.filename = filename
+        # raise exception if file doesn't exist
+        if not os.path.exists(filename):
+            raise BadFileError(filename)
+        self.fp = open(filename, 'r')
+                
+    # we read in the fsgd
+    def parse(self):
+        self.subjects_tp_map = StableDict()
+        for line in self.fp:
+            # valid line
+
+            if line.rfind('#') == -1:
+                strlst = line.split()
+                #print strlst[0].upper()
+                if strlst[0].upper() == 'CLASS':
+                    self.subjects_tp_map[strlst[1]] = []
+
+                if strlst[0].upper() == 'VARIABLES':
+                    self.variables= strlst[1:]
+                   
+                if strlst[0].upper() == 'INPUT':
+                    # check if key exists
+                    key = strlst[2]
+                    tp  = strlst[1]
+                    if not key in self.subjects_tp_map:
+                        print 'ERROR: Subject {0} was not defined as "Class"!'.format(key)
+                        sys.exit(1)
+                    # append tp and data to this subject
+                    self.subjects_tp_map[key].append([ tp ] + strlst[3:]  );                 
+                    
+                if strlst[0].upper() == 'DEFAULTVARIABLE':
+                    self.defaultvar = strlst[1]
+
+
+        # return
+        return self.subjects_tp_map, self.variables, self.defaultvar
+
