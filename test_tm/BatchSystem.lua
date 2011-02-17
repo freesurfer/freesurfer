91b5cd148926be25fa930a706a833fdde5c93e09
Richard G. Edgar <rge21@nmr.mgh.harvard.edu>
2011-02-17 16:42:59 +0000

    We have a new queue on launchpad....

--- test_tm/BatchSystem.lua
+++ test_tm/BatchSystem.lua
@@ -4,10 +4,11 @@ BatchSystems = {
          submitHeader= [[
                #PBS -N tm-$(JOBNAME)
                #PBS -l walltime="$(TIME)"
-               #PBS -l nodes=1:GPU:ppn=8
+               #PBS -l nodes=1:ppn=8
                #PBS -o std.out
                #PBS -e std.err
                #PBS -V
+               #PBS -q GPU
                #PBS -M rge21@nmr.mgh.harvard.edu
                #PBS -m a
                cd $PBS_O_WORKDIR
