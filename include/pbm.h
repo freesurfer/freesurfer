ec39b4ef7ee06c45e382110576087757a968e7b8
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-03-01 20:33:13 +0000

    FIX: license change

--- include/pbm.h
+++ include/pbm.h
@@ -1,33 +1,8 @@
 /**
  * @file  pbm.h
- * @brief REPLACE_WITH_ONE_LINE_SHORT_DESCRIPTION
- *
- * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
- */
-/*
- * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
- * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2006/12/29 02:09:00 $
- *    $Revision: 1.2 $
- *
- * Copyright (C) 2002-2007,
- * The General Hospital Corporation (Boston, MA). 
- * All rights reserved.
- *
- * Distribution, usage and copying of this software is covered under the
- * terms found in the License Agreement file named 'COPYING' found in the
- * FreeSurfer source code root directory, and duplicated here:
- * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
- *
- * General inquiries: freesurfer@nmr.mgh.harvard.edu
- * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+ * @brief header file for libpbm portable bitmap library
  *
  */
-
-
-/* pbm.h - header file for libpbm portable bitmap library
-*/
 
 #ifndef _PBM_H_
 #define _PBM_H_
