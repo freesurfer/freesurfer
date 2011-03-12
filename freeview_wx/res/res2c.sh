2e6647e25b424d851397bb0e20c4b33b70130d02
Krish Subramaniam <krish@nmr.mgh.harvard.edu>
2011-03-12 01:12:13 +0000

    MAJOR: legacy freeview

---
+++ freeview_wx/res/res2c.sh
@@ -0,0 +1,18 @@
+~/bin/bin2c ../../distribution/FreeSurferColorLUT.txt FreeSurferColorLUT FreeSurferColorLUT_binary
+~/bin/bin2c QuickRef.html QuickRef QuickRef_binary
+~/bin/bin2c images/cursor_pencil.png CursorPencil_png CursorPencil_png
+~/bin/bin2c images/cursor_fill.png CursorFill_png CursorFill_png
+~/bin/bin2c images/cursor_polyline.png CursorPolyline_png CursorPolyline_png
+~/bin/bin2c images/cursor_pan.png CursorPan_png CursorPan_png
+~/bin/bin2c images/cursor_zoom.png CursorZoom_png CursorZoom_png
+~/bin/bin2c images/cursor_colorpicker.png CursorColorPicker_png CursorColorPicker_png
+~/bin/bin2c images/cursor_grab.png CursorGrab_png CursorGrab_png
+~/bin/bin2c images/cursor_measure_line.png CursorMeasureLine_png CursorMeasureLine_png
+~/bin/bin2c images/cursor_measure_rectangle.png CursorMeasureRectangle_png CursorMeasureRectangle_png
+~/bin/bin2c images/cursor_measure_polyline.png CursorMeasurePolyline_png CursorMeasurePolyline_png
+~/bin/bin2c images/cursor_contour.png CursorContour_png CursorContour_png
+~/bin/bin2c images/cursor_add.png CursorAdd_png CursorAdd_png
+~/bin/bin2c images/cursor_remove.png CursorRemove_png CursorRemove_png
+
+ /usr/pubsw/packages/wxWidgets/current/bin/wxrc *.xrc -v -c -o resource.cpp
+
