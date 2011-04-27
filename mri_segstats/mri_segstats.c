74d190aa252a48c76d83114774a5d1c164c3ba44
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-04-27 22:18:58 +0000

    FIX: rge: Had forgotten to call AcquireCUDADevice

--- mri_segstats/mri_segstats.c
+++ mri_segstats/mri_segstats.c
@@ -11,9 +11,9 @@
 /*
  * Original Author: Dougas N Greve
  * CVS Revision Info:
- *    $Author: greve $
- *    $Date: 2011/04/19 21:33:41 $
- *    $Revision: 1.75.2.1 $
+ *    $Author: nicks $
+ *    $Date: 2011/04/27 22:18:58 $
+ *    $Revision: 1.75.2.2 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -114,7 +114,7 @@ int DumpStatSumTable(STATSUMENTRY *StatSumTable, int nsegid);
 int main(int argc, char *argv[]) ;
 
 static char vcid[] =
-  "$Id: mri_segstats.c,v 1.75.2.1 2011/04/19 21:33:41 greve Exp $";
+  "$Id: mri_segstats.c,v 1.75.2.2 2011/04/27 22:18:58 nicks Exp $";
 char *Progname = NULL, *SUBJECTS_DIR = NULL, *FREESURFER_HOME=NULL;
 char *SegVolFile = NULL;
 char *InVolFile = NULL;
@@ -219,6 +219,10 @@ int main(int argc, char **argv)
   MATRIX *vox2vox = NULL;
   nhits = 0;
   vol = 0;
+
+#if FS_CUDA
+  AcquireCUDADevice();
+#endif
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option (argc, argv, vcid, "$Name:  $");
