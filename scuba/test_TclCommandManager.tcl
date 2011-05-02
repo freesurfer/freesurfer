7823f992988b57af1a02400c4bb6f03cabd09b99
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-05-02 21:02:41 +0000

    FIX: license header update

--- scuba/test_TclCommandManager.tcl
+++ scuba/test_TclCommandManager.tcl
@@ -2,18 +2,19 @@
 ## test_TclCommandManager.tcl
 ##
 ## CVS Revision Info:
-##    $Author: kteich $
-##    $Date: 2007/07/05 22:19:29 $
-##    $Revision: 1.6 $
+##    $Author: nicks $
+##    $Date: 2011/05/02 21:02:44 $
+##    $Revision: 1.7 $
 ##
-## Copyright (C) 2002-2007,
-## The General Hospital Corporation (Boston, MA). 
-## All rights reserved.
+## Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
 ##
-## Distribution, usage and copying of this software is covered under the
-## terms found in the License Agreement file named 'COPYING' found in the
-## FreeSurfer source code root directory, and duplicated here:
-## https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+## Terms and conditions for use, reproduction, distribution and contribution
+## are found in the 'FreeSurfer Software License Agreement' contained
+## in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+##
+## https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+##
+## Reporting: freesurfer@nmr.mgh.harvard.edu
 ##
 ## General inquiries: freesurfer@nmr.mgh.harvard.edu
 ## Bug reports: analysis-bugs@nmr.mgh.harvard.edu
