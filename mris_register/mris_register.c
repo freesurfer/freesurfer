87db18be466716448e8c1976316217227d69aa30
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2011-09-20 17:53:32 +0000

    added first pass spherical registration at larger spatial scale of gradient smoothing. added parameter to INTEGRATION_PARMS struct to have MRISregister to a first pass at a larger spatial scale of gradient smoothing to first pass gradient smoothing with navgs=16384

--- mris_register/mris_register.c
+++ mris_register/mris_register.c
@@ -8,9 +8,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2011/03/02 00:04:33 $
- *    $Revision: 1.59 $
+ *    $Author: fischl $
+ *    $Date: 2011/09/20 17:53:32 $
+ *    $Revision: 1.60 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -44,7 +44,7 @@
 #include "gcsa.h"
 
 static char vcid[] =
-  "$Id: mris_register.c,v 1.59 2011/03/02 00:04:33 nicks Exp $";
+  "$Id: mris_register.c,v 1.60 2011/09/20 17:53:32 fischl Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -140,14 +140,14 @@ main(int argc, char *argv[])
 
   make_cmd_version_string
   (argc, argv,
-   "$Id: mris_register.c,v 1.59 2011/03/02 00:04:33 nicks Exp $",
+   "$Id: mris_register.c,v 1.60 2011/09/20 17:53:32 fischl Exp $",
    "$Name:  $",
    cmdline);
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
           (argc, argv,
-           "$Id: mris_register.c,v 1.59 2011/03/02 00:04:33 nicks Exp $",
+           "$Id: mris_register.c,v 1.60 2011/09/20 17:53:32 fischl Exp $",
            "$Name:  $");
   if (nargs && argc - nargs == 1)
   {
@@ -173,6 +173,7 @@ main(int argc, char *argv[])
   parms.l_pcorr = 0.0f ;
   parms.niterations = 25 ;
   parms.n_averages = 1024 ;   // used to be 256
+  parms.first_pass_averages = 1024*16 ;  // only used in first pass
   parms.write_iterations = 100 ;
   parms.dt_increase = 1.01 /* DT_INCREASE */;
   parms.dt_decrease = 0.99 /* DT_DECREASE*/ ;
@@ -550,6 +551,17 @@ main(int argc, char *argv[])
                    min_degrees, max_degrees, nangles) ;
 
   }
+#if 0
+  parms.l_dist *= 50 ;
+  MRISregister(mris, mrisp_template,
+                   &parms, max_passes,
+                   min_degrees, max_degrees, nangles) ;
+  parms.l_dist /= 50 ;
+  MRISregister(mris, mrisp_template,
+                   &parms, max_passes,
+                   min_degrees, max_degrees, nangles) ;
+#endif
+
   if (remove_negative)
   {
     parms.niterations = 1000 ;
