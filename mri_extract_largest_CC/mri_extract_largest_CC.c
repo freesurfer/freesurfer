c8960be6da6d3ee78972a654746a49c8ae61a3e7
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2011-09-21 14:58:22 +0000

    added -I and -O switches to allow the extraction of the background component, then saving of it's inverse (so that there are no interior holes in the brainmask.mgz)

--- mri_extract_largest_CC/mri_extract_largest_CC.c
+++ mri_extract_largest_CC/mri_extract_largest_CC.c
@@ -1,15 +1,15 @@
 /**
  * @file  mri_extract_largest_CC.c
- * @brief REPLACE_WITH_ONE_LINE_SHORT_DESCRIPTION
+ * @brief extract the largest connected component from a volume and write it into another volume
  *
  * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
  */
 /*
- * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
+ * Original Author: Florent Segonne
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2011/03/02 00:04:15 $
- *    $Revision: 1.6 $
+ *    $Author: fischl $
+ *    $Date: 2011/09/21 14:58:22 $
+ *    $Revision: 1.7 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -53,8 +53,10 @@ static int get_option(int argc, char *argv[]) ;
 static float threshold = 90;
 
 static char hemi[80] = "lh";
+static int inverse = 0 ;
 
 char *Progname;
+static MRI *mri_orig = NULL ;
 
 int main(int argc, char *argv[]) {
   char   **av;
@@ -67,10 +69,10 @@ int main(int argc, char *argv[]) {
 
   Progname = argv[0];
 
-  nargs = handle_version_option (argc, argv, "$Id: mri_extract_largest_CC.c,v 1.6 2011/03/02 00:04:15 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_extract_largest_CC.c,v 1.7 2011/09/21 14:58:22 fischl Exp $", "$Name:  $");
   argc -= nargs ;
   if (1 == argc)
-    exit (0);
+    usage (0);
 
   ac = argc ;
   av = argv ;
@@ -93,14 +95,26 @@ int main(int argc, char *argv[]) {
   if (!mri_seg)
     ErrorExit(ERROR_BADPARM, "%s: could not read label volume %s",
               Progname, argv[1]) ;
+  if (mri_orig == NULL)
+    mri_orig = MRIcopy(mri_seg, NULL) ;
 
   for (z = 0; z < mri_seg->depth; z++)
     for (y = 0; y < mri_seg->height; y++)
       for (x = 0; x < mri_seg->width; x++) {
-        if (MRIgetVoxVal(mri_seg, x, y, z, 0) < threshold)
-          MRIsetVoxVal(mri_seg, x, y, z, 0, 0);
-        else
-          MRIsetVoxVal(mri_seg, x, y, z, 0, 255);
+	if (inverse == 0)
+	{
+	  if (MRIgetVoxVal(mri_seg, x, y, z, 0) < threshold)
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, 0);
+	  else
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, 255);
+	}
+	else  // extract background
+	{
+	  if (MRIgetVoxVal(mri_seg, x, y, z, 0) > threshold)
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, 0);
+	  else
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, 255);
+	}
       }
 
   GetLargestCC6(mri_seg);
@@ -108,10 +122,23 @@ int main(int argc, char *argv[]) {
   for (z = 0; z < mri_seg->depth; z++)
     for (y = 0; y < mri_seg->height; y++)
       for (x = 0; x < mri_seg->width; x++) {
-        if (MRIgetVoxVal(mri_seg, x, y, z, 0) > 0)
-          MRIsetVoxVal(mri_seg, x, y, z, 0, target_value);
+	if (inverse == 0)
+	{
+	  if (MRIgetVoxVal(mri_seg, x, y, z, 0) > 0)
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, target_value);
+	}
+	else
+	{
+	  if (MRIgetVoxVal(mri_seg, x, y, z, 0) == 0)
+	  {
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, MRIgetVoxVal(mri_orig, x, y, z, 0));
+	  }
+	  else
+	    MRIsetVoxVal(mri_seg, x, y, z, 0, 0) ;
+	}
       }
 
+  printf("writing output to %s\n", argv[2]) ;
   MRIwrite(mri_seg, argv[2]);
 
   exit(0);
@@ -129,6 +156,8 @@ void usage(int exit_val) {
   fprintf(fout, "\t Options are: \n") ;
   fprintf(fout, "\t\t -T #: threshold for object \n") ;
   fprintf(fout, "\t\t -hemi lh/rh: set the target value corresponding to lh (255) or rh (127) \n") ;
+  fprintf(fout, "\t\t -I : find the largest CC in the background\n") ;
+  fprintf(fout, "\t\t -O <orig volume> : clone values from <orig volume> into output (used with -I)\n") ;
   exit(exit_val);
 
 }  /*  end usage()  */
@@ -154,6 +183,16 @@ get_option(int argc, char *argv[]) {
     printf("hemisphere = %s\n", hemi);
     nargs = 1;
   } else switch (toupper(*option)) {
+  case 'O':
+    mri_orig = MRIread(argv[2]) ;
+    if (mri_orig == NULL)
+      ErrorExit(ERROR_NOFILE, "%s: could not open orig volume %s", Progname, argv[2]) ;
+    nargs = 1 ;
+    break ;
+  case 'I':
+    inverse = 1 ;
+    printf("extracting CC from background instead of foreground\n") ;
+    break ;
     case '?':
     case 'U':
       usage(0) ;
