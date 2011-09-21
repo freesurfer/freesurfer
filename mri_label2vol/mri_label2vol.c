f8ea0ae5bb039f08d1338b5bfb33a2cd98028957
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-09-21 20:49:44 +0000

    FIX: must include --identity in the example using labels and vol in same space

--- mri_label2vol/mri_label2vol.c
+++ mri_label2vol/mri_label2vol.c
@@ -13,9 +13,9 @@
 /*
  * Original Author: Douglas N. Greve
  * CVS Revision Info:
- *    $Author: greve $
- *    $Date: 2011/09/08 20:29:53 $
- *    $Revision: 1.34.2.2 $
+ *    $Author: nicks $
+ *    $Date: 2011/09/21 20:49:44 $
+ *    $Revision: 1.34.2.3 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -77,7 +77,7 @@ static int *NthLabelMap(MRI *aseg, int *nlabels);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_label2vol.c,v 1.34.2.2 2011/09/08 20:29:53 greve Exp $";
+static char vcid[] = "$Id: mri_label2vol.c,v 1.34.2.3 2011/09/21 20:49:44 nicks Exp $";
 char *Progname = NULL;
 
 char *LabelList[100];
@@ -145,11 +145,11 @@ int main(int argc, char **argv) {
   char cmdline[CMD_LINE_LEN] ;
 
   make_cmd_version_string (argc, argv,
-                           "$Id: mri_label2vol.c,v 1.34.2.2 2011/09/08 20:29:53 greve Exp $", "$Name:  $", cmdline);
+                           "$Id: mri_label2vol.c,v 1.34.2.3 2011/09/21 20:49:44 nicks Exp $", "$Name:  $", cmdline);
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option (argc, argv,
-                                 "$Id: mri_label2vol.c,v 1.34.2.2 2011/09/08 20:29:53 greve Exp $", "$Name:  $");
+                                 "$Id: mri_label2vol.c,v 1.34.2.3 2011/09/21 20:49:44 nicks Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -890,6 +890,7 @@ static void print_help(void) {
     "  --label lh-avg_central_sulcus.label \n"
     "  --label lh-avg_calcarine_sulcus.label \n"
     "  --temp $SUBJECTS_DIR/bert/orig\n"
+    "  --identity\n"
     "  --o cent_calc.img\n"
     "\n"
     "The voxels corresponding to lh-avg_central_sulcus.label will have a of \n"
