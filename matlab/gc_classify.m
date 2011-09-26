fc4dd37180c87c3535d2122dfee15b27ed7ff8e4
Doug Greve <greve@nmr.mgh.harvard.edu>
2011-09-26 18:07:15 +0000

    Made changes to speed up (same results)

--- matlab/gc_classify.m
+++ matlab/gc_classify.m
@@ -6,7 +6,7 @@ function [datacid map p] = gc_classify(data,means,covar)
 % p - ndata-by-nclasses - p-value for each class
 % See also gc_synth_params, gc_synth_data, gc_train
 %
-% $Id: gc_classify.m,v 1.1 2011/09/19 21:32:43 greve Exp $
+% $Id: gc_classify.m,v 1.2 2011/09/26 18:07:15 greve Exp $
 
 [nClasses nVariates] = size(means);
 nData = size(data,1);
@@ -15,21 +15,28 @@ nData = size(data,1);
 e0 = (2*pi)^(nVariates/2);
 
 p = zeros(nData,nClasses);
-for nthData = 1:nData
-  d = data(nthData,:);
-  for nthClass = 1:nClasses
-    dm = d - means(nthClass,:);
-    C = covar(:,:,nthClass);
-    q = dm*inv(C)*dm';
-    p(nthData,nthClass) = exp(-q/2)/(e0*sqrt(det(C)));
-  end
+for nthClass = 1:nClasses
+  dm = data - repmat(means(nthClass,:),[nData 1]);
+  C = covar(:,:,nthClass);
+  q = sum(dm .* (inv(C)*dm')',2);
+  p(:,nthClass) = exp(-q/2)/(e0*sqrt(det(C)));
 end
 
 [map datacid] = max(p,[],2);
 
 return;
 
-
+% Old and slow
+% p = zeros(nData,nClasses);
+% for nthData = 1:nData
+%   d = data(nthData,:);
+%   for nthClass = 1:nClasses
+%     dm = d - means(nthClass,:);
+%     C = covar(:,:,nthClass);
+%     q = dm*inv(C)*dm';
+%     p(nthData,nthClass) = exp(-q/2)/(e0*sqrt(det(C)));
+%   end
+% end
 
 
 
