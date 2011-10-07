19e55fde453319bb33b749fd94d9aa9f52734222
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2011-10-07 01:40:43 +0000

    first working version of binary that takes two surfaces and interpolates the warp field mapping one to the other into the volume

--- mris_interpolate_warp/mris_interpolate_warp.c
+++ mris_interpolate_warp/mris_interpolate_warp.c
@@ -8,9 +8,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2011/03/22 22:57:17 $
- *    $Revision: 1.3 $
+ *    $Author: fischl $
+ *    $Date: 2011/10/07 01:40:43 $
+ *    $Revision: 1.4 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -54,7 +54,13 @@ static char *like_vol_name ;
 
 char *Progname ;
 static void usage_exit(int code) ;
-static int niter = 50 ;
+static int niter = 500 ;
+static MRI *mri_in = NULL ;
+static char *out_fname ;
+
+static int no_write = 0 ;
+
+static int pad = 20 ;
 
 static int
 write_surface_warp_into_volume(MRI_SURFACE *mris, MRI *mri, int niter)
@@ -65,30 +71,26 @@ write_surface_warp_into_volume(MRI_SURFACE *mris, MRI *mri, int niter)
   MRI     *mri_weights, *mri_ctrl, *mri_frame ;
   float    wt ;
 
-  mri_weights = MRIallocSequence(mri->width, mri->height, mri->height, MRI_FLOAT, 1) ;
-  mri_ctrl = MRIallocSequence(mri->width, mri->height, mri->height, MRI_UCHAR, 1) ;
-  MRIcopyHeader(mri, mri_weights) ;
-  MRIcopyHeader(mri, mri_ctrl) ;
+  mri_weights = MRIallocSequence(mri->width, mri->height, mri->depth, MRI_FLOAT, 1) ;
+  mri_ctrl = MRIallocSequence(mri->width, mri->height, mri->depth, MRI_UCHAR, 1) ;
+  MRIcopyHeader(mri, mri_weights) ; MRIcopyHeader(mri, mri_ctrl) ;
+  //   build a 3 frame volume with the voxel-coords warp (dx, dy, dz) in frames 0, 1 and 2 respectively
   for (vno = 0 ; vno < mris->nvertices ; vno++)
   {
     v = &mris->vertices[vno] ;
-    if (vno == Gdiag_no)
-    {
-      DiagBreak() ;
-    }
     if (v->ripflag)
-    {
       continue ;
-    }
+
     MRISsurfaceRASToVoxelCached(mris, mri, v->origx, v->origy, v->origz, &xv, &yv, &zv) ;
     MRISsurfaceRASToVoxelCached(mris, mri, v->x, v->y, v->z, &xv1, &yv1, &zv1) ;
-    dx = xv1-xv ;
-    dy = yv1-yv ;
-    dz = zv1-zv ;
-    //    dx = xv1 ; dy = yv1 ; dz = zv1 ;
-    xvi = nint(xv) ;
-    yvi = nint(yv) ;
-    zvi = nint(zv) ;
+    dx = xv1-xv ; dy = yv1-yv ; dz = zv1-zv ;
+    xvi = nint(xv) ; yvi = nint(yv) ; zvi = nint(zv) ;
+    if (vno == Gdiag_no)
+    {
+      printf("surface vertex %d: inflated (%2.0f, %2.0f, %2.0f), orig (%2.0f, %2.0f, %2.0f), "
+	     "dx=(%2.0f, %2.0f, %2.0f)\n", vno, xv1, yv1, zv1, xv, yv, zv, dx, dy, dz) ;
+      DiagBreak() ;
+    }
     if (xvi < 0 || xvi >= mri->width || yv < 0 || yv >= mri->height || zv < 0 || zv >= mri->depth)
     {
       continue ;
@@ -99,6 +101,8 @@ write_surface_warp_into_volume(MRI_SURFACE *mris, MRI *mri, int niter)
     MRIinterpolateIntoVolume(mri_weights, xv, yv, zv, 1.0) ;
   }
 
+#if 0
+  // set boundary conditions in the edge planes to be 0 warping
   for (xvi = 0 ; xvi < mri->width ; xvi++)
     for (yvi = 0 ; yvi < mri->height ; yvi++)
     {
@@ -141,40 +145,43 @@ write_surface_warp_into_volume(MRI_SURFACE *mris, MRI *mri, int niter)
       MRIsetVoxVal(mri, mri->width-1, yvi, zvi, 2, 0) ;
 
     }
+#endif
+
+  // normalize the warp field using a weighted average of all vertices that map to every voxel
   for (xvi = 0 ; xvi < mri->width ; xvi++)
     for (yvi = 0 ; yvi < mri->height ; yvi++)
       for (zvi = 0 ; zvi < mri->depth ; zvi++)
       {
         if (xvi == Gx && yvi == Gy && zvi == Gz)
-        {
           DiagBreak() ;
-        }
+
         wt = MRIgetVoxVal(mri_weights, xvi, yvi, zvi, 0) ;
         dx = MRIgetVoxVal(mri, xvi, yvi, zvi, 0) ;
         dy = MRIgetVoxVal(mri, xvi, yvi, zvi, 1) ;
         dz = MRIgetVoxVal(mri, xvi, yvi, zvi, 2) ;
         if (FZERO(wt))
-        {
           continue ;
-        }
-        dx /= wt ;
-        dy /= wt ;
-        dz /= wt ;
+
+        dx /= wt ; dy /= wt ; dz /= wt ;
         MRIsetVoxVal(mri, xvi, yvi, zvi, 0, dx) ;
         MRIsetVoxVal(mri, xvi, yvi, zvi, 1, dy) ;
         MRIsetVoxVal(mri, xvi, yvi, zvi, 2, dz) ;
         MRIsetVoxVal(mri_ctrl, xvi, yvi, zvi, 0, CONTROL_MARKED) ;  // it is a control point
       }
 
-  MRIwrite(mri, "warp0.mgz") ;
-  MRIwrite(mri_ctrl, "ctrl.mgz") ;
-  for (frame = 0 ; frame < 3 ; frame++)
+  if (Gdiag & DIAG_WRITE)
+  {
+    MRIwrite(mri, "warp0.mgz") ;
+    MRIwrite(mri_ctrl, "ctrl.mgz") ;
+  }
+
+  for (frame = 0 ; frame < mri->nframes ; frame++)
   {
     printf("interpolating frame %d\n", frame+1) ;
     mri_frame = MRIcopyFrame(mri, NULL, frame, 0) ;
     MRIbuildVoronoiDiagram(mri_frame, mri_ctrl, mri_frame) ;
-    MRIsoapBubble(mri_frame, mri_ctrl, mri_frame, niter) ;
-
+    MRIsoapBubble(mri_frame, mri_ctrl, mri_frame, niter, mri_frame->xsize*.05) ;
+#if 0
     {
       int x, y, z ;
       float val ;
@@ -200,12 +207,79 @@ write_surface_warp_into_volume(MRI_SURFACE *mris, MRI *mri, int niter)
             MRIsetVoxVal(mri_frame, x, y, z, 0, val) ;
           }
     }
+#endif
     MRIcopyFrame(mri_frame, mri, 0, frame) ;
     MRIfree(&mri_frame) ;
   }
   MRIfree(&mri_weights) ;
   MRIfree(&mri_ctrl) ;
   return(NO_ERROR) ;
+}
+
+static MRI *
+expand_mri_to_fit_surface(MRI_SURFACE *mris, MRI *mri)
+{
+  int      vno ;
+  VERTEX   *v ;
+  double   xv, yv, zv, xmin, ymin, zmin, xmax, ymax, zmax, x0, y0, z0, x1, y1, z1, 
+    dx_dx, dx_dy, dx_dz,
+    dy_dx, dy_dy, dy_dz,
+    dz_dx, dz_dy, dz_dz ;
+  MRI        *mri_dst ;
+
+  
+  MRISsurfaceRASToVoxelCached(mris, mri, 0, 0, 0, &x0, &y0, &z0) ;
+  MRISsurfaceRASToVoxelCached(mris, mri, 1, 0, 0, &x1, &y1, &z1) ;
+  dx_dx = x1-x0 ; dx_dy = y1-y0 ; dx_dz = z1-z0 ;
+  MRISsurfaceRASToVoxelCached(mris, mri, 0, 1, 0, &x1, &y1, &z1) ;
+  dy_dx = x1-x0 ; dy_dy = y1-y0 ; dy_dz = z1-z0 ;
+  MRISsurfaceRASToVoxelCached(mris, mri, 0, 0, 1, &x1, &y1, &z1) ;
+  dz_dx = x1-x0 ; dz_dy = y1-y0 ; dz_dz = z1-z0 ;
+  for (vno = 0 ; vno < mris->nvertices ; vno++)
+  {
+    v = &mris->vertices[vno] ;
+    if (v->ripflag)
+      continue ;
+    if (vno == Gdiag_no)
+      DiagBreak() ;
+
+    MRISsurfaceRASToVoxelCached(mris, mri, v->origx, v->origy, v->origz, &xv, &yv, &zv) ;
+    if (vno == 0)
+    {
+      xmin = xmax = xv ; ymin = ymax = yv ; zmin = zmax = zv ;
+    }
+    if (xv < xmin)
+      xmin = xv ;
+    if (yv < ymin)
+      ymin = yv ;
+    if (zv < zmin)
+      zmin = zv ;
+    if (xv > xmax)
+      xmax = xv ;
+    if (yv > ymax)
+      ymax = yv ;
+    if (zv > zmax)
+      zmax = zv ;
+#if 0
+    v->origy += 30 ;
+    v->y += 30 ;
+#endif
+  }
+  if (xmin > 0)
+    xmin = 0 ;
+  if (ymin > 0)
+    ymin = 0 ;
+  if (zmin > 0)
+    zmin = 0 ;
+  if (xmax < mri->width-1)
+    xmax = mri->width-1 ;
+  if (ymax < mri->height-1)
+    ymax = mri->height-1 ;
+  if (zmax < mri->depth-1)
+    zmax = mri->depth-1 ;
+
+  mri_dst = MRIextractRegionAndPad(mri, NULL, NULL, pad) ;
+  return(mri_dst) ;
 }
 
 int
@@ -220,7 +294,7 @@ main(int argc, char *argv[])
   MRI          *mri = NULL ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mris_interpolate_warp.c,v 1.3 2011/03/22 22:57:17 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mris_interpolate_warp.c,v 1.4 2011/10/07 01:40:43 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
   {
     exit (0);
@@ -247,16 +321,20 @@ main(int argc, char *argv[])
     usage_exit(1) ;
   }
 
+
+  /*
+    note that a "forward" morph means a retraction, so we reverse the order of the argvs here.
+    This means that for every voxel in the inflated image we have a vector that points to where in
+    the original image it came from, and *NOT* the reverse.
+  */
   mris = MRISread(argv[2]) ;
   if (mris == NULL)
-  {
     ErrorExit(ERROR_NOFILE, "%s: could not read source surface %s\n", Progname,argv[2]) ;
-  }
+
   MRISsaveVertexPositions(mris, ORIGINAL_VERTICES) ;
   if (MRISreadVertexPositions(mris, argv[1]) != NO_ERROR)
-  {
     ErrorExit(ERROR_NOFILE, "%s: could not read target surface %s\n", Progname,argv[1]) ;
-  }
+
   if (like_vol_name == NULL)
   {
     mri = MRIallocSequence(mris->vg.width, mris->vg.height, mris->vg.depth, MRI_FLOAT, 3) ;
@@ -270,24 +348,82 @@ main(int argc, char *argv[])
     {
       ErrorExit(ERROR_NOFILE, "%s: could not like volume %s\n", like_vol_name) ;
     }
-    mri = MRIallocSequence(mri->width, mri->height, mri->depth, MRI_FLOAT, 3) ;
+    mri = MRIallocSequence(mri_tmp->width, mri_tmp->height, mri_tmp->depth, MRI_FLOAT, 3) ;
     MRIcopyHeader(mri_tmp, mri) ;
     MRIfree(&mri_tmp) ;
   }
+  if (Gdiag & DIAG_SHOW && DIAG_VERBOSE_ON)
+  {
+    double xv, yv, zv ;
+    VERTEX *v = &mris->vertices[0] ;
+    MRISsurfaceRASToVoxel(mris, mri, v->x, v->y, v->z, &xv, &yv, &zv) ;
+    printf("v 0: sras (%f, %f, %f) --> vox (%f, %f, %f)\n", v->x,v->y,v->z,xv,yv,zv);
+    MRISsurfaceRASToVoxelCached(mris, mri, v->x, v->y, v->z, &xv, &yv, &zv) ;
+    printf("v 0: sras (%f, %f, %f) --> vox (%f, %f, %f)\n", v->x,v->y,v->z,xv,yv,zv);
+    DiagBreak() ;
+  }
+  {
+    MRI *mri_tmp ;
+    mri_tmp = expand_mri_to_fit_surface(mris, mri) ;
+    MRIfree(&mri) ; mri = mri_tmp ;
+  }
   write_surface_warp_into_volume(mris, mri, niter) ;
 
-  MRIwrite(mri, "warp.mgz") ;
+  if (Gdiag & DIAG_WRITE && DIAG_VERBOSE_ON)
+    MRIwrite(mri, "warp.mgz") ;
   gcam = GCAMalloc(mri->width, mri->height, mri->depth) ;
   GCAMinitVolGeom(gcam, mri, mri) ;
-  GCAMreadWarpFromMRI(gcam, mri) ;
+  GCAMremoveSingularitiesAndReadWarpFromMRI(gcam, mri) ;
+//  GCAMreadWarpFromMRI(gcam, mri) ;
   //  GCAsetVolGeom(gca, &gcam->atlas);
-  out_name = argv[3] ;
-  GCAMwrite(gcam, out_name) ;
+#if 0
+  gcam->gca = gcaAllocMax(1, 1, 1,
+			  mri->width, mri->height,
+			  mri->depth,
+			  0, 0) ;
+ GCAMinit(gcam, mri, NULL, NULL, 0) ;
+#endif
+#if 0
+  GCAMinvert(gcam, mri) ;
+  GCAMwriteInverseWarpToMRI(gcam, mri) ;
+  GCAMremoveSingularitiesAndReadWarpFromMRI(gcam, mri) ;  // should be inverse now
+#endif
+  if (mri_in)
+  {
+    MRI *mri_warped, *mri_tmp ;
+    printf("applying warp to %s and writing to %s\n", mri_in->fname, out_fname) ;
+    mri_tmp = MRIextractRegionAndPad(mri_in, NULL, NULL, pad) ; MRIfree(&mri_in) ; mri_in = mri_tmp ;
+    mri_warped = GCAMmorphToAtlas(mri_in, gcam, NULL, -1, SAMPLE_TRILINEAR) ;
+    MRIwrite(mri_warped, out_fname) ;
+    if (Gdiag_no >= 0)
+    {
+      double  xi, yi, zi, xo, yo, zo, val;
+      int     xp, yp, zp ;
+      GCA_MORPH_NODE *gcamn ;
+
+      VERTEX *v = &mris->vertices[Gdiag_no] ;
+      MRISsurfaceRASToVoxelCached(mris, mri, v->origx, v->origy, v->origz, &xi, &yi, &zi) ;
+      MRISsurfaceRASToVoxelCached(mris, mri, v->x, v->y, v->z, &xo, &yo, &zo) ;
+      printf("surface vertex %d: inflated (%2.0f, %2.0f, %2.0f), orig (%2.0f, %2.0f, %2.0f)\n", Gdiag_no, xi, yi, zi, xo, yo, zo) ;
+      MRIsampleVolume(mri_in, xo, yo, zo, &val) ;
+      xp = nint(xi) ; yp = nint(yi) ; zp = nint(zi) ;
+      gcamn = &gcam->nodes[xp][yp][zp] ;
+      printf("warp = (%2.1f, %2.1f, %2.1f), orig (%2.1f %2.1f %2.1f) = %2.1f \n", 
+	     gcamn->x, gcamn->y, gcamn->z,
+	     gcamn->origx, gcamn->origy, gcamn->origz,val) ;
+      DiagBreak() ;
+    }
+  }
+  if (no_write == 0)
+  {
+    out_name = argv[3] ;
+    GCAMwrite(gcam, out_name) ;
+  }
   msec = TimerStop(&start) ;
   seconds = nint((float)msec/1000.0f) ;
   minutes = seconds / 60 ;
   seconds = seconds % 60 ;
-  fprintf(stderr, "warp field calculation took  %d minutes and %d seconds.\n", minutes, seconds) ;
+  fprintf(stderr, "warp field calculation took %d minutes and %d seconds.\n", minutes, seconds) ;
   exit(0) ;
   return(0) ;
 }
@@ -305,6 +441,21 @@ get_option(int argc, char *argv[])
   option = argv[1] + 1 ;            /* past '-' */
   switch (toupper(*option))
   {
+  case 'N':
+    no_write = 1 ;
+    printf("not saving final warp (specify -a to save warped volume)\n") ;
+    break ;
+  case 'A':
+    mri_in = MRIread(argv[2]) ;
+    if (mri_in == NULL)
+      ErrorExit(ERROR_NOFILE, "%s: could not read volume %s", Progname, argv[2]) ;
+    out_fname = argv[3] ;
+    nargs = 2 ;
+    break ;
+    case 'V':
+      Gdiag_no = atoi(argv[2]) ;
+      nargs = 1 ;
+      break ;
   case 'I':
     niter = atoi(argv[2]) ;
     printf("using %d soap bubble iterations\n", niter) ;
