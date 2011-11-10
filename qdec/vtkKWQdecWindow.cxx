e9a4b4ca716f95c9147af211d2e45d3c75f0b76d
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2011-11-10 21:47:26 +0000

    ENH: include new wm/gm contrast data as an option

--- qdec/vtkKWQdecWindow.cxx
+++ qdec/vtkKWQdecWindow.cxx
@@ -11,8 +11,8 @@
  * Original Author: Kevin Teich
  * CVS Revision Info:
  *    $Author: nicks $
- *    $Date: 2011/09/12 19:05:01 $
- *    $Revision: 1.66 $
+ *    $Date: 2011/11/10 21:47:26 $
+ *    $Revision: 1.67 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -102,7 +102,7 @@ extern "C" {
 using namespace std;
 
 vtkStandardNewMacro( vtkKWQdecWindow );
-vtkCxxRevisionMacro( vtkKWQdecWindow, "$Revision: 1.66 $" );
+vtkCxxRevisionMacro( vtkKWQdecWindow, "$Revision: 1.67 $" );
 
 const char* vtkKWQdecWindow::ksSubjectsPanelName = "Subjects";
 const char* vtkKWQdecWindow::ksDesignPanelName = "Design";
@@ -1349,6 +1349,7 @@ vtkKWQdecWindow::SetCurrentSurfaceMeasure( const char* isMeasure ) {
     mMenuMorphMeasure->GetMenu()->AddRadioButton( "white.K" );
     mMenuMorphMeasure->GetMenu()->AddRadioButton( "white.H" );
 //NJS: not meaningful    mMenuMorphMeasure->GetMenu()->AddRadioButton( "jacobian_white" );
+    mMenuMorphMeasure->GetMenu()->AddRadioButton( "w-g.pct.mgh" );
     mMenuMorphMeasure->GetMenu()->AddRadioButton( "intensity.deep" );
     mMenuMorphMeasure->GetMenu()->AddRadioButton( "intensity.superficial" );
     mMenuMorphMeasure->GetMenu()->AddRadioButton( "intensity.deep.mgz" );
