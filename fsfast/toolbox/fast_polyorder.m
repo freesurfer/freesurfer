d824e17693c2d6a5e26ad638fab1ea090b9dc81b
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-02-17 23:55:40 +0000

    Computes the order of polynomial regressors needed to achieve a certain high-pass cutoff frequency

---
+++ fsfast/toolbox/fast_polyorder.m
@@ -0,0 +1,47 @@
+function [order, X] = fast_polyorder(ntp,TR,fCutOffHz)
+% [order X] = fast_polyorder(ntp,TR,fCutOffHz)
+%
+% Computes the order of polynomial regressors needed to achieve a
+% certain high-pass cutoff frequency given the number of times points
+% and TR. The method was derived empirically.
+%
+% X = fast_polytrendmtx(1,ntp,1,order);%  
+%
+% $Id: fast_polyorder.m,v 1.1 2012/02/17 23:55:40 greve Exp $
+
+%
+% fast_polyorder.m
+%
+% Original Author: Doug Greve
+% CVS Revision Info:
+%    $Author: greve $
+%    $Date: 2012/02/17 23:55:40 $
+%    $Revision: 1.1 $
+%
+% Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
+%
+% Terms and conditions for use, reproduction, distribution and contribution
+% are found in the 'FreeSurfer Software License Agreement' contained
+% in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+%
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+%
+% Reporting: freesurfer@nmr.mgh.harvard.edu
+%
+
+order = [];
+if(nargin ~= 3)
+  fprintf('[order X] = fast_polyorder(ntp,TR,fCutOffHz)\n')';
+  return;
+end
+
+b1 = [.3654 .3405]';
+bexp = b1/(TR*ntp);
+% To compute expected cutfoff frequency = bexp(1)+order*bexp(2);
+order = round((fCutOffHz-bexp(1))/bexp(2));
+if(nargout ==2)
+  X = fast_polytrendmtx(1,ntp,1,order);
+end
+
+return;
+
