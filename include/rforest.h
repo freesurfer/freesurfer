0d8c5a35979e72dc158fa764fcc764a98f609a85
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2012-03-08 13:30:09 +0000

    fixed calculation of class p-value and training and feature fractions. added training feature and data fractions.

--- include/rforest.h
+++ include/rforest.h
@@ -8,8 +8,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: fischl $
- *    $Date: 2012/03/05 16:03:19 $
- *    $Revision: 1.2 $
+ *    $Date: 2012/03/08 13:30:09 $
+ *    $Revision: 1.3 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -36,7 +36,7 @@ typedef struct node
   int    depth ;
   int    total_counts ;       // sum of all the class_counts
   int    ntraining ;
-  int    *training_set ;      // the training subject that are used in this node
+  int    *training_set ;      // 1 if a given training example was used for this node
 } NODE ;
 
 typedef struct
@@ -47,7 +47,6 @@ typedef struct
   int  depth ;
   int  nleaves ;
   NODE **leaves ;
-  int  ntraining ;
 } TREE ;
 
 typedef struct
@@ -63,16 +62,18 @@ typedef struct
   int    *training_classes ;
   double **training_data ;
   int    ntraining ;
-  int    nsteps ;         // # of steps to take in threshold search
-  double feature_overlap ;// the fraction of features that each tree contains
+  int    nsteps ;           // # of steps to take in threshold search
+  double feature_fraction ;  // the fraction of features that each tree contains
+  double training_fraction ; // the fraction of training data that each tree contains
 } RANDOM_FOREST, RF ;
 
 RANDOM_FOREST *RFalloc(int ntrees, int nfeatures, int nclasses, int max_depth,
-                       char **class_names) ;
+                       char **class_names, int nsteps) ;
 RANDOM_FOREST *RFread(char *fname) ;
-int  RFtrain(RANDOM_FOREST *rf, double feature_overlap, int *training_classes, double **training_data,int ntraining);
+int  RFtrain(RANDOM_FOREST *rf, double feature_overlap, double training_overlap, int *training_classes, double **training_data,int ntraining);
 int  RFwrite(RANDOM_FOREST *rf, char *fname) ;
-int  RFclassify(RANDOM_FOREST *rf, double *feature, int true_class) ;
+int  RFclassify(RANDOM_FOREST *rf, double *feature, double *p_pval, int true_class) ;
+int  RFcomputeOutOfBagCorrect(RANDOM_FOREST *rf, int *training_classes, double **training_data,int ntraining);
 
 
 #endif
