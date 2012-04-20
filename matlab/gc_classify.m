a0c97ee00ab216597dec5ff3077b760f3f29e2ac
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-04-20 16:03:47 +0000

    Updates

--- matlab/gc_classify.m
+++ matlab/gc_classify.m
@@ -3,10 +3,15 @@ function [datacid map p] = gc_classify(data,means,covar)
 % Apply gaussian classifier to a data set.
 % datacid - estimates of the class identification
 % map - maximum a posteriori p-value
-% p - ndata-by-nclasses - p-value for each class
+% p - ndata-by-nclasses - prob that data is in given class
+%  -- note: this is not a p-value. See gc_pvalue
 % See also gc_synth_params, gc_synth_data, gc_train
 %
-% $Id: gc_classify.m,v 1.2 2011/09/26 18:07:15 greve Exp $
+% data  - nData x nVariates
+% means - nClasses x nVariates
+% covar - nVariates x nVariates x nClasses
+%
+% $Id: gc_classify.m,v 1.3 2012/04/20 16:03:47 greve Exp $
 
 [nClasses nVariates] = size(means);
 nData = size(data,1);
@@ -18,35 +23,10 @@ p = zeros(nData,nClasses);
 for nthClass = 1:nClasses
   dm = data - repmat(means(nthClass,:),[nData 1]);
   C = covar(:,:,nthClass);
-  q = sum(dm .* (inv(C)*dm')',2);
+  q = sum(dm .* (inv(C)*dm')',2); % Square of Mahalanobis Dist
   p(:,nthClass) = exp(-q/2)/(e0*sqrt(det(C)));
 end
 
 [map datacid] = max(p,[],2);
 
 return;
-
-% Old and slow
-% p = zeros(nData,nClasses);
-% for nthData = 1:nData
-%   d = data(nthData,:);
-%   for nthClass = 1:nClasses
-%     dm = d - means(nthClass,:);
-%     C = covar(:,:,nthClass);
-%     q = dm*inv(C)*dm';
-%     p(nthData,nthClass) = exp(-q/2)/(e0*sqrt(det(C)));
-%   end
-% end
-
-
-
-
-
-
-
-
-
-
-
-
-
--- matlab/gc_synth_data.m
+++ matlab/gc_synth_data.m
@@ -1,11 +1,18 @@
 function [data datacid] = gc_synth_data(means,covar,nData)
 % [data datacid] = gc_synth_data(means,covar,nData)
-%   nData = nData per class.
-%   [means covar] = gc_synth_params(nClasses,nVariates)
-% datacid = class membership
 % Synthesize data to test a gaussian classifier. 
+%
+%  nData can be one of three things:
+%     1. A single number indicating the nData per class
+%     2. nClass numbers indicating the nData per class
+%     3. ClassId list (nDataTotal = length(nData))
+%  [means covar] = gc_synth_params(nClasses,nVariates)
+%
+% datacid = class membership
+%
 % See also gc_synth_params
-
+% 
+% $Id: gc_synth_data.m,v 1.2 2012/04/20 16:03:47 greve Exp $
 
 data = [];
 if(nargin ~= 3)
@@ -15,17 +22,45 @@ end
 
 [nClasses nVariates] = size(means);
 
+resort = 0;
+len_nData = length(nData);
+if(len_nData == 1)
+  % A single value
+  nData = repmat(nData(:),[nClasses 1]);
+elseif(len_nData ~= nClasses)
+  % List of class for each data point
+  cids = nData;
+  cidlist = unique(cids); % Must be nClasses long
+  for k=1:nClasses
+    nData(k) = length(find(cids==cidlist(k)));
+  end
+  resort = 1;
+end
+
 data = [];
 datacid = [];
 for nthClass = 1:nClasses
   m = means(nthClass,:);
   C = covar(:,:,nthClass);
   F = chol(C)';
-  r = F*randn(nVariates,nData);
-  %tmp = r*r'/nData;
-  r = r + repmat(m',[1 nData]);
+  r = F*randn(nVariates,nData(nthClass));
+  r = r + repmat(m',[1 nData(nthClass)]);
   data = [data; r'];
-  datacid = [datacid; nthClass*ones(nData,1)];
+  datacid = [datacid; nthClass*ones(nData(nthClass),1)];
+end
+
+% Resort if nData is class list
+if(resort)
+  data2    = zeros(size(data));
+  datacid2 = zeros(size(datacid));
+  for nthClass = 1:nClasses
+    indA = find(datacid==cidlist(nthClass));
+    indB = find(cids==cidlist(nthClass));
+    data2(indB,:) = data(indA,:);
+    datacid2(indB) = datacid(indA);
+  end
+  data = data2;
+  datacid = datacid2;
 end
 
 return;
