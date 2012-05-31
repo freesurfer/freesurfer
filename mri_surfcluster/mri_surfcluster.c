91f28507cb3d4901e8966011eaaa98637c2494bb
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-05-31 22:10:05 +0000

    Fixed so that input does not have to be float

--- mri_surfcluster/mri_surfcluster.c
+++ mri_surfcluster/mri_surfcluster.c
@@ -7,8 +7,8 @@
  * Original Author: Douglas N. Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2011/05/25 20:23:08 $
- *    $Revision: 1.51.2.2 $
+ *    $Date: 2012/05/31 22:10:05 $
+ *    $Revision: 1.51.2.3 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -67,7 +67,7 @@ static int  stringmatch(char *str1, char *str2);
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_surfcluster.c,v 1.51.2.2 2011/05/25 20:23:08 greve Exp $";
+static char vcid[] = "$Id: mri_surfcluster.c,v 1.51.2.3 2012/05/31 22:10:05 greve Exp $";
 char *Progname = NULL;
 
 char *subjectdir = NULL;
@@ -178,7 +178,7 @@ int main(int argc, char **argv) {
   double cmaxsize;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_surfcluster.c,v 1.51.2.2 2011/05/25 20:23:08 greve Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_surfcluster.c,v 1.51.2.3 2012/05/31 22:10:05 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -303,7 +303,7 @@ int main(int argc, char **argv) {
     }
 
     for (vtx = 0; vtx < srcsurf->nvertices; vtx++)
-      srcsurf->vertices[vtx].val = MRIFseq_vox(srcval,vtx,0,0,srcframe);
+      srcsurf->vertices[vtx].val = MRIgetVoxVal(srcval,vtx,0,0,srcframe);
     MRIfree(&srcval);
   }
 
@@ -1199,7 +1199,7 @@ static void print_help(void) {
     "summary file is shown below.\n"
     "\n"
     "Cluster Growing Summary (mri_surfcluster)\n"
-    "$Id: mri_surfcluster.c,v 1.51.2.2 2011/05/25 20:23:08 greve Exp $\n"
+    "$Id: mri_surfcluster.c,v 1.51.2.3 2012/05/31 22:10:05 greve Exp $\n"
     "Input :      minsig-0-lh.w\n"
     "Frame Number:      0\n"
     "Minimum Threshold: 5\n"
