695e210d876a21f6935268ac960a61682969334b
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2012-06-07 12:57:39 +0000

    files for utilities associated with Random Forests.

---
+++ include/rfutils.h
@@ -0,0 +1,44 @@
+/**
+ * @file  rfutils.h
+ * @brief utilities for Random Forests
+ *
+ */
+/*
+ * Original Author: Bruce Fischl
+ * CVS Revision Info:
+ *    $Author: fischl $
+ *    $Date: 2012/06/07 12:57:39 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
+ *
+ * Terms and conditions for use, reproduction, distribution and contribution
+ * are found in the 'FreeSurfer Software License Agreement' contained
+ * in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+ *
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+ *
+ * Reporting: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+
+#ifndef RFUTILS_H
+#define RFUTILS_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+#include "mri.h"
+#include "transform.h"
+#include "colortab.h"
+
+#include "affine.h"
+#include "rforest.h"
+#include "cma.h"
+
+
+
+#endif
