81c11342c0a9d9028d103e76431b7102085e9d7a
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-07-30 14:05:08 +0000

    Added SiemensCrit 0x51 0x1016 to dump output for TW

--- mri_probedicom/mri_probedicom.c
+++ mri_probedicom/mri_probedicom.c
@@ -16,8 +16,8 @@
  * Original Author: Doug Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2011/06/01 15:13:22 $
- *    $Revision: 1.33.2.2 $
+ *    $Date: 2012/07/30 14:05:08 $
+ *    $Revision: 1.33.2.3 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -63,7 +63,7 @@
 
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_probedicom.c,v 1.33.2.2 2011/06/01 15:13:22 greve Exp $";
+static char vcid[] = "$Id: mri_probedicom.c,v 1.33.2.3 2012/07/30 14:05:08 greve Exp $";
 char *Progname = NULL;
 
 static int  parse_commandline(int argc, char **argv);
@@ -144,7 +144,7 @@ int main(int argc, char **argv) {
   int n,nvoxs;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_probedicom.c,v 1.33.2.2 2011/06/01 15:13:22 greve Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_probedicom.c,v 1.33.2.3 2012/07/30 14:05:08 greve Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -1160,6 +1160,13 @@ int PartialDump(char *dicomfile, FILE *fp)
   e = GetElementFromFile(dicomfile, 0x20, 0x1041);
   if (e != NULL) {
     fprintf(fp,"SliceLocation %s\n",e->d.string);
+    FreeElementData(e);
+    free(e);
+  }
+
+  e = GetElementFromFile(dicomfile, 0x51, 0x1016);
+  if (e != NULL) {
+    fprintf(fp,"SiemensCrit %s\n",e->d.string);
     FreeElementData(e);
     free(e);
   }
