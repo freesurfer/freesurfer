cf697e1925b2fee164edd691da79892fc4282f0d
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-08-31 11:55:12 +0000

    Now handles analyze scaling properly.

--- utils/mriio.c
+++ utils/mriio.c
@@ -9,8 +9,8 @@
  * Original Author: Bruce Fischl
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2012/08/24 19:54:06 $
- *    $Revision: 1.402 $
+ *    $Date: 2012/08/31 11:55:12 $
+ *    $Revision: 1.403 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -6627,13 +6627,26 @@ static MRI *analyzeRead(const char *fname, int read_volume)
 
   if (N_Zero_Pad_Input < 0) fclose(fp);
 
+  printf("  analyzeRead() roi_scale %13.9f\n",hdr->dime.roi_scale);
+  if(fabs(hdr->dime.roi_scale-1) > FLT_EPSILON && fabs(hdr->dime.roi_scale) > FLT_EPSILON){
+    // Rescale if it is neither 1 nor 0
+    if(mri->type != MRI_FLOAT){
+      MRI *mritmp;
+      printf("  analyzeRead() changing type to float\n");
+      mritmp = MRISeqchangeType(mri, MRI_FLOAT,0,0,0);
+      MRIfree(&mri);
+      mri = mritmp;
+    }
+    printf("  analyzeRead() scaling by %13.9f\n",hdr->dime.roi_scale);
+    mri = MRImultiplyConst(mri, hdr->dime.roi_scale, mri);
+  }
   free(buf);
   free(hdr);
 
-  MRIlimits(mri,&min,&max);
-  if (Gdiag_no > 0)
+  if (Gdiag_no > 0){
+    MRIlimits(mri,&min,&max);
     printf("INFO: analyzeRead(): min = %g, max = %g\n",min,max);
-
+  }
   return(mri);
 }
 
