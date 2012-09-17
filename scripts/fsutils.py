df3ca050c12fc760225cbb74484682964e2f7490
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-09-17 16:37:46 +0000

    Now asegstats2table will report a field called EstimatedTotalIntraCranialVol (but still works with the old IntraCranialVol). Also reports SupraTentorialNotVent

--- scripts/fsutils.py
+++ scripts/fsutils.py
@@ -1,5 +1,5 @@
 # Original author - Krish Subramaniam
-# $Id: fsutils.py,v 1.9 2012/07/20 23:25:32 mreuter Exp $
+# $Id: fsutils.py,v 1.10 2012/09/17 16:37:46 greve Exp $
 import os
 import logging
 import sys
@@ -157,7 +157,9 @@ class AsegStatsParser(StatsParser):
                         ('# Measure TotalGray, TotalGrayVol,','TotalGrayVol'),
                         ('# Measure SuperTentorial, SuperTentorialVol,','SuperTentorialVol'),
                         ('# Measure SupraTentorial, SupraTentorialVol,','SupraTentorialVol'),
+                        ('# Measure SupraTentorialNotVent, SupraTentorialVolNotVent,','SupraTentorialVolNotVent'),
                         ('# Measure IntraCranialVol, ICV,','IntraCranialVol'),
+                        ('# Measure EstimatedTotalIntraCranialVol, eTIV,','EstimatedTotalIntraCranialVol'),
                         ('# Measure BrainSeg, BrainSegVol,','BrainSegVol'),
                         ('# Measure BrainSegNotVent, BrainSegVolNotVent,','BrainSegVolNotVent'),)
                 c = 0
