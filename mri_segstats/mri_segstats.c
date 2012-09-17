b8911cefeba8302622b39e4ab43d9f400f7a1a49
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-09-17 16:38:57 +0000

    Changed name of ICV to eTIV

--- mri_segstats/mri_segstats.c
+++ mri_segstats/mri_segstats.c
@@ -12,8 +12,8 @@
  * Original Author: Dougas N Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2012/09/10 19:09:22 $
- *    $Revision: 1.91 $
+ *    $Date: 2012/09/17 16:38:57 $
+ *    $Revision: 1.92 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -110,7 +110,7 @@ int DumpStatSumTable(STATSUMENTRY *StatSumTable, int nsegid);
 int main(int argc, char *argv[]) ;
 
 static char vcid[] =
-  "$Id: mri_segstats.c,v 1.91 2012/09/10 19:09:22 greve Exp $";
+  "$Id: mri_segstats.c,v 1.92 2012/09/17 16:38:57 greve Exp $";
 char *Progname = NULL, *SUBJECTS_DIR = NULL, *FREESURFER_HOME=NULL;
 char *SegVolFile = NULL;
 char *InVolFile = NULL;
@@ -1034,8 +1034,11 @@ int main(int argc, char **argv)
     }
     if (DoETIV)
     {
-      fprintf(fp,"# Measure IntraCranialVol, ICV, "
-              "Intracranial Volume, %f, mm^3\n",atlas_icv);
+      //fprintf(fp,"# Measure IntraCranialVol, ICV, "
+      //      "Intracranial Volume, %f, mm^3\n",atlas_icv);
+      fprintf(fp,"# Measure EstimatedTotalIntraCranialVol, eTIV, "
+              "Estimated Total Intracranial Volume, %f, mm^3\n",atlas_icv);
+
     }
     if (SegVolFile)
     {
