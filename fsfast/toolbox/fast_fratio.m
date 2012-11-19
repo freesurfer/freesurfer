aa95e6bd2d8e83e38e443d1b52cede9273c0eaeb
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-11-19 22:26:54 +0000

    Copied from dev. Now handles single voxel case.

--- fsfast/toolbox/fast_fratio.m
+++ fsfast/toolbox/fast_fratio.m
@@ -1,5 +1,5 @@
-function [F, Fsig, ces, edof] = fast_fratio(beta,X,rvar,C,Sn,dof2max,edofuse)
-% [F, Fsig, ces] = fast_fratio(beta,X,rvar,C,<Sn>,<dof2max>,<edofuse>)
+function [F, Fsig, ces, cesvar, edof] = fast_fratio(beta,X,rvar,C,Sn,dof2max,edofuse)
+% [F, Fsig, ces, cesvar] = fast_fratio(beta,X,rvar,C,<Sn>,<dof2max>,<edofuse>)
 %
 % Sn is the covariance matrix of the noise AFTER any filtering.
 % edofuse is the DOF to use instead of that computed form the
@@ -19,25 +19,23 @@ function [F, Fsig, ces, edof] = fast_fratio(beta,X,rvar,C,Sn,dof2max,edofuse)
 %
 % Original Author: Doug Greve
 % CVS Revision Info:
-%    $Author: nicks $
-%    $Date: 2007/01/10 22:02:30 $
-%    $Revision: 1.8 $
+%    $Author: greve $
+%    $Date: 2012/11/19 22:26:54 $
+%    $Revision: 1.9.2.1 $
 %
-% Copyright (C) 2002-2007,
-% The General Hospital Corporation (Boston, MA). 
-% All rights reserved.
+% Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
 %
-% Distribution, usage and copying of this software is covered under the
-% terms found in the License Agreement file named 'COPYING' found in the
-% FreeSurfer source code root directory, and duplicated here:
-% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+% Terms and conditions for use, reproduction, distribution and contribution
+% are found in the 'FreeSurfer Software License Agreement' contained
+% in the file 'LICENSE' found in the FreeSurfer distribution, and here:
 %
-% General inquiries: freesurfer@nmr.mgh.harvard.edu
-% Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+%
+% Reporting: freesurfer@nmr.mgh.harvard.edu
 %
 
 if(nargin < 4 | nargin > 7)
-  fprintf('[F, Fsig, ces, edof] = fast_fratio(beta,X,rvar,C,<Sn>,<dof2max>,<edofuse>)\n');
+  fprintf('[F, Fsig, ces, cesvar, edof] = fast_fratio(beta,X,rvar,C,<Sn>,<dof2max>,<edofuse>)\n');
   return;
 end
 
@@ -73,12 +71,20 @@ nv = size(beta,2);
 dof1 = J;
 dof2 = edof;
 
-indz  = find(rvar == 0);
-indnz = find(rvar ~= 0);
-if(~isempty(indz))
-  beta = beta(:,indnz);
-  rvar = rvar(:,indnz);
+if(J==1) cesvar = rvar./cescvm; end
+
+if(nv > 1)
+  indz  = find(rvar == 0);
+  indnz = find(rvar ~= 0);
+  if(~isempty(indz))
+    beta = beta(:,indnz);
+    rvar = rvar(:,indnz);
+  end
+else
+  indz = [];
+  indnz = 1;
 end
+
 
 % Contast Effect Size
 ces = C*beta;
