54d6211cff0f512c107cbaf6bccf96f623dd4d80
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-11-19 22:32:34 +0000

    Copied from dev. Fixed docs

--- fsfast/toolbox/fast_smooth2d.m
+++ fsfast/toolbox/fast_smooth2d.m
@@ -1,11 +1,10 @@
-function [volsm, G] = fast_smooth2d(vol,cfwhm,rfwhm,sfwhm)
-% [volsm, G] = fast_smooth2d(vol,cfwhm,rfwhm,sfwhm)
+function [volsm, G] = fast_smooth2d(vol,cfwhm,rfwhm)
+% [volsm, G] = fast_smooth2d(vol,cfwhm,rfwhm)
 % 
 % 3D gaussian smoother.
 %
 % cfwhm - fwhm for cols
 % rfwhm - fwhm for rows
-% sfwhm - fwhm for slice
 %
 % Note: does not attempt to handle wrap-around
 %
@@ -17,21 +16,19 @@ function [volsm, G] = fast_smooth2d(vol,cfwhm,rfwhm,sfwhm)
 %
 % Original Author: Doug Greve
 % CVS Revision Info:
-%    $Author: nicks $
-%    $Date: 2007/01/10 22:02:32 $
-%    $Revision: 1.2 $
+%    $Author: greve $
+%    $Date: 2012/11/19 22:32:34 $
+%    $Revision: 1.3.2.1 $
 %
-% Copyright (C) 2002-2007,
-% The General Hospital Corporation (Boston, MA). 
-% All rights reserved.
+% Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
 %
-% Distribution, usage and copying of this software is covered under the
-% terms found in the License Agreement file named 'COPYING' found in the
-% FreeSurfer source code root directory, and duplicated here:
-% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+% Terms and conditions for use, reproduction, distribution and contribution
+% are found in the 'FreeSurfer Software License Agreement' contained
+% in the file 'LICENSE' found in the FreeSurfer distribution, and here:
 %
-% General inquiries: freesurfer@nmr.mgh.harvard.edu
-% Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+%
+% Reporting: freesurfer@nmr.mgh.harvard.edu
 %
 
 if(nargin ~= 3)
