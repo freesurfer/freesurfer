3ca4506080e4fb77309c1ca76f981b21ad49bd7f
Doug Greve <greve@nmr.mgh.harvard.edu>
2012-12-05 20:57:13 +0000

    Added some shortcuts for --pix-only

--- mri_diff/mri_diff.c
+++ mri_diff/mri_diff.c
@@ -19,9 +19,9 @@
 /*
  * Original Author: Doug Greve
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2011/03/02 00:04:15 $
- *    $Revision: 1.31 $
+ *    $Author: greve $
+ *    $Date: 2012/12/05 20:57:13 $
+ *    $Revision: 1.32 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -172,7 +172,7 @@ static void print_version(void) ;
 static void dump_options(FILE *fp);
 int main(int argc, char *argv[]) ;
 
-static char vcid[] = "$Id: mri_diff.c,v 1.31 2011/03/02 00:04:15 nicks Exp $";
+static char vcid[] = "$Id: mri_diff.c,v 1.32 2012/12/05 20:57:13 greve Exp $";
 char *Progname = NULL;
 char *cmdline, cwd[2000];
 int debug=0;
@@ -666,7 +666,8 @@ static int parse_commandline(int argc, char **argv) {
     else if (!strcasecmp(option, "--qa")) {
       CheckPixVals = 0;
       CheckGeo     = 0;
-    } else if (!strcasecmp(option, "--pix-only")) {
+    } else if (!strcasecmp(option, "--pix-only") || !strcasecmp(option, "--po") ||
+	       !strcasecmp(option, "--pixonly")) {
       CheckPixVals = 1;
       CheckResolution = 0;
       CheckAcqParams = 0;
