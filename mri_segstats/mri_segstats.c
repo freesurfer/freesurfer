127aa56c932aedd4ecf62586a20854c30c8b3e62
Doug Greve <greve@nmr.mgh.harvard.edu>
2013-01-14 22:23:47 +0000

    Added  BrainVol-to-eTIV and MaskVol-to-eTIV. Good for QA

--- mri_segstats/mri_segstats.c
+++ mri_segstats/mri_segstats.c
@@ -12,8 +12,8 @@
  * Original Author: Dougas N Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2012/09/17 16:39:55 $
- *    $Revision: 1.75.2.6 $
+ *    $Date: 2013/01/14 22:23:47 $
+ *    $Revision: 1.75.2.7 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -110,7 +110,7 @@ int DumpStatSumTable(STATSUMENTRY *StatSumTable, int nsegid);
 int main(int argc, char *argv[]) ;
 
 static char vcid[] =
-  "$Id: mri_segstats.c,v 1.75.2.6 2012/09/17 16:39:55 greve Exp $";
+  "$Id: mri_segstats.c,v 1.75.2.7 2013/01/14 22:23:47 greve Exp $";
 char *Progname = NULL, *SUBJECTS_DIR = NULL, *FREESURFER_HOME=NULL;
 char *SegVolFile = NULL;
 char *InVolFile = NULL;
@@ -1040,6 +1040,14 @@ int main(int argc, char **argv)
               "Estimated Total Intracranial Volume, %f, mm^3\n",atlas_icv);
 
     }
+    if(DoETIV && BrainVolFromSeg){
+      fprintf(fp,"# Measure BrainVol-to-eTIV, BrainVol-to-eTIV, "
+              "Ratio of BrainVol to eTIV, %f, unitless\n",
+              BrainVolStats[0]/atlas_icv);
+      fprintf(fp,"# Measure MaskVol-to-eTIV, MaskVol-to-eTIV, "
+              "Ratio of MaskVol to eTIV, %f, unitless\n",
+              BrainVolStats[12]/atlas_icv);
+    }
     if (SegVolFile)
     {
       fprintf(fp,"# SegVolFile %s \n",SegVolFile);
@@ -1305,7 +1313,7 @@ int main(int argc, char **argv)
 #ifdef FS_CUDA
   PrintGPUtimers();
 #endif
-
+  printf("mri_segstats done\n");
   return(0);
 }
 /*-----------------------------------------------------------------*/
