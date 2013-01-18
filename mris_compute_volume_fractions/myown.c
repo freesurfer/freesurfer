7ca2ba44116bb46cd8b03322ac1d95923caf7ef7
Ender Konukoglu <enderk@nmr.mgh.harvard.edu>
2013-01-18 22:30:26 +0000

    First checkin. Still excited...

---
+++ mris_compute_volume_fractions/myown.c
@@ -0,0 +1,42 @@
+/**
+ * @file  connectgraph.c
+ * @brief Convert connectivity data to graphviz data format 
+ *
+ * REPLACE_WITH_LONG_DESCRIPTION_OR_REFERENCE
+ */
+/*
+ * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
+ * CVS Revision Info:
+ *    $Author: enderk $
+ *    $Date: 2013/01/18 22:30:27 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
+ *
+ * Terms and conditions for use, reproduction, distribution and contribution
+ * are found in the 'FreeSurfer Software License Agreement' contained
+ * in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+ *
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+ *
+ * Reporting: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+
+/**
+ * @file   myown.c
+ * @author Yasunari Tosa
+ * @date   Wed Oct 13 11:48:26 2004
+ *
+ * @brief  sample test program
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("just a sample program.\n");
+  exit(0);
+}
