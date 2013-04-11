d6cac07601761e443234617f331eb9edbd3bd42d
Doug Greve <greve@nmr.mgh.harvard.edu>
2013-04-11 20:42:48 +0000

    Copied file from dev. Now adds volume geometry to output surfaces

--- mris_make_average_surface/mris_make_average_surface.c
+++ mris_make_average_surface/mris_make_average_surface.c
@@ -13,21 +13,19 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: fischl $
- *    $Date: 2010/05/27 11:29:19 $
- *    $Revision: 1.28 $
+ *    $Author: greve $
+ *    $Date: 2013/04/11 20:42:48 $
+ *    $Revision: 1.29.2.1 $
  *
- * Copyright (C) 2002-2007,
- * The General Hospital Corporation (Boston, MA). 
- * All rights reserved.
+ * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
- * Distribution, usage and copying of this software is covered under the
- * terms found in the License Agreement file named 'COPYING' found in the
- * FreeSurfer source code root directory, and duplicated here:
- * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+ * Terms and conditions for use, reproduction, distribution and contribution
+ * are found in the 'FreeSurfer Software License Agreement' contained
+ * in the file 'LICENSE' found in the FreeSurfer distribution, and here:
  *
- * General inquiries: freesurfer@nmr.mgh.harvard.edu
- * Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+ *
+ * Reporting: freesurfer@nmr.mgh.harvard.edu
  *
  */
 
@@ -115,7 +113,7 @@ ENDHELP
 #include "gcamorph.h"
 
 static char vcid[] = 
-"$Id: mris_make_average_surface.c,v 1.28 2010/05/27 11:29:19 fischl Exp $";
+"$Id: mris_make_average_surface.c,v 1.29.2.1 2013/04/11 20:42:48 greve Exp $";
 
 int main(int argc, char *argv[]) ;
 
@@ -148,13 +146,14 @@ main(int argc, char *argv[]) {
   float        average_surface_area = 0.0 ;
   MATRIX *XFM=NULL;
   GCA_MORPH *gcam=NULL;
+  MRI *mritemplate;
 
   memset((void *) &vg, 0, sizeof (VOL_GEOM));
 
   /* rkt: check for and handle version tag */
   nargs = handle_version_option 
     (argc, argv, 
-     "$Id: mris_make_average_surface.c,v 1.28 2010/05/27 11:29:19 fischl Exp $",
+     "$Id: mris_make_average_surface.c,v 1.29.2.1 2013/04/11 20:42:48 greve Exp $",
      "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -368,6 +367,13 @@ main(int argc, char *argv[]) {
 #endif
     MRIScomputeMetricProperties(mris_ico) ;
   }
+
+  sprintf(fname, "%s/average/mni305.cor.mgz", mdir);
+  printf("Adding volume geometry from %s\n",fname);
+  mritemplate = MRIread(fname);
+  initVolGeom(&mris_ico->vg);
+  getVolGeom(mritemplate, &mris_ico->vg);
+  MRIfree(&mritemplate);
 
   sprintf(fname, "%s/%s/surf/%s.%s", sdirout,out_sname, hemi, avg_surf_name) ;
   printf("writing average %s surface to %s\n", avg_surf_name, fname);
