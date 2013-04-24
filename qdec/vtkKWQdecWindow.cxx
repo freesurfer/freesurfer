483669ba1e8f0fc7d67e99ad901aeff153d9bfd8
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2013-04-24 20:11:49 +0000

    disable DOSS option for now until its handled properly downstream

--- qdec/vtkKWQdecWindow.cxx
+++ qdec/vtkKWQdecWindow.cxx
@@ -11,8 +11,8 @@
  * Original Author: Kevin Teich
  * CVS Revision Info:
  *    $Author: nicks $
- *    $Date: 2013/04/14 21:09:16 $
- *    $Revision: 1.61.2.6 $
+ *    $Date: 2013/04/24 20:11:49 $
+ *    $Revision: 1.61.2.7 $
  *
  * Copyright © 2011-2013 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -102,7 +102,7 @@ extern "C" {
 using namespace std;
 
 vtkStandardNewMacro( vtkKWQdecWindow );
-vtkCxxRevisionMacro( vtkKWQdecWindow, "$Revision: 1.61.2.6 $" );
+vtkCxxRevisionMacro( vtkKWQdecWindow, "$Revision: 1.61.2.7 $" );
 
 const char* vtkKWQdecWindow::ksSubjectsPanelName = "Subjects";
 const char* vtkKWQdecWindow::ksDesignPanelName = "Design";
@@ -1013,7 +1013,7 @@ vtkKWQdecWindow::CreateWidget () {
   // call the radio-button handler once to set it up
   this->SetCurrentMeasure( "Surface-based" );
 
-#if 1 // HACK disable for now, until DOSS is handled properly downstream
+#if 0 // HACK disable for now, until DOSS is handled properly downstream
   //
   // Create the Design Matrix Type frame.
   vtkSmartPointer<vtkKWFrameWithLabel> designMatrixTypeFrame =
