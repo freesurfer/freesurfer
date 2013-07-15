5a5c72ed594f1b63425fe69287ff63eab6789c2b
Doug Greve <greve@nmr.mgh.harvard.edu>
2013-07-15 15:55:54 +0000

    Fixed naming of last cluster in sclustAnnot()

--- utils/surfcluster.c
+++ utils/surfcluster.c
@@ -10,8 +10,8 @@
  * Original Author: Doug Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2012/02/22 21:07:38 $
- *    $Revision: 1.33 $
+ *    $Date: 2013/07/15 15:55:54 $
+ *    $Revision: 1.34 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -52,7 +52,7 @@ static int sclustCompare(const void *a, const void *b);
   ---------------------------------------------------------------*/
 const char *sculstSrcVersion(void)
 {
-  return("$Id: surfcluster.c,v 1.33 2012/02/22 21:07:38 greve Exp $");
+  return("$Id: surfcluster.c,v 1.34 2013/07/15 15:55:54 greve Exp $");
 }
 
 /* ------------------------------------------------------------
@@ -805,7 +805,7 @@ int sclustAnnot(MRIS *surf, int NClusters)
   ct = CTABalloc(NClusters+1);
   surf->ct = ct;
 
-  for(n=1; n < NClusters; n++) // no cluster 0
+  for(n=1; n < NClusters+1; n++) // no cluster 0
     sprintf(surf->ct->entries[n]->name, "%s-%03d","cluster",n);
 
   for(vtxno = 0; vtxno < surf->nvertices; vtxno++)  {
