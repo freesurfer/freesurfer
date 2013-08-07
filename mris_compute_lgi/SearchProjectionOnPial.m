57faa02b07f7116eeb5fb2780b8ddee0fb86f9ad
Nick Schmansky <nicks@nmr.mgh.harvard.edu>
2013-08-07 21:47:04 +0000

    fix from marie schaer for problem running on matlab 2013

--- mris_compute_lgi/SearchProjectionOnPial.m
+++ mris_compute_lgi/SearchProjectionOnPial.m
@@ -4,7 +4,8 @@ function [verticeslist]=SearchProjectionOnPial(mesh_total,mesh_outer,perim, step
 % limit redundancies in the resulting path file.
 
 verticeslist=[];
-for t=1:step:size(perim,2)
+si=max(size(perim));
+for t=1:step:si
     [nearestIndexMT,nearestValuesMT]=mesh_vertex_nearest(mesh_total.vertices,mesh_outer.vertices(perim(t),:));
     verticeslist= [verticeslist nearestIndexMT];
 end
