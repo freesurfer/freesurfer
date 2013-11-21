c65d7c6efe6feb3c95ddb093c69e57c042055c98
Doug Greve <greve@nmr.mgh.harvard.edu>
2013-11-21 22:47:03 +0000

    Copied from dev. Added --avgwf-remove-mean

--- mri_segstats/mri_segstats.c
+++ mri_segstats/mri_segstats.c
@@ -12,8 +12,8 @@
  * Original Author: Dougas N Greve
  * CVS Revision Info:
  *    $Author: greve $
- *    $Date: 2013/11/21 20:49:15 $
- *    $Revision: 1.75.2.11 $
+ *    $Date: 2013/11/21 22:47:03 $
+ *    $Revision: 1.75.2.12 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -113,7 +113,7 @@ int CountEdits(char *subject, char *outfile);
 int main(int argc, char *argv[]) ;
 
 static char vcid[] =
-  "$Id: mri_segstats.c,v 1.75.2.11 2013/11/21 20:49:15 greve Exp $";
+  "$Id: mri_segstats.c,v 1.75.2.12 2013/11/21 22:47:03 greve Exp $";
 char *Progname = NULL, *SUBJECTS_DIR = NULL, *FREESURFER_HOME=NULL;
 char *SegVolFile = NULL;
 char *InVolFile = NULL;
@@ -131,6 +131,7 @@ char *FrameAvgVolFile = NULL;
 char *SpatFrameAvgFile = NULL;
 int DoFrameAvg = 0;
 int DoFrameSum = 0;
+int RmFrameAvgMn = 0;
 int DoAccumulate = 0;
 int frame = 0;
 int synth = 0;
@@ -1332,6 +1333,7 @@ int main(int argc, char **argv)
 	favgmn[n] += favg[n][f];
       }
       favgmn[n] /= invol->nframes;
+      if(RmFrameAvgMn) for(f=0; f < invol->nframes; f++) favg[n][f] -= favgmn[n];
     }
     printf("\n");
 
@@ -1764,6 +1766,7 @@ static int parse_commandline(int argc, char **argv)
       DoFrameAvg = 1;
       nargsused = 1;
     }
+    else if ( !strcmp(option, "--avgwf-remove-mean") ) RmFrameAvgMn = 1;
     else if ( !strcmp(option, "--sumwf") )
     {
       if (nargc < 1) argnerr(option,1);
