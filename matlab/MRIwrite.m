c91cb646e059633d654406075a89e6fec9a8090b
Doug Greve <greve@nmr.mgh.harvard.edu>
2013-12-02 16:34:23 +0000

    Copied from dev. now handles volres dimension order in a way consistent with MRIread

--- matlab/MRIwrite.m
+++ matlab/MRIwrite.m
@@ -32,20 +32,18 @@ function err = MRIwrite(mri,fstring,datatype)
 % Original Author: Doug Greve
 % CVS Revision Info:
 %    $Author: greve $
-%    $Date: 2010/04/07 17:20:08 $
-%    $Revision: 1.13 $
+%    $Date: 2013/12/02 16:34:23 $
+%    $Revision: 1.14.2.1 $
 %
-% Copyright (C) 2002-2007,
-% The General Hospital Corporation (Boston, MA). 
-% All rights reserved.
+% Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
 %
-% Distribution, usage and copying of this software is covered under the
-% terms found in the License Agreement file named 'COPYING' found in the
-% FreeSurfer source code root directory, and duplicated here:
-% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+% Terms and conditions for use, reproduction, distribution and contribution
+% are found in the 'FreeSurfer Software License Agreement' contained
+% in the file 'LICENSE' found in the FreeSurfer distribution, and here:
 %
-% General inquiries: freesurfer@nmr.mgh.harvard.edu
-% Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+%
+% Reporting: freesurfer@nmr.mgh.harvard.edu
 %
 
 
@@ -137,9 +135,9 @@ switch(fmt)
   end
   hdr.slice_start     = 0;
 
-  % volres is not permuted in MRIread()
-  %hdr.pixdim          = [0 mri.volres([2 1 3]) mri.tr]; % physical units
-  hdr.pixdim          = [0 mri.volres mri.tr]; % physical units
+  % volres is permuted in MRIread()
+  hdr.pixdim          = [0 mri.volres([2 1 3]) mri.tr]; % physical units
+  %hdr.pixdim          = [0 mri.volres mri.tr]; % physical units
   hdr.vox_offset      = 348; % will be set again
   hdr.scl_slope       = 0;
   hdr.scl_inter       = 0;
