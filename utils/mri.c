b536d863968f9cf2501d861721cf3c01148d8daf
Doug Greve <greve@nmr.mgh.harvard.edu>
2014-02-06 19:47:54 +0000

    Fixed MRIupsampleN(). The offset was slightly off.

--- utils/mri.c
+++ utils/mri.c
@@ -6,9 +6,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: fischl $
- *    $Date: 2014/01/29 20:40:03 $
- *    $Revision: 1.530 $
+ *    $Author: greve $
+ *    $Date: 2014/02/06 19:47:54 $
+ *    $Revision: 1.531 $
  *
  * Copyright © 2011-2012 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -23,7 +23,7 @@
  */
 
 extern const char* Progname;
-const char *MRI_C_VERSION = "$Revision: 1.530 $";
+const char *MRI_C_VERSION = "$Revision: 1.531 $";
 
 
 /*-----------------------------------------------------
@@ -8058,12 +8058,13 @@ MRI *MRIupsampleN(MRI *mri_src, MRI *mri_dst, int N)
 
   // Computes CRAS based on location of the 1st voxel in upsampled space
   // The new location is 1/(2*Nth) of a voxel from the corner. The RAS of
-  // a voxel is at the center of the voxel (unfortunately)
+  // a voxel is at the center of the voxel (unfortunately), so the corner
+  // is located at CRS=[-.5 -.5 -.5]
   Vox2RAS = MRIxfmCRS2XYZ(mri_src,0); // scanner vox2ras of source mri
   CRS0 = MatrixZero(4,1,NULL);
-  CRS0->rptr[1][1] = -1.0/(2*N);
-  CRS0->rptr[2][1] = -1.0/(2*N);
-  CRS0->rptr[3][1] = -1.0/(2*N);
+  CRS0->rptr[1][1] = -0.5+1.0/(2*N);
+  CRS0->rptr[2][1] = -0.5+1.0/(2*N);
+  CRS0->rptr[3][1] = -0.5+1.0/(2*N);
   CRS0->rptr[4][1] =  1.0;
   RAS0 = MatrixMultiply(Vox2RAS,CRS0,NULL);
   MRIp0ToCRAS(mri_dst, RAS0->rptr[1][1],RAS0->rptr[2][1],RAS0->rptr[3][1]);
