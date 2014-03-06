d3aa67476d2b9480ecfee5274fb2f7be55825ac3
Doug Greve <greve@nmr.mgh.harvard.edu>
2014-03-06 21:40:54 +0000

    Added ability to spec a volume to fill in the surface volume geometry

--- mris_convert/mris_convert.c
+++ mris_convert/mris_convert.c
@@ -6,9 +6,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: lzollei $
- *    $Date: 2014/02/10 20:28:08 $
- *    $Revision: 1.42 $
+ *    $Author: greve $
+ *    $Date: 2014/03/06 21:40:54 $
+ *    $Revision: 1.43 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -45,7 +45,7 @@
 
 //------------------------------------------------------------------------
 static char vcid[] =
-"$Id: mris_convert.c,v 1.42 2014/02/10 20:28:08 lzollei Exp $";
+"$Id: mris_convert.c,v 1.43 2014/03/06 21:40:54 greve Exp $";
 
 /*-------------------------------- CONSTANTS -----------------------------*/
 // this mini colortable is used when .label file gets converted to gifti
@@ -104,6 +104,7 @@ static MATRIX *XFM=NULL;
 static int write_vertex_neighbors = 0;
 static int combinesurfs_flag = 0;
 static int userealras_flag = 0;
+static MRI *VolGeomMRI=NULL;
 
 int DeleteCommands = 0;
 int MRISwriteVertexNeighborsAscii(MRIS *mris, char *out_fname);
@@ -123,7 +124,7 @@ main(int argc, char *argv[]) {
   /* rkt: check for and handle version tag */
   nargs = handle_version_option
     (argc, argv,
-     "$Id: mris_convert.c,v 1.42 2014/02/10 20:28:08 lzollei Exp $",
+     "$Id: mris_convert.c,v 1.43 2014/03/06 21:40:54 greve Exp $",
      "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
@@ -503,6 +504,7 @@ main(int argc, char *argv[]) {
     } else {
       // default output: 
       printf("Saving %s as a surface\n",out_fname);
+      if(VolGeomMRI) getVolGeom(VolGeomMRI,&mris->vg);
       MRISwrite(mris, out_fname) ;
     }
   }
@@ -557,6 +559,11 @@ get_option(int argc, char *argv[]) {
   } 
   else if (!stricmp(option, "-userealras")) {
     userealras_flag = 1;
+  } 
+  else if (!stricmp(option, "-vol-geom")) {
+    VolGeomMRI = MRIreadHeader(argv[2],MRI_VOLUME_TYPE_UNKNOWN);
+    if(VolGeomMRI == NULL) exit(1);
+    nargs = 1 ;
   } 
   else switch (toupper(*option)) {
   case 'A':
@@ -666,6 +673,7 @@ print_help(void) {
   printf( "  --combinesurfs <infile> <in2file> <outfile>\n") ;
   printf( "  --delete-cmds : delete command lines in surface\n") ;
   printf( "  --userealras : set the useRealRAS flag in the surface file to 1 \n") ;
+  printf( "  --vol-geom MRIVol : use MRIVol to set the volume geometry\n") ;
   printf( "\n") ;
   printf( "These file formats are supported:\n") ;
   printf( "  ASCII:       .asc\n");
