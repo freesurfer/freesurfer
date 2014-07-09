46829e1fceff8c3b87ed80097b81e979b56db7af
Doug Greve <greve@nmr.mgh.harvard.edu>
2014-07-09 23:17:13 +0000

    Added ability to remove chars from the end too

--- matlab/striphemi.m
+++ matlab/striphemi.m
@@ -1,16 +1,24 @@
-function roilist2 = striphemi(roilist)
-% roilist2 = striphemi(roilist)
+function roilist2 = striphemi(roilist,ncut)
+% roilist2 = striphemi(roilist,<ncut>)
 % Removes hemifield designations from list of roi names
-% $Id: striphemi.m,v 1.1 2014/07/08 16:03:25 greve Exp $
+% If ncut is nonempty and greater than 0 then removes ncut letters
+%   from the end of each roiname. This can be helpful when removing
+%   things like '_thickness'.
+% $Id: striphemi.m,v 1.2 2014/07/09 23:17:13 greve Exp $
+
+if(~exist('ncut','var')) ncut = []; end
+if(isempty(ncut)) ncut = 0; end
 
 nrois = size(roilist,1);
 
-hemistringlist = strvcat('Left-','Right-','ctx-lh-','ctx-rh-','wm-lh-','wm-rh-');
+hemistringlist = strvcat('Left-','Right-','ctx-lh-','ctx-rh-',...
+			 'wm-lh-','wm-rh-','rh_','lh_');
 nhemistrings = size(hemistringlist,1);
 
 roilist2 = '';
 for nth = 1:nrois
   s = deblank(roilist(nth,:));
+  s = s(1:end-ncut);
   hit = 0;
   for nthhs = 1:nhemistrings
     hs = deblank(hemistringlist(nthhs,:));
