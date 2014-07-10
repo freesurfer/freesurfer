375df1fb44edfe8c1231f405cdce006f2cb2bbff
Doug Greve <greve@nmr.mgh.harvard.edu>
2014-07-10 03:47:18 +0000

    Copied from dev. Fixed read

--- matlab/lta_read.m
+++ matlab/lta_read.m
@@ -7,21 +7,19 @@ function M = lta_read(fname)
 %
 % Original Author: Bruce Fischl
 % CVS Revision Info:
-%    $Author: nicks $
-%    $Date: 2007/01/10 22:55:09 $
-%    $Revision: 1.2 $
+%    $Author: greve $
+%    $Date: 2014/07/10 03:47:18 $
+%    $Revision: 1.3.2.1 $
 %
-% Copyright (C) 2002-2007,
-% The General Hospital Corporation (Boston, MA). 
-% All rights reserved.
+% Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
 %
-% Distribution, usage and copying of this software is covered under the
-% terms found in the License Agreement file named 'COPYING' found in the
-% FreeSurfer source code root directory, and duplicated here:
-% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferOpenSourceLicense
+% Terms and conditions for use, reproduction, distribution and contribution
+% are found in the 'FreeSurfer Software License Agreement' contained
+% in the file 'LICENSE' found in the FreeSurfer distribution, and here:
 %
-% General inquiries: freesurfer@nmr.mgh.harvard.edu
-% Bug reports: analysis-bugs@nmr.mgh.harvard.edu
+% https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+%
+% Reporting: freesurfer@nmr.mgh.harvard.edu
 %
 
 if (strcmp(fname((length(fname)-2):length(fname)), 'xfm') | ...
@@ -41,8 +39,7 @@ while ((length(tline) > 0) & (tline(1) == '#'))
 	tline = fgetl(fid) ;
 end
 
-
-tline = fgetl(fid) ;  % type
+%tline = fgetl(fid) ;  % type --> gotten above
 tline = fgetl(fid) ;  % nxforms
 tline = fgetl(fid) ;  % mean
 tline = fgetl(fid) ;  % sigma
@@ -50,9 +47,9 @@ tline = fgetl(fid) ;  % dimensions
 
 M = zeros(4,4) ;
 for row=1:4
-	tline = fgetl(fid) ;  % one row of matrix
-	tmp = sscanf(tline, '%f');
-	 M(row,:) = tmp';
+  tline = fgetl(fid) ;  % one row of matrix
+  tmp = sscanf(tline, '%f');
+  M(row,:) = tmp';
 end
 
 fclose(fid) ;
