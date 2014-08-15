80cd43912ac0c7b48410678ac689e28cafe265de
Zeke Kaufman <zkaufman@nmr.mgh.harvard.edu>
2014-08-15 16:15:07 +0000

    A revamp of the license checking scheme so that we are compatible with newer OS platforms but also backwards compatible.

--- utils/chklc.c
+++ utils/chklc.c
@@ -6,9 +6,9 @@
 /*
  * Original Author: Bruce Fischl
  * CVS Revision Info:
- *    $Author: rpwang $
- *    $Date: 2014/04/11 18:37:09 $
- *    $Revision: 1.18 $
+ *    $Author: zkaufman $
+ *    $Date: 2014/08/15 16:15:07 $
+ *    $Revision: 1.19 $
  *
  * Copyright © 2011-2013 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -51,7 +51,6 @@ static const char* licmsg =
 "  make sure to source the standard environment.\n"
 "--------------------------------------------------------------------------\n";
 
-#ifndef Darwin
 static const char* licmsg2 =
 "--------------------------------------------------------------------------\n"
 "ERROR: Invalid FreeSurfer license key found in license file %s\n"
@@ -61,7 +60,6 @@ static const char* licmsg2 =
 "  If you are inside the NMR-Martinos Center,\n"
 "  make sure to source the standard environment.\n"
 "--------------------------------------------------------------------------\n";
-#endif
 
 void chklc(void)
 {
@@ -70,9 +68,11 @@ void chklc(void)
   char* email;
   char* magic;
   char* key;
+  char* key2;
   char* gkey;
   char* lfilename;
   char  str[STRLEN] ;
+  char* crypt_gkey ;
 
   sprintf(str, "S%sER%sRONT%sOR", "URF", "_F", "DO") ;
   if (getenv(str) != NULL) return ;
@@ -90,6 +90,7 @@ void chklc(void)
   email = (char*)calloc(1,512);
   magic = (char*)calloc(1,512);
   key   = (char*)calloc(1,512);
+  key2  = (char*)calloc(1,512);
   gkey  = (char*)calloc(1,1024);
 
   sprintf(lfilename,"%s/.lic%s",dirname, "ense");
@@ -119,33 +120,46 @@ void chklc(void)
   fscanf(lfile,"%s\n",email);
   fscanf(lfile,"%s\n",magic);
   fscanf(lfile,"%s\n",key);
+  fscanf(lfile,"%s\n",key2); 
 
   sprintf(gkey,"%s.%s",email,magic);
-
-#ifndef Darwin
+  
+  // This code is meant to provide backwards compatibility
+  // of freesurfer license checking. Unfortunately previous 
+  // freesurfer license keys used an improper salt value of
+  // "*C" which is not valid because it should be purely 
+  // alpha-numeric. New license files have a 4th line with
+  // a key generated using a proper salt.
+  
+  if (strcmp(key2, "") != 0)
   {
-    char* crypt_gkey = crypt(gkey,"*C*O*R*T*E*C*H*S*0*1*2*3*");
-    // it seems some implementations of crypt (like Fedora18)
-    // don't like the input we give it and crypt returns NULL, which crashes 
-    // those systems, so check and skip
-    if (crypt_gkey == NULL)
-    {
-      //fprintf(stderr,"chklc: crypt returned NULL!\n");
-    }
-    else
-    {
-      if (strcmp(key,crypt(gkey,crypt_gkey))!=0)
-      {
-        fprintf(stderr,licmsg2,lfilename);
-        exit(-1);
-      }
-    }
+    // We have a 4 line license file.
+    key = key2;
+    crypt_gkey = crypt(gkey,"FS");
+  } 
+  else 
+  {
+    // We have a 3 line license file.
+    #ifdef Darwin
+      // On Darwin systems the key produced with a salt of '*C'
+      // is different than that produced on Linux. So to be backwards
+      // compatible we must avoid the call to crypt.
+      crypt_gkey = key;
+    #else 
+      crypt_gkey = crypt(gkey,"*C");
+    #endif
   }
-#endif
+
+  if (strcmp(key,crypt_gkey)!=0)
+  {
+    fprintf(stderr,licmsg2,lfilename);
+    exit(-1);
+  }
 
   free(email);
   free(magic);
   free(key);
+  free(key2);
   free(gkey);
   free(lfilename);
   fclose(lfile) ;
@@ -153,41 +167,46 @@ void chklc(void)
   return;
 }
 
+//  Unfortunately we need separate, but nearly identicle, license checking code
+//  for freeview. This is because the license checking above will exit(-1) 
+//  if the license check fails. But freeview is a clickable application on mac
+//  which might not have a terminal window open, so we want a message to appear
+//  if the license check fails. The only way to do this is to either modify 
+//  all existing calls the chklc throughout the code, or have separate license
+//  checking code for freeview. The first scares me, so Im going with the latter.
+//
+//  Also there are difference with the way the FREESURFER_HOME environment variable 
+//  is handled. It doesnt need to be defined for freeview to operate. So perhaps
+//  this somewhat redundant code is a tad more justified.
+//
 //  return value:   0 - failed
 //                  1 - passed
+//
 //  if failed, error msg will be returned in msg. make sure msg is pre-allocated with enough space
 int chklc2(char* msg)
 {
-  char  dirname[STRLEN], *cp ;
+
   FILE* lfile;
   char* email;
   char* magic;
   char* key;
+  char* key2;
   char* gkey;
   char* lfilename;
   char  str[STRLEN] ;
+  char* crypt_gkey ;
 
   sprintf(str, "S%sER%sRONT%sOR", "URF", "_F", "DO") ;
   if (getenv(str) != NULL) return 1;
-
-  cp = getenv("FREESURFER_HOME");
-  if (cp == NULL)
-  {
-    fprintf(stderr,"%s",errmsg);
-    if (msg)
-      sprintf(msg, "%s", errmsg);
-    return 0;
-  }
-
-  strncpy(dirname, cp, STRLEN) ;
 
   lfilename = (char*)calloc(1,512);
   email = (char*)calloc(1,512);
   magic = (char*)calloc(1,512);
   key   = (char*)calloc(1,512);
+  key2  = (char*)calloc(1,512);
   gkey  = (char*)calloc(1,1024);
 
-  sprintf(lfilename,"%s/.lic%s",dirname, "ense");
+  sprintf(lfilename,".lic%s", "ense");
 
   lfile = fopen(lfilename,"r");
   char permission_msg[] =
@@ -204,7 +223,7 @@ int chklc2(char* msg)
         sprintf(msg,permission_msg,lfilename,lfilename);
       return 0;
     }
-    sprintf(lfilename,"%s/lic%s",dirname, "ense.txt");
+    sprintf(lfilename,".lic%s", "ense.txt");
     lfile = fopen(lfilename,"r");
   }
   if (lfile == NULL)
@@ -224,35 +243,48 @@ int chklc2(char* msg)
   fscanf(lfile,"%s\n",email);
   fscanf(lfile,"%s\n",magic);
   fscanf(lfile,"%s\n",key);
+  fscanf(lfile,"%s\n",key2); 
 
   sprintf(gkey,"%s.%s",email,magic);
 
-#ifndef Darwin
+  // This code is meant to provide backwards compatibility
+  // of freesurfer license checking. Unfortunately previous 
+  // freesurfer license keys used an improper salt value of
+  // "*C" which is not valid because it should be purely 
+  // alpha-numeric. New license files have a 4th line with
+  // a key generated using a proper salt.
+  
+  if (strcmp(key2, "") != 0)
   {
-    char* crypt_gkey = crypt(gkey,"*C*O*R*T*E*C*H*S*0*1*2*3*");
-    // it seems some implementations of crypt (like Fedora18)
-    // don't like the input we give it and crypt returns NULL, which crashes
-    // those systems, so check and skip
-    if (crypt_gkey == NULL)
-    {
-      //fprintf(stderr,"chklc: crypt returned NULL!\n");
-    }
-    else
-    {
-      if (strcmp(key,crypt(gkey,crypt_gkey))!=0)
-      {
-        fprintf(stderr,licmsg2,lfilename);
-        if (msg)
-          sprintf(msg, licmsg2, lfilename);
-        return 0;
-      }
-    }
+    // We have a 4 line license file.
+    key = key2;
+    crypt_gkey = crypt(gkey,"FS");
+  } 
+  else 
+  {
+    // We have a 3 line license file.
+    #ifdef Darwin
+      // On Darwin systems the key produced with a salt of '*C'
+      // is different than that produced on Linux. So to be backwards
+      // compatible we must avoid the call to crypt.
+      crypt_gkey = key;
+    #else 
+      crypt_gkey = crypt(gkey,"*C");
+    #endif
   }
-#endif
+
+  if (strcmp(key,crypt_gkey)!=0)
+  {
+    fprintf(stderr,licmsg2,lfilename);
+    if (msg)
+      sprintf(msg, licmsg2, lfilename);
+    return 0;
+  }
 
   free(email);
   free(magic);
   free(key);
+  free(key2);
   free(gkey);
   free(lfilename);
   fclose(lfile) ;
