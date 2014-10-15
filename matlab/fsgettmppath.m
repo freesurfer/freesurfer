bd79c55d90257a90843c514c2d6e594c373ab263
Doug Greve <greve@nmr.mgh.harvard.edu>
2014-10-15 21:28:40 +0000

    Gets path to a temporary folder (does NOT generate a random file name) based on searching for locations in a certain order.

---
+++ matlab/fsgettmppath.m
@@ -0,0 +1,45 @@
+function tmppath = fsgettmppath(tmppathdefault)
+% tmppath = fsgettmppath(<tmppathdefault>)
+% Gets path to a temporary folder (does NOT generate a random file name)
+% First looks in the following order:
+%  1. $TMPDIR - env var must exist and folder must exist
+%  2. $TEMPDIR - env var must exist and folder must exist
+%  3. /scratch - folder must exist
+%  4. /tmp - folder must exist
+%  5. tmppathdefault - if passed
+%  6. current folder, ie, ./ (prints warning)
+
+
+tmppath = getenv('TMPDIR');
+if(~isempty(tmppath)) 
+  if(exist(tmppath)) 
+    return; 
+  end
+end
+
+tmppath = getenv('TEMPDIR');
+if(~isempty(tmppath)) 
+  if(exist(tmppath)) 
+    return; 
+  end
+end  
+
+tmppath = '/scratch';
+if(exist(tmppath)) return; end
+
+tmppath = '/tmp';
+if(exist(tmppath)) return; end
+
+if(nargin > 0)
+  tmppath = tmppathdefault;
+  if(exist(tmppath)) return; end
+end
+
+tmppath = './';
+fprintf(['WARNING: fsgettmppath: could not find a temporary folder,' ...
+	 ' using current folder\n']);
+
+return;
+
+
+
