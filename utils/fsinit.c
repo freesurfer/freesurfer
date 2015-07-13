33c8a394b2ee9156f6f00c7dca685bbeb89f46f5
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2015-07-13 17:56:52 +0000

    if OMP_NUM_THREADS is not defined in the env, set the number of threads to be 1

--- utils/fsinit.c
+++ utils/fsinit.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <locale.h>
 #include "error.h"
+#ifdef HAVE_OPENMP
+#include <omp.h>
+#endif
 
 
 
@@ -10,6 +13,8 @@ FSinit(void)
   char *cp ;
   if (getenv("FS_DISABLE_LANG") == NULL)
     cp = setlocale(LC_NUMERIC, "en_US");
+  if (getenv("OMP_NUM_THREADS") == NULL)
+    omp_set_num_threads(1);
   return(NO_ERROR) ;
 }
 
