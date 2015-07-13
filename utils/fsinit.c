7658822f1691d7943926cf4520c73e311ffbdc48
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2015-07-13 17:57:31 +0000

    check that openmp exists before calling omp_set_num_threads

--- utils/fsinit.c
+++ utils/fsinit.c
@@ -13,8 +13,10 @@ FSinit(void)
   char *cp ;
   if (getenv("FS_DISABLE_LANG") == NULL)
     cp = setlocale(LC_NUMERIC, "en_US");
+#ifdef HAVE_OPENMP
   if (getenv("OMP_NUM_THREADS") == NULL)
     omp_set_num_threads(1);
+#endif
   return(NO_ERROR) ;
 }
 
