ab701c40c0e0b626444f40f76724421d1c5fc5e8
Z K <zkaufman@nmr.mgh.harvard.edu>
2016-06-09 10:38:45 -0400

    Updated help text. -fischl

--- mri_extract/mri_extract.c
+++ mri_extract/mri_extract.c
@@ -7,9 +7,9 @@
 /*
  * Original Author: REPLACE_WITH_FULL_NAME_OF_CREATING_AUTHOR 
  * CVS Revision Info:
- *    $Author: nicks $
- *    $Date: 2011/03/02 00:04:15 $
- *    $Revision: 1.6 $
+ *    $Author: fischl $
+ *    $Date: 2016/06/08 13:42:17 $
+ *    $Revision: 1.8 $
  *
  * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
  *
@@ -42,6 +42,8 @@ static int get_option(int argc, char *argv[]) ;
 char *Progname ;
 static int reductions = 1 ;
 static int verbose = 0 ;
+static int pad = 0 ;
+static float thresh = 0.0 ;
 
 int
 main(int argc, char *argv[]) {
@@ -49,9 +51,10 @@ main(int argc, char *argv[]) {
   int    ac, nargs, x0, y0, z0, dx, dy, dz ;
   MRI    *mri_src, *mri_dst = NULL ;
   char   *in_dir, *out_dir ;
+  MRI_REGION box ;
 
   /* rkt: check for and handle version tag */
-  nargs = handle_version_option (argc, argv, "$Id: mri_extract.c,v 1.6 2011/03/02 00:04:15 nicks Exp $", "$Name:  $");
+  nargs = handle_version_option (argc, argv, "$Id: mri_extract.c,v 1.8 2016/06/08 13:42:17 fischl Exp $", "$Name:  $");
   if (nargs && argc - nargs == 1)
     exit (0);
   argc -= nargs;
@@ -63,6 +66,8 @@ main(int argc, char *argv[]) {
   ac = argc ;
   av = argv ;
   for ( ; argc > 1 && ISOPTION(*argv[1]) ; argc--, argv++) {
+    if (isdigit(*(argv[1]+1)))
+      break ; // not an option - a negative number
     nargs = get_option(argc, argv) ;
     argc -= nargs ;
     argv += nargs ;
@@ -76,7 +81,7 @@ main(int argc, char *argv[]) {
 
   if (argc < 8)
     ErrorExit(ERROR_BADPARM,
-              "usage: %s <src_dir> x0 y0 z0 dx dy dz <dst_dir>", Progname) ;
+              "usage: %s <src volume> x0 y0 z0 dx dy dz <dst volume>", Progname) ;
 
   in_dir = argv[1] ;
   if (sscanf(argv[2], "%d", &x0) != 1)
@@ -109,6 +114,21 @@ main(int argc, char *argv[]) {
   if (!mri_src)
     exit(1) ;
 
+  MRIboundingBox(mri_src, thresh, &box) ;
+  if (x0 < 0)
+    x0 = box.x-pad ;
+  if (y0 < 0)
+    y0 = box.y-pad ;
+  if (z0 < 0)
+    z0 = box.z-pad ;
+  if (dx < 0)
+    dx = box.dx+2*pad ;
+  if (dy < 0)
+    dy = box.dy+2*pad ;
+  if (dz < 0)
+    dz = box.dz+2*pad ;
+  printf("using bounding box (%d, %d, %d) -> (%d, %d, %d)\n", x0, y0,z0, x0+dx-1, y0+dy-1,z0+dz-1) ;
+
   mri_dst = MRIextract(mri_src, NULL, x0, y0, z0, dx, dy, dz) ;
 
   if (!mri_dst)
@@ -137,6 +157,16 @@ get_option(int argc, char *argv[]) {
   switch (toupper(*option)) {
   case 'V':
     verbose = 1 ;
+    break ;
+  case 'P':
+    pad = atoi(argv[2]) ;
+    printf("using pad = %d\n", pad) ;
+    nargs = 1 ;
+    break ;
+  case 'T':
+    thresh = atof(argv[2]) ;
+    printf("using threshold = %f\n", thresh) ;
+    nargs = 1 ;
     break ;
   case 'N':
     sscanf(argv[2], "%d", &reductions) ;
