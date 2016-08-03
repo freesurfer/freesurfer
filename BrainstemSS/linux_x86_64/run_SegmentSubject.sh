211d50dc8c32da2b6327cdd8a78c690da07190f9
Z K <zkaufman@nmr.mgh.harvard.edu>
2016-08-03 16:56:06 -0400

    Update to Eugenio BrainstemSS code.

--- BrainstemSS/linux_x86_64/run_SegmentSubject.sh
+++ BrainstemSS/linux_x86_64/run_SegmentSubject.sh
@@ -14,8 +14,7 @@ else
   echo Setting up environment variables
   MCRROOT="$1"
   echo ---
-
-  MCRJRE=${MCRROOT}/sys/java/jre/glnxa64/jre/lib/amd64 ;
+ MCRJRE=${MCRROOT}/sys/java/jre/glnxa64/jre/lib/amd64 ;
 
   LD_LIBRARY_PATH=.:${MCRROOT}/runtime/glnxa64:${MCRROOT}/bin/glnxa64:${MCRROOT}/sys/os/glnxa64:${MCRJRE}/native_threads:${MCRJRE}/server:${MCRJRE}/client:${MCRJRE}:$LD_LIBRARY_PATH ;
 
@@ -23,6 +22,9 @@ else
 
   export LD_LIBRARY_PATH;
   export XAPPLRESDIR;
+ 
+  unset JAVA_TOOL_OPTIONS
+
   echo LD_LIBRARY_PATH is ${LD_LIBRARY_PATH};
   shift 1
   args=
@@ -35,7 +37,7 @@ else
   RANDOMNUMBER=$(od -vAn -N4 -tu4 < /dev/urandom) ;
   MCR_CACHE_ROOT=$( echo "/tmp/MCR_${RANDOMNUMBER}/" | tr -d ' ' ) ;
   export MCR_CACHE_ROOT;
-  "${exe_dir}"/SegmentSubject.bin $args
+  "${exe_dir}"/segmentSubject $args
   rm -rf $MCR_CACHE_ROOT
 
 fi
--- BrainstemSS/mac_osx/run_SegmentSubject.sh
+++ BrainstemSS/mac_osx/run_SegmentSubject.sh
@@ -38,3 +38,4 @@ else
 fi
 exit
 
+
