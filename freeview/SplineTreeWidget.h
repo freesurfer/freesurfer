f1f41ee196d80775fbc2357d0d28cde5eaf91d55
Z K <zkaufman@nmr.mgh.harvard.edu>
2016-09-08 09:40:12 -0400

    Initial add of SplineTreeWidget.cpp SplineTreeWidget.h

---
+++ freeview/SplineTreeWidget.h
@@ -0,0 +1,24 @@
+#ifndef SplineTreeWidget_H
+#define SplineTreeWidget_H
+
+#include <QTreeWidget>
+
+class SplineTreeWidget : public QTreeWidget
+{
+    Q_OBJECT
+public:
+    explicit SplineTreeWidget(QWidget *parent = 0);
+
+  void contextMenuEvent(QContextMenuEvent *e);
+
+signals:
+
+public slots:
+  void OnItemLock();
+
+protected:
+  void drawRow ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const;
+
+};
+
+#endif // SplineTreeWidget_H
