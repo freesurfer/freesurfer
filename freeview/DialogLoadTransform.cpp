5037eae2c903aa407e4c4c8ae61f714c19a85088
Z K <zkaufman@nmr.mgh.harvard.edu>
2016-10-28 10:32:44 -0400

    Copy from dev. Initial add.

---
+++ freeview/DialogLoadTransform.cpp
@@ -0,0 +1,66 @@
+#include "DialogLoadTransform.h"
+#include "ui_DialogLoadTransform.h"
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QFileInfo>
+
+extern "C"
+{
+#include "mri.h"
+}
+
+
+DialogLoadTransform::DialogLoadTransform(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DialogLoadTransform)
+{
+    ui->setupUi(this);
+    connect(ui->toolButtonOpenRegistration, SIGNAL(clicked(bool)), SLOT(OnButtonOpen()));
+}
+
+DialogLoadTransform::~DialogLoadTransform()
+{
+    delete ui;
+}
+
+void DialogLoadTransform::OnOK()
+{
+    if (ui->lineEditRegistration->text().trimmed().isEmpty())
+    {
+        QMessageBox::warning(this, tr("Error"), tr("Please select transformation file to Load"));
+    }
+    else
+        accept();
+}
+
+QString DialogLoadTransform::GetFilename()
+{
+  return ui->lineEditRegistration->text().trimmed();
+}
+
+int DialogLoadTransform::GetSampleMethod()
+{
+  if ( ui->radioNearest->isChecked() )
+  {
+    return SAMPLE_NEAREST;
+  }
+  else if (ui->radioTrilinear->isChecked())
+  {
+    return SAMPLE_TRILINEAR;
+  }
+  else
+    return SAMPLE_CUBIC_BSPLINE;
+}
+
+void DialogLoadTransform::OnButtonOpen()
+{
+    QString fn = QFileDialog::getOpenFileName( this, "Select registration file",
+                       m_strLastDir,
+                       "Registration files (*)");
+    if ( !fn.isEmpty() )
+    {
+        ui->lineEditRegistration->setText(fn);
+        ui->lineEditRegistration->setCursorPosition( fn.size() );
+        m_strLastDir = QFileInfo(fn).absolutePath();
+    }
+}
---
+++ freeview/DialogLoadTransform.h
@@ -0,0 +1,32 @@
+#ifndef DIALOGLOADTRANSFORM_H
+#define DIALOGLOADTRANSFORM_H
+
+#include <QDialog>
+
+namespace Ui {
+class DialogLoadTransform;
+}
+
+class DialogLoadTransform : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogLoadTransform(QWidget *parent = 0);
+    ~DialogLoadTransform();
+
+    QString GetFilename();
+
+    int GetSampleMethod();
+
+public slots:
+    void OnOK();
+    void OnButtonOpen();
+
+private:
+    Ui::DialogLoadTransform *ui;
+
+    QString     m_strLastDir;
+};
+
+#endif // DIALOGLOADTRANSFORM_H
