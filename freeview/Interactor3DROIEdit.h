3371da4f3f9fa098082365b9cd477905907a911d
Z K <zkaufman@nmr.mgh.harvard.edu>
2016-12-10 00:14:46 -0500

    WIP: label editing/mapping to surface. -rpwang

---
+++ freeview/Interactor3DROIEdit.h
@@ -0,0 +1,44 @@
+/**
+ * @file  Interactor3DROIEdit.h
+ * @brief Interactor for navigating in 3D render view.
+ *
+ */
+/*
+ * Original Author: Ruopeng Wang
+ * CVS Revision Info:
+ *    $Author: rpwang $
+ *    $Date: 2016/12/08 21:47:08 $
+ *    $Revision: 1.1 $
+ *
+ * Copyright © 2011 The General Hospital Corporation (Boston, MA) "MGH"
+ *
+ * Terms and conditions for use, reproduction, distribution and contribution
+ * are found in the 'FreeSurfer Software License Agreement' contained
+ * in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+ *
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+ *
+ * Reporting: freesurfer@nmr.mgh.harvard.edu
+ *
+ *
+ */
+
+#ifndef Interactor3DROIEdit_h
+#define Interactor3DROIEdit_h
+
+#include "Interactor3D.h"
+
+class Interactor3DROIEdit : public Interactor3D
+{
+public:
+  Interactor3DROIEdit( QObject* parent );
+
+  virtual bool ProcessMouseDownEvent( QMouseEvent* event, RenderView* view );
+  virtual bool ProcessMouseMoveEvent( QMouseEvent* event, RenderView* view );
+  virtual bool ProcessMouseUpEvent( QMouseEvent* event, RenderView* view );
+
+};
+
+#endif
+
+
