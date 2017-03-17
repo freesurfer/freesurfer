7ca3bb2f47fbbf2a4df8e3855d742b7e930e9d01
Richard Edgar <freesurfer.rge@gmail.com>
2017-03-16 20:37:13 -0400

    Constructing single tetrahedron and test Currently failing

--- GEMS2/Testing/testatlasmeshvisitcounter.cpp
+++ GEMS2/Testing/testatlasmeshvisitcounter.cpp
@@ -53,6 +53,7 @@ BOOST_AUTO_TEST_CASE( OriginOnly )
 
   typedef kvl::interfaces::AtlasMeshVisitCounter::ImageType ImageType;
   typedef itk::AutomaticTopologyMeshSource<kvl::AtlasMesh> MeshSource;
+  typedef MeshSource::IdentifierType  IdentifierType;
   typedef kvl::AtlasMesh Mesh;
 
   ImageType::RegionType region;
@@ -87,10 +88,54 @@ BOOST_AUTO_TEST_CASE( OriginOnly )
 
   BOOST_TEST_CHECKPOINT("Cube set");
 
-  // Create mesh
+  // Create mesh with single tetrahedron
   MeshSource::Pointer meshSource = MeshSource::New();
 
-  Mesh::Pointer mesh = Mesh::New();
+  // Define tetrahedron enclosing origin and (0,0,1), (0,1,0) and (1,0,0)
+  const float delta = 0.5f;
+  const float  p0[] = { -delta, -delta, -delta };
+  const float  p1[] = { 1+delta,- delta, -delta };
+  const float  p2[] = { -delta, 1+delta, -delta };
+  const float  p3[] = { -delta, -delta, 1+delta };
+  const IdentifierType  id0 = meshSource->AddPoint( p0 );
+  const IdentifierType  id1 = meshSource->AddPoint( p1 );
+  const IdentifierType  id2 = meshSource->AddPoint( p2 );
+  const IdentifierType  id3 = meshSource->AddPoint( p3 );
+  meshSource->AddTetrahedron( id0, id1, id2, id3 );
+
+  Mesh::Pointer mesh = meshSource->GetOutput();
+
+  BOOST_TEST_CHECKPOINT("Mesh created");
+
+  kvl::AtlasMeshVisitCounterCPUWrapper visitCounter;
+
+  visitCounter.SetRegions( image->GetLargestPossibleRegion() );
+  visitCounter.VisitCount( mesh );
+
+  BOOST_TEST_CHECKPOINT("VisitCounter Complete");
+
+  // Check points in tetrahedron
+  const ImageType* result = visitCounter.GetImage();
+  for( int k=0; k<nz; k++ ) {
+    for( int j=0; j<ny; j++ ) {
+      for( int i=0; i<nx; i++ ) {
+	ImageType::IndexType idx;
+	idx[0] = i;
+	idx[1] = j;
+	idx[2] = k;
+
+	// Recall that the tetrahedron is constructed to be the lower one
+	// enclosing points with at most one index equal to 1
+	int expected = 0;
+	if( i+j+k <= 1 ) {
+	  expected = 1;
+	}
+
+	BOOST_TEST_INFO( "(" << i << "," << j << "," << k << ")" );
+	BOOST_CHECK_EQUAL( result->GetPixel(idx), expected );
+      }
+    }
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END();
