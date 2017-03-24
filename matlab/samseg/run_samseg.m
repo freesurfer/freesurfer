a719f7975f6d99db494ecbd7c2b519089b18081a
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2017-03-24 13:45:40 -0400

    extended to max 6 inputs

--- matlab/samseg/run_samseg.m
+++ matlab/samseg/run_samseg.m
@@ -1,4 +1,4 @@
-function run_samseg(imageFileName1,savePath,nThreadsStr,UseGPUStr,imageFileName2,imageFileName3)
+function run_samseg(imageFileName1,savePath,nThreadsStr,UseGPUStr,imageFileName2,imageFileName3,imageFileName4,imageFileName5,imageFileName6)
 % run_samseg(imageFileName1,savePath,nThreadsStr,UseGPUStr,imageFileName2,imageFileName3)
 % This function is a wrapper for running the samseg matlab
 % scripts. This wrapper can be compiled (meaning that all the
@@ -14,12 +14,18 @@ UseGPU = sscanf(UseGPUStr,'%d');
 fprintf('input file1 %s\n',imageFileName1);
 fprintf('input file2 %s\n',imageFileName2);
 fprintf('input file3 %s\n',imageFileName3);
+fprintf('input file4 %s\n',imageFileName4);
+fprintf('input file5 %s\n',imageFileName5);
+fprintf('input file6 %s\n',imageFileName6);
 fprintf('output path %s\n',savePath);
 fprintf('nThreads = %d\n',nThreads);
 fprintf('UseGPU = %d\n',UseGPU);
 
 if(strcmp(imageFileName2,'none')) imageFileName2 = ''; end
 if(strcmp(imageFileName3,'none')) imageFileName3 = ''; end
+if(strcmp(imageFileName2,'none')) imageFileName4 = ''; end
+if(strcmp(imageFileName3,'none')) imageFileName5 = ''; end
+if(strcmp(imageFileName3,'none')) imageFileName6 = ''; end
 
 imageFileName = imageFileName1;
 
@@ -54,6 +60,15 @@ if(strlen(imageFileName2)>0)
 end
 if(strlen(imageFileName3)>0) 
   imageFileNames{3} =  imageFileName3;
+end
+if(strlen(imageFileName4)>0) 
+  imageFileNames{4} =  imageFileName4;
+end
+if(strlen(imageFileName5)>0) 
+  imageFileNames{5} =  imageFileName5;
+end
+if(strlen(imageFileName6)>0) 
+  imageFileNames{6} =  imageFileName6;
 end
 downSamplingFactor = 1;  % Use 1 for no downsampling
 maxNuberOfIterationPerMultiResolutionLevel(1) = 5; % default 5
