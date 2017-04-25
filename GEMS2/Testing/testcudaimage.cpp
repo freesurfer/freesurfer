a3b9a61b268e61be6f1c8428d83b2bd047a1e49d
R Edgar <freesurfer.rge@gmail.com>
2017-04-25 10:14:09 -0400

    VisitCounter Unit Tests 04 (#57)
    * Adding class for the Image which will run on the device (as opposed to managing it from the host)
    
    * Adding tests for the indexing operators on the GPU
    Only done 1D so far
    
    * Adding tests of 2D indexing on the GPU
    
    * Refactoring the indexer tests to have a common driver
    
    * Upping sizes slightly for indexing tests
    
    * Adding test of 3D indexing

--- GEMS2/Testing/testcudaimage.cpp
+++ GEMS2/Testing/testcudaimage.cpp
@@ -8,10 +8,14 @@
 #include "dimensioncuda.hpp"
 #include "cudaimage.hpp"
 
+#include "cudaimagetests.hpp"
+
 #include "randomsupply.hpp"
 #include "testiosupport.hpp"
 
 typedef boost::mpl::list<unsigned char,char, unsigned short, short,unsigned int,int,unsigned long,long> IntegerTestTypes;
+
+typedef boost::mpl::list<unsigned char, int, float, double> PlusTestTypes;
 
 RandomSupply gPRNG;
 
@@ -52,6 +56,47 @@ void SendSetReceiveDriver() {
   }
 }
 
+template<typename T,int nDims>
+void PlusKernelDriver() {
+   BOOST_TEST_CONTEXT("Seed: " << gPRNG.Reseed()) {
+     kvl::cuda::Dimension<nDims,size_t> srcDims, resultDims;
+   
+     const T value = gPRNG.GetInteger( 120 );
+
+     srcDims[0] = 256 + gPRNG.GetInteger<size_t>( 512 );
+     for( int i=1; i<nDims; i++ ) {
+       srcDims[i] = ( 2 << (9-nDims) ) + gPRNG.GetInteger<size_t>( 512 >> nDims );
+     }
+     
+     std::vector<T> src, result;
+     src.resize(srcDims.ElementCount());
+     for( auto it=src.begin(); it!=src.end(); it++ ) {
+       *it = gPRNG.GetInteger<char>(120);
+     }
+     BOOST_TEST_CHECKPOINT("Created src : " << srcDims);
+     
+     kvl::cuda::CudaImage<T,nDims,size_t> d_src, d_dst;
+     
+     d_src.Send( src, srcDims );
+     BOOST_TEST_CHECKPOINT("Send data : " << srcDims);
+     runPlusTest( d_dst, d_src, value );
+     BOOST_TEST_CHECKPOINT("Kernel complete");
+     
+     d_dst.Recv( result, resultDims );
+     BOOST_TEST_CHECKPOINT("Recv data : " << resultDims);
+    
+     BOOST_REQUIRE_EQUAL( srcDims, resultDims );
+     BOOST_REQUIRE_EQUAL( resultDims.ElementCount(), result.size() );
+     BOOST_REQUIRE_EQUAL( src.size(), result.size() );
+     bool passed = true;
+     for( size_t idx = 0; idx < src.size(); idx++ ) {
+       passed = passed && ( result.at(idx) == (value + src.at(idx)) );
+     }
+     BOOST_REQUIRE_EQUAL( passed, true );
+   }
+}
+
+
 
 BOOST_AUTO_TEST_SUITE( CudaImage )
 
@@ -80,6 +125,25 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( SendSetReceive4d, ElementType, IntegerTestTypes )
 BOOST_AUTO_TEST_CASE_TEMPLATE( SendSetReceive5d, ElementType, IntegerTestTypes )
 {
   SendSetReceiveDriver<ElementType,5>();
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE(IndexOperators)
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(PlusKernel1D, ElementType, PlusTestTypes )
+{
+  PlusKernelDriver<ElementType,1>();
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(PlusKernel2D, ElementType, PlusTestTypes )
+{
+  PlusKernelDriver<ElementType,2>();
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(PlusKernel3D, ElementType, PlusTestTypes )
+{
+  PlusKernelDriver<ElementType,3>();
 }
 
 BOOST_AUTO_TEST_SUITE_END();
