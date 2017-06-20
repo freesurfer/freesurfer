85c83e7eb7d001e0cfd9e5e0ef17bc1ef3789089
R Edgar <freesurfer.rge@gmail.com>
2017-06-19 21:35:21 -0400

    Correct BlockSize computation for volume of tetrahedron vs bounding box

--- GEMS2/cuda/visitcountersimplecudaimpl.cu
+++ GEMS2/cuda/visitcountersimplecudaimpl.cu
@@ -251,8 +251,8 @@ namespace kvl {
 
       // Compute the average number of voxels in each tetrahedron
       const float avgTetVol = nVoxels / nTetrahedra;
-      // Compute the equivalent average cube
-      const float avgCubeSize = powf( avgTetVol, 0.3333333f );
+      // Compute the equivalent average cube (factor of 6 to get volume of bounding box)
+      const float avgCubeSize = powf( 6 * avgTetVol, 0.3333333f );
 
       // Check to see if we need to increase blockSize
       if( avgCubeSize > 32 ) {
