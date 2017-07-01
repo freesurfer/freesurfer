74bb8e8e04ec1807a8716a788b96f7206fd6fcd4
Richard Edgar <freesurfer.rge@gmail.com>
2017-07-01 19:56:33 -0400

    Starting to make CUDA type for a tetrahedral mesh

---
+++ GEMS2/Testing/testcudatetrahedralmesh.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+
+#include "cudatetrahedralmesh.hpp"
+
+#include "testiosupport.hpp"
+#include "testfileloader.hpp"
+
+// ========================
+
+BOOST_AUTO_TEST_SUITE( CudaTetrahedralMesh )
+
+BOOST_FIXTURE_TEST_SUITE( ActualImage, TestFileLoader )
+
+BOOST_AUTO_TEST_CASE( SendToGPU )
+{
+  kvl::cuda::CudaTetrahedralMesh<double,unsigned long> ctm;
+
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE_END();
---
+++ GEMS2/cuda/cudatetrahedralmesh.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "kvlAtlasMesh.h"
+
+#include "cudaimage.hpp"
+
+namespace kvl {
+  namespace cuda {
+    template<typename CoordinateType, typename MeshIndexType>
+    class CudaTetrahedralMesh {
+    public:
+
+    };
+  }
+}
