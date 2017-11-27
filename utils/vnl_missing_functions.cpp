fbc4e43400bc8d530e9f45d424c2f3b1b3664d65
Bevin R Brett <bevin_brett@hotmail.com>
2017-11-26 19:16:45 -0800

    Supply some missing abstract functions

---
+++ utils/vnl_missing_functions.cpp
@@ -0,0 +1,13 @@
+#include <vnl/vnl_nonlinear_minimizer.h>
+
+  //: Return the name of the class.
+  //  Used by polymorphic IO
+vcl_string vnl_nonlinear_minimizer::is_a() const
+{
+}
+
+  //: Return true if the name of the class matches the argument.
+  //  Used by polymorphic IO
+ bool vnl_nonlinear_minimizer::is_class(vcl_string const& s) const
+ {
+ }
