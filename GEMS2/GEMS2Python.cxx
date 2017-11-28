bd559d33be054d1f272c3fc86ad1668812dfa450
Yujan Shrestha <yshrestha@innolitics.com>
2017-11-28 09:49:56 -0700

    Create kvlImage class to simplify memory management of image buffers. These instances will be owned by Python and the destructor should be called when it goes out of scope in Python.
    References #20

--- GEMS2/GEMS2Python.cxx
+++ GEMS2/GEMS2Python.cxx
@@ -4,29 +4,36 @@
 
 namespace py = pybind11;
 
+typedef itk::Image< float, 3 >  ImageType;
+typedef kvl::CroppedImageReader::TransformType  TransformType;
 
-void kvlReadImage( const std::string imageFileName ) {
-    // Read the image
-    kvl::CroppedImageReader::Pointer reader = kvl::CroppedImageReader::New();
-    reader->Read( imageFileName.c_str() );
+class KvlImage {
+    ImageType *imageHandle;
+    TransformType::Pointer transform;
 
-    // Convert the image to float
-    typedef itk::Image< float, 3 >  ImageType;
-    typedef itk::CastImageFilter< kvl::CroppedImageReader::ImageType, ImageType >  CasterType;
-    CasterType::Pointer  caster = CasterType::New();
-    caster->SetInput( reader->GetImage() );
-    caster->Update();
+    public:
+        KvlImage(const std::string &imageFileName) {
+            // Read the image
+            kvl::CroppedImageReader::Pointer reader = kvl::CroppedImageReader::New();
+            reader->Read( imageFileName.c_str() );
 
+            // Convert the image to float
+            typedef itk::CastImageFilter< kvl::CroppedImageReader::ImageType, ImageType > CasterType;
+            CasterType::Pointer  caster = CasterType::New();
+            caster->SetInput( reader->GetImage() );
+            caster->Update();
 
-    // Store the image and transform in persistent memory
-    const auto imageHandle = caster->GetOutput();
-    typedef kvl::CroppedImageReader::TransformType  TransformType;
-    TransformType::Pointer  transform = TransformType::New();
-    reader->GetWorldToImageTransform()->GetInverse( transform );
-    std::cout << "Read image: " << imageFileName << std::endl;
-}
+            // Store the image and transform in persistent memory
+            imageHandle = caster->GetOutput();
+            transform = TransformType::New();
+            reader->GetWorldToImageTransform()->GetInverse( transform );
+            std::cout << "Read image: " << imageFileName << std::endl;
+        }
+
+};
 
 
 PYBIND11_MODULE(GEMS2Python, m) {
-    m.def("kvlReadImage", &kvlReadImage);
+    py::class_<KvlImage>(m, "KvlImage")
+    .def(py::init<const std::string &>());
 }
