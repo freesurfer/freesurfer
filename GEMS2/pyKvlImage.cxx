25faf7519b8f9bba3d58f81f66f285920e7a1b4e
Willy Mills <orwonthe@gmail.com>
2017-11-29 08:11:37 -0700

    Move translation of transform to numpy into function

--- GEMS2/pyKvlImage.cxx
+++ GEMS2/pyKvlImage.cxx
@@ -21,24 +21,7 @@ KvlImage::KvlImage(const std::string &imageFileName) {
 }
 
 py::array_t<double> KvlImage::getTransformMatrix() {
-    double data[16];
-    auto parameters = transform->GetParameters();
-    for ( unsigned int row = 0; row < 3; row++ )
-    {
-        for ( unsigned int col = 0; col < 3; col++ )
-        {
-            data[ col * 4 + row ] = parameters[ row * 3 + col ];
-        }
-        data[ 12 + row ] = parameters[ 9 + row ];
-    }
-    for ( unsigned int col = 0; col < 3; col++ )
-    {
-        data[ col * 4 + 3 ] = 0.0f;
-    }
-    data[ 15 ] = 1.0f;
-    auto result = py::array_t<double>(16, data);
-    result.resize({4, 4});
-    return result;
+    return transformToNumpy(transform);
 }
 
 
--- GEMS2/pyKvlTransform.h
+++ GEMS2/pyKvlTransform.h
@@ -1,9 +1,15 @@
 #ifndef GEMS_PYKVLTRANSFORM_H
 #define GEMS_PYKVLTRANSFORM_H
 
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+namespace py = pybind11;
+
 #include "kvlCroppedImageReader.h"
 
 typedef kvl::CroppedImageReader::TransformType  TransformType;
 typedef TransformType::Pointer TransformPointer;
+
+py::array_t<double> transformToNumpy(TransformPointer transform);
 
 #endif //GEMS_PYKVLTRANSFORM_H
