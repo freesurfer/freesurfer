aed4b3a05d3da76423587ea43f3357f0ec2919b7
Yujan Shrestha <yshrestha@innolitics.com>
2017-11-29 18:00:06 -0700

    Move Numpy creation code to its own file.
    Refs #20.

--- GEMS2/pyKvlTransform.h
+++ GEMS2/pyKvlTransform.h
@@ -7,9 +7,10 @@ namespace py = pybind11;
 
 #include "kvlCroppedImageReader.h"
 
-typedef kvl::CroppedImageReader::TransformType  TransformType;
+typedef kvl::CroppedImageReader::TransformType TransformType;
 typedef TransformType::Pointer TransformPointer;
 
 py::array_t<double> transformToNumpy(TransformPointer transform);
+
 
 #endif //GEMS_PYKVLTRANSFORM_H
