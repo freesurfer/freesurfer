63c5b91e76fb6bf4ce47641bb6c4e314d799644a
Willy Mills <orwonthe@gmail.com>
2017-11-30 17:21:56 -0700

    Add mesh get alphas and mesh collection construct

--- GEMS2/pyKvlMesh.cxx
+++ GEMS2/pyKvlMesh.cxx
@@ -19,8 +19,40 @@ py::array_t<double> KvlMesh::GetPointSet() const {
     return PointSetToNumpy(mesh->GetPoints());
 }
 
+py::array_t<double> KvlMesh::GetAlphas() const {
+    return AlphasToNumpy(mesh->GetPointData());
+}
+
+void KvlMesh::SetPointSet(py::array_t<double>)
+{
+
+}
+void KvlMesh::SetAlphas(py::array_t<double>)
+{
+
+}
+
 KvlMeshCollection::KvlMeshCollection() {
     meshCollection = kvl::AtlasMeshCollection::New();
+}
+
+void KvlMeshCollection::Construct(const SHAPE_3D &meshSize, const SHAPE_3D &domainSize,
+               double initialStiffness,
+               unsigned int numberOfClasses, unsigned int numberOfMeshes) {
+    if(meshSize.size() != 3) {
+        itkExceptionMacro("meshSize must have 3 values not " << meshSize.size());
+    }
+    if(domainSize.size() != 3) {
+        itkExceptionMacro("domainSize must have 3 values not " << domainSize.size());
+    }
+    unsigned int meshShape[3];
+    unsigned int domainShape[3];
+    for(int index=0; index < 3; index++) {
+        meshShape[index] = meshSize[index];
+        domainShape[index] = domainSize[index];
+    }
+    meshCollection->Construct( meshShape, domainShape, initialStiffness,
+                               numberOfClasses, numberOfMeshes );
 }
 
 void KvlMeshCollection::Read(const std::string &meshCollectionFileName) {
@@ -84,3 +116,17 @@ py::array_t<double> PointSetToNumpy(PointSetConstPointer points) {
     }
     return createNumpyArrayCStyle({numberOfNodes, XYZ_DIMENSIONS}, data);
 }
+
+py::array_t<double> AlphasToNumpy(PointDataConstPointer alphas) {
+    const unsigned int numberOfNodes = alphas->Size();
+    const unsigned int  numberOfLabels = alphas->Begin().Value().m_Alphas.Size();
+    auto *data = new double[numberOfNodes * numberOfLabels];
+    auto dataIterator = data;
+    for (auto alphasIterator = alphas->Begin(); alphasIterator != alphas->End(); ++alphasIterator) {
+        for (int label = 0; label < numberOfLabels; label++) {
+            *dataIterator++ = alphasIterator.Value().m_Alphas[label];
+        }
+    }
+    return createNumpyArrayCStyle({numberOfNodes, numberOfLabels}, data);
+}
+
--- GEMS2/pyKvlMesh.h
+++ GEMS2/pyKvlMesh.h
@@ -3,14 +3,19 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+#include <vector>
 #include "itkObject.h"
 #include "kvlAtlasMeshCollection.h"
+
 
 namespace py = pybind11;
 
 typedef kvl::AtlasMeshCollection::Pointer MeshCollectionPointer;
 typedef kvl::AtlasMesh::ConstPointer MeshPointer;
 typedef kvl::AtlasMesh::PointsContainer::ConstPointer PointSetConstPointer;
+typedef kvl::AtlasMesh::PointDataContainer::ConstPointer PointDataConstPointer;
+typedef std::vector<unsigned int> SHAPE_3D;
 
 class KvlMesh {
     MeshPointer mesh;
@@ -19,6 +24,9 @@ public:
     KvlMesh();
     int PointCount() const;
     py::array_t<double> GetPointSet() const;
+    void SetPointSet(py::array_t<double>);
+    py::array_t<double> GetAlphas() const;
+    void SetAlphas(py::array_t<double>);
 
     // C++ Only
     KvlMesh(MeshPointer& aMesh);
@@ -28,7 +36,6 @@ class KvlMeshCollection {
     MeshCollectionPointer  meshCollection;
 public:
     // Python accessible
-    KvlMeshCollection();
     void Read(const std::string &meshCollectionFileName);
     void Write(const std::string &meshCollectionFileName);
     double GetK() const;
@@ -36,8 +43,12 @@ public:
     unsigned int MeshCount() const;
     KvlMesh* GetMesh(int meshNumber);
     KvlMesh* GetReferenceMesh();
+    void Construct(const SHAPE_3D &meshSize, const SHAPE_3D &domainSize,
+                   double initialStiffness,
+                   unsigned int numberOfClasses, unsigned int numberOfMeshes);
 
     // C++ use only
+    KvlMeshCollection();
     const char *GetNameOfClass() const {
         return "KvlMeshCollection";
     }
@@ -47,5 +58,6 @@ public:
 };
 
 py::array_t<double> PointSetToNumpy(PointSetConstPointer points);
+py::array_t<double> AlphasToNumpy(PointDataConstPointer alphas);
 
 #endif //GEMS_PYKVLMESH_H_H
