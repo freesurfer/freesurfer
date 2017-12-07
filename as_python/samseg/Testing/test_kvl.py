Fix down sampling factor calculation

as_python/samseg/Testing/test_kvl.py
@@ -1,6 +1,7 @@
 import GEMS2Python
 import numpy as np
 from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_almost_equal
 
 from as_python.samseg.kvl import transform_product, voxel_spacing_of_transform, calculate_down_sampling_factors
 
@@ -31,25 +32,26 @@ def test_transform_product():
 
 def test_voxel_spacing_of_transform():
     test_matrix = GEMS2Python.KvlTransform(np.array([
-        [10, 10, 10, 999],
-        [2, 2, 2, 999],
-        [3, 3, 3, 999],
+        [200, 300, 500, 999],
+        [20, 30, 50, 999],
+        [2, 3, 5, 999],
         [0, 0, 0, 1],
     ], dtype=np.double, order='F'))
-    actual = voxel_spacing_of_transform(test_matrix)
-    actual_squared = actual * actual
-    assert 338.999 < actual_squared
-    assert 339.001 > actual_squared
+    actual_spacing = voxel_spacing_of_transform(test_matrix)
+    assert 3 == len(actual_spacing)
+    actual_spacing_squared = [v*v for v in actual_spacing]
+    expected_spacing_squared = [(1*1 + 10*10 + 100*100)*v for v in [2*2, 3*3, 5*5]]
+    assert_array_almost_equal(expected_spacing_squared, actual_spacing_squared)
 
 
 def test_calculate_down_sampling_factors():
     test_matrix = GEMS2Python.KvlTransform(np.array([
         [10, 0, 0, 0],
-        [0, 10, 0, 0],
-        [0, 0, 10, 0],
+        [0, 20, 0, 0],
+        [0, 0, 30, 0],
         [0, 0, 0, 1],
     ], dtype=np.double, order='F'))
     actual_small = calculate_down_sampling_factors(test_matrix, 10)
-    assert [1, 1, 1] == actual_small
+    assert_array_equal([1, 1, 1], actual_small)
     actual_big = calculate_down_sampling_factors(test_matrix, 135)
-    assert [8, 8, 8] == actual_big
+    assert_array_equal([14, 7, 4], actual_big)

as_python/samseg/kvl.py
@@ -1,7 +1,10 @@
+import logging
 import math
 
 import GEMS2Python
 import numpy as np
+
+logger = logging.getLogger(__name__)
 
 
 def transform_product(a, b):
@@ -16,8 +19,8 @@ def voxel_spacing_of_transform(t):
     m = t.as_numpy_array
     mm = m[0:3, 0:3]
     mm2 = mm * mm
-    ss = mm2.sum()
-    return math.sqrt(ss)
+    ss = mm2.sum(axis=0)
+    return np.sqrt(ss)
 
 
 def calculate_down_sampling_factors(transform, target_voxel_spacing):
@@ -25,9 +28,10 @@ def calculate_down_sampling_factors(transform, target_voxel_spacing):
     # voxelSpacing = sum( imageToWorldTransformMatrix( 1 : 3, 1 : 3 ).^2 ).^( 1/2 );
     # downSamplingFactors = max( round( targetDownsampledVoxelSpacing ./ voxelSpacing ), [ 1 1 1 ] )
     voxel_spacing = voxel_spacing_of_transform(transform)
-    tentative_spacing = round(target_voxel_spacing / voxel_spacing)
-    limited_spacing = max(1, tentative_spacing)
-    return [limited_spacing, limited_spacing, limited_spacing]
+    tentative_spacing = np.rint(target_voxel_spacing / voxel_spacing)
+    limited_spacing = np.maximum([1, 1, 1], tentative_spacing)
+    print(limited_spacing)
+    return [int(factor) for factor in limited_spacing]
 
 
 def create_transform_from_2d_list(list_2d):