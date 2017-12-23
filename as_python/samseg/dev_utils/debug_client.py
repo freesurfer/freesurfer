ace2e8b1c068d05424962e54f3a008120d0bc8c2
Yujan Shrestha <yshrestha@innolitics.com>
2017-12-22 20:23:27 -0700

    Add MATLAB to Python debugging utility script.
    Refs #24

---
+++ as_python/samseg/dev_utils/debug_client.py
@@ -0,0 +1,28 @@
+'''
+USAGE: First start up the debug server in MATLAB. Please see debug_server.m for instructions
+Then call `request_var` and pass in the name of the variable you want to auto-magically
+transfer from MATLAB to Python. This is intended to be called with both Python and MATLAB
+at a breakpoint to easily compare the values of variables for equality.
+'''
+import scipy.io
+import os
+import time
+
+MATLAB_DUMP_DIR = os.getenv('MATLAB_DUMP_DIR')
+
+
+def request_var(varname):
+    mat_filename = os.path.join(MATLAB_DUMP_DIR, varname + '.mat')
+    if os.path.exists(mat_filename):
+        os.remove(mat_filename)
+    request_file = os.path.join(MATLAB_DUMP_DIR, varname + '.request')
+    open(request_file, 'w')
+    for i in range(20):
+        if not os.path.exists(request_file):
+            var = scipy.io.loadmat(mat_filename, struct_as_record=False, squeeze_me=True)[varname]
+            os.remove(mat_filename)
+            return var
+        else:
+            time.sleep(0.1)
+    else:
+        raise Exception("Timed out waiting for matlab. Are you at a breakpoint and have the debug server running?")
