151ba8d98374b55ec0f03fd8f66809ded5a63b98
Nick Schmansky <nicks@terrier.nmr.mgh.harvard.edu>
2018-01-29 20:56:05 -0500

    bf: still trying to fix the issue with path to local netcdf.h

--- include/minc_volume_io.h
+++ include/minc_volume_io.h
@@ -34,7 +34,7 @@
 // instead of using the libminc included with an external MNI package build,
 // use the local one found in minc_1_5_1
 
-#include "netcdf.h"
+#include "netcdf_3_6_0_p1/netcdf.h"
 #include "minc_multidim_arrays.h"
 
 typedef bool BOOLEAN;
