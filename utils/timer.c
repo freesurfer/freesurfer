2ed061c9b9536acbde270c3e2471e46c379ffaba
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2018-02-01 13:56:02 -0500

    removed old todo comment

--- utils/timer.c
+++ utils/timer.c
@@ -41,7 +41,7 @@ int mach_gettime(clockid_t clk_id, struct timespec *tp)
   int ret;
   clock_serv_t cclock;
   mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), clk_id, &cclock);  // todo: calendar time should be clk_id I think
+  host_get_clock_service(mach_host_self(), clk_id, &cclock);
   ret = clock_get_time(cclock, &mts);
   mach_port_deallocate(mach_task_self(), cclock);
   tp->tv_sec = mts.tv_sec;
