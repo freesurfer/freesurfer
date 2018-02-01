60635eebf0d37150bc2f5ef49ebf2ae332679801
Bevin R Brett <bevin_brett@hotmail.com>
2018-02-02 10:48:06 +1300

    Forgot to add this file

---
+++ include/faster_variants.h
@@ -0,0 +1,33 @@
+/**
+ * @file  faster_variants.h
+ * @brief define the macros that conditionalize faster variants of some algorithms
+ *        before those variants completely replace the original variant
+ *
+ *        keep both variants when the faster variant is 
+ *           1) not yet fully tested, or
+ *           2) substantially more complex than the original
+ *                  because comparing the two results can test the faster one for correctness
+ */
+/*
+ * Original Author: Bevin Brett
+ * CVS Revision Info:
+ *    $Author: brettb $
+ *    $Date: 2018/1 $
+ *    $Revision: 1.0 $
+ *
+ * Copyright © 2018 The General Hospital Corporation (Boston, MA) "MGH"
+ *
+ * Terms and conditions for use, reproduction, distribution and contribution
+ * are found in the 'FreeSurfer Software License Agreement' contained
+ * in the file 'LICENSE' found in the FreeSurfer distribution, and here:
+ *
+ * https://surfer.nmr.mgh.harvard.edu/fswiki/FreeSurferSoftwareLicense
+ *
+ * Reporting: freesurfer@nmr.mgh.harvard.edu
+ *
+ */
+
+#pragma once
+
+#define FASTER_gcamSmoothnessEnergy
+#define FASTER_MRI_EM_REGISTER
