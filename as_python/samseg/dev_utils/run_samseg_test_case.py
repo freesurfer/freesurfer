94729ac39d06fc54fdd46b6e9bb17d63b34eff6a
Willy Mills <orwonthe@gmail.com>
2018-02-22 11:17:37 -0700

    Add atlas_only command argument

--- as_python/samseg/dev_utils/run_samseg_test_case.py
+++ as_python/samseg/dev_utils/run_samseg_test_case.py
@@ -4,6 +4,7 @@ from samseg.dev_utils.debug_client import create_checkpoint_manager, run_test_ca
 from samseg.run_samseg_ported import run_samseg
 
 USE_CHECKPOINT_MANAGER = False
+THREAD_COUNT = 6
 
 def run_single_full_case(case_name, case_file_folder, savePath):
     image_file_path = os.path.join(case_file_folder, 'orig.mgz')
@@ -13,7 +14,9 @@ def run_single_full_case(case_name, case_file_folder, savePath):
         run_samseg(
             imageFileNames=[image_file_path],
             savePath=savePath,
-            checkpoint_manager=checkpoint_manager
+            checkpoint_manager=checkpoint_manager,
+            numberOfThreads=THREAD_COUNT,
+            atlas_only=False,
         )
     else:
         print("{0} is not a file".format(image_file_path))
