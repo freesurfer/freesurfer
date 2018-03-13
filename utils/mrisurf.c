8c89ec056e204af49c6a6e925f885c08e02df81d
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2018-03-13 11:18:55 -0400

    bf: use face normal struct instead

--- utils/mrisurf.c
+++ utils/mrisurf.c
@@ -9659,8 +9659,8 @@ static double MRIScomputeSSE_CUDA(MRI_SURFACE *mris, MRI_CUDA_SURFACE *mrisc, IN
       if (face->ripflag) {
         continue;
       }
-
-      delta = (double)(area_scale * face->area - face->orig_area);
+      FaceNormCacheEntry const * fnorm = getFaceNorm(mris, fno);
+      delta = (double)(area_scale * face->area - fnorm->orig_area);
 #if ONLY_NEG_AREA_TERM
       if (face->area < 0.0f) {
         sse_neg_area += delta * delta;
