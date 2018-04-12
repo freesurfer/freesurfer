46b4ba7f0a73d50ea597ca1bc3cbb985aaae49f3
Ruopeng Wang <rpwang@nmr.mgh.harvard.edu>
2018-04-12 17:11:21 -0400

    Fixed bug that 3D slice frames were not shown by default

--- freeview/MainWindow.cpp
+++ freeview/MainWindow.cpp
@@ -1180,7 +1180,7 @@ bool MainWindow::DoParseCommand(MyCmdLineParser* parser, bool bAutoQuit)
     ((RenderView3D*)m_views[3])->HideSlices();
   }
 
-  if (parser->Found("hide-3d-frames"), &sa)
+  if (parser->Found("hide-3d-frames", &sa) )
   {
     ((RenderView3D*)m_views[3])->SetShowSliceFrames(false);
   }
