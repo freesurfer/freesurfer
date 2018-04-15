60f9062f14542cf231d228ab797f265cf770b1d7
Willy Mills <orwonthe@gmail.com>
2018-04-15 09:56:53 -0600

    Dynamically locate gems2 library file

--- GEMS2/setup.py
+++ GEMS2/setup.py
@@ -3,19 +3,29 @@
 
 """The setup script."""
 import os
+import glob
 from setuptools import setup, find_packages, Distribution
+
 
 class BinaryDistribution(Distribution):
     def is_pure(self):
         return False
+
     def has_ext_modules(self):
         return True
 
+
 def create_init_file():
-    file_path = os.path.join(os.path.dirname(__file__), 'bin','__init__.py')
+    file_path = os.path.join(os.path.dirname(__file__), 'bin', '__init__.py')
     if not os.path.exists(file_path):
         with open(file_path, 'w'):
             pass
+
+
+def find_gems2_libraries():
+    search_pattern = os.path.join(os.path.dirname(__file__), 'bin', 'GEMS2Python.*.so')
+    return [os.path.basename(filename) for filename in glob.glob(search_pattern)]
+
 
 create_init_file()
 
@@ -24,10 +34,10 @@ setup(
     name='GEMS2Python',
     version='0.1.0',
     description="Python wrapper for GEMS2 C++ tooling",
-    package_dir={'gems2python':'bin'},
+    package_dir={'gems2python': 'bin'},
     packages=['gems2python'],
     package_data={
-        'gems2python':['GEMS2Python.cpython-35m-x86_64-linux-gnu.so'],
+        'gems2python': find_gems2_libraries(),
     },
     zip_safe=False,
     keywords='gems2python samseg gems2 freesurfer',
