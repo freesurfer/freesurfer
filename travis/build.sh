531c5814e9fdec1d5858bfdb1237246b38ed77fa
Andrew Hoopes <ahoopes@users.noreply.github.com>
2018-05-17 17:43:18 -0400

    output to build.log

--- travis/build.sh
+++ travis/build.sh
@@ -5,10 +5,10 @@ set -ex
 # script to run the travis build step
 #
 
-./setup_configure
+./setup_configure &>> build.log
 
 config_flags="--with-pkgs-dir=${PWD}/build-packages --disable-Werror --disable-GUI-build"
 [[ "$TRAVIS_OS_NAME" == "osx" ]] && config_flags="${configure_flags} F77=/usr/local/bin/gfortran-4.9 CC=/usr/local/bin/gcc-4.9 CXX=/usr/local/bin/g++-4.9"
 
-./configure ${config_flags}
-make -j4
+./configure ${config_flags} &>> build.log
+make -j4 &>> build.log
