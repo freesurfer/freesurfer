9544f1a3f2dbdc43726b002891c16e3c2194f6e0
Andrew Hoopes <ahoopes@users.noreply.github.com>
2018-05-18 01:12:52 -0400

    deal with travis and bash error handling

--- travis/build.sh
+++ travis/build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-set -ex
 
 #
 # script to run the travis build step
 #
 
-./setup_configure
+function runstep {
+  $1 >> build.log 2>&1 || { echo $1; tail -n 100 build.log; exit 1; }
+}
+
+runstep ./setup_configure
 
 config_flags="--with-pkgs-dir=${PWD}/build-packages --disable-Werror --disable-GUI-build"
 [[ "$TRAVIS_OS_NAME" == "osx" ]] && config_flags="${configure_flags} F77=/usr/local/bin/gfortran-4.9 CC=/usr/local/bin/gcc-4.9 CXX=/usr/local/bin/g++-4.9"
 
-./configure ${config_flags}
-make -j4
+runstep ./configure ${config_flags}
+runstep make -j4
