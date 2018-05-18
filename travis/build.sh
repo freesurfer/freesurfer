b43643830b6e1d230e1ab3a0a61d38cd7ab23f7c
Andrew Hoopes <ahoopes@users.noreply.github.com>
2018-05-18 01:14:28 -0400

    messed up the args

--- travis/build.sh
+++ travis/build.sh
@@ -5,10 +5,10 @@
 #
 
 function runstep {
-  $1 >> build.log 2>&1 || { echo $1; tail -n 100 build.log; exit 1; }
+  $@ >> build.log 2>&1 || { echo $@; tail -n 100 build.log; exit 1; }
 }
 
-runstep ./setup_configure
+runstep "./setup_configure"
 
 config_flags="--with-pkgs-dir=${PWD}/build-packages --disable-Werror --disable-GUI-build"
 [[ "$TRAVIS_OS_NAME" == "osx" ]] && config_flags="${configure_flags} F77=/usr/local/bin/gfortran-4.9 CC=/usr/local/bin/gcc-4.9 CXX=/usr/local/bin/g++-4.9"
