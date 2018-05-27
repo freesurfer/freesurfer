665dd156fb43022c7a5a536bc21b7ec4a76cc37a
Bevin R Brett <bevin_brett@hotmail.com>
2018-05-27 16:54:47 +1200

    Maybe the old compiler will like this more

--- include/romp_for_begin.h
+++ include/romp_for_begin.h
@@ -73,22 +73,11 @@
         ROMP_pf_static.line = romp_for_line;
 #endif
 
-#ifndef ROMP_FOR_LEVEL
-#define ROMP_FOR_LEVEL assume_reproducible;
-#endif
-
-#ifdef HAVE_OPENMP
-#define ROMP_FOR_PRAGMA _Pragma("omp parallel for if_ROMPLEVEL(ROMP_FOR_LEVEL)")
-#else
-#define ROMP_FOR_PRAGMA
-#endif
-
 // When this is not in a macro, but is directly here instead, the compilers use this file 
 // as the source correlation of the "omp parallel for" loop body, which hinders using performance analysis tools
 //
 #ifndef ROMP_for_begin
 #define ROMP_for_begin                                                                      \
-        ROMP_FOR_PRAGMA                                                                     \
   	for (ROMP_index = 0; ROMP_index < ROMP_distributor.partialSize; ROMP_index++) {     \
             ROMP_PF_begin                                                                   \
                                                                                             \
@@ -102,3 +91,12 @@
 	        ROMP_PFLB_begin                                                             \
     // end of macro
 #endif
+
+#ifndef ROMP_FOR_LEVEL
+#define ROMP_FOR_LEVEL assume_reproducible;
+#endif
+
+#ifdef HAVE_OPENMP
+#pragma omp parallel for if_ROMPLEVEL(ROMP_FOR_LEVEL)
+#endif
+// ROMP_for_begin should be the next line after the include
