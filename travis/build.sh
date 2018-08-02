1f36ad464a73872432707e1ad1365a4796b825f6
Andrew Hoopes <athoopes1@gmail.com>
2018-08-02 17:44:32 -0400

    added glu lib

--- travis/build.sh
+++ travis/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
 set -ex
+set -o pipefail
 
 #
-# the gcc5 mac build output is riddled with innocuous 
+# the gcc5 mac output is riddled with innocuous deprecation warnings -
+# we'll have to deal with these until gcc6, but for now, we should filter the output
+# so the travis log doesn't get filled up
 #
 
 cmake . -DFS_PACKAGES_DIR="packages" -DBUILD_GUIS=OFF
