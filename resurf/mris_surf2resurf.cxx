445573197911a1a30d2c49381c60c43f90a9ff2f
Viv Siless <viviana.siless@gmail.com>
2018-08-17 16:51:12 -0400

    read pial, create mesh, save vtk

--- resurf/mris_surf2resurf.cxx
+++ resurf/mris_surf2resurf.cxx
@@ -3,6 +3,8 @@
 #include <map>
 #include "itkDefaultStaticMeshTraits.h"
 #include "itkMesh.h"
+#include "itkTriangleCell.h"
+//#include "itkMeshFileWriter.h"
 #include <set>
 #include "GetPot.h"
 #include <string>
@@ -13,6 +15,7 @@ extern "C" {
 }
 
 #include "mrisurf.h"
+#include "itkVTKPolyDataWriter.h"
 
 int main(int narg, char*  arg[])
 {
@@ -25,15 +28,19 @@ int main(int narg, char*  arg[])
   ct = CTABreadASCII(tmpstr);
   */
 
-   MRI_SURFACE *surf;
-
-  surf = MRISread(arg[1]);
-
 	try{
 
-		enum {Dimension =3};
-		typedef double                                                        PixelType;
+		constexpr unsigned int Dimension = 3;
+		typedef double CoordType;
+		typedef itk::Mesh< CoordType, Dimension > MeshType;
+		typedef MeshType::PointsContainer PointsContainer;
+		typedef MeshType::PointType PointType;
+		typedef MeshType::PointIdentifier PointIdentifier;
+	
+//		typedef itk::MeshFileWriter< MeshType > MeshWriterType;
 
+ 		typedef MeshType::CellType CellType;
+  		typedef itk::TriangleCell< CellType > TriangleType;
 
 		GetPot cl(narg, const_cast<char**>(arg));
 		if(cl.size()==1 || cl.search(2,"--help","-h"))
@@ -42,10 +49,56 @@ int main(int narg, char*  arg[])
 			std::cout<< arg[0] << " -i surface -o surface"  << std::endl;   
 			return -1;
 		}
-		int numClusters = cl.follow(0,"-c");
-		const char *segFile = cl.follow ("", "-s1");
+		const char *inSurf= cl.follow ("", "-i");
+		const char *outSurf = cl.follow ("", "-o");
 
-		segFile = cl.follow ("", "-s2");
+		
+		MRI_SURFACE *surf;
+		surf = MRISread(inSurf);
+
+  
+		MeshType::Pointer mesh = MeshType::New();
+
+
+		PointsContainer::Pointer points = PointsContainer::New();
+		points->Reserve( surf->nvertices );
+
+		PointIdentifier k = 0;
+
+		for( int i = 0; i < surf->nvertices; i++ )
+		{
+			PointType p;
+			p[0]=surf->vertices[i].x;
+			p[1]=surf->vertices[i].y;
+			p[2]=surf->vertices[i].z;
+			points->SetElement( i, p );
+		}
+
+		mesh->SetPoints( points );
+  CellType::CellAutoPointer cellpointer;
+
+		for( int i = 0; i < surf->nfaces; i++ )
+		{
+			cellpointer.TakeOwnership( new TriangleType );
+			cellpointer->SetPointId( 0, surf->faces[i].v[0]);
+			cellpointer->SetPointId( 1, surf->faces[i].v[1]);
+			cellpointer->SetPointId( 2, surf->faces[i].v[2]);
+			mesh->SetCell( i, cellpointer );
+		}
+
+		typedef itk::VTKPolyDataWriter<MeshType> WriterType;
+		WriterType::Pointer writer = WriterType::New();
+		writer->SetInput(mesh);
+		writer->SetFileName(outSurf);
+		writer->Update();
+
+
+		/*MeshWriterType::Pointer writer = MeshWriterType::New();
+		writer->SetFileName( outSurf);
+		writer->SetInput( mesh );
+		writer->Update();
+	*/
+
 	}catch(...)
 	{
 		std::cout << "Error --> ";
@@ -55,6 +108,7 @@ int main(int narg, char*  arg[])
 		}
 		std::cout << std::endl;
 
+		return EXIT_FAILURE;
 	}
-	return 0;
+	return EXIT_SUCCESS;
 }
