f06cfe073289ddc719c35f1b437675fa1395d9d5
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2018-08-18 09:04:01 -0400

    enabled default itk modules

--- packages/source/build_itk.sh
+++ packages/source/build_itk.sh
@@ -12,13 +12,12 @@ mkdir build
 cd build
 
 cmake ../ITK-5.0a01 \
-  -G "Unix Makefiles" \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
-  -DITK_BUILD_DEFAULT_MODULES=OFF \
   -DITKGroup_Core=ON \
   -DITKGroup_Filtering=ON \
   -DITKGroup_Segmentation=ON \
   -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_TESTING=OFF \
   -DCMAKE_CXX_FLAGS="-msse2 -mfpmath=sse" \
   -DCMAKE_C_FLAGS="-msse2 -mfpmath=sse" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10
