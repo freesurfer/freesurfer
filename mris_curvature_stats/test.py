47b3a6dee999e372a8eef8cde26f6cd26d7acab9
buildqa <rd521@nmr.mgh.harvard.edu>
2018-08-30 17:09:11 -0400

    differentiate reference output for linux and mac as slight numerical differences from math/integral results

--- mris_curvature_stats/test.py
+++ mris_curvature_stats/test.py
@@ -2,13 +2,20 @@
 import sys, os.path as op
 sys.path.append(op.join(op.dirname(sys.argv[0]), '../python'))
 import freesurfer.test as fst
+import platform
 
+platform = platform.system()
 rt = fst.RegressionTest()
 
 rt.run('mris_curvature_stats -h 10 -G cvs_avg35 rh rh.curv 2>&1 | tee -a rh.curv.out && \
 mris_curvature_stats -h 10 -G cvs_avg35 lh lh.curv 2>&1 | tee -a lh.curv.out')
 
-rt.diff('rh.curv.out', 'rh.curv.out.ref')
-rt.diff('lh.curv.out', 'lh.curv.out.ref')
+if (platform == "Linux"):
+   rt.diff('rh.curv.out', 'rh.curv.linux.out.ref')
+   rt.diff('lh.curv.out', 'lh.curv.linux.out.ref')
+elif (platform == "Darwin"):
+   rt.diff('rh.curv.out', 'rh.curv.darwin.out.ref')
+   rt.diff('lh.curv.out', 'lh.curv.darwin.out.ref')
+
 rt.cleanup()
 
