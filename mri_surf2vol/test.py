f0a42b24247dd2c9c269a8c8a8c4556acc6c0b17
buildqa <s1152yb68@contbay.com>
2018-08-30 18:43:41 -0400

    differentiate reference output for linux and mac

--- mri_surf2vol/test.py
+++ mri_surf2vol/test.py
@@ -2,10 +2,16 @@
 import sys, os, os.path as op
 sys.path.append(op.join(op.dirname(sys.argv[0]), '../python'))
 import freesurfer.test as fst
+import platform
 
+platform = platform.system()
 rt = fst.RegressionTest()
 
 rt.run('.//mri_surf2vol --o out.nii.gz --subject cvs_avg35 --so lh.white lh.thickness --so rh.white rh.thickness')
-rt.diff('out.nii.gz', 'out_ref.nii.gz')
 
-# rt.cleanup()
+if (platform == "Linux"):
+   rt.diff('out.nii.gz', 'out_linux_ref.nii.gz')
+elif (platform == "Darwin"):
+   rt.diff('out.nii.gz', 'out_darwin_ref.nii.gz')
+
+rt.cleanup()
