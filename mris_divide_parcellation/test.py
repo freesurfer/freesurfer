fe4ae0039b508cf1f8cacb049999f4258eff92fe
buildqa <s1152yb68@contbay.com>
2018-08-30 17:56:27 -0400

    filter output difference between mac and linux

--- mris_divide_parcellation/test.py
+++ mris_divide_parcellation/test.py
@@ -5,7 +5,7 @@ import freesurfer.test as fst
 
 rt = fst.RegressionTest()
 
-rt.run('mris_divide_parcellation bert lh aparc.annot splittable.txt split-sfg+pc 2>&1 | tee -a divide_parcel.out') 
+rt.run('mris_divide_parcellation bert lh aparc.annot splittable.txt split-sfg+pc 2>&1 | tee -a divide_parcel.out.raw && cat divide_parcel.out.raw | grep -v "supposed to be reproducible" > divide_parcel.out') 
 
 rt.diff('divide_parcel.out', 'divide_parcel.out.ref')
 
