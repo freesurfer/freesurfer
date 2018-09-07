47f1da1f6324f0cc66f616dd9c0da2a758e7856f
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2018-09-07 15:17:45 -0400

    ported mergeAlphas updates

--- python/freesurfer/gems/merge_alphas.py
+++ python/freesurfer/gems/merge_alphas.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 import colorsys
 
+from freesurfer import errorExit
+
 
 logger = logging.getLogger(__name__)
 
@@ -29,98 +31,37 @@ def kvlMergeAlphas(alphas, names, mergeOptions, FreeSurferLabels=None, colors=No
     # Make sure we're dealing with a valid mesh
     meshValidityTest(alphas, 'alphas')
 
+    numberOfClasses = len(mergeOptions)
 
-    # ath ---------------------------------------------------------------
-
-    # to-port: numberOfStructures = size(alphas, 2);  # Number of freesurfer labels being segmented/outputed
-    # to-port: translationTable = zeros( numberOfClasses, numberOfStructures );
+    translationTable = np.zeros((numberOfClasses, len(names)))
     mergedNames = [];
     for classNumber, mergeOption in enumerate(mergeOptions):
         mergedNames.append(mergeOption.mergedName.strip())
         for searchString in mergeOption.searchStrings:
-            # to-port: some search stuff
-            translationTable[classNumber, structureNumbers] = 1
-    # to-port: error check
-    # to-port: translationTable = translationTable ./ sum( translationTable );
-    mergedAlphas = alphas * translationTable.T
-
-    # ath ---------------------------------------------------------------
-
-
-
-    # Start by copying everything
-    mergedAlphas = np.copy(alphas)
-    mergedNames = [str(name).strip() for name in names]
-    mergedFreeSurferLabels = [label for label in FreeSurferLabels]
-    mergedColors = [color for color in colors]
-
-    # Also keep track of what class ends up being merged into what
-    mergingTable = [set([index]) for index in range(mergedAlphas.shape[1])]
-
-    zeroes_for_appending = np.zeros((alpha_count, 1), dtype=np.double)
-    for mergeOption in mergeOptions:
-        logger.debug('starting shape = %s', str(mergedAlphas.shape))
-        mergedName = mergeOption.mergedName.strip()
-        searchStrings = mergeOption.searchStrings
-        if isinstance(searchStrings, str):
-            searchStrings = [searchStrings]
-        searchStrings = {str(value).strip() for value in searchStrings}
-        def is_target_string(candidate):
-            for pattern in searchStrings:
-                if pattern in candidate:
-                    return True
-            return False
-            logger.debug('searching for %s', searchStrings)
-        # Retrieve the class number corresponding to the merged label. If it doesn't exist yet,
-        # create a new class
-        matching_name_indices = [index for index, name in enumerate(mergedNames) if name == mergedName]
-        if len(matching_name_indices) == 1:
-            classNumberOfMerged = matching_name_indices[0]
-            logger.debug('keeping class number %d', classNumberOfMerged)
-        else:
-            classNumberOfMerged = mergedAlphas.shape[1]
-            logger.debug('adding class number %d', classNumberOfMerged)
-            mergedAlphas = np.append(mergedAlphas, zeroes_for_appending, axis=1)
-            mergedNames.append(mergedName)
-            mergedFreeSurferLabels.append(None)
-            mergedColors.append(None)
-            mergingTable.append(set())
-        # Get class numbers of all structures to be merged into classNumberOfMerged
-        mergingClassNumbers = [index for index, name in enumerate(mergedNames) if is_target_string(name)]
-        logger.debug('merging from %s', str(mergingClassNumbers))
-        # Now merge (i.e., add the probabilities) of those class numbers to that of classNumberOfMerged
-        alphaValuesToMerge = np.zeros((alpha_count, ), dtype=np.double)
-        for mergeIndex in mergingClassNumbers:
-            alphaValuesToMerge += mergedAlphas[:, mergeIndex]
-            mergingTable[classNumberOfMerged] |= mergingTable[mergeIndex]
-        mergedAlphas[:, classNumberOfMerged] = alphaValuesToMerge
-        # Remove the corresponding rows/columns in the resulting variables
-        disappearingClassNumbers = [number for number in mergingClassNumbers if number != classNumberOfMerged]
-        disappearingClassNumbers.sort()
-        disappearingClassNumbers.reverse()
-        logger.debug('removing these %s', str(disappearingClassNumbers))
-        for disappearingClassNumber in disappearingClassNumbers:
-            mergedAlphas = np.delete(mergedAlphas, disappearingClassNumber, 1)
-        mergedFreeSurferLabels = [label for index, label in enumerate(mergedFreeSurferLabels) if not index in disappearingClassNumbers]
-        mergedNames = [name for index, name in enumerate(mergedNames) if not index in disappearingClassNumbers]
-        mergedColors = [color for index, color in enumerate(mergedColors) if not index in disappearingClassNumbers]
-        mergingTable = [sources for index, sources in enumerate(mergingTable) if not index in disappearingClassNumbers]
-        logger.debug('ending shape = %s', str(mergedAlphas.shape))
-    # Make sure we still have a valid mesh
+            for structureNumber, name in enumerate(names):
+                if searchString in name: translationTable[classNumber, structureNumber] = 1.0
+    
+    if not translationTable.any():
+        errorExit('some structures are not associated with any super-structures')
+    
+    translationTable = translationTable / np.sum(translationTable, 0) 
+    
+    mergedAlphas = np.dot(alphas, translationTable.T)
     meshValidityTest(mergedAlphas, 'mergedAlphas')
-    # Take care of NaN's we temporily put in the mergedFreeSurferLabels and mergedColors
-    newlyInventedClasses = [index for index, label in enumerate(mergedFreeSurferLabels) if label is None]
-    numberOfNewlyInvitedClasses = len(newlyInventedClasses)
-    saturation = 1.0
-    color_value = 1.0
-    for index, newIndex in enumerate(newlyInventedClasses):
-        mergedFreeSurferLabels[newIndex] = -(1 + index)
-        hue = index / numberOfNewlyInvitedClasses
-        mergedColors[newIndex]= [255 * component for component in colorsys.hsv_to_rgb(hue, saturation, color_value)] + [255]
-    # Also compute lookup table indicating for each original class number (column number in alphas) the final
-    # class number (column number) in mergedAlphas
-    translationTable = [0] * label_count
-    for mergedClassNumber, mergingTableValueSet in enumerate(mergingTable):
-        for mergingTableValue in mergingTableValueSet:
-            translationTable[mergingTableValue] = 1 + mergedClassNumber # 1 based index
+
+    mergedFreeSurferLabels = -(np.arange(numberOfClasses) + 1)
+
+    mergedColors = []
+    for n in range(numberOfClasses):
+        hue = n / numberOfClasses
+        mergedColors.append([255 * component for component in colorsys.hsv_to_rgb(hue, 1, 1)] + [255])
+
+    # Print out merge info
+    for classNumber in range(numberOfClasses):
+      print(mergedNames[classNumber])
+      for structureNumber in range(len(names)):
+        percentage = int(translationTable[classNumber, structureNumber] * 100)
+        if percentage > 0:
+            print('    %s (%d%%)' % (names[structureNumber].ljust(len(max(names, key=len))), percentage))
+
     return mergedAlphas, mergedNames, mergedFreeSurferLabels, mergedColors, translationTable
