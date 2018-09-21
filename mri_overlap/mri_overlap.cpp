ecc7e994834cb0cab2478defc1f60d42addfbece
Andrew Hoopes <athoopes1@gmail.com>
2018-09-21 09:58:55 -0400

    continue working

--- mri_overlap/mri_overlap.cpp
+++ mri_overlap/mri_overlap.cpp
@@ -46,6 +46,77 @@ int main(int argc, const char **argv)
     fs_fatal(1) << "input volumes must have the same number of frames";
   }
 
+  // retrieve any user-specified measure options
+  bool reportDice = false;
+  bool reportJaccard = false;
+  if (parser.exists("measures")) {
+    // report measures specified by the user
+    for(auto const &str : parser.retrieve<std::vector<std::string>>("measures")) {
+      if (str == "dice") reportDice = true;
+      else if (str == "jaccard") reportJaccard = true;
+      else fs_fatal(1) << "unknown measure '" << str << "'. Options are: dice, jaccard, voldiff";
+    }
+  } else {
+    // by default, only report dice scores
+    reportDice = true;
+  }
+
+  // a few sanity checks on the user input
+  if (parser.exists("labelfile") && parser.exists("labels")) {
+    fs_fatal(1) << "can't use both the --labels and --labelfile options together";
+  } else if (parser.exists("names") && !parser.exists("labels")) {
+    fs_fatal(1) << "todo";
+  } else if (parser.exists("seg") && (parser.exists("labelfile") || parser.exists("labels"))) {
+    fs_fatal(1) << "todo";
+  }
+
+  // check if user wants to ignore label names
+  bool reportNames = !parser.exists("no-names");
+
+  // determine which labels to report on
+  std::vector<int> labels;
+  std::vector<std::string> labelnames;
+  if (parser.exists("labels")) {
+    // user specified labels on the command line
+    labels = parser.retrieve<std::vector<int>>("labels");
+    if (reportNames && parser.exists("names")) {
+      // they provided custom label names as well 
+      labelnames = parser.retrieve<std::vector<std::string>>("names");
+      if (labelnames.size() != names.size()) {
+        fs_fatal(1) << "number of label names must match the number of specified labels";
+      }
+    }
+  } else if (parser.exists("labelfile")) {
+    // user specified labels via a file - assume lookup-table format
+    // todo: io read color lut
+    // foreach label in colorlut;
+    //   add to labels
+  } else if (parser.exists("seg")) {
+    // use the major anatomical segmentation structures
+    labels = {
+      17, 53,  // Hippocampus
+      11, 50,  // Caudate
+      12, 51,  // Putamen
+      13, 52,  // Pallidum
+      18, 54,  // Amygdala
+      10, 49,  // Thalamus Proper
+      4,  43,  // Lateral Ventricle
+      14, 15,  // Third and Fourth Ventricles
+      5,  44,  // Inf Lateral Vent
+      2,  41,  // Cerebral White Matter
+      3,  42,  // Cerebral Cortex
+      26, 58   // Accumbens Area
+    }
+  } else {
+    // use all valid labels
+  }
+
+  // determine the appropriate label names if not already known
+  std::vector<std::string> labelnames;
+  if (reportNames && labelnames.empty()) {
+    // todo: read in the primary FreeSurferColorLUT.txt and get label names
+  }
+
   // --- setup ----
 
   struct OverlapMetrics {
@@ -78,7 +149,7 @@ int main(int argc, const char **argv)
     }
   }
 
-  // cleanup
+  // free the volumes
   MRIfree(&seg1);
   MRIfree(&seg2);
 
@@ -89,42 +160,17 @@ int main(int argc, const char **argv)
     overlap.jaccard = (double)overlap.intersect / (double)overlap.total;
   }
 
-  // retrieve any user-specified labels
-  std::vector<int> labels;
-  if (parser.exists("labels")) {
-    labels = parser.retrieve<std::vector<int>>("labels");
-    if (parser.exists("names")) {
-      labelnames = parser.retrieve<std::vector<std::string>>("names");
-      if (labelnames.size() != labels.size()) {
-        fs_fatal(1) << "number of label names must match the number the specified labels";
-      }
-    }
-  }
-
-  // retrieve any user-specified measure options
-  bool reportDice = false;
-  bool reportJaccard = false;
-  if (parser.exists("measures")) {
-    // report measures specified by the user
-    for(auto const &str : parser.retrieve<std::vector<std::string>>("measures")) {
-      if (str == "dice") reportDice = true;
-      else if (str == "jaccard") reportJaccard = true;
-      else fs_fatal(1) << "unknown measure '" << str << "'. Options are: dice, jaccard, voldiff";
-    }
-  } else {
-    // by default, only report dice scores
-    reportDice = true;
-  }
-
   // print results to stdout
   if (!parser.exists("quiet")) {
     std::cout << std::fixed << std::left << std::setprecision(4) << fs::term::bold();
     std::cout << std::setw(7) << "label";
+    if (reportNames) std::cout << std::setw(9) << "names";
     if (reportDice) std::cout << std::setw(9) << "dice";
     if (reportJaccard) std::cout << std::setw(9) << "jaccard";
     std::cout << fs::term::reset() << std::endl;
     for (auto &lm : labelmetrics) {
       std::cout << std::setw(7) << lm.first;
+      if (reportNames) std::cout << std::setw(9) << "names";
       if (reportDice) std::cout << std::setw(9) << lm.second.dice;
       if (reportJaccard) std::cout << std::setw(9) << lm.second.jaccard;
       std::cout << std::endl;
