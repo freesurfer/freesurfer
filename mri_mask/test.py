f5249cc9ab36560f54f45c3693d5b0910c4eddba
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2018-11-02 17:14:46 -0400

    Test transferring edits using inverse GCAM.

--- mri_mask/test.py
+++ mri_mask/test.py
@@ -10,24 +10,24 @@ rt.run('mri_mask nu.1.mgz brainmask.1.mgz mask.mgz')
 rt.mridiff('mask.mgz', 'mask.ref.mgz')
 
 # transform mask using LTA
-rt.run('mri_mask -xform 2_to_1.lta nu.1.mgz brainmask.2.mgz lta.mgz')
-rt.mridiff('lta.mgz', 'lta.ref.mgz')
+rt.run('mri_mask -xform 2_to_1.lta nu.1.mgz brainmask.2.mgz mask.lta.mgz')
+rt.mridiff('mask.lta.mgz', 'mask.lta.ref.mgz')
 
 # transform mask using inverse LTA
-rt.run('mri_mask -xform 1_to_2.lta nu.1.mgz brainmask.2.mgz inv.mgz')
-rt.mridiff('inv.mgz', 'inv.ref.mgz')
+rt.run('mri_mask -xform 1_to_2.lta nu.1.mgz brainmask.2.mgz mask.lta.inv.mgz')
+rt.mridiff('mask.lta.inv.mgz', 'mask.lta.inv.ref.mgz')
 
 # transform mask with FSLMAT
 rt.run(('mri_mask'
         ' -xform 2_to_1.fslmat'
         ' -lta_src nu.2.mgz'
         ' -lta_dst nu.1.mgz'
-        ' nu.1.mgz brainmask.2.mgz fslmat.mgz'))
-rt.mridiff('fslmat.mgz', 'fslmat.ref.mgz')
+        ' nu.1.mgz brainmask.2.mgz mask.fsl.mgz'))
+rt.mridiff('mask.fsl.mgz', 'mask.fsl.ref.mgz')
 
 # transform mask with GCAM
-rt.run('mri_mask -xform 2_to_1.m3z nu.1.mgz brainmask.2.mgz gcam.mgz')
-rt.mridiff('gcam.mgz', 'gcam.ref.mgz')
+rt.run('mri_mask -xform 2_to_1.m3z nu.1.mgz brainmask.2.mgz mask.gcam.mgz')
+rt.mridiff('mask.gcam.mgz', 'mask.gcam.ref.mgz')
 
 # transfer edits
 rt.run(('mri_mask'
@@ -51,6 +51,13 @@ rt.run(('mri_mask'
         ' -keep_mask_deletion_edits'
         ' nu.1.mgz wm.2.mgz edits.gcam.mgz'))
 rt.mridiff('edits.gcam.mgz', 'edits.gcam.ref.mgz')
+
+# transfer 255 only using inverse GCAM
+rt.run(('mri_mask'
+        ' -xform 1_to_2.m3z'
+        ' -transfer 255'
+        ' nu.1.mgz wm.2.mgz edits.gcam.inv.mgz'))
+rt.mridiff('edits.gcam.inv.mgz', 'edits.gcam.inv.ref.mgz')
 
 rt.cleanup()
 
