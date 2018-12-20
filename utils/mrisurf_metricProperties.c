9b40b3d3b7d5fe37a9f8360b26d7606e9b2fc011
Bevin R Brett <bevin_brett@hotmail.com>
2018-12-20 21:43:29 +1300

    Preserve the old behavior for avg_nbrs for now

--- utils/mrisurf_metricProperties.c
+++ utils/mrisurf_metricProperties.c
@@ -4688,10 +4688,10 @@ static void MRISsetNeighborhoodSizeAndDistWkr(MRIS *mris, int nsize, bool always
     sequentially after it.
   */
 
-  bool const neighborsChanged = (nsize > mris->max_nsize);
+  bool const max_nsize_grew = (nsize > mris->max_nsize);
   
   if (nsize <= mris->max_nsize) {
-    cheapAssert(!neighborsChanged);
+    cheapAssert(!max_nsize_grew);
     
     ROMP_PF_begin
     int vno;
@@ -4716,7 +4716,7 @@ static void MRISsetNeighborhoodSizeAndDistWkr(MRIS *mris, int nsize, bool always
     mris->nsize = nsize;
 
   } else {
-    cheapAssert(neighborsChanged);
+    cheapAssert(max_nsize_grew);
 
     alwaysDoDist |= (mris->dist_alloced_flags & 1);
 
@@ -4816,40 +4816,42 @@ static void MRISsetNeighborhoodSizeAndDistWkr(MRIS *mris, int nsize, bool always
 
     mris->max_nsize = nsize;
   }
+  mris->nsize = nsize;
   
   MRIS_check_vertexNeighbours(mris);
 
   // Recalculate the avg_nbrs
   //
-  int ntotal = 0, vtotal = 0;
-  
-  int vno;
-  for (vno = 0; vno < mris->nvertices; vno++) {    
-    VERTEX_TOPOLOGY const * const vt = &mris->vertices_topology[vno];    
-    VERTEX                * const v  = &mris->vertices         [vno];
-    
-    if (v->ripflag) continue;
+  if (max_nsize_grew) {
+    int ntotal = 0, vtotal = 0;
 
-    vtotal += vt->vtotal;
-    ntotal++;
+    int vno;
+    for (vno = 0; vno < mris->nvertices; vno++) {    
+      VERTEX_TOPOLOGY const * const vt = &mris->vertices_topology[vno];    
+      VERTEX                * const v  = &mris->vertices         [vno];
+
+      if (v->ripflag) continue;
+
+      vtotal += vt->vtotal;
+      ntotal++;
+    }
+    mris->avg_nbrs = (float)vtotal / (float)ntotal;
+    if (Gdiag & DIAG_SHOW && mris->nsize > 1 && DIAG_VERBOSE_ON) fprintf(stdout, "avg_nbrs = %2.1f\n", mris->avg_nbrs);
   }
-
-  mris->avg_nbrs = (float)vtotal / (float)ntotal;
-  mris->nsize = nsize;
-  if (Gdiag & DIAG_SHOW && mris->nsize > 1 && DIAG_VERBOSE_ON) fprintf(stdout, "avg_nbrs = %2.1f\n", mris->avg_nbrs);
-
+  
   if (nsize <= mris->dist_nsize) {
-    // FIXED BUG this used to precede the "Recalculate the avg_nbrs"
-    // but that left the avg_nbrs and mris->nsize wrongly set
+    // this used to precede the "Recalculate the avg_nbrs"
+    // but I have shifted it after, and am awaiting a decision from the main developers 
+    // about whether avg_nbrs should be adjusted even when !max_nsize_grew
     return;
   }
   	
   // Adjust the dist and dist_orig
   // unless they are already valid
   //
-  bool newCodeWouldDoDist     = alwaysDoDist || neighborsChanged || mris->dist_nsize < nsize ;
+  bool newCodeWouldDoDist     = alwaysDoDist || max_nsize_grew || mris->dist_nsize < nsize ;
   bool newCodeWouldDoDistOrig = false;
-  if (alwaysDoDist || neighborsChanged || mris->dist_orig_nsize < nsize) {
+  if (alwaysDoDist || max_nsize_grew || mris->dist_orig_nsize < nsize) {
     switch (copeWithLogicProblem("FREESURFER_fix_MRISsetNeighborhoodSizeAndDist","should not be creating dist_origs")) {
     case LogicProblemResponse_old:
       newCodeWouldDoDistOrig = true;
