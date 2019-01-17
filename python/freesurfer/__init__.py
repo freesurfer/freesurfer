512f831052b785a9d1a0a6bd9b2dc53aa469594c
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2019-01-17 09:18:42 -0500

    refactor imports

--- python/freesurfer/__init__.py
+++ python/freesurfer/__init__.py
@@ -6,12 +6,7 @@ from .parser import ArgParser
 # primary utilities
 from .utility import *
 
-from .geometry import *
-from ._surface import *
-from ._normalize import *
-from .freeview import *
-from . import metrics
-
+# c bindings
 try:
     from . import bindings
 except ImportError:
@@ -27,3 +22,9 @@ except ImportError:
 
 from .surface import *
 from .volume import *
+
+from .geometry import *
+from ._surface import *
+from ._normalize import *
+from .freeview import *
+from . import metrics
