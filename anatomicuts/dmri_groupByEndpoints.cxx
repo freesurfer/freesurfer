4689654bc4c560cfca4ba5e7160478c3ff6972cf
fsuser2 <fsuser2>
2019-07-02 14:44:20 -0400

    Adding cxx file

---
+++ anatomicuts/dmri_groupByEndpoints.cxx
@@ -0,0 +1,24 @@
+/* Andrew Zhang
+ * Professor Siless
+ * dmri_groupingByEndpoints.cxx
+ *
+ */
+
+#include <iostream>
+
+#include "GetPot.h"
+
+using namespace std;
+
+int main(int narg, char* arg[]) 
+{
+/*	GetPot c1(narg, const_cast<char**>(arg));
+	GetPot c12(narg, const_cast<char**>(arg)); 
+
+	if (c1.size() == 1 || c1.search(2, "--help", "-h"))
+	{
+		cout << "Usage: " << endl; 
+		cout << arg[0] << " -i anatomicutsFolder -n numClusters -c correspondenceFile -m <numMeasures> -o output" << endl; 
+		return -1; 
+	}*/
+}
