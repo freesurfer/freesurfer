dc7a997914b623c32f762cdabfec8bb960dc269b
fsuser2 <fsuser2>
2019-07-02 14:44:17 -0400

    adding file

---
+++ anatomicuts/dmri_extractSurfaceMeasurements.cxx
@@ -0,0 +1,45 @@
+/* Alexander Zsikla
+ * Professor Siless
+ * August 2019
+ * dmri_extractSurfaceMeasurements.cxx
+ *
+ *
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+//#include "GetPot.h"
+using namespace std;
+
+int main(int narg, char* arg[])
+{
+	
+	return 0;
+}
+
+/*
+ * 1) Why do you have GetPot cl2 when it is only used once (line 65)
+ * 2) Remind me how everything is supposed to be split up (line 72-75)
+ * 3) .h file possibly or just explain how follow, search, and the constructor
+ *    works
+ * 4) 
+ */
+
+
+
+	/*// Input Parsing
+	//GetPot num1(narg, const_cast<char**>(arg));
+	GetPot num2(narg, const_cast<char**>(arg));
+
+	if ((num1.size() <= 3) or (num1.search(2, "--help", "-h")))
+	{
+		cout << "Usage: " << endl
+		     << arg[0] << "-i anatomicutsFolder -n numClusters"
+		     << "-c correspondenceFile -m <numMeasures> <measure1Name>"
+		     << "<measure1> ... <measureName> <measureN> -o output"
+		     << endl;
+
+	//	return -1;
+	//}
+*/
