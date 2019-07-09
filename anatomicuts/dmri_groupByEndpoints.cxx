508316a16edf91b90ccf1092d38957a2a7bf9582
fsuser2 <fsuser2>
2019-07-09 10:23:45 -0400

    Added image processing code

--- anatomicuts/dmri_groupByEndpoints.cxx
+++ anatomicuts/dmri_groupByEndpoints.cxx
@@ -39,16 +39,6 @@ int main(int narg, char* arg[])
 	GetPot c1(narg, const_cast<char**>(arg));
 	GetPot c2(narg, const_cast<char**>(arg)); 
 
-	enum {Dimension =3};
-	typedef int                                                        PixelType;
-	const unsigned int PointDimension = 3;
-	typedef std::vector<int>                  PointDataType;
-	
-	typedef itk::Mesh< PixelType, PointDimension > ColorMeshType;
-	typedef ColorMeshType::PointType PointType;
-	typedef ColorMeshType::CellType        CellType;
-	typedef ColorMeshType::CellAutoPointer CellAutoPointer;
-
 	//Usage error
 	if (c1.size() == 1 || c1.search(2, "--help", "-h"))
 	{
@@ -57,10 +47,77 @@ int main(int narg, char* arg[])
 		return -1; 
 	}
 
+	//More variable definitions
+	enum {Dimension = 3};
+	typedef int                                                        PixelType;
+	const unsigned int PointDimension = 3;
+	typedef vector<int>                  PointDataType;
+	
+	typedef Mesh<PixelType, PointDimension> ColorMeshType; //Error with PixelType with no _
+	typedef ColorMeshType::PointType PointType;
+	typedef ColorMeshType::CellType        CellType;
+	typedef ColorMeshType::CellAutoPointer CellAutoPointer;
+
+	//Image variables
+	//Some variabel names are repeated but have _img added
+	using PixelType_img = unsigned char; 
+	constexpr unsigned int Dimension_img = 2; 
+
+	using ImageType = Image<PixelType_img, Dimension_img>;
+	using PointSetType = PointSet<PixelType_img, Dimension_img>; 
+	using ReaderType = ImageFileReader<ImageType>; 
+
+	ReaderType::Pointer reader = ReaderType::New(); 
+
+	const char* inputFilename = arg[4]; 
+	reader->SetFileName(inputFilename); 
+
+	try
+	{
+		reader->Update(); 
+	}
+	catch(ExceptionObject &err)
+	{
+		cout << "ExceptionObject caught !" << endl; 
+		cout << err << endl; 
+		return EXIT_FAILURE; 
+	}
+
+	PointSetType::Pointer pointSet = PointSetType::New(); 
+
+	using IteratorType = ImageRegionConstIterator<ImageType>; 
+
+	const ImageType* image = reader->GetOutput(); 
+
+	IteratorType it(image, image->GetBufferedRegion());
+
+	it.GoToBegin(); 
+
+	using PointType_img = PointSetType::PointType; 
+	PointType_img point; 
+
+	unsigned long pointId = 0; 
+
 	//Take in information
 	const char *stream_lines = c1.follow("string_file.trk", "-s"); 
-	const char *image = c1.follow("image_file.nii.gz", "-i"); 
+	const char *image_file = c1.follow("image_file.nii.gz", "-i"); 
 	const char *output = c1.follow("output_directory", "-o"); 
+
+	//Image processing function
+	while (!it.IsAtEnd()) 
+	{
+		//Convert the pixel position into a Point
+		image->TransformIndexToPhysicalPoint(it.GetIndex(), point); 
+		pointSet->SetPoint(pointId, point); 
+
+		//Transfer the pixel data to the value associated with the point
+		pointSet->SetPointData(pointId, it.Get()); 
+
+		++it; 
+		++pointId; 
+	}
+
+	cout << "Number of Points = " << pointSet->GetNumberOfPoints() << endl; 
 
 	//Declaration of vectors
 	vector<ColorMeshType::Pointer>* meshes; 
@@ -68,10 +125,9 @@ int main(int narg, char* arg[])
 	vector<vtkSmartPointer<vtkPolyData>> polydatas; 
 
 	//TO DELETE
-	cout << stream_lines << endl << image << endl << output << endl; 
+	//cout << stream_lines << endl << image_file << endl << output << endl; 
 
 	return 0; 	
-
 }
 
 /*
