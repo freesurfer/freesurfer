92d615c3294f1ae38b147e5b5a1f0efe7d604b6f
= <alesandor00@gmail.com>
2019-07-25 13:25:53 -0400

    created a new file

---
+++ anatomicuts/dmri_changeEndpoints.cxx
@@ -0,0 +1,17 @@
+/* Alexander Zsikla
+ * Professor Siless
+ * Summer 2019
+ * dmri_changeEndpoints.cxx
+ *
+ * Changing the endpoint values to a different value
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int narg, char *argv[])
+{
+	//TODO
+	return 0;
+}
