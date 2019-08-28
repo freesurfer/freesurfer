f69c2937a5e933dd3b9530c0bf6ac26ddc11df80
Douglas Greve <greve@nmr.mgh.harvard.edu>
2019-08-28 13:37:02 -0400

    Added MRIScortexLabelDECC()

--- utils/mrisutils.cpp
+++ utils/mrisutils.cpp
@@ -1517,16 +1517,16 @@ LABEL *MRIScortexLabel(MRI_SURFACE *mris, MRI *mri_aseg, int min_vertices)  // B
 
   mri_aseg = MRIcopy(mri_aseg, NULL);  // so we can mess with it
 
-// remove the posterior few mm of the ventricles to prevent
-// them poking into the calcarine
-#define ERASE_MM 3
+  // remove the posterior few mm of the ventricles to prevent
+  // them poking into the calcarine
+  int erase_mm = 3;
   for (l = 0; l < 2; l++) {
     if (l == 0)
       target_label = Left_Lateral_Ventricle;
     else
       target_label = Right_Lateral_Ventricle;
     MRIlabelBoundingBox(mri_aseg, target_label, &box);
-    for (z = box.z + box.dz - (ERASE_MM + 1); z < box.z + box.dz; z++)
+    for (z = box.z + box.dz - (erase_mm + 1); z < box.z + box.dz; z++)
       for (y = 0; y < mri_aseg->height; y++)
         for (x = 0; x < mri_aseg->width; x++) {
           label = (int)MRIgetVoxVal(mri_aseg, x, y, z, 0);
@@ -1688,6 +1688,51 @@ LABEL *MRIScortexLabel(MRI_SURFACE *mris, MRI *mri_aseg, int min_vertices)  // B
   MRIfree(&mri_aseg);  // a locally edited copy, not the original
   return (lcortex);
 }
+
+/*!
+  \fn LABEL *MRIScortexLabelCC(MRIS *mris, MRI *mri_aseg, int min_vertices)
+  \brief Creates a label of cortex that is better defined than that
+  produced by MRIScortexLabel(). It runs MRIScortexLabel() first, then
+  refines it by dilating (D) and eroding (E) then excluding all but
+  the largest connected component (CC). This replaces what is in
+  mris_make_surfaces when ndilate=nerode=4 and min_vertices=-1.
+ */
+LABEL *MRIScortexLabelDECC(MRIS *mris, MRI *mri_aseg, int ndilate, int nerode, int min_vertices)
+{
+  LABEL *lcortex, **labels ;
+  int   n, max_l, max_n, nlabels ;
+  
+  lcortex = MRIScortexLabel(mris, mri_aseg, min_vertices) ;
+  LabelErode(lcortex, mris, nerode) ; // Erode the label by 4
+  LabelDilate(lcortex, mris, ndilate, CURRENT_VERTICES) ;// Dilate the label by 4
+
+  // Create clusters of the labels and take the biggest
+  MRISclearMarks(mris) ;
+  LabelMark(lcortex, mris) ;
+  MRISsegmentMarked(mris, &labels, &nlabels, 1) ; 
+  
+  // Find the label with the most points in it
+  max_n = 0 ;
+  max_l = labels[0]->n_points ;
+  for (n = 1 ; n < nlabels ; n++){
+    if (labels[n]->n_points > max_l){
+      max_l = labels[n]->n_points ;
+      max_n = n ;
+    }
+  }
+  // Unmark the vertices if they are not part of the biggest label 
+  for (n = 0 ; n < nlabels ; n++){
+    if (n != max_n){
+      LabelUnmark(labels[n], mris) ;
+    }
+    LabelFree(&labels[n]) ;
+  }
+  LabelFree(&lcortex) ;
+  
+  // Get the final cortex label
+  lcortex = LabelFromMarkedSurface(mris) ;
+  return(lcortex);
+ }
 
 /*!
   \fn int MRISfindPath ( int *vert_vno, int num_vno, int max_path_length,
