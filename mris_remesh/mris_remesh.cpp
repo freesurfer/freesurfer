d7f063eb15091f9cbec541080968a3aeb56391c6
Douglas Greve <greve@nmr.mgh.harvard.edu>
2019-11-01 13:43:46 -0400

    Now removes intersections and prints quality stats (but currently seg faults whe computing metric properties)

--- mris_remesh/mris_remesh.cpp
+++ mris_remesh/mris_remesh.cpp
@@ -1,10 +1,13 @@
 #include "argparse.h"
 #include "mrisurf.h"
 #include "remesher.h"
+#include "surfgrad.h"
 
 
 int main(int argc, const char **argv) 
 {
+  int nv0;
+
   ArgumentParser parser;
   parser.addArgument("-i", "--input", 1, String, true);
   parser.addArgument("-o", "--output", 1, String, true);
@@ -15,6 +18,7 @@ int main(int argc, const char **argv)
   std::string inputname = parser.retrieve<std::string>("input");
   MRIS *surf = MRISread(inputname.c_str());
   if (!surf) fs::fatal() << "could not read input surface " << inputname;
+  nv0 = surf->nvertices;
 
   int iters = parser.exists("iters") ? parser.retrieve<int>("iters") : 5;
   float length = parser.exists("edge-len") ? parser.retrieve<float>("edge-len") : 0.8;
@@ -23,7 +27,25 @@ int main(int argc, const char **argv)
   remesher.remeshBK(iters, length, false);
   remesher.updateSurface(surf);
 
+  printf("Computing metric properties\n"); fflush(stdout);
+  MRIScomputeMetricProperties(surf);
+
+  printf("Removing intersectios\n");
+  MRISremoveIntersections(surf);
+
+  printf("Remeshed surface quality stats nv0 = %d  nv = %d  %g\n",nv0,surf->nvertices,(double)surf->nvertices/nv0);
+  MRISedges(surf);
+  MRIScorners(surf);
+  MRISfaceMetric(surf,0);
+  MRISedgeMetric(surf,0);
+  MRIScornerMetric(surf,0);
+  MRISprettyPrintSurfQualityStats(stdout, surf);
+
   std::string outputname = parser.retrieve<std::string>("output");
   MRISwrite(surf, outputname.c_str());
+
   MRISfree(&surf);
+  surf = MRISread(outputname.c_str());
+
+  exit(0);
 }
