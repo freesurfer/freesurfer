2803bdbc96f50c6d59f1c167b1986603e097cc3b
Bruce Fischl <fischl@nmr.mgh.harvard.edu>
2020-03-25 09:36:39 -0400

    correct threshold for # of lh/rh voxels adjacent for voxel volume

--- mri_fill/mri_fill.cpp
+++ mri_fill/mri_fill.cpp
@@ -3348,11 +3348,12 @@ main(int argc, char *argv[])
   if (lta && !lhonly && !rhonly)
   {
     int totRH, totLH;
+    double voxvol = mri_fill->xsize * mri_fill->ysize * mri_fill->zsize;
     totRH = 0;
     totLH = 0;
     verifyLRSplit(mri_fill, lta, cc_tal_x, &badRH, &badLH, &totRH, &totLH);
 
-    if ((badRH > 10000) || (badLH > 10000))
+    if ((badRH*voxvol > 10000) || (badLH*voxvol > 10000))
     {
       if (lta)
       {
