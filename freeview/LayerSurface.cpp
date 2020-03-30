2fa8f160b4f8d4a689a2ba1a5a25653541f8fe09
rpwang17 <rpwang@nmr.mgh.harvard.edu>
2020-03-30 17:11:01 -0400

    Disable showing vertices on 2D slice as they are not the true projection of the vertices

--- freeview/LayerSurface.cpp
+++ freeview/LayerSurface.cpp
@@ -806,17 +806,19 @@ void LayerSurface::InitializeActors()
     m_sliceActor3D[i]->GetProperty()->SetLineWidth( line_w*ratio );
     //    m_sliceActor3D[i]->GetProperty()->SetInterpolationToFlat();
 
-    vtkSmartPointer<vtkPolyDataMapper> mapper3 = vtkSmartPointer<vtkPolyDataMapper>::New();
-    vtkSmartPointer<vtkMaskPoints> pts = vtkSmartPointer<vtkMaskPoints>::New();
-    pts->GenerateVerticesOn();
-    pts->SetOnRatio(1);
-    pts->SetInputConnection(m_cutter[i]->GetOutputPort());
-    mapper3->SetInputConnection( pts->GetOutputPort() );
-    mapper3->ScalarVisibilityOff();
-    m_vertexActor2D[i]->SetMapper(mapper3);
-    m_vertexActor2D[i]->SetProperty( m_vertexActor2D[i]->MakeProperty() );
-    m_vertexActor2D[i]->GetProperty()->SetPointSize(3*ratio);
-    m_vertexActor2D[i]->GetProperty()->SetInterpolationToFlat();
+//    disable this part for now
+//
+//    vtkSmartPointer<vtkPolyDataMapper> mapper3 = vtkSmartPointer<vtkPolyDataMapper>::New();
+//    vtkSmartPointer<vtkMaskPoints> pts = vtkSmartPointer<vtkMaskPoints>::New();
+//    pts->GenerateVerticesOn();
+//    pts->SetOnRatio(1);
+//    pts->SetInputConnection(m_cutter[i]->GetOutputPort());
+//    mapper3->SetInputConnection( pts->GetOutputPort() );
+//    mapper3->ScalarVisibilityOff();
+//    m_vertexActor2D[i]->SetMapper(mapper3);
+//    m_vertexActor2D[i]->SetProperty( m_vertexActor2D[i]->MakeProperty() );
+//    m_vertexActor2D[i]->GetProperty()->SetPointSize(3*ratio);
+//    m_vertexActor2D[i]->GetProperty()->SetInterpolationToFlat();
 
     // Set ourselves up.
     this->OnSlicePositionChanged( i );
