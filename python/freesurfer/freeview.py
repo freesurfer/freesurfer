e2d826df6aa9219804bf242ab94eb4ce6616d812
Andrew Hoopes <athoopes1@gmail.com>
2020-05-28 13:57:05 -0400

    nf: forward title param to freeview windows

--- python/freesurfer/freeview.py
+++ python/freesurfer/freeview.py
@@ -132,11 +132,12 @@ class Freeview:
         flag = '-f ' + filename + self._kwargs_to_tags(kwargs)
         self.add_flag(flag)
 
-    def show(self, background=True, opts='', verbose=False, noclean=False, threads=None):
+    def show(self, background=True, title=None, opts='', verbose=False, noclean=False, threads=None):
         '''Opens the configured freeview session.
 
         Args:
             background: Run freeview as a background process. Defaults to True.
+            title: Title for the freeview window. Defaults to None.
             opts: Additional arguments to append to the command.
             verbose: Print the freeview command before running. Defaults to False.
             noclean: Do not remove temporary directory for debugging purposes.
@@ -145,6 +146,9 @@ class Freeview:
 
         # compile the command
         command = '%s freeview %s %s' % (self._vgl_wrapper(), opts, ' '.join(self.flags))
+
+        if title is not None:
+            command += ' -title "%s"' % title.replace('"', '\\"')
 
         # be sure to remove the temporary directory (if it exists) after freeview closes
         if self.tempdir and not noclean:
@@ -337,6 +341,7 @@ def fv(*args, **kwargs):
     Args:
         opts: Additional string of flags to add to the command.
         background: Run freeview as a background process. Defaults to True.
+        kwargs: kwargs are forwarded to the Freeview.show() call.
     '''
     background = kwargs.pop('background', True)
     opts = kwargs.pop('opts', '')
@@ -367,10 +372,10 @@ def fv(*args, **kwargs):
             # assume anything else is a volume
             fv.vol(arg)
 
-    fv.show(background=background, opts=opts)
+    fv.show(background=background, opts=opts, **kwargs)
 
 
-def fvoverlay(surface, overlay, background=True, opts='', verbose=False):
+def fvoverlay(surface, overlay, background=True, opts='', verbose=False, **kwargs):
     '''Freeview wrapper to quickly load an overlay onto a surface.
 
     Args:
@@ -378,7 +383,8 @@ def fvoverlay(surface, overlay, background=True, opts='', verbose=False):
         overlay: An existing volume filename, a numpy array, or a nibabel image to apply as an overlay.
         background: Run freeview as a background process. Defaults to True.
         verbose: Print the freeview command before running. Defaults to False.
+        kwargs: kwargs are forwarded to the Freeview.show() call.
     '''
     fv = Freeview()
     fv.surf(surface, overlay=overlay)
-    fv.show(background=background, opts=opts, verbose=verbose)
+    fv.show(background=background, opts=opts, verbose=verbose, **kwargs)
