35e280bb6cf7fae2e61c8c5bc22efb975a0615e1
Anastasia Yendiki <ayendiki@mgh.harvard.edu>
2020-06-03 01:10:23 -0400

    Updated lookup table with labels for infant tract atlas

--- include/cma.h
+++ include/cma.h
@@ -313,29 +313,43 @@
 #define SUSPICIOUS 999 //                               255 100 100 0
 
 
-// Tracula labeling
+// TRACULA labeling
+// Interhemispheric tracts
 #define fmajor 5100 //                                  204 102 102 0
 #define fminor 5101 //                                  204 102 102 0
-#define lh_atr 5102 //                                  255 255 102 0
-#define lh_cab 5103 //                                  153 204 0   0
-#define lh_ccg 5104 //                                  0   153 153 0
-#define lh_cst 5105 //                                  204 153 255 0
-#define lh_ilf 5106 //                                  255 153 51  0
-#define lh_slfp 5107 //                                 204 204 204 0
-#define lh_slft 5108 //                                 153 255 255 0
-#define lh_unc 5109 //                                  102 153 255 0
-#define rh_atr 5110 //                                  255 255 102 0
-#define rh_cab 5111 //                                  153 204 0   0
-#define rh_ccg 5112 //                                  0   153 153 0
-#define rh_cst 5113 //                                  204 153 255 0
-#define rh_ilf 5114 //                                  255 153 51  0
-#define rh_slfp 5115 //                                 204 204 204 0
-#define rh_slft 5116 //                                 153 255 255 0
-#define rh_unc 5117 //                                  102 153 255 0
-#define lh_ifof 5118 //                                 153 255 255 0
-#define rh_ifof 5119 //                                 153 255 255 0
-#define lh_fornix 5120 //                               204 102 153 0
-#define rh_fornix 5121 //                               204 102 153 0
+#define cc.body 5102 //                                 204 102 102 0
+#define cc.genu 5103 //                                 204 102 102 0
+#define cc.splenium 5104 //                             204 102 102 0
+
+// Left hemi tracts
+#define lh.atr 5200 //                                  255 255 102 0
+#define lh.cab 5201 //                                  153 204 0   0
+#define lh.ccg 5202 //                                  0   153 153 0
+#define lh.cst 5203 //                                  204 153 255 0
+#define lh.ilf 5204 //                                  255 153 51  0
+#define lh.slfp 5205 //                                 204 204 204 0
+#define lh.slft 5206 //                                 153 255 255 0
+#define lh.unc 5207 //                                  102 153 255 0
+#define lh.cb 5208 //                                   0   153 153 0
+#define lh.slf 5209 //                                  204 204 204 0
+#define lh.af 5210 //                                   153 255 255 0
+#define lh.ifof 5211 //                                 153 204 51  0
+#define lh.fx 5212 //                                   204 102 153 0
+
+// Right hemi tracts
+#define rh.atr 5300 //                                  255 255 102 0
+#define rh.cab 5301 //                                  153 204 0   0
+#define rh.ccg 5302 //                                  0   153 153 0
+#define rh.cst 5303 //                                  204 153 255 0
+#define rh.ilf 5304 //                                  255 153 51  0
+#define rh.slfp 5305 //                                 204 204 204 0
+#define rh.slft 5306 //                                 153 255 255 0
+#define rh.unc 5307 //                                  102 153 255 0
+#define rh.cb 5308 //                                   0   153 153 0
+#define rh.slf 5309 //                                  204 204 204 0
+#define rh.af 5310 //                                   153 255 255 0
+#define rh.ifof 5311 //                                 153 204 51  0
+#define rh.fx 5213 //                                   204 102 153 0
 
 
 /*
