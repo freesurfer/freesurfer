c293e49ed626ba8892ea727865f112b8b646383d
Hans Johnson <hans-johnson@uiowa.edu>
2020-07-30 13:45:50 -0500

    COMP: Add comment describing how freesurfer must be compiled
    The language dialects must be carefully managed when building
    with a combination of tools that use mixed cmake
    conformant versions (2.12 for ITK and 3.9 for freesurfer).
    The mechansim for setting the language dialect is different
    in these two cmake versions.

--- packages/source/build_itk.sh
+++ packages/source/build_itk.sh
@@ -16,8 +16,17 @@ cd build
 
 
 
-
-
+# NOTE: ITK 4.13 and uses cmake featues from cmake 2.12
+# this older cmake < 3.3 version does not support
+# respecting the CMAKE_CXX_STANDARD values,
+# and therefore the compiler is self identified
+# as C++14 for gcc 7.5.0+ compilers.
+#
+# If using a compiler that defaults to C++14 or C++17
+# then freesurfer proper will need to be compiled
+# with at least that level of support
+# -DCMAKE_CXX_STANDARD:STRING=14 or 17 as dictated
+# by the compiler.
 cmake ../ITK \
   -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
   -DCMAKE_BUILD_TYPE:STRING=Release \
