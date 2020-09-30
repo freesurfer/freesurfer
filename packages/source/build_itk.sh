2a2fe7150a89990551c0764eb8a1e72f22c233fe
Andrew Hoopes <ahoopes@mgh.harvard.edu>
2020-09-30 15:33:43 -0400

    partial merge of PR #791: ubuntu fixes

--- packages/source/build_itk.sh
+++ packages/source/build_itk.sh
@@ -5,24 +5,42 @@ set -e
 [ "$#" != "1" ] && echo "error: usage: build.sh <prefix>" && exit 1
 INSTALL_DIR="$1"
 
-export CC=$(which gcc)
-export CXX=$(which g++)
+export CC=${CC:-$(which gcc)}
+export CXX=${CXX:-$(which g++)}
+
+export CFLAGS=${CFLAGS:-"-msse2 -mfpmath=sse"}
+export CXXFLAGS=${CXXFLAGS:-"-msse2 -mfpmath=sse"}
 
 mkdir build
 cd build
 
-cmake ../ITK \
-  -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_FLAGS="-msse2 -mfpmath=sse" \
-  -DCMAKE_C_FLAGS="-msse2 -mfpmath=sse" \
-  -DITK_BUILD_DEFAULT_MODULES=OFF \
-  -DITKGroup_Core=ON \
-  -DITKGroup_Filtering=ON \
-  -DITKGroup_Segmentation=ON \
-  -DITKGroup_IO=ON \
-  -DModule_AnisotropicDiffusionLBR=ON \
-  -DBUILD_TESTING=OFF
 
-make -j8
-make install
+
+# NOTE: ITK 4.13 and uses cmake featues from cmake 2.12
+# this older cmake < 3.3 version does not support
+# respecting the CMAKE_CXX_STANDARD values,
+# and therefore the compiler is self identified
+# as C++14 for gcc 7.5.0+ compilers.
+#
+# If using a compiler that defaults to C++14 or C++17
+# then freesurfer proper will need to be compiled
+# with at least that level of support
+# -DCMAKE_CXX_STANDARD:STRING=14 or 17 as dictated
+# by the compiler.
+cmake ../ITK \
+  -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DCMAKE_CXX_FLAGS:STRING="${CXXFLAGS}" \
+  -DCMAKE_C_FLAGS:STRING="${CFLAGS}" \
+  -DCMAKE_CXX_STANDARD:STRING=11 \
+  -DCMAKE_C_STANDARD:STRING=11 \
+  -DITK_BUILD_DEFAULT_MODULES:BOOL=OFF \
+  -DITKGroup_Core:BOOL=ON \
+  -DITKGroup_Filtering:BOOL=ON \
+  -DITKGroup_Segmentation:BOOL=ON \
+  -DITKGroup_IO:BOOL=ON \
+  -DModule_AnisotropicDiffusionLBR:BOOL=ON \
+  -DBUILD_TESTING:BOOL=OFF
+
+cmake --build . --target all -j8
+cmake --build . --target install
