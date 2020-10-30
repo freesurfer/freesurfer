0a392a25c670b6dbee8b6b3c93409eb99786c86a
buildqa <s1152yb68@contbay.com>
2020-10-30 02:23:09 -0400

    another test script update

--- AntsN4BiasFieldCorrectionFs/test.sh
+++ AntsN4BiasFieldCorrectionFs/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/../test.sh"
 
-test_command ../mri_convert/mri_convert -ds 6 6 6 -i T1.mgz -o T1_downsample.mgz && AntsN4BiasFieldCorrectionFs -i T1_downsample.mgz -o T1.out.mgz
+test_command ../../mri_convert/mri_convert -ds 6 6 6 -i T1.mgz -o T1_downsample.mgz && AntsN4BiasFieldCorrectionFs -i T1_downsample.mgz -o T1.out.mgz
 compare_vol T1.ref.mgz T1.out.mgz
 
