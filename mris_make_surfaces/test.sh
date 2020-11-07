283b82e57b2960022f0f4ff9a0578e637bf02829
buildqa <s1152yb68@contbay.com>
2020-11-06 22:08:00 -0500

    update make/place surface testing

--- mris_make_surfaces/test.sh
+++ mris_make_surfaces/test.sh
@@ -6,14 +6,16 @@ if [ "$FSTEST_REGENERATE" != true ]; then
     export OMP_NUM_THREADS=8
 fi
 
+# export MAKE="make -j1"
+
 # make surfaces
 
-# test_command mris_make_surfaces -aseg aseg.presurf -white white.preaparc -noaparc -whiteonly -mgz -T1 brain.finalsurfs subject lh
-# compare_surf subject/surf/lh.white.preaparc subject/surf/lh.white.preaparc.REF
-# 
-# test_command mris_make_surfaces -orig_white white.preaparc -orig_pial white.preaparc -aseg aseg.presurf -mgz -T1 brain.finalsurfs subject lh
-# compare_surf subject/surf/lh.white subject/surf/lh.white.REF
-# compare_surf subject/surf/lh.pial subject/surf/lh.pial.REF
+test_command mris_make_surfaces -aseg aseg.presurf -white white.preaparc -noaparc -whiteonly -mgz -T1 brain.finalsurfs subject lh
+compare_surf subject/surf/lh.white.preaparc subject/surf/lh.white.preaparc.REF
+
+test_command mris_make_surfaces -orig_white white.preaparc -orig_pial white.preaparc -aseg aseg.presurf -mgz -T1 brain.finalsurfs subject lh
+compare_surf subject/surf/lh.white subject/surf/lh.white.REF
+compare_surf subject/surf/lh.pial subject/surf/lh.pial.REF
 
 # place surfaces
 
