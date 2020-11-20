712fb64cf997a85314d87ca5cb4a25f8580d1ac2
Anastasia Yendiki <ayendiki@mgh.harvard.edu>
2020-11-20 16:52:13 -0500

    Added check for a MY_MORPHS_DO_NOT_CONFORM_DEAL_WITH_IT environment variable cuz Doug was like all weird about it

--- mri_vol2vol/mri_vol2vol.cpp
+++ mri_vol2vol/mri_vol2vol.cpp
@@ -2299,7 +2299,12 @@ MRI *MRIvol2volGCAM(MRI *src, LTA *srclta, GCA_MORPH *gcam, LTA *dstlta, MRI *vs
     if(vgdst_dst->width != 256 || vgdst_dst->height != 256 ||
        vgdst_dst->depth != 256 || vgdst_dst->xsize != 1 ||
        vgdst_dst->ysize != 1   || vgdst_dst->zsize != 1)
-      printf("WARN: MRIvol2volGCAM(): neither src nor dst VG of Dest LTA is conformed\n");
+      if(getenv("MY_MORPHS_DO_NOT_CONFORM_DEAL_WITH_IT") == NULL) {
+        printf("ERROR: MRIvol2volGCAM(): neither src nor dst VG of Dest LTA is conformed\n");
+        return(NULL);
+      }
+      else
+        printf("WARN: MRIvol2volGCAM(): neither src nor dst VG of Dest LTA is conformed\n");
     else {
       printf("MRIvol2volGCAM(): Inverting Destination LTA\n");
       LTAinvert(dstlta,dstlta);
