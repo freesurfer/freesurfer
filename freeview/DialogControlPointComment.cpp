5a2477ef7d6bc5ef11b81db02af7ecfb8d73255f
rpwang17 <rpwang@nmr.mgh.harvard.edu>
2021-01-03 21:22:59 -0500

    Added option to edit control point comment and options to select pre-filled comments

---
+++ freeview/DialogControlPointComment.cpp
@@ -0,0 +1,71 @@
+#include "DialogControlPointComment.h"
+#include "ui_DialogControlPointComment.h"
+#include <QMessageBox>
+
+DialogControlPointComment::DialogControlPointComment(QWidget *parent) :
+  QDialog(parent),
+  ui(new Ui::DialogControlPointComment)
+{
+  ui->setupUi(this);
+
+  m_listCheckBoxes << ui->checkBoxMain << ui->checkBoxMain_2 << ui->checkBoxMain_3
+    << ui->checkBoxMain_4 << ui->checkBoxMain_5 << ui->checkBoxMain_6
+    << ui->checkBoxMain_7
+    << ui->checkBoxSolution << ui->checkBoxSolution_2 << ui->checkBoxSolution_3
+    << ui->checkBoxSolution_4
+    << ui->checkBoxProgress << ui->checkBoxProgress_2 << ui->checkBoxProgress_3;
+
+  foreach (QCheckBox* box, m_listCheckBoxes)
+    connect(box, SIGNAL(toggled(bool)), SLOT(OnCheckBoxToggled(bool)), Qt::QueuedConnection);
+}
+
+DialogControlPointComment::~DialogControlPointComment()
+{
+  delete ui;
+}
+
+void DialogControlPointComment::SetLabel(const QString &text)
+{
+  ui->label->setText(text);
+}
+
+void DialogControlPointComment::OnCheckBoxToggled(bool bChecked)
+{
+  QStringList list;
+  foreach (QCheckBox* box, m_listCheckBoxes)
+  {
+    if (box->isChecked())
+      list << box->text();
+  }
+  ui->textEdit->clear();
+  ui->textEdit->appendPlainText(list.join(". ") + ".");
+}
+
+QString DialogControlPointComment::GetComment()
+{
+  return ui->textEdit->toPlainText().trimmed();
+}
+
+void DialogControlPointComment::SetComment(const QString &text)
+{
+  ui->textEdit->clear();
+  ui->textEdit->appendPlainText(text);
+
+  foreach (QCheckBox* box, m_listCheckBoxes)
+  {
+    if (text.contains(box->text(), Qt::CaseInsensitive))
+    {
+      box->blockSignals(true);
+      box->setChecked(true);
+      box->blockSignals(false);
+    }
+  }
+}
+
+void DialogControlPointComment::OnOk()
+{
+  if (GetComment().isEmpty())
+    QMessageBox::warning(this, "Error", "Please enter your comment");
+  else
+    accept();
+}
---
+++ freeview/DialogControlPointComment.h
@@ -0,0 +1,37 @@
+#ifndef DIALOGCONTROLPOINTCOMMENT_H
+#define DIALOGCONTROLPOINTCOMMENT_H
+
+#include <QDialog>
+#include <QList>
+
+namespace Ui {
+class DialogControlPointComment;
+}
+
+class QCheckBox;
+
+class DialogControlPointComment : public QDialog
+{
+  Q_OBJECT
+
+public:
+  explicit DialogControlPointComment(QWidget *parent = nullptr);
+  ~DialogControlPointComment();
+
+  void SetComment(const QString& text);
+
+  void SetLabel(const QString& text);
+
+  QString GetComment();
+
+public slots:
+  void OnCheckBoxToggled(bool bChecked);
+  void OnOk();
+
+private:
+  Ui::DialogControlPointComment *ui;
+
+  QList<QCheckBox*> m_listCheckBoxes;
+};
+
+#endif // DIALOGCONTROLPOINTCOMMENT_H
