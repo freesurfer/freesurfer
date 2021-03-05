e469949a110f8cf93059c3c3e4eff53a12cf96b7
Paul Wighton <paul@corticometrics.com>
2021-03-05 22:41:49 +0000

    [FIX] entrypoint

--- infant/docker/entrypoint.bash
+++ infant/docker/entrypoint.bash
@@ -5,3 +5,5 @@ mkdir -p $FREESURFER_HOME/average/sscnn_skullstripping
 ln -s $FS_INFANT_MODEL/sscnn_skullstrip/cor_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/cor_sscnn.h5
 ln -s $FS_INFANT_MODEL/sscnn_skullstrip/ax_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/ax_sscnn.h5
 ln -s $FS_INFANT_MODEL/sscnn_skullstrip/sag_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/sag_sscnn.h5 
+
+eval "$@"
