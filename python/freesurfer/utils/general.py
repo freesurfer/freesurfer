e7da7199d09456dc9e7eb8d66b01510c1cd4cc87
Andrew Hoopes <athoopes1@gmail.com>
2021-04-08 22:23:04 -0400

    disable tf checking now that it will be distributed

--- python/freesurfer/utils/general.py
+++ python/freesurfer/utils/general.py
@@ -33,27 +33,6 @@ def read_pickle(filename):
     return item
 
 
-def check_tensorflow():
-    '''Ensures that tensorflow is installed in fspython.'''
-    try:
-        import tensorflow
-    except ModuleNotFoundError:
-        # NOTE: The reason we specify h5py below is to deal with a bug in
-        # this version of TF. It shouldn't be needed with newer TF versions
-        error('This tool requires TensorFlow, but fspython does not ship with tensorflow')
-        print('by default. You (or a sys admin) can install the CPU version via:\n')
-        print('  fspython -m pip install h5py==2.10.0 tensorflow==1.13.1\n')
-        print('or the GPU version via:\n')
-        print('  fspython -m pip install h5py==2.10.0 tensorflow-gpu==1.13.1\n')
-        sys.exit(1)
-    except ImportError as err:
-        print(traceback.format_exc())
-        error('Found fspython TensorFlow, but could not import (see error above).')
-        print('If you\'ve install the GPU version, please ensure that the appropriate')
-        print('CUDA libraries are installed on your system and available in your environment.')
-        sys.exit(1)
-
-
 class Timer:
     '''A simple timer class to track process speed.'''
     def __init__(self, message=None):
