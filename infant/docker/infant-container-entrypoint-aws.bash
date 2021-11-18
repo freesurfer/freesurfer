addad3e89e7a0a367c92c759016fcfac3b207978
Paul Wighton <paul.wighton@gmail.com>
2021-11-18 12:05:46 -0500

    [ENH] New env var: SSCNN_MODEL_DIR
    Used to overide the default location of sscnn_skullstrip model files

--- infant/docker/infant-container-entrypoint-aws.bash
+++ infant/docker/infant-container-entrypoint-aws.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: remove `FS_INFANT_MODEL` (no longer needed?)
 echo "==============================================================="
 echo "ENVIRONMENT VARIABLES"
 echo ""
@@ -11,6 +12,7 @@ echo "FREESURFER_HOME:                   $FREESURFER_HOME"
 echo "FS_INFANT_MODEL:                   $FS_INFANT_MODEL"
 echo "SUBJECTS_DIR:                      $SUBJECTS_DIR"
 echo "FS_SUB_NAME:                       $FS_SUB_NAME"
+echo "SSCNN_MODEL_DIR:                   $SSCNN_MODEL_DIR"
 echo "---------------------------------------------------------------"
 echo "FS_NIFTI_INPUT_S3_FILEPATH:        $FS_NIFTI_INPUT_S3_FILEPATH"
 echo "FS_NIFTI_INPUT_LOCAL_FILEPATH:     $FS_NIFTI_INPUT_LOCAL_FILEPATH"
@@ -35,11 +37,12 @@ if [ -n "${FS_NIFTI_INPUT_S3_FILEPATH}" ] && [ -n "${FS_NIFTI_INPUT_LOCAL_FILEPA
 fi
 
 # Symlink the volume_mounted model files to where FreeSurfer expects them
+# PW 2021/11/18 No longer needed, since `SSCNN_MODEL_DIR` can now be used in `sscnn_skullstrip`
 # -----------------------------------------------------------------------
-mkdir -p $FREESURFER_HOME/average/sscnn_skullstripping
-ln -s $FS_INFANT_MODEL/sscnn_skullstrip/cor_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/cor_sscnn.h5
-ln -s $FS_INFANT_MODEL/sscnn_skullstrip/ax_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/ax_sscnn.h5
-ln -s $FS_INFANT_MODEL/sscnn_skullstrip/sag_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/sag_sscnn.h5 
+#mkdir -p $FREESURFER_HOME/average/sscnn_skullstripping
+#ln -s $FS_INFANT_MODEL/sscnn_skullstrip/cor_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/cor_sscnn.h5
+#ln -s $FS_INFANT_MODEL/sscnn_skullstrip/ax_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/ax_sscnn.h5
+#ln -s $FS_INFANT_MODEL/sscnn_skullstrip/sag_sscnn.h5 $FREESURFER_HOME/average/sscnn_skullstripping/sag_sscnn.h5 
 
 eval "$@"
 
