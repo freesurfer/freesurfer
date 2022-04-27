a3dd4bc74ee79066b94d296c7b88c8446c7f3d5d
Yujing Huang <yhuang43@mgh.harvard.edu>
2022-04-27 10:15:22 -0400

    change VentFix::relabelSegANeighboringSegB() to take number of elements in segB as input

--- include/ventfix.h
+++ include/ventfix.h
@@ -11,12 +11,12 @@ class VentFix
 public:
   static MRI *fixasegps(MRI *asegps, MRI *brainmask, char *segids, float threshmin, int niters, int nmax, int topo);
 
-  static MRI *relabelSegANeighboringSegB(MRI *asegps, int segA, int topo, int *segB, int *newsegid, fsPointSet *centroid);
+  static MRI *relabelSegANeighboringSegB(MRI *asegps, int segA, int topo, int *segB, int *newsegid, int numSegs, fsPointSet *centroid);
 
 private:
   static MRI *ExpandSegIndices(MRI *seg, int segid, MRI *mask, int niters, int nmax, int topo, int *nexpansions, fsPointSet &centroid, MRI* newseg);
 
-  static int hasneighbor(MRI* asegps, VOLCLUSTER *vc, int *newsegid, int topo);
+  static int hasneighbor(MRI* asegps, VOLCLUSTER *vc, int *newsegid, int numSegs, int topo);
   static void relabelCluster(MRI *asegps, VOLCLUSTER *vc, int newsegid, MATRIX *vox2ras, fsPointSet *centroid);
 };
 
