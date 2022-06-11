3216ce33cfde43cac4a421e2f5eb0d08bf6e3652
buildqa <buildqa@users.noreply.github.com>
2022-06-10 20:35:09 -0400

    Revert "mri_convert tests" (#1001)

--- mri_convert/test.sh
+++ mri_convert/test.sh
@@ -37,21 +37,3 @@ compare_vol vnav.non-mosaic.mgz vnav.non-mosaic.ref.mgz
 # DICOM with identical geometry - but mosaic'd
 test_command mri_convert --mosaic-fix-noascii vnav.mosaic.dcm vnav.mosaic.mgz
 compare_vol vnav.mosaic.mgz vnav.mosaic.ref.mgz
-
-# -dcm2niix DICOM conversion - kSliceOrientMosaicNegativeDeterminant
-test_command mri_convert -dcm2niix dtitest_Siemens_ccbbi/DTI_sag_002_001_00001.dcm dtinegativemosaic.mgz
-compare_vol dtinegativemosaic.mgz dtitest_Siemens_ccbbi/ref/dtinegativemosaic.ref.mgz
-compare_file dtinegativemosaic.bvals dtitest_Siemens_ccbbi/ref/dtinegativemosaic.ref.bvals
-compare_file dtinegativemosaic.voxel_space.bvecs dtitest_Siemens_ccbbi/ref/dtinegativemosaic.ref.voxel_space.bvecs
-
-# -dcm2niix DICOM conversion - sliceDir < 0 
-test_command mri_convert -dcm2niix enhanced28/34983475 enhanced28.mgz
-compare_vol enhanced28.mgz enhanced28/ref/enhanced28.ref.mgz
-compare_file enhanced28.bvals enhanced28/ref/enhanced28.ref.bvals
-compare_file enhanced28.voxel_space.bvecs enhanced28/ref/enhanced28.ref.voxel_space.bvecs
-
-# -dcm2niix DICOM conversion - dti default case
-test_command mri_convert -dcm2niix enhanced35/34984333 enhanced35.mgz
-compare_vol enhanced35.mgz enhanced35/ref/enhanced35.ref.mgz
-compare_file enhanced35.bvals enhanced35/ref/enhanced35.ref.bvals
-compare_file enhanced35.voxel_space.bvecs enhanced35/ref/enhanced35.ref.voxel_space.bvecs
