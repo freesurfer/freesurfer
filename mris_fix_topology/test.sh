87ec507ffb66e18576984760bd3c0e04af76a9c5
buildqa <s1152yb68@contbay.com>
2022-07-28 22:39:34 -0400

    Allow same changes for building on Ubuntu22 as we did for Ubunntu20 w.r.t compiler warnings and comparing test output

--- mris_fix_topology/test.sh
+++ mris_fix_topology/test.sh
@@ -7,14 +7,14 @@ if [ "$FSTEST_REGENERATE" != true ]; then
 fi
 
 test_command mris_fix_topology -mgz -sphere qsphere.nofix -ga -seed 1234 subj1 lh
-if [[ "$TESTDATA_SUFFIX" != "" ]] && [[ "$host_os" == "ubuntu20" ]] || [[ "$host_os" == "centos8" ]] || [[ "$host_os" == "macos10" ]]; then
+if [[ "$TESTDATA_SUFFIX" != "" ]] && [[ "$host_os" == "ubuntu20" ]] || [[ "$host_os" == "ubuntu22" ]] || [[ "$host_os" == "centos8" ]] || [[ "$host_os" == "macos10" ]]; then
    compare_surf subj1/surf/lh.orig subj1/surf/lh.orig.ref${TESTDATA_SUFFIX}
 else
    compare_surf subj1/surf/lh.orig subj1/surf/lh.orig.ref
 fi
 
 test_command mris_fix_topology -mgz -sphere qsphere.nofix -inflated inflated.nofix -orig orig.nofix -out orig -ga -seed 1234 subj3 rh
-if [[ "$TESTDATA_SUFFIX" != "" ]] && [[ "$host_os" == "ubuntu20" ]] || [[ "$host_os" == "centos8" ]] || [[ "$host_os" == "macos10" ]]; then
+if [[ "$TESTDATA_SUFFIX" != "" ]] && [[ "$host_os" == "ubuntu20" ]] || [[ "$host_os" == "ubuntu22" ]] || [[ "$host_os" == "centos8" ]] || [[ "$host_os" == "macos10" ]]; then
    compare_surf subj3/surf/rh.orig subj3/surf/rh.orig.ref${TESTDATA_SUFFIX}
 else
    compare_surf subj3/surf/rh.orig subj3/surf/rh.orig.ref
