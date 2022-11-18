bf47961e6fd3a9e902fe23aea7d875c7c638a20d
rpwang17 <rpwang@nmr.mgh.harvard.edu>
2022-11-18 15:49:15 -0500

    Hide vertices that are not in patch when show vertices is checked

--- freeview/FSSurface.cpp
+++ freeview/FSSurface.cpp
@@ -900,12 +900,6 @@ void FSSurface::UpdatePolyData( MRIS* mris,
       normal[i] = bInvertNormal?(orig[i] - normal[i]):(normal[i] - orig[i]);
     vtkMath::Normalize(normal);
     newNormals->InsertNextTuple( normal );
-
-    if ( polydata_verts )
-    {
-      vtkIdType n = vno;
-      verts->InsertNextCell( 1, &n );
-    }
   }
 
   // Go through and add the face indices.
@@ -917,6 +911,8 @@ void FSSurface::UpdatePolyData( MRIS* mris,
   }
   vtkIdType face[VERTICES_PER_FACE];
   float dx = 0, dy = 0, dz = 0;
+  unsigned char* vert_mask = new unsigned char[cVertices];
+  memset(vert_mask, 0, cVertices);
   for ( int fno = 0; fno < cFaces; fno++ )
   {
     if ( mris->faces[fno].ripflag == 0 )
@@ -944,8 +940,22 @@ void FSSurface::UpdatePolyData( MRIS* mris,
         dz = qMax(dz, qAbs(mris->vertices[face[1]].z - mris->vertices[face[2]].z));
         dz = qMax(dz, qAbs(mris->vertices[face[0]].z - mris->vertices[face[2]].z));
       }
+      for (int i = 0; i < 3; i++)
+        vert_mask[face[i]] = 1;
     }
   }
+  if ( polydata_verts )
+  {
+    for (int i = 0; i < cVertices; i++)
+    {
+      if (vert_mask[i])
+      {
+        vtkIdType n = i;
+        verts->InsertNextCell( 1, &n );
+      }
+    }
+  }
+  delete[] vert_mask;
   if (create_segs)
   {
     m_dMaxSegmentLength = sqrt(dx*dx+dy*dy+dz*dz);
