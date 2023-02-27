1770b378e9ced04f1ba3391e38a27c59a7506f56
ste93ste <ste.45.ste@gmail.com>
2023-02-27 11:10:17 -0500

    fix imports

--- python/gems/SamsegLongitudinalLesion.py
+++ python/gems/SamsegLongitudinalLesion.py
@@ -1,6 +1,6 @@
 import os
-from .SamsegLesion import SamsegLesion
-from .SamsegLongitudinal import SamsegLongitudinal
+from gems.SamsegLesion import SamsegLesion
+from gems.SamsegLongitudinal import SamsegLongitudinal
 from .SamsegUtility import *
 
 
