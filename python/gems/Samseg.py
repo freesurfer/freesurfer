9d69337db0628dfff8f86a728ccd211712aca63c
Douglas Greve <greve@nmr.mgh.harvard.edu>
2023-06-27 16:15:36 -0400

    Samseg.py. Changed the way it writes the output FS stats table to sort based on index

--- python/gems/Samseg.py
+++ python/gems/Samseg.py
@@ -524,13 +524,26 @@ class Samseg:
         with open(os.path.join(self.savePath, 'samseg.fs.stats'), 'w') as fid:
             fid.write('# Measure EstimatedTotalIntraCranialVol, eTIV, Estimated Total Intracranial Volume, %0.6f, mm^3\n'%(sbtiv));
             # Could add other measures here like total brain volume
-            fid.write('# # ColHeaders  Index SegId NVoxels Volume_mm3 StructName\n');
+            fid.write('# ColHeaders  Index SegId NVoxels Volume_mm3 StructName\n');
             k = 0;
             voxsize = self.voxelSpacing[0]*self.voxelSpacing[1]*self.voxelSpacing[2];
-            # It would be nice to sort this by index instead of compression
+            # Sort them by seg index like with the aseg.stats. Exclude Unknown, but keep WM and Cortex
+            seglist = [];
+            k = 0;
             for volume, name in zip(volumesInCubicMm, names):
-                nvox = round(volume/voxsize,2);
+                if(name == 'Unknown'): 
+                    k = k+1;
+                    continue
                 idx = fslabels[k];
+                seglist.append([idx,volume,name]);
+                k = k+1;
+            seglist.sort()
+            k = 0;
+            for seg in seglist:
+                idx = seg[0];
+                volume = seg[1];
+                name = seg[2];
+                nvox = round(volume/voxsize,2);
                 fid.write('%3d %4d %7d %9.1f %s\n' % (k+1,idx,nvox,volume,name));
                 k = k+1;
 
