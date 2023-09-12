5e2ed846804d385770ba81683fb8d71f5180d40a
rpwang17 <rpwang@nmr.mgh.harvard.edu>
2023-09-12 15:31:50 -0400

    Check existence of files before loading them for command-line -recon option

--- freeview/MainWindow.cpp
+++ freeview/MainWindow.cpp
@@ -2348,10 +2348,12 @@ void MainWindow::CommandLoadSubject(const QStringList &sa)
                          //                         "%1/surf/lh.orig:edgecolor=green:visible=0 "
                          //                         "%1/surf/rh.orig:edgecolor=green:visible=0 "
                          "%1/surf/lh.inflated:annot=aparc:visible=0 "
-                         "%1/surf/rh.inflated:annot=aparc:visible=0 "
-                         "%1/surf/lh.orig.nofix:overlay=%1/surf/lh.defect_labels:edgecolor=overlay:overlay_threshold=0.01,100,percentile:visible=0 "
-                         "%1/surf/rh.orig.nofix:overlay=%1/surf/rh.defect_labels:edgecolor=overlay:overlay_threshold=0.01,100,percentile:visible=0 "
-                         "-viewport coronal ").arg(subject_path);
+                         "%1/surf/rh.inflated:annot=aparc:visible=0 ").arg(subject_path);
+  if (QFile::exists(QString("%1/surf/lh.orig.nofix").arg(subject_path)))
+      args += QString("%1/surf/lh.orig.nofix:overlay=%1/surf/lh.defect_labels:edgecolor=overlay:overlay_threshold=0.01,100,percentile:visible=0 ").arg(subject_path);
+  if (QFile::exists(QString("%1/surf/rh.orig.nofix").arg(subject_path)))
+      args += QString("%1/surf/rh.orig.nofix:overlay=%1/surf/rh.defect_labels:edgecolor=overlay:overlay_threshold=0.01,100,percentile:visible=0 ").arg(subject_path);
+  args +=  "-viewport coronal ";
   QString control_pt_file = QString("%1/tmp/control.dat").arg(subject_path);
   if (QFile::exists(control_pt_file))
     args += "-c " + control_pt_file;
