b4931ee1cf10e751065ff27bfff3a6353b75c2cc
Yujing Huang <yhuang43@mgh.harvard.edu>
2023-11-01 10:27:40 -0400

    fix MacOS openssl compilation/linking error   1. set OpenSSL_INCLUDE_DIR to its parent directory after find_path(), add it to include_directories   2. add -L${OpenSSL_LIB_DIR} when linking openssl

--- cmake/FindOpenSSL.cmake
+++ cmake/FindOpenSSL.cmake
@@ -1,7 +1,7 @@
 # OpenSSL Find Module
 
 if(APPLE)
-   set(SSL_SHARED_LIB libssl.dylib)
+   set(SSL_SHARED_LIB libcrypto.dylib)
    # newer homebrew installations, e.g., darwin_arm64
    set(SSL_INC_1 /opt/homebrew/opt/openssl/include)
    set(SSL_LIB_1 /opt/homebrew/opt/openssl/lib)
@@ -9,7 +9,7 @@ if(APPLE)
    set(SSL_INC_2 /usr/local/opt/openssl/include)
    set(SSL_LIB_2 /usr/local/opt/openssl/lib)
 else()
-   set(SSL_SHARED_LIB libssl.so)
+   set(SSL_SHARED_LIB libcrypto.so)
    # Ubuntu Linux
    set(SSL_LIB_1 /usr/lib/x86_64-linux-gnu)
    set(SSL_INC_1 /usr/include)
@@ -19,7 +19,8 @@ else()
 endif()
 
 # find the include dir - search 1st for newer homebrew installation (which only exists on darwin_arm64 machine)
-find_path(OpenSSL_INCLUDE_DIR HINTS ${SSL_INC_1} ${SSL_INC_2} NAMES conf.h PATH_SUFFIXES openssl)
+find_path(OpenSSL_INCLUDE_DIR HINTS ${SSL_INC_1} ${SSL_INC_2} NAMES openssl/conf.h PATH_SUFFIXES openssl)
+set(OpenSSL_INCLUDE_DIR "${OpenSSL_INCLUDE_DIR}/..")
 
 # find the lib dir - search 1st for Ubuntu path which will not be found on CentOS/RedHat (as the reverse is not true)
 find_path(OpenSSL_LIB_DIR HINTS ${SSL_LIB_1} ${SSL_LIB_2} NAMES ${SSL_SHARED_LIB})
