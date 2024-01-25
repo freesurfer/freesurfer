b42683aad62005c17be4fcb29106052f7a8bb14b
Yujing Huang <yhuang43@mgh.harvard.edu>
2024-01-25 12:36:57 -0500

    ignore spaces when comparing table output from 'mris_anatomical_stats -f'   extra spaces are inserted after structure name in commit 1ba420d

--- mris_anatomical_stats/test.sh
+++ mris_anatomical_stats/test.sh
@@ -5,10 +5,10 @@ test_command mris_anatomical_stats -log lh.stats bert lh
 compare_file lh.stats lh.ref.stats
 
 test_command mris_anatomical_stats -a aparc.annot -f stats.table bert lh
-compare_file stats.table stats.ref.table -I#
+compare_file stats.table stats.ref.table -I# -bw
 
 test_command mris_anatomical_stats -cortex bert/label/lh.cortex.label -crosscheck -a bert/label/lh.aparc.annot -f aparc.stats.table bert lh
-compare_file aparc.stats.table aparc.stats.ref.table -I#
+compare_file aparc.stats.table aparc.stats.ref.table -I# -bw
 
 test_command mris_anatomical_stats -cortex bert/label/lh.cortex.label -crosscheck -a bert/label/lh.aparc.a2009s.annot -f a2009s.stats.table bert lh
-compare_file a2009s.stats.table a2009s.stats.ref.table -I#
+compare_file a2009s.stats.table a2009s.stats.ref.table -I# -bw
