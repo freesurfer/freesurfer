9c48c7b915028a9f43c50e70d180c63865b81e95
Douglas Greve <greve@nmr.mgh.harvard.edu>
2024-03-21 12:36:07 -0400

    mri2.cpp. Continued to tweak VolTopoFix

--- include/mri2.h
+++ include/mri2.h
@@ -170,6 +170,7 @@ int QuadEulerCharChangeTest2(int TurnOnTestVoxel);
 int QuadEulerCharChangeCheckReorder(MRI *mri, const char *testname, int decExpected);
 MRI *MRIvolTopoFix(MRI *binseg0, int onval, MRI *binseg, int ndilationsmax=1000, int nerodesmax=1000, int ntopoerodesmax=1000);
 int MRIvolTopoErodeOne(MRI *binvol, const MRI *keepmask);
+int MRIvolTopoDilateOne(MRI *binvol, int onval=1);
 
 MRI *MRIfindBrightNonWM(MRI *mri_T1, MRI *mri_wm);
 MRI *MRIzconcat(MRI *mri1, MRI *mri2, int nskip, MRI *out);
