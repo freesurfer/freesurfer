a9a3b4e512494ee075532d1135b8219c07f737a5
Yujing Huang <yhuang43@mgh.harvard.edu>
2024-04-04 17:45:47 -0400

    update mri_diff/test.sh   mri_diff now prints the commandline that is running.   update the test to ignore the commandline.

--- mri_diff/test.sh
+++ mri_diff/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/../test.sh"
 
-EXPECT_FAILURE=1 test_command "mri_diff lh.ribbon.mgz rh.ribbon.mgz --ssd --count | tee diff.log"
+EXPECT_FAILURE=1 test_command "mri_diff lh.ribbon.mgz rh.ribbon.mgz --ssd --count | grep -v 'mri_diff' | tee diff.log"
 compare_file diff.log diff_ref.log
