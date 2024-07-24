c3fd7a6e1eb36c65fc622c96e6aae302a07fb4bd
Douglas Greve <greve@nmr.mgh.harvard.edu>
2024-07-24 18:51:27 -0400

    mris_remove_intersection.cpp. Fixed bugs in the notch detection

--- mris_fix_topology/mris_remove_intersection.cpp
+++ mris_fix_topology/mris_remove_intersection.cpp
@@ -45,7 +45,7 @@ static void usage_exit(void) ;
 static void print_usage(void) ;
 static void print_help(void) ;
 static void print_version(void) ;
-int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nmin, double stdthresh, char *psfile, char *ocnfile, MRIS *csurf=NULL);
+int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nmin, int nmax, double stdthresh, char *psfile, char *ocnfile, MRIS *csurf=NULL);
 
 const char *Progname ;
 int FillHoles = 0;
@@ -170,29 +170,30 @@ get_option(int argc, char *argv[])
   }
   else if (!stricmp(option, "notches"))
   {
-    // 2=insurf 3=projdist(1) 4=k1thresh(.05) 5=label(cortex) 6=nmin(3) 7=stdthresh(.2) 8=pointset 9=ocn <10=pial>
+    // 2=insurf 3=projdist(1) 4=k1thresh(.05) 5=label(cortex) 6=nmin(3) 7=nmax 8=stdthresh(.2) 9=pointset 10=ocn <11=pial>
     MRIS *surf = MRISread(argv[2]);
     if(surf==NULL) exit(1);
     double projdistmm, k1thresh, stdthresh;
-    int nmin;
+    int nmin, nmax = -1;
     sscanf(argv[3],"%lf",&projdistmm);
     sscanf(argv[4],"%lf",&k1thresh);
     LABEL *lb = LabelRead(NULL,argv[5]);
     if(!lb) exit(1);
     sscanf(argv[6],"%d",&nmin);
-    sscanf(argv[7],"%lf",&stdthresh);
+    sscanf(argv[7],"%d",&nmax);
+    sscanf(argv[8],"%lf",&stdthresh);
     MRIS *csurf=NULL;
-    if(argc > 10){
-      csurf = MRISread(argv[10]);
+    if(argc > 11){
+      csurf = MRISread(argv[11]);
       if(!csurf) exit(1);
     }
-    int err = GetNotches(surf, projdistmm, k1thresh, lb, nmin, stdthresh, argv[8], argv[9], csurf);
+    int err = GetNotches(surf, projdistmm, k1thresh, lb, nmin, nmax, stdthresh, argv[9], argv[10], csurf);
     if(err) exit(err);
     printf("\n");
     printf("fsvglrun freeview --hide-3d-slices -neuro-view -rotate-around-cursor ");
-    printf("  -f %s:annot=%s ",argv[2],argv[9]);
-    if(argc > 10) printf("  -f %s:annot=%s ",argv[10],argv[9]);
-    printf(" -c %s\n",argv[8]);
+    printf("-f %s:annot=%s ",argv[2],argv[10]);
+    if(argc > 11) printf("-f %s:annot=%s ",argv[11],argv[10]);
+    printf("-c %s\n",argv[9]);
     printf("\n");
     exit(0);
   }
@@ -276,11 +277,11 @@ print_version(void)
    coordinates from csurf (eg, to have the point set on the pial
    instead of the white).
  */
-int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nmin, double stdthresh, char *psfile, char *ocnfile, MRIS *csurf)
+int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nmin, int nmax, double stdthresh, char *psfile, char *ocnfile, MRIS *csurf)
 {
   int FillHoles = 1;
 
-  printf("GetNotches(): %g %g %d %g\n",projdistmm,k1thresh,nmin,stdthresh);
+  printf("GetNotches(): %g %g %d %d %g\n",projdistmm,k1thresh,nmin,nmax,stdthresh);
 
   // This is needed to compute K1
   MRISsetNeighborhoodSizeAndDist(surf,2); // neighborhood size 2 
@@ -389,14 +390,16 @@ int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nm
 
   // Create a point set of those clusters with enough vertices and whose dot stddev meets threshold
   int kClusters = 0; // number of clusters after excluding small and < stdthresh clusters
+  std::vector<int> kcnolist;
   fsPointSet  ps;
   ps.vox2ras = "tkreg";
   for(int cno=0; cno < nClusters; cno++){
     SCS *scs = &(SurfClustSorted[cno]);
     if(scs->nmembers < nmin) continue;
+    if(nmax > 0 && scs->nmembers > nmax) continue;
     if(scs->maxval < stdthresh) continue;
     fsPointSet::Point p = fsPointSet::Point();
-    printf("%3d %3d %4d  %6.4lf %6.4lf  %5.2lf %5.2lf %5.2lf\n",
+    printf("%3d %3d %4d  %6.4lf %6.4lf  %6.2lf %6.2lf %6.2lf\n",
 	   kClusters+1,scs->clusterno,scs->nmembers,scs->maxval,scs->pval_clusterwise,scs->cx,scs->cy,scs->cz);
     p.index = kClusters;
     p.count = scs->nmembers;
@@ -405,6 +408,7 @@ int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nm
     p.y = scs->cy;
     p.z = scs->cz;
     ps.add(p);
+    kcnolist.push_back(scs->clusterno);
     kClusters++;
   }
   // Write the point set file
@@ -420,9 +424,15 @@ int GetNotches(MRIS *surf, double projdistmm, double k1thresh, LABEL *lb, int nm
 	MRIsetVoxVal(mritmp,vtxno,0,0,0,0);
 	continue;
       }
-      int cno = 0; // sorted
-      for(cno=0; cno < kClusters; cno++) if(SurfClustSorted[cno].clusterno == cno0) break;
-      MRIsetVoxVal(mritmp,vtxno,0,0,0,cno+1);
+      int k, ok = 0; // sorted
+      for(k=0; k < kClusters; k++) {
+	if(kcnolist[k] == cno0){
+	  ok = 1;
+	  //printf("%d %d %d\n",vtxno,cno0,k);
+	  break;
+	}
+      }
+      if(ok) MRIsetVoxVal(mritmp,vtxno,0,0,0,k+1);
     }
     mritmp->ct = CTABalloc(kClusters+1);
     CTABunique(mritmp->ct, kClusters+1);
